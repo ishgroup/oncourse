51bfc03a530c17156791bc79d4c54e64693f736d
Marek Wawrzyczny <marek@ish.com.au>
2010-06-01 01:34:38 +0000

    Further changes to schema... WebNodeContent should never have had link to WebNodeType.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@9829 fb2e276d-fae9-0310-b578-b71ac85840b9

--- docs/version_1/schema_changes.sql
+++ docs/version_1/schema_changes.sql
@@ -1,11 +1,7 @@
-﻿USE oncourse_realdata_willow_college;
-
-
-CREATE TABLE `WebNodeContent` (
-  `id` bigint(20) NOT NULL,
+﻿CREATE TABLE `WebNodeContent` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `angelId` bigint(20) DEFAULT NULL,
   `webNodeId` bigint(20) DEFAULT NULL,
-  `webNodeTypeId` bigint(20) DEFAULT NULL,
   `regionKey` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime DEFAULT NULL,
   `isDeleted` int(11) DEFAULT NULL,
@@ -15,7 +11,6 @@ CREATE TABLE `WebNodeContent` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `websiteid_layoutkey_uniq_idx` (`webNodeId`,`regionKey`),
   CONSTRAINT `WebNodeContent_ibfk_1` FOREIGN KEY (`webNodeId`) REFERENCES `WebNode` (`id`),
-  CONSTRAINT `WebNodeContent_ibfk_2` FOREIGN KEY (`webNodeTypeId`) REFERENCES `WebNodeType` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
@@ -31,5 +26,7 @@ CREATE TABLE `WebNodeContentWebBlock` (
 ALTER TABLE `College` ADD (`collegeKey` VARCHAR(25));
 ALTER TABLE `WebSite` ADD (`siteKey` VARCHAR(25));
 ALTER TABLE `WebNodeType` ADD (`webSiteId` BIGINT);
+ALTER TABLE `WebNodeType` ADD (`templateKey` VARCHAR(50));
+ALTER TABLE `WebNodeType` ADD (`numberOfRegions` int(11));
 
 ALTER TABLE `WebNodeType` ADD CONSTRAINT `WebNodeType_ibfk_2` FOREIGN KEY (`webSiteId`) REFERENCES `WebSite` (`id`);
