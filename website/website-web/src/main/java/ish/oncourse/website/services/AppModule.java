990f9f5d26b8da3846ac4f92fd1d5471a0a25fe8
Anton Sakalouski <asakalouski@objectstyle.com>
2010-06-21 08:35:47 +0000

    #5116 Fix ClassNotFoundException
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10004 fb2e276d-fae9-0310-b578-b71ac85840b9

--- website/website-web/src/main/java/ish/oncourse/website/services/AppModule.java
+++ website/website-web/src/main/java/ish/oncourse/website/services/AppModule.java
@@ -6,19 +6,12 @@ import ish.oncourse.ui.services.UIModule;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.annotations.SubModule;
-// TODO: Reinstate when Tapestry-jQuery suports 5.2
-/*
-import org.got5.tapestry5.clientresources.ClientResourcesConstants;
-import org.got5.tapestry5.jquery.JQueryClientResourcesConstants;
-import org.got5.tapestry5.jquery.services.JQueryModule;
-*/
 
 /**
  * The module that is automatically included as part of the Tapestry IoC
  * registry.
  */
-// TODO: Reinstate when Tapestry-jQuery suports 5.2
-//@SubModule( { JQueryModule.class, ServiceModule.class, UIModule.class })
+@SubModule( { ServiceModule.class, UIModule.class /*JQueryModule.class,*/ })
 public class AppModule {
 
 	public static void contributeApplicationDefaults(
