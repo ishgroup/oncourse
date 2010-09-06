1fb2de8a28ad5366ec867aa20f3d1a6f363ddb08
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2010-09-06 15:47:03 +0000

    Task #5558. Added check for the convertation of the WebBlockRegion
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10657 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-ui/src/main/java/ish/oncourse/ui/components/WebBlockDisplay.java
+++ frameworks/common-ui/src/main/java/ish/oncourse/ui/components/WebBlockDisplay.java
@@ -1,11 +1,15 @@
 package ish.oncourse.ui.components;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import ish.oncourse.model.WebBlock;
 import ish.oncourse.services.textile.ITextileConverter;
+import ish.oncourse.services.textile.TextileUtil;
 import ish.oncourse.util.ValidationErrors;
 import ish.oncourse.util.ValidationException;
 
@@ -13,15 +17,20 @@ public class WebBlockDisplay {
 
 	@Inject
 	private ITextileConverter textileConverter;
-	
+
 	@Property
 	@Parameter
 	private WebBlock displayedBlock;
-	
+
 	public String getContent() {
 		ValidationErrors errors = new ValidationErrors();
-		String content = textileConverter.convert(displayedBlock.getContent(), errors);
-		if(errors.hasFailures()){
+		String content = displayedBlock.getContent();
+		Pattern pattern = Pattern.compile(TextileUtil.TEXTILE_REGEXP);
+		Matcher matcher = pattern.matcher(content);
+		if (matcher.find()) {
+			content = textileConverter.convert(content, errors);
+		}
+		if (errors.hasFailures()) {
 			try {
 				throw new ValidationException(errors);
 			} catch (ValidationException e) {
