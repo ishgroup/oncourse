e6ae2ed123155f52654ff718ac508104bbef63be
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2010-09-09 11:36:13 +0000

    Task #5565. Fixed incorrect body class of the main page
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10704 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-ui/src/main/java/ish/oncourse/ui/components/PageWrapper.java
+++ frameworks/common-ui/src/main/java/ish/oncourse/ui/components/PageWrapper.java
@@ -14,26 +14,36 @@ public class PageWrapper {
 	private static final String WEB_NODE_PAGE_NAME = "Page";
 	private static final String MAIN_PAGE_NAME = "Main";
 	private static final String MAIN_PAGE_REAL_NAME = "Index";
-	
+
 	@Inject
 	private ComponentResources componentResources;
 
 	public String getBodyId() {
-		
+
 		String pageName = componentResources.getPageName();
-		
+
 		if (MAIN_PAGE_REAL_NAME.equals(pageName)) {
 			return MAIN_PAGE_NAME;
 		} else if (WEB_NODE_PAGE_NAME.equals(pageName)) {
-			Long nodeNumber = ((WebNodeTemplate) componentResources
-					.getPage().getComponentResources().getEmbeddedComponent(
+			Long nodeNumber = ((WebNodeTemplate) componentResources.getPage()
+					.getComponentResources().getEmbeddedComponent(
 							WEB_NODE_COMPONENT_NAME)).getNode().getId();
 			return pageName.toLowerCase() + nodeNumber;
-		}else if(COURSES_PAGE_REAL_NAME.equals(pageName)){
+		} else if (COURSES_PAGE_REAL_NAME.equals(pageName)) {
 			return COURSES_PAGE_NAME;
 		}
 
 		return pageName;
 
+	}
+
+	public String getBodyClass() {
+		String pageName = componentResources.getPageName();
+
+		if (MAIN_PAGE_REAL_NAME.equals(pageName)) {
+			return "main-page";
+		} else {
+			return "internal-page";
+		}
 	}
 }
