66f9ba17020a97b53a3fb08650ab62711f9bf726
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2010-09-10 15:01:37 +0000

    Task #6607. Fixed some the references to pages names
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10720 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-ui/src/main/java/ish/oncourse/ui/components/PageWrapper.java
+++ frameworks/common-ui/src/main/java/ish/oncourse/ui/components/PageWrapper.java
@@ -9,9 +9,9 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 public class PageWrapper {
 
 	private static final String COURSES_PAGE_NAME = "ListPage";
-	private static final String COURSES_PAGE_REAL_NAME = "Courses";
+	private static final String COURSES_PAGE_REAL_NAME = "ui/Courses";
 	private static final String WEB_NODE_COMPONENT_NAME = "webNodeTemplate";
-	private static final String WEB_NODE_PAGE_NAME = "Page";
+	private static final String WEB_NODE_PAGE_NAME = "ui/Page";
 	private static final String MAIN_PAGE_NAME = "Main";
 	private static final String MAIN_PAGE_REAL_NAME = "Index";
 
@@ -28,7 +28,7 @@ public class PageWrapper {
 			Long nodeNumber = ((WebNodeTemplate) componentResources.getPage()
 					.getComponentResources().getEmbeddedComponent(
 							WEB_NODE_COMPONENT_NAME)).getNode().getId();
-			return pageName.toLowerCase() + nodeNumber;
+			return pageName.toLowerCase().replaceFirst("ui/", "") + nodeNumber;
 		} else if (COURSES_PAGE_REAL_NAME.equals(pageName)) {
 			return COURSES_PAGE_NAME;
 		}
