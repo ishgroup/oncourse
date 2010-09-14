08198e054485b81ab003796b48c313f844c4e6fc
Anton Sakalouski <asakalouski@objectstyle.com>
2010-09-14 18:15:18 +0000

    #6730. Delta updates.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10750 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-services/src/main/java/ish/oncourse/services/search/SearchService.java
+++ frameworks/common-services/src/main/java/ish/oncourse/services/search/SearchService.java
@@ -19,138 +19,145 @@ import java.util.Map;
 
 public class SearchService implements ISearchService {
 
-    private static final Logger logger = Logger.getLogger(SearchService.class);
+	private static final Logger logger = Logger.getLogger(SearchService.class);
 
-    @Inject
-    private IWebSiteService webSiteService;
+	@Inject
+	private IWebSiteService webSiteService;
 
-    @Inject
-    private IPropertyService propertyService;
+	@Inject
+	private IPropertyService propertyService;
 
-    private SolrServer solrServer;
+	private SolrServer solrServer;
 
-    private SolrServer getSolrServer() {
-        if (solrServer == null) {
+	private SolrServer getSolrServer() {
+		if (solrServer == null) {
 
-            try {
-                String solrURL = null;
+			try {
+				String solrURL = null;
 
-                try {
-                    Context ctx = new InitialContext();
-                    solrURL = (String) ctx.lookup("java:comp/env/" + Property.SolrServer.value());
-                    if (logger.isInfoEnabled()) {
-                        logger.info("SolrUrl configured through JNDI to: " + solrURL);
-                    }
-                } catch (NamingException ne) {
-                    logger.warn("SolrUrl not defined by JNDI, falling to secondary config", ne);
-                }
+				try {
+					Context ctx = new InitialContext();
+					solrURL = (String) ctx.lookup("java:comp/env/"
+							+ Property.SolrServer.value());
+					if (logger.isInfoEnabled()) {
+						logger.info("SolrUrl configured through JNDI to: "
+								+ solrURL);
+					}
+				} catch (NamingException ne) {
+					logger.warn(
+							"SolrUrl not defined by JNDI, falling to secondary config",
+							ne);
+				}
 
-                solrURL = (solrURL == null) ? propertyService.string(Property.SolrServer) : solrURL;
-                solrURL = (solrURL == null) ? System.getProperty(Property.SolrServer.value()) : solrURL;
+				solrURL = (solrURL == null) ? propertyService
+						.string(Property.SolrServer) : solrURL;
+				solrURL = (solrURL == null) ? System
+						.getProperty(Property.SolrServer.value()) : solrURL;
 
-                if (solrURL == null) {
-                    throw new IllegalStateException("Undefined property: "
-                            + Property.SolrServer);
-                }
+				if (solrURL == null) {
+					throw new IllegalStateException("Undefined property: "
+							+ Property.SolrServer);
+				}
 
-                CommonsHttpSolrServer httpSolrServer = new CommonsHttpSolrServer(solrURL);
+				CommonsHttpSolrServer httpSolrServer = new CommonsHttpSolrServer(
+						solrURL);
 
-                solrServer = httpSolrServer;
+				solrServer = httpSolrServer;
 
-            } catch (Exception e) {
-                throw new RuntimeException("Unable to connect to solr server.",
-                        e);
-            }
+			} catch (Exception e) {
+				throw new RuntimeException("Unable to connect to solr server.",
+						e);
+			}
 
-        }
-        return solrServer;
-    }
+		}
+		return solrServer;
+	}
 
-    public QueryResponse searchCourses(Map<SearchParam, String> params,
-                                       int start, int rows) {
-        try {
+	public QueryResponse searchCourses(Map<SearchParam, String> params,
+			int start, int rows) {
+		try {
 
-            String collegeId = String.valueOf(webSiteService.getCurrentCollege().getId());
+			String collegeId = String.valueOf(webSiteService
+					.getCurrentCollege().getId());
 
-            SolrQuery q = new SolrQuery();
-            q.setParam("fl", "id, name");
-            q.setStart(start);
-            q.setRows(rows);
-            q.setIncludeScore(true);
+			SolrQuery q = new SolrQuery();
+			q.setParam("fl", "id, name");
+			q.setStart(start);
+			q.setRows(rows);
+			q.setIncludeScore(true);
 
-            q.setParam("fq", String.format("collegeId:%s doctype:course", collegeId));
+			q.setParam("fq",
+					String.format("collegeId:%s doctype:course", collegeId));
 
+			if (params.size() == 1 && params.get(SearchParam.s) != null) {
+				q.setQueryType("dismax");
+				q.setQuery(params.get(SearchParam.s).toLowerCase());
+			} else {
+				StringBuilder qString = new StringBuilder();
+				if (params.containsKey(SearchParam.day)) {
+					String day = params.get(SearchParam.day);
+					qString.append(String.format("when:" + day)).append(" ");
+				}
 
-            if (params.size() == 1 && params.get(SearchParam.s) != null) {
-                q.setQueryType("dismax");
-                q.setQuery(params.get(SearchParam.s).toLowerCase());
-            } else {
-                StringBuilder qString = new StringBuilder();
-                if (params.containsKey(SearchParam.day)) {
-                    String day = params.get(SearchParam.day);
-                    qString.append(
-                            String.format("(dayName:%s || dayType:%s)", day,
-                                    day)).append(" ");
-                }
+				if (params.containsKey(SearchParam.time)) {
+					String time = params.get(SearchParam.time);
+					qString.append("when:" + time).append(" ");
+				}
 
-                if (params.containsKey(SearchParam.time)) {
-                    String time = params.get(SearchParam.time);
-                    qString.append("time:" + time).append(" ");
-                }
+				if (params.containsKey(SearchParam.near)) {
+					String near = params.get(SearchParam.near);
+					double[] points = GeoHashUtils.decode(near);
+					qString.append("{!sfilt fl=course_loc}");
+					q.setParam(
+							"pt",
+							String.valueOf(points[0]) + ","
+									+ String.valueOf(points[1]));
+					q.setParam("d", "10");
+				}
 
-                if (params.containsKey(SearchParam.near)) {
-                    String near = params.get(SearchParam.near);
-                    double[] points = GeoHashUtils.decode(near);
-                    qString.append("{!sfilt fl=course_loc}");
-                    q.setParam(
-                            "pt",
-                            String.valueOf(points[0]) + ","
-                                    + String.valueOf(points[1]));
-                    q.setParam("d", "10");
-                }
+				q.setQuery(qString.toString());
+			}
 
-                q.setQuery(qString.toString());
-            }
+			return getSolrServer().query(q);
+		} catch (Exception e) {
+			logger.error("Failed to search courses.", e);
+			throw new SearchException("Unable to find courses.", e);
+		}
+	}
 
+	public QueryResponse autoSuggest(String term) {
+		try {
 
-            return getSolrServer().query(q);
-        } catch (Exception e) {
-            logger.error("Failed to search courses.", e);
-            throw new SearchException("Unable to find courses.", e);
-        }
-    }
+			College college = webSiteService.getCurrentCollege();
+			String collegeId = String.valueOf(college.getId());
 
-    public QueryResponse autoSuggest(String term) {
-        try {
+			SolrQuery q = new SolrQuery();
+			q.setParam("wt", "javabin");
 
-            College college = webSiteService.getCurrentCollege();
-            String collegeId = String.valueOf(college.getId());
+			StringBuilder query = new StringBuilder();
 
-            SolrQuery q = new SolrQuery();
-            q.setParam("wt", "javabin");
+			String[] terms = term.split("\\s");
+			for (int i = 0; i < terms.length; i++) {
+				String t = terms[i].toLowerCase().trim() + "*";
 
-            StringBuilder query = new StringBuilder();
+				query.append(
+						String.format("(name:%s && collegeId:%s)", t, collegeId))
+						.append("||");
 
-            String[] terms = term.split("\\s");
-            for (int i = 0; i < terms.length; i++) {
-                String t = terms[i].toLowerCase().trim() + "*";
+				query.append(String.format(
+						"(doctype:place suburb:%s postcode:%s) ", t, t));
 
-                query.append(String
-                        .format("(name:%s && collegeId:%s)", t, collegeId)).append("||");
+				if (i + 1 != terms.length) {
+					query.append(" || ");
+				}
+			}
 
-                query.append(String.format("(doctype:place suburb:%s postcode:%s) ", t, t));
+			q.setQuery(query.toString());
 
-                if (i + 1 != terms.length) {
-                    query.append(" || ");
-                }
-            }
-
-            q.setQuery(query.toString());
-
-            return getSolrServer().query(q);
-        } catch (Exception e) {
-            logger.error("Failed to search courses.", e);
-            throw new SearchException("Unable to find courses.", e);
-        }
-    }
+			return getSolrServer().query(q);
+		} catch (Exception e) {
+			logger.error("Failed to search courses.", e);
+			throw new SearchException("Unable to find courses.", e);
+		}
+	}
 }
