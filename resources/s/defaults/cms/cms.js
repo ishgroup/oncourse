7cde9ed6e2348900e48984067f5ac1185958d7a6
Aristedes Maniatis <ari@ish.com.au>
2010-09-17 08:03:13 +0000

    #6788 Add basic stylesheets and javascript for the CMS application
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10767 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ resources/s/defaults/cms/cms.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au
+ * No copying or use of this code is allowed without permission in writing from ish.
+ */
+
+jQuery(document).ready(function() {
+	jQuery(".cms_editor_tabs").tabs();
+});
