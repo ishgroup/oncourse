5cc511b455853ef4bd8edee8e974a1513cd9ee66
Anton Sakalouski <asakalouski@objectstyle.com>
2010-10-12 09:08:51 +0000

    #4951. Some refactoring.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10925 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-ui/src/main/java/ish/oncourse/ui/components/PageWrapper.java
+++ frameworks/common-ui/src/main/java/ish/oncourse/ui/components/PageWrapper.java
@@ -3,6 +3,8 @@ package ish.oncourse.ui.components;
 import ish.oncourse.services.security.IAuthenticationService;
 
 import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 
@@ -12,26 +14,17 @@ import org.apache.tapestry5.services.Request;
 public class PageWrapper {
 
 	private static final String MSIE = "MSIE";
-	private static final String COURSES_PAGE_NAME = "ListPage";
-	private static final String COURSES_PAGE_REAL_NAME = "ui/Courses";
-	private static final String SITES_PAGE_NAME = "SiteList";
-	private static final String SITES_PAGE_REAL_NAME = "ui/Sites";
-	private static final String WEB_NODE_COMPONENT_NAME = "webNodeTemplate";
-	private static final String WEB_NODE_PAGE_NAME = "ui/Page";
-	private static final String MAIN_PAGE_NAME = "Main";
 	private static final String MAIN_PAGE_REAL_NAME = "Index";
-	private static final String DETAILS_PAGE_NAME = "DetailsPage";
-	private static final String COURSE_PAGE_REAL_NAME = "ui/CourseDetails";
-	private static final String COURSE_CLASS_PAGE_REAL_NAME = "ui/CourseClassDetails";
-	private static final String SITE_PAGE_REAL_NAME = "ui/SiteDetails";
-	private static final String ROOM_PAGE_REAL_NAME = "ui/RoomDetails";
-	private static final String TUTOR_PAGE_REAL_NAME = "ui/TutorDetails";
 
 	@Inject
 	private ComponentResources componentResources;
 
 	@Inject
 	private IAuthenticationService authenticationService;
+	
+	@Parameter
+	@Property
+	private String bodyId;
 
 	public boolean isLoggedIn() {
 		return authenticationService.getUser() != null;
@@ -39,34 +32,6 @@ public class PageWrapper {
 
 	@Inject
 	private Request request;
-
-	public String getBodyId() {
-
-		String pageName = componentResources.getPageName();
-
-		if (MAIN_PAGE_REAL_NAME.equals(pageName)) {
-			return MAIN_PAGE_NAME;
-		} else if (WEB_NODE_PAGE_NAME.equals(pageName)) {
-			Long nodeNumber = ((WebNodeTemplate) componentResources.getPage()
-					.getComponentResources()
-					.getEmbeddedComponent(WEB_NODE_COMPONENT_NAME)).getNode()
-					.getId();
-			return pageName.toLowerCase().replaceFirst("ui/", "") + nodeNumber;
-		} else if (COURSES_PAGE_REAL_NAME.equals(pageName)) {
-			return COURSES_PAGE_NAME;
-		} else if (COURSE_PAGE_REAL_NAME.equals(pageName)
-				|| COURSE_CLASS_PAGE_REAL_NAME.equals(pageName)
-				|| SITE_PAGE_REAL_NAME.equals(pageName)
-				|| TUTOR_PAGE_REAL_NAME.equals(pageName)
-				|| ROOM_PAGE_REAL_NAME.equals(pageName)) {
-			return DETAILS_PAGE_NAME;
-		} else if (SITES_PAGE_REAL_NAME.equals(pageName)) {
-			return SITES_PAGE_NAME;
-		}
-
-		return pageName;
-
-	}
 
 	public String getBodyClass() {
 		String pageName = componentResources.getPageName();
