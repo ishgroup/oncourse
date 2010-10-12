f83239409d7e441f2b8b1e1353751b33cad817f1
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2010-10-12 15:33:04 +0000

    Task #7108. New experimental view of the date range component
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@10937 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-ui/src/main/java/ish/oncourse/ui/components/DateRangeDisplay.java
+++ frameworks/common-ui/src/main/java/ish/oncourse/ui/components/DateRangeDisplay.java
@@ -6,12 +6,15 @@ import java.util.Date;
 import java.util.TimeZone;
 
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
 
 public class DateRangeDisplay {
 
 	private static final String doubleDateformat = "EE d MMM";
 	private static final String singleDateformat = "EEEE d MMMM";
 	private static final String yearformat = "yyyy";
+	private static final String machineFormat = "YYYY-MM-DDTHH:MM:SS+ZZ:ZZ";
 
 	@Parameter
 	private Date date1;
@@ -22,9 +25,23 @@ public class DateRangeDisplay {
 	@Parameter
 	private TimeZone timeZone;
 
-	public String getDisplayedRange() {
-		String year = TimestampUtilities.userPresentableDate(date1,
-				this.yearformat, timeZone);
+	@Property
+	private boolean hasFullRange;
+
+	@Property
+	private String humanStartDate;
+
+	@Property
+	private String humanEndDate;
+
+	@SetupRender
+	void beforeRender() {
+		setupDisplayedRange();
+	}
+
+	public void setupDisplayedRange() {
+		String year = TimestampUtilities.userPresentableDate(date1, yearformat,
+				timeZone);
 
 		String startDay = TimestampUtilities.userPresentableDate(date1,
 				"DDD yyyy", timeZone);
@@ -32,27 +49,36 @@ public class DateRangeDisplay {
 				"DDD yyyy", timeZone);
 
 		if (startDay.equals(finishDay)) {
-			return TimestampUtilities.userPresentableDate(date1,
-					this.singleDateformat, timeZone)
+			hasFullRange = false;
+			humanStartDate = TimestampUtilities.userPresentableDate(date1,
+					singleDateformat, timeZone)
 					+ " " + year;
+
+		} else {
+			hasFullRange = true;
+			humanStartDate = TimestampUtilities.userPresentableDate(date1,
+					doubleDateformat, timeZone);
+
+			String year2 = TimestampUtilities.userPresentableDate(date2,
+					yearformat, timeZone);
+
+			if (!year.equals(year2)) {
+				humanStartDate += " " + year;
+			}
+			humanEndDate = TimestampUtilities.userPresentableDate(date2,
+					doubleDateformat, timeZone)
+					+ " " + year2;
 		}
-
-		StringBuffer buff = new StringBuffer();
-		buff.append(
-				TimestampUtilities.userPresentableDate(date1,
-						this.doubleDateformat, timeZone)).append(" ");
-
-		String year2 = TimestampUtilities.userPresentableDate(date2,
-				this.yearformat, timeZone);
-
-		if (!year.equals(year2)) {
-			buff.append(year).append(" ");
-		}
-		buff.append(" &#8211; ").append(
-				TimestampUtilities.userPresentableDate(date2,
-						this.doubleDateformat, timeZone));
-
-		buff.append(" ").append(year2);
-		return buff.toString();
 	}
+
+	public String getMachineStartDate() {
+		return TimestampUtilities.userPresentableDate(date1, machineFormat,
+				timeZone);
+	}
+
+	public String getMachineEndDate() {
+		return TimestampUtilities.userPresentableDate(date2, machineFormat,
+				timeZone);
+	}
+
 }
