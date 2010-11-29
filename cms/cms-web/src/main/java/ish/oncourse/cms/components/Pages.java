0a6d4654533e896e9abbcf99bb18b0c2e352422b
Anton Sakalouski <asakalouski@objectstyle.com>
2010-11-29 10:12:17 +0000

    #4953. Cms refactoring. Significantly simplify layout mechanism. Save-Preview implementation in Edit page.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@11343 fb2e276d-fae9-0310-b578-b71ac85840b9

--- cms/cms-web/src/main/java/ish/oncourse/cms/components/Pages.java
+++ cms/cms-web/src/main/java/ish/oncourse/cms/components/Pages.java
@@ -3,35 +3,61 @@ package ish.oncourse.cms.components;
 import java.io.IOException;
 import java.net.URL;
 
+import org.apache.cayenne.ObjectContext;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 
 import ish.oncourse.model.WebNode;
+import ish.oncourse.model.WebNodeType;
+import ish.oncourse.model.WebSite;
+import ish.oncourse.model.services.persistence.ICayenneService;
 import ish.oncourse.services.node.IWebNodeService;
+import ish.oncourse.services.node.IWebNodeTypeService;
+import ish.oncourse.services.site.IWebSiteService;
 import ish.oncourse.services.visitor.LastEditedVisitor;
 
 public class Pages {
-	
+
 	@Property
 	@Inject
 	private IWebNodeService webNodeService;
 	
 	@Inject
-	private Request request;
+	private ICayenneService cayenneService;
 	
 	@Inject
-	private Messages messages;
+	private IWebSiteService webSiteService;
 	
+	@Inject
+	private IWebNodeTypeService webNodeTypeService;
+
+	@Inject
+	private Request request;
+
+	@Inject
+	private Messages messages;
+
 	@Property
 	private WebNode webNode;
-	
+
 	public Object onActionFromNewPage() throws IOException {
-		WebNode node = webNodeService.createNewPage();
-		return new URL("http://" + request.getServerName() + "/page/" + node.getNodeNumber());
+		
+		ObjectContext ctx = cayenneService.newContext();
+
+		WebNode newPageNode = ctx.newObject(WebNode.class);
+		newPageNode.setName("New Page");
+		newPageNode.setWebSite((WebSite) ctx.localObject(webSiteService.getCurrentWebSite().getObjectId(), null));
+		newPageNode.setNodeNumber(webNodeService.getNextNodeNumber());
+
+		newPageNode.setWebNodeType((WebNodeType) ctx.localObject(webNodeTypeService.getDefaultWebNodeType().getObjectId(), null));
+
+		ctx.commitChanges();
+		
+		return new URL("http://" + request.getServerName() + "/page/" + newPageNode.getNodeNumber());
 	}
-	
+
 	public String getLastEdited() {
 		return webNode.accept(new LastEditedVisitor(messages));
 	}
