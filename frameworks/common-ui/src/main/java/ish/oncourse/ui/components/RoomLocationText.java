f60a104371f1ad38b5eb617cc82ecd52b23dde92
Marek Wawrzyczny <marek@ish.com.au>
2010-12-10 04:35:09 +0000

    #4943 - What if null is passed for the room?
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@11501 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-ui/src/main/java/ish/oncourse/ui/components/RoomLocationText.java
+++ frameworks/common-ui/src/main/java/ish/oncourse/ui/components/RoomLocationText.java
@@ -5,27 +5,29 @@ import ish.oncourse.model.Room;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 
+
 public class RoomLocationText {
 
-	@Parameter(required=true)
+	@Parameter(required = true)
 	@Property
 	private Room room;
-
 	@Parameter
 	@Property
 	private boolean withRoomName;
-
 	@Parameter
 	@Property
 	private boolean withSiteAddress;
 
 	public boolean isHasRoomName() {
-		return room.getName() != null && !"".equals(room.getName());
+		return (room != null) 
+				&& (room.getName() != null)
+				&& ! "".equals(room.getName());
 	}
 
 	public boolean isHasSiteName() {
-		return room.getSite() != null && room.getSite().getName() != null
-				&& !"".equals(room.getSite().getName());
+		return (room != null) 
+				&& (room.getSite() != null)
+				&& (room.getSite().getName() != null)
+				&& ! "".equals(room.getSite().getName());
 	}
-
 }
