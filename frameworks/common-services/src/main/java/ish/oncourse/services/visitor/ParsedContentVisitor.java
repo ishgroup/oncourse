054951ef5f60ce63b088cda07a3f5df192f19cb3
Marek Wawrzyczny <marek@ish.com.au>
2010-12-12 23:45:30 +0000

    #7726 - Made ParsedContentVisitor more robust to bad content - added Logging
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@11519 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-services/src/main/java/ish/oncourse/services/visitor/ParsedContentVisitor.java
+++ frameworks/common-services/src/main/java/ish/oncourse/services/visitor/ParsedContentVisitor.java
@@ -9,38 +9,46 @@ import ish.oncourse.services.textile.ITextileConverter;
 import ish.oncourse.services.textile.TextileUtil;
 import ish.oncourse.util.ValidationErrors;
 import ish.oncourse.util.ValidationException;
+import org.apache.log4j.Logger;
+
 
 public class ParsedContentVisitor extends BaseVisitor<String> {
 
 	private ITextileConverter textileConverter;
+	private static final Logger LOGGER = Logger.getLogger(ParsedContentVisitor.class);
+
 
 	public ParsedContentVisitor(ITextileConverter textileConverter) {
 		this.textileConverter = textileConverter;
 	}
 
 	@Override
+	/**
+	 * Checks if web content contains any textile and converts any textile
+	 * blocks to HTML content.
+	 *
+	 * @param block web content to check
+	 *
+	 * @return parsed web content
+	 */
 	public String visitWebContent(WebContent block) {
-		String text = block.getContent();
+		String text = "";
 
-		Pattern pattern = Pattern.compile(TextileUtil.TEXTILE_REGEXP);
+		if ((block != null) && (block.getContent() != null)) {
+			text = block.getContent();
+			Pattern pattern = Pattern.compile(TextileUtil.TEXTILE_REGEXP);
+			Matcher matcher = pattern.matcher(text);
 
-		Matcher matcher = pattern.matcher(text);
+			if (matcher.find()) {
+				ValidationErrors errors = new ValidationErrors();
+				text = textileConverter.convertCustomTextile(text, errors);
 
-		if (matcher.find()) {
-
-			ValidationErrors errors = new ValidationErrors();
-			text = textileConverter.convertCustomTextile(text, errors);
-
-			if (errors.hasFailures()) {
-				try {
-					throw new ValidationException(errors);
-				} catch (ValidationException e) {
-					e.printStackTrace();
+				if (errors.hasFailures()) {
+					LOGGER.error("Validation errors on Textile cnversion", new ValidationException(errors));
 				}
 			}
 		}
-
 		return text;
 	}
-
+	
 }
