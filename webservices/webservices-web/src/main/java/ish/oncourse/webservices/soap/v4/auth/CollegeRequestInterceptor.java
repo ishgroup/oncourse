e29c30d7b1ea909d19beb935aae35218faa91f05
Anton Sakalouski <asakalouski@objectstyle.com>
2011-01-21 09:32:17 +0000

    #8171. Track IP address, date/time and Angel version of college accessing reference service.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@11850 fb2e276d-fae9-0310-b578-b71ac85840b9

--- webservices/webservices-web/src/main/java/ish/oncourse/webservices/soap/v4/auth/CollegeRequestInterceptor.java
+++ webservices/webservices-web/src/main/java/ish/oncourse/webservices/soap/v4/auth/CollegeRequestInterceptor.java
@@ -1,5 +1,9 @@
 package ish.oncourse.webservices.soap.v4.auth;
 
+import ish.oncourse.model.College;
+import ish.oncourse.services.system.ICollegeService;
+import ish.oncourse.webservices.util.SoapUtil;
+
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +15,17 @@ import org.apache.cxf.phase.Phase;
 import org.apache.cxf.service.model.BindingOperationInfo;
 import org.apache.cxf.transport.http.AbstractHTTPDestination;
 import org.apache.log4j.Logger;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CollegeRequestInterceptor extends AbstractSoapInterceptor {
 
 	private static final Logger LOGGER = Logger.getLogger(CollegeRequestInterceptor.class);
-
+	
+	@Inject
+	@Autowired
+	private ICollegeService collegeService;
+	
 	public CollegeRequestInterceptor() {
 		super(Phase.PRE_INVOKE);
 	}
@@ -28,7 +38,17 @@ public class CollegeRequestInterceptor extends AbstractSoapInterceptor {
 		Date time = new Date();
 		
 		BindingOperationInfo boi = message.getExchange().get(BindingOperationInfo.class);
+		
+		String securityCode = SoapUtil.getHeader(message, SoapUtil.SECURITY_CODE_HEADER);
+		
+		Long version = null;
+		if (securityCode != null) {
+			College college = collegeService.findBySecurityCode(securityCode);
+			if (college != null) {
+				//assing ish version here
+			}
+		}
 
-		LOGGER.info(String.format("Invoke %s from %s at %s.", boi.getName(),  ip, time));
+		LOGGER.info(String.format("Invoke %s from %s with version %s at %s.", boi.getName(),  ip, version, time));
 	}
 }
