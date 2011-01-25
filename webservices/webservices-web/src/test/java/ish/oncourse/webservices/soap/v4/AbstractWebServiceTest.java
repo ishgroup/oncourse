6b437d6332443b01ca3dc0504610322d7ceee88d
Marek Wawrzyczny <marek@ish.com.au>
2011-01-25 00:10:45 +0000

    #7656 - This might disable logging for the Jetty server used in tests...
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@11882 fb2e276d-fae9-0310-b578-b71ac85840b9

--- webservices/webservices-web/src/test/java/ish/oncourse/webservices/soap/v4/AbstractWebServiceTest.java
+++ webservices/webservices-web/src/test/java/ish/oncourse/webservices/soap/v4/AbstractWebServiceTest.java
@@ -21,6 +21,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.eclipse.jetty.plus.jndi.Resource;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -60,6 +61,7 @@ public abstract class AbstractWebServiceTest {
 	@BeforeClass
 	public static void setup() throws Exception {
 
+		Log.setLog(null);
 		server = new Server();
 		server.setStopAtShutdown(true);
 
