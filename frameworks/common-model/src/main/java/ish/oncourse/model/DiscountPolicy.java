460ac594305904bb7194804349967b81bd07728a
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2011-01-27 15:58:20 +0000

    Task #8095. Moved the "chooseBestDiscountsVariant" method to the utils class as it is universal for all the policies.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@11932 fb2e276d-fae9-0310-b578-b71ac85840b9

--- frameworks/common-model/src/main/java/ish/oncourse/model/DiscountPolicy.java
+++ frameworks/common-model/src/main/java/ish/oncourse/model/DiscountPolicy.java
@@ -1,14 +1,9 @@
 package ish.oncourse.model;
 
-import ish.math.Money;
 import ish.oncourse.utils.DiscountUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
-
-import org.apache.cayenne.exp.Expression;
-import org.apache.cayenne.exp.ExpressionFactory;
 
 /**
  * Defines the rules of choosing appropriate discounts. {@see
@@ -48,7 +43,7 @@ public abstract class DiscountPolicy {
 
 		result = getApplicableByPolicy(discounts);
 
-		result = chooseBestDiscountsVariant(result);
+		result = DiscountUtils.chooseBestDiscountsVariant(result);
 
 		return result;
 	}
@@ -62,52 +57,6 @@ public abstract class DiscountPolicy {
 	 */
 	protected abstract List<Discount> getApplicableByPolicy(List<Discount> discounts);
 
-	/**
-	 * Chooses the best option (the max discount value) from the proposed
-	 * discounts : uses the combination of "combinable" discounts, or uses the
-	 * best one of "uncombinable".<br/>
-	 * The processing of combined/notToCombine discounts is based on
-	 * angel/client/ish.oncourse.cayenne.InvoiceLine.updateDiscount().
-	 * 
-	 * @param discounts
-	 * @return
-	 */
-
-	private List<Discount> chooseBestDiscountsVariant(List<Discount> discounts) {
-		Vector<Discount> chosenDiscounts = new Vector<Discount>();
-		if (discounts != null && !discounts.isEmpty()) {
-			// figure out the best deal for the customer.
-			// first try all the discounts which could be combined.
-			Expression exp = ExpressionFactory.matchExp(Discount.COMBINATION_TYPE_PROPERTY,
-					Boolean.TRUE);
-			List<Discount> discountsToCombine = (List<Discount>) exp.filterObjects(discounts);
-			List<Discount> discountsNotToCombine = (List<Discount>) exp.notExp().filterObjects(
-					discounts);
-
-			Money maxDiscount = Money.ZERO;
-			Discount bestDeal = null;
-			Money feeExGst = new Money("100");
-			maxDiscount = DiscountUtils.discountValueForList(discountsToCombine, feeExGst);
-
-			for (Discount d : discountsNotToCombine) {
-				Money val = DiscountUtils.discountValue(d, feeExGst);
-
-				if (val.compareTo(maxDiscount) > 0) {
-					bestDeal = d;
-					maxDiscount = val;
-				}
-			}
-
-			if (bestDeal == null) {
-				// go with combined discounts, remove all not combinable
-				chosenDiscounts.addAll(discountsToCombine);
-			} else {
-				// go with not combined discount
-				chosenDiscounts.add(bestDeal);
-			}
-		}
-		return chosenDiscounts;
-
-	}
+	
 
 }
