cd394d5c865ab66e5a34aa154da6b2ebf78ece32
Olga Tkachova <otkachova@objectstyle.com>
2011-04-28 16:17:08 +0000

    Task #9573 added InvoiceLineUpdater and InvoiceUpdater
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@13081 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ webservices/webservices-web/src/main/java/ish/oncourse/webservices/replication/updaters/InvoiceUpdater.java
@@ -0,0 +1,42 @@
+package ish.oncourse.webservices.replication.updaters;
+
+import ish.common.types.PaymentSource;
+import ish.oncourse.model.Contact;
+import ish.oncourse.model.Invoice;
+import ish.oncourse.model.InvoiceStatus;
+import ish.oncourse.webservices.v4.stubs.replication.InvoiceStub;
+
+public class InvoiceUpdater extends AbstractWillowUpdater<InvoiceStub, Invoice> {
+
+	@Override
+	protected void updateEntity(InvoiceStub stub, Invoice entity,
+			RelationShipCallback callback) {
+		entity.setContact(callback.updateRelationShip(stub.getContactId(),
+				Contact.class));
+		entity.setAmountOwing(stub.getAmountOwing());
+		entity.setBillToAddress(stub.getBillToAddress());
+		entity.setCreated(stub.getCreated());
+		entity.setCustomerPO(stub.getCustomerPO());
+		entity.setCustomerReference(stub.getCustomerReference());
+		entity.setDateDue(stub.getDateDue());
+		entity.setDescription(stub.getDescription());
+		entity.setInvoiceDate(stub.getInvoiceDate());
+		entity.setInvoiceNumber(stub.getInvoiceNumber());
+		entity.setModified(stub.getModified());
+		entity.setPublicNotes(stub.getPublicNotes());
+		entity.setShippingAddress(stub.getShippingAddress());
+
+		entity.setTotalExGst(stub.getTotalExGst());
+
+		if (stub.getStatus() != null) {
+			entity.setStatus(InvoiceStatus.valueOf(stub.getStatus()));
+		}
+
+		if (stub.getSource() != null) {
+			entity.setSource(PaymentSource.valueOf(stub.getSource()));
+		}
+
+		entity.setTotalGst(stub.getTotalGst());
+	}
+
+}
