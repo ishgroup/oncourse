3d774401cc102902e87670c4adaacb2f41d46eb9
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2011-05-05 16:47:32 +0000

    Task #9601. "Edit" and "Delete" page functionality
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@13124 fb2e276d-fae9-0310-b578-b71ac85840b9

--- cms/cms-web/src/main/java/ish/oncourse/cms/components/Pages.java
+++ cms/cms-web/src/main/java/ish/oncourse/cms/components/Pages.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.apache.cayenne.ObjectContext;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
@@ -23,13 +25,13 @@ public class Pages {
 	@Property
 	@Inject
 	private IWebNodeService webNodeService;
-	
+
 	@Inject
 	private ICayenneService cayenneService;
-	
+
 	@Inject
 	private IWebSiteService webSiteService;
-	
+
 	@Inject
 	private IWebNodeTypeService webNodeTypeService;
 
@@ -42,23 +44,38 @@ public class Pages {
 	@Property
 	private WebNode webNode;
 
+	@InjectComponent
+	private Zone pagesListZone;
+
 	public Object onActionFromNewPage() throws IOException {
-		
+
 		ObjectContext ctx = cayenneService.newContext();
 
 		WebNode newPageNode = ctx.newObject(WebNode.class);
 		newPageNode.setName("New Page");
-		newPageNode.setWebSite((WebSite) ctx.localObject(webSiteService.getCurrentWebSite().getObjectId(), null));
+		newPageNode.setWebSite((WebSite) ctx.localObject(webSiteService.getCurrentWebSite()
+				.getObjectId(), null));
 		newPageNode.setNodeNumber(webNodeService.getNextNodeNumber());
 
-		newPageNode.setWebNodeType((WebNodeType) ctx.localObject(webNodeTypeService.getDefaultWebNodeType().getObjectId(), null));
+		newPageNode.setWebNodeType((WebNodeType) ctx.localObject(webNodeTypeService
+				.getDefaultWebNodeType().getObjectId(), null));
 
 		ctx.commitChanges();
-		
+
 		return new URL("http://" + request.getServerName() + "/page/" + newPageNode.getNodeNumber());
 	}
 
 	public String getLastEdited() {
 		return webNode.accept(new LastEditedVisitor(messages));
+	}
+
+	public Object onActionFromDeletePage(Integer nodeNumber) {
+		ObjectContext ctx = cayenneService.newContext();
+		WebNode node = webNodeService.getNodeForNodeNumber(nodeNumber);
+		if (node != null) {
+			ctx.deleteObject(ctx.localObject(node.getObjectId(), null));
+			ctx.commitChanges();
+		}
+		return pagesListZone.getBody();
 	}
 }
