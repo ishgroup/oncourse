6bfd726d8db17deea2e2b66fbfb605525881d46b
Pavel Nikanovich <pavel@ish.com.au>
2011-05-30 16:55:52 +0000

    #9501
    SCWT home page & courses page
    
    
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@13448 fb2e276d-fae9-0310-b578-b71ac85840b9

--- resources/s/scc/js/extra.js
+++ resources/s/scc/js/extra.js
@@ -6,5 +6,14 @@
 // Load All
 jQuery(document).ready(function() {
 
+    jQuery('.internal-page #sidebarRight .courses-list-sub li a').toggle(function() {
+        jQuery('#sidebarRight .courses-list-sub li a.active').click();
+        jQuery(this).addClass('active').siblings('ul').slideDown('slow');
+        return false;
+    }, function(){
+        jQuery(this).removeClass('active').siblings('ul').slideUp('slow');
+        return false;
+    });
+
     jQuery('.content div.image_banners div').cycle({ fx:'fade', speed:1000, pause:0, sync:true, random: 1 });
 });
