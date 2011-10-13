5c5dff289044d21fcbe5d4fb7cf26c95d10e9762
Aristedes Maniatis <ari@ish.com.au>
2011-10-13 06:15:28 +0000

    #0000 Move parent project to new svn layout
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@16348 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ parent/build_all_skip_tests.sh
@@ -0,0 +1,17 @@
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../common/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../asset/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../search/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../web/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../cms/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../enrol/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../portal/trunk/
+mvn clean install -Dmaven.test.skip=true -U
+cd ../../services/trunk/
+mvn clean install -Dmaven.test.skip=true -U
