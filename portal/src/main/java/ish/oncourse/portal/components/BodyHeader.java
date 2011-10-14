57569c788beefbf38f29481f13d8a8fbd70a060e
Anton Sakalouski <asakalouski@objectstyle.com>
2011-10-14 16:36:52 +0000

    #0000. Fix css path because of /portal.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@16421 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/components/BodyHeader.java
+++ portal/src/main/java/ish/oncourse/portal/components/BodyHeader.java
@@ -3,12 +3,21 @@ package ish.oncourse.portal.components;
 import ish.oncourse.portal.access.IAuthenticationService;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
 
 public class BodyHeader {
+	
 	@Inject
 	private IAuthenticationService authenticationService;
+	
+	@Inject
+	private Request request;
 
 	public boolean isUserInSession() {
 		return authenticationService.getUser() != null;
+	}
+	
+	public String getContextPath() {
+		return request.getContextPath();
 	}
 }
--- portal/src/main/java/ish/oncourse/portal/components/PageHeader.java
+++ portal/src/main/java/ish/oncourse/portal/components/PageHeader.java
@@ -1,10 +1,21 @@
 package ish.oncourse.portal.components;
 
+import javax.inject.Inject;
+
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.services.Request;
 
 public class PageHeader {
+	
 	@Property
 	@Parameter
 	private String title;
+	
+	@Inject
+	private Request request;
+	
+	public String getContextPath() {
+		return request.getContextPath();
+	}
 }
