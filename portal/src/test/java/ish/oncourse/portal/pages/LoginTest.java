2d34b29e10202db2cfa3742c706847faf7aa4aac
Olga Tkachova <otkachova@objectstyle.com>
2011-10-31 13:00:22 +0000

    Task #12032 temporary ignore test
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@16821 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/test/java/ish/oncourse/portal/pages/LoginTest.java
+++ portal/src/test/java/ish/oncourse/portal/pages/LoginTest.java
@@ -7,6 +7,7 @@ import org.apache.tapestry5.dom.Document;
 import org.apache.tapestry5.test.PageTester;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class LoginTest {
@@ -15,14 +16,15 @@ public class LoginTest {
 	private static final String appName = "App";
 	
 	@Test
+	@Ignore
 	public void testPageLoaded() {
 		PageTester tester = new PageTester(appPackage, appName, "src/main/webapp", TestModule.class);
 		
 		assertNotNull(tester);
-//		Document doc = tester.renderPage("Login");
-//		assertNotNull(doc.getElementById("email"));
-//		assertNotNull(doc.getElementById("password"));
-//		tester.shutdown();
+		Document doc = tester.renderPage("Login");
+		assertNotNull(doc.getElementById("email"));
+		assertNotNull(doc.getElementById("password"));
+		tester.shutdown();
 	}
 	
 	@Before
