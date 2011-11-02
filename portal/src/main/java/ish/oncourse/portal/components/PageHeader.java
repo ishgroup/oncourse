9bd05fe3b5c394eb9628a4ea5d93b1a9c6204083
Xenia Khailenka <xenia_khailenka@objectstyle.com>
2011-11-02 07:46:00 +0000

    Task #0000. Added build version info to the header
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@16865 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/components/PageHeader.java
+++ portal/src/main/java/ish/oncourse/portal/components/PageHeader.java
@@ -1,5 +1,8 @@
 package ish.oncourse.portal.components;
 
+import ish.oncourse.services.environment.IEnvironmentService;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -14,7 +17,35 @@ public class PageHeader {
 	@Inject
 	private Request request;
 	
+	@Inject
+	private IEnvironmentService environmentService;
+	
 	public String getContextPath() {
 		return request.getContextPath();
+	}
+	
+	public String getMetaGeneratorContent() {
+		StringBuilder buff = new StringBuilder(
+				environmentService.getApplicationName());
+
+		String buildServerID = environmentService.getBuildServerID();
+		if (!StringUtils.isEmpty(buildServerID)) {
+			buff.append(' ').append(buildServerID);
+		}
+
+		String scmVersion = environmentService.getScmVersion();
+		if (!StringUtils.isEmpty(scmVersion)) {
+			buff.append(StringUtils.isEmpty(buildServerID) ? ' ' : '/');
+			buff.append('r');
+			buff.append(scmVersion);
+		}
+
+		String ciVersion = environmentService.getCiVersion();
+		if (!StringUtils.isEmpty(ciVersion)) {
+			buff.append(StringUtils.isEmpty(buildServerID) ? ' ' : '/');
+			buff.append('r');
+			buff.append(ciVersion);
+		}
+		return buff.toString();
 	}
 }
