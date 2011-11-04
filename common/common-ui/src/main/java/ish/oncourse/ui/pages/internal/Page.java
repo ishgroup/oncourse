7d25442af8721375cffc2a3a9c2d7d2ee77d3aed
Olga Tkachova <otkachova@objectstyle.com>
2011-11-04 07:55:19 +0000

    Task #12190 fixed: failure reading parameter 'bodyClass' of component ui/internal/Page:pagestructure:
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@16930 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/Page.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/Page.java
@@ -1,27 +1,56 @@
 package ish.oncourse.ui.pages.internal;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletResponse;
+
+import ish.oncourse.linktransform.PageIdentifier;
 import ish.oncourse.model.WebNode;
+import ish.oncourse.services.courseclass.CourseClassService;
 import ish.oncourse.services.node.IWebNodeService;
 
+import org.apache.log4j.Logger;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.internal.EmptyEventContext;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PageRenderRequestParameters;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.RequestGlobals;
 
 public class Page {
 
 	@Property
 	private WebNode node;
+	
+	@Inject
+	private Request request;
+	
+	@Inject
+	private RequestGlobals requestGlobals;
 
 	@Inject
 	private IWebNodeService webNodeService;
+	
+	private static final Logger logger = Logger.getLogger(Page.class);
 
 	@SetupRender
-	public void beforeRender() {
+	public boolean beforeRender() throws IOException {
 		node = webNodeService.getCurrentNode();
+		if (node == null) {
+			logger.error("CurrentNode is null in " + request.getServerName());
+			
+			HttpServletResponse httpServletResponse = requestGlobals.getHTTPServletResponse();
+			httpServletResponse.setContentType("text/html;charset=UTF-8");
+			httpServletResponse.sendRedirect("http://" + request.getServerName() + "/PageNotFound");
+		}
+		return true;
 	}
-
+	
 	public String getBodyId() {
-		return (isHomePage()) ? "Main" : ("page" + this.node.getNodeNumber());
+		return (isHomePage() || this.node == null) ? "Main" : ("page" + this.node.getNodeNumber());
 	}
 
 	public String getBodyClass() {
@@ -33,6 +62,6 @@ public class Page {
 		if(homePage==null){
 			return false;
 		}
-		return node.getId() != null && node.getId().equals(homePage.getId());
+		return node != null && node.getId() != null && node.getId().equals(homePage.getId());
 	}
 }
