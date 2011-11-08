c00eb684b52cf5836377977174e91765d6ea7de0
Anton Sakalouski <asakalouski@objectstyle.com>
2011-11-08 11:54:36 +0000

    #11737. Invoke NTIS update in separate thread.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@16974 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/pages/NTIS.java
+++ admin/src/main/java/ish/oncourse/admin/pages/NTIS.java
@@ -28,13 +28,13 @@ public class NTIS {
 	private static final String NTIS_DATA_ATTR = "NTISData";
 
 	@Inject
-	private final INTISUpdater ntisUpdater;
+	private INTISUpdater ntisUpdater;
 
 	@Inject
-	private final Request request;
+	private Request request;
 
 	@Inject
-	private final ThreadSource threadSource;
+	private ThreadSource threadSource;
 
 	@Property
 	private String dateFrom;
@@ -45,14 +45,6 @@ public class NTIS {
 	@Property
 	private String ntisResultUrl;
 
-	@Inject
-	public NTIS(INTISUpdater ntisUpdater, Request request, ThreadSource threadSource) {
-		super();
-		this.ntisUpdater = ntisUpdater;
-		this.request = request;
-		this.threadSource = threadSource;
-	}
-
 	@SetupRender
 	void setupRender() {
 		this.ntisResultUrl = request.getContextPath() + "/NTISJson";
@@ -62,69 +54,89 @@ public class NTIS {
 	void submitted() throws Exception {
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		
 		final Date from = dateFormat.parse(dateFrom);
 		final Date to = dateFormat.parse(dateTo);
+		final Session session = request.getSession(false);
+		final INTISUpdater updater = ntisUpdater;
+		
+		threadSource.runInThread(new NTISTask(from, to, session, updater));
+	}
+	
+	private static class NTISTask implements Runnable {
 
-		threadSource.runInThread(new Runnable() {
+		private Date from;
+		private Date to;
+		private Session session;
+		private INTISUpdater ntisUpdater;
+		
+		public NTISTask(Date from, Date to, Session session, INTISUpdater ntisUpdater) {
+			super();
+			this.from = from;
+			this.to = to;
+			this.session = session;
+			this.ntisUpdater = ntisUpdater;
+		}
 
-			@Override
-			public void run() {
-
-				List<String> ntisData = new LinkedList<String>();
-				Session session = request.getSession(false);
-
-				synchronized (session) {
-					session.setAttribute(NTIS_DATA_ATTR, ntisData);
-				}
-
-				Calendar cal = Calendar.getInstance();
-				cal.setTime(from);
-
-				while (cal.getTime().before(to)) {
-
-					Date fromDate = cal.getTime();
-					cal.add(Calendar.MONTH, 1);
-					Date toDate = cal.getTime();
-					if (toDate.after(to)) {
-						toDate = to;
-					}
-
-					LOGGER.debug("Updating records from " + fromDate + " to " + toDate);
-					ntisData.add("Updating records from " + fromDate + " to " + toDate);
-
-					try {
-
-						NTISResult moduleResult = ntisUpdater.doUpdate(fromDate, toDate, Module.class);
-
-						LOGGER.debug("Modules: " + moduleResult.getNumberOfNew() + " new, " + moduleResult.getNumberOfUpdated()
-								+ " updated.");
-						ntisData.add("Modules: " + moduleResult.getNumberOfNew() + " new, " + moduleResult.getNumberOfUpdated()
-								+ " updated.");
-
-						NTISResult qualificationResult = ntisUpdater.doUpdate(fromDate, toDate, Qualification.class);
-
-						LOGGER.debug("Qualifications: " + qualificationResult.getNumberOfNew() + " new, "
-								+ qualificationResult.getNumberOfUpdated() + " updated.");
-
-						ntisData.add("Qualifications: " + qualificationResult.getNumberOfNew() + " new, "
-								+ qualificationResult.getNumberOfUpdated() + " updated.");
-
-						NTISResult trainingPackageResult = ntisUpdater.doUpdate(fromDate, toDate, TrainingPackage.class);
-
-						LOGGER.debug("Training Packages: " + trainingPackageResult.getNumberOfNew() + " new, "
-								+ trainingPackageResult.getNumberOfUpdated() + " updated.");
-
-						ntisData.add("Training Packages: " + trainingPackageResult.getNumberOfNew() + " new, "
-								+ trainingPackageResult.getNumberOfUpdated() + " updated.");
-
-					} catch (Exception e) {
-						LOGGER.error("NTIS update failed with exception.", e);
-						ntisData.add(String.format("NTIS update failed with exception:%s", e.getMessage()));
-					}
-				}
-
-				ntisData.add("Update finished.");
+		/*
+		 * @see java.lang.Runnable#run()
+		 */
+		@Override
+		public void run() {
+			
+			List<String> ntisData = new LinkedList<String>();
+			
+			synchronized (session) {
+				session.setAttribute(NTIS_DATA_ATTR, ntisData);
 			}
-		});
+
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(from);
+
+			while (cal.getTime().before(to)) {
+
+				Date fromDate = cal.getTime();
+				cal.add(Calendar.MONTH, 1);
+				Date toDate = cal.getTime();
+				if (toDate.after(to)) {
+					toDate = to;
+				}
+
+				LOGGER.debug("Updating records from " + fromDate + " to " + toDate);
+				ntisData.add("Updating records from " + fromDate + " to " + toDate);
+
+				try {
+
+					NTISResult moduleResult = ntisUpdater.doUpdate(fromDate, toDate, Module.class);
+
+					LOGGER.debug("Modules: " + moduleResult.getNumberOfNew() + " new, " + moduleResult.getNumberOfUpdated()
+							+ " updated.");
+					ntisData.add("Modules: " + moduleResult.getNumberOfNew() + " new, " + moduleResult.getNumberOfUpdated()
+							+ " updated.");
+
+					NTISResult qualificationResult = ntisUpdater.doUpdate(fromDate, toDate, Qualification.class);
+
+					LOGGER.debug("Qualifications: " + qualificationResult.getNumberOfNew() + " new, "
+							+ qualificationResult.getNumberOfUpdated() + " updated.");
+
+					ntisData.add("Qualifications: " + qualificationResult.getNumberOfNew() + " new, "
+							+ qualificationResult.getNumberOfUpdated() + " updated.");
+
+					NTISResult trainingPackageResult = ntisUpdater.doUpdate(fromDate, toDate, TrainingPackage.class);
+
+					LOGGER.debug("Training Packages: " + trainingPackageResult.getNumberOfNew() + " new, "
+							+ trainingPackageResult.getNumberOfUpdated() + " updated.");
+
+					ntisData.add("Training Packages: " + trainingPackageResult.getNumberOfNew() + " new, "
+							+ trainingPackageResult.getNumberOfUpdated() + " updated.");
+
+				} catch (Exception e) {
+					LOGGER.error("NTIS update failed with exception.", e);
+					ntisData.add(String.format("NTIS update failed with exception:%s", e.getMessage()));
+				}
+			}
+
+			ntisData.add("Update finished.");
+		}
 	}
 }
