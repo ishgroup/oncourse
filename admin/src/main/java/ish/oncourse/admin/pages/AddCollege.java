3eb6b4f54ccc5b5c88f5b09f82a077c9c465e711
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2011-11-21 09:27:09 +0000

    #12250. 'Add College' page improvements.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17133 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/pages/AddCollege.java
+++ admin/src/main/java/ish/oncourse/admin/pages/AddCollege.java
@@ -1,8 +1,19 @@
 package ish.oncourse.admin.pages;
 
+import java.math.BigDecimal;
+
+import ish.oncourse.admin.pages.college.Billing;
+import ish.oncourse.model.College;
+import ish.oncourse.model.LicenseFee;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.services.system.ICollegeService;
+
+import org.apache.cayenne.ObjectContext;
+import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PageRenderLinkSource;
 import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.services.Response;
 
@@ -11,14 +22,63 @@ public class AddCollege {
 	@Property
 	private String findCollegeUrl;
 	
+	@Property
+	private Long collegeId;
+	
+	@Property
+	private String billingCode;
+	
+	@Property
+	private String servicesPassword;
+	
 	@Inject
 	private Request request;
 	
 	@Inject
 	private Response response;
 	
+	@Inject
+	private ICayenneService cayenneService;
+	
+	@Inject
+	private ICollegeService collegeService;
+	
+	@Inject
+	private PageRenderLinkSource prls;
+	
 	@SetupRender
 	void setupRender() {
 		this.findCollegeUrl = response.encodeURL(request.getContextPath() + "/findCollege");
+	}
+	
+	@OnEvent(component="newCollegeForm", value="success")
+	Object newCollege() {
+		ObjectContext context = cayenneService.newNonReplicatingContext();
+		
+		College college = (College) context.localObject(collegeService.findById(collegeId).getObjectId(), null);
+		if (college != null) {
+			college.setBillingCode(billingCode);
+			college.setWebServicesPass(servicesPassword);
+		}
+		
+		createFee(context, college, "sms");
+		createFee(context, college, "cc-office");
+		createFee(context, college, "cc-web");
+		createFee(context, college, "ecommerce");
+		createFee(context, college, "support");
+		createFee(context, college, "hosting");
+		
+		context.commitChanges();
+		
+		return prls.createPageRenderLinkWithContext(Billing.class, collegeId);
+	}
+	
+	private LicenseFee createFee(ObjectContext context, College college, String keyCode) {
+		LicenseFee fee = context.newObject(LicenseFee.class);
+		fee.setCollege(college);
+		fee.setKeyCode(keyCode);
+		fee.setFreeTransactions(0);
+		fee.setFee(new BigDecimal(0));
+		return fee;
 	}
 }
--- admin/src/main/java/ish/oncourse/admin/pages/FindCollege.java
+++ admin/src/main/java/ish/oncourse/admin/pages/FindCollege.java
@@ -5,6 +5,7 @@ import ish.oncourse.services.system.ICollegeService;
 
 import org.apache.tapestry5.StreamResponse;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONObject;
 import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.util.TextStreamResponse;
 
@@ -18,18 +19,26 @@ public class FindCollege {
 	
 	public StreamResponse onActivate() {
 		String serviceKey = request.getParameter("key");
+		JSONObject response = new JSONObject();
 		
 		College college = collegeService.findBySecurityCode(serviceKey);
 		if (college != null) {
 			if (college.getBillingCode() != null) {
-				return new TextStreamResponse("text/html", "College already active.");
+				response.put("status", "ACTIVE");
+				response.put("message", "College already active.");
+				
 			}
 			else {
-				String billingLink = "<a href=\"" + request.getContextPath() + "/college/billing/" + college.getId() + "\">Set up billing.</a>";
-				return new TextStreamResponse("text/html", "Found. " + billingLink);
+				response.put("status", "SETUP");
+				response.put("message", "Found. Please fill following fields, then press Next to proceed to billing setup.");
+				response.put("id", college.getId());
 			}
 		}
+		else {
+			response.put("status", "NOT FOUND");
+			response.put("message", "Could not find key.");
+		}
 		
-		return new TextStreamResponse("text/html", "Could not find key.");
+		return new TextStreamResponse("text/json", response.toString());
 	}
 }
