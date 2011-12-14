d31ad69987755fbbcdb9a277f2f3fc4cd1e16cfd
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2011-12-14 07:53:47 +0000

    task #4384. Change tag updater.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17548 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/replication/updaters/TagUpdater.java
+++ services/src/main/java/ish/oncourse/webservices/replication/updaters/TagUpdater.java
@@ -1,5 +1,7 @@
 package ish.oncourse.webservices.replication.updaters;
 
+import org.apache.commons.lang.StringUtils;
+
 import ish.common.types.NodeSpecialType;
 import ish.common.types.TypesUtil;
 import ish.oncourse.model.Tag;
@@ -10,32 +12,28 @@ public class TagUpdater extends AbstractWillowUpdater<TagStub, Tag>{
 	
 	private ITextileConverter textileConverter;
 
-	public TagUpdater(ITextileConverter textileConverter) {
+	public TagUpdater(final ITextileConverter textileConverter) {
 		this.textileConverter = textileConverter;
 	}
 
 	@Override
-	protected void updateEntity(TagStub stub, Tag entity, RelationShipCallback callback) {
+	protected void updateEntity(final TagStub stub, final Tag entity, final RelationShipCallback callback) {
 		entity.setCreated(stub.getCreated());
-		if(stub.getDetailTextile()!=null){
+		if (StringUtils.trimToNull(stub.getDetailTextile()) != null) {
 			entity.setDetail(textileConverter.convertCoreTextile(stub.getDetailTextile()));
 		}
 		entity.setDetailTextile(stub.getDetailTextile());
-		entity.setIsTagGroup(stub.isTagGroup());
-		entity.setIsWebVisible(stub.isWebVisible());
+		entity.setIsTagGroup(Boolean.TRUE.equals(stub.isTagGroup()));
+		entity.setIsWebVisible(Boolean.TRUE.equals(stub.isWebVisible()));
 		entity.setModified(stub.getModified());
 		entity.setName(stub.getName());
 		entity.setNodeType(stub.getNodeType());
-		
 		if (stub.getSpecialType() != null) {
 			entity.setSpecialType(TypesUtil.getEnumForDatabaseValue(stub.getSpecialType(), NodeSpecialType.class));
 		}
-		
-		
 		if (stub.getParentId() != null) {
 			entity.setParent(callback.updateRelationShip(stub.getParentId(), Tag.class));
 		}
-		
 		entity.setShortName(stub.getShortName());
 		entity.setWeighting(stub.getWeighting());
 	}
