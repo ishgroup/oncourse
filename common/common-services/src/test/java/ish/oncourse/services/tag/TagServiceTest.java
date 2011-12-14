83c00854ef1622d79a813f70db6f4505b261bd91
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2011-12-14 10:50:52 +0000

    task #4384. Update unit test for changed tag service logic
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17552 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/test/java/ish/oncourse/services/tag/TagServiceTest.java
+++ common/common-services/src/test/java/ish/oncourse/services/tag/TagServiceTest.java
@@ -3,16 +3,13 @@ package ish.oncourse.services.tag;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Date;
-
 import ish.oncourse.model.College;
 import ish.oncourse.model.Tag;
 import ish.oncourse.services.persistence.ICayenneService;
 import ish.oncourse.services.site.IWebSiteService;
 import ish.oncourse.test.ContextUtils;
-
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
-
 import org.apache.cayenne.ObjectContext;
 import org.apache.log4j.Logger;
 import org.junit.Before;
@@ -26,11 +23,18 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class TagServiceTest {
 
+private static final String LEFT_SLASH_CHARACTER = "/";
+
+private static final String UTF_8_ENCODING = "UTF-8";
+
 private static final Logger LOGGER = Logger.getLogger(TagServiceTest.class);
 
 	private static final String TAG_NAME = "tag";
+	private static final String TAG_NAME_2 = "tag2";
 	
 	private static final String TAG_GROUP_NAME = "tag group";
+	private static final String TAG_GROUP_NAME_2 = "tag group2";
+	
 	
 	private ITagService tagService;
 	
@@ -43,7 +47,7 @@ private static final Logger LOGGER = Logger.getLogger(TagServiceTest.class);
 	
 	@Mock
 	private ICayenneService cayenneService;
-
+	
 	@BeforeClass
 	public static void initTest() throws Exception {
 		LOGGER.info("init data for TagServiceTest");
@@ -53,25 +57,51 @@ private static final Logger LOGGER = Logger.getLogger(TagServiceTest.class);
 		college.setName("testCollege");
 		college.setTimeZone("Australia/Sydney");
 		college.setFirstRemoteAuthentication(new Date());
-		Tag subjects = context.newObject(Tag.class);
+		final Tag subjects = context.newObject(Tag.class);
 		subjects.setName(Tag.SUBJECTS_TAG_NAME);
 		subjects.setCollege(college);
 		subjects.setIsTagGroup(true);
 		subjects.setIsWebVisible(true);
-
-		Tag tagGroup = context.newObject(Tag.class);
+		
+		final Tag tagGroup = context.newObject(Tag.class);
 		tagGroup.setName(TAG_GROUP_NAME);
 		tagGroup.setCollege(college);
 		tagGroup.setIsTagGroup(true);
 		tagGroup.setIsWebVisible(true);
 		tagGroup.setParent(subjects);
-
-		Tag tag = context.newObject(Tag.class);
+		
+		final Tag alternativeTagGroup = context.newObject(Tag.class);
+		alternativeTagGroup.setName(TAG_GROUP_NAME);
+		alternativeTagGroup.setCollege(college);
+		alternativeTagGroup.setIsTagGroup(true);
+		alternativeTagGroup.setIsWebVisible(true);
+		
+		final Tag alternativeTagGroup2 = context.newObject(Tag.class);
+		alternativeTagGroup2.setName(TAG_GROUP_NAME_2);
+		alternativeTagGroup2.setCollege(college);
+		alternativeTagGroup2.setIsTagGroup(true);
+		alternativeTagGroup2.setIsWebVisible(true);
+		
+		final Tag tag = context.newObject(Tag.class);
 		tag.setName(TAG_NAME);
 		tag.setCollege(college);
 		tag.setIsTagGroup(false);
 		tag.setIsWebVisible(true);
 		tag.setParent(tagGroup);
+		
+		final Tag alternativeTagLeaf = context.newObject(Tag.class);
+		alternativeTagLeaf.setName(TAG_NAME);
+		alternativeTagLeaf.setCollege(college);
+		alternativeTagLeaf.setIsTagGroup(false);
+		alternativeTagLeaf.setIsWebVisible(true);
+		alternativeTagLeaf.setParent(alternativeTagGroup);
+		
+		final Tag alternativeTagLeaf2 = context.newObject(Tag.class);
+		alternativeTagLeaf2.setName(TAG_NAME_2);
+		alternativeTagLeaf2.setCollege(college);
+		alternativeTagLeaf2.setIsTagGroup(false);
+		alternativeTagLeaf2.setIsWebVisible(true);
+		alternativeTagLeaf2.setParent(alternativeTagGroup2);
 
 		context.commitChanges();
 	}
@@ -89,6 +119,7 @@ private static final Logger LOGGER = Logger.getLogger(TagServiceTest.class);
 		Tag tag = tagService.getSubjectsTag();
 		assertNotNull(tag);
 		assertEquals(Tag.SUBJECTS_TAG_NAME, tag.getName());
+		assertNull(tag.getParent());
 	}
 
 	/**
@@ -106,17 +137,37 @@ private static final Logger LOGGER = Logger.getLogger(TagServiceTest.class);
 	public void getTagByFullPathTest() throws UnsupportedEncodingException {
 		LOGGER.info("check getTagByFullPath() method");
 		//try to find by encoded and not encoded names
-		String encodedTagName = URLEncoder.encode(TAG_GROUP_NAME, "UTF-8");
-		Tag tag = tagService.getTagByFullPath(Tag.SUBJECTS_TAG_NAME + "/" + encodedTagName + "/" + TAG_NAME);
+		Tag tag = tagService.getTagByFullPath(Tag.SUBJECTS_TAG_NAME + LEFT_SLASH_CHARACTER + URLEncoder.encode(TAG_GROUP_NAME, UTF_8_ENCODING) + LEFT_SLASH_CHARACTER + 
+			TAG_NAME);
 		assertNotNull(tag);
 		assertEquals(TAG_NAME, tag.getName());
-		tag = tagService.getTagByFullPath(TAG_GROUP_NAME + "/" + TAG_NAME);
+		assertNotNull(tag.getParent());
+		assertNotNull(tag.getParent().getParent());
+		assertNull(tag.getParent().getParent().getParent());//here we should receive Subjects null parent
+		tag = tagService.getTagByFullPath(TAG_GROUP_NAME + LEFT_SLASH_CHARACTER + TAG_NAME);
 		assertNotNull(tag);
 		assertEquals(TAG_NAME, tag.getName());
+		assertNotNull(tag.getParent());
+		assertNotNull(tag.getParent().getParent());//here we should receive Subjects as parent because alternativeTagGroup should be hidden
+		assertNull(tag.getParent().getParent().getParent());//here we should receive Subjects null parent 
 		tag = tagService.getTagByFullPath(TAG_GROUP_NAME);
 		assertNotNull(tag);
 		assertEquals(TAG_GROUP_NAME, tag.getName());
+		assertNotNull(tag.getParent());//here we should receive Subjects as parent because alternativeTagGroup should be hidden
+		assertNull(tag.getParent().getParent());//here we should receive Subjects null parent 
 		tag = tagService.getTagByFullPath(TAG_NAME);
+		assertNull(tag);
+		//and now check not subjects tags
+		tag = tagService.getTagByFullPath(URLEncoder.encode(TAG_GROUP_NAME_2, UTF_8_ENCODING) + LEFT_SLASH_CHARACTER + TAG_NAME_2);
+		assertNotNull(tag);
+		assertEquals(TAG_NAME_2, tag.getName());
+		assertNotNull(tag.getParent());
+		assertNull(tag.getParent().getParent());
+		tag = tagService.getTagByFullPath(TAG_GROUP_NAME_2);
+		assertNotNull(tag);
+		assertEquals(TAG_GROUP_NAME_2, tag.getName());
+		assertNull(tag.getParent());
+		tag = tagService.getTagByFullPath(TAG_NAME_2);
 		assertNull(tag);
 	}
 }
