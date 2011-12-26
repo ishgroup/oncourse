bfabcac2640f014f474367d1dac59c593aa092e1
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2011-12-26 12:57:52 +0000

    #11738. Replication Status page with basic functionality.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17712 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ admin/src/main/java/ish/oncourse/admin/pages/ReplicationStatus.java
@@ -0,0 +1,119 @@
+package ish.oncourse.admin.pages;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import ish.oncourse.model.College;
+import ish.oncourse.model.QueuedRecord;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.services.system.ICollegeService;
+
+import org.apache.cayenne.Cayenne;
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.exp.Expression;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.Ordering;
+import org.apache.cayenne.query.SelectQuery;
+import org.apache.cayenne.query.SortOrder;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+public class ReplicationStatus {
+	
+	@Inject
+	private ICollegeService collegeService;
+	
+	@Inject
+	private ICayenneService cayenneService;
+	
+	@Property
+	private List<College> colleges;
+	
+	@Property
+	private College currentCollege;
+	
+	private DateFormat dateFormat;
+	
+	@SetupRender
+	void setupRender() {
+		dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		this.colleges = collegeService.allColleges();
+	}
+	
+	public String getLastReplicationDate() {
+		return dateFormat.format(currentCollege.getLastRemoteAuthentication());
+	}
+	
+	public String getOldestQueuedItemTimestamp() {
+		QueuedRecord oldestRecord = getOldestQueuedItem();
+		if (oldestRecord != null) {
+			return dateFormat.format(oldestRecord.getLastAttemptTimestamp());
+		}
+		return "";
+	}
+	
+	public String getLastError() {
+		QueuedRecord lastRecord = getLastQueuedItem();
+		if (lastRecord != null) {
+			return lastRecord.getErrorMessage();
+		}
+		return "";
+	}
+	
+	public String getNumberOfRecordsInQueue() {
+		ObjectContext context = cayenneService.sharedContext();
+		
+		College college = (College) context.localObject(currentCollege.getObjectId(), null);
+		if (college != null) {
+			Expression exp = ExpressionFactory.matchExp(QueuedRecord.COLLEGE_PROPERTY, college);
+			SelectQuery query = new SelectQuery(QueuedRecord.class, exp);
+			
+			List<QueuedRecord> records = context.performQuery(query);
+			return String.valueOf(records.size());
+		}
+		return "0";
+	}
+	
+	Object onActionFromResetReplication(String billingCode) {
+		
+		return null;
+	}
+	
+	private QueuedRecord getOldestQueuedItem() {
+		ObjectContext context = cayenneService.sharedContext();
+		
+		College college = (College) context.localObject(currentCollege.getObjectId(), null);
+		if (college != null) {
+			Expression exp = ExpressionFactory.matchExp(QueuedRecord.COLLEGE_PROPERTY, college);
+			SelectQuery query = new SelectQuery(QueuedRecord.class, exp);
+			query.addOrdering(QueuedRecord.LAST_ATTEMPT_TIMESTAMP_PROPERTY, SortOrder.DESCENDING);
+			query.setFetchLimit(1);
+			
+			QueuedRecord record = (QueuedRecord) Cayenne.objectForQuery(context, query);
+			
+			return record;
+		}
+		return null;
+	}
+	
+	private QueuedRecord getLastQueuedItem() {
+		ObjectContext context = cayenneService.sharedContext();
+		
+		College college = (College) context.localObject(currentCollege.getObjectId(), null);
+		if (college != null) {
+			Expression exp = ExpressionFactory.matchExp(QueuedRecord.COLLEGE_PROPERTY, college);
+			SelectQuery query = new SelectQuery(QueuedRecord.class, exp);
+			query.addOrdering(QueuedRecord.LAST_ATTEMPT_TIMESTAMP_PROPERTY, SortOrder.ASCENDING);
+			query.setFetchLimit(1);
+			
+			QueuedRecord record = (QueuedRecord) Cayenne.objectForQuery(context, query);
+			
+			return record;
+		}
+		return null;
+	}
+
+}
