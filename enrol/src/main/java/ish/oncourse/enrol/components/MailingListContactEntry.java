310564acec6a9bbc5a619de20fb52f55b3ec7202
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2012-01-05 15:49:09 +0000

    #12961. Mailing list subscription page implementation.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17786 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ enrol/src/main/java/ish/oncourse/enrol/components/MailingListContactEntry.java
@@ -0,0 +1,218 @@
+package ish.oncourse.enrol.components;
+
+import ish.oncourse.enrol.pages.Mail;
+import ish.oncourse.enrol.services.concessions.IConcessionsService;
+import ish.oncourse.enrol.services.student.IStudentService;
+import ish.oncourse.model.College;
+import ish.oncourse.model.Contact;
+import ish.oncourse.model.Student;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.services.site.IWebSiteService;
+
+import java.util.List;
+
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.PersistenceState;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ValidationTracker;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.InjectPage;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.TextField;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+
+public class MailingListContactEntry {
+	
+	/**
+	 * ish services
+	 */
+	@Inject
+	private IWebSiteService webSiteService;
+
+	@Inject
+	private IStudentService studentService;
+
+	@Inject
+	private ICayenneService cayenneService;
+
+	/**
+	 * tapestry services
+	 */
+	@Inject
+	private Request request;
+
+	@Inject
+	private Messages messages;
+
+	/**
+	 * components
+	 */
+	@InjectComponent
+	@Property
+	private Zone addStudentBlock;
+
+	@InjectComponent
+	@Property
+	private Form shortDetailsForm;
+
+	@InjectComponent
+	private TextField firstName;
+
+	@InjectComponent
+	private TextField lastName;
+
+	@InjectComponent
+	private TextField email;
+	
+	@InjectPage
+	private Mail mail;
+
+	/**
+	 * properties
+	 */
+	@Property
+	private boolean needMoreInfo;
+
+	@Persist
+	@Property
+	private boolean hasContact;
+
+	@Property
+	@Persist
+	private Contact contact;
+
+	@Persist
+	private ObjectContext context;
+
+	private boolean reset;
+
+	/**
+	 * these properties are used for validation list and for hints
+	 */
+	@Property
+	private String firstNameErrorMessage;
+
+	@Property
+	private String lastNameErrorMessage;
+
+	@Property
+	private String emailErrorMessage;
+
+	@SetupRender
+	void beforeRender() {
+		context = cayenneService.newContext();
+		hasContact = false;
+		contact = context.newObject(Contact.class);
+
+		College currentCollege = webSiteService.getCurrentCollege();
+		College college = (College) context.localObject(currentCollege.getObjectId(), currentCollege);
+		contact.setCollege(college);
+
+		contact.createNewStudent();
+		
+		contact.setIsMarketingViaEmailAllowed(true);
+		contact.setIsMarketingViaPostAllowed(true);
+		contact.setIsMarketingViaSMSAllowed(true);
+
+		reset = true;
+		shortDetailsForm.clearErrors();
+	}
+
+	public String getAddStudentBlockClass() {
+		List<Long> shortlistStudents = studentService.getContactsIdsFromShortList();
+		return (shortlistStudents == null || shortlistStudents.isEmpty() || needMoreInfo) ? "show" : "collapse";
+	}
+
+	public boolean isNewStudent() {
+		return contact.getPersistenceState() == PersistenceState.NEW;
+	}
+
+	@OnEvent(component = "addStudentAction", value = "selected")
+	void onSelectedFromAddStudentAction() {
+		reset = false;
+	}
+
+	@OnEvent(component = "resetAll", value = "selected")
+	void onSelectedFromReset() {
+		reset = true;
+	}
+
+	@OnEvent(component = "shortDetailsForm", value = "validate")
+	void validate() {
+		if (reset) {
+			shortDetailsForm.clearErrors();
+		} else {
+			firstNameErrorMessage = contact.validateGivenName();
+			if (firstNameErrorMessage != null) {
+				shortDetailsForm.recordError(firstName, firstNameErrorMessage);
+			}
+			lastNameErrorMessage = contact.validateFamilyName();
+			if (lastNameErrorMessage != null) {
+				shortDetailsForm.recordError(lastName, lastNameErrorMessage);
+			}
+			emailErrorMessage = contact.validateEmail();
+			if (emailErrorMessage != null) {
+				shortDetailsForm.recordError(email, emailErrorMessage);
+			}
+
+		}
+	}
+
+	@OnEvent(component = "shortDetailsForm", value = "failure")
+	Block refreshContactEntry() {
+		return addStudentBlock.getBody();
+	}
+
+	@OnEvent(component = "shortDetailsForm", value = "success")
+	Object submittedSuccessfully() {
+		if (reset) {
+			contact.setGivenName(null);
+			contact.setFamilyName(null);
+			contact.setEmailAddress(null);
+		} else {
+			Contact studentContact = studentService.getStudentContact(contact.getGivenName(), contact.getFamilyName(),
+					contact.getEmailAddress());
+			if (studentContact != null) {
+				Student newStudent=contact.getStudent();
+				contact.setStudent(null);
+				context.deleteObject(contact);
+				contact = (Contact) context.localObject(studentContact.getObjectId(), null);
+				if (contact.getStudent() == null) {
+					contact.setStudent(newStudent);
+					context.commitChanges();
+				}
+				
+				mail.setContact(contact);
+				return mail;
+			}
+			hasContact = true;
+		}
+		return addStudentBlock.getBody();
+	}
+
+	public String getFirstNameInput() {
+		return getInputSectionClass(firstName);
+	}
+
+	public String getLastNameInput() {
+		return getInputSectionClass(lastName);
+	}
+
+	public String getEmailInput() {
+		return getInputSectionClass(email);
+	}
+
+	private String getInputSectionClass(TextField field) {
+		ValidationTracker defaultTracker = shortDetailsForm.getDefaultTracker();
+		return defaultTracker == null || !defaultTracker.inError(field) ? messages.get("validInput") : messages
+				.get("validateInput");
+	}
+
+}
---
+++ enrol/src/main/java/ish/oncourse/enrol/pages/Mail.java
@@ -0,0 +1,178 @@
+package ish.oncourse.enrol.pages;
+
+import ish.oncourse.model.College;
+import ish.oncourse.model.Contact;
+import ish.oncourse.model.Tag;
+import ish.oncourse.model.Taggable;
+import ish.oncourse.model.TaggableTag;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.services.site.IWebSiteService;
+import ish.oncourse.services.tag.ITagService;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.cayenne.Cayenne;
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.PersistenceState;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+public class Mail {
+
+	@Persist
+	@Property
+	private List<Tag> mailingLists;
+
+	@Property
+	private Tag currentMailingList;
+	
+	@Property
+	private int listIndex;
+	
+	@Persist
+	private Contact contact;
+	
+	@Property
+	@Persist
+	private boolean submissionSucceded;
+	
+	@InjectComponent
+	@Property
+	private Zone addStudentBlock;
+	
+	@InjectComponent
+	@Property
+	private Form detailsForm;
+	
+	@Inject
+	private IWebSiteService webSiteService;
+	
+	@Inject
+	private ICayenneService cayenneService;
+	
+	@Inject
+	private ITagService tagService;
+	
+	private boolean reset;
+	
+	@Persist
+	@Property
+	private List<Tag> selectedMailingLists;
+	
+	@SetupRender
+	void setupRender() {
+		this.mailingLists = tagService.getMailingLists();
+		this.selectedMailingLists = new ArrayList<Tag>();
+		
+		if (contact != null) {
+			for (Tag mailingList : tagService.getMailingListsContactSubscribed(contact)) {
+				this.mailingLists.remove(mailingList);
+			}
+		}
+
+		detailsForm.clearErrors();
+	}
+	
+	@AfterRender
+	void afterRender() {
+		submissionSucceded = false;
+	}
+	
+	public boolean getListChecked() {
+		if (this.contact != null) {
+			return tagService.getMailingListsContactSubscribed(contact).contains(currentMailingList);
+		}
+		return false;
+	}
+	
+	public void setListChecked(boolean checked) {
+		if (checked) {
+			this.selectedMailingLists.add(mailingLists.get(listIndex));
+		}
+	}
+	
+	@OnEvent(component = "addStudentAction", value = "selected")
+	void onSelectedFromAddStudentAction() {
+		reset = false;
+	}
+
+	@OnEvent(component = "resetAll", value = "selected")
+	void onSelectedFromReset() {
+		reset = true;
+	}
+
+	@OnEvent(component = "detailsForm", value = "failure")
+	Block refreshContactEntry() {
+		this.selectedMailingLists.clear();
+		return addStudentBlock.getBody();
+	}
+	
+	@OnEvent(component="detailsForm", value="success")
+	Object submit() {
+		if (reset) {
+			this.contact = null;
+			this.selectedMailingLists.clear();
+		}
+		else {
+			if (!selectedMailingLists.isEmpty()) {
+				ObjectContext context = cayenneService.newContext();
+				
+				Date date = new Date();
+				College col = webSiteService.getCurrentCollege();
+				College college = (College) context.localObject(col.getObjectId(), null);
+				
+				Contact c = (Contact) Cayenne.objectForPK(context, Contact.class, contact.getId());
+				
+				if (college != null) {
+					for (Tag list : selectedMailingLists) {
+						Tag mailingList = (Tag) context.localObject(list.getObjectId(), null);
+						
+						Taggable taggable = context.newObject(Taggable.class);
+						taggable.setCollege(college);
+						taggable.setCreated(date);
+						taggable.setModified(date);
+						taggable.setEntityIdentifier(Contact.class.getSimpleName());
+						taggable.setEntityWillowId(c.getId());
+						taggable.setEntityAngelId(c.getAngelId());
+						
+						TaggableTag taggableTag = context.newObject(TaggableTag.class);
+						taggableTag.setCollege(college);
+						taggableTag.setTag(mailingList);
+						taggable.addToTaggableTags(taggableTag);
+					}
+					
+					context.commitChanges();
+					this.submissionSucceded = true;
+				}
+			}
+		}
+		return addStudentBlock.getBody();
+	}
+	
+	public boolean isNewStudent() {
+		return contact.getPersistenceState() == PersistenceState.NEW;
+	}
+	
+	public Tag getCurrentList() {
+		return mailingLists.get(listIndex);
+	}
+	
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+	
+	public boolean getHasContact() {
+		return this.contact != null;
+	}
+	
+}
