879b924a92b74ac8ed17feba9f8ca957340d14a5
Anton Sakalouski <asakalouski@objectstyle.com>
2012-01-11 17:36:53 +0000

    #13044. We don't need quartz in admin app at all. Cleaning up.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17870 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/services/AppModule.java
+++ admin/src/main/java/ish/oncourse/admin/services/AppModule.java
@@ -5,19 +5,15 @@ import ish.oncourse.admin.services.billing.IBillingDataService;
 import ish.oncourse.admin.services.ntis.INTISUpdater;
 import ish.oncourse.admin.services.ntis.NTISUpdaterImpl;
 import ish.oncourse.admin.services.ntis.TrainingComponentServiceBuilder;
-import ish.oncourse.admin.services.quartz.QuartzInitializer;
-import ish.oncourse.services.site.IWebSiteService;
-import ish.oncourse.services.ServiceModule;
 import ish.oncourse.model.services.ModelModule;
+import ish.oncourse.services.ServiceModule;
+import ish.oncourse.services.site.IWebSiteService;
 
 import org.apache.tapestry5.MetaDataConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
-import org.apache.tapestry5.ioc.ServiceBuilder;
-import org.apache.tapestry5.ioc.ServiceResources;
 import org.apache.tapestry5.ioc.annotations.Local;
 import org.apache.tapestry5.ioc.annotations.SubModule;
-import org.apache.tapestry5.ioc.services.RegistryShutdownHub;
 
 import au.gov.training.services.trainingcomponent.ITrainingComponentService;
 
@@ -34,16 +30,6 @@ public class AppModule {
 		binder.bind(ITrainingComponentService.class, new TrainingComponentServiceBuilder());
 		binder.bind(INTISUpdater.class, NTISUpdaterImpl.class);
 		binder.bind(IWebSiteService.class, WebSiteServiceOverride.class).withId("WebSiteServiceAdmin");
-		
-		binder.bind(QuartzInitializer.class, new ServiceBuilder<QuartzInitializer>() {
-			@Override
-			public QuartzInitializer buildService(ServiceResources res) {
-				QuartzInitializer quartzInit = new QuartzInitializer(res);
-				RegistryShutdownHub hub = res.getService(RegistryShutdownHub.class);
-				hub.addRegistryShutdownListener(quartzInit);
-				return quartzInit;
-			}
-		}).eagerLoad();
 	}
 
 	public void contributeMetaDataLocator(MappedConfiguration<String, String> configuration) {
@@ -53,5 +39,4 @@ public class AppModule {
 	public void contributeServiceOverride(MappedConfiguration<Class<?>, Object> configuration, @Local IWebSiteService webSiteService) {
 		configuration.add(IWebSiteService.class, webSiteService);
 	}
-	
 }
