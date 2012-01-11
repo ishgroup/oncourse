300eced80978de4b392d078af10abaad58a576f4
Anton Sakalouski <asakalouski@objectstyle.com>
2012-01-11 01:02:39 +0000

    #12925. Extended unit test, added case with multiple job execution, ensure that duplicated refunds is not created in this case.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17850 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/test/java/ish/oncourse/webservices/jobs/PaymentInExpireJobTest.java
+++ common/common-services/src/test/java/ish/oncourse/webservices/jobs/PaymentInExpireJobTest.java
@@ -157,4 +157,73 @@ public class PaymentInExpireJobTest extends ServiceTest {
 				String.format("select * from QueuedRecord where entityIdentifier='Enrolment' and entityWillowId=2001"));
 		assertTrue("at least 1 Enrolment in the queue.", 1 <= actualData.getRowCount());
 	}
+	
+	@Test
+	public void testMultyExecution() throws Exception {
+		ObjectContext objectContext = cayenneService.newContext();
+
+		PaymentIn p = Cayenne.objectForPK(objectContext, PaymentIn.class, 2000);
+		p.failPayment();
+
+		objectContext.commitChanges();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MINUTE, -PaymentIn.EXPIRE_INTERVAL);
+		
+		// simulate EXPIRE_INTERVAL wait by directly updating enrolments with sql statement
+		Connection connection = null;
+		try {
+			connection = getDataSource("jdbc/oncourse").getConnection();
+			PreparedStatement prepStat = connection.prepareStatement("update PaymentIn set modified=?");
+			prepStat.setDate(1, new java.sql.Date(cal.getTime().getTime()));
+			int affected = prepStat.executeUpdate();
+			assertEquals("Expected update on 1 paymentIn.", 1, affected);
+			
+			prepStat.close();
+			
+			//cleanup the queue before running job
+			Statement st = connection.createStatement();
+			st.execute("delete from QueuedRecord");
+			st.execute("delete from QueuedTransaction");
+			st.close();
+		}
+		finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		
+		JobExecutionContext jobContext = mock(JobExecutionContext.class);
+		
+		//Execute job three times
+		job.execute(jobContext);
+		job.execute(jobContext);
+		job.execute(jobContext);
+		
+		DatabaseConnection dbUnitConnection = new DatabaseConnection(getDataSource("jdbc/oncourse").getConnection(), null);
+		ITable actualData = dbUnitConnection.createQueryTable("PaymentIn",
+				String.format("select * from PaymentIn"));
+		
+		assertEquals("There should be only two PaymentIn", 2, actualData.getRowCount());
+		
+		actualData = dbUnitConnection.createQueryTable("PaymentIn",
+				String.format("select * from PaymentIn where status=4"));
+		
+		assertEquals("Expecting only one failed paymentIn", 1, actualData.getRowCount());
+		
+		actualData = dbUnitConnection.createQueryTable("PaymentIn",
+				String.format("select * from PaymentIn where amount=0 and type=5 and status=3"));
+		
+		assertEquals("There should only be one PaymentIn refund.", 1, actualData.getRowCount());
+		
+		// check that in transaction Payment has failed.
+		p = Cayenne.objectForPK(objectContext, PaymentIn.class, 2000);
+		assertEquals("Payment has failed.", PaymentStatus.FAILED, p.getStatus());
+
+		Enrolment enrolment = Cayenne.objectForPK(objectContext, Enrolment.class, 2000);
+		assertEquals("Enrolment has failed.", EnrolmentStatus.FAILED, enrolment.getStatus());
+
+		Enrolment enrolment2 = Cayenne.objectForPK(objectContext, Enrolment.class, 2001);
+		assertEquals("Enrolment2 has failed.", EnrolmentStatus.FAILED, enrolment2.getStatus());
+	}
 }
