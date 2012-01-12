5bd5f15522e5c60442e4aa642214c570d4ecc1d2
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2012-01-12 13:54:42 +0000

    #13046. Add log for null Outcome error.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17891 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/replication/updaters/CertificateOutcomeUpdater.java
+++ services/src/main/java/ish/oncourse/webservices/replication/updaters/CertificateOutcomeUpdater.java
@@ -1,17 +1,28 @@
 package ish.oncourse.webservices.replication.updaters;
 
+import org.apache.log4j.Logger;
+
 import ish.oncourse.model.Certificate;
 import ish.oncourse.model.CertificateOutcome;
 import ish.oncourse.model.Outcome;
 import ish.oncourse.webservices.v4.stubs.replication.CertificateOutcomeStub;
 
 public class CertificateOutcomeUpdater extends AbstractWillowUpdater<CertificateOutcomeStub, CertificateOutcome> {
+	
+	private static final Logger logger = Logger.getLogger(CertificateOutcomeUpdater.class);
 
 	@Override
 	protected void updateEntity(CertificateOutcomeStub stub, CertificateOutcome entity, RelationShipCallback callback) {
 		entity.setCertificate(callback.updateRelationShip(stub.getCertificateId(), Certificate.class));
 		entity.setCreated(stub.getCreated());
 		entity.setModified(stub.getModified());
-		entity.setOutcome(callback.updateRelationShip(stub.getOutcomeId(), Outcome.class));
+		
+		Outcome outcome = callback.updateRelationShip(stub.getOutcomeId(), Outcome.class);
+		if (outcome == null) {
+			logger.error("Couldn't find Outcome with angelId=" + stub.getOutcomeId() + 
+					" for CertificateOutcome with angelId=" + stub.getAngelId() + 
+					", created on " + stub.getCreated() + ", modified on " + stub.getModified());
+		}
+		entity.setOutcome(outcome);
 	}
 }
