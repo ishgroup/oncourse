30b57aa3089521c0681485e458b5e14a9127dfc5
Anton Sakalouski <asakalouski@objectstyle.com>
2012-01-22 09:20:00 +0000

    #0000. Reduce log level to debug.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@17988 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/linktransform/PageLinkTransformer.java
+++ common/common-services/src/main/java/ish/oncourse/linktransform/PageLinkTransformer.java
@@ -122,9 +122,14 @@ public class PageLinkTransformer implements PageRenderLinkTransformer {
 		case Courses:
 			final boolean isCMSCoursesSearch = requestGlobals.getHTTPServletRequest().getRequestURI().toLowerCase()
 					.startsWith(CMS_PATH + COURSES_PATH);
+			
 			String tagsPath = requestGlobals.getHTTPServletRequest().getRequestURI().toLowerCase()
 					.replaceFirst(isCMSCoursesSearch ? CMS_PATH + COURSES_PATH : COURSES_PATH, StringUtils.EMPTY);
-			LOGGER.warn("tagsPath:" + tagsPath);
+			
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug(String.format("tagsPath:%s", tagsPath));
+			}
+			
 			if (!tagsPath.startsWith(CMS_PATH)) {
 				if (tagsPath.startsWith(LEFT_SLASH_CHARACTER)) {
 					tagsPath = tagsPath.replaceFirst(LEFT_SLASH_CHARACTER, StringUtils.EMPTY);
