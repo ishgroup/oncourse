ec9bcfbe5c2fded762d9648a7bc9c2bc47262b50
Anton Sakalouski <asakalouski@objectstyle.com>
2012-02-02 15:22:49 +0000

    #0000. Nothing changed just add some more logging.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18173 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/replication/updaters/PaymentInLineUpdater.java
+++ services/src/main/java/ish/oncourse/webservices/replication/updaters/PaymentInLineUpdater.java
@@ -5,15 +5,27 @@ import ish.oncourse.model.PaymentIn;
 import ish.oncourse.model.PaymentInLine;
 import ish.oncourse.webservices.v4.stubs.replication.PaymentInLineStub;
 
+import org.apache.log4j.Logger;
+
 public class PaymentInLineUpdater extends AbstractWillowUpdater<PaymentInLineStub, PaymentInLine> {
+
+	/**
+	 * Logger
+	 */
+	private static final Logger logger = Logger.getLogger(PaymentInLineUpdater.class);
 
 	@Override
 	protected void updateEntity(PaymentInLineStub stub, PaymentInLine entity, RelationShipCallback callback) {
 		entity.setAmount(stub.getAmount());
 		entity.setCreated(stub.getCreated());
+
 		Invoice invoice = callback.updateRelationShip(stub.getInvoiceId(), Invoice.class);
-		entity.setInvoice(invoice);
-		
+		if (invoice != null) {
+			entity.setInvoice(invoice);
+		} else {
+			logger.error(String.format("Can not find invoice by angelId:%s", stub.getInvoiceId()));
+		}
+
 		entity.setModified(stub.getModified());
 		entity.setPaymentIn(callback.updateRelationShip(stub.getPaymentInId(), PaymentIn.class));
 	}
