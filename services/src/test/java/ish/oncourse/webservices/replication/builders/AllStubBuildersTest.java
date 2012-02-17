1e0a0db448d8465914ba94b514cf594820776358
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-02-17 09:36:43 +0000

    #0000 junit for all stub builders has been added
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18332 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ services/src/test/java/ish/oncourse/webservices/replication/builders/AllStubBuildersTest.java
@@ -0,0 +1,236 @@
+package ish.oncourse.webservices.replication.builders;
+
+import ish.oncourse.model.*;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.test.ServiceTest;
+import ish.oncourse.webservices.soap.v4.ReplicationTestModule;
+import ish.oncourse.webservices.v4.stubs.replication.ReplicationStub;
+import org.apache.cayenne.Cayenne;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+
+/**
+ * The bash command line generates test methods for all stubs
+ * ls | grep -Ev '(^Abstract|^Willow)' | awk -F . '{print $1}' | awk '{print "@Test\npublic void test"$1"(){\n         this.testStubBuilder(.class, new "$1"());\n}"}'
+ */
+public class AllStubBuildersTest extends ServiceTest {
+
+    @Before
+    public void setupDataSet() throws Exception {
+        initTest("ish.oncourse.webservices.services", "", ReplicationTestModule.class);
+
+        initReferenceDataSet();
+        initOncourseBinaryDataSet();
+        initOncourseDataSet();
+    }
+
+    private void initOncourseBinaryDataSet() throws Exception {
+
+        InputStream st = WillowStubBuilderTest.class.getClassLoader().getResourceAsStream("ish/oncourse/webservices/replication/builders/oncourseBinaryDataSet.xml");
+        FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+        builder.setColumnSensing(true);
+        FlatXmlDataSet dataSet = builder.build(st);
+
+
+        DataSource onDataSource = getDataSource("jdbc/oncourse_binary");
+        DatabaseConnection dbConnection = new DatabaseConnection(onDataSource.getConnection(), null);
+        dbConnection.getConfig().setProperty(DatabaseConfig.FEATURE_CASE_SENSITIVE_TABLE_NAMES, false);
+
+        DatabaseOperation.CLEAN_INSERT.execute(dbConnection, dataSet);
+    }
+
+
+    private void initOncourseDataSet() throws Exception {
+
+
+        InputStream st = WillowStubBuilderTest.class.getClassLoader().getResourceAsStream("ish/oncourse/webservices/replication/builders/oncourseDataSet.xml");
+        FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+        builder.setColumnSensing(true);
+        FlatXmlDataSet dataSet = builder.build(st);
+
+
+        DataSource onDataSource = getDataSource("jdbc/oncourse");
+        DatabaseConnection dbConnection = new DatabaseConnection(onDataSource.getConnection(), null);
+        dbConnection.getConfig().setProperty(DatabaseConfig.FEATURE_CASE_SENSITIVE_TABLE_NAMES, false);
+
+        DatabaseOperation.CLEAN_INSERT.execute(dbConnection, dataSet);
+    }
+
+    private void initReferenceDataSet() throws Exception {
+        InputStream st = WillowStubBuilderTest.class.getClassLoader().getResourceAsStream("ish/oncourse/webservices/soap/v4/referenceDataSet.xml");
+
+        FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().build(st);
+
+        DataSource refDataSource = getDataSource("jdbc/oncourse_reference");
+
+        DatabaseOperation.CLEAN_INSERT.execute(new DatabaseConnection(refDataSource.getConnection(), null), dataSet);
+    }
+
+    private <E extends Queueable, S extends ReplicationStub> void testStubBuilder(Class<E> entityClass,
+                                                                                  AbstractWillowStubBuilder<E, S> stubBuilder, Long entityId,
+                                                                                  String... excludeProperty
+    ) {
+        ICayenneService cayenneService = getService(ICayenneService.class);
+        E entity = Cayenne.objectForPK(cayenneService.sharedContext(), entityClass, entityId);
+        StubBuilderTestHelper<E, S> stubBuilderTestHelper = new StubBuilderTestHelper<E, S>(entity,excludeProperty);
+        stubBuilderTestHelper.assertStubBuilder(stubBuilder);
+    }
+
+    private <E extends Queueable, S extends ReplicationStub> void testStubBuilder(Class<E> entityClass, 
+                                                                                  AbstractWillowStubBuilder<E, S> stubBuilder, 
+                                                                                  String... excludeProperty
+    ) {
+        this.testStubBuilder(entityClass,stubBuilder, 1l, excludeProperty);
+    }
+
+
+    @Test
+    public void testAttendanceStubBuilder() {
+        this.testStubBuilder(Attendance.class, new AttendanceStubBuilder());
+    }
+
+    @Test
+    public void testBinaryDataStubBuilder() {
+        this.testStubBuilder(BinaryData.class, new BinaryDataStubBuilder());
+    }
+
+    @Test
+    public void testBinaryInfoStubBuilder() {
+        this.testStubBuilder(BinaryInfo.class, new BinaryInfoStubBuilder());
+    }
+
+    @Test
+    public void testConcessionTypeStubBuilder() {
+        this.testStubBuilder(ConcessionType.class, new ConcessionTypeStubBuilder());
+    }
+
+    @Test
+    public void testContactStubBuilder() {
+        this.testStubBuilder(Contact.class, new ContactStubBuilder());
+    }
+
+    @Test
+    public void testCourseClassStubBuilder() {
+        this.testStubBuilder(CourseClass.class, new CourseClassStubBuilder(),
+                "materialsTextile","modified","sessionDetailTextile", "startDate","startingMinutePerSession", "timeZone");
+    }
+
+    @Test
+    public void testCourseStubBuilder() {
+        this.testStubBuilder(Course.class, new CourseStubBuilder());
+    }
+
+    @Test
+    public void testDiscountStubBuilder() {
+        this.testStubBuilder(Discount.class, new DiscountStubBuilder(), "codeRequired");
+    }
+
+    @Test
+    public void testEnrolmentStubBuilder() {
+        this.testStubBuilder(Enrolment.class, new EnrolmentStubBuilder());
+    }
+
+
+    @Test
+    public void testInvoiceLineDiscountStubBuilder() {
+        this.testStubBuilder(InvoiceLineDiscount.class, new InvoiceLineDiscountStubBuilder());
+    }
+
+    @Test
+    public void testInvoiceLineStubBuilder() {
+        this.testStubBuilder(InvoiceLine.class, new InvoiceLineStubBuilder());
+    }
+
+    @Test
+    public void testInvoiceStubBuilder() {
+        this.testStubBuilder(Invoice.class, new InvoiceStubBuilder(), "status");
+    }
+
+    @Test
+    public void testMessagePersonStubBuilder() {
+        this.testStubBuilder(MessagePerson.class, new MessagePersonStubBuilder(),1l, "tutorId");
+        this.testStubBuilder(MessagePerson.class, new MessagePersonStubBuilder(),2l, "studentId");
+    }
+
+    @Test
+    public void testOutcomeStubBuilder() {
+        this.testStubBuilder(Outcome.class, new OutcomeStubBuilder(),"endDate","hoursAttended","startDate","status");
+    }
+
+    @Test
+    public void testPaymentInLineStubBuilder() {
+        this.testStubBuilder(PaymentInLine.class, new PaymentInLineStubBuilder());
+    }
+
+    @Test
+    public void testPaymentInStubBuilder() {
+        this.testStubBuilder(PaymentIn.class, new PaymentInStubBuilder());
+    }
+
+    @Test
+    public void testPaymentOutStubBuilder() {
+        this.testStubBuilder(PaymentOut.class, new PaymentOutStubBuilder());
+    }
+
+    @Test
+    public void testPreferenceStubBuilder() {
+        this.testStubBuilder(Preference.class, new PreferenceStubBuilder());
+    }
+
+    @Test
+    public void testRoomStubBuilder() {
+        this.testStubBuilder(Room.class, new RoomStubBuilder(), "directions","facilities");
+    }
+
+    @Test
+    public void testSiteStubBuilder() {
+        /**
+         * TODO: The stub builder sets drivingDirectionsTextile to  drivingDirections
+         * TODO: The stub builder sets publicTransportDirections_textile to publicTransportDirections
+         * TODO: The stub builder sets specialInstructions_textile to specialInstructions
+         */
+        this.testStubBuilder(Site.class, new SiteStubBuilder(), "drivingDirections", "publicTransportDirections",
+                "specialInstructions","timeZone", "webVisible");
+    }
+
+    @Test
+    public void testStudentConcessionStubBuilder() {
+        this.testStubBuilder(StudentConcession.class, new StudentConcessionStubBuilder());
+    }
+
+    @Test
+    public void testStudentStubBuilder() {
+        this.testStubBuilder(Student.class, new StudentStubBuilder());
+    }
+
+    @Test
+
+    public void testTaggableStubBuilder() {
+        this.testStubBuilder(Taggable.class, new TaggableStubBuilder());
+    }
+
+    @Test
+    public void testTutorRoleStubBuilder() {
+        this.testStubBuilder(TutorRole.class, new TutorRoleStubBuilder(), "inPublicity");
+    }
+
+    @Test
+    public void testTutorStubBuilder() {
+        this.testStubBuilder(Tutor.class, new TutorStubBuilder(), "resume");
+    }
+
+    @Test
+    public void testWaitingListStubBuilder() {
+        this.testStubBuilder(WaitingList.class, new WaitingListStubBuilder());
+    }
+
+
+}
---
+++ services/src/test/java/ish/oncourse/webservices/replication/builders/StubBuilderTestHelper.java
@@ -0,0 +1,258 @@
+package ish.oncourse.webservices.replication.builders;
+
+import ish.common.types.*;
+import ish.math.Money;
+import ish.math.MoneyRounding;
+import ish.oncourse.model.*;
+import ish.oncourse.webservices.v4.stubs.replication.PaymentInStub;
+import ish.oncourse.webservices.v4.stubs.replication.ReplicationStub;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.lang.WordUtils;
+import org.apache.log4j.Logger;
+import org.springframework.util.ReflectionUtils;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * The bash commond line gets all getters from the Java Class:
+ * cat ContactStub.java  | grep --color -E 'public\ [a-zA-Z]{1,}\ (get|is)[a-zA-Z_-0-9]{1,}\(\)' | awk '{print "contactStub."$3";"}' | uniq | sort
+ */
+
+public class StubBuilderTestHelper<E extends Queueable, S extends ReplicationStub> {
+    private static final Logger LOGGER = Logger.getLogger(StubBuilderTestHelper.class);
+
+    private E entity;
+    private String[] excludeProperties;
+
+
+    public StubBuilderTestHelper(E entity, String... excludeProperty) {
+        if (entity == null)
+             throw new IllegalArgumentException("parameter \"entity\" is null");
+        this.entity = entity;
+        this.excludeProperties = excludeProperty;
+    }
+    
+    public boolean shouldProcess(String propertyName)
+    {
+        if (propertyName.equals("class"))
+               return false;
+        if (excludeProperties != null)
+        {
+            for (String excludeProperty : excludeProperties) {
+                if (propertyName.equals(excludeProperty))
+                    return false;
+            }
+        }
+
+        return true;
+
+
+    }
+
+
+    public void assertStubBuilder(AbstractWillowStubBuilder<E, S>  stubBuilder) {
+        LOGGER.info(String.format("test stubBuilder - \"%s\"", stubBuilder.getClass().getSimpleName()));
+        ReplicationStub stub = stubBuilder.convert(entity);
+
+        PropertyDescriptor[] contactDescriptors = PropertyUtils.getPropertyDescriptors(stub);
+        for (PropertyDescriptor descriptor : contactDescriptors) {
+            String propertyName = descriptor.getName();
+            if (shouldProcess(propertyName))
+            {
+                LOGGER.info(String.format("test property - \"%s\"", propertyName));
+                Object stubValue = getStubValueBy(descriptor, stub);
+                assertTrue(String.format("property \"%s\" is not set", propertyName), stubValue != null);
+                Object entityValue = getEntityValueBy(descriptor);
+                entityValue = convert(entityValue, stubValue.getClass());
+                assertEquals("test property - " + descriptor.getName(), entityValue, stubValue);
+            }
+        }
+
+    }
+
+    /**
+     * Get value for the descriptor from the entity object
+     * @param descriptor -  property descriptor
+     * @return - value for the descriptor from the entity object
+     */
+    public Object getEntityValueBy(PropertyDescriptor descriptor) {
+        String propertyName = descriptor.getName();
+        String simpleName = this.entity.getClass().getSimpleName();
+
+        if (entity instanceof PaymentIn)
+        {
+            if (propertyName.equals("privateNotes"))
+            {
+                propertyName = PaymentIn.STATUS_NOTES_PROPERTY;
+            }
+        } else if (entity instanceof PaymentOut)
+        {
+            if (propertyName.equals("amount"))
+            {
+                propertyName = PaymentOut.TOTAL_AMOUNT_PROPERTY;
+            }
+            else if (propertyName.equals("type"))
+            {
+                return PaymentType.CREDIT_CARD.getDatabaseValue();
+            }
+        } else if (entity instanceof Taggable)
+        {
+            if (propertyName.equals("entityName"))
+            {
+                propertyName = Taggable.ENTITY_IDENTIFIER_PROPERTY;
+            } else if (propertyName.equals("tagId"))
+            {
+                return ((Taggable)entity).getTaggableTags().get(0).getTag().getId();
+            }
+        } else if (entity instanceof TutorRole)
+        {
+            if (propertyName.equals("confirmedOn"))
+            {
+                propertyName = TutorRole.CONFIRMED_DATE_PROPERTY;
+            }
+        } else if (entity instanceof WaitingList)
+        {
+            if (propertyName.equals("studentCount"))
+            {
+                propertyName = WaitingList.POTENTIAL_STUDENTS_PROPERTY;
+            }
+        }
+
+        if (propertyName.equals("willowId"))
+        {
+            return entity.getId();
+        }
+
+        PropertyDescriptor expectedDescriptor;
+        try {
+            expectedDescriptor = PropertyUtils.getPropertyDescriptor(entity, propertyName);
+
+            /**
+             * Workaround for Boolean properties (JavaBean's name convention for Object's properties requries "get" prefix not "is")
+             */
+            if (expectedDescriptor == null && descriptor.getPropertyType().equals(Boolean.class)) {
+                propertyName = "is" + WordUtils.capitalize(propertyName);
+                expectedDescriptor = PropertyUtils.getPropertyDescriptor(entity, propertyName);
+            }
+
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(String.format("cannot get %s's PropertyDescriptor for property \"%s\"", simpleName, propertyName), e);
+        }
+
+        Object expected;
+        if (expectedDescriptor != null) {
+            try {
+                expected = PropertyUtils.getProperty(entity, expectedDescriptor.getName());
+            } catch (Throwable e) {
+                throw new IllegalArgumentException(String.format("cannot get %s's value for property  \"%s\"" ,simpleName, propertyName), e);
+            }
+        } else {
+
+            /**
+             * Workaround for stub properties like getCountyId - entity has property without id and returns entiry not primary key
+             */
+            if (propertyName.endsWith("Id")) {
+                propertyName = propertyName.substring(0, propertyName.length() - 2);
+                try {
+                    expected = PropertyUtils.getProperty(entity, propertyName);
+                    Method method = ReflectionUtils.findMethod(expected.getClass(), "getId");
+                    expected = method.invoke(expected);
+                } catch (Throwable e) {
+                    throw new IllegalArgumentException(String.format("cannot get %s's value for property  \"%s\"" , simpleName, propertyName), e);
+                }
+            } else if (propertyName.equals("entityIdentifier")) {
+                expected = entity.getObjectId().getEntityName();
+            } else {
+                throw new IllegalArgumentException(String.format("cannot get %s's value for property  \"%s\"",simpleName,propertyName));
+            }
+        }
+        return expected;
+    }
+
+    /**
+     * Get value for the descriptor from the stub object
+     * @param descriptor -  property descriptor
+     * @param stub -  stub object
+     * @return - value for the descriptor from the stub object
+     */
+    public Object getStubValueBy(PropertyDescriptor descriptor, ReplicationStub stub) {
+        Object actual;
+        try {
+            if (descriptor.getReadMethod() == null) {
+                String readMethod = "is" + WordUtils.capitalize(descriptor.getName());
+                Method method = ReflectionUtils.findMethod(stub.getClass(), readMethod);
+                actual = method.invoke(stub);
+            } else {
+                actual = PropertyUtils.getProperty(stub, descriptor.getName());
+            }
+
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(String.format("cannot get %s's value for property  \"%s\"" ,stub.getClass().getSimpleName(), descriptor.getName()), e);
+        }
+        return actual;
+
+
+    }
+
+
+    /**
+     * The method was introduced because there is deference between stub-property type and entity-property type, example:
+     * CourseStub.nominalHours and Course.nominalHours
+     * @param expectedValue - entity-property value
+     * @param actualClass - stub-property type
+     * @return - converted value
+     */
+    public Object convert(Object expectedValue, Class actualClass)
+    {
+        Class expectedClass = expectedValue.getClass();
+        if (expectedClass == Float.class && actualClass == BigDecimal.class)
+            return new BigDecimal((Float)expectedValue);
+        if (expectedClass == Double.class && actualClass == BigDecimal.class)
+            return new BigDecimal((Double)expectedValue);
+        else if (expectedClass == Money.class && actualClass == BigDecimal.class)
+            return ((Money) expectedValue).toBigDecimal();
+        else if (expectedClass == DiscountType.class && actualClass == Integer.class)
+            return ((DiscountType) expectedValue).getDatabaseValue();
+        else if (expectedClass == MoneyRounding.class && actualClass == Integer.class)
+            return ((MoneyRounding) expectedValue).getDatabaseValue();
+        else if (expectedClass == PaymentSource.class && actualClass == String.class)
+            return ((PaymentSource)expectedValue).getDatabaseValue();
+        else if (expectedClass == EnrolmentStatus.class && actualClass == Integer.class)
+            return ((EnrolmentStatus) expectedValue).getDatabaseValue();
+        else if (expectedClass == EnrolmentStatus.class && actualClass == Integer.class)
+            return ((EnrolmentStatus) expectedValue).getDatabaseValue();
+        else if (expectedClass == EnrolmentStatus.class && actualClass == String.class)
+            return ((EnrolmentStatus) expectedValue).name();
+        else if (expectedClass == MessageStatus.class && actualClass == Integer.class)
+            return ((MessageStatus)expectedValue).getDatabaseValue();
+        else if (expectedClass == MessageType.class && actualClass == Integer.class)
+            return ((MessageType) expectedValue).getDatabaseValue();
+        else if (expectedClass == CreditCardType.class && actualClass == String.class)
+            return ((CreditCardType) expectedValue).getDatabaseValue();
+        else if (expectedClass == PaymentStatus.class && actualClass == Integer.class)
+            return ((PaymentStatus)expectedValue).getDatabaseValue();
+        else if (expectedClass == PaymentType.class && actualClass == Integer.class)
+            return  ((PaymentType)expectedValue).getDatabaseValue();
+        else if (expectedClass == AvetmissStudentDisabilityType.class && actualClass == Integer.class)
+            return  ((AvetmissStudentDisabilityType)expectedValue).getDatabaseValue();
+        else if (expectedClass == AvetmissStudentEnglishProficiency.class && actualClass == Integer.class)
+            return ((AvetmissStudentEnglishProficiency)expectedValue).getDatabaseValue();
+        else if (expectedClass == AvetmissStudentSchoolLevel.class && actualClass == Integer.class)
+            return ((AvetmissStudentSchoolLevel)expectedValue).getDatabaseValue();
+        else if (expectedClass == AvetmissStudentIndigenousStatus.class && actualClass == Integer.class)
+            return ((AvetmissStudentIndigenousStatus)expectedValue).getDatabaseValue();
+        else if (expectedClass == AvetmissStudentIndigenousStatus.class && actualClass == Integer.class)
+            return ((AvetmissStudentIndigenousStatus)expectedValue).getDatabaseValue();
+        else if (expectedClass == AvetmissStudentPriorEducation.class && actualClass == Integer.class)
+            return ((AvetmissStudentPriorEducation)expectedValue).getDatabaseValue();
+        return expectedValue;
+
+
+    }
+
+}
