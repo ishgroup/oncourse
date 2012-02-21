b06c30931f0cb3e26f04bb652a2f88d730d5a409
Anton Sakalouski <asakalouski@objectstyle.com>
2012-02-21 14:46:24 +0000

    #13155. Added more JavaDocs.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18368 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/Taggable.java
+++ common/common-model/src/main/java/ish/oncourse/model/Taggable.java
@@ -3,6 +3,18 @@ package ish.oncourse.model;
 import ish.oncourse.model.auto._Taggable;
 import ish.oncourse.utils.QueueableObjectUtils;
 
+/**
+ * Represents the entity which can be linked to one for more tags. It looks similar to NodeRelation<entityAngelId, entityWillowId, entityIdentifier, tagId> table in angel, 
+ * but Marek put <entityAngelId, entityWillowId, entityIdentifier> into separate table I think to avoid duplication of the same values 
+ * if the same entity is assigned of more then one tag.
+ * 
+ * Thus NodeRelation from Angel, corresponds to two records in two tables in willow. The first record is <entityAngelId, entityWillowId, entityIdentifier> 
+ * in Taggable, the second record <taggableId, tagId> in TaggableTag table.
+ *
+ * 
+ * @author anton
+ *
+ */
 public class Taggable extends _Taggable implements Queueable {
 	private static final long serialVersionUID = 6153917245616586209L;
 
--- common/common-model/src/main/java/ish/oncourse/model/TaggableTag.java
+++ common/common-model/src/main/java/ish/oncourse/model/TaggableTag.java
@@ -3,6 +3,12 @@ package ish.oncourse.model;
 import ish.oncourse.model.auto._TaggableTag;
 import ish.oncourse.utils.QueueableObjectUtils;
 
+/**
+ * Check the brief description in Taggable class. @see {@link Taggable}
+ * 
+ * @author anton
+ *
+ */
 public class TaggableTag extends _TaggableTag implements Queueable {
 	private static final long serialVersionUID = 1659724430367750104L;
 
