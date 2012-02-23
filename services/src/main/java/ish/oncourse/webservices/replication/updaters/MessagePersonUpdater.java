c52fa0596a0874e50acad7e35d0bc096d8d70024
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-02-23 14:49:46 +0000

    task#13556. Add additional check to avoid state when Message Person will be replicated without message or with incorrect type.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18415 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/replication/updaters/MessagePersonUpdater.java
+++ services/src/main/java/ish/oncourse/webservices/replication/updaters/MessagePersonUpdater.java
@@ -17,29 +17,35 @@ public class MessagePersonUpdater extends AbstractWillowUpdater<MessagePersonStu
 	@Override
 	protected void updateEntity(MessagePersonStub stub, MessagePerson entity,
 			RelationShipCallback callback) {
-		// TODO Auto-generated method stub
 		entity.setContact(callback.updateRelationShip(stub.getContactId(), Contact.class));
 		entity.setCreated(stub.getCreated());
 		entity.setDestinationAddress(stub.getDestinationAddress());
-		entity.setMessage(callback.updateRelationShip(stub.getMessageId(), Message.class));
+		Message message = callback.updateRelationShip(stub.getMessageId(), Message.class);
+		if (message != null) {
+			entity.setMessage(message);
+		} else {
+			final String errorMessage = String.format("No message object found for message person with angelid:%s willowid:%s  and messageid:%s", 
+				stub.getAngelId(), entity.getId(), stub.getMessageId());
+			throw new UpdaterException(errorMessage);
+		}
 		entity.setModified(stub.getModified());
 		entity.setNumberOfAttempts(stub.getNumberOfAttempts());
 		entity.setResponse(stub.getResponse());
-		
 		entity.setStatus(TypesUtil.getEnumForDatabaseValue(stub.getStatus(), MessageStatus.class));
-		//entity.setStatus(MessageStatus.getEnumForDatabaseValue(stub.getStatus()));
-		
 		if (stub.getStudentId() != null) {
 			entity.setStudent(callback.updateRelationShip(stub.getStudentId(), Student.class));
 		}
 		else {
 			entity.setTutor(callback.updateRelationShip(stub.getTutorId(), Tutor.class));
 		}
-		
-		
 		entity.setTimeOfDelivery(stub.getTimeOfDelivery());
-		entity.setType(TypesUtil.getEnumForDatabaseValue(stub.getType(), MessageType.class));
-		//entity.setType(MessageType.getEnumForDatabaseValue(stub.getType()));
+		MessageType type = TypesUtil.getEnumForDatabaseValue(stub.getType(), MessageType.class);
+		if (MessageType.SMS.equals(type)) {
+			entity.setType(type);
+		} else {
+			throw new UpdaterException(String.format("Only sms type message person should be replicated but received %s with angelid:%s willowid:%s", 
+				type, stub.getAngelId(), entity.getId()));
+		}
 	}
 }
 
--- services/src/main/java/ish/oncourse/webservices/replication/updaters/MessageUpdater.java
+++ services/src/main/java/ish/oncourse/webservices/replication/updaters/MessageUpdater.java
@@ -1,5 +1,7 @@
 package ish.oncourse.webservices.replication.updaters;
 
+import org.apache.commons.lang.StringUtils;
+
 import ish.oncourse.model.Message;
 import ish.oncourse.webservices.v4.stubs.replication.MessageStub;
 
@@ -21,6 +23,12 @@ public class MessageUpdater extends AbstractWillowUpdater<MessageStub, Message> 
 		entity.setEmailBody(stub.getEmailBody());
 		entity.setEmailSubject(stub.getEmailSubject());
 		entity.setModified(stub.getModified());
-		entity.setSmsText(stub.getSmsText());
+		if (StringUtils.trimToNull(stub.getSmsText()) != null) {
+			entity.setSmsText(stub.getSmsText());
+		} else {
+			throw new UpdaterException(String.format(
+				"Only message with not empty sms text should be replicated! but received empty text for message with angelid:%s willowid:%s", 
+					stub.getAngelId(), entity.getId()));
+		}
 	}
 }
