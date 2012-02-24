f67839b1ed90a748fe85d3b8f9cae7e3628a3c7c
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-02-24 11:45:48 +0000

    task #000. Deny to change SMS message status from sent to any other.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18428 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/MessagePerson.java
+++ common/common-model/src/main/java/ish/oncourse/model/MessagePerson.java
@@ -1,5 +1,7 @@
 package ish.oncourse.model;
 
+import ish.common.types.MessageStatus;
+import ish.common.types.MessageType;
 import ish.oncourse.model.auto._MessagePerson;
 import ish.oncourse.utils.QueueableObjectUtils;
 
@@ -9,4 +11,25 @@ public class MessagePerson extends _MessagePerson implements Queueable {
 	public Long getId() {
 		return QueueableObjectUtils.getId(this);
 	}
+
+	/** 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setStatus(final MessageStatus status) {
+		if (getStatus() != null && MessageType.SMS.equals(getType())) {
+			switch (getStatus()) {
+			case SENT:
+				if (!MessageStatus.SENT.equals(status)) {
+					throw new IllegalStateException("We should not allow to change status from SENT to any other state! But try to change it to " + status);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		super.setStatus(status);
+	}
+	
+	
 }
