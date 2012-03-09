e03c85c62adffc45c32117298d7b615d55a24504
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2012-03-09 15:09:26 +0000

    #12463. Unit test for the 'taggables without entity willow ids' bug.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18571 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-services/src/test/java/ish/oncourse/services/lifecycle/TaggableListenerTest.java
@@ -0,0 +1,56 @@
+package ish.oncourse.services.lifecycle;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
+import org.apache.cayenne.ObjectContext;
+import org.junit.Before;
+import org.junit.Test;
+
+import ish.oncourse.model.College;
+import ish.oncourse.model.Course;
+import ish.oncourse.model.Taggable;
+import ish.oncourse.services.ServiceTestModule;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.test.ServiceTest;
+
+public class TaggableListenerTest extends ServiceTest {
+	
+	private ICayenneService cayenneService;
+	
+	@Before
+	public void setup() throws Exception {
+		initTest("ish.oncourse.services", "service", ServiceTestModule.class);
+		
+		this.cayenneService = getService(ICayenneService.class);
+	}
+	
+	@Test
+	public void testTaggableIds() {
+		ObjectContext context = cayenneService.newContext();
+		
+		College college = context.newObject(College.class);
+		college.setFirstRemoteAuthentication(new Date());
+		college.setTimeZone("Australia/Sydney");
+		college.setWebServicesSecurityCode("345ttn44$%9");
+		
+		Course course = context.newObject(Course.class);
+		course.setCollege(college);
+		course.setAngelId(1L);
+		
+		context.commitChanges();
+		
+		Taggable t = context.newObject(Taggable.class);
+		t.setCollege(college);
+		t.setEntityIdentifier("Course");
+		t.setEntityAngelId(1L);
+		
+		context.commitChanges();
+		
+		assertNotNull(t.getEntityWillowId());
+		assertEquals(course.getId(), t.getEntityWillowId());
+	}
+
+}
