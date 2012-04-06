fa5df863d2478bdb02891c3e5b55c064ff03d457
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-04-06 12:59:44 +0000

    task#13890. Remove session-managed data from replication requests and make them request-managed and each-time calculated. Now even if we process big amount of data we will not receive session timeout exception.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@18843 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/services/site/WebSiteServiceOverride.java
+++ common/common-services/src/main/java/ish/oncourse/services/site/WebSiteServiceOverride.java
@@ -5,9 +5,7 @@ import ish.oncourse.model.WebHostName;
 import ish.oncourse.model.WebSite;
 import ish.oncourse.model.access.SessionToken;
 import ish.oncourse.services.system.ICollegeService;
-
 import java.util.List;
-
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.services.Session;
@@ -32,11 +30,8 @@ public class WebSiteServiceOverride implements IWebSiteService {
 
 	@Override
 	public College getCurrentCollege() {
-
 		Session session = request.getSession(false);
-		
 		Long collegeId = null;
-		
 		if (session != null && !session.isInvalidated()) {
 			if (session.getAttribute(SessionToken.SESSION_TOKEN_KEY) != null) {
 				SessionToken token = (SessionToken) session.getAttribute(SessionToken.SESSION_TOKEN_KEY);
@@ -45,13 +40,14 @@ public class WebSiteServiceOverride implements IWebSiteService {
 			else if (session.getAttribute(College.REQUESTING_COLLEGE_ATTRIBUTE) != null) {
 				collegeId = (Long) session.getAttribute(College.REQUESTING_COLLEGE_ATTRIBUTE);
 			}
-		}
-		else {
-			if (request.getAttribute(College.REQUESTING_COLLEGE_ATTRIBUTE) != null) {
+		} else {
+			if (request.getAttribute(SessionToken.SESSION_TOKEN_KEY) != null) {
+				SessionToken token = (SessionToken) request.getAttribute(SessionToken.SESSION_TOKEN_KEY);
+				collegeId = token.getCollegeId();
+			} else if (request.getAttribute(College.REQUESTING_COLLEGE_ATTRIBUTE) != null) {
 				collegeId = (Long) request.getAttribute(College.REQUESTING_COLLEGE_ATTRIBUTE);
 			}
 		}
-		
 		if (collegeId == null) {
 			return null;
 		}
