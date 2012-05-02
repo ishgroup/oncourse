2865a0f3328b709556c5d7902bb929aeac00e344
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-05-02 14:04:59 +0000

    #11634 Login page style has been adjusted,  the validation messages are to the right of each field.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@19178 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/webapp/js/script.js
+++ portal/src/main/webapp/js/script.js
@@ -22,3 +22,38 @@ jQuery(document).ready(function() {
 	/*timetable*/
 	jQuery('.timetable .timetable-item:first-child').addClass('first');
 });
+
+function initHints(parentBlockId){
+    parentBlockToUpdate='';
+    if(parentBlockId!=null){
+        parentBlockToUpdate = '#'+parentBlockId+' ';
+    }
+    // Show us hints on entering the field if the input is not invalid
+    jQuery(parentBlockToUpdate+'span.valid input').bind("focus blur", function() {
+        parent=jQuery(this).next().next();
+        if(parent.children('.hint').length==0){
+            //for the inputs that have one more image before hint (like calendar tapestry component)
+            parent=parent.next();
+        }
+        parent.children('.hint').toggleClass('hidden-text');
+        //$j(this).parent().nextAll('.hint:first').toggle();
+    });
+
+    // Show us reasons for errors on entering the field if the input IS invalid
+    jQuery(parentBlockToUpdate+'span.validate input').bind("focus blur", function() {
+        parent=jQuery(this).next().next();
+        if(parent.children('.reason').length==0){
+            //for the inputs that have one more image before hint (like calendar tapestry component)
+            parent=parent.next();
+        }
+        parent.children('.reason').toggleClass('hidden-text');
+    });
+    jQuery(parentBlockToUpdate+'span.validate select').bind("focus blur", function() {
+        parent=jQuery(this).next().next();
+        if(parent.children('.reason').length==0){
+            //for the inputs that have one more image before hint (like calendar tapestry component)
+            parent=parent.next();
+        }
+        parent.children('.reason').toggleClass('hidden-text');
+    });
+}
