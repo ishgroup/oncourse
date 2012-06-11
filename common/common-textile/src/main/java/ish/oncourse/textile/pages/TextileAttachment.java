0c4a791efa748fea520e46990e6b8b1b1679bd9a
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-06-11 14:52:43 +0000

    #13807 TextileAttachment has been adjusted to show attachment as it shown in course page.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@19814 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-textile/src/main/java/ish/oncourse/textile/pages/TextileAttachment.java
+++ common/common-textile/src/main/java/ish/oncourse/textile/pages/TextileAttachment.java
@@ -4,13 +4,12 @@ import ish.oncourse.model.BinaryInfo;
 import ish.oncourse.services.binary.IBinaryDataService;
 import ish.oncourse.services.textile.TextileUtil;
 import ish.oncourse.services.textile.attrs.AttachmentTextileAttributes;
-
-import java.util.Map;
-
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
+
+import java.util.Map;
 
 public class TextileAttachment {
 	
@@ -20,34 +19,17 @@ public class TextileAttachment {
 	@Inject
 	private IBinaryDataService binaryDataService;
 	
-	@SuppressWarnings("all")
-	@Property
-	private String attachmentLink;
-	
-	@SuppressWarnings("all")
-	@Property
-	private String attachmentTitle;
-	
-	@Property
-	private String attachmentName;
+    @Property
+    private BinaryInfo attachment;
 	
 	@SetupRender
 	void beforeRender() {
 		@SuppressWarnings("unchecked")
 		Map<String, String> tagParams = (Map<String, String>) request.getAttribute(TextileUtil.TEXTILE_ATTACHMENT_PAGE_PARAM);
 		
-		attachmentName = tagParams.get(AttachmentTextileAttributes.ATTACHMENT_PARAM_NAME.getValue());
-		
-		BinaryInfo binaryInfo = binaryDataService.getBinaryInfo(BinaryInfo.NAME_PROPERTY, attachmentName);
-		
-		if (binaryInfo != null) {
-			attachmentLink = binaryInfo.getId() != null ? "/asset/binarydata?id=" + binaryInfo.getId() : "";
-			
-			String size = binaryInfo.getByteSize() != null ? String.valueOf(binaryInfo.getByteSize() / 1024) + " kB" : "";
-			String type = binaryInfo.getMimeType() != null ? binaryInfo.getMimeType() : "";
-			
-			attachmentTitle = attachmentName + " " + size + " " + type;
-		}
+		String attachmentName = tagParams.get(AttachmentTextileAttributes.ATTACHMENT_PARAM_NAME.getValue());
+
+        attachment = binaryDataService.getBinaryInfo(BinaryInfo.NAME_PROPERTY, attachmentName);
 	}
 
 }
