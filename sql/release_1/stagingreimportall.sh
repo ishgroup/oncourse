281ccd24880e5c43619bfe2e6ad0c771a6bc4cae
Daniel Solsona <daniel@ish.com.au>
2012-06-13 06:54:44 +0000

    #0000 SQL to update staging lightbulb homepage after the dump from willow1 .
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@19862 fb2e276d-fae9-0310-b578-b71ac85840b9

--- sql/release_1/stagingreimportall.sh
+++ sql/release_1/stagingreimportall.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-ids="3184 1234"
+ids="3184 2291"
 #111, 3014, 2703, 2074, 2006, 1222, 10,3699, 19, 3219, 2396, 2956, 3861, 299, 334, 2871, 338, 3093, 12, 50, 1296, 272, 6, 3768 have been completed, 3438 tas test, removed
 
 for id in ${ids}; do
@@ -11,6 +11,7 @@ done
 #Update the HomePage of CityEast with new content 
 #/usr/local/bin/mysql -h 10.100.33.2 -ustagingrestore -pneiDu9Oi w2staging_college < eastHomePageContent.sql
 #/usr/local/bin/mysql -h 10.100.33.2 -ustagingrestore -pneiDu9Oi w2staging_college < aftrs.sql
+/usr/local/bin/mysql -h 10.100.33.2 -ustagingrestore -pneiDu9Oi w2staging_college < lightbulb.sql
 
 #### start a full solr index
 cd /var/tomonCourseStaging/scripts; ./runsolrindex.sh -a full
