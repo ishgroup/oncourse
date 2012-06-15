a6dfa7ba4e1005f16d2400972ef65db01dfd0e02
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-06-15 11:48:32 +0000

    task#13332. Add additional check for case when db have corrupted relationships or object returned in hollow status (weren't load with course class)
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@19909 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/Course.java
+++ common/common-model/src/main/java/ish/oncourse/model/Course.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.cayenne.PersistenceState;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.SelectQuery;
+
 public class Course extends _Course implements Queueable {
 	private static final long serialVersionUID = 254942637990278217L;
 	public static final String COURSE_TAG = "courseTag";
@@ -62,14 +66,32 @@ public class Course extends _Course implements Queueable {
 	public List<Module> getModules() {
 		final List<Module> result = new ArrayList<Module>();
 		for (final CourseModule courseModule : getCourseModules()) {
-			result.add(courseModule.getModule());
+			Module module = courseModule.getModule();
+			if (module != null && module.getPersistenceState() == PersistenceState.HOLLOW) {
+				final SelectQuery moduleQuery = new SelectQuery(Module.class);
+				moduleQuery.andQualifier(ExpressionFactory.matchDbExp(Module.ID_PK_COLUMN, module.getId()));
+				@SuppressWarnings("unchecked")
+				List<Module> queryResult = getObjectContext().performQuery(moduleQuery);
+				module = queryResult.isEmpty() ? null : queryResult.get(0); 
+			}
+			if (module != null) {
+				result.add(module);
+			}
 		}
 		return result;
 	}
 
 	@Override
 	public Qualification getQualification() {
-		return super.getQualification();
+		Qualification qualification = super.getQualification();
+		if (qualification != null && qualification.getPersistenceState() == PersistenceState.HOLLOW) {
+			final SelectQuery qualificationQuery = new SelectQuery(Qualification.class);
+			qualificationQuery.andQualifier(ExpressionFactory.matchDbExp(Qualification.ID_PK_COLUMN, qualification.getId()));
+			@SuppressWarnings("unchecked")
+			List<Qualification> result = getObjectContext().performQuery(qualificationQuery);
+			qualification = result.isEmpty() ? null : result.get(0);
+		}
+		return qualification;
 	}
 	
 }
