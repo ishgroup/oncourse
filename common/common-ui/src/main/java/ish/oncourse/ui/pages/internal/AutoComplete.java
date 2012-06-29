94673d4329b04a45e7771245d577d938b419f9d8
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-06-29 10:13:44 +0000

    task#14742. Add validation to be sure that autocomplete search operate with correct values. Return empty result in other case.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20124 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/AutoComplete.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/AutoComplete.java
@@ -1,6 +1,8 @@
 package ish.oncourse.ui.pages.internal;
 
 import ish.oncourse.services.search.ISearchService;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.tapestry5.StreamResponse;
@@ -20,11 +22,20 @@ public class AutoComplete {
 
 	StreamResponse onActionFromSub() {
 		String term = request.getParameter("term");
-
-		SolrDocumentList responseResults = searchService.searchSuburbs(term)
-				.getResults();
-
-		JSONArray array = new JSONArray();
+		final JSONArray array = new JSONArray();
+		if (StringUtils.trimToNull(term) == null || StringUtils.trimToNull(term).length() < 3) {
+			//this is incorrect state which mean that next js code not able to evaluate input item value in some browser or pass less then 3 characters 
+			// so we should return empty result.Used to avoid search fail in #14742 task.
+			/*
+			 * $j(".suburb-autocomplete").autocomplete({source: '/ui/internal/autocomplete.sub', minLength: 3, 
+			 *	select: function(event, ui) {
+			 *	setPostcodeAndStateFromSuburb(this.form, ui.item.value);
+			 *	} 
+			 *	});
+			 */
+			return new TextStreamResponse("text/json", array.toString());
+		}
+		SolrDocumentList responseResults = searchService.searchSuburbs(term).getResults();
 
 		for (SolrDocument doc : responseResults) {
 			String val = (String) doc.getFieldValue("suburb") + " "
