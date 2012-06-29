2c79cdf26a03ecab52632a1f4424fda2ac332332
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-06-29 10:37:08 +0000

    #14742  small refactoring
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20125 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/AutoComplete.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/AutoComplete.java
@@ -1,7 +1,6 @@
 package ish.oncourse.ui.pages.internal;
 
 import ish.oncourse.services.search.ISearchService;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
@@ -12,44 +11,47 @@ import org.apache.tapestry5.json.JSONObject;
 import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.util.TextStreamResponse;
 
+import static ish.oncourse.services.search.SolrQueryBuilder.FIELD_postcode;
+import static ish.oncourse.services.search.SolrQueryBuilder.FIELD_suburb;
+
 public class AutoComplete {
 
-	@Inject
-	private Request request;
+    private final static String REQUEST_PARAM = "term";
+    @Inject
+    private Request request;
 
-	@Inject
-	private ISearchService searchService;
+    @Inject
+    private ISearchService searchService;
 
-	StreamResponse onActionFromSub() {
-		String term = request.getParameter("term");
-		final JSONArray array = new JSONArray();
-		if (StringUtils.trimToNull(term) == null || StringUtils.trimToNull(term).length() < 3) {
-			//this is incorrect state which mean that next js code not able to evaluate input item value in some browser or pass less then 3 characters 
-			// so we should return empty result.Used to avoid search fail in #14742 task.
-			/*
-			 * $j(".suburb-autocomplete").autocomplete({source: '/ui/internal/autocomplete.sub', minLength: 3, 
-			 *	select: function(event, ui) {
-			 *	setPostcodeAndStateFromSuburb(this.form, ui.item.value);
-			 *	} 
-			 *	});
-			 */
-			return new TextStreamResponse("text/json", array.toString());
-		}
-		SolrDocumentList responseResults = searchService.searchSuburbs(term).getResults();
+    StreamResponse onActionFromSub() {
+        String term = request.getParameter(REQUEST_PARAM);
+        final JSONArray array = new JSONArray();
 
-		for (SolrDocument doc : responseResults) {
-			String val = (String) doc.getFieldValue("suburb") + " "
-					+ (String) doc.getFieldValue("postcode");
-			
-			JSONObject obj = new JSONObject();
-			
-			obj.put("id", val);
-			obj.put("label", val);
-			obj.put("value", val);
-			
-			array.put(obj);
-		}
+        //this is incorrect state which mean that next js code not able to evaluate input item value in some browser or pass less then 3 characters
+        // so we should return empty result.Used to avoid search fail in #14742 task.
+        /*
+           * $j(".suburb-autocomplete").autocomplete({source: '/ui/internal/autocomplete.sub', minLength: 3,
+           *	select: function(event, ui) {
+           *	setPostcodeAndStateFromSuburb(this.form, ui.item.value);
+           *	}
+           *	});
+        */
+        if (StringUtils.trimToNull(term) == null || StringUtils.trimToNull(term).length() < 3) {
+            SolrDocumentList responseResults = searchService.searchSuburbs(term).getResults();
 
-		return new TextStreamResponse("text/json", array.toString());
-	}
+            for (SolrDocument doc : responseResults) {
+                String val = doc.getFieldValue(FIELD_suburb) + " "
+                        + doc.getFieldValue(FIELD_postcode);
+
+                JSONObject obj = new JSONObject();
+
+                obj.put("id", val);
+                obj.put("label", val);
+                obj.put("value", val);
+
+                array.put(obj);
+            }
+        }
+        return new TextStreamResponse("text/json", array.toString());
+    }
 }
