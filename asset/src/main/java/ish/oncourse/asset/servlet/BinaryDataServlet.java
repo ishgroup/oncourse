f02c4a2d085064857f9e328dc0c42626f7655ef6
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-06-30 08:57:12 +0000

    task#14743. Add parameter validation to avoid internal errors.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20142 fb2e276d-fae9-0310-b578-b71ac85840b9

--- asset/src/main/java/ish/oncourse/asset/servlet/BinaryDataServlet.java
+++ asset/src/main/java/ish/oncourse/asset/servlet/BinaryDataServlet.java
@@ -10,33 +10,35 @@ import java.io.OutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+
 public class BinaryDataServlet extends ServiceAwareServlet {
+	private static final String DIGIT_REGEXP = "\\d+";
+	private static final String DOT_CHARACTER = ".";
+	private static final String BINARY_INFO_ID_PARAMETER = "id";
 	private static final long serialVersionUID = 1376738011903521168L;
 
 	@Override
 	public synchronized void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
 		IBinaryDataService service = getService(IBinaryDataService.class);
-
-		BinaryInfo binaryInfo = service.getBinaryInfoById(req.getParameter("id"));
-
-		if (binaryInfo != null) {
-			String mimeType = binaryInfo.getMimeType();
-			resp.setContentType(mimeType);
-			String filename = binaryInfo.getName();
-			if (!binaryInfo.getType().isEmpty()) {
-				filename += "." + binaryInfo.getType();
+		final String idParameter = req.getParameter(BINARY_INFO_ID_PARAMETER);
+		if (idParameter != null && idParameter.matches(DIGIT_REGEXP)) {
+			BinaryInfo binaryInfo = service.getBinaryInfoById(idParameter);
+			if (binaryInfo != null) {
+				resp.setContentType(binaryInfo.getMimeType());
+				final StringBuilder fileName = new StringBuilder(binaryInfo.getName());
+				if (StringUtils.trimToNull(binaryInfo.getType()) != null) {
+					fileName.append(DOT_CHARACTER).append(binaryInfo.getType());
+				}
+				resp.setHeader("Content-disposition", "filename=" + fileName.toString());
+				OutputStream out = resp.getOutputStream();
+				final BinaryData binaryData = binaryInfo.getBinaryData();
+				if (binaryData != null) {
+					byte[] content = binaryData.getContent();
+					out.write(content);
+				}
+				out.close();
 			}
-			resp.setHeader("Content-disposition", "filename=" + filename);
-
-			OutputStream out = resp.getOutputStream();
-
-			BinaryData binaryData = binaryInfo.getBinaryData();
-			if (binaryData != null) {
-				byte[] content = binaryData.getContent();
-				out.write(content);
-			}
-			out.close();
 		}
 	}
 }
