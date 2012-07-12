fd6430c9a08fe82b6b00a4519736ecb8e73aae5e
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-07-12 13:09:12 +0000

    #14747 the upgrade has been adjusted to deploy application without shutdown instances.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20290 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/utils/MigrateReferencesUpgrage.java
+++ common/common-model/src/main/java/ish/oncourse/utils/MigrateReferencesUpgrage.java
@@ -1,13 +1,5 @@
 package ish.oncourse.utils;
 
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
 import liquibase.change.custom.CustomSqlChange;
 import liquibase.database.Database;
 import liquibase.exception.CustomChangeException;
@@ -16,85 +8,87 @@ import liquibase.exception.ValidationErrors;
 import liquibase.resource.ResourceAccessor;
 import liquibase.statement.SqlStatement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MigrateReferencesUpgrage implements CustomSqlChange {
-	private String oncourseSchema;
-	private String oncourseReferenceSchema;
-	private Statement statement;
-	@Override
-	public String getConfirmationMessage() {
-		return "References tables migrated successfully.";
-	}
+    private String oncourseSchema;
+    private String oncourseReferenceSchema;
+    private Statement statement;
 
-	@Override
-	public void setUp() throws SetupException {
-		try {
-			Context context = (Context) new InitialContext().lookup("java:comp/env/jdbc");
-			DataSource ds1 = (DataSource) context.lookup("/oncourse");
-			DataSource ds2 = (DataSource) context.lookup("/oncourse_reference");
-			if (ds1 != null && !ds1.getConnection().isClosed() && !ds1.getConnection().isReadOnly()) {
-				oncourseSchema = ds1.getConnection().getCatalog();
-			} else {
-				throw new SetupException("oncourse connection closed or read only");
-			}
-			if (ds2 != null && !ds2.getConnection().isClosed() && !ds2.getConnection().isReadOnly()) {
-				oncourseReferenceSchema = ds2.getConnection().getCatalog();
-			}  else {
-				throw new SetupException("oncourse_reference connection closed or read only");
-			}
-			if (oncourseSchema != null && oncourseReferenceSchema != null) {
-				statement = ds1.getConnection().createStatement();
-			}
-		} catch (NamingException e) {
-			throw new SetupException(e);
-		} catch (SQLException e) {
-			throw new SetupException(e);
-		}
-	}
+    @Override
+    public String getConfirmationMessage() {
+        return "References tables migrated successfully.";
+    }
 
-	@Override
-	public void setFileOpener(ResourceAccessor resourceAccessor) {		
-	}
+    @Override
+    public void setUp() throws SetupException {
+        try {
+            Context context = (Context) new InitialContext().lookup("java:comp/env/jdbc");
+            DataSource ds1 = (DataSource) context.lookup("/oncourse");
+            DataSource ds2 = (DataSource) context.lookup("/oncourse_reference");
+            if (ds1 != null && !ds1.getConnection().isClosed() && !ds1.getConnection().isReadOnly()) {
+                oncourseSchema = ds1.getConnection().getCatalog();
+            } else {
+                throw new SetupException("oncourse connection closed or read only");
+            }
+            if (ds2 != null && !ds2.getConnection().isClosed() && !ds2.getConnection().isReadOnly()) {
+                oncourseReferenceSchema = ds2.getConnection().getCatalog();
+            } else {
+                throw new SetupException("oncourse_reference connection closed or read only");
+            }
+            if (oncourseSchema != null && oncourseReferenceSchema != null) {
+                statement = ds1.getConnection().createStatement();
+            }
+        } catch (NamingException e) {
+            throw new SetupException(e);
+        } catch (SQLException e) {
+            throw new SetupException(e);
+        }
+    }
 
-	@Override
-	public ValidationErrors validate(Database database) {
-		return null;
-	}
+    @Override
+    public void setFileOpener(ResourceAccessor resourceAccessor) {
+    }
 
-	@Override
-	public SqlStatement[] generateStatements(Database database) throws CustomChangeException {
-		if (statement != null) {
-			try {
-				//delete tables with the same name if exists
-				statement.execute(String.format("drop table if exists %s.Country;", oncourseSchema));
-				statement.execute(String.format("drop table if exists %s.Language;", oncourseSchema));
-				statement.execute(String.format("drop table if exists %s.Module;", oncourseSchema));
-				statement.execute(String.format("drop table if exists %s.Qualification;", oncourseSchema));
-				statement.execute(String.format("drop table if exists %s.TrainingPackage;", oncourseSchema));
-				statement.execute(String.format("drop table if exists %s.postcode;", oncourseSchema));
-				statement.execute(String.format("drop table if exists %s.postcode_db;", oncourseSchema));
-				//now move data
-				statement.execute(String.format("alter table %s.Country rename %s.Country; ", oncourseReferenceSchema, oncourseSchema));
-				statement.execute(String.format("alter table %s.Language rename %s.Language; ", oncourseReferenceSchema, oncourseSchema));
-				statement.execute(String.format("alter table %s.Module rename %s.Module; ", oncourseReferenceSchema, oncourseSchema));
-				statement.execute(String.format("alter table %s.Qualification rename %s.Qualification; ", oncourseReferenceSchema, oncourseSchema));
-				statement.execute(String.format("alter table %s.TrainingPackage rename %s.TrainingPackage; ", oncourseReferenceSchema, oncourseSchema));
-				statement.execute(String.format("alter table %s.postcode rename %s.postcode; ", oncourseReferenceSchema, oncourseSchema));
-				statement.execute(String.format("alter table %s.postcode_db rename %s.postcode_db; ", oncourseReferenceSchema, oncourseSchema));
-				if (!statement.getConnection().getAutoCommit()) {
-					statement.getConnection().commit();
-				}
-				return null;
-			} catch (SQLException e) {
-				/*try {
-					statement.getConnection().rollback();
-				} catch (SQLException e1) {
-					throw new CustomChangeException(e1);
-				}*/
-				throw new CustomChangeException(e);
-			}
-		} else {
-			throw new CustomChangeException("Failed to execute reference tables migration");
-		}
-	}
+    @Override
+    public ValidationErrors validate(Database database) {
+        return null;
+    }
+
+    @Override
+    public SqlStatement[] generateStatements(Database database) throws CustomChangeException {
+        if (statement != null) {
+            try {
+                statement.execute("SET foreign_key_checks = 0;");
+                migrateTable("Country");
+                migrateTable("Language");
+                migrateTable("TrainingPackage");
+                migrateTable("Qualification");
+                migrateTable("Module");
+                migrateTable("postcode");
+                migrateTable("postcode_db");
+                statement.execute("SET foreign_key_checks = 1;");
+                if (!statement.getConnection().getAutoCommit()) {
+                    statement.getConnection().commit();
+                }
+                return null;
+            } catch (SQLException e) {
+                throw new CustomChangeException(e);
+            }
+        } else {
+            throw new CustomChangeException("Failed to execute reference tables migration");
+        }
+    }
+
+    private void migrateTable(String tableName) throws SQLException {
+        statement.execute(String.format("drop table if exists %s.%s;", oncourseSchema, tableName));
+        statement.execute(String.format("create table %s.%s like %s.%s;", oncourseSchema, tableName, oncourseReferenceSchema, tableName));
+        statement.execute(String.format("insert into %s.%s SELECT * FROM %s.%s;", oncourseSchema , tableName, oncourseReferenceSchema, tableName));
+    }
 
 }
