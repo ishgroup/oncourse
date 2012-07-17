17bdd1afdc0bba731b840ca911284477fae66f2a
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2012-07-17 10:27:32 +0000

    #14181. Unit tests adjustment.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20344 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/test/java/ish/oncourse/admin/services/billing/BillingDataServiceImplTest.java
+++ admin/src/test/java/ish/oncourse/admin/services/billing/BillingDataServiceImplTest.java
@@ -19,83 +19,92 @@ import static org.junit.Assert.assertTrue;
 
 public class BillingDataServiceImplTest {
 
-    @Test
-    public void testHostingFormat()
-    {
-        String original = "DI\tCHERSON01\tOCW-21\tonCourse standard hosting plan, 1 year valid to March, 2013\t1\t1234567.890\tonCourse March, 2012\n";
-       Map<Long, Map<String, Object>> licenseData = new HashMap<Long, Map<String, Object>>();
+	@Test
+	public void testHostingFormat()
+	{
+		String original = "DI\tCHERSON01\tOCW-21\tonCourse standard hosting plan for Cherson: March, 2013 (contract until March, 2013)\t1\t1234567.890\tonCourse March, 2012\n";
+		Map<Long, Map<String, Object>> licenseData = new HashMap<Long, Map<String, Object>>();
+		Map<String,Object> cData = new HashMap<String, Object>();
+		cData.put(MWExportFormat.HostingFormat.getPlanKey(), BillingPlan.standard.name());
+		cData.put(MWExportFormat.HostingFormat.getValueKey(), 1234567.89d);
+		licenseData.put(1L, cData);
 
-       Map<String,Object> cData = new HashMap<String, Object>();
-        cData.put(MWExportFormat.HostingFormat.getPlanKey(), BillingPlan.standard.name());
-        cData.put(MWExportFormat.HostingFormat.getValueKey(), 1234567.89d);
-        licenseData.put(1L, cData);
+		College college = new College()
+		{
+			@Override
+			public Long getId() {
+				return 1L;
+			}
 
-        College college = new College()
-        {
-            @Override
-            public Long getId() {
-                return 1L;
-            }
+			@Override
+			public String getBillingCode() {
+				return "CHERSON01";
+			}
+			
+			@Override
+			public String getName() {
+				return "Cherson";
+			}
+		};
 
-            @Override
-            public String getBillingCode() {
-                return "CHERSON01";
-            }
-        };
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2012);
+		calendar.set(Calendar.MONTH, 2);
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 2012);
-        calendar.set(Calendar.MONTH, 2);
+		SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATE_MONTH_FORMAT);
+		String monthAndYear = dateFormat.format(calendar.getTime());
+		String description = "onCourse " + monthAndYear;
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATE_MONTH_FORMAT);
-        String monthAndYear = dateFormat.format(calendar.getTime());
-        String description = "onCourse " + monthAndYear;
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2013);
+		calendar.set(Calendar.MONTH, 2);
+		String result = MWExportFormat.HostingFormat.format(licenseData, college, calendar.getTime(), calendar.getTime(), description);
+		assertEquals("Check result message", result,original);
+	}
 
-        calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 2013);
-        calendar.set(Calendar.MONTH, 2);
-        String result = MWExportFormat.HostingFormat.format(licenseData, college, calendar.getTime(), description);
-        assertEquals("Check result message", result,original);
-    }
+	@Test
+	public void testSupportFormat()
+	{
+		String original = "DI\tCHERSON01\tOCW-21\tonCourse standard support plan for Cherson: March, 2013 (contract until March, 2013)\t1\t1234567.890\tonCourse March, 2012\n";
+		Map<Long, Map<String, Object>> licenseData = new HashMap<Long, Map<String, Object>>();
 
-    @Test
-    public void testSupportFormat()
-    {
-        String original = "DI\tCHERSON01\tOCW-21\tonCourse standard support plan, 1 year valid to March, 2013\t1\t1234567.890\tonCourse March, 2012\n";
-        Map<Long, Map<String, Object>> licenseData = new HashMap<Long, Map<String, Object>>();
+		Map<String,Object> cData = new HashMap<String, Object>();
+		cData.put(MWExportFormat.SupportFormat.getPlanKey(), BillingPlan.standard.name());
+		cData.put(MWExportFormat.SupportFormat.getValueKey(), 1234567.89d);
+		licenseData.put(1L, cData);
 
-        Map<String,Object> cData = new HashMap<String, Object>();
-        cData.put(MWExportFormat.SupportFormat.getPlanKey(), BillingPlan.standard.name());
-        cData.put(MWExportFormat.SupportFormat.getValueKey(), 1234567.89d);
-        licenseData.put(1L, cData);
+		College college = new College()
+		{
+			@Override
+			public Long getId() {
+				return 1L;
+			}
 
-        College college = new College()
-        {
-            @Override
-            public Long getId() {
-                return 1L;
-            }
+			@Override
+			public String getBillingCode() {
+				return "CHERSON01";
+			}
+			
+			@Override
+			public String getName() {
+				return "Cherson";
+			}
+		};
 
-            @Override
-            public String getBillingCode() {
-                return "CHERSON01";
-            }
-        };
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2012);
+		calendar.set(Calendar.MONTH, 2);
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 2012);
-        calendar.set(Calendar.MONTH, 2);
+		SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATE_MONTH_FORMAT);
+		String monthAndYear = dateFormat.format(calendar.getTime());
+		String description = "onCourse " + monthAndYear;
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.DATE_MONTH_FORMAT);
-        String monthAndYear = dateFormat.format(calendar.getTime());
-        String description = "onCourse " + monthAndYear;
-
-        calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 2013);
-        calendar.set(Calendar.MONTH, 2);
-        String result = MWExportFormat.SupportFormat.format(licenseData, college, calendar.getTime(), description);
-        assertEquals("Check result message", result,original);
-    }
+		calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2013);
+		calendar.set(Calendar.MONTH, 2);
+		String result = MWExportFormat.SupportFormat.format(licenseData, college, calendar.getTime(), calendar.getTime(), description);
+		assertEquals("Check result message", result,original);
+	}
 
 
 }
