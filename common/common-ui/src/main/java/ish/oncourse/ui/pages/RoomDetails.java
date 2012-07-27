0150c76f1cfe8d164c3e0be14c546e5e6a5ebb58
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-07-27 11:44:42 +0000

    #13538 RoomDetails template has been adjusted to show room name and room directions
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20540 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/pages/RoomDetails.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/pages/RoomDetails.java
@@ -1,7 +1,9 @@
 package ish.oncourse.ui.pages;
 
 import ish.oncourse.model.Room;
-
+import ish.oncourse.services.textile.ITextileConverter;
+import ish.oncourse.util.ValidationErrors;
+import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -9,7 +11,10 @@ import org.apache.tapestry5.services.Request;
 
 public class RoomDetails {
 
-	@Inject
+    @Inject
+    private ITextileConverter textileConverter;
+
+    @Inject
 	private Request request;
 
 	@Property
@@ -19,5 +24,13 @@ public class RoomDetails {
 	public void beforeRender() {
 		room = (Room) request.getAttribute(Room.class.getSimpleName());
 	}
+
+    public String getDirections() {
+        if (room != null && StringUtils.trimToNull(room.getDirections()) != null)
+            return textileConverter.convertCustomTextile(room.getDirections(), new ValidationErrors());
+        else
+            return null;
+    }
+
 
 }
