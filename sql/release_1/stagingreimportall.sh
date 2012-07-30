5a63ee0407027a9686163c1617201e6b73662307
Daniel Solsona <daniel@ish.com.au>
2012-07-30 08:56:19 +0000

    #0000 Adding lightbulb again, test subject #1
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20567 fb2e276d-fae9-0310-b578-b71ac85840b9

--- sql/release_1/stagingreimportall.sh
+++ sql/release_1/stagingreimportall.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-ids="3184"
+ids="3184 2291"
 #111, 3014, 2703, 2074, 2006, 1222, 10,3699, 19, 3219, 2396, 2956, 3861, 299, 334, 2871, 338, 3093, 12, 50, 1296, 272, 6, 3768 have been completed, 3438 tas test, removed
 
 for id in ${ids}; do
@@ -9,6 +9,7 @@ done
 
 #### Do some miscellaneous SQL
 /usr/local/bin/mysql -h 10.100.33.2 -ustagingrestore -pneiDu9Oi w2staging_college < tasmania.sql
+/usr/local/bin/mysql -h 10.100.33.2 -ustagingrestore -pneiDu9Oi w2staging_college < lightbulb.sql
 
 #### start a full solr index
 cd /var/tomonCourseStaging/scripts; ./runsolrindex.sh -a full
