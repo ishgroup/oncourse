07fa34392c184f31b85a8f068eaf0ac6a2a1d7d6
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-08-21 14:16:24 +0000

    task#12839. Add logic to load the build info
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20971 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/mbean/ApplicationData.java
+++ common/common-services/src/main/java/ish/oncourse/mbean/ApplicationData.java
@@ -1,24 +1,52 @@
 package ish.oncourse.mbean;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import ish.oncourse.services.persistence.ICayenneService;
 import javax.management.AttributeChangeNotification;
 import javax.management.MBeanNotificationInfo;
 import javax.management.NotificationBroadcasterSupport;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class ApplicationData extends NotificationBroadcasterSupport implements ApplicationDataMBean {
-
+	private static Logger LOGGER = Logger.getLogger(ApplicationData.class);	
 	private final String version;
 	private final String application;
+	private final Properties properties = new Properties();
 	
 	@Inject
 	private ICayenneService cayenneService;
 	
-	public ApplicationData(final String version, final String application) {
+	public ApplicationData(final String application) {
 		super();
-		this.version = version;
 		this.application = application;
+		loadProperties();
+		String version = properties.getProperty(VERSION_PROPERTY);
+		this.version  = isCorrectVersion(version) ? version : UNKNOWN_VERSION_VALUE;
+	}
+	
+	private boolean isCorrectVersion(final String version) {
+		return StringUtils.trimToNull(version) != null && !DEFAULT_PROJECT_VERSION.equalsIgnoreCase(version);
+	}
+	
+	private boolean isPropertiesCorrectlyInited() {
+		return !properties.isEmpty() && properties.size() == 2;
+	}
+	
+	private void loadProperties() {
+		if (!isPropertiesCorrectlyInited()) {
+			try {
+				properties.clear();
+				properties.load(getClass().getClassLoader().getResourceAsStream(BUILD_INFO_PROPERTIES_FILE));
+			} catch (IOException e) {
+				LOGGER.warn(FAILED_TO_LOAD_BUILD_INFO_PROPERTIES_FILE_MESSAGE, e);
+				properties.put(VERSION_PROPERTY, UNKNOWN_VERSION_VALUE);
+			}
+		}
 	}
 	
 	@Override
--- common/common-services/src/main/java/ish/oncourse/mbean/ApplicationDataMBean.java
+++ common/common-services/src/main/java/ish/oncourse/mbean/ApplicationDataMBean.java
@@ -1,6 +1,12 @@
 package ish.oncourse.mbean;
 
 public interface ApplicationDataMBean {
+	static final String FAILED_TO_LOAD_BUILD_INFO_PROPERTIES_FILE_MESSAGE = "Failed to load build info properties file!";
+	static final String BUILD_INFO_PROPERTIES_FILE = "buildInfo.properties";
+	static final String RELEASE_TIME_PROPERTY = "releaseTime";
+	static final String VERSION_PROPERTY = "version";
+	static final String UNKNOWN_VERSION_VALUE = "unknown version";
+	static final String DEFAULT_PROJECT_VERSION = "${currentBuildVersion}";
 	public String getVersion();
 	public String getApplication();
 	public boolean isCayenneLoaded();
