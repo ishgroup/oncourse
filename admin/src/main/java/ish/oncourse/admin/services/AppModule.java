1206318b71e596abd04bb38d2be2fe7f87bdc9cd
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-08-22 14:43:04 +0000

    task#12839. Final Admin version
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@20982 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/services/AppModule.java
+++ admin/src/main/java/ish/oncourse/admin/services/AppModule.java
@@ -5,17 +5,20 @@ import ish.oncourse.admin.services.billing.IBillingDataService;
 import ish.oncourse.admin.services.ntis.INTISUpdater;
 import ish.oncourse.admin.services.ntis.NTISUpdaterImpl;
 import ish.oncourse.admin.services.ntis.TrainingComponentServiceBuilder;
-import ish.oncourse.mbean.ApplicationData;
-import ish.oncourse.mbean.MBeanRegisterUtil;
 import ish.oncourse.model.services.ModelModule;
 import ish.oncourse.services.ServiceModule;
+import ish.oncourse.services.jmx.IJMXInitService;
+import ish.oncourse.services.jmx.JMXInitService;
 import ish.oncourse.services.site.IWebSiteService;
 
 import org.apache.tapestry5.MetaDataConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.EagerLoad;
 import org.apache.tapestry5.ioc.annotations.Local;
 import org.apache.tapestry5.ioc.annotations.SubModule;
+import org.apache.tapestry5.ioc.services.RegistryShutdownHub;
+import org.apache.tapestry5.services.ApplicationGlobals;
 
 import au.gov.training.services.trainingcomponent.ITrainingComponentService;
 
@@ -32,7 +35,13 @@ public class AppModule {
 		binder.bind(ITrainingComponentService.class, new TrainingComponentServiceBuilder());
 		binder.bind(INTISUpdater.class, NTISUpdaterImpl.class);
 		binder.bind(IWebSiteService.class, WebSiteServiceOverride.class).withId("WebSiteServiceAdmin");
-		MBeanRegisterUtil.registerMbeanService(new ApplicationData("admin"), "ish.oncourse:type=AdminApplicationData");
+	}
+	
+	@EagerLoad
+	public static IJMXInitService buildJMXInitService(ApplicationGlobals applicationGlobals, RegistryShutdownHub hub) {
+		JMXInitService jmxService = new JMXInitService(applicationGlobals,"admin","ish.oncourse:type=AdminApplicationData");
+		hub.addRegistryShutdownListener(jmxService);
+		return jmxService;
 	}
 
 	public void contributeMetaDataLocator(MappedConfiguration<String, String> configuration) {
