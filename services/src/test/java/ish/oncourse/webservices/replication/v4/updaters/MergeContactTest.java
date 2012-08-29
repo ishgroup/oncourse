541b5f5686e124124812701771925f4d3638d33e
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-08-29 12:43:04 +0000

    task#15437. Finish the JUnit which describe incorrect 1-1 relationship management on contacts merge.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@21129 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/test/java/ish/oncourse/webservices/replication/v4/updaters/MergeContactTest.java
+++ services/src/test/java/ish/oncourse/webservices/replication/v4/updaters/MergeContactTest.java
@@ -1,21 +1,32 @@
 package ish.oncourse.webservices.replication.v4.updaters;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.SelectQuery;
 import org.apache.commons.lang.StringUtils;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.Before;
 import org.junit.Test;
-
 import ish.oncourse.model.Contact;
 import ish.oncourse.model.Student;
 import ish.oncourse.services.persistence.ICayenneService;
 import ish.oncourse.test.ServiceTest;
+import ish.oncourse.webservices.replication.builders.WillowStubBuilderTest;
 import ish.oncourse.webservices.soap.v4.ReplicationTestModule;
 
 public class MergeContactTest extends ServiceTest {
@@ -23,6 +34,15 @@ public class MergeContactTest extends ServiceTest {
 	@Before
     public void setupDataSet() throws Exception {
         initTest("ish.oncourse.webservices.services", StringUtils.EMPTY, ReplicationTestModule.class);
+        
+        InputStream st = WillowStubBuilderTest.class.getClassLoader().getResourceAsStream("ish/oncourse/webservices/replication/v4/updaters/MergeContactTest.xml");
+        FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().build(st);
+
+        DataSource onDataSource = getDataSource("jdbc/oncourse");
+        DatabaseConnection dbConnection = new DatabaseConnection(onDataSource.getConnection(), null);
+        dbConnection.getConfig().setProperty(DatabaseConfig.FEATURE_CASE_SENSITIVE_TABLE_NAMES, false);
+
+        DatabaseOperation.CLEAN_INSERT.execute(dbConnection, dataSet);
     }
 	
 	@Test
@@ -31,23 +51,116 @@ public class MergeContactTest extends ServiceTest {
 		List<Contact> data = createTwoContacts(objectContext);
 		Contact contact1 = data.get(0);
 		Contact contact2 = data.get(1);
-		Student student1 = createFirstStudent(objectContext);
-		contact1.setStudent(student1);
+		Student student1 = contact1.getStudent();
+		objectContext.commitChanges();
 		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
 		assertNotNull("student 1 linked with the contact 1", student1.getContact());
 		assertNull("contact 2 have no link to students", contact2.getStudent());
-		//contact2.setStudent(student1);
-		//use the orthodox setter logic
-		contact2.setToOneTarget(Contact.STUDENT_PROPERTY, student1, true);
-		assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
-		assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
-		assertNotNull("student 1 linked with the contact 2", student1.getContact());
-		assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
-		//Next 2 asserts should fail because ToOneTarget should handle both sides relationship
-		assertNotNull("contact 1 have no link to students", contact1.getStudent());
-		assertTrue("Student 1 have link with the contact 2", contact1.getStudent().getContact().equals(contact2));
+		try {
+			//contact2.setStudent(student1);
+			//use the orthodox setter logic
+			contact2.setToOneTarget(Contact.STUDENT_PROPERTY, student1, true);
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+
+			assertNull("contact 1 have no link to students", contact1.getStudent());
+		} finally {
+			objectContext.rollbackChanges();
+		}
+	}
+	
+	@Test
+	public void testContactsStudentsMergeFail() {
+		final ObjectContext objectContext = getService(ICayenneService.class).newContext();
+		List<Contact> data = createTwoMoreContacts(objectContext);
+		Contact contact1 = data.get(0);
+		Contact contact2 = data.get(1);
+		Student student1 = contact1.getStudent();
+		Student student2 = contact2.getStudent();
+		objectContext.commitChanges();
+		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
+		assertNotNull("student 1 linked with the contact 1", student1.getContact());
+		assertNotNull("contact 2 linked with the student 2", contact2.getStudent());
+		assertNotNull("student 2 linked with the contact 2", student2.getContact());
 		
-		objectContext.rollbackChanges();
+		try {
+			//contact2.setStudent(student1);
+			//use the orthodox setter logic
+			contact2.setToOneTarget(Contact.STUDENT_PROPERTY, student1, true);
+			//objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			
+			assertNull("contact 1 have no more link to students", contact1.getStudent());
+		
+			//contact1.setStudent(student2);
+			//use the orthodox setter logic
+			contact1.setToOneTarget(Contact.STUDENT_PROPERTY, student2, true);
+			//objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			assertNotNull("contact 1 have no link to students", contact1.getStudent());
+			assertTrue("Student 1 have link with the contact 2 only", contact1.getStudent().getContact().equals(contact1));
+			assertTrue("Student 1 have link with the contact 2", contact1.getStudent().equals(student2));
+			boolean commitFailed = false;
+			try {
+				objectContext.commitChanges();
+			} catch (Exception e) {
+				commitFailed = true;
+			}
+			assertFalse(commitFailed);
+		} finally {
+			objectContext.rollbackChanges();
+		}
+	}
+	
+	@Test
+	public void testContactsStudentsMergeWithCheckRelationShip() {
+		final ObjectContext objectContext = getService(ICayenneService.class).newContext();
+		List<Contact> data = createTwoMoreContacts(objectContext);
+		Contact contact1 = data.get(0);
+		Contact contact2 = data.get(1);
+		Student student1 = contact1.getStudent();
+		Student student2 = contact2.getStudent();
+		objectContext.commitChanges();
+		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
+		assertNotNull("student 1 linked with the contact 1", student1.getContact());
+		assertNotNull("contact 2 linked with the student 2", contact2.getStudent());
+		assertNotNull("student 2 linked with the contact 2", student2.getContact());
+
+		try {
+			//contact2.setStudent(student1);
+			//use the correct setter logic
+			Contact.setObjectToOneTargetWithCheck(Contact.STUDENT_PROPERTY, student1, true, contact2);
+			objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			assertNull("contact 1 have no more link to students", contact1.getStudent());
+		
+			//contact1.setStudent(student2);
+			//use the correct setter logic
+			Contact.setObjectToOneTargetWithCheck(Contact.STUDENT_PROPERTY, student2, true, contact1);
+			objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			
+			assertNotNull("contact 1 have no link to students", contact1.getStudent());
+			assertTrue("Student 1 have link with the contact 2", contact1.getStudent().getContact().equals(contact1));
+			assertTrue("Student 1 have link with the contact 2", contact1.getStudent().equals(student2));
+		} finally {
+			objectContext.rollbackChanges();
+		}
 	}
 	
 	@Test
@@ -56,21 +169,24 @@ public class MergeContactTest extends ServiceTest {
 		List<Contact> data = createTwoContacts(objectContext);
 		Contact contact1 = data.get(0);
 		Contact contact2 = data.get(1);
-		Student student1 = createFirstStudent(objectContext);
-		contact1.setStudent(student1);
+		Student student1 = contact1.getStudent();
+		objectContext.commitChanges();
 		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
 		assertNotNull("student 1 linked with the contact 1", student1.getContact());
 		assertNull("contact 2 have no link to students", contact2.getStudent());
-		//contact2.setStudent(student1);
-		//use the correct setter logic
-		Contact.setObjectToOneTargetWithCheck(Contact.STUDENT_PROPERTY, student1, true, contact2);
-		assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
-		assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
-		assertNotNull("student 1 linked with the contact 2", student1.getContact());
-		assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
-		assertNull("contact 1 have no link to students", contact1.getStudent());
-		
-		objectContext.rollbackChanges();
+
+		try {
+			//contact2.setStudent(student1);
+			//use the correct setter logic
+			Contact.setObjectToOneTargetWithCheck(Contact.STUDENT_PROPERTY, student1, true, contact2);
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			assertNull("contact 1 have no link to students", contact1.getStudent());
+		} finally {
+			objectContext.rollbackChanges();
+		}
 	}
 	
 	@Test
@@ -79,28 +195,95 @@ public class MergeContactTest extends ServiceTest {
 		List<Contact> data = createTwoContacts(objectContext);
 		Contact contact1 = data.get(0);
 		Contact contact2 = data.get(1);
-		Student student1 = createFirstStudent(objectContext);
-		contact1.setStudent(student1);
+		Student student1 = contact1.getStudent();
+		objectContext.commitChanges();
 		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
 		assertNotNull("student 1 linked with the contact 1", student1.getContact());
 		assertNull("contact 2 have no link to students", contact2.getStudent());
-		//use the orthodox setter logic
-		student1.setContact(contact2);
-		assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
-		assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
-		assertNotNull("student 1 linked with the contact 2", student1.getContact());
-		assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
-		assertNull("contact 1 have no link to students", contact1.getStudent());
-		
-		objectContext.rollbackChanges();
+
+		try {
+			student1.setContact(contact2);
+			//use the orthodox setter logic in this case will be correct 
+			//student1.setToOneTarget(Student.CONTACT_PROPERTY, contact2, true);
+			objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			assertNull("contact 1 have no link to students", contact1.getStudent());
+		} finally {
+			objectContext.rollbackChanges();
+		}
+	}
+	
+	@Test
+	public void testStudentContactMergeWithCheckRelationShip() {
+		final ObjectContext objectContext = getService(ICayenneService.class).newContext();
+		List<Contact> data = createTwoMoreContacts(objectContext);
+		Contact contact1 = data.get(0);
+		Contact contact2 = data.get(1);
+		Student student1 = contact1.getStudent();
+		Student student2 = contact2.getStudent();
+		objectContext.commitChanges();
+		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
+		assertNotNull("student 1 linked with the contact 1", student1.getContact());
+		assertNotNull("contact 2 linked with the student 2", contact2.getStudent());
+		assertNotNull("student 2 linked with the contact 2", student2.getContact());
+
+		try { 
+			//student1.setContact(contact2);
+			//use the correct setter logic
+			Contact.setObjectToOneTargetWithCheck(Student.CONTACT_PROPERTY, contact2, true, student1);
+			objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			assertNull("contact 1 have no link to students", contact1.getStudent());
+
+			assertNull("student 2 should have no link to contacts", student2.getContact());
+		} finally {
+			objectContext.rollbackChanges();
+		}
+	}
+	
+	@Test
+	public void testStudentsContactsMergeFail() {
+		final ObjectContext objectContext = getService(ICayenneService.class).newContext();
+		List<Contact> data = createTwoMoreContacts(objectContext);
+		Contact contact1 = data.get(0);
+		Contact contact2 = data.get(1);
+		Student student1 = contact1.getStudent();
+		Student student2 = contact2.getStudent();
+		objectContext.commitChanges();
+		assertNotNull("contact 1 linked with the student 1", contact1.getStudent());
+		assertNotNull("student 1 linked with the contact 1", student1.getContact());
+		assertNotNull("contact 2 linked with the student 2", contact2.getStudent());
+		assertNotNull("student 2 linked with the contact 2", student2.getContact());
+
+		try { 
+			student1.setContact(contact2);
+			//use the orthodox setter logic
+			//student1.setToOneTarget(Student.CONTACT_PROPERTY, contact2, true);
+			objectContext.commitChanges();
+			assertNotNull("contact 2 linked with the student 1", contact2.getStudent());
+			assertTrue("Contact 2 have link to student 1", contact2.getStudent().equals(student1));
+			assertNotNull("student 1 linked with the contact 2", student1.getContact());
+			assertTrue("Student 1 have link with the contact 2", student1.getContact().equals(contact2));
+			assertNull("contact 1 have no link to students", contact1.getStudent());
+
+			assertNull("student 2 should have no link to contacts", student2.getContact());
+		} finally {
+			objectContext.rollbackChanges();
+		}
 	}
 	
 	private List<Contact> createTwoContacts(final ObjectContext objectContext) {
-		Contact contact1 = objectContext.newObject(Contact.class);
-		contact1.setAngelId(1L);
-		Contact contact2 = objectContext.newObject(Contact.class);
-		contact1.setAngelId(2L);
-		assertNull("contact 1 have no link to student", contact1.getStudent());
+		Contact contact1 = (Contact) objectContext.performQuery(
+			new SelectQuery(Contact.class, ExpressionFactory.matchDbExp(Contact.ID_PK_COLUMN, 2L))).get(0);		
+		Contact contact2 = (Contact) objectContext.performQuery(
+			new SelectQuery(Contact.class, ExpressionFactory.matchDbExp(Contact.ID_PK_COLUMN, 1L))).get(0);;
+		assertNotNull("contact 1 have no link to student", contact1.getStudent());
 		assertNull("contact 1 have no link to tutor", contact1.getTutor());
 		assertNull("contact 2 have no link to student", contact2.getStudent());
 		assertNull("contact 2 have no link to tutor", contact2.getTutor());
@@ -110,11 +293,19 @@ public class MergeContactTest extends ServiceTest {
 		return result;
 	}
 	
-	private Student createFirstStudent(final ObjectContext objectContext) {
-		Student student = objectContext.newObject(Student.class);
-		student.setAngelId(1L);
-		assertNull("student1 have no link to contact", student.getContact());
-		return student;
+	private List<Contact> createTwoMoreContacts(final ObjectContext objectContext) {
+		Contact contact1 = (Contact) objectContext.performQuery(
+			new SelectQuery(Contact.class, ExpressionFactory.matchDbExp(Contact.ID_PK_COLUMN, 3L))).get(0);		
+		Contact contact2 = (Contact) objectContext.performQuery(
+			new SelectQuery(Contact.class, ExpressionFactory.matchDbExp(Contact.ID_PK_COLUMN, 4L))).get(0);;
+		assertNotNull("contact 1 have no link to student", contact1.getStudent());
+		assertNull("contact 1 have no link to tutor", contact1.getTutor());
+		assertNotNull("contact 2 have no link to student", contact2.getStudent());
+		assertNull("contact 2 have no link to tutor", contact2.getTutor());
+		List<Contact> result = new ArrayList<Contact>(2);
+		result.add(contact1);
+		result.add(contact2);
+		return result;
 	}
 
 }
