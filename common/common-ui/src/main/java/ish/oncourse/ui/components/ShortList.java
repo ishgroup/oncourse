4393724a7be5db3e7122eeab85bf98dff8a2dafd
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-09-14 12:54:03 +0000

    #14452. Adjust the shortlist component
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@21441 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/components/ShortList.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/components/ShortList.java
@@ -1,10 +1,12 @@
 package ish.oncourse.ui.components;
 
 import ish.oncourse.model.CourseClass;
+import ish.oncourse.model.VoucherProduct;
 import ish.oncourse.services.cookies.ICookiesService;
 import ish.oncourse.services.courseclass.ICourseClassService;
 import ish.oncourse.services.preference.PreferenceController;
 import ish.oncourse.services.site.IWebSiteService;
+import ish.oncourse.services.voucher.IVoucherService;
 import ish.oncourse.util.FormatUtils;
 import org.apache.log4j.Logger;
 import org.apache.tapestry5.annotations.Parameter;
@@ -30,6 +32,7 @@ public class ShortList {
 	@Property
 	private boolean expandComponent;
 
+	@SuppressWarnings("unused")
 	@Inject
 	private IWebSiteService webSiteService;
 	
@@ -41,12 +44,21 @@ public class ShortList {
 
 	@Inject
 	private ICourseClassService courseClassService;
+	
+	@Inject
+	private IVoucherService voucherService;
 
 	@Property
 	private List<CourseClass> items;
+	
+	@Property
+	private List<VoucherProduct> voucherProductItems;
 
 	@Property
 	private CourseClass courseClass;
+	
+	@Property
+	private VoucherProduct voucherProduct;
 
 	@Inject
 	private Request request;
@@ -54,11 +66,13 @@ public class ShortList {
 	@Inject
 	private Messages messages;
 
-	private static final Logger LOGGER = Logger.getLogger(ShortList.class);
+	static final Logger LOGGER = Logger.getLogger(ShortList.class);
 
 	@SetupRender
 	void beforeRender() {
 		List<Long> classIds = cookiesService.getCookieCollectionValue(CourseClass.SHORTLIST_COOKIE_KEY, Long.class);
+		//also load the vouchers
+		List<Long> voucherProductIds = cookiesService.getCookieCollectionValue(VoucherProduct.SHORTLIST_COOKIE_KEY, Long.class);
 
 		String key = request.getParameter("key");
 
@@ -79,12 +93,37 @@ public class ShortList {
 
 			}
 		}
+		
+		if (VoucherProduct.SHORTLIST_COOKIE_KEY.equalsIgnoreCase(key)) {
+			String addItem = request.getParameter("addItemId");
+			String removeItem = request.getParameter("removeItemId");
+			if (addItem != null && addItem.matches("\\d+")) {
 
+				if (!voucherProductIds.contains(Long.valueOf(addItem))) {
+					voucherProductIds.add(Long.valueOf(addItem));
+				}
+
+			}
+			if (removeItem != null && removeItem.matches("\\d+")) {
+				if (voucherProductIds.contains(Long.valueOf(removeItem))) {
+					voucherProductIds.remove(Long.valueOf(removeItem));
+				}
+
+			}
+		}
+		
 		if ((classIds != null) && !(classIds.isEmpty())) {
 			items = courseClassService.loadByIds(classIds);
 		} else {
 			items = Collections.emptyList();
 		}
+
+		if ((voucherProductIds != null) && !(voucherProductIds.isEmpty())) {
+			voucherProductItems = voucherService.loadByIds(voucherProductIds);
+		} else {
+			voucherProductItems = Collections.emptyList();
+		}
+		
 	}
 
 	/**
@@ -113,6 +152,33 @@ public class ShortList {
 	public boolean isHasMultipleItems() {
 		return getItemCount() > 1;
 	}
+	
+	/**
+	 * Obtain the count of items in the shortlist
+	 * 
+	 * @return a count of items
+	 */
+	public Integer getVoucherProductItemCount() {
+		return (voucherProductItems == null) ? 0 : voucherProductItems.size();
+	}
+
+	/**
+	 * Test to see if there are any items in shortlist
+	 * 
+	 * @return true if shortlist has ANY items
+	 */
+	public boolean isHasVoucherProductItems() {
+		return getVoucherProductItemCount() > 0;
+	}
+
+	/**
+	 * Test to see if there are multiple items in the shortlist
+	 * 
+	 * @return true if the shortlist contains more than one item
+	 */
+	public boolean isHasMultipleVoucherProductItems() {
+		return getVoucherProductItemCount() > 1;
+	}
 
 	/**
 	 * Checks if the payment gateway processing is enabled for the current
@@ -133,6 +199,10 @@ public class ShortList {
 
 	public Format getDateFormat() {
 		return FormatUtils.getShortDateFormat(courseClass.getCollege().getTimeZone());
+	}
+	
+	public Format getVoucherProductDateFormat() {
+		return FormatUtils.getShortDateFormat(voucherProduct.getCollege().getTimeZone());
 	}
 
 }
