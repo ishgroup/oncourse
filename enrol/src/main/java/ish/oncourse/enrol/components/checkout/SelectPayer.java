305e94bbb2d174f4d5c5f0d46e480bfcbd9bcd6a
Andrey Koyro <andrey.koyro@objectstyle.com>
2012-10-09 18:15:36 +0000

    #14450 Checkout form components Select-Payer have been adjusted.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@21800 fb2e276d-fae9-0310-b578-b71ac85840b9

--- enrol/src/main/java/ish/oncourse/enrol/components/checkout/SelectPayer.java
+++ enrol/src/main/java/ish/oncourse/enrol/components/checkout/SelectPayer.java
@@ -1,26 +1,23 @@
 package ish.oncourse.enrol.components.checkout;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import ish.oncourse.enrol.utils.PurchaseController;
 import ish.oncourse.enrol.utils.PurchaseController.Action;
 import ish.oncourse.enrol.utils.PurchaseController.ActionParameter;
 import ish.oncourse.model.Contact;
 import ish.oncourse.selectutils.ListSelectModel;
 import ish.oncourse.selectutils.ListValueEncoder;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.EventConstants;
-import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
-import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.services.PropertyAccess;
 import org.apache.tapestry5.services.Request;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SelectPayer {
 	public static final String PROPERTY_CONTACT_FULL_NAME = "fullName";
@@ -36,18 +33,15 @@ public class SelectPayer {
     private Request request;
 	
 	@Property
-	private Contact currentSelection;
-	
-	@InjectComponent
-	private Zone contactsZone;
-	
+	private Contact selectedContact;
+
 	@Parameter(required = false)
 	@Property
 	private String componentForRerender;
 		
 	@SetupRender
 	void beforeRender() {
-		currentSelection = purchaseController.getModel().getPayer();
+		selectedContact = purchaseController.getModel().getPayer();
 		if (StringUtils.trimToNull(componentForRerender) == null) {
 			componentForRerender = null;
 		}
