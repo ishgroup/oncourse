51364c7135856c852b6c8c17eb62dfe7a9c21dae
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2012-11-20 12:23:40 +0000

    #16024. Custom implementation for college list view which is more flexible than tapestry grid.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@22538 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/pages/Index.java
+++ admin/src/main/java/ish/oncourse/admin/pages/Index.java
@@ -3,8 +3,13 @@ package ish.oncourse.admin.pages;
 import ish.oncourse.model.College;
 import ish.oncourse.services.system.ICollegeService;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.cayenne.query.Ordering;
+import org.apache.cayenne.query.SortOrder;
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
@@ -14,6 +19,8 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.BeanModelSource;
 
 public class Index {
+	
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy");
 	
 	@Inject
 	private ICollegeService collegeService;
@@ -43,5 +50,11 @@ public class Index {
 			collegeModel.add("action", null);
 		}
 		this.colleges = collegeService.allColleges();
+		Ordering.orderList(this.colleges, Arrays.asList(new Ordering(College.NAME_PROPERTY, SortOrder.ASCENDING_INSENSITIVE)));
+	}
+	
+	public String getLastRemoteAuthentication() {
+		Date lastRemoteAuth = college.getLastRemoteAuthentication();
+		return lastRemoteAuth != null ? DATE_FORMAT.format(lastRemoteAuth) : "";
 	}
 }
