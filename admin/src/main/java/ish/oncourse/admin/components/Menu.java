91d25f4fd979ca59f2d25e73ba61d43513ae9c22
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2012-11-27 06:45:15 +0000

    #16276. Move logout logic from PageStructure to menu component where logout link was moved before.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@22661 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/components/Menu.java
+++ admin/src/main/java/ish/oncourse/admin/components/Menu.java
@@ -1,8 +1,13 @@
 package ish.oncourse.admin.components;
 
+import ish.oncourse.admin.pages.Index;
+
 import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Session;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 public class Menu {
@@ -10,11 +15,24 @@ public class Menu {
 	@Inject
 	private JavaScriptSupport jsSupport;
 	
+	@InjectPage
+	private Index index;
+	
+	@Inject
+	private Request request;
+	
+	
 	@Parameter
 	private String selected;
 	
 	@AfterRender
 	void afterRender() {
 		jsSupport.addScript("jQuery('#%s').parents('li.selectable').addClass('active');", selected);
+	}
+	
+	public Object onActionFromLogout() throws Exception {
+		Session session = request.getSession(false);
+		session.invalidate();
+		return index;
 	}
 }
--- admin/src/main/java/ish/oncourse/admin/components/PageStructure.java
+++ admin/src/main/java/ish/oncourse/admin/components/PageStructure.java
@@ -1,14 +1,9 @@
 package ish.oncourse.admin.components;
 
-import ish.oncourse.admin.pages.Index;
-
 import org.apache.tapestry5.ComponentResources;
-import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.Request;
-import org.apache.tapestry5.services.Session;
 
 public class PageStructure {
 	
@@ -16,20 +11,8 @@ public class PageStructure {
 	@Parameter
 	private String title;
 	
-	@InjectPage
-	private Index index;
-	
-	@Inject
-	private Request request;
-	
 	@Inject
 	private ComponentResources resources;
-	
-	public Object onActionFromLogout() throws Exception {
-		Session session = request.getSession(false);
-		session.invalidate();
-		return index;
-	}
 	
 	public String getCurrentPage() {
 		return resources.getPage().getClass().getSimpleName();
