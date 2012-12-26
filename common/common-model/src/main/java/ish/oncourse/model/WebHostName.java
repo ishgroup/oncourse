14636b6ebdddb965f7f9ea7cb1187ffb822c3d4b
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2012-12-26 10:30:49 +0000

    #16641. Commit missed change
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@23132 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/WebHostName.java
+++ common/common-model/src/main/java/ish/oncourse/model/WebHostName.java
@@ -1,8 +1,12 @@
 package ish.oncourse.model;
 
 import ish.oncourse.model.auto._WebHostName;
+import ish.oncourse.utils.QueueableObjectUtils;
 
 public class WebHostName extends _WebHostName {
 	private static final long serialVersionUID = -1619903647679895866L;
-
+	
+	public Long getId() {
+		return QueueableObjectUtils.getId(this);
+	}
 }
