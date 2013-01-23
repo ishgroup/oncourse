2cc4f5172cd254d4fdf2c5b383ecb7253cd0be1d
Andrey Koyro <andrey.koyro@objectstyle.com>
2013-01-23 21:36:11 +0000

    #16920 initialization of TestServer  was adjusted to exclude hung up. All a few dependencies  were added to gradle (see comments in build.gradle).
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@23330 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/test/java/ish/oncourse/webservices/soap/TestServer.java
+++ services/src/test/java/ish/oncourse/webservices/soap/TestServer.java
@@ -7,66 +7,75 @@ import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class TestServer {
-	private static Logger LOGGER = Logger.getLogger(TestServer.class);
-	private Server server;
-	private int port = 9091;
-	private int stopPort = 9092;
-	private String contextPath = "/services";
-	private String webPath = "src/test/webapp/WEB-INF";
-	private String host = "127.0.0.1";
+    private static Logger LOGGER = Logger.getLogger(TestServer.class);
+    private transient Server server;
+    private int port = 9091;
+    private int stopPort = 9092;
+    private String contextPath = "/services";
+    private String webPath = "src/test/webapp/WEB-INF";
+    private String host = "127.0.0.1";
 
-	public static final String SERVER_URL = "http://127.0.0.1:9091";
+    public static final String SERVER_URL = "http://127.0.0.1:9091";
+
+    private transient Exception exception;
 
 
-	public void start() throws Exception {
-		Thread t = new Thread() {
-			public void run() {
-				server = new Server();
-				SocketConnector connector = new SocketConnector();
-				connector.setPort(port);
-				server.setConnectors(new Connector[] { connector });
+    public void start() throws Exception {
+        server = new Server();
+        SocketConnector connector = new SocketConnector();
+        connector.setPort(port);
+        server.setConnectors(new Connector[]{connector});
 
-				WebAppContext context = new WebAppContext();
-				context.setServer(server);
-				context.setContextPath(contextPath);
-				//context.setWar(warFilePath);
+        WebAppContext context = new WebAppContext();
+        context.setServer(server);
+        context.setContextPath(contextPath);
+        //context.setWar(warFilePath);
 
-				//Note: Set WAR assumes all resources etc in place like genuine WAR,
-				//in our case resources scattered across so use the following instead:
-				context.setResourceBase("src/test");
-				context.setDescriptor(webPath + "/web.xml");
-				server.setHandler(context);
+        //Note: Set WAR assumes all resources etc in place like genuine WAR,
+        //in our case resources scattered across so use the following instead:
+        context.setResourceBase("src/test");
+        context.setDescriptor(webPath + "/web.xml");
+        server.setHandler(context);
 
-				try {
-					server.start();
-					server.join();
-				}
-				catch(Exception e) {
-					throw new RuntimeException(e);
-				}
-			}
-		};
+        try {
+            server.start();
+        } catch (Exception e) {
+            server.stop();
+            server.destroy();
+            throw new RuntimeException(e);
+        }
+        LOGGER.info("[start] Started Server @ " + host + ":" + port);
+        LOGGER.info("[start] Server Ready & Running - " + server.isRunning());
 
-		t.setDaemon(true);
-		t.start();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    server.join();
+                } catch (InterruptedException e) {
+                }
+            }
+        };
 
-		while(server==null || !server.isStarted()) {
-			Thread.sleep(1000);
-		}
+        Thread t = new Thread(runnable);
+        t.setDaemon(true);
+        t.start();
 
-		LOGGER.info("[start] Started Server @ " + host + ":" + port );
-		LOGGER.info("[start] Server Ready & Running - " + server.isRunning());
-	}
+        while (!server.isStarted())
+        {
+           Thread.sleep(1000);
+        }
+    }
 
-	public void stop() throws Exception {
-		if (server!=null && server.isStarted()) {
-			server.stop();
-		}
-	}
+    public void stop() throws Exception {
+        if (server != null && server.isStarted()) {
+            server.stop();
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		TestServer server = new TestServer();
-		server.start();
-	}
+        TestServer server = new TestServer();
+        server.start();
+    }
 }
