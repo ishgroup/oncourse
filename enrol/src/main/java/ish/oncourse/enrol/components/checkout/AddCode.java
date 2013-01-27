219884b5ecdfb542779da86b6b8e711166e25682
Andrey Koyro <andrey.koyro@objectstyle.com>
2013-01-27 10:59:27 +0000

    #16535 message Code "%s" successfully added was added to this functionality.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@23406 fb2e276d-fae9-0310-b578-b71ac85840b9

--- enrol/src/main/java/ish/oncourse/enrol/components/checkout/AddCode.java
+++ enrol/src/main/java/ish/oncourse/enrol/components/checkout/AddCode.java
@@ -6,12 +6,16 @@ import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 
 public class AddCode {
 
 	public static final String FIELD_ADD_CODE = "add_code";
+
+    public static final String MESSAGE_KEY_successMessage = "message-successMessage";
+
 	@Parameter(required = true)
 	private PurchaseController purchaseController;
 
@@ -21,6 +25,11 @@ public class AddCode {
 
     @Inject
 	private Request request;
+
+    @Inject
+    private Messages messages;
+
+    private String lastAddDiscountCode;
 
 
 	@OnEvent(value = "addCodeEvent")
@@ -33,6 +42,16 @@ public class AddCode {
 		PurchaseController.ActionParameter actionParameter = new PurchaseController.ActionParameter(PurchaseController.Action.addDiscount);
 		actionParameter.setValue(code);
 		purchaseController.performAction(actionParameter);
+        if (purchaseController.getErrors().isEmpty())
+            lastAddDiscountCode = code;
 		return blockToRefresh;
 	}
+
+    public String getSuccessMessage()
+    {
+        if (lastAddDiscountCode != null)
+            return messages.format(MESSAGE_KEY_successMessage,lastAddDiscountCode);
+        else
+            return null;
+    }
 }
