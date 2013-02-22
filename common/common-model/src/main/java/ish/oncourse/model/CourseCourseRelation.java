a94efc78e8f7989d86a4997a4563238806582a47
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2013-02-22 09:39:21 +0000

    #15792. Add CourseCourseRelation entity to willow side
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@23788 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-model/src/main/java/ish/oncourse/model/CourseCourseRelation.java
@@ -0,0 +1,8 @@
+package ish.oncourse.model;
+
+import ish.oncourse.model.auto._CourseCourseRelation;
+
+public class CourseCourseRelation extends _CourseCourseRelation {
+	private static final long serialVersionUID = 1L;
+
+}
