6b010a05811469a341d300e692bdd0733df16f00
Andrey Koyro <andrey.koyro@objectstyle.com>
2013-03-03 11:27:44 +0000

    #17229 Logout link was deleted because admin app uses BASIC auth method. There is only one 100% method to logout for BASIC auth - close browser.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@23893 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/main/java/ish/oncourse/admin/components/Menu.java
+++ admin/src/main/java/ish/oncourse/admin/components/Menu.java
@@ -1,13 +1,11 @@
 package ish.oncourse.admin.components;
 
 import ish.oncourse.admin.pages.Index;
-
 import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
-import org.apache.tapestry5.services.Session;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 public class Menu {
@@ -28,11 +26,5 @@ public class Menu {
 	@AfterRender
 	void afterRender() {
 		jsSupport.addScript("jQuery('#%s').parents('li.selectable').addClass('active');", selected);
-	}
-	
-	public Object onActionFromLogout() throws Exception {
-		Session session = request.getSession(false);
-		session.invalidate();
-		return index;
 	}
 }
