955d9c081ee7b4ecf2ce83c7c840d728a5c5372f
Andrey Koyro <andrey.koyro@objectstyle.com>
2013-03-04 20:59:41 +0000

    #17315 The changes was done to  be sure that these invoices will be loaded from database and amountOwing will have actual value.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@23907 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/util/InvoiceUtils.java
+++ common/common-services/src/main/java/ish/oncourse/util/InvoiceUtils.java
@@ -3,27 +3,34 @@ package ish.oncourse.util;
 import ish.math.Money;
 import ish.oncourse.model.Contact;
 import ish.oncourse.model.Invoice;
+import org.apache.cayenne.exp.Expression;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.QueryCacheStrategy;
+import org.apache.cayenne.query.SelectQuery;
 
 import java.util.List;
 
 public class InvoiceUtils {
 
-	/**
-	 * sums all invoice amount owing for a payer
-	 *
-	 * @param payer to be analysed
-	 * @return Money sum of amount owing
-	 */
-	public static Money amountOwingForPayer(Contact payer) {
-		Money result = Money.ZERO;
-		// update invoice owing
-		if (payer != null) {
-			List<Invoice> invoices = payer.getInvoices();
-			for (Invoice invoice : invoices) {
-				result = result.add(invoice.getAmountOwing());
-			}
-		}
-		return result;
-	}
-
+    /**
+     * sums all invoice amount owing for a payer
+     *
+     * @param contact to be analysed
+     * @return Money sum of amount owing
+     */
+    public static Money amountOwingForPayer(Contact contact) {
+        Expression exp = ExpressionFactory.matchExp(Invoice.CONTACT_PROPERTY, contact);
+        /**
+         * we use query with Strategy.LOCAL_CACHE_REFRESH to be sure that
+         * these invoices will be loaded from database and amountOwing will have actual value.
+         */
+        SelectQuery q = new SelectQuery(Invoice.class, exp);
+        q.setCacheStrategy(QueryCacheStrategy.LOCAL_CACHE_REFRESH);
+        List<Invoice> invoices = contact.getObjectContext().performQuery(q);
+        Money result = Money.ZERO;
+        for (Invoice invoice : invoices) {
+            result.add(Money.valueOf(invoice.getAmountOwing()));
+        }
+        return result;
+    }
 }
