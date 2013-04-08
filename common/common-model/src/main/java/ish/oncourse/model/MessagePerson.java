bdc2c2fc943c2d6ced3d091d2435b883c8ac693d
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2013-04-08 11:40:08 +0000

    #000. Adjust MessagePerson illegal status change message text.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@24420 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/MessagePerson.java
+++ common/common-model/src/main/java/ish/oncourse/model/MessagePerson.java
@@ -22,17 +22,18 @@ public class MessagePerson extends _MessagePerson implements Queueable {
 			switch (getStatus()) {
 			case QUEUED:
 				if (status == null) {
-					throw new IllegalArgumentException("Can't update to empty MessagePerson status from queued!");
+					throw new IllegalArgumentException(String.format("Can't update to empty MessagePerson status from queued with id = %s !", getId()));
 				}
 				break;
 			case SENT:
 			case FAILED:
 				if (!(getStatus().equals(status))) {
-					throw new IllegalArgumentException(String.format("Can't set the %s status for MessagePerson with %s status!", status, getStatus()));
+					throw new IllegalArgumentException(String.format("Can't set the %s status for MessagePerson with %s status and id = %s !", 
+						status, getStatus(), getId()));
 				}
 				break;
 			default:
-				throw new IllegalArgumentException(String.format("Unsupported status %s found for MessagePerson", getStatus()));
+				throw new IllegalArgumentException(String.format("Unsupported status %s found for MessagePerson with id = %s ", getStatus(), getId()));
 			}
 		}
 		super.setStatus(status);
