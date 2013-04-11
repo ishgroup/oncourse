a65d8fe97801e079c2ca449303cba9641e192baf
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2013-04-11 14:48:15 +0000

    #16951. Add real website search query and near filtering tests
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@24507 fb2e276d-fae9-0310-b578-b71ac85840b9

--- search/src/test/java/ish/oncourse/solr/CustomizedAbstractSolrTestCase.java
+++ search/src/test/java/ish/oncourse/solr/CustomizedAbstractSolrTestCase.java
@@ -2,6 +2,7 @@ package ish.oncourse.solr;
 
 import org.apache.solr.SolrTestCaseJ4;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.CoreDescriptor;
@@ -14,10 +15,44 @@ import org.apache.solr.logging.jul.JulWatcher;
 import org.apache.solr.schema.IndexSchema;
 import org.apache.solr.util.AbstractSolrTestCase;
 import org.apache.solr.util.TestHarness;
+import org.junit.After;
 
 public abstract class CustomizedAbstractSolrTestCase extends AbstractSolrTestCase {
+	protected static final String SCHEMA_FILE_PATH = "/conf/schema.xml";
+	protected static final String SOLRCONFIG_FILE_PATH = "/conf/solrconfig.xml";
+	protected static final String SOLR_RESOURCES_PATH = "src/main/resources/solr/";
+	protected static final String TEST_SOLR_HOME = "src/test/resources/";
+	
 	protected static String factoryProp;
 	protected SolrServer server;
+	
+	@After
+    public void destroy() {
+    	clearIndex();
+    	server.shutdown();
+    }
+	
+	protected void prepareCore(final String coreName) throws Exception {
+        testSolrHome = TEST_SOLR_HOME;
+        System.setProperty("SOLR_DATA", testSolrHome);
+		System.setProperty("solr.master.url", "http://localhost:8081/search-internal");
+		System.setProperty("solr.master.enable", "true");
+		System.setProperty("solr.slave.enable", "false");
+		System.setProperty("solr.core.name", coreName);
+		System.setProperty("solr.poll", "1");
+		configString = getConfigPath(coreName);
+		schemaString = getSchemaPath(coreName);
+        customInitCore(coreName);
+        server = new EmbeddedSolrServer(h.getCoreContainer(), h.getCore().getName());
+	}
+	
+	protected String getConfigPath(final String coreName) {
+    	return String.format("%s%s%s", SOLR_RESOURCES_PATH, coreName, SOLRCONFIG_FILE_PATH);
+    }
+    
+    protected String getSchemaPath(final String coreName) {
+    	return String.format("%s%s%s", SOLR_RESOURCES_PATH, coreName, SCHEMA_FILE_PATH);
+    }
 	
 	protected static void customInitCore(final String coreName) throws Exception {
     	log.info("####initCore");
--- search/src/test/java/ish/oncourse/solr/SolrCourseCoreTest.java
+++ search/src/test/java/ish/oncourse/solr/SolrCourseCoreTest.java
@@ -1,19 +1,20 @@
 package ish.oncourse.solr;
 
 import ish.oncourse.services.search.SearchParams;
+import ish.oncourse.services.search.SearchParamsParser;
 import ish.oncourse.services.search.SolrQueryBuilder;
 
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.util.Arrays;
 import java.util.Calendar;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.SolrParams;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -38,38 +39,24 @@ public class SolrCourseCoreTest extends CustomizedAbstractSolrTestCase {
 	private static final String DOCTYPE_FIELD_NAME = "doctype";
 	private static final String COURSE_ID_FIELD_NAME = "id";
 	private static final String COURSES_CORE_NAME = "courses";
+	private static final String COURSE_LOCATION_FIELD_NAME = "course_loc";
+	private static final String SCORE_FIELD_NAME = "score";
 	
     @Before
     @Override
     public void setUp() throws Exception {
-    	System.setProperty("SOLR_DATA", "src/test/resources/");
-		System.setProperty("solr.master.url", "http://localhost:8081/search-internal");
-		System.setProperty("solr.master.enable", "true");
-		System.setProperty("solr.slave.enable", "false");
-		System.setProperty("solr.core.name", COURSES_CORE_NAME);
-		System.setProperty("solr.poll", "1");
         super.setUp();
-        testSolrHome = "src/test/resources/";
-        configString = "src/main/resources/solr/courses/conf/solrconfig.xml";
-        schemaString = "src/main/resources/solr/courses/conf/schema.xml";
-        customInitCore(COURSES_CORE_NAME);
-        server = new EmbeddedSolrServer(h.getCoreContainer(), h.getCore().getName());
+        prepareCore(COURSES_CORE_NAME);
     }
-    
-    @After
-    public void destroy() {
-    	server.shutdown();
-    }
-            
+
     @Test
     public void testEmptyIndex() throws SolrServerException {
         SolrParams params = new SolrQuery(SolrQueryBuilder.FILTER_TEMPLATE_ALL);
         QueryResponse response = server.query(params);
         assertEquals(0L, response.getResults().getNumFound());
     }
-        
-    @Test
-    public void testAddReadDeleteDocuments() throws SolrServerException, IOException {
+    
+    private SolrInputDocument prepareInitCourseDocument() {
     	SolrInputDocument document = new SolrInputDocument();
     	document.setField(COURSE_ID_FIELD_NAME, "1");
     	document.setField(DOCTYPE_FIELD_NAME, "course");
@@ -80,23 +67,118 @@ public class SolrCourseCoreTest extends CustomizedAbstractSolrTestCase {
     	document.setField(CLASS_CODE_FIELD_NAME, "courseTestCode-courseClassTestCode");
     	document.setField(TUTOR_FIELD_NAME, "tutorTest Name");
     	document.setField(WHEN_FIELD_NAME, "monday weekend evening");
-    	
     	Calendar cal = Calendar.getInstance();
     	document.setField(COURSE_START_DATE_FIELD_NAME, cal.getTime());
     	document.setField(CLASS_START_FIELD_NAME, cal.getTime());
     	cal.add(Calendar.HOUR, 2);
     	document.setField(COURSE_END_FIELD_NAME, cal.getTime());
-    	    	
-    	//course_loc
-    	
+    	document.setField(COURSE_LOCATION_FIELD_NAME, Arrays.asList("-31.94777400,115.82577800","-30.94777400,110.82577800"));
     	document.setField(COURSE_SUBURB_FIELD_NAME, "courseTestSuburb 0001");
     	document.setField(COURSE_POSTCODE_FIELD_NAME, "1");
     	Float price = 2.0f;
     	document.setField(COURSE_PRICE_FIELD_NAME, price);
     	Long tagId = 1l;
     	document.setField(COURSE_TAG_FIELD_NAME, tagId);
-    	
-    	//now add document to server
+    	return document;
+    }
+    
+    @Test
+    public void testDistanceFiltering() throws SolrServerException, IOException {
+    	SolrInputDocument document = prepareInitCourseDocument();
+    	//add document to server
+    	server.add(document);
+        server.commit();
+        
+    	//prepare data for distance filtering
+        SearchParams searchParams = new SearchParams();
+        //set the default 100km distance for test
+		searchParams.setKm(SearchParamsParser.parseKm(null));
+		SolrDocumentList solrSuburbs = new SolrDocumentList();
+		SolrDocument suburb = new SolrDocument();
+		suburb.addField(SolrQueryBuilder.PARAMETER_loc, document.getFieldValues(COURSE_LOCATION_FIELD_NAME).iterator().next());
+		suburb.addField(SolrQueryBuilder.FIELD_postcode, 
+			SearchParamsParser.convertPostcodeParameterToLong((String) document.getFieldValue(COURSE_POSTCODE_FIELD_NAME)));
+		solrSuburbs.add(suburb);
+		searchParams.setNear(solrSuburbs);
+		
+		//check distance filtering
+		SolrQueryBuilder solrQueryBuilder = new SolrQueryBuilder(searchParams, ((Integer) document.getFieldValue(COLLEGE_ID_FIELD_NAME)).toString(), 0, 5);
+		SolrParams params = solrQueryBuilder.create();
+		String value = URLDecoder.decode(params.toString(), "UTF-8");
+        System.out.println(value);
+        
+        QueryResponse response = server.query(params);
+        assertEquals(1L, response.getResults().getNumFound());
+        SolrDocument result = response.getResults().get(0);
+        assertNotNull("Result document should not be empty", result);
+        assertEquals("Course id should be equal to original document", document.getFieldValue(COURSE_ID_FIELD_NAME), 
+        	result.getFieldValue(COURSE_ID_FIELD_NAME));
+        assertEquals("Course name should be equal to original document", document.getFieldValue(COURSE_NAME_FIELD_NAME), 
+        	result.getFieldValue(COURSE_NAME_FIELD_NAME));
+        Float score = (Float) result.getFieldValue(SCORE_FIELD_NAME);
+        System.out.println(String.format("Calculated score value = %s", score));
+        assertEquals("Calculated score for distance filtering should be equal to 1.9999998", Float.valueOf(1.9999998f), score);
+        
+        //and now try again with updated location
+        searchParams = new SearchParams();
+        searchParams.setKm(SearchParamsParser.parseKm("10"));
+		solrSuburbs = new SolrDocumentList();
+		suburb = new SolrDocument();
+		suburb.addField(SolrQueryBuilder.PARAMETER_loc, "-40.94777400,110.82577800");
+		suburb.addField(SolrQueryBuilder.FIELD_postcode, 
+			SearchParamsParser.convertPostcodeParameterToLong((String) document.getFieldValue(COURSE_POSTCODE_FIELD_NAME)));
+		solrSuburbs.add(suburb);
+		searchParams.setNear(solrSuburbs);
+		
+		solrQueryBuilder = new SolrQueryBuilder(searchParams, ((Integer) document.getFieldValue(COLLEGE_ID_FIELD_NAME)).toString(), 0, 5);
+		params = solrQueryBuilder.create();
+        value = URLDecoder.decode(params.toString(), "UTF-8");
+        System.out.println(value);
+        
+        response = server.query(params);
+        assertEquals(0L, response.getResults().getNumFound());
+    }
+    
+    @Test
+    public void testRealWebSiteSearch() throws SolrServerException, IOException {
+    	SolrInputDocument document = prepareInitCourseDocument();
+    	//test the real calls
+        SolrQueryBuilder solrQueryBuilder = new SolrQueryBuilder(new SearchParams(), 
+        	((Integer) document.getFieldValue(COLLEGE_ID_FIELD_NAME)).toString(), 0, 100);
+        SolrParams params = solrQueryBuilder.create();
+        String value = URLDecoder.decode(params.toString(), "UTF-8");
+        assertEquals("Commons parameters",  EXPECTED_GENERATED_QUERY_STRING, value);
+        
+        //check that document not exist
+        QueryResponse response = server.query(params);
+        assertEquals(0L, response.getResults().getNumFound());
+        
+        //add document to server
+    	server.add(document);
+        server.commit();
+        
+        response = server.query(params);
+        assertEquals(1L, response.getResults().getNumFound());
+        SolrDocument result = response.getResults().get(0);
+        assertNotNull("Result document should not be empty", result);
+        assertEquals("Course id should be equal to original document", document.getFieldValue(COURSE_ID_FIELD_NAME), 
+        	result.getFieldValue(COURSE_ID_FIELD_NAME));
+        assertEquals("Course name should be equal to original document", document.getFieldValue(COURSE_NAME_FIELD_NAME), 
+        	result.getFieldValue(COURSE_NAME_FIELD_NAME));
+        //get the 4 digits after dot because score is runtime calculated value
+        Float score = (Float) result.getFieldValue(SCORE_FIELD_NAME);
+        System.out.println(String.format("Calculated score value = %s", score));//"0.5792748"
+        assertTrue("Calculated score should be more then 0.5791", score.compareTo(0.5791f) > 0);
+        assertTrue("Calculated score should be less then 0.5793", score.compareTo(0.5793f) < 0);
+        assertNotNull("Course location should not be empty", result.getFieldValues(COURSE_LOCATION_FIELD_NAME));
+        assertFalse("Course location should not be empty", result.getFieldValues(COURSE_LOCATION_FIELD_NAME).isEmpty());
+        assertEquals("Course should have 2 locations", 2, result.getFieldValues(COURSE_LOCATION_FIELD_NAME).size());
+    }
+        
+    @Test
+    public void testAddReadDeleteDocuments() throws SolrServerException, IOException {
+    	SolrInputDocument document = prepareInitCourseDocument();
+    	//add document to server
     	server.add(document);
         server.commit();
         
@@ -112,30 +194,5 @@ public class SolrCourseCoreTest extends CustomizedAbstractSolrTestCase {
         //check that document deleted
         response = server.query(params);
         assertEquals(0L, response.getResults().getNumFound());
-        
-        //test the real calls
-        SolrQueryBuilder solrQueryBuilder = new SolrQueryBuilder(new SearchParams(),"1",0,100);
-        params = solrQueryBuilder.create();
-        String value = URLDecoder.decode(solrQueryBuilder.create().toString(), "UTF-8");
-        assertEquals("Commons parameters",  EXPECTED_GENERATED_QUERY_STRING, value);
-        
-        //re-add the document
-        server.add(document);
-        server.commit();
-        
-        response = server.query(params);
-        assertEquals(1L, response.getResults().getNumFound());
-        SolrDocument result = response.getResults().get(0);
-        assertNotNull("Result document should not be empty", result);
-        assertEquals("Course id should be equal to original document", document.getFieldValue(COURSE_ID_FIELD_NAME), 
-        	result.getFieldValue(COURSE_ID_FIELD_NAME));
-        assertEquals("Course name should be equal to original document", document.getFieldValue(COURSE_NAME_FIELD_NAME), 
-        	result.getFieldValue(COURSE_NAME_FIELD_NAME));
-        //get the 5 digits after dot because score is runtime calculated value
-        Float score = (Float) result.getFieldValue("score");
-        System.out.println(String.format("Calculated score value = %s", score));//"0.5792748"
-        assertTrue("Calculated score should be more then 0.5792", score.compareTo(0.5792f) > 0);
-        assertTrue("Calculated score should be less then 0.5793", score.compareTo(0.5793f) < 0);
-        System.out.println("testAddAndReadDocuments passed");
     }
 }
