ef3a3b8e13fd59f808209f9be5aa8ff93599af13
Viacheslav Davidovich <viacheslav.davidovich@objectstyle.com>
2013-04-15 08:00:47 +0000

    #17277. Change the code of textile attribute rendering. Now true and yes parsed ignore cases.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@24550 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/services/textile/renderer/AbstractRenderer.java
+++ common/common-services/src/main/java/ish/oncourse/services/textile/renderer/AbstractRenderer.java
@@ -1,14 +1,27 @@
 package ish.oncourse.services.textile.renderer;
 
+import java.util.Map;
+
 import ish.oncourse.services.textile.validator.IValidator;
 import ish.oncourse.util.ValidationErrors;
 
 public abstract class AbstractRenderer implements IRenderer {
-	
+	protected static final String YES_REQUIRED_PARAM_VALUE = "yes";
 	protected IValidator validator;
 
 	public String render(String tag, ValidationErrors errors) {
 		validator.validate(tag, errors);
 		return tag;
+	}
+	
+	/**
+	 * Parse potentially boolean attribute value.
+	 * @param params - parameters map
+	 * @param attributeValue - attribute value for check
+	 * @return true if attributeValue match yes or true ignore cases.
+	 */
+	protected static boolean parseBooleanTextileAttribute(Map<String, String> params, String attributeValue) {
+		String paramStringValue = params.get(attributeValue);
+		return Boolean.getBoolean(paramStringValue.toLowerCase()) || YES_REQUIRED_PARAM_VALUE.equals(paramStringValue.toLowerCase());
 	}
 }
--- common/common-services/src/main/java/ish/oncourse/services/textile/renderer/FormTextileRenderer.java
+++ common/common-services/src/main/java/ish/oncourse/services/textile/renderer/FormTextileRenderer.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
+
 import ish.oncourse.model.TextileFormField;
 import ish.oncourse.services.textile.TextileType;
 import ish.oncourse.services.textile.TextileUtil;
@@ -19,7 +21,6 @@ import ish.oncourse.util.IPageRenderer;
 import ish.oncourse.util.ValidationErrors;
 
 public class FormTextileRenderer extends AbstractRenderer {
-
 	private static final int DEFAULT_TEXTAREA_ROWS = 10;
 	private IPageRenderer pageRenderer;
 
@@ -37,18 +38,16 @@ public class FormTextileRenderer extends AbstractRenderer {
 		Map<String, Object> parameters = new HashMap<String, Object>();
 		Map<String, String> tagParams = TextileUtil.getTagParams(tag, FormTextileAttributes.getAttrValues());
 		parameters.put(TextileUtil.TEXTILE_FORM_PAGE_NAME_PARAM, tagParams.get(FormTextileAttributes.NAME.getValue()));
-		parameters
-				.put(TextileUtil.TEXTILE_FORM_PAGE_EMAIL_PARAM, tagParams.get(FormTextileAttributes.EMAIL.getValue()));
+		parameters.put(TextileUtil.TEXTILE_FORM_PAGE_EMAIL_PARAM, tagParams.get(FormTextileAttributes.EMAIL.getValue()));
 		parameters.put(TextileUtil.TEXTILE_FORM_PAGE_URL_PARAM, tagParams.get(FormTextileAttributes.URL.getValue()));
 
 		List<TextileFormField> fields = new ArrayList<TextileFormField>();
 		Pattern textPattern = Pattern.compile(TextileType.FORM_FIELDS_PATTERN);
 		Matcher textMatcher = textPattern.matcher(tag);
-		String content = tag.replaceAll("\\{form([^}]*)}", "");
-		String result = "";
+		String content = tag.replaceAll("\\{form([^}]*)}", StringUtils.EMPTY);
+		String result = StringUtils.EMPTY;
 		while (textMatcher.find()) {
 			String text = textMatcher.group();
-
 			List<String> paramKeys = null;
 			boolean isRadio = false;
 			boolean isText = false;
@@ -70,13 +69,12 @@ public class FormTextileRenderer extends AbstractRenderer {
 			if (isText) {
 				textileFormField.setText(isText);
 				textileFormField.setLabel(textParams.get(TextTextileAttributes.LABEL.getValue()));
-				String requiredParam = textParams.get(TextTextileAttributes.REQUIRED.getValue());
-				textileFormField.setRequired(Boolean.TRUE.equals(requiredParam) || "yes".equals(requiredParam));
+				textileFormField.setRequired(parseBooleanTextileAttribute(textParams, TextTextileAttributes.REQUIRED.getValue()));
 				String linesParam = textParams.get(TextTextileAttributes.LINES.getValue());
 				if (linesParam == null) {
 					textileFormField.setTextAreaRows(null);
 				} else {
-					if (Boolean.TRUE.equals(linesParam) || "yes".equals(linesParam)) {
+					if (parseBooleanTextileAttribute(textParams, TextTextileAttributes.LINES.getValue())) {
 						textileFormField.setTextAreaRows(DEFAULT_TEXTAREA_ROWS);
 					} else {
 						textileFormField.setTextAreaRows(Integer.parseInt(linesParam));
@@ -90,8 +88,7 @@ public class FormTextileRenderer extends AbstractRenderer {
 			if (isRadio) {
 				textileFormField.setRadio(isRadio);
 				textileFormField.setLabel(textParams.get(RadioListTextileAttributes.LABEL.getValue()));
-				String requiredParam = textParams.get(RadioListTextileAttributes.REQUIRED.getValue());
-				textileFormField.setRequired(Boolean.TRUE.equals(requiredParam) || "yes".equals(requiredParam));
+				textileFormField.setRequired(parseBooleanTextileAttribute(textParams, RadioListTextileAttributes.REQUIRED.getValue()));
 				textileFormField.setDefaultValue(textParams.get(RadioListTextileAttributes.DEFAULT.getValue()));
 				String options = textParams.get(RadioListTextileAttributes.OPTIONS.getValue());
 				if (options != null) {
@@ -102,8 +99,7 @@ public class FormTextileRenderer extends AbstractRenderer {
 			if (isPopup) {
 				textileFormField.setPopup(isPopup);
 				textileFormField.setLabel(textParams.get(PopupListTextileAttributes.LABEL.getValue()));
-				String requiredParam = textParams.get(PopupListTextileAttributes.REQUIRED.getValue());
-				textileFormField.setRequired(Boolean.TRUE.equals(requiredParam) || "yes".equals(requiredParam));
+				textileFormField.setRequired(parseBooleanTextileAttribute(textParams, PopupListTextileAttributes.REQUIRED.getValue()));
 				textileFormField.setDefaultValue(textParams.get(PopupListTextileAttributes.DEFAULT.getValue()));
 				String options = textParams.get(PopupListTextileAttributes.OPTIONS.getValue());
 				if (options != null) {
