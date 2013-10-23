136acf51cee28847e1d9ab4cc3804a9ce1203007
Andrey Koyro <andrey.koyro@objectstyle.com>
2013-10-23 13:54:49 +0000

    #19230 menu classes has been implemented
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@32950 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/components/LoginUser.java
+++ portal/src/main/java/ish/oncourse/portal/components/LoginUser.java
@@ -3,7 +3,8 @@ package ish.oncourse.portal.components;
 import ish.oncourse.model.Contact;
 import ish.oncourse.portal.access.IAuthenticationService;
 import ish.oncourse.portal.pages.Login;
-
+import ish.oncourse.services.courseclass.CourseClassFilter;
+import ish.oncourse.services.courseclass.ICourseClassService;
 import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
@@ -19,10 +20,17 @@ public class LoginUser {
 	
 	@InjectPage
 	private Login login;
+
+	@Inject
+	private ICourseClassService courseClassService;
+
+	@Property
+	private int approvalsCount = 0;
 	
 	@SetupRender
 	void setupRender() {
 		this.user = authService.getUser();
+		approvalsCount = courseClassService.getContactCourseClasses(this.user, CourseClassFilter.UNCONFIRMED).size();
 	}
 	
 	public Object onActionFromLogout() throws Exception {
--- portal/src/main/java/ish/oncourse/portal/components/Menu.java
+++ portal/src/main/java/ish/oncourse/portal/components/Menu.java
@@ -1,11 +1,19 @@
 package ish.oncourse.portal.components;
 
+import ish.oncourse.model.CourseClass;
 import ish.oncourse.portal.access.IAuthenticationService;
 import ish.oncourse.portal.pages.WaitingLists;
 import ish.oncourse.portal.services.discussion.IDiscussionService;
 import ish.oncourse.services.courseclass.CourseClassFilter;
+import ish.oncourse.services.courseclass.ICourseClassService;
+import ish.oncourse.util.FormatUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
+
+import java.util.List;
 
 public class Menu {
 
@@ -16,7 +24,40 @@ public class Menu {
 	private IDiscussionService discussionService;
 
 	@Inject
+	@Property
 	private Request request;
+
+	@Inject
+	private ICourseClassService courseClassService;
+
+	@Property
+	private List<CourseClass> courseClasses;
+
+	@Property
+	private CourseClass courseClass;
+
+	@Property
+	private List<CourseClass> pastCourseClasses;
+
+	@Property
+	private CourseClass nearestCourseClass;
+
+	@SetupRender
+	public void setupRender() {
+		courseClasses = courseClassService.getContactCourseClasses(authenticationService.getUser(), CourseClassFilter.CURRENT);
+		pastCourseClasses = courseClassService.getContactCourseClasses(authenticationService.getUser(), CourseClassFilter.PAST);
+		nearestCourseClass = !courseClasses.isEmpty() ? courseClasses.get(0) : null;
+		if (nearestCourseClass == null)
+			nearestCourseClass = !pastCourseClasses.isEmpty() ? pastCourseClasses.get(0) : null;
+	}
+
+	public String getDate()
+	{
+		if (courseClass.getStartDate() != null)
+			return FormatUtils.getDateFormat(FormatUtils.dateFormatString,courseClass.getTimeZone()).format(courseClass.getStartDate());
+		else
+			return StringUtils.EMPTY;
+	}
 
 	public boolean isHasNewMessages() {
 		return getNumberOfNewMessages() > 0;
@@ -49,26 +90,26 @@ public class Menu {
 	public String getMailinglistsPageName() {
 		return "mailinglists";
 	}
-	
+
 	public String getSurveysPageName() {
 		return getIsTutor() ? "tutor/surveys" : "student/surveys";
 	}
-	
+
 	public String getUnconfirmed() {
 		return CourseClassFilter.UNCONFIRMED.name();
 	}
 
-    public String getCurrent() {
-        return CourseClassFilter.CURRENT.name();
-    }
+	public String getCurrent() {
+		return CourseClassFilter.CURRENT.name();
+	}
 
-    public String getPast() {
-        return CourseClassFilter.PAST.name();
-    }
+	public String getPast() {
+		return CourseClassFilter.PAST.name();
+	}
 
-    public String getWaitingListsPageName() {
-        return WaitingLists.class.getSimpleName();
-    }
+	public String getWaitingListsPageName() {
+		return WaitingLists.class.getSimpleName();
+	}
 
 	public String getContextPath() {
 		return request.getContextPath();
