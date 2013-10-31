925b330226b79cf86b13d538729e8a33af0a54eb
Artyom Kravenko <artyom@ish.com.au>
2013-10-31 10:11:33 +0000

    #19228 resources page was done
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@33054 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/pages/Resources.java
+++ portal/src/main/java/ish/oncourse/portal/pages/Resources.java
@@ -1,4 +1,60 @@
 package ish.oncourse.portal.pages;
 
+import ish.oncourse.model.BinaryInfo;
+import ish.oncourse.model.CourseClass;
+import ish.oncourse.portal.access.IAuthenticationService;
+
+import ish.oncourse.services.binary.IBinaryDataService;
+import ish.oncourse.services.courseclass.ICourseClassService;
+import ish.oncourse.util.FormatUtils;
+
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.util.List;
+import java.util.TimeZone;
+
 public class Resources {
+
+    @Inject
+    private IAuthenticationService authenticationService;
+
+    @Inject
+    private ICourseClassService courseClassService;
+
+    @Property
+    private CourseClass courseClass;
+
+    @Property
+    private List<CourseClass> courseClasses;
+
+    @Inject
+    private IBinaryDataService binaryDataService;
+
+
+    @SetupRender
+    void  setupRender(){
+
+        courseClasses = courseClassService.getContactCourseClasses(authenticationService.getUser());
+    }
+
+
+    public String getDate(CourseClass courseClass)
+    {
+         TimeZone timeZone = FormatUtils.getClientTimeZone(courseClass);
+
+        return String.format("%s - %s",
+                FormatUtils.getDateFormat("dd MMM EEE", timeZone).format(courseClass.getStartDate()),
+                FormatUtils.getDateFormat("dd MMM EEE", timeZone).format(courseClass.getEndDate()));
+    }
+
+
+        public boolean hasResources(CourseClass courseClass){
+
+        List<BinaryInfo> materials = binaryDataService.getAttachedFiles(courseClass.getId(), CourseClass.class.getSimpleName(), false);
+        return !materials.isEmpty();
+    }
+
+
 }
