443bfdd0241e62bca78ab3bdc1f692d327010f52
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-04-24 06:20:11 +0000

    #21327  VoucherPaymentIn.isAsyncReplicationAllowed returns true when we discard the payment. As result we delete the VoucherPaymentIn instance and we should replicated the changes to angel side
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@35269 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/VoucherPaymentIn.java
+++ common/common-model/src/main/java/ish/oncourse/model/VoucherPaymentIn.java
@@ -2,6 +2,7 @@ package ish.oncourse.model;
 
 import ish.oncourse.model.auto._VoucherPaymentIn;
 import ish.oncourse.utils.QueueableObjectUtils;
+import org.apache.cayenne.PersistenceState;
 
 import java.util.Date;
 
@@ -14,7 +15,7 @@ public class VoucherPaymentIn extends _VoucherPaymentIn implements Queueable {
 
 	@Override
 	public boolean isAsyncReplicationAllowed() {
-		return getPayment().isAsyncReplicationAllowed();
+		return getPersistenceState() ==  PersistenceState.DELETED || getPayment().isAsyncReplicationAllowed();
 	}
 
     @Override
