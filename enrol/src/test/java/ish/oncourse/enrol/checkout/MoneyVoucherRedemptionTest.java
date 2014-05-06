d63c574b426ece3fef6c4a6822984ffd96a6ecbd
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-05-06 07:51:06 +0000

    #21451 Junits for the following use cases were added: redeeming personal voucher, success result redeeming two vouchers on different stages of payment, success result
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@35428 fb2e276d-fae9-0310-b578-b71ac85840b9

--- enrol/src/test/java/ish/oncourse/enrol/checkout/MoneyVoucherRedemptionTest.java
+++ enrol/src/test/java/ish/oncourse/enrol/checkout/MoneyVoucherRedemptionTest.java
@@ -36,46 +36,46 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		setup("ish/oncourse/enrol/checkout/MoneyVoucherRedemptionTest.xml");
 
 	}
-	
-	private void assertInvoices(Invoice expected,Invoice actual,int feeExGst, int feeGst){
-		assertNotSame(expected,actual);
-		assertEquals(expected.getObjectId(),actual.getObjectId());
-		assertEquals(feeGst+feeExGst,actual.getTotalGst().intValue());
-		assertEquals(feeExGst,actual.getTotalExGst().intValue());
-		assertEquals(0,actual.getAmountOwing().intValue());
+
+	private void assertInvoices(Invoice expected, Invoice actual, int feeExGst, int feeGst) {
+		assertNotSame(expected, actual);
+		assertEquals(expected.getObjectId(), actual.getObjectId());
+		assertEquals(feeGst + feeExGst, actual.getTotalGst().intValue());
+		assertEquals(feeExGst, actual.getTotalExGst().intValue());
+		assertEquals(0, actual.getAmountOwing().intValue());
 	}
 
-	private void assertInvoiceLines(InvoiceLine expected, InvoiceLine actual, int feeExGst, int feeGst){
+	private void assertInvoiceLines(InvoiceLine expected, InvoiceLine actual, int feeExGst, int feeGst) {
 		assertEquals(expected.getObjectId(), actual.getObjectId());
 		assertEquals(feeExGst, actual.getPriceEachExTax().intValue());
 		assertEquals(feeGst, actual.getTaxEach().intValue());
 		assertEquals(0, actual.getDiscountEachExTax().intValue());
 	}
-	
+
 	private void assertModel(int feeExGst, int feeGst, int money, int voucherCount) {
 		ObjectContext context = purchaseController.getCayenneService().newContext();
 		Invoice invoice = purchaseController.getModel().getInvoice();
 		Invoice invoiceDB = Cayenne.objectForPK(context, Invoice.class, invoice.getId());
 		//check Invoice
-		assertInvoices(invoice,invoiceDB,feeExGst,feeGst);
-		
+		assertInvoices(invoice, invoiceDB, feeExGst, feeGst);
+
 		List<InvoiceLine> invoiceLines = invoice.getInvoiceLines();
 		List<InvoiceLine> invoiceLinesDB = invoiceDB.getInvoiceLines();
 		//check InvoiceLine
 		assertEquals(1, invoiceLinesDB.size());
 		assertEquals(invoiceLines.size(), invoiceLinesDB.size());
-		assertInvoiceLines(invoiceLines.get(0),invoiceLinesDB.get(0),feeExGst,feeGst);
-		
+		assertInvoiceLines(invoiceLines.get(0), invoiceLinesDB.get(0), feeExGst, feeGst);
+
 		Enrolment enrolment = invoiceLines.get(0).getEnrolment();
 		Enrolment enrolmentDB = invoiceLinesDB.get(0).getEnrolment();
 		//check Enrolment
 		assertNotSame(enrolment, enrolmentDB);
 		assertEquals(enrolment.getId(), enrolmentDB.getId());
-		assertEquals(Status_SUCCES,enrolmentDB.getStatus().name().toString());
+		assertEquals(Status_SUCCES, enrolmentDB.getStatus().name().toString());
 
 		List<PaymentInLine> paymentInLines = invoice.getPaymentInLines();
 		List<PaymentInLine> paymentInLinesDB = invoiceDB.getPaymentInLines();
-		assertEquals(voucherCount+1, paymentInLinesDB.size());
+		assertEquals(voucherCount + 1, paymentInLinesDB.size());
 		assertEquals(paymentInLines.size(), paymentInLinesDB.size());
 
 		int paymentInEqualsCount = 0;
@@ -86,46 +86,46 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 					paymentInEqualsCount++;
 					PaymentIn paymentIn = paymentInLine.getPaymentIn();
 					PaymentIn paymentInDB = paymentInLineDB.getPaymentIn();
-					assertEquals(paymentIn.getId(),paymentInDB.getId());
-					
-					if(!paymentInDB.getVoucherPaymentIns().isEmpty()){
+					assertEquals(paymentIn.getId(), paymentInDB.getId());
+
+					if (!paymentInDB.getVoucherPaymentIns().isEmpty()) {
 						payIn_VoucherPayIn_Links++;
 						VoucherPaymentIn voucherPaymentIn = paymentIn.getVoucherPaymentIns().get(0);
 						VoucherPaymentIn voucherPaymentInDB = paymentInDB.getVoucherPaymentIns().get(0);
 						//check VoucherPaymentIn
-						assertEquals(voucherPaymentIn.getId(),voucherPaymentInDB.getId());
-						assertEquals(VoucherPaymentStatus_APPROVED,voucherPaymentInDB.getStatus().toString());
+						assertEquals(voucherPaymentIn.getId(), voucherPaymentInDB.getId());
+						assertEquals(VoucherPaymentStatus_APPROVED, voucherPaymentInDB.getStatus().toString());
 						//check PaymentInLine and PaymentIn that has relation with Voucher						
-						if (voucherCount<2){
-							assertEquals(VoucherCode_1,voucherPaymentInDB.getVoucher().getCode());
-							assertEquals(feeExGst+feeGst-money,paymentInLineDB.getAmount().intValue());
+						if (voucherCount < 2) {
+							assertEquals(VoucherCode_1, voucherPaymentInDB.getVoucher().getCode());
+							assertEquals(feeExGst + feeGst - money, paymentInLineDB.getAmount().intValue());
 						} else {
-							if (paymentInLineDB.getAmount().intValue()==money){
-								assertEquals(VoucherCode_2,voucherPaymentInDB.getVoucher().getCode());
+							if (paymentInLineDB.getAmount().intValue() == money) {
+								assertEquals(VoucherCode_2, voucherPaymentInDB.getVoucher().getCode());
 							} else {
-								assertEquals(feeExGst+feeGst-money,paymentInLineDB.getAmount().intValue());
-								assertEquals(VoucherCode_1,voucherPaymentInDB.getVoucher().getCode());
+								assertEquals(feeExGst + feeGst - money, paymentInLineDB.getAmount().intValue());
+								assertEquals(VoucherCode_1, voucherPaymentInDB.getVoucher().getCode());
 							}
-						
+
 						}
-						assertEquals(Status_SUCCES,paymentInDB.getStatus().name().toString());
-						assertEquals(PaymentTypeVoucher,paymentInDB.getType().toString());
-						if(voucherPaymentInDB.getVoucher().isMoneyVoucher()){
+						assertEquals(Status_SUCCES, paymentInDB.getStatus().name().toString());
+						assertEquals(PaymentTypeVoucher, paymentInDB.getType().toString());
+						if (voucherPaymentInDB.getVoucher().isMoneyVoucher()) {
 							assertNull(voucherPaymentInDB.getInvoiceLine());
 						} else {
 							//check another PaymentInLine and PaymentIn
-							assertEquals(money,paymentInLineDB.getAmount().intValue());
-							assertEquals(Status_SUCCES,paymentInDB.getStatus().name().toString());
-							assertEquals(PaymentTypeZero,paymentInDB.getType().toString());						
-						}						
+							assertEquals(money, paymentInLineDB.getAmount().intValue());
+							assertEquals(Status_SUCCES, paymentInDB.getStatus().name().toString());
+							assertEquals(PaymentTypeZero, paymentInDB.getType().toString());
+						}
 					}
 				}
 			}
 		}
-		assertEquals(voucherCount+1, paymentInEqualsCount);
-		assertEquals(voucherCount,payIn_VoucherPayIn_Links);		
+		assertEquals(voucherCount + 1, paymentInEqualsCount);
+		assertEquals(voucherCount, payIn_VoucherPayIn_Links);
 	}
-	
+
 	private void assertQueuedTransaction(int voucherCount, int discountCount) {
 		ObjectContext context = purchaseController.getCayenneService().newContext();
 		SelectQuery selectQuery = new SelectQuery(QueuedTransaction.class);
@@ -133,7 +133,7 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		assertEquals(2, listQT.size());
 
 		//check first QueuedTransaction contains right QueuedRecords
-		assertEquals(3+voucherCount+discountCount, listQT.get(0).getQueuedRecords().size());
+		assertEquals(3 + voucherCount + discountCount, listQT.get(0).getQueuedRecords().size());
 		List<QueuedRecord> qRecords = listQT.get(0).getQueuedRecords();
 		Expression matchExp1 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "Student");
 		assertEquals(1, matchExp1.filterObjects(qRecords).size());
@@ -146,7 +146,7 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		Expression matchExp5 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "Discount");
 		assertEquals(discountCount, matchExp5.filterObjects(qRecords).size());
 
-		assertEquals(5 + voucherCount * 4+ discountCount, listQT.get(1).getQueuedRecords().size());
+		assertEquals(5 + voucherCount * 4 + discountCount, listQT.get(1).getQueuedRecords().size());
 		//check second QueuedTransaction contains right QueuedRecords
 		List<QueuedRecord> queuedRecords = listQT.get(1).getQueuedRecords();
 		Expression exp1 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "Enrolment");
@@ -156,9 +156,9 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		Expression exp3 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "Invoice");
 		assertEquals(1, exp3.filterObjects(queuedRecords).size());
 		Expression exp4 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "PaymentInLine");
-		assertEquals(voucherCount+1, exp4.filterObjects(queuedRecords).size());
+		assertEquals(voucherCount + 1, exp4.filterObjects(queuedRecords).size());
 		Expression exp5 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "PaymentIn");
-		assertEquals(voucherCount+1, exp5.filterObjects(queuedRecords).size());
+		assertEquals(voucherCount + 1, exp5.filterObjects(queuedRecords).size());
 		Expression exp6 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "VoucherPaymentIn");
 		assertEquals(voucherCount, exp6.filterObjects(queuedRecords).size());
 		Expression exp7 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "Voucher");
@@ -166,8 +166,6 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		Expression exp8 = ExpressionFactory.matchExp(QueuedRecord.ENTITY_IDENTIFIER_PROPERTY, "InvoiceLineDiscount");
 		assertEquals(discountCount, exp8.filterObjects(queuedRecords).size());
 	}
-	
-	
 
 	@Test
 	public void testMoneyVoucherRedemptionWithoutMoney() throws InterruptedException {
@@ -184,12 +182,12 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		makeInvalidPayment();
 		assertTrue(purchaseController.isPaymentResult());
 		assertTrue(purchaseController.isFinished());
-		assertModel(400,50,0,1);
+		assertModel(400, 50, 0, 1);
 		//check Voucher
-		assertEquals(0,model.getVouchers().get(0).getValueRemaining().intValue());
-		assertEquals(VoucherStatus_REDEEMED,model.getVouchers().get(0).getStatus().name().toString());
+		assertEquals(0, model.getVouchers().get(0).getValueRemaining().intValue());
+		assertEquals(VoucherStatus_REDEEMED, model.getVouchers().get(0).getStatus().name().toString());
 
-		assertQueuedTransaction(1,0);
+		assertQueuedTransaction(1, 0);
 	}
 
 	@Test
@@ -207,12 +205,12 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		makeInvalidPayment();
 		assertTrue(purchaseController.isPaymentResult());
 		assertTrue(purchaseController.isFinished());
-		assertModel(100,10,0,1);
+		assertModel(100, 10, 0, 1);
 		//check Voucher
-		assertEquals(340,model.getVouchers().get(0).getValueRemaining().intValue());
-		assertEquals("ACTIVE",model.getVouchers().get(0).getStatus().name().toString());
+		assertEquals(340, model.getVouchers().get(0).getValueRemaining().intValue());
+		assertEquals("ACTIVE", model.getVouchers().get(0).getStatus().name().toString());
 
-		assertQueuedTransaction(1,0);
+		assertQueuedTransaction(1, 0);
 	}
 
 	@Test
@@ -229,15 +227,15 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		assertTrue("purchaseController is not in state 'EditPayment'", purchaseController.isEditPayment());
 		makeValidPayment();
 		assertTrue(purchaseController.isPaymentResult());
-		assertTrue(purchaseController.isFinished());		
-		assertModel(500,50,100,1);
+		assertTrue(purchaseController.isFinished());
+		assertModel(500, 50, 100, 1);
 		//check Voucher
-		assertEquals(0,model.getVouchers().get(0).getValueRemaining().intValue());
-		assertEquals(VoucherStatus_REDEEMED,model.getVouchers().get(0).getStatus().name().toString());
-		assertQueuedTransaction(1,0);
+		assertEquals(0, model.getVouchers().get(0).getValueRemaining().intValue());
+		assertEquals(VoucherStatus_REDEEMED, model.getVouchers().get(0).getStatus().name().toString());
+		assertQueuedTransaction(1, 0);
 	}
-	
-	
+
+
 	@Test
 	public void testMoneyVoucherRedemptionWithTwoVouchers() throws InterruptedException {
 		PurchaseController purchaseController = init(Arrays.asList(1002L), Collections.EMPTY_LIST, Collections.EMPTY_LIST, false);
@@ -254,11 +252,13 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		makeInvalidPayment();
 		assertTrue(purchaseController.isPaymentResult());
 		assertTrue(purchaseController.isFinished());
-		assertModel(500,50,100,2);		
+		assertModel(500, 50, 100, 2);
 		//check Voucher
-		assertEquals(0,model.getVouchers().get(0).getValueRemaining().intValue());
-		assertEquals(VoucherStatus_REDEEMED,model.getVouchers().get(0).getStatus().name().toString());
-		assertQueuedTransaction(2,0);	
+		assertEquals(0, model.getVouchers().get(0).getValueRemaining().intValue());
+		assertEquals(VoucherStatus_REDEEMED, model.getVouchers().get(0).getStatus().name().toString());
+		assertEquals(0, model.getVouchers().get(1).getValueRemaining().intValue());
+		assertEquals(VoucherStatus_REDEEMED, model.getVouchers().get(1).getStatus().name().toString());
+		assertQueuedTransaction(2, 0);
 	}
 
 	@Test
@@ -280,21 +280,21 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		//check Vouchers
 		Voucher moneyVoucher;
 		Voucher courseClassVoucher;
-		if (model.getVouchers().get(0).isMoneyVoucher()){
-			moneyVoucher=model.getVouchers().get(0);
-			courseClassVoucher=model.getVouchers().get(1);
+		if (model.getVouchers().get(0).isMoneyVoucher()) {
+			moneyVoucher = model.getVouchers().get(0);
+			courseClassVoucher = model.getVouchers().get(1);
 		} else {
-			moneyVoucher=model.getVouchers().get(1);
-			courseClassVoucher=model.getVouchers().get(0);
+			moneyVoucher = model.getVouchers().get(1);
+			courseClassVoucher = model.getVouchers().get(0);
 		}
-		assertEquals(450,moneyVoucher.getValueRemaining().intValue());
-		assertEquals("ACTIVE",moneyVoucher.getStatus().name().toString());
-		assertEquals(VoucherStatus_REDEEMED,courseClassVoucher.getStatus().name().toString());		
+		assertEquals(450, moneyVoucher.getValueRemaining().intValue());
+		assertEquals("ACTIVE", moneyVoucher.getStatus().name().toString());
+		assertEquals(VoucherStatus_REDEEMED, courseClassVoucher.getStatus().name().toString());
 	}
-	
+
 	@Test
 	public void testMoneyVoucherRedemptionWithDiscount() throws InterruptedException {
-		PurchaseController purchaseController = init(Arrays.asList(1001L), Collections.EMPTY_LIST, Arrays.asList(1001L) , false);
+		PurchaseController purchaseController = init(Arrays.asList(1001L), Collections.EMPTY_LIST, Arrays.asList(1001L), false);
 		assertTrue("purchaseController is not in state 'AddContact'", purchaseController.isAddContact());
 		addFirstContact(1001L);
 		assertTrue("purchaseController is not in state 'EditCheckout'", purchaseController.isEditCheckout());
@@ -303,7 +303,7 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		addCode(VoucherCode_1);
 		assertEquals(1, model.getSelectedVouchers().size());
 		addCode("d1001");
-		assertEquals(1,model.getDiscounts().size());
+		assertEquals(1, model.getDiscounts().size());
 
 		proceedToPayment();
 		assertTrue("purchaseController is not in state 'EditPayment'", purchaseController.isEditPayment());
@@ -311,14 +311,44 @@ public class MoneyVoucherRedemptionTest extends ACheckoutTest {
 		assertTrue(purchaseController.isPaymentResult());
 		assertTrue(purchaseController.isFinished());
 		//check Voucher
-	
-		assertEquals(113,model.getVouchers().get(0).getValueRemaining().intValue());
-		assertEquals("ACTIVE",model.getVouchers().get(0).getStatus().name().toString());
 
-		assertQueuedTransaction(1,1);
+		assertEquals(113, model.getVouchers().get(0).getValueRemaining().intValue());
+		assertEquals("ACTIVE", model.getVouchers().get(0).getStatus().name().toString());
+
+		assertQueuedTransaction(1, 1);
 	}
-	
-	
 
-
+	@Test
+	public void testMoneyVoucherRedemptionOnDifferentStages() throws InterruptedException {
+		PurchaseController purchaseController = init(Arrays.asList(1001L), Collections.EMPTY_LIST, Collections.EMPTY_LIST, false);
+		assertTrue("purchaseController is not in state 'AddContact'", purchaseController.isAddContact());
+		addFirstContact(1001L);
+		assertTrue("purchaseController is not in state 'EditCheckout'", purchaseController.isEditCheckout());
+		PurchaseModel model = purchaseController.getModel();
+		assertTrue(model.getSelectedVouchers().isEmpty());
+		addCode(VoucherCode_2);
+		assertEquals(1, model.getSelectedVouchers().size());
+		proceedToPayment();
+		addCode(VoucherCode_1);
+		assertEquals(2, model.getSelectedVouchers().size());
+		assertTrue("purchaseController is not in state 'EditPayment'", purchaseController.isEditPayment());
+		makeInvalidPayment();
+		assertTrue(purchaseController.isPaymentResult());
+		assertTrue(purchaseController.isFinished());
+		assertModel(400, 50, 100, 2);
+		//check Vouchers
+		Voucher moneyVoucher1;
+		Voucher moneyVoucher2;
+		if (model.getVouchers().get(0).getStatus().name().toString().equals("ACTIVE")) {
+			moneyVoucher1 = model.getVouchers().get(0);
+			moneyVoucher2 = model.getVouchers().get(1);
+		} else {
+			moneyVoucher1 = model.getVouchers().get(1);
+			moneyVoucher2 = model.getVouchers().get(0);
+		}
+		assertEquals(100, moneyVoucher1.getValueRemaining().intValue());
+		assertEquals(0, moneyVoucher2.getValueRemaining().intValue());
+		assertEquals(VoucherStatus_REDEEMED, moneyVoucher2.getStatus().name().toString());
+		assertQueuedTransaction(2, 0);
+	}
 }
---
+++ enrol/src/test/java/ish/oncourse/enrol/checkout/PesonalVoucherRedemptionTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+
+package ish.oncourse.enrol.checkout;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PesonalVoucherRedemptionTest extends ACheckoutTest {
+
+	@Before
+	public void setup() throws Exception {
+		setup("ish/oncourse/enrol/checkout/PersonalVoucherRedemptionTest.xml");
+	}
+
+	@Test
+	public void testPersonalVoucherRedemption() throws InterruptedException {
+		PurchaseController purchaseController = init(Arrays.asList(1001L), Collections.EMPTY_LIST, Collections.EMPTY_LIST, false);
+		assertTrue("purchaseController is not in state 'AddContact'", purchaseController.isAddContact());
+		addFirstContact(1001L);
+		assertTrue("purchaseController is not in state 'EditCheckout'", purchaseController.isEditCheckout());
+		PurchaseModel model = purchaseController.getModel();
+		assertTrue(model.getSelectedVouchers().isEmpty());
+		addCode("v1003");
+		assertEquals(1, model.getSelectedVouchers().size());
+		proceedToPayment();
+		assertTrue("purchaseController is not in state 'EditPayment'", purchaseController.isEditPayment());
+		makeInvalidPayment();
+		assertTrue(purchaseController.isPaymentResult());
+		assertTrue(purchaseController.isFinished());
+		assertEquals(0, model.getVouchers().get(0).getValueRemaining().intValue());
+		assertEquals("Redeemed", model.getVouchers().get(0).getStatus().toString());
+	}
+
+}
