f134ad24fa390875fdc859fdea6b06c37bfc85d3
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2014-05-26 17:14:14 +0000

    #21292. Set created/modified dates for WebTemplate in post-add and pre-update callbacks.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@35598 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/WebTemplate.java
+++ common/common-model/src/main/java/ish/oncourse/model/WebTemplate.java
@@ -2,6 +2,25 @@ package ish.oncourse.model;
 
 import ish.oncourse.model.auto._WebTemplate;
 
+import java.util.Date;
+
 public class WebTemplate extends _WebTemplate {
 
+	@Override
+	protected void onPostAdd() {
+		Date now = new Date();
+		
+		if (getCreated() == null) {
+			setCreated(now);
+		}
+		
+		if (getModified() == null) {
+			setModified(now);
+		}
+	}
+
+	@Override
+	protected void onPreUpdate() {
+		setModified(new Date());
+	}
 }
