18204ecca54a735f7fb02c57d8adc23fc7dc00b9
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-06-07 09:48:03 +0000

    #21690 common validation mecanism  was introduced for WebContent.name and  WebNode.name and WebSiteLayout.layoutKey (properties which are used as parh of webdav urls). Array ResourceNameValidator.INVALID_CHARS contains list of chars which cannot be used in webdav file name.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@35846 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-model/src/main/java/ish/oncourse/utils/ResourceNameValidator.java
@@ -0,0 +1,30 @@
+package ish.oncourse.utils;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * The class is used to validate name property for webdav ressources like WebNode, WebContent, WebLayout.
+ *
+ */
+
+public class ResourceNameValidator {
+    public final static char[] INVALID_CHARS = {
+            '/',
+            '\\',
+            '%',
+    };
+
+    public String validate(String name) {
+        name = StringUtils.trimToEmpty(name);
+        if (name.length() < 3) {
+            return "There should be at least 3 characters in block name";
+        }
+
+        for (char invalidChar : INVALID_CHARS) {
+            if (name.indexOf(invalidChar) > -1)
+                return String.format("Symbol '%c' cannot be used in name", invalidChar);
+        }
+        return null;
+    }
+}
