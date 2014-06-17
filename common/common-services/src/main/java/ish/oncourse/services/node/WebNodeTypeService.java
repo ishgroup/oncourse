1c671e9a61699eff63dcefb1817a97f44fa4327f
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-06-17 13:31:01 +0000

    #21846 WebNodeTypeService.getWebNodeTypes() always sends new request to db (does not use sharedContext) to get all WebNodeTypes. The list is storted with modification date.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@35973 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/services/node/WebNodeTypeService.java
+++ common/common-services/src/main/java/ish/oncourse/services/node/WebNodeTypeService.java
@@ -1,15 +1,18 @@
 package ish.oncourse.services.node;
 
 import ish.oncourse.model.WebNodeType;
+import ish.oncourse.model.WebSiteVersion;
 import ish.oncourse.services.BaseService;
-
-import java.util.List;
-
 import ish.oncourse.services.site.IWebSiteService;
 import ish.oncourse.services.site.IWebSiteVersionService;
+import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.exp.Expression;
 import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.query.SelectQuery;
+import org.apache.cayenne.query.SortOrder;
 import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.util.List;
 
 public class WebNodeTypeService extends BaseService<WebNodeType> implements
 		IWebNodeTypeService {
@@ -34,10 +37,18 @@ public class WebNodeTypeService extends BaseService<WebNodeType> implements
 	}
 
 	public List<WebNodeType> getWebNodeTypes() {
-		IWebSiteService webSiteService = getWebSiteService();
-		
-		return findByQualifier(ExpressionFactory.matchExp(
-				WebNodeType.WEB_SITE_VERSION_PROPERTY,
-				webSiteVersionService.getCurrentVersion(webSiteService.getCurrentWebSite())));
+
+        WebSiteVersion version = webSiteVersionService.getCurrentVersion(getWebSiteService().getCurrentWebSite());
+
+        ObjectContext context = getCayenneService().newNonReplicatingContext();
+
+        Expression expression = ExpressionFactory.matchExp(
+                WebNodeType.WEB_SITE_VERSION_PROPERTY,context.localObject(version)
+        );
+
+        SelectQuery selectQuery = new SelectQuery(WebNodeType.class, expression);
+        selectQuery.addOrdering(WebNodeType.MODIFIED_PROPERTY, SortOrder.DESCENDING);
+
+        return context.performQuery(selectQuery);
 	}
 }
