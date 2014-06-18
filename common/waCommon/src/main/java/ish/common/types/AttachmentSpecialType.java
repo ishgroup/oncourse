0062ccbb4123d31be99cd86bb6486398a5ebfcc6
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2014-06-18 10:11:38 +0000

    #21130. Enumeration for special types of attachments such as profile picture, course image etc. For now we handle only one special type of attachments but more will be added in future.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@35986 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/waCommon/src/main/java/ish/common/types/AttachmentSpecialType.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+package ish.common.types;
+
+import ish.common.util.DisplayableExtendedEnumeration;
+
+public enum AttachmentSpecialType implements DisplayableExtendedEnumeration<Integer> {
+	
+	PROFILE_PICTURE(0, "Profile picture");
+	
+	private int value;
+	private String displayName;
+
+	private AttachmentSpecialType(int value, String displayName) {
+		this.value = value;
+		this.displayName = displayName;
+	}
+
+	/**
+	 * @see ish.common.util.DisplayableExtendedEnumeration#getDatabaseValue()
+	 */
+	@Override
+	public Integer getDatabaseValue() {
+		return value;
+	}
+
+	/**
+	 * @see ish.common.util.DisplayableExtendedEnumeration#getDisplayName()
+	 */
+	@Override
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	/**
+	 * @see java.lang.Enum#toString()
+	 */
+	@Override
+	public String toString() {
+		return getDisplayName();
+	}
+}
