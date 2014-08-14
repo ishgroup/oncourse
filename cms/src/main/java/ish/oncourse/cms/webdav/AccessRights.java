5d32ec3eb27e5621c0840550bdc57dd5587ea74e
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-08-14 11:01:28 +0000

    #22439 Allowed http methods for webdav folders blocks, pages, templates were extended with MOVE. It needs to allow an user rename files.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@36652 fb2e276d-fae9-0310-b578-b71ac85840b9

--- cms/src/main/java/ish/oncourse/cms/webdav/AccessRights.java
+++ cms/src/main/java/ish/oncourse/cms/webdav/AccessRights.java
@@ -7,8 +7,9 @@ import static io.milton.http.Request.Method.*;
 public interface AccessRights {
     /**
      * Allowed request methods for read only dir and which can add child dir (like /cms/webdav/templates)
+     * MOVE needs to allow rename files and the method needs for save neew content
      */
-    public static final Request.Method[] DIR_READ_ONLY_AND_ADD_CHILD = new Request.Method[]{GET, HEAD, OPTIONS, PROPFIND, MKCOL, PUT, POST};
+    public static final Request.Method[] DIR_READ_ONLY_AND_ADD_CHILD = new Request.Method[]{GET, HEAD, OPTIONS, PROPFIND, MKCOL, PUT, POST, MOVE};
 
     /**
      * Allowed request methods for read only dir (like /cms/webdav)
