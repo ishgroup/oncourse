d5a64455c52e2122aa3c7cb9eb28eb24d649c025
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-08-15 12:31:43 +0000

    #22458 'Parent' item was introduced to the steps across the top when a parent is required.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@36676 fb2e276d-fae9-0310-b578-b71ac85840b9

--- enrol/src/main/java/ish/oncourse/enrol/components/checkout/Progress.java
+++ enrol/src/main/java/ish/oncourse/enrol/components/checkout/Progress.java
@@ -1,6 +1,7 @@
 package ish.oncourse.enrol.components.checkout;
 
 import ish.oncourse.enrol.checkout.PurchaseController;
+import ish.oncourse.enrol.checkout.ValidationResult;
 import ish.oncourse.util.FormatUtils;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
@@ -35,17 +36,34 @@ public class Progress {
     private String paymentUrl;
 
 
+    @Property
+    private String parentClass;
+
+    @Property
+    private String parentUrl;
+
+    @Property
+    private boolean showParentBeforCheckout;
+
+    @Property
+    private boolean showParentAfterCheckout;
+
     @SetupRender
     void beforeRender() {
+        showParentBeforCheckout = false;
+        showParentAfterCheckout = false;
+
         if (purchaseController == null)
         {
             detailsClass = CLASS_disable;
             checkoutClass = CLASS_disable;
             paymentClass = CLASS_disable;
+            parentClass = CLASS_disable;
 
             detailsUrl = URL_current;
             checkoutUrl = URL_current;
             paymentUrl = URL_current;
+            parentUrl = URL_current;
             return;
         }
 
@@ -57,44 +75,71 @@ public class Progress {
                 detailsClass = CLASS_disable;
                 checkoutClass = CLASS_disable;
                 paymentClass = CLASS_disable;
+                parentClass = CLASS_disable;
 
                 detailsUrl = URL_current;
                 checkoutUrl = URL_current;
                 paymentUrl = URL_current;
+                parentClass = URL_current;
                 break;
             case editCheckout:
                 detailsClass = CLASS_disable;
                 checkoutClass = CLASS_active;
                 paymentClass = CLASS_disable;
+                parentClass = CLASS_disable;
 
                 detailsUrl = URL_current;
                 checkoutUrl = URL_current;
                 paymentUrl = URL_current;
+                parentClass = URL_current;
+                showParentAfterCheckout = showParent();
                 break;
             case addContact:
             case editContact:
             case editConcession:
-                detailsClass = CLASS_active;
+                showParentBeforCheckout = showParent();
+                if (showParentBeforCheckout)
+                {
+                    detailsClass = CLASS_disable;
+                    parentClass = CLASS_active;
+                }
+                else
+                {
+                    detailsClass = CLASS_active;
+                    parentClass = CLASS_disable;
+                }
                 checkoutClass = CLASS_disable;
                 paymentClass = CLASS_disable;
 
                 detailsUrl = URL_current;
                 checkoutUrl = URL_current;
                 paymentUrl = URL_current;
+                parentUrl = URL_current;
                 break;
             case editPayment:
             case editCorporatePass:
                 detailsClass = CLASS_disable;
                 checkoutClass = FormatUtils.EMPTY_STRING;
                 paymentClass = CLASS_active;
+                parentClass = CLASS_disable;
 
                 detailsUrl = URL_current;
                 checkoutUrl = URL_checkout;
                 paymentUrl = URL_current;
+                parentUrl = URL_checkout;
                 break;
             default:
                 throw new IllegalArgumentException();
         }
+    }
+
+    private boolean showParent()
+    {
+        if (purchaseController == null)
+            return false;
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.setMessages(purchaseController.getMessages());
+        return !purchaseController.getModelValidator().validateContacts(validationResult);
     }
 
 
