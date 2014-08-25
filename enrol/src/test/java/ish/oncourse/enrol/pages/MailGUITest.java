41dfad878b4b1011e44741480bb5dfb9d0b9385f
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-08-25 16:48:42 +0000

    #22511 Mail page was adjusted to use new implementation of Errors component. Junit added
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@36797 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ enrol/src/test/java/ish/oncourse/enrol/pages/MailGUITest.java
@@ -0,0 +1,40 @@
+package ish.oncourse.enrol.pages;
+
+import ish.oncourse.enrol.checkout.ACheckoutTest;
+import ish.oncourse.model.CourseClass;
+import ish.oncourse.services.cookies.ICookiesService;
+import org.apache.log4j.Logger;
+import org.apache.tapestry5.dom.Element;
+import org.apache.tapestry5.internal.test.TestableResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MailGUITest extends ACheckoutTest {
+	private static final Logger LOGGER = Logger.getLogger(MailGUITest.class);
+	@Before
+	public void setup() throws Exception {
+		setup("ish/oncourse/enrol/pages/CheckoutGUITest.xml");
+
+	}
+
+	@Test
+	public void testSuccessfulPayment() throws InterruptedException {
+		getPageTester().getRegistry().getService("testCookiesService", ICookiesService.class).writeCookieValue(CourseClass.SHORTLIST_COOKIE_KEY, "1001");
+
+		//init load checkout
+		TestableResponse response = getPageTester().renderPageAndReturnResponse("Mail");
+		assertResponse(response);
+		Element element = response.getRenderedDocument().getElementById("addstudent-block");
+		assertNotNull(element);
+	}
+
+	private void assertResponse(TestableResponse response) {
+		if (LOGGER.isDebugEnabled())
+			LOGGER.debug(response.getRenderedDocument());
+		assertEquals(200, response.getStatus());
+		assertNull("Not Error500 page", response.getRenderedDocument().getElementById("exception"));
+
+	}
+}
