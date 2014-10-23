8d9e4883430c0e69c1908c3cfaae6c3400aa5127
Artyom Kravenko <artyom@ish.com.au>
2014-10-23 09:40:50 +0000

    #22331 Some code adjustment, add coments
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@37323 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/static/js/html5fix.js
+++ portal/src/main/static/js/html5fix.js
@@ -5,21 +5,29 @@ goog.require('jquery');
 
 var $j = jQuery.noConflict();
 
-$j(document).ready(function() {
-    jQuery(function() {
-        jQuery.support.placeholder = false;
-        webkit_type = document.createElement('input');
-        if('placeholder' in webkit_type) jQuery.support.placeholder = true;
-    });
+
+/**
+ * The method check that the browser supports placeholders for input tags (html5).
+ * If not (IE 9 and below) then restore placeholders.
+ *
+ */
+function placeholderFix() {
+
+    jQuery.support.placeholder = false;
+    webkit_type = document.createElement('input');
+    if ('placeholder' in webkit_type) jQuery.support.placeholder = true;
+
     
-    $j(function() {
-    
-        if(!jQuery.support.placeholder) {
-    
+    // part of the code is moved to an anonymous function - to properly refrexhing of placeholders
+    // this code is borrowed from NIDA's web site
+    $j(function () {
+
+        if (!jQuery.support.placeholder) {
+
             var active = document.activeElement;
-    
+
             $j(':text, textarea, :password').focus(function () {
-    
+
                 if (($j(this).attr('placeholder')) && ($j(this).attr('placeholder').length > 0) && ($j(this).attr('placeholder') != '') && $j(this).val() == $j(this).attr('placeholder')) {
                     $j(this).val('').removeClass('hasPlaceholder');
                 }
@@ -28,12 +36,18 @@ $j(document).ready(function() {
                     $j(this).val($j(this).attr('placeholder')).addClass('hasPlaceholder');
                 }
             });
-    
+
             $j(':text, textarea, :password').blur();
             $j(active).focus();
             $j('form').submit(function () {
-                $j(this).find('.hasPlaceholder').each(function() { $j(this).val(''); });
+                $j(this).find('.hasPlaceholder').each(function () {
+                    $j(this).val('');
+                });
             });
         }
     });
+}
+
+$j(document).ready(function() {
+    placeholderFix();
 });
