797f6214e0d6da795b3ded75062bd66686e27c13
Andrey Narut <anarut@ish.com.au>
2014-10-27 13:15:54 +0000

    #23105 added enum class enrolmentType for course.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@37357 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/waCommon/src/main/java/ish/common/types/EnrolmentType.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+package ish.common.types;
+
+import ish.common.util.DisplayableExtendedEnumeration;
+
+public enum EnrolmentType implements DisplayableExtendedEnumeration<Integer> {
+	
+	OPEN(1, "Open enrolment"),
+	
+	BY_APPLICATION(2, "Enrolment by application");
+
+	private String displayName;
+	private int value;
+
+	private EnrolmentType(int value, String displayName) {
+		this.value = value;
+		this.displayName = displayName;
+	}
+
+	@Override
+	public Integer getDatabaseValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getDisplayName() {
+		return this.displayName;
+	}
+
+	@Override
+	public String toString() {
+		return getDisplayName();
+	}
+}
