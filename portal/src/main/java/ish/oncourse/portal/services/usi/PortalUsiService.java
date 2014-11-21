9f9f7bdb6a99e60efd4f23868a021023df8e1d84
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-11-21 14:42:06 +0000

    #23189 USI Step3 form was implemented
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@37674 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/services/usi/PortalUsiService.java
+++
@@ -1,7 +0,0 @@
-package ish.oncourse.portal.services.usi;
-
-/**
- * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
- */
-public class PortalUsiService {
-}
