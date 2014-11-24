ca489204d5475246165a12e52a5e4a0ad6a74957
Andrey Koyro <andrey.koyro@objectstyle.com>
2014-11-24 07:29:38 +0000

    #23271 browser cache buster  was added for portal-min.js and portal.css.  metaGeneratorContent property as buster string was replaced to  ciVersion (build id).
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@37685 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/components/PageHeader.java
+++ portal/src/main/java/ish/oncourse/portal/components/PageHeader.java
@@ -1,7 +1,6 @@
 package ish.oncourse.portal.components;
 
 import ish.oncourse.services.environment.IEnvironmentService;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
@@ -9,21 +8,31 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 
 public class PageHeader {
-	
+
 	@Property
 	@Parameter
 	private String title;
-	
+
 	@Inject
 	private Request request;
-	
+
 	@Inject
 	private IEnvironmentService environmentService;
-	
+
 	public String getContextPath() {
 		return request.getContextPath();
 	}
-	
+
+    public String getCiVersion()
+    {
+        String ciVersion = environmentService.getCiVersion();
+        if (StringUtils.isEmpty(StringUtils.trimToEmpty(ciVersion))) {
+            return "r" + ciVersion;
+        } else {
+          return "dvelopment";
+        }
+    }
+
 	public String getMetaGeneratorContent() {
 		StringBuilder buff = new StringBuilder(
 				environmentService.getApplicationName());
@@ -40,10 +49,9 @@ public class PageHeader {
 			buff.append(scmVersion);
 		}
 
-		String ciVersion = environmentService.getCiVersion();
-		if (!StringUtils.isEmpty(ciVersion)) {
+		String ciVersion = getCiVersion();
+		if (ciVersion != null) {
 			buff.append(StringUtils.isEmpty(buildServerID) ? ' ' : '/');
-			buff.append('r');
 			buff.append(ciVersion);
 		}
 		return buff.toString();
