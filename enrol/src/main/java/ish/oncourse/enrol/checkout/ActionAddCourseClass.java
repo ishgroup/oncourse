273660e1f5c2fc0baa64fbe29390c3df809b6b70
Artyom Kravenko <artyom@ish.com.au>
2014-12-23 09:17:27 +0000

    #23633  Introduce  application  creation to AddCourseClass action - if courses enrolments available only by application and the student has no offered application for this course then create new application else proceed to enrolment creation
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@38033 fb2e276d-fae9-0310-b578-b71ac85840b9

--- enrol/src/main/java/ish/oncourse/enrol/checkout/ActionAddCourseClass.java
+++ enrol/src/main/java/ish/oncourse/enrol/checkout/ActionAddCourseClass.java
@@ -1,5 +1,7 @@
 package ish.oncourse.enrol.checkout;
 
+import ish.common.types.CourseEnrolmentType;
+import ish.oncourse.model.Application;
 import ish.oncourse.model.Contact;
 import ish.oncourse.model.CourseClass;
 import ish.oncourse.model.Enrolment;
@@ -20,15 +22,36 @@ public class ActionAddCourseClass extends APurchaseAction {
         List<Contact> contacts = getModel().getContacts();
         for (Contact contact : contacts) {
 			if (!contact.getIsCompany()) {
-                Enrolment enrolment = getModel().getEnrolmentBy(contact, courseClass);
-                if (enrolment == null) {
-					enrolment = getController().createEnrolment(courseClass, contact.getStudent());
-					getModel().addEnrolment(enrolment);
+				
+				//if courses enrolments available only by application and the student has no offered application for this course
+				//then create new application
+				//else proceed to enrolment creation 
+				if (CourseEnrolmentType.ENROLMENT_BY_APPLICATION.equals(courseClass.getCourse().getEnrolmentType()) &&
+						getController().getApplicationService().findOfferedApplicationBy(courseClass.getCourse(),contact.getStudent()) == null) {
+					
+					Application application = getModel().getApplicationBy(contact, courseClass.getCourse());
+					if (application == null) {
+						application = getController().createApplication(contact.getStudent(), courseClass.getCourse());
+						getModel().addApplication(application);
 
-					if (!getModel().addedAsGuardian(contact)) {
-						ActionEnableEnrolment action = PurchaseController.Action.enableEnrolment.createAction(getController());
-						action.setEnrolment(enrolment);
-						action.action();
+						if (!getModel().addedAsGuardian(contact)) {
+							ActionEnableApplication action = PurchaseController.Action.enableApplication.createAction(getController());
+							action.setApplication(application);
+							action.action();
+						}
+					}
+					
+				} else {
+					Enrolment enrolment = getModel().getEnrolmentBy(contact, courseClass);
+					if (enrolment == null) {
+						enrolment = getController().createEnrolment(courseClass, contact.getStudent());
+						getModel().addEnrolment(enrolment);
+
+						if (!getModel().addedAsGuardian(contact)) {
+							ActionEnableEnrolment action = PurchaseController.Action.enableEnrolment.createAction(getController());
+							action.setEnrolment(enrolment);
+							action.action();
+						}
 					}
 				}
             }
