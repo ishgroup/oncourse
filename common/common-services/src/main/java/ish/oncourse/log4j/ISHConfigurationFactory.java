685e35febc338fa862394401b2526cdecb55ae49
Andrey Koyro <andrey.koyro@objectstyle.com>
2015-04-16 09:20:49 +0000

    #24645 Default XmlConfigurationFactory overwritten to avoid an error message when custom log4j2 configuration file does not exist
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@38945 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-services/src/main/java/ish/oncourse/log4j/ISHConfigurationFactory.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+package ish.oncourse.log4j;
+
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Order;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.xml.XmlConfigurationFactory;
+
+import java.io.File;
+import java.net.URI;
+
+
+/**
+ * Default XmlConfigurationFactory overwritten to avoid an error message when custom log4j2 configuration file does not exist
+ */
+@Plugin(name = "XmlConfigurationFactory", category = ConfigurationFactory.CATEGORY)
+@Order(5)
+public class ISHConfigurationFactory extends XmlConfigurationFactory {
+    public static final String[] SUFFIXES = new String[] {".xml"};
+
+    @Override
+    public String[] getSupportedTypes() {
+        return SUFFIXES;
+    }
+
+    public Configuration getConfiguration(final String name, final URI configLocation) {
+        if (!isActive()) {
+            return null;
+        }
+        if (configLocation != null) {
+            if (! new File(configLocation).exists()) {
+                LOGGER.info("Log4j2 configuration file {} is not found.", configLocation.toString());
+                return null;
+            }
+            final ConfigurationSource source = getInputFromUri(configLocation);
+            if (source != null) {
+                return getConfiguration(source);
+            }
+        }
+        return null;
+    }
+
+
+}
