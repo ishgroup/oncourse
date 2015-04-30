960673330ebd2c264062613732b416ecc5d3445c
Artyom Kravenko <artyom@ish.com.au>
2015-04-30 10:00:44 +0000

    #24332 Adjust  PaymentInModelBuilder: three possible cases for building model: - by transaction group (for oldest replication version) - by PaymentInModel parameters - by session id
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@39013 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/replication/services/PaymentInModelBuilder.java
+++ services/src/main/java/ish/oncourse/webservices/replication/services/PaymentInModelBuilder.java
@@ -7,6 +7,7 @@ import ish.common.types.PaymentSource;
 import ish.common.types.PaymentType;
 import ish.oncourse.model.Enrolment;
 import ish.oncourse.model.Invoice;
+import ish.oncourse.model.InvoiceLine;
 import ish.oncourse.model.PaymentIn;
 import ish.oncourse.model.PaymentInLine;
 import ish.oncourse.utils.PaymentInUtil;
@@ -16,6 +17,7 @@ import ish.oncourse.webservices.util.GenericReplicatedRecord;
 import ish.oncourse.webservices.util.StubUtils;
 import org.apache.cayenne.Cayenne;
 import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.query.ObjectSelect;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -30,12 +32,16 @@ public class PaymentInModelBuilder {
 	private List<GenericReplicatedRecord> replicatedRecords;
 	private String sessionId;
 
+	private PaymentInModelBuilder(){}
+	
 	public PaymentInModel build() throws Exception {
 		validate();
 		PaymentInModel model;
 
 		if (parametersMap != null) {
 			model = buildFromParametersMap();
+		} else if (sessionId != null) {
+			model = buildFromSessionId();
 		} else {
 			model = buildFromReplicatedRecords();
 		}
@@ -63,7 +69,6 @@ public class PaymentInModelBuilder {
 				}
 				// ignore voucher payments here since further processing is based on money payment
 				if (!PaymentType.VOUCHER.equals(p.getType())) {
-					p.setSessionId(sessionId);
 					model.setPaymentIn(p);
 				}
 			}
@@ -80,7 +85,6 @@ public class PaymentInModelBuilder {
 			Invoice invoice = line.getInvoice();
 			invoices.add(invoice);
 		}
-		invoices.first().setSessionId(sessionId);
 		model.getInvoices().add(invoices.first());
 		
 		return model;
@@ -108,7 +112,6 @@ public class PaymentInModelBuilder {
 			if (ReplicationUtils.getEntityName(Invoice.class).equalsIgnoreCase(r.getStub().getEntityIdentifier()) &&
 					invoicesIds.contains(r.getStub().getAngelId())) {
 				Invoice invoice =  (Invoice) Cayenne.objectForPK(context, ReplicationUtils.getEntityName(Invoice.class),r.getStub().getWillowId());
-				invoice.setSessionId(sessionId);
 				model.getInvoices().add(invoice);
 			} else if (ReplicationUtils.getEntityName(Enrolment.class).equalsIgnoreCase(r.getStub().getEntityIdentifier()) &&
 					enrolmentsIds.contains(r.getStub().getAngelId())) {
@@ -124,7 +127,6 @@ public class PaymentInModelBuilder {
 									.getAngelId(), r.getStub().getWillowId()));
 				}
 				
-				paymentIn.setSessionId(sessionId);
 				model.setPaymentIn(paymentIn);
 			}
 		}
@@ -132,7 +134,23 @@ public class PaymentInModelBuilder {
 		
 		return model;
 	}
+	
+	private PaymentInModel buildFromSessionId() {
+		
+		PaymentInModel model = new PaymentInModel();
 
+		PaymentIn paymentIn = ObjectSelect.query(PaymentIn.class).where(PaymentIn.SESSION_ID.eq(sessionId)).selectOne(context);
+		model.setPaymentIn(paymentIn);
+		List<Invoice> invoices = ObjectSelect.query(Invoice.class).where(Invoice.SESSION_ID.eq(sessionId)).select(context);
+		for (Invoice invoice : invoices) {
+			model.getInvoices().add(invoice);
+			for (InvoiceLine invoiceLine : invoice.getInvoiceLines()) {
+				model.getEnrolments().add(invoiceLine.getEnrolment());
+			}
+		}
+		return model;
+	}
+	
 	private void validate() throws Exception {
 		// check if group from angel is empty
 		if (replicatedRecords.isEmpty()) {
@@ -146,19 +164,41 @@ public class PaymentInModelBuilder {
 		}
 	}
 
-	public void setParametersMap(GenericParametersMap parametersMap) {
+	public static PaymentInModelBuilder valueOf(ObjectContext context, List<GenericReplicatedRecord> replicatedRecords) {
+		PaymentInModelBuilder builder = new PaymentInModelBuilder();
+		builder.setContext(context);
+		builder.setReplicatedRecords(replicatedRecords);
+		return builder;
+	}
+
+	public static PaymentInModelBuilder valueOf(ObjectContext context, List<GenericReplicatedRecord> replicatedRecords, GenericParametersMap parametersMap) {
+		PaymentInModelBuilder builder = new PaymentInModelBuilder();
+		builder.setContext(context);
+		builder.setReplicatedRecords(replicatedRecords);
+		builder.setParametersMap(parametersMap);
+		return builder;
+	}
+	
+	public static PaymentInModelBuilder valueOf(ObjectContext context, String sessionId) {
+		PaymentInModelBuilder builder = new PaymentInModelBuilder();
+		builder.setContext(context);
+		builder.setSessionId(sessionId);
+		return builder;
+	}
+
+	private void setParametersMap(GenericParametersMap parametersMap) {
 		this.parametersMap = parametersMap;
 	}
 
-	public void setContext(ObjectContext context) {
+	private void setContext(ObjectContext context) {
 		this.context = context;
 	}
 
-	public void setReplicatedRecords(List<GenericReplicatedRecord> replicatedRecords) {
+	private void setReplicatedRecords(List<GenericReplicatedRecord> replicatedRecords) {
 		this.replicatedRecords = replicatedRecords;
 	}
-	
-	public void setSessionId(String sessionId) {
+
+	private void setSessionId(String sessionId) {
 		this.sessionId = sessionId;
 	}
 }
