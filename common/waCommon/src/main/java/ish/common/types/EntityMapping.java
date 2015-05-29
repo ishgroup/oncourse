01194f18a83bc7d8828589f90226d7358ce3be7e
Andrey Narut <anarut@ish.com.au>
2015-05-29 11:43:53 +0000

    #24258 EnrolmentTagRelation was mapped for replication from Angel to Willow
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@39397 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/waCommon/src/main/java/ish/common/types/EntityMapping.java
+++ common/waCommon/src/main/java/ish/common/types/EntityMapping.java
@@ -49,6 +49,7 @@ public final class EntityMapping {
 		ANGEL_TO_WILLOW.put("TutorTagRelation", "Taggable");
 		ANGEL_TO_WILLOW.put("DocumentTagRelation", "Taggable");
 		ANGEL_TO_WILLOW.put("ApplicationTagRelation", "Taggable");
+		ANGEL_TO_WILLOW.put("EnrolmentTagRelation", "Taggable");
 
 		WILLOW_TO_ANGEL.put("BinaryData", "AttachmentData");
 		WILLOW_TO_ANGEL.put("BinaryInfo", "AttachmentInfo");
