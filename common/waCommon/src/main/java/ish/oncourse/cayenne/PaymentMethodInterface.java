12a9dabd30642bc7d49ecb79613b482af85c1fc4
Artyom Kravenko <artyom@ish.com.au>
2015-06-08 09:28:52 +0000

    #24821 Add name property
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@39494 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/waCommon/src/main/java/ish/oncourse/cayenne/PaymentMethodInterface.java
+++ common/waCommon/src/main/java/ish/oncourse/cayenne/PaymentMethodInterface.java
@@ -9,7 +9,8 @@ import java.util.Date;
 public interface PaymentMethodInterface extends PersistentObjectI  {
 
 	public static final String REAL_TIME_CREDIT_CARD_PROPERTY = "realTimeCreditCard";
-	
+	public static final String NAME_PROPERTY = "name";
+
 	public String getName();
 
 	public Boolean getRealTimeCreditCard();
