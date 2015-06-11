c7a38a661e1d9a9019b6eaaab84c30a66b5d8ef5
Artyom Kravenko <artyom@ish.com.au>
2015-06-11 12:10:56 +0000

    #24821 Replace 'isRealTimeCC' flag with 'type' property
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@39565 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/waCommon/src/main/java/ish/oncourse/cayenne/PaymentMethodInterface.java
+++ common/waCommon/src/main/java/ish/oncourse/cayenne/PaymentMethodInterface.java
@@ -4,22 +4,22 @@
 package ish.oncourse.cayenne;
 
 
+import ish.common.types.PaymentType;
+
 public interface PaymentMethodInterface extends PersistentObjectI {
 
-	public static final String REAL_TIME_CREDIT_CARD_PROPERTY = "realTimeCreditCard";
+	public static final String TYPE_PROPERTY = "type";
 	public static final String NAME_PROPERTY = "name";
 
 	public String getName();
-
-	public Boolean getRealTimeCreditCard();
 
 	public Boolean getActive();
 
 	public AccountInterface getAccount();
 
 	public Boolean getBankedAutomatically();
-
-	public Boolean getSystem();
 	
 	public Boolean getReconcilable();
+	
+	public PaymentType getType();
 }
