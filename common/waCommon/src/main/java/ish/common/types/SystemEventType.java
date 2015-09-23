d4121160d49187ef59634d3765aba465b45a3d98
Artyom Kravenko <artyom@ish.com.au>
2015-09-23 09:35:39 +0000

    #24904 Replace SystemEventType to waCommon
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@40804 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/waCommon/src/main/java/ish/common/types/SystemEventType.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+package ish.common.types;
+
+import ish.common.util.DisplayableExtendedEnumeration;
+
+/**
+ * Enumeration containing all available onCourse system events.
+ */
+public enum SystemEventType implements DisplayableExtendedEnumeration<Integer> {
+
+	ENROLMENT_SUCCESSFUL(1, "Enrolment successful"),
+	ENROLMENT_CANCELLED(2, "Enrolment cancelled");
+
+	private int value;
+	private String displayName;
+
+	SystemEventType(int value, String displayName) {
+		this.value = value;
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	@Override
+	public Integer getDatabaseValue() {
+		return value;
+	}
+}
