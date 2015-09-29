920d6ffdd3e51a1f78db2e871ad9277b5c47fae7
Artyom Kravenko <artyom@ish.com.au>
2015-09-29 14:48:56 +0000

    #22533 adjust postcodeDb for replication
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@40871 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/PostcodeDb.java
+++ common/common-model/src/main/java/ish/oncourse/model/PostcodeDb.java
@@ -1,10 +1,15 @@
 package ish.oncourse.model;
 
 import ish.oncourse.model.auto._PostcodeDb;
+import ish.oncourse.utils.QueueableObjectUtils;
 
 public class PostcodeDb extends _PostcodeDb {
 	private static final String SPACE_CHARACTER = " ";
 	private static final long serialVersionUID = -4566199827275845903L;
+
+	public Long getId() {
+		return QueueableObjectUtils.getId(this);
+	}
 
 	public String getDisplayName(){
 		return getSuburb() + SPACE_CHARACTER + getPostcode();
