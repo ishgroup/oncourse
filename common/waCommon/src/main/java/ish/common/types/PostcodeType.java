a71bd39dd8fa022e3e7fff462a411faf3de4e4bf
Artyom Kravenko <artyom@ish.com.au>
2015-09-29 11:10:01 +0000

    #22533 add postcode type enum
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@40863 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/waCommon/src/main/java/ish/common/types/PostcodeType.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+package ish.common.types;
+
+import ish.common.util.DisplayableExtendedEnumeration;
+
+/**
+ * Australian postcodes range 
+ * @PublicApi
+ */
+public enum PostcodeType implements DisplayableExtendedEnumeration<Integer> {
+	
+	/**
+	 * Delivery Area
+	 * Database value: 1
+	 *
+	 * @PublicApi
+	 */
+	DELIVERY_AREA(1, "Delivery Area"),
+	
+	/**
+	 * Large Volume Receiver
+	 * Database value: 2
+	 *
+	 * @PublicApi
+	 */
+	POST_OFFICE_BOXES(2, "Post Office Boxes"),
+	
+	/**
+	 * Large Volume Receiver
+	 * Database value: 3
+	 *
+	 * @PublicApi
+	 */
+	LARGE_VOLUME_RECEIVER(3, "Large Volume Receiver");
+	
+
+	private int value;
+	private String displayName;
+
+	PostcodeType(int value, String displayName) {
+		this.value = value;
+		this.displayName = displayName;
+	}
+	
+	@Override
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	@Override
+	public Integer getDatabaseValue() {
+		return value;
+	}
+}
