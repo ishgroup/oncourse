9e0daaf618a8031835be794ac5843c160b85a4b5
Andrey Koyro <andrey.koyro@objectstyle.com>
2015-10-29 13:09:29 +0000

    #26376 enrol application allows to purchase products by corporate pass
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@41227 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-model/src/main/java/ish/oncourse/model/CorporatePassProduct.java
@@ -0,0 +1,9 @@
+package ish.oncourse.model;
+
+import ish.oncourse.model.auto._CorporatePassProduct;
+
+public class CorporatePassProduct extends _CorporatePassProduct {
+
+    private static final long serialVersionUID = 1L; 
+
+}
