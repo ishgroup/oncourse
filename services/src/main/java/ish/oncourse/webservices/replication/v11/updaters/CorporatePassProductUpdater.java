32d69fb6aaf128dc842102a961e8e18aac3f0597
Artyom Kravenko <artyom@ish.com.au>
2015-11-13 12:18:20 +0000

    #19808 Fix updater to find related Product for each Product type
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@41414 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/replication/v11/updaters/CorporatePassProductUpdater.java
+++ services/src/main/java/ish/oncourse/webservices/replication/v11/updaters/CorporatePassProductUpdater.java
@@ -3,12 +3,17 @@
  */
 package ish.oncourse.webservices.replication.v11.updaters;
 
+import ish.oncourse.model.ArticleProduct;
 import ish.oncourse.model.CorporatePass;
 import ish.oncourse.model.CorporatePassProduct;
+import ish.oncourse.model.MembershipProduct;
 import ish.oncourse.model.Product;
+import ish.oncourse.model.VoucherProduct;
 import ish.oncourse.webservices.replication.updaters.AbstractWillowUpdater;
 import ish.oncourse.webservices.replication.updaters.RelationShipCallback;
 import ish.oncourse.webservices.v11.stubs.replication.CorporatePassProductStub;
+
+import java.util.Arrays;
 
 public class CorporatePassProductUpdater extends AbstractWillowUpdater<CorporatePassProductStub, CorporatePassProduct> {
 
@@ -17,6 +22,19 @@ public class CorporatePassProductUpdater extends AbstractWillowUpdater<Corporate
 		entity.setCreated(stub.getCreated());
 		entity.setModified(stub.getModified());
 		entity.setCorporatePass(callback.updateRelationShip(stub.getCorporatePassId(), CorporatePass.class));
-		entity.setProduct(callback.updateRelationShip(stub.getProductId(), Product.class));
+		entity.setProduct(getProduct(stub.getProductId(), callback));
+	}
+	
+	private Product getProduct(Long angelId, RelationShipCallback callback) {
+		
+		for (Class aClass : Arrays.asList(Product.class, VoucherProduct.class, ArticleProduct.class, MembershipProduct.class)) {
+			try {
+				return (Product) callback.updateRelationShip(angelId, aClass);
+			} catch (IllegalArgumentException e) {
+				continue;
+			}
+		}
+		String message = String.format("Uncommitted object with angelId:%s and entityName: Product wasn't found.", angelId);
+		throw new IllegalArgumentException(message);
 	}
 }
