19203cde4fe6740279af01a31df831ad463e1356
Dzmitry Kazimirchyk <dkazimirchyk@objectstyle.com>
2015-12-08 15:56:08 +0000

    #26583. Generic URL signing/verifying logic.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@41655 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/waCommon/src/main/java/ish/util/UrlUtil.java
+++ common/waCommon/src/main/java/ish/util/UrlUtil.java
@@ -30,7 +30,8 @@ public class UrlUtil {
 	public static final String VALID_UNTIL = "valid";
 	public static final String KEY = "key";
 
-    public static final Pattern REGEXP_linkPattern = Pattern.compile("https://(.*)/portal(.*)&key=");
+    private static final Pattern PORTAL_LINK_PATTERN = Pattern.compile("https://(.*)/portal(.*)&key=");
+	private static final Pattern GENERIC_LINK_PATTERN = Pattern.compile("(.*)&key=");
 
 	/**
 	 * Creates link to portal's USI details entry page and signs it with hash.
@@ -46,6 +47,7 @@ public class UrlUtil {
 	 * @param expiry - URL expiry date
 	 * @param hashSalt - salt for hashing
 	 * @return - temporary URL to portal USI details collection page
+	 * @deprecated use {@link UrlUtil#signUrl(String, Date, String)} instead
 	 */
 	public static String createPortalUsiLink(String contactCode, Date expiry, String hashSalt) {
 		
@@ -90,10 +92,11 @@ public class UrlUtil {
 	 * @param validUntil - URL will be considered expired and therefore invalid its expiry date is
 	 *                   before this date   
 	 * @return - true if URL is valid and not expired
+	 * @deprecated use {@link UrlUtil#validateSignedUrl(String, String, Date)} instead
 	 */
 	public static boolean validatePortalUsiLink(String link, String salt, Date validUntil) {
 
-		Matcher linkMatcher = REGEXP_linkPattern.matcher(link);
+		Matcher linkMatcher = PORTAL_LINK_PATTERN.matcher(link);
 		
 		if (linkMatcher.find()) {
 			String strippedLink = linkMatcher.group(2);
@@ -127,6 +130,100 @@ public class UrlUtil {
 			}
 		}
 		
+		return false;
+	}
+
+	/**
+	 * Transforms given URL into signed URL by appending 'valid' and 'key' parameters.
+	 *
+	 * Complete URL will look like this:
+	 *
+	 * https://skillsoncourse.com.au/portal/usi/uniqueCode?valid=21140101&key=k9_S8uk68W5PoCvq5lSUp70sqQY
+	 *
+	 *
+	 * @param url - unsigned URL
+	 * @param expiry - URL expiry date
+	 * @param hashSalt - salt for hashing
+	 * @return - temporary signed URL
+	 */
+	public static String signUrl(String url, Date expiry, String hashSalt) {
+		if (StringUtils.trimToNull(url) == null) {
+			throw new IllegalArgumentException("URL cannnot be null.");
+		}
+		if (StringUtils.trimToNull(hashSalt) == null) {
+			throw new IllegalArgumentException("Hash salt cannot be null.");
+		}
+		if (expiry == null) {
+			throw new IllegalArgumentException("Expiry date cannot be null.");
+		}
+
+		DateFormat expiryDateFormat = new SimpleDateFormat(EXPIRY_DATE_FORMAT);
+
+		StringBuilder urlBuilder = new StringBuilder(url);
+
+		if (!url.contains(URL_PARAM_START)) {
+			urlBuilder.append(URL_PARAM_START);
+		} else {
+			urlBuilder.append(URL_PARAM_DELIMITER);
+		}
+		urlBuilder.append(VALID_UNTIL).append('=');
+		urlBuilder.append(expiryDateFormat.format(expiry));
+
+		String unsignedUrl = urlBuilder.toString();
+
+		String hashKey = sha1Base64(hashSalt + unsignedUrl);
+
+		urlBuilder.append(URL_PARAM_DELIMITER);
+		urlBuilder.append(KEY).append('=');
+		urlBuilder.append(hashKey);
+
+		return urlBuilder.toString();
+	}
+
+	/**
+	 * Checks validity of given signed URL.
+	 *
+	 * @param url - signed URL
+	 * @param salt - salt used to verify hash
+	 * @param validUntil - URL will be considered expired and therefore invalid its expiry date is
+	 *                   before this date
+	 * @return - true if URL is valid and not expired
+	 */
+	public static boolean validateSignedUrl(String url, String salt, Date validUntil) {
+		Matcher linkMatcher = GENERIC_LINK_PATTERN.matcher(url);
+
+		if (linkMatcher.find()) {
+			String strippedLink = linkMatcher.group(1);
+
+			Pattern expiryPattern = Pattern.compile("valid=(.*)");
+			Matcher expiryMatcher = expiryPattern.matcher(strippedLink);
+
+			if (expiryMatcher.find()) {
+				DateFormat expiryDateFormat = new SimpleDateFormat(EXPIRY_DATE_FORMAT);
+
+				String dateStr = expiryMatcher.group(1);
+
+				Date expiryDate;
+
+				try {
+					expiryDate = expiryDateFormat.parse(dateStr);
+				} catch (ParseException e) {
+					return false;
+				}
+
+				if (DateUtils.truncatedCompareTo(validUntil, expiryDate, Calendar.DAY_OF_MONTH) < 0) {
+					Pattern signPattern = Pattern.compile("&key=(.*)");
+					Matcher signMatcher = signPattern.matcher(url);
+
+					if (signMatcher.find()) {
+						String signature = signMatcher.group(1);
+
+						return signature.equals(sha1Base64(salt + strippedLink));
+					}
+				}
+			}
+		}
+
 		return false;
 	}
 	
--- common/waCommon/src/test/java/ish/util/UrlUtilTest.java
+++ common/waCommon/src/test/java/ish/util/UrlUtilTest.java
@@ -43,4 +43,46 @@ public class UrlUtilTest {
 
 		assertFalse(UrlUtil.validatePortalUsiLink(url, "saltstring", format.parse("01/01/2015")));
 	}
+
+	@Test
+	public void testSignUrl() throws Exception {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		Date expiry = format.parse("01/01/2114");
+
+		String url = UrlUtil.signUrl("https://skillsoncourse.com.au/portal/survey/1531", expiry, "saltstring");
+
+		assertTrue(url.startsWith("https://skillsoncourse.com.au/portal/survey/1531"));
+
+		assertTrue(UrlUtil.validateSignedUrl(url, "saltstring", format.parse("01/01/2015")));
+	}
+
+	@Test
+	public void testSignUrlWithParams() throws Exception{
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		Date expiry = format.parse("01/01/2115");
+
+		String url = UrlUtil.signUrl("https://skillsoncourse.com.au/portal/survey/1531?param=test", expiry, "saltstring");
+
+		assertTrue(url.startsWith("https://skillsoncourse.com.au/portal/survey/1531?param=test"));
+
+		assertTrue(UrlUtil.validateSignedUrl(url, "saltstring", format.parse("01/01/2015")));
+	}
+
+	@Test
+	public void testSignatureExpired() throws Exception {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+		String url = UrlUtil.signUrl("https://skillsoncourse.com.au/portal/survey/1531", format.parse("31/12/2014"), "saltstring");
+
+		assertFalse(UrlUtil.validateSignedUrl(url, "saltstring", format.parse("01/01/2015")));
+	}
+
+	@Test
+	public void testInvalidSignature() throws Exception {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+		String url = UrlUtil.signUrl("https://skillsoncourse.com.au/portal/survey/1531",format.parse("31/12/2014"), "wrongsalt");
+
+		assertFalse(UrlUtil.validateSignedUrl(url, "saltstring", format.parse("01/01/2015")));
+	}
 }
