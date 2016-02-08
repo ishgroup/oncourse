6674b7d64e25748c21adc4f1917ae66e259d9cf3
Artyom Kravenko <artyom@ish.com.au>
2016-02-08 07:37:13 +0000

    #27361 Adjust replication to allow link multiple invoices to enrolment. Remove unit test which is not relevant anymore, since we allow multiple invoiceLines linked to enrolment now.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42027 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/test/java/ish/oncourse/webservices/replication/updaters/v12/InvoiceLineUpdaterTest.java
+++ services/src/test/java/ish/oncourse/webservices/replication/updaters/v12/InvoiceLineUpdaterTest.java
@@ -29,41 +29,7 @@ public class InvoiceLineUpdaterTest extends ServiceTest {
     public void setupDataSet() throws Exception {
         initTest("ish.oncourse.webservices.services", StringUtils.EMPTY, ReplicationTestModule.class);
     }
-	
-	@Test
-	public void testValidInvoiceLineWithEnrolment() {
-		InvoiceLineUpdater updater = new InvoiceLineUpdater();
-		InvoiceLineStub stub = new InvoiceLineStub();
-		stub.setAngelId(1l);
-		stub.setWillowId(1l);
-		stub.setEnrolmentId(1l);
-		stub.setInvoiceId(1l);
-		
-		final ObjectContext objectContext = getService(ICayenneService.class).newContext();
-		try {
-			InvoiceLine entity = objectContext.newObject(InvoiceLine.class);
-			College college = objectContext.newObject(College.class);
-			college.setAngelVersion("4.1b3");
-			entity.setCollege(college);
-			RelationShipCallback relationShipCallback = new RelationShipCallback() {
-                @Override
-                public <M extends Queueable> M updateRelationShip(Long entityId, Class<M> clazz) {
-                    if (clazz == Invoice.class && entityId == 1l) return objectContext.newObject(clazz);
-                    if (clazz == Enrolment.class && entityId == 1l) return objectContext.newObject(clazz);
-                    return null;
-                }
-            };
-            updater.updateEntity(stub, entity, relationShipCallback);
-            assertNotNull("Invoice should not be empty", entity.getInvoice());
-            assertNull(String.format("Enrolment should not be updated for angel version %s", college.getAngelVersion()), entity.getEnrolment());
-		} catch (UpdaterException e) {
-			logger.error( e.getMessage(), e);
-            assertTrue(e.getMessage(), false);
-		} finally {
-			objectContext.rollbackChanges();
-		}
-	}
-	
+
 	@Test
 	public void testInvalidInvoiceLineWithEnrolment() {
 		InvoiceLineUpdater updater = new InvoiceLineUpdater();
