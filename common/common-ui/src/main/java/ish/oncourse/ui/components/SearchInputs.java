cd1d9e63dfdc85488f1d98c1d4366730b75964c1
Andrey Koyro <andrey.koyro@objectstyle.com>
2016-02-16 12:33:46 +0000

    #27168 The first selected search tag is added as  a path item to the result url. Example: selected tag is  "/English" -> the result path /courses/English. All empty search parameters are not added to the result url.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42105 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/components/SearchInputs.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/components/SearchInputs.java
@@ -1,10 +1,11 @@
 package ish.oncourse.ui.components;
 
 import ish.oncourse.components.ISHCommon;
+import ish.oncourse.model.SearchParam;
 import ish.oncourse.model.Tag;
 import ish.oncourse.services.tag.ITagService;
 import ish.oncourse.util.URLUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -14,21 +15,26 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 
+import static ish.oncourse.model.SearchParam.near;
+import static ish.oncourse.model.SearchParam.s;
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.isNoneBlank;
+
 public class SearchInputs extends ISHCommon {
 
-    private static final String URL_PATH_SEARCH_PATTERN = "/courses?s=%s" +
-            "%s" +
-            "&near=%s" +
+	private static final String URL_PATH_SEARCH_PATTERN = "/courses?s=%s" +
+			"%s" +
+			"&near=%s" +
 			"&km=%s" +
-            "&price=%s" +
-            "&time=%s" +
-            "&day=%s";
+			"&price=%s" +
+			"&time=%s" +
+			"&day=%s";
 	private static final String SEARCH_TAG_NAMES_SEPARATOR = ";";
 	@Inject
 	private ITagService tagService;
-	
+
 	@Inject
-    @Property
+	@Property
 	private Request request;
 
 	@Property
@@ -80,6 +86,7 @@ public class SearchInputs extends ISHCommon {
 
 	/**
 	 * Methods added for old template backward compatibility
+	 *
 	 * @return Subjects sub tag names.
 	 */
 	@Deprecated
@@ -89,6 +96,7 @@ public class SearchInputs extends ISHCommon {
 
 	/**
 	 * Methods added for old template backward compatibility
+	 *
 	 * @return Subjects selected sub tag name.
 	 */
 	@Deprecated
@@ -98,6 +106,7 @@ public class SearchInputs extends ISHCommon {
 
 	/**
 	 * Methods added for old template backward compatibility
+	 *
 	 * @param value - Subjects selected sub tag name
 	 */
 	@Deprecated
@@ -111,6 +120,7 @@ public class SearchInputs extends ISHCommon {
 	/**
 	 * Getter to receive the default mapped value for match of browseTag.
 	 * Used to access values from map by dynamic tagGroup on component render.
+	 *
 	 * @return default mapped value
 	 */
 	public String getSelectedTagValue() {
@@ -119,6 +129,7 @@ public class SearchInputs extends ISHCommon {
 
 	/**
 	 * Setter for write values in map when submit pressed.
+	 *
 	 * @param value - drop down selection values.
 	 */
 	public void setSelectedTagValue(String value) {
@@ -130,6 +141,7 @@ public class SearchInputs extends ISHCommon {
 
 	/**
 	 * Init tag groups, tag map, tag model and selected tag map by searchTagNames parameter.
+	 *
 	 * @param checkBrowseTag - describe the root search elements.
 	 */
 	private void fillTagGroups(boolean checkBrowseTag) {
@@ -173,45 +185,88 @@ public class SearchInputs extends ISHCommon {
 	 * We need to use onPrepare() tapestry lifecycle method to be sure that tagGroups will be intitalize for
 	 * any request including form submit.
 	 */
-	void onPrepare()
-	{
+	void onPrepare() {
 		fillTagGroups(true);
 	}
 
 	URL onActionFromSearch2() {
-		StringBuilder tags = new StringBuilder();
-		for (String tagName : tagGroups) {
-			String value = selectedTagMap.get(tagName);
-			for (Tag tag : tagGroupMap.get(tagName)) {
-				if (StringUtils.trimToNull(value) != null) {
-					if (value.equals(tag.getName())) {
-						//add selected tag if something selected
-						tags.append("&tag=").append(tag.getDefaultPath());
-						break;
-					}
-				}
-				//this code commented because in old implementation (before #20305) when we select all option, we skip tag filtering
-				//but with new implementation this option may be required
-				/* else {
-					//add tag group if selected all
-					tags.append("&tag=").append(tagName);
-					break;
-				}*/
-			}
-		}
 		try {
-            String path = String.format(URL_PATH_SEARCH_PATTERN,
-                    (advKeyword == null ? StringUtils.EMPTY : advKeyword),
-                    (tags.length() > 0 ? tags.toString() : StringUtils.EMPTY),
-                    (searchNear == null ? StringUtils.EMPTY : searchNear),
-					(km == null ? StringUtils.EMPTY : km),
-                    (searchPrice == null ? StringUtils.EMPTY : searchPrice),
-                    (daytime ? "daytime" : (evening ? "evening" : StringUtils.EMPTY)),
-                    (weekday ? "weekday" : (weekend ? "weekend" : StringUtils.EMPTY)));
-			return URLUtils.buildURL(request, path, false);
+			return BuildURL.valueOf(this).build();
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+
+	public static class BuildURL {
+		private Request request;
+
+		private String browseTag;
+		private List<String> tags;
+		private Map<SearchParam, String> searchParams;
+
+		private List<String> queries = new ArrayList<>();
+
+		public URL build() throws MalformedURLException {
+			for (SearchParam key : SearchParam.values()) {
+				if (isNoneBlank(searchParams.get(key))) {
+					queries.add(format("%s=%s", key.name(), searchParams.get(key)));
+				}
+			}
+
+			for (String tag : tags) {
+				queries.add(format("%s=%s", SearchParam.tag.name(), tag));
+			}
+
+			String path = "/courses";
+			if (browseTag != null) {
+				path += browseTag;
+			}
+			if (queries.size() > 0) {
+				path += "?" + StringUtils.join(queries, "&");
+			}
+			return URLUtils.buildURL(request, path, false);
+		}
+
+		private void initSearchParams(SearchInputs form) {
+			searchParams = new HashMap<>();
+			searchParams.put(s, form.advKeyword);
+			searchParams.put(near, form.searchNear);
+			searchParams.put(SearchParam.km, form.km);
+			searchParams.put(SearchParam.price, form.searchPrice);
+			searchParams.put(SearchParam.time, (form.daytime ? "daytime" : (form.evening ? "evening" : StringUtils.EMPTY)));
+			searchParams.put(SearchParam.day, (form.weekday ? "weekday" : (form.weekend ? "weekend" : StringUtils.EMPTY)));
+		}
+
+		private void initTags(SearchInputs form) {
+			this.tags = new ArrayList<>();
+			for (String tagName : form.tagGroups) {
+				String value = form.selectedTagMap.get(tagName);
+				for (Tag tag : form.tagGroupMap.get(tagName)) {
+					if (StringUtils.trimToNull(value) != null) {
+						if (value.equals(tag.getName())) {
+							//add selected tag if something selected
+							if (browseTag == null) {
+								browseTag = tag.getDefaultPath();
+							} else {
+								tags.add(tag.getDefaultPath());
+							}
+							break;
+						}
+					}
+				}
+			}
+		}
+
+
+		public static BuildURL valueOf(SearchInputs form) {
+			BuildURL result = new BuildURL();
+			result.initSearchParams(form);
+			result.initTags(form);
+			result.request = form.request;
+			return result;
+		}
+
 	}
 }
