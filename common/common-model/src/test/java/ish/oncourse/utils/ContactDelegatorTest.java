29dae152ce963ada57c93bc0604ee24d95549f30
Sasha Shestak <ashestak@objectstyle.com>
2016-04-13 07:25:00 +0000

    #28173 exclude duplicated tests, add test to ContactDelegator
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42674 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-model/src/test/java/ish/oncourse/utils/ContactDelegatorTest.java
@@ -0,0 +1,44 @@
+package ish.oncourse.utils;
+
+import ish.oncourse.model.Contact;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ContactDelegatorTest {
+
+    @Test
+    public void testContactDelegator() throws Exception {
+        Contact contact = mock(Contact.class);
+        when(contact.getGivenName()).thenReturn("givenName");
+        when(contact.getFamilyName()).thenReturn("familyName");
+        when(contact.getMobilePhoneNumber()).thenReturn("mobilePhone");
+        when(contact.getPostcode()).thenReturn("postCode");
+        when(contact.getStreet()).thenReturn("street");
+        Date currentDate = new Date();
+        when(contact.getDateOfBirth()).thenReturn(currentDate);
+        when(contact.getIsCompany()).thenReturn(true);
+        when(contact.getState()).thenReturn("state");
+        when(contact.getHomePhoneNumber()).thenReturn("homePhone");
+        when(contact.getFaxNumber()).thenReturn("fax");
+        when(contact.getEmailAddress()).thenReturn("test@com.au");
+
+        ContactDelegator contactDelegator = ContactDelegator.valueOf(contact);
+
+        assertEquals(contact.getGivenName(), contactDelegator.getFirstName());
+        assertEquals(contact.getFamilyName(), contactDelegator.getLastName());
+        assertEquals(contact.getMobilePhoneNumber(), contactDelegator.getMobilePhone());
+        assertEquals(contact.getPostcode(), contactDelegator.getPostcode());
+        assertEquals(contact.getStreet(), contactDelegator.getStreet());
+        assertEquals(contact.getDateOfBirth(), contactDelegator.getBirthDate());
+        assertEquals(contact.getIsCompany(), contactDelegator.getIsCompany());
+        assertEquals(contact.getState(), contactDelegator.getState());
+        assertEquals(contact.getHomePhoneNumber(), contactDelegator.getHomePhone());
+        assertEquals(contact.getFaxNumber(), contactDelegator.getFax());
+        assertEquals(contact.getEmailAddress(), contactDelegator.getEmail());
+    }
+}
