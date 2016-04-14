efdc9542777cc7155a3abb7d2992168c2c57e34b
Sasha Shestak <ashestak@objectstyle.com>
2016-04-14 07:57:11 +0000

    #28173 refactoring. add test
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42687 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/FillValidationResult.java
+++ common/common-model/src/main/java/ish/oncourse/model/FillValidationResult.java
@@ -5,52 +5,55 @@ import org.apache.cayenne.validation.ValidationResult;
 
 import java.util.Map;
 
-public class ContactFailureWriter {
+public class FillValidationResult {
 
     public static final String INVALID_EMAIL_MESSAGE = "The email address does not appear to be valid.";
     public static final String LENGTH_FAILURE_MESSAGE = "Field %s exceeds maximum allowed length";
 
     private ValidationResult result;
     private Map<String, ContactErrorCode> errorCodeMap;
-    private Object source;
+    private Contact contact;
 
 
-    private ContactFailureWriter() {
+    private FillValidationResult() {
     }
 
-    public static ContactFailureWriter valueOf(ValidationResult result, Map<String, ContactErrorCode> errorCodeMap, Object source) {
-        ContactFailureWriter validationFailureWriter = new ContactFailureWriter();
+    public static FillValidationResult valueOf(ValidationResult result, Map<String, ContactErrorCode> errorCodeMap, Contact contact) {
+        FillValidationResult validationFailureWriter = new FillValidationResult();
         validationFailureWriter.result = result;
         validationFailureWriter.errorCodeMap = errorCodeMap;
-        validationFailureWriter.source = source;
+        validationFailureWriter.contact = contact;
 
         return validationFailureWriter;
     }
 
-    public void write() {
-        for (Map.Entry<String, ContactErrorCode> errorCodeEntry : errorCodeMap.entrySet()) {
-            switch(errorCodeEntry.getValue()) {
+    public ValidationResult fill() {
+        for (Map.Entry<String, ContactErrorCode> entry : errorCodeMap.entrySet()) {
+            switch(entry.getValue()) {
                 case birthDateCanNotBeInFuture:
-                    result.addFailure(ValidationFailure.validationFailure(source, errorCodeEntry.getKey(),
+                    result.addFailure(ValidationFailure.validationFailure(contact, entry.getKey(),
                             "The birth date cannot be in the future."));
                     break;
                 case firstNameNeedToBeProvided:
-                    result.addFailure(ValidationFailure.validationFailure(source, errorCodeEntry.getKey(),
+                    result.addFailure(ValidationFailure.validationFailure(contact, entry.getKey(),
                             "First name is required."));
                     break;
                 case lastNameNeedToBeProvided:
-                    result.addFailure(ValidationFailure.validationFailure(source,  errorCodeEntry.getKey(),
+                    result.addFailure(ValidationFailure.validationFailure(contact,  entry.getKey(),
                             "Last name is required."));
                     break;
                 case incorrectEmailFormat:
-                    result.addFailure(ValidationFailure.validationFailure(source,  errorCodeEntry.getKey(),
+                    result.addFailure(ValidationFailure.validationFailure(contact,  entry.getKey(),
                             INVALID_EMAIL_MESSAGE));
                     break;
                 case incorrectPropertyLength:
-                    result.addFailure(ValidationFailure.validationFailure(source, errorCodeEntry.getKey(),
-                            String.format(LENGTH_FAILURE_MESSAGE, errorCodeEntry.getKey())));
+                    result.addFailure(ValidationFailure.validationFailure(contact, entry.getKey(),
+                            String.format(LENGTH_FAILURE_MESSAGE, entry.getKey())));
                     break;
+                default:
+                    throw new IllegalArgumentException();
             }
         }
+        return result;
     }
 }
