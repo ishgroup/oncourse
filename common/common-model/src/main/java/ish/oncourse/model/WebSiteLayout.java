6496fc88d8b32a96b442434e8dc6815a05d0ea58
Andrey Koyro <andrey.koyro@objectstyle.com>
2016-04-21 14:31:09 +0000

    #28114 WebSiteLayout got the getId() method
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42752 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/WebSiteLayout.java
+++ common/common-model/src/main/java/ish/oncourse/model/WebSiteLayout.java
@@ -1,17 +1,22 @@
 package ish.oncourse.model;
 
 import ish.oncourse.model.auto._WebSiteLayout;
+import ish.oncourse.utils.QueueableObjectUtils;
 import ish.oncourse.utils.ResourceNameValidator;
 import org.apache.cayenne.validation.ValidationResult;
 
 public class WebSiteLayout extends _WebSiteLayout {
 
-    @Override
+    public Long getId() {
+        return QueueableObjectUtils.getId(this);
+    }
+
+
     protected void validateForSave(ValidationResult validationResult) {
         super.validateForSave(validationResult);
         String error = new ResourceNameValidator().validate(getLayoutKey());
-        if (error != null)
-            validationResult.addFailure(ValidationFailure.validationFailure(this, WebSiteLayout.LAYOUT_KEY_PROPERTY, error));
-
+        if (error != null) {
+            validationResult.addFailure(ValidationFailure.validationFailure(this, WebSiteLayout.LAYOUT_KEY.getName(), error));
+        }
     }
 }
