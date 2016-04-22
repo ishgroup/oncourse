d87ef9342fee37bfbb50e6e58f5a041bd82704a7
Andrey Koyro <andrey.koyro@objectstyle.com>
2016-04-22 11:36:56 +0000

    #00000 try/catch block was introduced to log sessionId when we get some exception
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42769 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/util/payment/PaymentInModelFromSessionIdBuilder.java
+++ common/common-services/src/main/java/ish/oncourse/util/payment/PaymentInModelFromSessionIdBuilder.java
@@ -7,13 +7,18 @@ import ish.oncourse.model.PaymentIn;
 import ish.oncourse.utils.PaymentInUtil;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.query.ObjectSelect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+
+import static java.lang.String.format;
 
 /**
  * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
  */
 public class PaymentInModelFromSessionIdBuilder {
+	private static final Logger logger = LogManager.getLogger();
 
     private ObjectContext context;
     private String sessionId;
@@ -22,21 +27,26 @@ public class PaymentInModelFromSessionIdBuilder {
 
     public PaymentInModelFromSessionIdBuilder build() {
 
-        PaymentIn paymentIn = ObjectSelect.query(PaymentIn.class).where(PaymentIn.SESSION_ID.eq(sessionId)).and(PaymentIn.STATUS.in(PaymentStatus.IN_TRANSACTION, PaymentStatus.NEW, PaymentStatus.CARD_DETAILS_REQUIRED)).selectOne(context);
-        model.setPaymentIn(paymentIn);
-        List<Invoice> invoices = ObjectSelect.query(Invoice.class).where(Invoice.SESSION_ID.eq(sessionId)).select(context);
-        for (Invoice invoice : invoices) {
-            model.getInvoices().add(invoice);
-            for (InvoiceLine invoiceLine : invoice.getInvoiceLines()) {
-                model.getEnrolments().add(invoiceLine.getEnrolment());
-            }
-        }
-		
-		if (model.getPaymentIn() != null) {
-			initVoucherPayments();
+		try {
+			PaymentIn paymentIn = ObjectSelect.query(PaymentIn.class).where(PaymentIn.SESSION_ID.eq(sessionId)).and(PaymentIn.STATUS.in(PaymentStatus.IN_TRANSACTION, PaymentStatus.NEW, PaymentStatus.CARD_DETAILS_REQUIRED)).selectOne(context);
+			model.setPaymentIn(paymentIn);
+			List<Invoice> invoices = ObjectSelect.query(Invoice.class).where(Invoice.SESSION_ID.eq(sessionId)).select(context);
+			for (Invoice invoice : invoices) {
+				model.getInvoices().add(invoice);
+				for (InvoiceLine invoiceLine : invoice.getInvoiceLines()) {
+					model.getEnrolments().add(invoiceLine.getEnrolment());
+				}
+			}
+
+			if (model.getPaymentIn() != null) {
+				initVoucherPayments();
+			}
+		} catch (Exception e) {
+			String message = format("Cannot create PaymentInModel for sessionId: %s", sessionId);
+			logger.error(message, e);
+			throw new IllegalStateException(message, e);
 		}
-		
-        return this;
+		return this;
     }
 
 	private void initVoucherPayments() {
