9c03800a125babde1ce638d1e7f240b4ca280c15
Sasha Shestak <ashestak@objectstyle.com>
2016-05-05 12:34:22 +0000

    #28173 add tests to EnrolContactValidator inner classses
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@42845 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ enrol/src/test/java/ish/oncourse/enrol/utils/PhoneValidatorTest.java
@@ -0,0 +1,43 @@
+package ish.oncourse.enrol.utils;
+
+import ish.oncourse.enrol.utils.EnrolContactValidator.PhoneValidator;
+import ish.validation.ContactErrorCode;
+import org.apache.tapestry5.ioc.Messages;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class PhoneValidatorTest {
+
+    private static final String INCORRECT_PHONE_FORMAT_MESSAGE = "Enter 10 digit %s phone number including area code for Australian numbers";
+
+    private Messages messages;
+
+    @Before
+    public void setUp() throws Exception {
+        messages = Mockito.mock(Messages.class);
+    }
+
+    @Test
+    public void testIncorrectLength() throws Exception {
+        PhoneValidator phoneValidator = PhoneValidator.homePhoneValidator("111112222333344441", false, ContactErrorCode.incorrectPropertyLength, messages).validate();
+        assertNull(phoneValidator.getMessage());
+    }
+
+    @Test
+    public void testIncorrectFormat() throws Exception {
+        PhoneValidator phoneValidator = PhoneValidator.homePhoneValidator("12345678901", true, null, messages).validate();
+        assertEquals(String.format(INCORRECT_PHONE_FORMAT_MESSAGE, "home phone"), phoneValidator.getMessage());
+    }
+
+    @Test
+    public void testCorrectFormat() throws Exception {
+        PhoneValidator phoneValidator = PhoneValidator.homePhoneValidator("1234 5678 90", true, null, messages).validate();
+        assertNotNull(phoneValidator.getMessage());
+        assertEquals("1234567890", phoneValidator.getValue());
+    }
+}
