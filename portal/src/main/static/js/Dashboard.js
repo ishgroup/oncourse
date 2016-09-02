4e4952c0736fc205cd48ed65be7c423df26fde27
Artyom Kravenko <artyom@ish.com.au>
2016-09-02 09:18:28 +0000

    #28792  Adjust dashboard style: use extended sass style applicable to certain (dashboard) children
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@44254 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/static/js/Dashboard.js
+++ portal/src/main/static/js/Dashboard.js
@@ -8,9 +8,7 @@ var $j = jQuery.noConflict();
 $j(document).ready(function () {
 
     if ($j('#dashboard').length) {
-        $j('.main-container').removeClass('col-lg-9 col-sm-8');
-        $j('.main-container').addClass('col-lg-12 col-sm-12');
-        $j('#wrap').css('background-color', '#F5F0DF')
+        $j('#wrap').addClass('dashboard')
         $j('div#dashboard').masonry({
             itemSelector: 'div.dashboard-card',
             columnWidth: 10
