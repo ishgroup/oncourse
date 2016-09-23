b55a6add9828b9886a438d4858083020ad10abfa
Sasha Shestak <ashestak@objectstyle.com>
2016-09-23 09:55:49 +0000

    #29454 add gradle task which combine svg files in one with name: ${project.name}.svg, adjust svg files structure, change templates and css files to use one common svg icon
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@44449 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ buildSrc/src/main/groovy/CleanCombineSvg.groovy
@@ -0,0 +1,15 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+class CleanCombineSvg extends DefaultTask{
+
+    @Input
+    def String imgDirectory
+
+    @TaskAction
+    def void cleanCombineSvg() {
+        def imgDirectory = new File(imgDirectory)
+        imgDirectory.deleteDir()
+    }
+}
---
+++ buildSrc/src/main/groovy/CombineSvg.groovy
@@ -0,0 +1,63 @@
+import groovy.xml.StreamingMarkupBuilder
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+public class CombineSvg extends DefaultTask {
+
+    @Input
+    def String projectName
+
+    @Input
+    def String outputDirectoryName
+
+    @Input
+    def String imgDirectoryName
+
+    @TaskAction
+    def void combineSvg() {
+        def imgDirectory = new File(imgDirectoryName)
+        if (imgDirectory.exists()) {
+            def xmlParser = new XmlParser()
+            def xml = new StreamingMarkupBuilder().with {
+                it.encoding = 'UTF-8'
+                it
+            }.bind {
+                mkp.xmlDeclaration()
+                svg(xmlns: 'http://www.w3.org/2000/svg', version: '1.2', baseProfile: 'tiny', x: '0px', y: '0px', width: '128px', height: '128px', viewBox: '0 0 128 128',
+                        'xml:space': 'preserve', 'xmlns:xml': 'http://www.w3.org/XML/1998/namespace') {
+                    defs(xmlns: "http://www.w3.org/2000/svg") {
+                        style {
+                            mkp.yieldUnescaped("<![CDATA[.sprite { display: none; }.sprite:target { display: inline; }]]>")
+                        }
+                    }
+                }
+            }
+            def result = xmlParser.parseText(xml.toString())
+
+            imgDirectory.eachFile {
+                if (it.name.endsWith('.svg')) {
+                    def node = xmlParser.parse(it)
+                    def imageNode = node.children().get(0)
+                    imageNode.@class = 'sprite'
+                    result.append(imageNode)
+                }
+            }
+
+            new File(outputDirectoryName).mkdir()
+            new File(outputDirectoryName + '/img').mkdir()
+            def outputFileName = outputDirectoryName + "/img/" + projectName + ".svg"
+            def outputFile = new File(outputFileName)
+            outputFile.createNewFile()
+
+            def out = new PrintWriter(outputFile, 'UTF-8')
+            out.write('<?xml version="1.0" encoding="utf-8"?>\n')
+            new XmlNodePrinter(out).with {
+                preserveWhitespace = true
+                expandEmptyElements = true
+                quote = '\''
+                it
+            }.print(result)
+        }
+    }
+}
