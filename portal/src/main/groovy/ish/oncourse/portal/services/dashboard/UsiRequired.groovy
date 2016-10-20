51929a4dd9034d6be76f20c293a63cb77403ff71
Artyom Kravenko <artyom@ish.com.au>
2016-10-20 10:22:03 +0000

    #30285 Check for success enrolments only (do not show USI required if enrolment failed)
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@44596 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/groovy/ish/oncourse/portal/services/dashboard/UsiRequired.groovy
+++ portal/src/main/groovy/ish/oncourse/portal/services/dashboard/UsiRequired.groovy
@@ -1,6 +1,7 @@
 package ish.oncourse.portal.services.dashboard
 
 import groovy.time.TimeCategory
+import ish.common.types.EnrolmentStatus
 import ish.oncourse.model.Contact
 
 class UsiRequired {
@@ -14,7 +15,8 @@ class UsiRequired {
 		if (contact.student && !contact.student.usi) {
 				def now = new Date()
 				return !contact.student.enrolments
-						.findAll {e ->e.courseClass.course.isVETCourse &&
+						.findAll {e ->e.courseClass.course.isVETCourse && 
+								EnrolmentStatus.SUCCESS.equals(e.status) &&
 								use(TimeCategory) {(e.courseClass.endDate ? (e.courseClass.endDate + 1.year).after(now) : (e.createdOn + 1.year).after(now))}
 								}.empty
 		} else {
