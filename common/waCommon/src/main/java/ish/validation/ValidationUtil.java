874942be055dedc0b2341733b774e05471c95c77
Andrey Narut <anarut@ish.com.au>
2016-10-25 10:39:12 +0000

    #30390. Email validation was refactored.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@44618 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/waCommon/src/main/java/ish/validation/ValidationUtil.java
+++ common/waCommon/src/main/java/ish/validation/ValidationUtil.java
@@ -5,6 +5,8 @@
 
 package ish.validation;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
@@ -14,27 +16,25 @@ public class ValidationUtil {
 	 * Validate the form of an email address.
 	 */
 	public static boolean isValidEmailAddress(String anEmailAddress) {
-		if (anEmailAddress == null) {
+		if (StringUtils.isBlank(anEmailAddress)) {
 			return false;
 		}
 
-		boolean result = true;
-
 		try {
-			new InternetAddress(anEmailAddress);
+			InternetAddress internetAddress = new InternetAddress(anEmailAddress);
+			internetAddress.validate();
 		} catch (AddressException ex) {
-			result = false;
+			return false;
 		}
 
+		if (anEmailAddress.contains(",")) {
+			return false;
+		}
+		
 		// since the above does not throw an exception for local email addresses
 		// of the form "username" we need to test for them separately.
 		String[] tokens = anEmailAddress.split("@");
 
-		if (tokens.length != 2 || tokens[0].length() == 0 || tokens[1].length() == 0) {
-			result = false;
-		}
-
-		return result;
+		return !(tokens.length != 2 || tokens[0].length() == 0 || tokens[1].length() == 0);
 	}
-
 }
