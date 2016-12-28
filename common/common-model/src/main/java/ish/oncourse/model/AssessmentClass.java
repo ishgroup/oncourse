c0851df5a608075e8250aee7be5454a804be2588
Andrey Narut <anarut@ish.com.au>
2016-12-28 15:03:38 +0000

    OD-10003 Update waCommon version on willow.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@45152 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-model/src/main/java/ish/oncourse/model/AssessmentClass.java
+++ common/common-model/src/main/java/ish/oncourse/model/AssessmentClass.java
@@ -1,9 +1,10 @@
 package ish.oncourse.model;
 
+import ish.oncourse.cayenne.AssessmentClassInterface;
 import ish.oncourse.model.auto._AssessmentClass;
 import ish.oncourse.utils.QueueableObjectUtils;
 
-public class AssessmentClass extends _AssessmentClass implements Queueable {
+public class AssessmentClass extends _AssessmentClass implements Queueable, AssessmentClassInterface {
 
     private static final long serialVersionUID = 1L;
 
--- common/common-model/src/main/java/ish/oncourse/model/AssessmentClassModule.java
+++ common/common-model/src/main/java/ish/oncourse/model/AssessmentClassModule.java
@@ -1,9 +1,10 @@
 package ish.oncourse.model;
 
+import ish.oncourse.cayenne.AssessmentClassModuleInterface;
 import ish.oncourse.model.auto._AssessmentClassModule;
 import ish.oncourse.utils.QueueableObjectUtils;
 
-public class AssessmentClassModule extends _AssessmentClassModule implements Queueable {
+public class AssessmentClassModule extends _AssessmentClassModule implements Queueable, AssessmentClassModuleInterface {
 
     private static final long serialVersionUID = 1L;
 
