e412abd85721601ac4f52da0a27dd48ba81e0920
Aristedes Maniatis <ari@ish.com.au>
2017-01-17 05:10:48 +0000

    Arrange things a bit more clearly
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@45401 fb2e276d-fae9-0310-b578-b71ac85840b9

--- web-design/js/base.js
+++ web-design/js/base.js
@@ -1,8 +1,19 @@
-//= require vendor/jquery-3.1.1.js
-//= require vendor/jquery.nyroModal-2.0.0.min.js
-//= require jquery.utils.js
-//= require vendor/jQuery-validation/jquery.validate.min.js
-//= require vendor/jQuery-validation/additional-methods.min.js
+/*
+    These files are due to be removed
+*/
+
+//= require deprecated/jquery-3.1.1.js
+//= require deprecated/jquery.nyroModal-2.0.0.min.js
+//= require deprecated/jquery.utils.js
+//= require deprecated/jquery.validate.min.js
+//= require deprecated/additional-methods.min.js
+
+
+/*
+    These are part of the onCourse application
+	Don't remove them or the application may break
+*/
+
 //= require application.js
 //= require gmap.js
 //= require search.js
