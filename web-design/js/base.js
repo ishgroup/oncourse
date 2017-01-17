ea9e6c2ce7484874863972e07b78502725c9353e
Aristedes Maniatis <ari@ish.com.au>
2017-01-17 05:18:35 +0000

    Add the dynamic library from our Jenkins react build
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@45402 fb2e276d-fae9-0310-b578-b71ac85840b9

--- web-design/js/base.js
+++ web-design/js/base.js
@@ -1,12 +1,30 @@
 /*
+    A dynamic library which contains pre-packaged
+    functionality for driving dyanamic parts of the website.
+    It includes:
+
+	classnames 2.2.5
+	react 0.14.8
+	react-dom 0.14.8
+	react-redux 5.0.1
+	redux 3.6.0
+	redux-thunk 2.1.0
+	jquery 3.1.1
+*/
+
+//= require vendor/dynamic.js
+
+
+
+/*
     These files are due to be removed
 */
 
-//= require deprecated/jquery-3.1.1.js
 //= require deprecated/jquery.nyroModal-2.0.0.min.js
 //= require deprecated/jquery.utils.js
 //= require deprecated/jquery.validate.min.js
 //= require deprecated/additional-methods.min.js
+
 
 
 /*
