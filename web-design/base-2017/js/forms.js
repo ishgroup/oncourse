1ea6c260e495f8d574232b8bb36f23ee748bdc07
Chintan Kotadia <chintan@ish.com.au>
2017-02-03 06:50:55 +0000

    [#OW-1589] Updated form styles and sample content added of theme-a, [#OW-1590] Removed common files from templates-b
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@45573 fb2e276d-fae9-0310-b578-b71ac85840b9

--- web-design/base-2017/js/forms.js
+++ web-design/base-2017/js/forms.js
@@ -1,23 +1,23 @@
 
 function initTextileFormHandle(){
-	$(document).on("click", "button.form-submit-button", function() {
-		var form = $(this).parent("form")[0];
+	$j(document).on("click", "button.form-submit-button", function() {
+		var form = $j(this).parent("form")[0];
 		var formId = form.id;
 		//get redirect url from data-url attr
-		var url = $(form).data("url");
+		var url = $j(form).data("url");
 
 		//next line responsible for formName send
 		var formName = form.name;
 
 		//prepare data for ajax request. Method serialize() should be run before valid() otherwise it doesn't work on IE
-		var dataString = $(form).serialize() + "&formName=" + formName + "&pagePath="+window.location;
+		var dataString = $j(form).serialize() + "&formName=" + formName + "&pagePath="+window.location;
 
-		if($(form).valid()) {
+		if($j(form).valid()) {
 			//get sendMail html block to show waiting message for user.
-			var sendingHtml = $('#sendingMail').html();
-			$('#emailFormBlock_'+formId).html(sendingHtml);
+			var sendingHtml = $j('#sendingMail').html();
+			$j('#emailFormBlock_'+formId).html(sendingHtml);
 
-			$.ajax({
+			$j.ajax({
 				type: "POST",
 				data: dataString,
 				url: '/textile/textileform.send',
@@ -25,13 +25,13 @@ function initTextileFormHandle(){
 					if(url) {
 						window.location=url;
 					} else {
-						$('#emailFormBlock_'+formId).html(data.responseText);
+						$j('#emailFormBlock_'+formId).html(data.responseText);
 					}
 				}
 			});
 
 		} else {
-			$('#errorMessage_'+formId).html("Please, fill all the required fields");
+			$j('#errorMessage_'+formId).html("Please, fill all the required fields");
 		}
 		return false;
 	});
