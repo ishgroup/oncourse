f89aaf5d1cb404561675b91d6d15fc4db827b2be
Chintan Kotadia <chintan@ish.com.au>
2017-03-07 05:25:17 +0000

    [#OW-1616] Styling updated of template-c
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@45897 fb2e276d-fae9-0310-b578-b71ac85840b9

--- web-design/base-2017/js/main.js
+++ web-design/base-2017/js/main.js
@@ -1,2 +1,34 @@
 
 var $j = jQuery.noConflict();
+
+(function($) {
+  $.app = {
+    getDimention: function(oWidth, oHeight, nWidth) {
+      return {
+        height: (oHeight / oWidth) * nWidth,
+        width: nWidth
+      };
+    },
+    getImageDimension: function(el, onReady) {
+      var src = typeof el.attr === 'function' ? el.attr('src') : el.src !== undefined ? el.src : el;
+
+      var image = new Image();
+      image.onload = function(){
+        if(typeof(onReady) == 'function') {
+          var oWidth = image.width, oHeight = image.height;
+          //var nWidth = $(window).width(), nHeight = (oHeight / oWidth) * nWidth;
+          var nDim = $.app.getDimention(oWidth, oHeight, $(window).width());
+
+          onReady({
+            src: src,
+            width: image.width,
+            height: image.height,
+            nWidth: nDim.width,
+            nHeight: nDim.height
+          });
+        }
+      };
+      image.src = src;
+    }
+  }
+})(jQuery);
