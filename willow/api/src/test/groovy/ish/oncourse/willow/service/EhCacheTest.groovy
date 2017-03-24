4722575e2a1b55bfe052c69bd5442fa3cf065e46
Artyom Kravenko <artyom@ish.com.au>
2017-03-24 10:05:05 +0000

    OD-10304 Add test for ehcache configuration
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46112 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ willow/api/src/test/groovy/ish/oncourse/willow/service/EhCacheTest.groovy
@@ -0,0 +1,32 @@
+package ish.oncourse.willow.service
+
+import ish.oncourse.willow.cache.JCacheModule
+import ish.oncourse.willow.cache.JCacheQueryCache
+import org.apache.cayenne.cache.QueryCache
+import org.apache.cayenne.configuration.server.ServerRuntime
+import org.ehcache.jsr107.Eh107CacheManager
+import org.junit.Test
+
+import javax.cache.CacheManager
+import javax.naming.Context
+import javax.naming.InitialContext
+
+import static org.junit.Assert.assertTrue
+import static org.junit.Assert.assertEquals
+
+
+class EhCacheTest {
+    
+    @Test 
+    void ehCacheTest() {
+
+        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, InitialContextFactoryMock.class.getName())
+        InitialContextFactoryMock.bind("java:comp/env", new InitialContext())
+        ServerRuntime cayenneRuntime = new ServerRuntime("cayenne-oncourse.xml", new JCacheModule())
+
+        QueryCache  cache = cayenneRuntime.injector.getInstance(QueryCache)
+        assertTrue(cache instanceof JCacheQueryCache)
+        CacheManager cacheManager = ((JCacheQueryCache)cache).cacheManager
+        assertEquals(cacheManager.configurationMerger.xmlConfiguration.cacheConfigurations.size(), 4)
+    }
+}
