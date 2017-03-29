f02364ce46d1c3b7321d4a812cac75b920e5b84c
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-03-29 04:51:33 +0000

    OD-10382 reactjs karma.conf.js config was adjusted to fix tests: API_ROOT and APP_VERSION variables have been added
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46176 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/karma.conf.js
+++ react/karma.conf.js
@@ -3,79 +3,84 @@ const webpack = require('webpack');
 const fs = require('fs');
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-    files: [
-      'spec.js'
-    ],
-    webpack: {
-      // Shared with webpack.config.js
-      // Can be moved to common module...
-      externals: [function (context, request, callback) {
-        const p = path.resolve(context, request) + '.js';
 
-        if (/.custom.js$/.test(p)) {
-          fs.stat(p, (err) => {
-            if (err) {
-              callback(null, "{}");
-              return;
+    const API_ROOT = 'http://localhost:10080';
+    const APP_VERSION = 'DEV';
+
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'spec.js'
+        ],
+        webpack: {
+            // Shared with webpack.config.js
+            // Can be moved to common module...
+            externals: [function (context, request, callback) {
+                const p = path.resolve(context, request) + '.js';
+
+                if (/.custom.js$/.test(p)) {
+                    fs.stat(p, (err) => {
+                        if (err) {
+                            callback(null, "{}");
+                            return;
+                        }
+
+                        callback();
+                    });
+                    return;
+                }
+
+                callback();
+            }],
+            resolve: {
+                extensions: ['.ts', '.tsx', '.js']
+            },
+            module: {
+                rules: [{
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                }, {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['es2015', {loose: true}], 'react'],
+                        plugins: ['transform-object-rest-spread']
+                    },
+                    exclude: /node_modules/
+                }]
+            },
+            devtool: 'inline-source-map',
+            plugins: [
+                new webpack.DefinePlugin({
+                    'process.env': {
+                        'NODE_ENV': JSON.stringify("DEVELOPMENT")
+                    },
+                    _API_ROOT: JSON.stringify(API_ROOT),
+                    _APP_VERSION: JSON.stringify(APP_VERSION)
+                }),
+                new webpack.ProvidePlugin({
+                    'React': 'react',
+                    '$': 'jquery'
+                })
+            ]
+        },
+        webpackMiddleware: {
+            stats: {
+                chunkModules: false,
+                colors: true
             }
-
-            callback();
-          });
-          return;
-        }
-
-        callback();
-      }],
-      resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-      },
-      module: {
-        rules: [{
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-        }, {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          options: {
-            presets: [['es2015', {loose: true}], 'react'],
-            plugins: ['transform-object-rest-spread']
-          },
-          exclude: /node_modules/
-        }]
-      },
-      devtool: 'inline-source-map',
-      plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify("DEVELOPMENT")
-          },
-          API_ROOT: JSON.stringify("http://localhost:8080/api/v1")
-        }),
-        new webpack.ProvidePlugin({
-          'React': 'react',
-          '$': 'jquery'
-        })
-      ]
-    },
-    webpackMiddleware: {
-      stats: {
-        chunkModules: false,
-        colors: true
-      }
-    },
-    preprocessors: {
-      'spec.js': ['webpack', 'sourcemap']
-    },
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['PhantomJS'],
-    singleRun: true
-  });
+        },
+        preprocessors: {
+            'spec.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['PhantomJS'],
+        singleRun: true
+    });
 };
