7b1197f1dd8446c635e2426c374f598cdbbc12b3
Andrey Narut <anarut@ish.com.au>
2017-03-31 11:06:25 +0000

    OD-10280 Updated expression for tags to get the same results as for query.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46217 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/groovy/ish/oncourse/solr/functions/tag/Functions.groovy
+++ common/common-services/src/main/groovy/ish/oncourse/solr/functions/tag/Functions.groovy
@@ -1,13 +1,17 @@
 package ish.oncourse.solr.functions.tag
 
 import ish.oncourse.model.Tag
+import ish.oncourse.model.Taggable
+import ish.oncourse.model.TaggableTag
 import ish.oncourse.solr.model.SolrTag
 import org.apache.cayenne.ObjectContext
 import org.apache.cayenne.ResultIterator
 import org.apache.cayenne.query.ObjectSelect
 
 class Functions {
-
+    
+    private static final String COURSE_IDENTIFIER= 'Course'
+    
     static Closure<SolrTag> getSolrTag = { Tag tag ->
         return new SolrTag().with {
             it.id = "${tag.id}"
@@ -18,7 +22,11 @@ class Functions {
     }
     
     static Closure<Iterator<SolrTag>> getSolrTags = { ObjectContext context ->
-        ResultIterator<Tag> tags = ObjectSelect.query(Tag).orderBy(Tag.NAME.asc()).iterator(context)
+        ResultIterator<Tag> tags = ObjectSelect.query(Tag)
+                .where(Tag.IS_WEB_VISIBLE.eq(true))
+                .and(Tag.TAGGABLE_TAGS.dot(TaggableTag.TAGGABLE).dot(Taggable.ENTITY_IDENTIFIER).eq(COURSE_IDENTIFIER))
+                .iterator(context)
+        
         return [hasNext: { return tags.hasNextRow() },
                 next   : { return getSolrTag.call(tags.nextRow()) },
                 remove : { tags.skipRow() }
