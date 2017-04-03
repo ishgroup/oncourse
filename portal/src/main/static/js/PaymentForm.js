109d297b193b4d9db9b718ef24721f0fb26d97fd
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-04-03 14:15:15 +0000

    OD-10407 PaymentForm.date field accepts value in the following format: "MM/YY", jquery.Inputmask plugin for this mask was configured correctly.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46230 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/static/js/PaymentForm.js
+++ portal/src/main/static/js/PaymentForm.js
@@ -7,6 +7,37 @@ goog.require('handlebars');
 
 var $j = jQuery.noConflict();
 
+Inputmask.extendDefinitions({
+    M: {
+        validator: "0[1-9]|1[012]",
+        cardinality: 2,
+        placeholder: 'M',
+        prevalidator: [{
+            validator: function (chrs, maskset, pos, strict, opts) {
+                var isNumeric = new RegExp("[0-9]");
+                if(!isNumeric.test(chrs)) return false;
+                if(chrs > "1") {
+                    maskset.buffer[pos] = "0";
+                    return {
+                        "pos": pos+1,
+                        "c": chrs
+                    };
+                }
+                else return true;
+            },
+            cardinality: 1
+        }]
+    },
+    Y: {
+        validator: "[0-9]",
+        cardinality: 2,
+        placeholder: 'Y',
+        prevalidator: null
+    }
+});
+
+
+
 var Action = {
     init: 'init',
     make: 'make',
@@ -188,7 +219,7 @@ PaymentForm.prototype = {
 
     initInputDate: function () {
         this.inputDate = $j("div#payment-controls input[name='date']");
-        this.inputDate.inputmask("m/y", { placeholder: " ", showMaskOnHover: false, showMaskOnFocus: false });
+        this.inputDate.inputmask("M/Y", { placeholder: " ", showMaskOnHover: false, showMaskOnFocus: false });
     },
 
     fillCardDetails: function () {
