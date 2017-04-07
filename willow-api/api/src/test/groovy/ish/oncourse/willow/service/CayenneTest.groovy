8a8c96136161ef7d05889710b947e4526a993b27
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-04-07 04:55:48 +0000

    OD-10370 CayenneTest shows the use case where cayenne 4.0M5 is not compatible with groovy
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46261 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ willow-api/api/src/test/groovy/ish/oncourse/willow/service/CayenneTest.groovy
@@ -0,0 +1,20 @@
+package ish.oncourse.willow.service
+
+import groovy.transform.CompileStatic
+import ish.oncourse.model.College
+import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.query.ObjectSelect
+import org.junit.Test
+
+@CompileStatic
+class CayenneTest {
+
+    @Test
+    void test_4_0_M5() {
+        ObjectContext context = null
+
+        ObjectSelect.query(ish.oncourse.model.Product.class).where(ish.oncourse.model.Product.COLLEGE.eq(new College())).select(context).each { ish.oncourse.model.Product p->
+            println p.id
+        }
+    }
+}
