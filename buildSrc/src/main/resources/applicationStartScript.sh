d24584dab3d78dcfdccba838af09f63debbe63d4
Aristedes Maniatis <ari@ish.com.au>
2017-04-22 08:49:53 +0000

    Completely rebuild the start/stop script so that it saves a PID and is therefore capable of stopping the application. I added code to theoretically make it work on OSX, but haven't tested it.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46397 fb2e276d-fae9-0310-b578-b71ac85840b9

--- buildSrc/src/main/resources/applicationStartScript.sh
+++ buildSrc/src/main/resources/applicationStartScript.sh
@@ -6,84 +6,65 @@
 ##
 ##############################################################################
 
-usage() {
-  echo "\$0 stop|start"
-  exit 1;
+# There is no realpath command on OSX, so we need to fake it
+command -v realpath >/dev/null 2>&1 || realpath() {
+    [[ \$1 = /* ]] && echo "\$1" || echo "\$PWD/\${1#./}"
 }
 
-pidfile="/var/run/onCourse/${applicationName}.pid"
+PID_FILE="/var/run/onCourse/${applicationName}.pid"
+BASEDIR=\$(realpath \${0})
+APP_HOME=\$(dirname \${BASEDIR} | xargs dirname)
+cd $APP_HOME
 
-# Attempt to set APP_HOME
-# Resolve links: \$0 may be a link
-PRG="\$0"
-# Need this for relative symlinks.
-while [ -h "\$PRG" ] ; do
-    ls=`ls -ld "\$PRG"`
-    link=`expr "\$ls" : '.*-> \\(.*\\)\$'`
-    if expr "\$link" : '/.*' > /dev/null; then
-        PRG="\$link"
-    else
-        PRG=`dirname "\$PRG"`"/\$link"
-    fi
-done
-SAVED="`pwd`"
-cd "`dirname \"\$PRG\"`/${appHomeRelativePath}" >/dev/null
-APP_HOME="`pwd -P`"
-cd "\$SAVED" >/dev/null
-
-APP_NAME="${applicationName}"
-APP_BASE_NAME=`basename "\$0"`
-
-# Add default JVM options here. You can also use JAVA_OPTS and ${optsEnvironmentVar} to pass JVM options to this script.
+# Default JVM options. You can also use JAVA_OPTS and ${optsEnvironmentVar} to pass JVM options to this script.
 DEFAULT_JVM_OPTS=${defaultJvmOpts}
 
-die() {
-    echo
-    echo "\$*"
-    echo
-    exit 1
+usage() {
+    echo "\$0 stop|start"
+    exit 1;
 }
 
-CLASSPATH=$classpath
-
-# Determine the Java command to use to start the JVM.
-if [ -n "\$JAVA_HOME" ] ; then
-    JAVACMD="\$JAVA_HOME/bin/java"
-    if [ ! -x "\$JAVACMD" ] ; then
-        die "ERROR: JAVA_HOME is set to an invalid directory: \$JAVA_HOME
-
-Please set the JAVA_HOME variable in your environment to match the
-location of your Java installation."
-    fi
-else
-    JAVACMD="java"
-    which java >/dev/null 2>&1 || die "ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-
-Please set the JAVA_HOME variable in your environment to match the
-location of your Java installation."
-fi
-
-# Collect all arguments for the java command, following the shell quoting and substitution rules
-eval set -- \$DEFAULT_JVM_OPTS \$JAVA_OPTS \$${optsEnvironmentVar} <% if ( appNameSystemProperty ) { %>"\"-D${appNameSystemProperty}=\$APP_BASE_NAME\"" <% } %>-classpath "\"\$CLASSPATH\"" ${mainClassName}
-
-
 stop() {
-    cat $pidfile | kill
+    cat "\$PID_FILE" | xargs kill
 }
 
 start() {
-    exec "\$JAVACMD" "\$@" & echo $! > $pidfile
+    CLASSPATH="${classpath}"
+
+    # Determine the Java command to use to start the JVM.
+    if [ -n "\$JAVA_HOME" ] ; then
+        JAVACMD="\$JAVA_HOME/bin/java"
+        if [ ! -x "\$JAVACMD" ] ; then
+            echo "ERROR: JAVA_HOME is set to an invalid directory: \$JAVA_HOME"
+            exit 1
+        fi
+    else
+        JAVACMD="java"
+        which java >/dev/null 2>&1 || echo "ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH."; exit 1
+    fi
+
+    CMD="\$JAVACMD \\
+        \$DEFAULT_JVM_OPTS \\
+        \$JAVA_OPTS \\
+        \$${optsEnvironmentVar} \\
+        -classpath \$CLASSPATH \\
+        ${mainClassName}"
+
+    # FreeBSD has a daemon starting binary which creates the pidfile for us
+    if [ -x "/usr/sbin/daemon" ] ; then
+        \$(/usr/sbin/daemon -p \$PID_FILE \$CMD)
+    else
+        exec "\$JAVACMD" "\$@" & echo \$! > "\$pidfile"
+    fi
 }
 
 case "\$1" in
-   stop)
-    stop
-    ;;
-   start)
-    start
-    ;;
-   *)
-    usage
+    stop)
+        stop
+        ;;
+    start)
+        start
+        ;;
+    *)
+        usage
 esac
-
-
