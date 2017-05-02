84628357b64046e10322b103f7cc28822bb31275
Artyom Kravenko <artyom@ish.com.au>
2017-05-02 09:58:01 +0000

    OD-10347 Additional item for fieldSet enum
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46466 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/model/FieldSet.java
@@ -0,0 +1,42 @@
+package ish.oncourse.willow.model;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Gets or Sets FieldSet
+ */
+public enum FieldSet {
+  
+  ENROLMENT("enrolment"),
+  
+  WAITINGLIST("waitinglist"),
+
+  APPLICATION("application"),
+
+  MAILINGLIST("mailinglist");
+
+  private String value;
+
+  FieldSet(String value) {
+    this.value = value;
+  }
+
+  @Override
+  @JsonValue
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static FieldSet fromValue(String text) {
+    for (FieldSet b : FieldSet.values()) {
+      if (String.valueOf(b.value).equals(text)) {
+        return b;
+      }
+    }
+    return null;
+  }
+}
+
