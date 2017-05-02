c94e0df50b57c6c1ae0b1e37805735f62d0ff75d
Artyom Kravenko <artyom@ish.com.au>
2017-05-02 10:00:15 +0000

    OD-10347 Find empty fields for contact/student based on related field configs
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46467 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/service/impl/ContactDetailsBuilder.groovy
@@ -0,0 +1,59 @@
+package ish.oncourse.willow.service.impl
+
+import ish.oncourse.common.field.ContextType
+import ish.oncourse.common.field.FieldProperty
+import ish.oncourse.common.field.PropertyGetSet
+import ish.oncourse.common.field.PropertyGetSetFactory
+import ish.oncourse.model.Contact
+import ish.oncourse.model.Course
+import ish.oncourse.model.Field
+import ish.oncourse.model.FieldConfiguration
+import ish.oncourse.willow.model.FieldSet
+
+class ContactDetailsBuilder {
+
+    List<Field> getContactDetails(Contact contact, Course courses, FieldSet fieldSet) {
+
+        PropertyGetSetFactory factory = new PropertyGetSetFactory('ish.oncourse.model')
+        List<Field> fields = new ArrayList<>()
+
+        courses.each { c ->
+            
+            FieldConfiguration scheme
+            switch (fieldSet) {
+                case FieldSet.ENROLMENT:
+                    scheme = c.fieldConfigurationScheme.enrolFieldConfiguration
+                    break
+                case FieldSet.WAITINGLIST:
+                    scheme = c.fieldConfigurationScheme.waitingListFieldConfiguration
+                    break
+                case FieldSet.APPLICATION:
+                    scheme = c.fieldConfigurationScheme.applicationFieldConfiguration
+                    break
+                default: throw new IllegalArgumentException()
+            }
+
+            scheme.fields.each { f ->
+                
+                FieldProperty property = FieldProperty.getByKey(f.property)
+                Object source
+                switch (property.contextType) {
+                    case ContextType.CONTACT:
+                        source = contact
+                        break
+                    case ContextType.STUDENT:
+                        source = contact.student
+                        break
+                    default:
+                        throw new IllegalArgumentException()
+                }
+                
+                PropertyGetSet getSet  = factory.get(f, source)
+                if (!getSet.get()) {
+                    fields << f
+                }
+            }
+        }
+        fields
+    }
+}
---
+++ willow-api/api/src/test/groovy/ish/oncourse/willow/service/ContactDetailsTest.groovy
@@ -0,0 +1,30 @@
+package ish.oncourse.willow.service
+
+import ish.oncourse.model.Contact
+import ish.oncourse.model.Course
+import ish.oncourse.willow.model.FieldSet
+import ish.oncourse.willow.service.impl.ContactDetailsBuilder
+import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.query.ObjectSelect
+import org.apache.cayenne.query.SelectById
+import org.junit.Test
+
+class ContactDetailsTest extends  ApiTest{
+
+
+    @Override
+    protected String getDataSetResource() {
+        return 'ish/oncourse/willow/service/ContactDetailsTest.xml'
+
+    }
+    
+    @Test
+    void test() {
+        ObjectContext context = cayenneRuntime.newContext()
+        
+        Contact contact = SelectById.query(Contact, 1001L).selectOne(context)
+        Course course = SelectById.query(Course, 1001L).selectOne(context)
+
+        new ContactDetailsBuilder().getContactDetails(contact, course, FieldSet.ENROLMENT)
+    }
+}
