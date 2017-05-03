6686c7eb9c67e0fe6d34387caf8d0e9b04060bd9
Artyom Kravenko <artyom@ish.com.au>
2017-05-03 14:05:28 +0000

    OD-10506 Order fields by heading, order
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46493 fb2e276d-fae9-0310-b578-b71ac85840b9

--- willow-api/api/src/main/groovy/ish/oncourse/willow/functions/ContactDetailsBuilder.groovy
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/functions/ContactDetailsBuilder.groovy
@@ -12,48 +12,46 @@ import ish.oncourse.willow.model.FieldSet
 
 class ContactDetailsBuilder {
 
-    List<Field> getContactDetails(Contact contact, Course courses, FieldSet fieldSet) {
+    List<Field> getContactDetails(Contact contact, Course course, FieldSet fieldSet) {
 
         PropertyGetSetFactory factory = new PropertyGetSetFactory('ish.oncourse.model')
         List<Field> fields = new ArrayList<>()
+        
+        FieldConfiguration scheme
+        switch (fieldSet) {
+            case FieldSet.ENROLMENT:
+                scheme = course.fieldConfigurationScheme.enrolFieldConfiguration
+                break
+            case FieldSet.WAITINGLIST:
+                scheme = course.fieldConfigurationScheme.waitingListFieldConfiguration
+                break
+            case FieldSet.APPLICATION:
+                scheme = course.fieldConfigurationScheme.applicationFieldConfiguration
+                break
+            default: throw new IllegalArgumentException()
+        }
 
-        courses.each { c ->
+        scheme.fields.each { f ->
             
-            FieldConfiguration scheme
-            switch (fieldSet) {
-                case FieldSet.ENROLMENT:
-                    scheme = c.fieldConfigurationScheme.enrolFieldConfiguration
+            FieldProperty property = FieldProperty.getByKey(f.property)
+            Object source
+            switch (property.contextType) {
+                case ContextType.CONTACT:
+                    source = contact
                     break
-                case FieldSet.WAITINGLIST:
-                    scheme = c.fieldConfigurationScheme.waitingListFieldConfiguration
+                case ContextType.STUDENT:
+                    source = contact.student
                     break
-                case FieldSet.APPLICATION:
-                    scheme = c.fieldConfigurationScheme.applicationFieldConfiguration
-                    break
-                default: throw new IllegalArgumentException()
+                default:
+                    throw new IllegalArgumentException()
             }
-
-            scheme.fields.each { f ->
-                
-                FieldProperty property = FieldProperty.getByKey(f.property)
-                Object source
-                switch (property.contextType) {
-                    case ContextType.CONTACT:
-                        source = contact
-                        break
-                    case ContextType.STUDENT:
-                        source = contact.student
-                        break
-                    default:
-                        throw new IllegalArgumentException()
-                }
-                
-                PropertyGetSet getSet  = factory.get(f, source)
-                if (!getSet.get()) {
-                    fields << f
-                }
+            
+            PropertyGetSet getSet  = factory.get(f, source)
+            if (!getSet.get()) {
+                fields << f
             }
         }
-        fields
+        
+        fields.sort {a,b -> a.fieldHeading.name <=> b.fieldHeading.name ?: a.order <=> b.order }
     }
 }
