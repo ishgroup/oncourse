51d904dd0e7fea520cde92526fd1dadf705e3050
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-05-12 12:39:33 +0000

    OD-10491 Add createStringEnum helper function to have a possibility to create string-enumerations types in typescript
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46612 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ react/src/js/common/utils/EnumUtils.ts
@@ -0,0 +1,9 @@
+/**
+ *
+ */
+export const createStringEnum = <T extends string>(o: Array<T>): {[K in T]: K} => {
+  return o.reduce((res, key) => {
+    res[key] = key;
+    return res;
+  }, Object.create(null));
+};
