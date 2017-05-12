952d0468aad85ed5821d2d9a4edaced9ff34d86c
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-05-12 16:27:46 +0000

    OD-10444 add DataType test
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46638 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ react/src/test/jest/model/field/DataType.test.ts
@@ -0,0 +1,28 @@
+import * as Types from "../../../../js/model/field/DataType";
+
+class Class1 {
+  type:Types.DataType;
+}
+
+test('test Enum1.ts', () => {
+
+  let class1:Class1 = createClass();
+
+  expect(class1 instanceof Class1).toBe(true);
+  expect(class1.type).toBe(Types.DataType.string);
+
+  switch (class1.type) {
+    case Types.DataType.string:
+      expect(true).toBe(true);
+      break;
+    default:
+      throw new Error();
+  }
+});
+
+
+const createClass = ():Class1 =>{
+  let class1:Class1 = new Class1();
+  class1.type = Types.DataType.string;
+  return class1;
+};
