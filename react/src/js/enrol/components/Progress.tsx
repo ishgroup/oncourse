a87d7d3b6d68d3b9a47206d92e458661f7e65815
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-05-13 12:01:39 +0000

    OD-10252 Progress component has got Model property, this model contains all information about which tabs are active or disabled
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46648 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/enrol/components/Progress.tsx
+++ react/src/js/enrol/components/Progress.tsx
@@ -1,24 +1,39 @@
 import * as React from "react";
+import * as Lodash from "lodash";
 import classnames from "classNames";
 
 /**
  * Progress bar for the enrol application
  */
-export class Progress extends React.Component<Props, State> {
+
+const Classes = {
+  active: "active",
+  disable: "disable",
+  last: "last"
+};
+
+export class Progress extends React.Component<Props, any> {
 
   render() {
     const {onChange} = this.props;
-    const detailsClass = this.isActive(Tab.details)? "active": "disable";
-    const summaryClass = this.isActive(Tab.summary)? "active": "disable";
-    const paymentClass = classnames("last", this.isActive(Tab.payment)? "active": "disable");
-    const onDetails = (e) => {onChange(Tab.details)};
-    const onSummary = (e) => {onChange(Tab.summary)};
-    const onPayment = (e) => {onChange(Tab.payment)};
+    const detailsClass = this.className(Tab.details);
+    const summaryClass = this.className(Tab.summary);
+    const paymentClass = classnames(Classes.last, this.className(Tab.payment));
+
+    const onDetails = () => {
+      onChange(Tab.details)
+    };
+    const onSummary = () => {
+      onChange(Tab.summary)
+    };
+    const onPayment = () => {
+      onChange(Tab.payment)
+    };
 
     return (
       <div className="progress-steps">
         <ul>
-          <li className={detailsClass} ><a onClick={onDetails} href="#">Your details</a></li>
+          <li className={detailsClass}><a onClick={onDetails} href="#">Your details</a></li>
           <li className={summaryClass}><a onClick={onSummary} href="#">Summary</a></li>
           <li className={paymentClass}><a onClick={onPayment} href="#">Payment</a></li>
         </ul>
@@ -26,9 +41,18 @@ export class Progress extends React.Component<Props, State> {
     )
   }
 
-  private isActive = (tab:Tab):boolean => {
-    const {selected} = this.props;
-    return selected === tab;
+  private className = (tab: Tab): string => {
+    return this.isActive(tab) ? Classes.active : this.isDisabled(tab) ? Classes.disable : ""
+  };
+
+  private isActive = (tab: Tab): boolean => {
+    const {model} = this.props;
+    return model.active === tab;
+  };
+
+  private isDisabled = (tab: Tab): boolean => {
+    const {model} = this.props;
+    return !Lodash.isNil(model.disabled.find((t) => t === tab));
   }
 }
 
@@ -38,9 +62,13 @@ export enum Tab {
   payment
 }
 
+export interface Model {
+  active: Tab,
+  disabled: Tab[]
+}
 
 interface Props {
-  selected: Tab,
+  model: Model
   onChange: (Tab) => void
 }
 
