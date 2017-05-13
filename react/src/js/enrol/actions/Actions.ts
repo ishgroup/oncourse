33aedf547e36d71fb7ab8f3357ff44942efaf53d
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-05-13 12:04:29 +0000

    OD-10444 actions Init and PhaseChange have been added to the Enrol app action list
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46649 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/enrol/actions/Actions.ts
+++ react/src/js/enrol/actions/Actions.ts
@@ -1,5 +1,13 @@
 import {_toRejectType, _toRequestType} from "../../common/actions/ActionUtils";
 
+//initialize enrol application
+const Init:string = "enrol/init";
+export const InitRequest:string = _toRequestType(Init);
+export const InitReject:string = _toRejectType(Init);
+
+//change current phase action
+export const PhaseChange:string = "enrol/phase/change";
+
 export const ContactAdd:string = "enrol/contact/add";
 export const ContactAddRequest:string = _toRequestType(ContactAdd);
 export const ContactAddReject:string = _toRejectType(ContactAdd);
