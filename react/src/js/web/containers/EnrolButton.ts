482ca67ff32ffa960d6590803dffa82632316d87
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-05-13 17:56:32 +0000

    OD-10444 refactor a few method in EnrolButton.ts container to make this class more readable
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46656 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/web/containers/EnrolButton.ts
+++ react/src/js/web/containers/EnrolButton.ts
@@ -1,38 +1,40 @@
 import * as EnrolButton from "../components/EnrolButton";
+import {isNil} from "lodash";
 import {connect} from "react-redux";
 import {CourseClassCartState, IshState} from "../../services/IshState";
 import {CourseClass} from "../../model/web/CourseClass";
 import {Actions} from "../actions/Actions";
 import * as CourseClassService from "../services/CourseClassService";
 
+const isAdded = (items: CourseClassCartState, id: string): boolean => {
+    const item = items.result.find(itemId => itemId === id);
+    return !isNil(item);
+};
+
+const mapStateToProps = (state: IshState, props) => {
+    const courseClass: CourseClass = CourseClassService.htmlProps2CourseClass(props);
+    return {
+        id: courseClass.id,
+        courseClass: state.courses.entities[courseClass.id] || {},
+        isAdded: isAdded(state.cart.courses, props.id)
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addCourseClass: (courseClass: CourseClass) => {
+            dispatch({
+                type: Actions.ADD_CLASS_TO_CART,
+                payload: courseClass
+            });
+        },
+        requestCourseClassById: (id: string) => {
+            dispatch({
+                type: Actions.REQUEST_COURSE_CLASS,
+                payload: id
+            });
+        }
+    };
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(EnrolButton.EnrolButton as any);
-
-function isAdded(items: CourseClassCartState, id) {
-  let item = items.result.find(itemId => itemId === id);
-  return !!item;
-}
-
-function mapStateToProps(state: IshState, props) {
-  const courseClass:CourseClass = CourseClassService.htmlProps2CourseClass(props);
-  return {
-    courseClass: courseClass || state.courses.entities[courseClass.id] || {},
-    isAdded: isAdded(state.cart.courses, props.id)
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    addCourseClass: (courseClass: CourseClass) => {
-      dispatch({
-        type: Actions.ADD_CLASS_TO_CART,
-        payload: courseClass
-      });
-    },
-    requestCourseClassById: (id: string) => {
-      dispatch({
-        type: Actions.REQUEST_COURSE_CLASS,
-        payload: id
-      });
-    }
-  };
-}
