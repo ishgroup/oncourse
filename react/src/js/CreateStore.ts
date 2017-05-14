6dc9c6d86be4d48977c20e835733840e0beffdee
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-05-14 14:25:15 +0000

    OD-10528 All java script names should start from Upper case letter
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46678 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/CreateStore.ts
+++ react/src/js/CreateStore.ts
@@ -3,12 +3,12 @@ import {createEpicMiddleware} from "redux-observable";
 import {createLogger} from "redux-logger";
 import {applyMiddleware, createStore, GenericStoreEnhancer} from "redux";
 import {IshState} from "./services/IshState";
-import {rootEpic} from "./rootEpic";
+import {RootEpic} from "./RootEpic";
 import {combinedReducers} from "./reducers/reducers";
 import {EnvironmentConstants} from "./config/EnvironmentConstants";
 import {Actions} from "./web/actions/Actions";
 
-export function configureStore(): Store<IshState> {
+export function CreateStore(): Store<IshState> {
   const store = createStore(
     combinedReducers,
     getMiddlewares()
@@ -23,9 +23,9 @@ export function configureStore(): Store<IshState> {
      * Split middlewares which we using in development and in production.
      */
     if (process.env.NODE_ENV === EnvironmentConstants.development) {
-      return applyMiddleware(createEpicMiddleware(rootEpic), logger);
+      return applyMiddleware(createEpicMiddleware(RootEpic), logger);
     } else {
-      return applyMiddleware(createEpicMiddleware(rootEpic));
+      return applyMiddleware(createEpicMiddleware(RootEpic));
     }
   }
 
