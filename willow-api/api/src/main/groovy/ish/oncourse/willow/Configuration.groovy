251bc5cdf384aa3a43b0a44bf79af057cf33fe48
Artyom Kravenko <artyom@ish.com.au>
2017-05-23 07:37:02 +0000

    OD-0000 Add string constants for each configured property
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46811 fb2e276d-fae9-0310-b578-b71ac85840b9

--- willow-api/api/src/main/groovy/ish/oncourse/willow/Configuration.groovy
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/Configuration.groovy
@@ -7,6 +7,12 @@ class Configuration {
     static final String BD_URL = 'jdbc:mysql://%s:%s/%s?autoReconnect=true&zeroDateTimeBehavior=convertToNull&useUnicode=true&characterEncoding=utf8'
     static final String API_VERSION = 'ish.api.version'
     static final String ZK_HOST_PROPERTY = 'zk.host.property'
+    static final String JETTY_PORT_PROPERTY = 'bq.jetty.connector.port'
+    static final String JETTY_HOST_PROPERTY = 'bq.jetty.connector.host'
+    static final String JDBC_URL_PROPERTY = 'bq.jdbc.ish.url'
+    static final String JDBC_USERNAME_PROPERTY = 'bq.jdbc.ish.username'
+    static final String JDBC_PASSWORD_PROPERTY = 'bq.jdbc.ish.password'
+    static final String CXF_URLPATTERN_PROPERTY = 'bq.cxf.urlPattern'
 
 
     static configure() {
@@ -19,15 +25,15 @@ class Configuration {
         if (propFile.exists()) {
             Properties prop = new Properties()
             prop.load(new FileInputStream(propFile))
-            System.setProperty('bq.jetty.connector.port', prop.get('port') as String)
-            System.setProperty('bq.jetty.connector.host', prop.get('host') as String)
-            System.setProperty('bq.jdbc.ish.url', String.format(BD_URL, prop.get('db_host'), prop.get('db_port'), prop.get('db_name')))
-            System.setProperty('bq.jdbc.ish.username', prop.get('db_user') as String)
-            System.setProperty('bq.jdbc.ish.password', prop.get('db_pass') as String)
+            System.setProperty(JETTY_PORT_PROPERTY, prop.get('port') as String)
+            System.setProperty(JETTY_HOST_PROPERTY, prop.get('host') as String)
+            System.setProperty(JDBC_URL_PROPERTY, String.format(BD_URL, prop.get('db_host'), prop.get('db_port'), prop.get('db_name')))
+            System.setProperty(JDBC_USERNAME_PROPERTY, prop.get('db_user') as String)
+            System.setProperty(JDBC_PASSWORD_PROPERTY, prop.get('db_pass') as String)
             System.setProperty(ZK_HOST_PROPERTY, prop.get('zk_host') as String)
             
             if (prop.get('path')) {
-                System.setProperty('bq.cxf.urlPattern',prop.get('path') as String)
+                System.setProperty(CXF_URLPATTERN_PROPERTY, prop.get('path') as String)
             }
 
 
