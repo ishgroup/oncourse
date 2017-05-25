503c25f9eb231d46d1715466bf5a89eb18866ab1
Artyom Kravenko <artyom@ish.com.au>
2017-05-25 14:17:37 +0000

    OD-10531 Method to extract current website
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@46858 fb2e276d-fae9-0310-b578-b71ac85840b9

--- willow-api/api/src/main/groovy/ish/oncourse/willow/service/impl/CollegeService.groovy
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/service/impl/CollegeService.groovy
@@ -2,6 +2,7 @@ package ish.oncourse.willow.service.impl
 
 import com.google.inject.Inject
 import ish.oncourse.model.College
+import ish.oncourse.model.WebSite
 import ish.oncourse.services.site.GetWebSite
 import ish.oncourse.willow.filters.RequestFilter
 import org.apache.cayenne.configuration.server.ServerRuntime
@@ -16,6 +17,11 @@ class CollegeService {
     }
     
     College getCollege() {
-       new GetWebSite(RequestFilter.ThreadLocalXOrigin.get(),runtime.newContext()).get().college
+        getWebSite().college
     }
+
+    WebSite getWebSite() {
+        new GetWebSite(RequestFilter.ThreadLocalXOrigin.get(),runtime.newContext()).get()
+    }
+    
 }
