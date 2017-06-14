6892ec043a867a20a97ba4c89b1ef988fa485499
Artyom Kravenko <artyom@ish.com.au>
2017-06-14 14:58:54 +0000

    OD-10671 do not check errors for unselected items
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@47107 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/enrol/epics/EpicProceedToPayment.ts
+++ react/src/js/enrol/epics/EpicProceedToPayment.ts
@@ -42,7 +42,11 @@ export class ProcessCheckoutModel {
     const result = [];
     if (!L.isEmpty(nodes)) {
       nodes.forEach((node: ContactNode) => {
-        if (node.enrolments.find((e) => !L.isEmpty(e.errors))) {
+        if (node.enrolments.find((e) => !L.isEmpty(e.errors) && e.selected) 
+            || node.applications.find((a) => !L.isEmpty(a.errors) && a.selected) 
+            || node.memberships.find((m) => !L.isEmpty(m.errors) && m.selected)
+            || node.articles.find((ar) => !L.isEmpty(ar.errors)  && ar.selected)
+            || node.vouchers.find((v) => !L.isEmpty(v.errors) && v.selected)) {
           result.push(changePhase(Phase.Summary));
         }
         result.push(updateContactNode(node));
