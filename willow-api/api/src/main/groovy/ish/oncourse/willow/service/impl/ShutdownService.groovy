bc56ebcc1b13c26876d15283b22267a84c0b61fd
Artyom Kravenko <artyom@ish.com.au>
2017-06-21 14:25:45 +0000

    OD-10680 Add logging
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@47197 fb2e276d-fae9-0310-b578-b71ac85840b9

--- willow-api/api/src/main/groovy/ish/oncourse/willow/service/impl/ShutdownService.groovy
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/service/impl/ShutdownService.groovy
@@ -1,14 +1,17 @@
 package ish.oncourse.willow.service.impl
 
 import com.google.inject.Inject
-import io.bootique.shutdown.ShutdownManager
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import sun.misc.Signal
 import sun.misc.SignalHandler
 
 import java.util.concurrent.atomic.AtomicBoolean
 
 class ShutdownService implements SignalHandler {
-
+    
+    final static  Logger logger = LoggerFactory.getLogger(ShutdownService.class)
+    
     private AtomicBoolean signalReceived = new AtomicBoolean(false)
 
     @Inject
@@ -19,6 +22,8 @@ class ShutdownService implements SignalHandler {
 
     @Override
     void handle(Signal signal) {
+        logger.warn("$signal signal resived, kill health check")
+
         signalReceived.set(true)
     }
 
