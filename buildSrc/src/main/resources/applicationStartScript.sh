e3589dc7878e19aae7034365f8fdc0c8534c5d5f
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-06-28 10:35:32 +0000

    OD-10721 "realpath" function should work in the same way how it works on free bsd. the existing implementation has ben replaced. the command which start this application on MacOSX has been adjusted to work properly when CMD variable is long, this changes fix this error: "File name too long"
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@47273 fb2e276d-fae9-0310-b578-b71ac85840b9

--- buildSrc/src/main/resources/applicationStartScript.sh
+++ buildSrc/src/main/resources/applicationStartScript.sh
@@ -8,7 +8,7 @@
 
 # There is no realpath command on OSX, so we need to fake it
 command -v realpath >/dev/null 2>&1 || realpath() {
-    echo "\$(cd "\$(dirname "\${BASH_SOURCE[0]}")" && pwd)/\$(basename "\${BASH_SOURCE[0]}")"
+    echo \$(cd \$(dirname \$1) && pwd)/\$(basename \$1)
 }
 
 PID_FILE="/var/run/onCourse/${applicationName}.pid"
