83eb8b3862913e83fd2fb00edfe847d45db40d42
Artyom Kravenko <artyom@ish.com.au>
2017-08-01 14:12:18 +0000

    OD-10717 Remove unsupported reference endpoint (v4)
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@47765 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/webservices-client/src/main/java/ish/oncourse/webservices/util/SupportedVersions.java
+++ common/webservices-client/src/main/java/ish/oncourse/webservices/util/SupportedVersions.java
@@ -1,5 +1,5 @@
 package ish.oncourse.webservices.util;
 
 public enum SupportedVersions {
-	V4, V5, V6, V10, V14, V15, V16
+	V5, V6, V10, V14, V15, V16
 }
--- common/webservices-client/src/main/java/ish/oncourse/webservices/v4/stubs/reference/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ref.v4.soap.webservices.oncourse.ish/")
-package ish.oncourse.webservices.v4.stubs.reference;
