d08d1f68d49282f85e22ee61ecd2170026e67948
Artyom Kravenko <artyom@ish.com.au>
2017-08-14 07:44:21 +0000

    OD-10920 Add space between label and reference number
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@47953 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/enrol/containers/result/components/Successful.tsx
+++ react/src/js/enrol/containers/result/components/Successful.tsx
@@ -14,7 +14,7 @@ export class Successful extends React.Component<Props, any> {
         <h2>Payment <span>»</span> Successful</h2>
         <p>Thank you for enrolling, applying or purchasing from omega.</p>
         <p>Your payment was <strong>SUCCESSFUL</strong> and recorded in our system against reference number 
-          <strong>{refId}</strong>.</p>
+          <strong> {refId}</strong>.</p>
         <p>Each student will shortly receive an enrolment or application confirmation, if a fee was incurred a tax
           invoice will also be sent. If you don't receive these within 24 hours, please contact us.</p>
         <p><strong>Please press continue to view further important information</strong></p>
