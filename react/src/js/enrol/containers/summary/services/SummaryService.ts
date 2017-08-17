86cd8f8545f424eeeb9dd162441b9fbeebba3e59
Andrey Davidovich <ancreo@ish.com.au>
2017-08-17 08:52:25 +0000

    OD-10920 updated check for continue proceed to checkout
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@47991 fb2e276d-fae9-0310-b578-b71ac85840b9

--- react/src/js/enrol/containers/summary/services/SummaryService.ts
+++ react/src/js/enrol/containers/summary/services/SummaryService.ts
@@ -1,17 +1,25 @@
 import {State} from "../reducers/State";
-import {Enrolment, Application, Membership, Article, Voucher} from "../../../../model";
+import {PurchaseItem} from "../../../../model";
 export class SummaryService {
   /**
    * Return if state has at least one selected purchased item
    */
   static hasSelected = (state: State): boolean => {
-    const items: Membership[] | Article[] | Enrolment[] | Application[] | Voucher[] =
-      [].concat(state.entities.enrolments ? Object.values(state.entities.enrolments) : [])
-        .concat(state.entities.applications ? Object.values(state.entities.applications) : [])
-        .concat(state.entities.memberships ? Object.values(state.entities.memberships) : [])
-        .concat(state.entities.articles ? Object.values(state.entities.articles) : [])
-        .concat(state.entities.vouchers ? Object.values(state.entities.vouchers) : [])
+
+    const enrolmentsIds = [].concat(...state.result.map(id => state.entities.contactNodes[id].enrolments));
+    const voucherIds = [].concat(...state.result.map(id => state.entities.contactNodes[id].vouchers));
+    const applicationsIds = [].concat(...state.result.map(id => state.entities.contactNodes[id].applications));
+    const membershipsIds = [].concat(...state.result.map(id => state.entities.contactNodes[id].memberships));
+    const articlesIds = [].concat(...state.result.map(id => state.entities.contactNodes[id].articles));
+
+    const items: PurchaseItem[] =
+      [].concat(state.entities.enrolments ? enrolmentsIds.map(id => state.entities.enrolments[id]) : [])
+        .concat(state.entities.vouchers ? voucherIds.map(id => state.entities.vouchers[id]) : [])
+        .concat(state.entities.applications ? applicationsIds.map(id => state.entities.applications[id]) : [])
+        .concat(state.entities.memberships ? membershipsIds.map(id => state.entities.memberships[id]) : [])
+        .concat(state.entities.articles ? articlesIds.map(id => state.entities.articles[id]) : [])
         .find(e => (e.selected && e.errors.length === 0));
+
     return !!items;
   }
 }
