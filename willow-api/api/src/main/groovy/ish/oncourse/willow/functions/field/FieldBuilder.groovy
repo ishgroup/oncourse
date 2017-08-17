4272c8a6a44ca043be13b8bc9a95455aba041294
Artyom Kravenko <artyom@ish.com.au>
2017-08-17 12:25:52 +0000

    OD-10920 Fix change parent code - restore missing 'return' statement
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48000 fb2e276d-fae9-0310-b578-b71ac85840b9

--- willow-api/api/src/main/groovy/ish/oncourse/willow/functions/field/FieldBuilder.groovy
+++ willow-api/api/src/main/groovy/ish/oncourse/willow/functions/field/FieldBuilder.groovy
@@ -1,16 +1,26 @@
 package ish.oncourse.willow.functions.field
 
 import ish.common.util.DisplayableExtendedEnumeration
+import ish.oncourse.model.College
+import ish.oncourse.model.Contact
+import ish.oncourse.model.CustomFieldType
 import ish.oncourse.model.Field
 import ish.oncourse.willow.model.common.Item
 import ish.oncourse.willow.model.field.DataType
+import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.query.ObjectSelect
+import org.apache.commons.lang3.StringUtils
 
 import static ish.oncourse.common.field.FieldProperty.*
 
 class FieldBuilder {
     
+    private static final String OTHER_CHOICE = 'Other'
+    
     Field field
     Class aClass
+    College college
+    ObjectContext context
     
     ish.oncourse.willow.model.field.Field build() {
         new ish.oncourse.willow.model.field.Field().with { f ->
@@ -51,6 +61,28 @@ class FieldBuilder {
 
                     aClass.enumConstants.each { DisplayableExtendedEnumeration item ->
                         f.enumItems  << new Item(value: item.displayName, key: item.databaseValue.toString())
+                    }
+                    break
+                case CUSTOM_FIELD_CONTACT:
+                    f.dataType = DataType.STRING
+                    String key = field.property.replace("${CUSTOM_FIELD_CONTACT.key}.", '')
+                    
+                    CustomFieldType type = ObjectSelect.query(CustomFieldType)
+                            .where(CustomFieldType.ENTITY_NAME.eq(Contact.simpleName))
+                            .and(CustomFieldType.KEY.eq(key))
+                            .and(CustomFieldType.COLLEGE.eq(college))
+                            .selectOne(context)
+                    
+                    String defaultValue = StringUtils.trimToNull(type.getDefaultValue())
+                    if (defaultValue) {
+                        String[] choices = defaultValue.split(";")
+                        if (choices.length > 1) {
+                            choices.each { item ->
+                                f.enumItems  << new Item(value: item.trim() == '*' ? OTHER_CHOICE : item.trim(), key: item.trim())
+                            }
+                        } else {
+                            f.defaultValue = defaultValue
+                        }
                     }
                     break
                 default:
