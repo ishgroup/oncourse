5efeb02abdda93f7db0ebbaaf5735f9f4a77e5be
Artyom Kravenko <artyom@ish.com.au>
2017-08-23 08:23:10 +0000

    0000 Add missing service binding for admin test module
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48077 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/test/java/ish/oncourse/admin/services/AdminTestModule.java
+++ admin/src/test/java/ish/oncourse/admin/services/AdminTestModule.java
@@ -6,6 +6,8 @@ import ish.oncourse.admin.services.ntis.OrganisationServiceBuilder;
 import ish.oncourse.admin.services.ntis.TrainingComponentServiceBuilder;
 import ish.oncourse.model.services.ModelModule;
 import ish.oncourse.services.ServiceModule;
+import ish.oncourse.services.cache.IRequestCacheService;
+import ish.oncourse.services.cache.RequestCacheService;
 import ish.oncourse.services.site.IWebSiteVersionService;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.SubModule;
@@ -16,5 +18,6 @@ public class AdminTestModule {
 		binder.bind(ITrainingComponentService.class, new TrainingComponentServiceBuilder());
 		binder.bind(IOrganisationService.class, new OrganisationServiceBuilder());
 		binder.bind(IWebSiteVersionService.class, WebSiteVersionServiceOverride.class).withId("WebSiteVersionServiceAdmin");
+		binder.bind(IRequestCacheService.class, RequestCacheService.class);
 	}
 }
