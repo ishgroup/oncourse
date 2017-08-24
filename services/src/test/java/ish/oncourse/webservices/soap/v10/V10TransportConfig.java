8a9eb44a9b1d2d8b548521c43eaf1d24ec3f7929
Artyom Kravenko <artyom@ish.com.au>
2017-08-24 10:29:51 +0000

    OD-10944 Add Transport configs for V10 replication version
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48108 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ services/src/test/java/ish/oncourse/webservices/soap/v10/V10TransportConfig.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au No copying or use of this code is allowed without permission in writing from ish.
+ */
+package ish.oncourse.webservices.soap.v10;
+
+import ish.oncourse.webservices.soap.TestServer;
+import ish.oncourse.webservices.soap.TransportConfig;
+import ish.oncourse.webservices.soap.v5.ReferencePortType;
+import ish.oncourse.webservices.v10.stubs.replication.ReplicationStub;
+import ish.oncourse.webservices.v10.stubs.replication.TransactionGroup;
+import ish.oncourse.webservices.v5.stubs.reference.ReferenceStub;
+
+public class V10TransportConfig extends TransportConfig<TransactionGroup, ReplicationStub, ReferenceStub, ReferencePortType, ReplicationPortType, PaymentPortType> {
+    
+    static V10TransportConfig valueOf(TestServer testServer) {
+        V10TransportConfig config =  new V10TransportConfig();
+        config.server(testServer);
+        return config;
+    }
+}
