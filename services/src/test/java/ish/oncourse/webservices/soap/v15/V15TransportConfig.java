cb3b27f3419f5afb744e1f889bd9244d647b163b
Alex <alex@ish.com.au>
2017-08-28 11:57:11 +0000

    OD-10944 overrided V15TranstortConfig like in V16. Created V15TestEnv
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48145 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/test/java/ish/oncourse/webservices/soap/v15/V15TransportConfig.java
+++ services/src/test/java/ish/oncourse/webservices/soap/v15/V15TransportConfig.java
@@ -1,22 +1,28 @@
 package ish.oncourse.webservices.soap.v15;
 
+import ish.oncourse.services.system.ICollegeService;
+import ish.oncourse.webservices.function.TestEnv;
 import ish.oncourse.webservices.function.TransportConfig;
-import ish.oncourse.webservices.soap.TestServer;
 import ish.oncourse.webservices.soap.v6.ReferencePortType;
+import ish.oncourse.webservices.util.SupportedVersions;
 import ish.oncourse.webservices.v15.stubs.replication.ReplicationStub;
 import ish.oncourse.webservices.v15.stubs.replication.TransactionGroup;
 import ish.oncourse.webservices.v6.stubs.reference.ReferenceStub;
+
+import static ish.oncourse.webservices.soap.TestConstants.DEFAULT_COLLEGE_KEY;
 
 /**
  * Created by alex on 8/24/17.
  */
 public class V15TransportConfig extends TransportConfig<TransactionGroup, ReplicationStub, ReferenceStub, ReferencePortType, ReplicationPortType, PaymentPortType> {
 
-    private V15TransportConfig() {
-    }
-
-    public static V15TransportConfig valueOf(TestServer server) {
-        V15TransportConfig config = new V15TransportConfig();
-        return config;
+    public V15TransportConfig(TestEnv testEnv) {
+        this.serverURI(testEnv.getURI());
+        this.replicationVersion(SupportedVersions.V15);
+        this.referenceVersion(SupportedVersions.V6);
+        this.communicationKey(() -> testEnv.getPageTester().getService(ICollegeService.class)
+                .findBySecurityCode(DEFAULT_COLLEGE_KEY).getCommunicationKey());
+        this.securityCode(() -> testEnv.getPageTester().getService(ICollegeService.class)
+                .findBySecurityCode(DEFAULT_COLLEGE_KEY).getWebServicesSecurityCode());
     }
 }
