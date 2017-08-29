881cc80f3bb85f1088df19e8d79ff6f353f663bb
Alex <alex@ish.com.au>
2017-08-29 12:31:07 +0000

    OD-10944 created V10TestEnv like V16, adjusted V10TransportConfig
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48169 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/test/java/ish/oncourse/webservices/soap/v10/V10TransportConfig.java
+++ services/src/test/java/ish/oncourse/webservices/soap/v10/V10TransportConfig.java
@@ -3,18 +3,28 @@
  */
 package ish.oncourse.webservices.soap.v10;
 
+import ish.oncourse.services.system.ICollegeService;
+import ish.oncourse.webservices.function.TestEnv;
 import ish.oncourse.webservices.function.TransportConfig;
-import ish.oncourse.webservices.soap.TestServer;
 import ish.oncourse.webservices.soap.v5.ReferencePortType;
+import ish.oncourse.webservices.util.SupportedVersions;
 import ish.oncourse.webservices.v10.stubs.replication.*;
 import ish.oncourse.webservices.v5.stubs.reference.ReferenceStub;
+
+import static ish.oncourse.webservices.soap.TestConstants.DEFAULT_COLLEGE_KEY;
+
 
 public class V10TransportConfig extends TransportConfig<
 		TransactionGroup, ParametersMap, ReplicationRecords, ReplicationResult,
 		ReplicationStub, ReferenceStub, ReferencePortType, ReplicationPortType, PaymentPortType> {
 
-	static V10TransportConfig valueOf(TestServer testServer) {
-		V10TransportConfig config = new V10TransportConfig();
-		return config;
+	public V10TransportConfig(TestEnv testEnv) {
+		this.serverURI(testEnv.getURI());
+		this.replicationVersion(SupportedVersions.V10);
+		this.referenceVersion(SupportedVersions.V5);
+		this.communicationKey(() -> testEnv.getPageTester().getService(ICollegeService.class)
+				.findBySecurityCode(DEFAULT_COLLEGE_KEY).getCommunicationKey());
+		this.securityCode(() -> testEnv.getPageTester().getService(ICollegeService.class)
+				.findBySecurityCode(DEFAULT_COLLEGE_KEY).getWebServicesSecurityCode());
 	}
 }
