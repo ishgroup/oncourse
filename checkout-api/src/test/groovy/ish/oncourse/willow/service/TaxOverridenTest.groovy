6316680392c7510ea120d89d79e1b783ae9f7956
Artyom Kravenko <artyom@ish.com.au>
2017-09-14 12:54:20 +0000

    OD-0000 Fix wrong test extension (inherit abstract class and not test class)
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48360 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout-api/src/test/groovy/ish/oncourse/willow/service/TaxOverridenTest.groovy
+++ checkout-api/src/test/groovy/ish/oncourse/willow/service/TaxOverridenTest.groovy
@@ -22,7 +22,7 @@ import org.junit.Test
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertTrue
 
-class TaxOverridenTest extends MakePaymentTest {
+class TaxOverridenTest extends AbstractPaymentTest {
     
     @Override
     protected String getDataSetResource() {
