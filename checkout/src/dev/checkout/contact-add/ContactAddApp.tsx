5c7344ecc8cc5a71522a614405c1236844c4f7b1
Andrey Davidovich <ancreo@ish.com.au>
2017-09-15 14:04:33 +0000

    OD-00000 fix paths (ts loader errors)
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48392 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout/src/dev/checkout/contact-add/ContactAddApp.tsx
+++ checkout/src/dev/checkout/contact-add/ContactAddApp.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import {Provider} from "react-redux";
-import ContactAddForm from "enrol/containers/contact-add/ContactAddForm";
-import {CreateStore} from "CreateStore";
-import {Progress, Tab} from "enrol/components/Progress";
-import {Messages} from "enrol/containers/Functions";
+import ContactAddForm from "../../../js/enrol/containers/contact-add/ContactAddForm";
+import {CreateStore} from "../../../js/CreateStore";
+import {Progress, Tab} from "../../../js/enrol/components/Progress";
+import {Messages} from "../../../js/enrol/containers/Functions";
 
 const store = CreateStore();
 
