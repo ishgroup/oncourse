4a7744225dca777b45e36ba211d4460fb3c314ad
Artyom Kravenko <artyom@ish.com.au>
2017-10-20 10:27:21 +0000

    OD-11187 Remove useless api
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48745 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout-api/src/main/groovy/ish/oncourse/willow/WillowApiModule.groovy
+++ checkout-api/src/main/groovy/ish/oncourse/willow/WillowApiModule.groovy
@@ -22,7 +22,6 @@ import ish.oncourse.willow.preference.PreferenceApiImpl
 import ish.oncourse.willow.search.SearchApiImpl
 import ish.oncourse.willow.search.SearchService
 import ish.oncourse.willow.service.impl.*
-import ish.oncourse.willow.waitinglist.WaitingListApiImpl
 import org.apache.cayenne.configuration.ObjectContextFactory
 import org.apache.cayenne.configuration.server.ServerModule
 import org.apache.cayenne.di.Module
@@ -52,7 +51,6 @@ class WillowApiModule extends ConfigModule {
         CXFModule.contributeResources(binder).addBinding().to(CheckoutApiImpl)
         CXFModule.contributeResources(binder).addBinding().to(CorporatePassApiImpl)
         CXFModule.contributeResources(binder).addBinding().to(PreferenceApiImpl)
-        CXFModule.contributeResources(binder).addBinding().to(WaitingListApiImpl)
         CXFModule.contributeResources(binder).addBinding().to(CayenneService)
     }
 
