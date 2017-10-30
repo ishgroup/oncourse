2bf84d31f5864db0bc19f4233cec3729289e93e5
Artyom Kravenko <artyom@ish.com.au>
2017-10-30 12:50:05 +0000

    OD-11237 Use test-function common code for checkout API
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@48888 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ checkout-api/src/test/groovy/ish/oncourse/willow/service/ApiTest.groovy
@@ -0,0 +1,35 @@
+package ish.oncourse.willow.service
+
+import ish.oncourse.test.TestContext
+import ish.oncourse.willow.WillowApiModule
+import ish.oncourse.willow.cayenne.CayenneService
+import org.apache.cayenne.configuration.server.ServerRuntime
+import org.junit.After
+import org.junit.Before
+
+import static ish.oncourse.test.TestContext.*
+
+
+abstract class ApiTest {
+
+    protected ServerRuntime cayenneRuntime
+    protected CayenneService cayenneService
+    private TestContext testContext
+
+    @Before
+    void setup() throws Exception {
+        testContext = new TestContext().params([ (SHOULD_CREATE_SCHEMA) : false, (SHOULD_DROP_SCHEMA) : false]).init()
+        testContext.cleanInsert(dataSetResource)
+        cayenneRuntime = new ServerRuntime("cayenne-oncourse.xml", new WillowApiModule.WillowApiCayenneModule())
+        cayenneService = new CayenneService(cayenneRuntime)
+    }
+
+    @After
+    void after() {
+        cayenneRuntime.shutdown()
+        testContext.close()
+    }
+
+    protected abstract String getDataSetResource()
+
+}
