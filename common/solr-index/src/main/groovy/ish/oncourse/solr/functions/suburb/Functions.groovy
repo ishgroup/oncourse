1df1472a5753668887e2d4007cb963c04ba26d53
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-11-10 13:27:27 +0000

    OD-11215 rename getSolrSuburb to getSSuburb
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49056 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/main/groovy/ish/oncourse/solr/functions/suburb/Functions.groovy
+++ common/solr-index/src/main/groovy/ish/oncourse/solr/functions/suburb/Functions.groovy
@@ -12,7 +12,7 @@ import static org.apache.cayenne.query.ObjectSelect.query
 class Functions {
     static final ObjectSelect<PostcodeDb> PostcodesQuery = query(PostcodeDb).orderBy(PostcodeDb.POSTCODE.asc())
 
-    static Closure<SSuburb> getSolrSuburb = { PostcodeDb postcode ->
+    static Closure<SSuburb> getSSuburb = { PostcodeDb postcode ->
         return new SSuburb().with {
             it.id = "${postcode.postcode}${postcode.suburb}"
             it.suburb = postcode.suburb
@@ -25,6 +25,6 @@ class Functions {
 
     static Closure<Observable<SSuburb>> getSolrSuburbs = { ObjectContext context ->
         ResultIterator<PostcodeDb> postcodeDbs = PostcodesQuery.iterator(context)
-        return Observable.fromIterable(postcodeDbs).map({ p -> getSolrSuburb.call(p) })
+        return Observable.fromIterable(postcodeDbs).map({ p -> getSSuburb.call(p) })
     }
 }
