d8e83249e22882bf5417173ea81e2b93ec464de7
Alex <alex@ish.com.au>
2017-11-14 08:20:28 +0000

    OD-10214 refactored Course and CourseClass functions to separate queries from closures
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49096 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/main/groovy/ish/oncourse/solr/functions/course/CourseClassFunctions.groovy
+++ common/solr-index/src/main/groovy/ish/oncourse/solr/functions/course/CourseClassFunctions.groovy
@@ -2,6 +2,7 @@ package ish.oncourse.solr.functions.course
 
 import ish.oncourse.model.*
 import org.apache.cayenne.ResultIterator
+import org.apache.cayenne.query.ObjectSelect
 
 import static ish.oncourse.model.auto._CourseClass.TUTOR_ROLES
 import static ish.oncourse.model.auto._Room.COURSE_CLASSES
@@ -15,21 +16,38 @@ import static org.apache.cayenne.query.ObjectSelect.query
  * Date: 9/11/17
  */
 class CourseClassFunctions {
+
+    static final ObjectSelect<Session> sessionsQuery(CourseClass courseClass) {
+        query(Session).where(COURSE_CLASS.eq(courseClass)).orderBy(Session.START_DATE.asc())
+    }
+
+    static final ObjectSelect<Contact> contactsQuery(CourseClass courseClass) {
+        query(Contact).where(Contact.TUTOR.dot(TUTOR_ROLES).dot(TutorRole.COURSE_CLASS).eq(courseClass))
+    }
+
+    static final ObjectSelect<Site> courseClassSitesQuery(CourseClass courseClass) {
+        query(Site).where(Site.IS_WEB_VISIBLE.eq(TRUE)).and(ROOMS.dot(COURSE_CLASSES).eq(courseClass))
+    }
+
+    static final ObjectSelect<Site> sessionSitesQuery(CourseClass courseClass) {
+        query(Site).where(Site.IS_WEB_VISIBLE.eq(TRUE)).and(ROOMS.dot(Room.SESSIONS).dot(COURSE_CLASS).eq(courseClass))
+    }
+
+
     public static final Closure<ResultIterator<Session>> Sessions = { CourseClass courseClass ->
-        query(Session).where(COURSE_CLASS.eq(courseClass)).orderBy(Session.START_DATE.asc()).iterator(courseClass.objectContext)
+        sessionsQuery(courseClass).iterator(courseClass.objectContext)
     }
 
     public static final Closure<ResultIterator<Contact>> Contacts = { CourseClass courseClass ->
-        return query(Contact).where(Contact.TUTOR.dot(TUTOR_ROLES).dot(TutorRole.COURSE_CLASS).eq(courseClass)).iterator(courseClass.objectContext)
+        return contactsQuery(courseClass).iterator(courseClass.objectContext)
     }
 
     public static final Closure<ResultIterator<Site>> CourseClassSites = { CourseClass courseClass ->
-        query(Site).where(Site.IS_WEB_VISIBLE.eq(TRUE)).and(ROOMS.dot(COURSE_CLASSES).eq(courseClass)).iterator(courseClass.objectContext)
+        courseClassSitesQuery(courseClass).iterator(courseClass.objectContext)
 
     }
 
     public static final Closure<ResultIterator<Site>> SessionSites = { CourseClass courseClass ->
-        query(Site).where(Site.IS_WEB_VISIBLE.eq(TRUE)).and(ROOMS.dot(Room.SESSIONS).dot(COURSE_CLASS).eq(courseClass)).iterator(courseClass.objectContext)
+        sessionSitesQuery(courseClass).iterator(courseClass.objectContext)
     }
-
 }
--- common/solr-index/src/main/groovy/ish/oncourse/solr/functions/course/CourseFunctions.groovy
+++ common/solr-index/src/main/groovy/ish/oncourse/solr/functions/course/CourseFunctions.groovy
@@ -4,6 +4,7 @@ import ish.oncourse.model.*
 import ish.oncourse.solr.model.SCourse
 import org.apache.cayenne.ObjectContext
 import org.apache.cayenne.ResultIterator
+import org.apache.cayenne.query.ObjectSelect
 
 import static ish.oncourse.model.auto._CourseClass.*
 import static org.apache.cayenne.query.ObjectSelect.query
@@ -14,18 +15,25 @@ class CourseFunctions {
         query(Course).iterator(context)
     }
 
+    static final ObjectSelect<CourseClass> courseClassQuery(CourseContext context) {
+        query(CourseClass).where(COURSE.eq(context.course))
+                .and(IS_WEB_VISIBLE.eq(true))
+                .and(CANCELLED.eq(false))
+                .and(START_DATE.gt(context.current).andExp(IS_DISTANT_LEARNING_COURSE.eq(false)).orExp(IS_DISTANT_LEARNING_COURSE.eq(true))
+        ).orderBy(START_DATE.asc())
+    }
+
     public static final Closure<ResultIterator<CourseClass>> CourseClasses = { CourseContext context ->
-            query(CourseClass).where(COURSE.eq(context.course))
-                    .and(IS_WEB_VISIBLE.eq(true))
-                    .and(CANCELLED.eq(false))
-                    .and(START_DATE.gt(context.current).andExp(IS_DISTANT_LEARNING_COURSE.eq(false)).orExp(IS_DISTANT_LEARNING_COURSE.eq(true))
-            ).orderBy(START_DATE.asc()).iterator(context.context)
+        courseClassQuery(context).iterator(context.context)
+    }
+
+    static final ObjectSelect<Tag> courseTagsQuery(Course course){
+        query(Tag).where(Tag.TAGGABLE_TAGS.dot(TaggableTag.TAGGABLE).dot(Taggable.ENTITY_IDENTIFIER).eq("Course"))
+                .and(Tag.TAGGABLE_TAGS.dot(TaggableTag.TAGGABLE).dot(Taggable.ENTITY_WILLOW_ID).eq(course.id))
     }
 
     public static final Closure<ResultIterator<Tag>> Tags = { Course course ->
-        query(Tag).where(Tag.TAGGABLE_TAGS.dot(TaggableTag.TAGGABLE).dot(Taggable.ENTITY_IDENTIFIER).eq("Course"))
-                .and(Tag.TAGGABLE_TAGS.dot(TaggableTag.TAGGABLE).dot(Taggable.ENTITY_WILLOW_ID).eq(course.id))
-                .iterator(course.objectContext)
+        courseTagsQuery(course).iterator(course.objectContext)
     }
 
     public static final Closure<SCourse> BuildSCourse = { Course course ->
