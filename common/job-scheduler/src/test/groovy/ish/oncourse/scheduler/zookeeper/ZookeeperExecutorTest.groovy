c36bb7a29b513d613bca2d3e40ee9bcb23080612
Andrey Koyro <andrey.koyro@objectstyle.com>
2017-11-15 09:10:38 +0000

    OD-10214 move initialization of the test zookeeper server to before test method
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49114 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/job-scheduler/src/test/groovy/ish/oncourse/scheduler/zookeeper/ZookeeperExecutorTest.groovy
+++ common/job-scheduler/src/test/groovy/ish/oncourse/scheduler/zookeeper/ZookeeperExecutorTest.groovy
@@ -2,26 +2,27 @@ package ish.oncourse.scheduler.zookeeper
 
 import org.apache.zookeeper.server.ServerCnxnFactory
 import org.apache.zookeeper.server.ZooKeeperServer
+import org.junit.After
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class ZookeeperExecutorTest {
     private int zooKeeperPort = 2181
     private int maxClientConnections = 10
+    private ZooKeeperServer zkServer
 
+    @Before
+    void before() {
+        ServerCnxnFactory cnxnFactory = ServerCnxnFactory.createFactory()
+        cnxnFactory.configure(new InetSocketAddress(zooKeeperPort), maxClientConnections)
+        File home = File.createTempDir()
+        zkServer = new ZooKeeperServer(new File(home, 'snap'), new File(home, 'log'), 200)
+        cnxnFactory.startup(zkServer)
+    }
 
     @Test
     void test() {
-
-        ServerCnxnFactory cnxnFactory = ServerCnxnFactory.createFactory()
-        cnxnFactory.configure(new InetSocketAddress(zooKeeperPort), maxClientConnections)
-
-
-        File home = File.createTempDir()
-        ZooKeeperServer zkServer = new ZooKeeperServer(new File(home, 'snap'), new File(home, 'log'), 200)
-        cnxnFactory.startup(zkServer)
-
-
         ZookeeperExecutor executor1 = ZookeeperExecutor.valueOf("127.0.0.1:$zooKeeperPort", "/ish")
         ZookeeperExecutor executor2 = ZookeeperExecutor.valueOf("127.0.0.1:$zooKeeperPort", "/ish")
 
@@ -41,5 +42,10 @@ class ZookeeperExecutorTest {
         })
 
         executor2.release()
+    }
+
+    @After
+    void after() {
+        zkServer.shutdown()
     }
 }
