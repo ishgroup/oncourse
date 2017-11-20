b020608ca4e7031b5a32ed6f873555d01129b140
Alex <alex@ish.com.au>
2017-11-20 13:31:57 +0000

    OD-10214 overrided postcodeDb test entity and adjusted postcodeDbQueryTest
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49212 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/test/groovy/ish/oncourse/solr/functions/suburb/PostcodeDbQueryTest.groovy
+++ common/solr-index/src/test/groovy/ish/oncourse/solr/functions/suburb/PostcodeDbQueryTest.groovy
@@ -1,9 +1,9 @@
 package ish.oncourse.solr.functions.suburb
 
 import ish.oncourse.model.PostcodeDb
-import ish.oncourse.solr.model.PostcodeDbContext
 import ish.oncourse.test.TestContext
-import org.apache.cayenne.configuration.server.ServerRuntime
+import ish.oncourse.test.context.CPostcodeDb
+import org.apache.cayenne.ObjectContext
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -16,25 +16,23 @@ import static ish.oncourse.test.functions.Functions.createRuntime
  */
 class PostcodeDbQueryTest {
     private TestContext testContext
-    private ServerRuntime serverRuntime
-    private PostcodeDbContext postcodeDbContext
+    private ObjectContext context
 
     @Before
     void before() {
 
         testContext = new TestContext()
         testContext.open()
-        serverRuntime = createRuntime()
-        postcodeDbContext = new PostcodeDbContext(context: serverRuntime.newContext())
+        context = createRuntime().newContext()
     }
 
     @Test
     void test() {
-        postcodeDbContext.postcodeDb(1)
-        postcodeDbContext.postcodeDb(3)
-        postcodeDbContext.postcodeDb(2)
+        CPostcodeDb.instance(context, 1).build()
+        CPostcodeDb.instance(context, 3).build()
+        CPostcodeDb.instance(context, 2).build()
 
-        List<PostcodeDb> actualOrderedList = Functions.PostcodesQuery.select(serverRuntime.newContext())
+        List<PostcodeDb> actualOrderedList = Functions.PostcodesQuery.select(context)
         Assert.assertEquals(3, actualOrderedList.size())
         for (int i = 1; i < actualOrderedList.size(); i++) {
             Assert.assertTrue("query returns tags ordered by postcode in asc, each item's postcode must be not bigger that next one", actualOrderedList.get(i).postcode >= actualOrderedList.get(i-1).postcode)
---
+++ common/solr-index/src/test/groovy/ish/oncourse/test/context/CPostcodeDb.groovy
@@ -0,0 +1,39 @@
+package ish.oncourse.test.context
+
+import com.github.javafaker.Faker
+import ish.common.types.PostcodeType
+import ish.oncourse.model.PostcodeDb
+import org.apache.cayenne.ObjectContext
+
+/**
+ * Created by alex on 11/13/17.
+ */
+class CPostcodeDb {
+    private ObjectContext objectContext
+    PostcodeDb postcodeDb
+
+    private static Faker faker = new Faker()
+
+    private CPostcodeDb(){}
+
+    static CPostcodeDb instance(ObjectContext context, Long postcode){
+        CPostcodeDb cPostcodeDb = new CPostcodeDb()
+        cPostcodeDb.objectContext = context
+
+        cPostcodeDb.postcodeDb = cPostcodeDb.objectContext.newObject(PostcodeDb.class)
+        cPostcodeDb.postcodeDb.postcode = postcode
+        cPostcodeDb.postcodeDb.suburb = 'some suburb'
+        cPostcodeDb.postcodeDb.state = faker.address().stateAbbr()
+        cPostcodeDb.postcodeDb.dc = faker.address().state()
+        cPostcodeDb.postcodeDb.lat = faker.number().randomDouble(7, -37, 51)
+        cPostcodeDb.postcodeDb.lon = faker.number().randomDouble(7, -5, 151)
+        cPostcodeDb.postcodeDb.ishVersion = 1529L
+        cPostcodeDb.postcodeDb.type = PostcodeType.DELIVERY_AREA
+        cPostcodeDb
+    }
+
+    CPostcodeDb build(){
+        objectContext.commitChanges()
+        this
+    }
+}
