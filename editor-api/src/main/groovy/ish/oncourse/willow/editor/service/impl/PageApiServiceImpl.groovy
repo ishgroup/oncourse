051d609c4a25a394ece73142de8962ac85860667
Andrey Davidovich <ancreo@ish.com.au>
2017-11-20 10:41:30 +0000

    OD-10844 adding addPage, getList functions
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49205 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor-api/src/main/groovy/ish/oncourse/willow/editor/service/impl/PageApiServiceImpl.groovy
+++ editor-api/src/main/groovy/ish/oncourse/willow/editor/service/impl/PageApiServiceImpl.groovy
@@ -1,13 +1,16 @@
 package ish.oncourse.willow.editor.service.impl
 
 import com.google.inject.Inject
+import ish.oncourse.model.WebNode
 import ish.oncourse.services.persistence.ICayenneService
+import ish.oncourse.willow.editor.model.PageUrl
 import ish.oncourse.willow.editor.service.*
 import ish.oncourse.willow.editor.model.Page
 import ish.oncourse.willow.editor.model.api.PageRenderResponse
 
 import groovy.transform.CompileStatic
 import ish.oncourse.willow.editor.services.RequestService
+import ish.oncourse.willow.editor.website.WebNodeFunctions
 
 @CompileStatic
 class PageApiServiceImpl implements PageApi {
@@ -22,7 +25,13 @@ class PageApiServiceImpl implements PageApi {
     }
 
     Page addPage() {
-        return null
+        WebNode node = WebNodeFunctions.createNewNode(requestService.request, cayenneService.newContext())
+        new Page().with {page ->
+            page.id = node.id
+            page.title = node.name
+
+            page
+        }
     }
     
     void deletePage(Double id) {
@@ -38,15 +47,28 @@ class PageApiServiceImpl implements PageApi {
     }
     
     PageRenderResponse getPageRender(Double pageId) {
-        // TODO: Implement...
         
         return null
     }
     
     List<Page> getPages() {
-        // TODO: Implement...
-        
-        return null
+        List<WebNode> nodes = WebNodeFunctions.getNodes(requestService.request, cayenneService.newContext())
+
+        nodes.collect {node ->
+            new Page().with {page ->
+                page.id = node.id
+                page.title = node.name
+                page.visible = node.published
+                page.urls = node.webUrlAliases.collect {urlAlias ->
+                        new PageUrl().with {url ->
+                            url.link = urlAlias.urlPath
+                            url.isBase = urlAlias.default
+                            url.isDefault = urlAlias.default
+                            url
+                        }}
+                page
+            }
+        }
     }
     
     Page savePage(Page pageParams) {
