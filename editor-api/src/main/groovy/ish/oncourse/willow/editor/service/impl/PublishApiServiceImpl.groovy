8b8aa31ff346a0f39b4936cb06d003183b5214f3
Artyom Kravenko <artyom@ish.com.au>
2017-12-01 10:30:54 +0000

    OD-10840 Implement 'publish' Api
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49384 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor-api/src/main/groovy/ish/oncourse/willow/editor/service/impl/PublishApiServiceImpl.groovy
+++ editor-api/src/main/groovy/ish/oncourse/willow/editor/service/impl/PublishApiServiceImpl.groovy
@@ -1,15 +1,31 @@
 package ish.oncourse.willow.editor.service.impl
 
 import com.google.inject.Inject
+import ish.oncourse.model.WebSite
+import ish.oncourse.model.WebSiteVersion
 import ish.oncourse.services.persistence.ICayenneService
+import ish.oncourse.services.site.GetDeployedVersion
+import ish.oncourse.services.site.WebSitePublisher
+import ish.oncourse.services.site.WebSiteVersionsDelete
 import ish.oncourse.willow.editor.service.*
 import ish.oncourse.willow.editor.model.Version
 import ish.oncourse.willow.editor.model.api.SetVersionRequest
 
 import groovy.transform.CompileStatic
 import ish.oncourse.willow.editor.services.RequestService
+import ish.oncourse.willow.editor.services.access.AuthenticationService
+import ish.oncourse.willow.editor.website.WebSiteFunctions
+import ish.oncourse.willow.editor.website.WebSiteVersionFunctions
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.eclipse.jetty.server.Request
+
+import javax.ws.rs.ClientErrorException
+import javax.ws.rs.InternalServerErrorException
+import javax.ws.rs.NotAllowedException
+import javax.ws.rs.NotSupportedException
+import javax.ws.rs.ServerErrorException
+import javax.ws.rs.core.Response
 
 @CompileStatic
 class PublishApiServiceImpl implements PublishApi {
@@ -18,29 +34,39 @@ class PublishApiServiceImpl implements PublishApi {
 
     private ICayenneService cayenneService
     private RequestService requestService
+    private AuthenticationService authenticationService
 
     @Inject
-    PublishApiServiceImpl(ICayenneService cayenneService, RequestService requestService) {
+    PublishApiServiceImpl(ICayenneService cayenneService, RequestService requestService, AuthenticationService authenticationService) {
         this.cayenneService = cayenneService
         this.requestService = requestService
+        this.authenticationService = authenticationService
+    }
+
+    void publish() {
+        Request request = requestService.request
+        WebSiteVersion draftVersion = WebSiteVersionFunctions.getCurrentVersion(request, cayenneService.newContext())
+        
+        WebSitePublisher.valueOf(draftVersion,
+                authenticationService.systemUser,
+                authenticationService.userEmail,
+                cayenneService.newContext()).publish()
+
+        //refresh draft version after publishing
+        draftVersion = WebSiteVersionFunctions.getCurrentVersion(request, cayenneService.newContext())
+
+        WebSite webSite = WebSiteFunctions.getCurrentWebSite(request, cayenneService.newContext())
+        WebSiteVersionsDelete.valueOf(webSite, draftVersion,
+                GetDeployedVersion.valueOf(cayenneService.sharedContext(), webSite, false).get(),
+                cayenneService.newContext()).delete()
     }
     
     List<Version> getVersions() {
-        // TODO: Implement...
-        
-        return null
-    }
-    
-    void publish() {
-        // TODO: Implement...
-        
-        
+        throw new ServerErrorException(Response.Status.NOT_IMPLEMENTED)
     }
     
     void setVersion(SetVersionRequest setVersionRequest) {
-        // TODO: Implement...
-        
-        
+        throw new NotAllowedException('publish')
     }
     
 }
