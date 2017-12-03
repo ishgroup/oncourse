00bcba1262543c5a08a9ec7ba3ea9598927c971d
Artyom Kravenko <artyom@ish.com.au>
2017-12-03 14:52:51 +0000

    OD-0000 Adjust unit tests
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49413 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor-api/src/test/groovy/ish/oncourse/willow/editor/service/PageApiTest.groovy
+++ editor-api/src/test/groovy/ish/oncourse/willow/editor/service/PageApiTest.groovy
@@ -47,7 +47,7 @@ class PageApiTest extends AbstractEditorTest{
         Page newPage = api.addPage()
         Assert.assertNotNull(newPage)
 
-        api.deletePage(newPage.number.toString())
+        api.deletePage(newPage.number.toInteger().toString())
         Integer intNumber = newPage.number.toInteger()
 
         WebNode node = WebNodeFunctions.getNodeForNumber(intNumber, requestService.request, cayenneService.newContext())
