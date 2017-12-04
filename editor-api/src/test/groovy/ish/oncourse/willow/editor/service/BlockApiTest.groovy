5596d7c7544f62b8402ae1c233e86a401eb1e251
Artyom Kravenko <artyom@ish.com.au>
2017-12-04 06:57:56 +0000

    OD-10850 Cover bocks api by unit tests
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49415 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor-api/src/test/groovy/ish/oncourse/willow/editor/service/BlockApiTest.groovy
+++ editor-api/src/test/groovy/ish/oncourse/willow/editor/service/BlockApiTest.groovy
@@ -1,17 +1,20 @@
-package ish.oncourse.willow.editor.service;
+package ish.oncourse.willow.editor.service
 
-import ish.oncourse.willow.editor.model.Block;
+import ish.oncourse.model.WebContent
+import ish.oncourse.model.WebSite
+import ish.oncourse.model.WebSiteVersion
+import ish.oncourse.willow.editor.model.Block
 import ish.oncourse.willow.editor.model.common.CommonError
 import ish.oncourse.willow.editor.service.impl.BlockApiServiceImpl
-import org.junit.Assert
-import org.junit.Test;
-import org.junit.Before;
-import static org.junit.Assert.*;
+import ish.oncourse.willow.editor.website.WebSiteVersionFunctions
+import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.query.ObjectSelect
+import org.junit.Test
+import org.junit.Before
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.ws.rs.ClientErrorException
+
+import static org.junit.Assert.*
 
 /**
  * API tests for BlockApi
@@ -46,11 +49,28 @@ class BlockApiTest extends AbstractEditorTest{
      */
     @Test
     void deleteBlockTest() {
-	//api.deleteBlock();
-        
-        // TODO: test validations
-        
-        
+        try {
+            api.deleteBlock('New block (1)')
+        } catch (ClientErrorException e) {
+            assertTrue(e.response.entity instanceof CommonError)
+            assertEquals('The block (name: New block (1)) could not be removed', (e.response.entity as CommonError).message)
+        }
+
+        ObjectContext context = cayenneService.newContext()
+        WebContent webContent = context.newObject(WebContent)
+        webContent.name = 'New block (100)'
+        webContent.webSiteVersion =  WebSiteVersionFunctions.getCurrentVersion(requestService.request,context)
+        webContent.content = 'Content'
+        webContent.contentTextile = 'Content'
+        context.commitChanges()
+        WebContent block = ObjectSelect.query(WebContent).where(WebContent.NAME.eq('New block (100)')).selectOne(cayenneService.newContext())
+        assertNotNull(block)
+
+        api.deleteBlock('New block (100)')
+
+        block = ObjectSelect.query(WebContent).where(WebContent.NAME.eq('New block (100)')).selectOne(cayenneService.newContext())
+        assertNull(block)
+
     }
     
     /**
@@ -72,12 +92,49 @@ class BlockApiTest extends AbstractEditorTest{
      */
     @Test
     void saveBlockTest() {
-        Block saveBlockRequest = null;
-	//Block response = api.saveBlock(saveBlockRequest);
-        //assertNotNull(response);
-        // TODO: test validations
+        WebContent block = ObjectSelect.query(WebContent).where(WebContent.NAME.eq('New block (1)')).selectOne(cayenneService.newContext())
         
+        api.saveBlock(new Block().with { b ->
+            b.id = block.id.doubleValue()
+            b.title = 'changed name'
+            b.content = 'changed content'
+            b
+        })
         
+        block = ObjectSelect.query(WebContent).where(WebContent.NAME.eq('New block (1)')).selectOne(cayenneService.newContext())
+        assertNull(block)
+        block = ObjectSelect.query(WebContent).where(WebContent.NAME.eq('changed name')).selectOne(cayenneService.newContext())
+        assertNotNull(block)
+        assertEquals('changed content', block.content)
+    }
+
+    /**
+     * try to change block for other site
+     * Save block params
+     */
+    @Test
+    void crossSiteRequests() {
+        //create structure for additional web site
+        AbstractEditorTest.CreateDefaultWebSiteStructure.valueOf('other', cayenneService.newContext()).create()
+
+        WebContent block = (ObjectSelect.query(WebContent).where(WebContent.NAME.eq('New block (1)')) & WebContent.WEB_SITE_VERSION.dot(WebSiteVersion.WEB_SITE).dot(WebSite.SITE_KEY).eq('other'))
+                .selectOne(cayenneService.newContext())
+        
+        assertNotNull(block)
+        assertEquals(2L, block.webSiteVersion.webSite.id)
+        
+        Block blockRequest = new Block().with { b ->
+            b.id = block.id.doubleValue()
+            b.title = 'changed name'
+            b.content = 'changed content'
+            b
+        }
+        try {
+            api.saveBlock(blockRequest)
+        } catch (ClientErrorException e) {
+            assertTrue(e.response.entity instanceof CommonError)
+            assertEquals("There are no block for blockParams: $blockRequest".toString(), (e.response.entity as CommonError).message)
+        }
     }
     
 }
