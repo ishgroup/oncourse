6337e3efbd911a460266585fa2fe0e711455870a
Alex <alex@ish.com.au>
2018-01-10 13:43:19 +0000

    OD-10214 adjusted methods, which creates classes with sessions
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@49949 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithSessionTest.groovy
+++ common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithSessionTest.groovy
@@ -43,26 +43,17 @@ class SolrCourseClassQueryWithSessionTest extends ASolrTest{
     void testSortClassesWithSessionsAndDistantLearning() throws IOException, SolrServerException {
         SolrClient solrClient = new EmbeddedSolrServer(h.getCore())
 
-        cCollege.newCourse("course1").newCourseClass("past")
-                .withSession(-5).withSession(-4).build()
-        cCollege.newCourse("course2").newCourseClass("pastStartsFirst")
-                .withSession( -6).withSession( -4).build()
-        cCollege.newCourse("course3").newCourseClass("pastEndsLast")
-                .withSession( -5).withSession(-3).build()
+        cCollege.newCourse("course1").newCourseClassWithSessions("past", -5, -4).build()
+        cCollege.newCourse("course2").newCourseClassWithSessions("pastStartsFirst", -6, -4).build()
+        cCollege.newCourse("course3").newCourseClassWithSessions("pastEndsLast", -5, -3).build()
         
-        cCollege.newCourse("course4").newCourseClass("current")
-                .withSession(-1).withSession(1).build()
-        cCollege.newCourse("course5").newCourseClass("currentStartsFirst")
-                .withSession(-2).withSession(1).build()
-        cCollege.newCourse("course6").newCourseClass("currentEndsLast")
-                .withSession(-1).withSession(2).build()
+        cCollege.newCourse("course4").newCourseClassWithSessions("current", -1, 1).build()
+        cCollege.newCourse("course5").newCourseClassWithSessions("currentStartsFirst", -2, 1).build()
+        cCollege.newCourse("course6").newCourseClassWithSessions("currentEndsLast", -1, 2).build()
         
-        cCollege.newCourse("course7").newCourseClass("future")
-                .withSession(5).withSession(6).build()
-        cCollege.newCourse("course8").newCourseClass("futureStartsFirst")
-                .withSession(4).withSession(6).build()
-        cCollege.newCourse("course9").newCourseClass("futureEndsLast")
-                .withSession(5).withSession(7).build()
+        cCollege.newCourse("course7").newCourseClassWithSessions("future", 5, 6).build()
+        cCollege.newCourse("course8").newCourseClassWithSessions("futureStartsFirst", 4, 6).build()
+        cCollege.newCourse("course9").newCourseClassWithSessions("futureEndsLast", 5, 7).build()
 
         cCollege.newCourse("course10").newCourseClass("distantLearning").isDistantLearningCourse(true).build()
                 
@@ -73,32 +64,32 @@ class SolrCourseClassQueryWithSessionTest extends ASolrTest{
             Thread.sleep(100)
         }
 
-        List<SCourse> actualSClasses = solrClient.query("courses",
+        List<SCourse> actualSCourses = solrClient.query("courses",
                 SolrQueryBuilder.valueOf(new SearchParams(s: "course*"), cCollege.college.id.toString(), null, null).build())
                 .getBeans(SCourse.class)
-        assertEquals(10, actualSClasses.size())
+        assertEquals(10, actualSCourses.size())
 
-        List<SCourse> currentClasses = actualSClasses.subList(0, 3)
-        List<SCourse> futureClasses = actualSClasses.subList(4, 7)
-        List<SCourse> pastClasses = actualSClasses.subList(7, 10)
+        List<SCourse> currentCourses = actualSCourses.subList(0, 3)
+        List<SCourse> futureCourses = actualSCourses.subList(4, 7)
+        List<SCourse> pastCourses = actualSCourses.subList(7, 10)
         
         //first group of courses will be with current classes. First one will be the class, which starts first. 
-        assertEquals("course5", currentClasses.first().name)
-        assertNotNull(currentClasses.find {c -> c.name == "course4" })
-        assertNotNull(currentClasses.find {c -> c.name == "course6" })
+        assertEquals("course5", currentCourses.first().name)
+        assertNotNull(currentCourses.find {c -> c.name == "course4" })
+        assertNotNull(currentCourses.find {c -> c.name == "course6" })
 
         //distant learning classes are going between current and future classes
-        assertEquals("course10", actualSClasses.get(3).name)
+        assertEquals("course10", actualSCourses.get(3).name)
         
         //second group of courses will be with future classes. First one will be the class, which starts first. 
-        assertEquals("course8", futureClasses.first().name)
-        assertNotNull(futureClasses.find {c -> c.name == "course7" })
-        assertNotNull(futureClasses.find {c -> c.name == "course9" })
+        assertEquals("course8", futureCourses.first().name)
+        assertNotNull(futureCourses.find {c -> c.name == "course7" })
+        assertNotNull(futureCourses.find {c -> c.name == "course9" })
 
         //last group of courses will be with past classes. sort order isn't set for past classes.
-        assertNotNull(pastClasses.find {c -> c.name == "course1" })
-        assertNotNull(pastClasses.find {c -> c.name == "course2" })
-        assertNotNull(pastClasses.find {c -> c.name == "course3" })
+        assertNotNull(pastCourses.find {c -> c.name == "course1" })
+        assertNotNull(pastCourses.find {c -> c.name == "course2" })
+        assertNotNull(pastCourses.find {c -> c.name == "course3" })
         
         solrClient.close()
     }
