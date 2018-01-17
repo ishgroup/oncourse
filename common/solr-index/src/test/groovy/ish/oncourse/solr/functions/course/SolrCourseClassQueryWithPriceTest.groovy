9afa4c5cd2a0bad3a54ef04d57c7b854d3afc465
Alex <alex@ish.com.au>
2018-01-17 11:01:37 +0000

    OD-10214 added '..Test' to test name
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50000 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithPriceTest.groovy
+++ common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithPriceTest.groovy
@@ -20,7 +20,7 @@ import org.junit.Test
 /**
  * Created by alex on 1/9/18.
  */
-class SolrCourseClassQueryWithPrice  extends ASolrTest{
+class SolrCourseClassQueryWithPriceTest extends ASolrTest{
     private TestContext testContext
     private ObjectContext objectContext
     private InitSolr initSolr
