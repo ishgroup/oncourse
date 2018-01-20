a46427da02a5e96b601327bf0ab4e16526910778
Andrey Narut <anarut@ish.com.au>
2018-01-20 09:53:17 +0000

    OD-11510 Update unit test to check exactly format util method.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50027 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-functions/src/test/groovy/ish/oncourse/util/FormatUtilsTest.groovy
+++ common/common-functions/src/test/groovy/ish/oncourse/util/FormatUtilsTest.groovy
@@ -12,38 +12,34 @@ class FormatUtilsTest {
     
     private Money amount
     private String expectedResult
-    private String expectedFResult
     
     @Parameterized.Parameters
     static Collection primeNumbers() {
         [
-                [new Money('10'), '$10.00', '$10'],
-                [new Money('10.40'), '$10.40', '$10.40'],
-                [new Money('100'), '$100.00', '$100'],
-                [new Money('100.60'), '$100.60', '$100.60'],
-                [new Money('1000'), '$1,000.00', '$1,000'],
-                [new Money('1000.60'), '$1,000.60', '$1,000.60'],
-                [new Money('-10'), '($10.00)', '-$10'],
-                [new Money('-10.40'), '($10.40)', '-$10.40'],
-                [new Money('-100'), '($100.00)', '-$100'],
-                [new Money('-100.60'), '($100.60)', '-$100.60'],
-                [new Money('-1000'), '($1,000.00)', '-$1,000'],
-                [new Money('-1000.60'), '($1,000.60)', '-$1,000.60']
+                [new Money('10'), '$10'],
+                [new Money('10.40'), '$10.40'],
+                [new Money('100'), '$100'],
+                [new Money('100.60'), '$100.60'],
+                [new Money('1000'), '$1,000'],
+                [new Money('1000.60'), '$1,000.60'],
+                [new Money('-10'), '-$10'],
+                [new Money('-10.40'), '-$10.40'],
+                [new Money('-100'), '-$100'],
+                [new Money('-100.60'), '-$100.60'],
+                [new Money('-1000'), '-$1,000'],
+                [new Money('-1000.60'),'-$1,000.60']
         ]*.toArray()
     }
     
-    FormatUtilsTest(Money amount, String expectedResult, String expectedFResult) {
+    FormatUtilsTest(Money amount, String expectedResult) {
         this.amount = amount
         this.expectedResult = expectedResult
-        this.expectedFResult = expectedFResult
     }
 
     @Test
     void testMoneyFormat() {
-        assertEquals(expectedResult, amount.toString())
-        
         def formatter = FormatUtils.chooseMoneyFormat(amount)
         def result = formatter.format(amount)
-        assertEquals(expectedFResult, result)
+        assertEquals(expectedResult, result)
     }
 }
