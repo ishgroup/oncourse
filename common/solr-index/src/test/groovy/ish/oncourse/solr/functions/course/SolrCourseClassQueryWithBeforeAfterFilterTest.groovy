736ce63e2ebce145d3c7930dd62e6ebc6f260427
Alex <alex@ish.com.au>
2018-01-24 10:07:19 +0000

    OD-10214 added SolrCourseQuery test with 'before/after' filter without cases.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50071 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithBeforeAfterFilterTest.groovy
@@ -0,0 +1,66 @@
+package ish.oncourse.solr.functions.course
+
+import io.reactivex.schedulers.Schedulers
+import ish.oncourse.solr.ASolrTest
+import ish.oncourse.solr.InitSolr
+import ish.oncourse.solr.model.SCourse
+import ish.oncourse.solr.query.SearchParams
+import ish.oncourse.solr.query.SolrQueryBuilder
+import ish.oncourse.solr.reindex.ReindexCoursesJob
+import ish.oncourse.test.TestContext
+import ish.oncourse.test.context.CCollege
+import ish.oncourse.test.context.DataContext
+import org.apache.cayenne.ObjectContext
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by alex on 1/24/18.
+ */
+class SolrCourseClassQueryWithBeforeAfterFilterTest extends ASolrTest {
+    private TestContext testContext
+    private ObjectContext objectContext
+    private InitSolr initSolr
+    private CCollege cCollege
+
+    @Before
+    void before() throws Exception {
+        initSolr = InitSolr.coursesCore()
+        initSolr.init()
+
+        testContext = new TestContext()
+        testContext.open()
+        objectContext = testContext.getServerRuntime().newContext()
+        DataContext dataContext = new DataContext(objectContext: objectContext)
+        cCollege = dataContext.newCollege()
+    }
+
+    @Test
+    void testSortCoursesWithDeforeAfterFilter() {
+        SolrClient solrClient = new EmbeddedSolrServer(h.getCore())
+
+        ReindexCoursesJob job = new ReindexCoursesJob(objectContext, solrClient)
+        job.run()
+        while (job.isActive()) {
+            Thread.sleep(100)
+        }
+
+        List<SCourse> actualSCourses = solrClient.query("courses",
+                SolrQueryBuilder.valueOf(new SearchParams(s: "course*", after: new Date(), before: new Date() + 7), cCollege.college.id.toString(), null, null).build())
+                .getBeans(SCourse.class)
+        assertEquals(2, actualSCourses.size())
+
+    }
+
+    @After
+    void after() {
+        Schedulers.shutdown()
+
+        // Can't drop DB cause 2 mariaDB threads is still working.
+        // TODO: define mariaDB daemon threads and shut them down
+        testContext.close(false)
+    }
+}
