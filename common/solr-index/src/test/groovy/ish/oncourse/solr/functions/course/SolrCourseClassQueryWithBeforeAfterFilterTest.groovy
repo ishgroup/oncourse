4fc4ffef3074ef0a0e03e14e81505f7ea53820b3
Alex <alex@ish.com.au>
2018-01-26 11:30:40 +0000

    OD-10214 added cases to 'testSortCoursesWithBeforeAfterFilter' test
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50088 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithBeforeAfterFilterTest.groovy
+++ common/solr-index/src/test/groovy/ish/oncourse/solr/functions/course/SolrCourseClassQueryWithBeforeAfterFilterTest.groovy
@@ -39,8 +39,30 @@ class SolrCourseClassQueryWithBeforeAfterFilterTest extends ASolrTest {
     }
 
     @Test
-    void testSortCoursesWithDeforeAfterFilter() {
+    void testSortCoursesWithBeforeAfterFilter() {
         SolrClient solrClient = new EmbeddedSolrServer(h.getCore())
+
+        String collegeId = cCollege.college.id.toString()
+        List<SCourse> actualSCourses
+        
+        Calendar checkDate = Calendar.instance
+        checkDate.set(Calendar.HOUR_OF_DAY, 0)
+        checkDate.set(Calendar.MINUTE, 0)
+        checkDate.set(Calendar.SECOND, 0)
+
+        cCollege.newCourse("course1").newCourseClassWithSessions("past", -5, -4).build()
+        cCollege.newCourse("course2").newCourseClassWithSessions("pastStartsFirst", -6, -4).build()
+        cCollege.newCourse("course3").newCourseClassWithSessions("pastEndsLast", -5, -3).build()
+
+        cCollege.newCourse("course4").newCourseClassWithSessions("current", -1, 1).build()
+        cCollege.newCourse("course5").newCourseClassWithSessions("currentStartsFirst", -2, 1).build()
+        cCollege.newCourse("course6").newCourseClassWithSessions("currentEndsLast", -1, 2).build()
+
+        cCollege.newCourse("course7").newCourseClassWithSessions("future", 5, 6).build()
+        cCollege.newCourse("course8").newCourseClassWithSessions("futureStartsFirst", 4, 6).build()
+        cCollege.newCourse("course9").newCourseClassWithSessions("futureEndsLast", 5, 7).build()
+
+        cCollege.newCourse("course10").withSelfPacedClass("distantLearning").build()
 
         ReindexCoursesJob job = new ReindexCoursesJob(objectContext, solrClient)
         job.run()
@@ -48,11 +70,47 @@ class SolrCourseClassQueryWithBeforeAfterFilterTest extends ASolrTest {
             Thread.sleep(100)
         }
 
-        List<SCourse> actualSCourses = solrClient.query("courses",
-                SolrQueryBuilder.valueOf(new SearchParams(s: "course*", after: new Date(), before: new Date() + 7), cCollege.college.id.toString(), null, null).build())
+        //if we use 'before' filter - past classes doesn't selected by solr query because they have startDate = now + 100 years
+        actualSCourses = solrClient.query("courses",
+                SolrQueryBuilder.valueOf(new SearchParams(s: "course*", before: checkDate.time + 10, after: new Date() - 10), collegeId, null, null).build())
                 .getBeans(SCourse.class)
-        assertEquals(2, actualSCourses.size())
+        assertEquals(7, actualSCourses.size())
+        assertEquals("course5", actualSCourses.first().name)
+        assertNotNull(actualSCourses.find {c -> c.name == "course4" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course6" })
+        assertEquals("course10", actualSCourses.get(3).name)
+        assertEquals("course8", actualSCourses.get(4).name)
+        assertNotNull(actualSCourses.find {c -> c.name == "course7" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course9" })
 
+        //'before' date filter EXCLUDE classes, which started on filtered date. 
+        actualSCourses = solrClient.query("courses",
+                SolrQueryBuilder.valueOf(new SearchParams(s: "course*", before: checkDate.time + 5), collegeId, null, null).build())
+                .getBeans(SCourse.class)
+        assertEquals(5, actualSCourses.size())
+        assertEquals("course5", actualSCourses.first().name)
+        assertNotNull(actualSCourses.find {c -> c.name == "course4" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course6" })
+        assertEquals("course10", actualSCourses.get(3).name)
+        assertEquals("course8", actualSCourses.last().name)
+
+        //'after' filter with value = 2+ days won't select selfpaced (distant learning) classes, cause that classes are getting 'classStart' date = tomorrow
+        actualSCourses = solrClient.query("courses",
+                SolrQueryBuilder.valueOf(new SearchParams(s: "course*", after: checkDate.time + 2), collegeId, null, null).build())
+                .getBeans(SCourse.class)
+        assertEquals(6, actualSCourses.size())
+        assertEquals("course8", actualSCourses.first().name)
+        assertNotNull(actualSCourses.find {c -> c.name == "course7" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course9" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course1" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course2" })
+        assertNotNull(actualSCourses.find {c -> c.name == "course3" })
+        
+        //when we try to select classes 'before' past date, we can get only current classes (if they have started before past date). No selfpased, no past classes.
+        actualSCourses = solrClient.query("courses",
+                SolrQueryBuilder.valueOf(new SearchParams(s: "course*", before: checkDate.time - 3), collegeId, null, null).build())
+                .getBeans(SCourse.class)
+        assertTrue(actualSCourses.isEmpty())
     }
 
     @After
