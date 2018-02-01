a8c95b481df979f2933c5b5c547c6172bff6c840
Artyom Kravenko <artyom@ish.com.au>
2018-02-01 14:51:05 +0000

    OD-11501 make search request in parallel thread, shout it down by timeout after 1 sec to release bootique  thread
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50169 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout-api/src/main/groovy/ish/oncourse/willow/search/SearchService.groovy
+++ checkout-api/src/main/groovy/ish/oncourse/willow/search/SearchService.groovy
@@ -11,6 +11,11 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import javax.ws.rs.InternalServerErrorException
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 
 import static ish.oncourse.configuration.Configuration.AppProperty.ZK_HOST
 
@@ -19,8 +24,8 @@ class SearchService {
     private static final String SOLR_SYNTAX_CHARACTERS_STRING = '[\\!\\^\\(\\)\\{\\}\\[\\]\\:\"\\?\\+\\~\\*\\|\\&\\;\\\\]'
 
     private static final int ZK_TIMEOUT = 1000
-    
-    final static  Logger logger = LoggerFactory.getLogger(SearchService.class)
+
+    final static Logger logger = LoggerFactory.getLogger(SearchService.class)
 
     private String zkHost
 
@@ -33,52 +38,64 @@ class SearchService {
     }
 
 
-    private List<Item> searchSuburbs(String qualifier, String value) {
+    private QueryResponse searchSuburbs(String qualifier, String value) {
         CloudSolrClient client
+        ExecutorService executorService = Executors.newCachedThreadPool()
+
         try {
             value = normalizeString(value)
-            List<Item> result = []
-
+            
             SolrQuery q = new SolrQuery()
             q.query = "doctype:suburb AND $qualifier:$value*"
-            
-            long time = System.currentTimeMillis()
-            client = new CloudSolrClient.Builder().withZkHost(zkHost).build()
-            client.setZkClientTimeout(ZK_TIMEOUT)
-            client.setZkConnectTimeout(ZK_TIMEOUT)
-            
-            QueryResponse response = client.query('suburbs', q)
-            logger.info("Query finished. Time: '{}' ms", (System.currentTimeMillis() - time))
-            
-            response.results.each { doc ->
-                result << new Item().with { i ->
-                    String postcode = doc.getFieldValue('postcode') as String
-                    String state = doc.getFieldValue('state') as String
-                    String suburb = doc.getFieldValue('suburb') as String
-                    i.key = postcode
-                    i.value = new Suburb(postcode:postcode, state: state, suburb: suburb)
-                    i
-                }
-            }
 
-            result
-            
+            long time = System.currentTimeMillis()
+           
+
+            Future<QueryResponse> searchFuture = executorService.submit({
+                client = new CloudSolrClient.Builder().withZkHost(zkHost).build()
+                client.setZkClientTimeout(ZK_TIMEOUT)
+                client.setZkConnectTimeout(ZK_TIMEOUT)
+                return client.query('suburbs', q)
+            } as Callable<QueryResponse>)
+            QueryResponse response = searchFuture.get(ZK_TIMEOUT, TimeUnit.MILLISECONDS)
+
+            logger.info("Query finished. Time: '{}' ms", (System.currentTimeMillis() - time))
+            response
+
         } catch (Exception e) {
             logger.error("Failed to search suburbs.", e)
             throw new InternalServerErrorException(e)
         } finally {
+            executorService.shutdown()
             if (client) {
                 client.close()
             }
         }
     }
-    
+
     List<Item> searchSuburbsByName(String term) {
-        searchSuburbs('suburb', term)
+        serealize(searchSuburbs('suburb', term))
     }
 
-    List<Item> searchSuburbsByPostcode(String term) { 
-        searchSuburbs('postcode', term)
+    List<Item> searchSuburbsByPostcode(String term) {
+        serealize(searchSuburbs('postcode', term))
+    }
+
+    List<Item> serealize(QueryResponse response) {
+        List<Item> result = []
+        
+        response.results.each {
+            doc ->
+            result << new Item().with { i ->
+                String postcode = doc.getFieldValue('postcode') as String
+                String state = doc.getFieldValue('state') as String
+                String suburb = doc.getFieldValue('suburb') as String
+                i.key = postcode
+                i.value = new Suburb(postcode: postcode, state: state, suburb: suburb)
+                i
+            }
+        }
+        result
     }
 
     static String normalizeString(String original) {
