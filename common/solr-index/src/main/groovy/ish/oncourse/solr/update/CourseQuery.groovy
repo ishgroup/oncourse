965bf825d992c0dddefce3c3e77ace89d01c59fa
Alex <alex@ish.com.au>
2018-02-06 14:59:29 +0000

    OD-10214 added Course queries by CourseClass, Session, Room and Site
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50216 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/solr-index/src/main/groovy/ish/oncourse/solr/update/CourseQuery.groovy
+++ common/solr-index/src/main/groovy/ish/oncourse/solr/update/CourseQuery.groovy
@@ -1,9 +1,7 @@
 package ish.oncourse.solr.update
 
 import ish.oncourse.model.*
-import org.apache.cayenne.exp.Expression
 import org.apache.cayenne.query.ObjectSelect
-
 
 import static org.apache.cayenne.query.ObjectSelect.query
 
@@ -11,31 +9,39 @@ import static org.apache.cayenne.query.ObjectSelect.query
  * Created by alex on 2/6/18.
  */
 class CourseQuery {    
-    private static Expression courseWhereExpression() {
-        return Course.IS_WEB_VISIBLE.isTrue()
-    }
-
-    private static Expression classWhereExpression() {
-        return CourseClass.IS_WEB_VISIBLE.eq(true)
-                .andExp(CourseClass.CANCELLED.eq(false))
-                .andExp(CourseClass.END_DATE.gt(new Date()).andExp(CourseClass.IS_DISTANT_LEARNING_COURSE.eq(false)).orExp(CourseClass.IS_DISTANT_LEARNING_COURSE.eq(true)))
-    }
-
     static final ObjectSelect<Course> coursesToReindexQuery(Contact contact){
-        query(Course).where(courseWhereExpression())
-                .and(classWhereExpression())
-                .and(Course.COURSE_CLASSES.dot(CourseClass.TUTOR_ROLES).dot(TutorRole.TUTOR).dot(Tutor.CONTACT).eq(contact))
+        query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) & 
+                Course.COURSE_CLASSES.dot(CourseClass.TUTOR_ROLES).dot(TutorRole.TUTOR).dot(Tutor.CONTACT).eq(contact)
     }
 
     static final ObjectSelect<Course> coursesToReindexQuery(Tutor tutor){
-        query(Course).where(courseWhereExpression())
-                .and(classWhereExpression())
-                .and(Course.COURSE_CLASSES.dot(CourseClass.TUTOR_ROLES).dot(TutorRole.TUTOR).eq(tutor))
+        query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) & 
+                Course.COURSE_CLASSES.dot(CourseClass.TUTOR_ROLES).dot(TutorRole.TUTOR).eq(tutor)
     }
 
     static final ObjectSelect<Course> coursesToReindexQuery(TutorRole tutorRole){
-        query(Course).where(courseWhereExpression())
-                .and(classWhereExpression())
-                .and(Course.COURSE_CLASSES.dot(CourseClass.TUTOR_ROLES).dot(TutorRole.ID_PK_COLUMN).eq(tutorRole.id))
+        query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) & 
+                Course.COURSE_CLASSES.dot(CourseClass.TUTOR_ROLES).dot(TutorRole.ID_PK_COLUMN).eq(tutorRole)
+    }
+
+    static final ObjectSelect<Course> coursesToReindexQuery(CourseClass courseClass){
+        query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) & 
+                Course.COURSE_CLASSES.dot(CourseClass.ID_PK_COLUMN).eq(courseClass)
+    }
+    
+    static final ObjectSelect<Course> coursesToReindexQuery(Session session){
+        query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) &
+                Course.COURSE_CLASSES.dot(CourseClass.SESSIONS).dot(Session.ID_PK_COLUMN).eq(session)
+    }
+
+    static final ObjectSelect<Course> coursesToReindexQuery(Site site){
+        (query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) &
+                Site.IS_WEB_VISIBLE.isTrue()) & 
+                Course.COURSE_CLASSES.dot(CourseClass.SESSIONS).dot(Session.ROOM).dot(Room.SITE).eq(site).orExp(Course.COURSE_CLASSES.dot(CourseClass.ROOM).dot(Room.SITE).eq(site))
+    }
+
+    static final ObjectSelect<Course> coursesToReindexQuery(Room room){
+        query(Course).where(Course.IS_WEB_VISIBLE.isTrue()) & 
+                Course.COURSE_CLASSES.dot(CourseClass.SESSIONS).dot(Session.ROOM).eq(room).orExp(Course.COURSE_CLASSES.dot(CourseClass.ROOM).eq(room))
     }
 }
