87aadd0a59f08680e82dda8871ed4d44211e3e74
Andrey Davidovich <ancreo@ish.com.au>
2018-02-08 15:19:37 +0000

    OD-10852 fix set version request
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50272 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor/src/js/services/PublishService.ts
+++ editor/src/js/services/PublishService.ts
@@ -3,19 +3,19 @@ import {PublishApi} from "../http/PublishApi";
 import {DefaultHttpService} from "../common/services/HttpService";
 
 class PublishService {
-    readonly publishApi = new PublishApi(new DefaultHttpService());
+  readonly publishApi = new PublishApi(new DefaultHttpService());
 
-    public getVersions(): Promise<Version[]> {
-        return this.publishApi.getVersions();
-    }
+  public getVersions(): Promise<Version[]> {
+    return this.publishApi.getVersions();
+  }
 
-    public setVersion(id): Promise<any> {
-        return this.publishApi.setVersion(id);
-    }
+  public setVersion(id): Promise<any> {
+    return this.publishApi.setVersion({id});
+  }
 
-    public publish(): Promise<any> {
-        return this.publishApi.publish();
-    }
+  public publish(): Promise<any> {
+    return this.publishApi.publish();
+  }
 }
 
 export default new PublishService();
