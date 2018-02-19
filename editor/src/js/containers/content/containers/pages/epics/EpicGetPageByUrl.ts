7f22c44413f225c0910a4e9170fe90860e624de3
Andrey Davidovich <ancreo@ish.com.au>
2018-02-19 08:55:54 +0000

    OD-11593 fix get page by url epic
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50429 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor/src/js/containers/content/containers/pages/epics/EpicGetPageByUrl.ts
+++ editor/src/js/containers/content/containers/pages/epics/EpicGetPageByUrl.ts
@@ -6,6 +6,7 @@ import {GET_PAGE_BY_URL_FULFILLED, GET_PAGE_BY_URL_REQUEST} from "../actions";
 import {Page} from "../../../../../model";
 import PageService from "../../../../../services/PageService";
 import {getHistoryInstance} from "../../../../../history";
+import {LOG_OUT_REQUEST} from "../../../../auth/actions/index";
 
 const request: EpicUtils.Request<any, any> = {
   type: GET_PAGE_BY_URL_REQUEST,
@@ -27,6 +28,10 @@ const request: EpicUtils.Request<any, any> = {
 
     if (data.status && 403 === data.status) {
       return [];
+    }
+
+    if (data.status && 401 === data.status) {
+      return [{type: LOG_OUT_REQUEST}];
     }
 
     return [
