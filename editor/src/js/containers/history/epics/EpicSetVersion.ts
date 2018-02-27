151e5a38c6aa4b2589e369c3872cc97afcaf9576
Andrey Davidovich <ancreo@ish.com.au>
2018-02-27 06:53:51 +0000

    OD-11627 refresh version after changes
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50562 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor/src/js/containers/history/epics/EpicSetVersion.ts
+++ editor/src/js/containers/history/epics/EpicSetVersion.ts
@@ -2,7 +2,7 @@ import {Epic} from "redux-observable";
 import {success} from 'react-notification-system-redux';
 import "rxjs";
 import * as EpicUtils from "../../../epics/EpicUtils";
-import {SET_VERSION_FULFILLED, SET_VERSION_REQUEST} from "../actions";
+import {getHistory, SET_VERSION_FULFILLED, SET_VERSION_REQUEST} from "../actions";
 import PublishService from "../../../services/PublishService";
 import {notificationParams} from "../../../common/utils/NotificationSettings";
 
@@ -16,6 +16,7 @@ const request: EpicUtils.Request<any, any> = {
         type: SET_VERSION_FULFILLED,
         payload: response,
       },
+      getHistory(),
     ];
   },
 };
