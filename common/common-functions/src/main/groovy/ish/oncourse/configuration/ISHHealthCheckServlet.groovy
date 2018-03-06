ac710df87d08e7e09570102720b4df2680e8bad1
Andrey Koyro <andrey.koyro@objectstyle.com>
2018-03-06 15:23:08 +0000

    OD-11650 Adjust ISHHealthCheckServlet servlet in a way to load application version value on init of this servlet. Application version can be changed only after restart the whole application.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50715 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-functions/src/main/groovy/ish/oncourse/configuration/ISHHealthCheckServlet.groovy
+++ common/common-functions/src/main/groovy/ish/oncourse/configuration/ISHHealthCheckServlet.groovy
@@ -1,7 +1,10 @@
 package ish.oncourse.configuration
 
 import groovy.servlet.GroovyServlet
+import org.apache.commons.lang3.StringUtils
 
+import javax.servlet.ServletConfig
+import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -11,10 +14,18 @@ class ISHHealthCheckServlet extends GroovyServlet {
     public static final String ISH_HEALTH_CHECK_PATTERN = "/ISHHealthCheck"
     public static final Set<String> urlPatterns =  Collections.singleton(ISH_HEALTH_CHECK_PATTERN)
     public static final String SERVLET_NAME = "ISHHealthCheck"
-    
+
+    private String appVersion = StringUtils.EMPTY
+
+    @Override
+    void init(ServletConfig config) throws ServletException {
+        appVersion = ApplicationUtils.getAppVersion()
+        super.init(config)
+    }
+
     @Override
     void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.writer.print(ApplicationUtils.getAppVersion())
+        response.writer.print(appVersion)
     }
        
 }
