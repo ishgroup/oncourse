f436898cc2fca6248d52cf0a6e23aec046f23085
Andrey Koyro <andrey.koyro@objectstyle.com>
2018-03-06 15:51:03 +0000

    OD-11650 Any open file Reader should be closed.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50716 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-functions/src/main/groovy/ish/oncourse/configuration/ApplicationUtils.groovy
+++ common/common-functions/src/main/groovy/ish/oncourse/configuration/ApplicationUtils.groovy
@@ -1,17 +1,32 @@
 package ish.oncourse.configuration
 
+import org.apache.commons.io.IOUtils
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
 class ApplicationUtils {
+    private static final Logger logger = LogManager.getLogger()
+
     static final String VERSION_DEVELOPMENT = 'DEVELOPMENT'
     static final String VERSION_FILE_NAME = 'VERSION'
-    
+
     static String getAppVersion() {
         String userDir = System.getProperties().get('user.dir') as String
-        File versionFile = new File( "${userDir}/${VERSION_FILE_NAME}")
-
+        File versionFile = new File("${userDir}/${VERSION_FILE_NAME}")
         if (versionFile.exists()) {
-           return versionFile.newReader().readLine()
+            Reader reader = null
+            try {
+                reader = versionFile.newReader()
+                return reader.readLine()
+            } catch (Throwable e) {
+                logger.error(e)
+                return VERSION_DEVELOPMENT
+            } finally {
+                IOUtils.closeQuietly(reader)
+            }
         } else {
             return VERSION_DEVELOPMENT
         }
+
     }
 }
