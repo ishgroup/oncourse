37a8cd2696d386ad1cf9a7cd3e7494da06c205e2
Andrey Koyro <andrey.koyro@objectstyle.com>
2018-03-12 21:01:33 +0000

    OD-11692 revert commit #50169 (OD-11501 make search request in parallel thread, shout it down by timeout after 1 sec to release bootique thread) because best practices of using  Zookeeper advices to use one Zookeeper client per service, moreover Zookeeper server restricts amount of clients
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50760 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout-api/src/main/groovy/ish/oncourse/willow/search/SearchService.groovy
+++ checkout-api/src/main/groovy/ish/oncourse/willow/search/SearchService.groovy
@@ -11,89 +11,76 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import javax.ws.rs.InternalServerErrorException
-import java.util.concurrent.Callable
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
 
 import static ish.oncourse.configuration.Configuration.AppProperty.ZK_HOST
 
 class SearchService {
 
-    private static final String SOLR_SYNTAX_CHARACTERS_STRING = '[\\!\\^\\(\\)\\{\\}\\[\\]\\:\"\\?\\+\\~\\*\\|\\&\\;\\\\]'
+    private static
+    final String SOLR_SYNTAX_CHARACTERS_STRING = '[\\!\\^\\(\\)\\{\\}\\[\\]\\:\"\\?\\+\\~\\*\\|\\&\\;\\\\]'
 
-    private static final int ZK_TIMEOUT = 1000
+    private static final int ZK_TIMEOUT = 3000
 
     final static Logger logger = LoggerFactory.getLogger(SearchService.class)
 
     private String zkHost
+    private CloudSolrClient client
 
-    @Inject
-    SearchService() {
-        zkHost = Configuration.getValue(ZK_HOST)
+    SearchService(String zkHost) {
         if (zkHost == null) {
             throw new IllegalStateException('Zookeeper host property undefined')
         }
+        this.zkHost = zkHost
+        this.client = new CloudSolrClient.Builder().withZkHost(zkHost).build()
+        client.setZkClientTimeout(ZK_TIMEOUT)
+        client.setZkConnectTimeout(ZK_TIMEOUT)
+    }
+
+    @Inject
+    SearchService() {
+        this(Configuration.getValue(ZK_HOST))
     }
 
 
     private QueryResponse searchSuburbs(String qualifier, String value) {
-        CloudSolrClient client
-        ExecutorService executorService = Executors.newCachedThreadPool()
-
         try {
+            logger.warn(value)
             value = normalizeString(value)
-            
+
             SolrQuery q = new SolrQuery()
             q.query = "doctype:suburb AND $qualifier:$value*"
 
             long time = System.currentTimeMillis()
-           
-
-            Future<QueryResponse> searchFuture = executorService.submit({
-                client = new CloudSolrClient.Builder().withZkHost(zkHost).build()
-                client.setZkClientTimeout(ZK_TIMEOUT)
-                client.setZkConnectTimeout(ZK_TIMEOUT)
-                return client.query('suburbs', q)
-            } as Callable<QueryResponse>)
-            QueryResponse response = searchFuture.get(ZK_TIMEOUT, TimeUnit.MILLISECONDS)
-
+            QueryResponse response = client.query('suburbs', q)
             logger.info("Query finished. Time: '{}' ms", (System.currentTimeMillis() - time))
             response
-
         } catch (Exception e) {
             logger.error("Failed to search suburbs.", e)
             throw new InternalServerErrorException(e)
-        } finally {
-            executorService.shutdown()
-            if (client) {
-                client.close()
-            }
         }
     }
 
     List<Item> searchSuburbsByName(String term) {
-        serealize(searchSuburbs('suburb', term))
+        serialize(searchSuburbs('suburb', term))
     }
 
     List<Item> searchSuburbsByPostcode(String term) {
-        serealize(searchSuburbs('postcode', term))
+        serialize(searchSuburbs('postcode', term))
     }
 
-    List<Item> serealize(QueryResponse response) {
+    List<Item> serialize(QueryResponse response) {
         List<Item> result = []
-        
+
         response.results.each {
             doc ->
-            result << new Item().with { i ->
-                String postcode = doc.getFieldValue('postcode') as String
-                String state = doc.getFieldValue('state') as String
-                String suburb = doc.getFieldValue('suburb') as String
-                i.key = postcode
-                i.value = new Suburb(postcode: postcode, state: state, suburb: suburb)
-                i
-            }
+                result << new Item().with { i ->
+                    String postcode = doc.getFieldValue('postcode') as String
+                    String state = doc.getFieldValue('state') as String
+                    String suburb = doc.getFieldValue('suburb') as String
+                    i.key = postcode
+                    i.value = new Suburb(postcode: postcode, state: state, suburb: suburb)
+                    i
+                }
         }
         result
     }
