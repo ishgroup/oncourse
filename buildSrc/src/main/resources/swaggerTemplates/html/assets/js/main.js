49f6c107e619084ad814d021374e552ea53f1844
Chintan Kotadia <chintan@ish.com.au>
2018-03-14 10:57:00 +0000

    [#OD-11608] Fixed popup dialog, updated stylesheet and index.mustache for editor api documentation
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50788 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ buildSrc/src/main/resources/swaggerTemplates/html/assets/js/main.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+  choose(window.location.href.toString());
+}
+
+var selectionImage;
+
+function choose(url) {
+  var f = url.split("/").slice(-1)[0].split("?")[0];
+  if(f.match(/#/g) && f.match(/#/g).length > 0) {
+    f = f.split("#")[0];
+  }
+  if(f !== "") {
+    $('div.non-sidebar').empty();
+    $('div.non-sidebar').load("operations/" + f + ".html", function(){
+      goToAnchor();
+    });
+  }
+}
+
+function goToAnchor() {
+  var doARead = $($('a')[0]).offset();
+  var anchorArr = window.location.href.toString().split("#");
+  if(anchorArr.length > 2) {
+    var anchor = anchorArr[anchorArr.length-1];
+    window.scrollTo(0,$('a[name='+anchor+']').offset().top - 80);
+  }
+}
+
+$(function() {
+  var url = window.location.href.toString();
+  $(window).bind('hashchange', function() {
+    url = window.location.href.toString();
+    choose(url);
+  });
+
+  $(document.body).on("click", ".model a", function(e){
+    e.preventDefault();
+    var model = $(this).parent().attr("data-model");
+    var parentOffset = $(this).parent().offset();
+    var encodedWord = encodeURI(model);
+
+    $.get("/models/" + encodedWord + ".html", function(data) {
+      if(!selectionImage) {
+        selectionImage = $('<div>').attr({
+          title: 'Model detail',
+          target: '_blank',
+          class: 'model-detail-popup'
+        })
+        $('#modal-container .modal-body').append(selectionImage);
+      }
+
+      selectionImage.html(data);
+      selectionImage.attr('href',url.replace('{term}',encodeURI(model)));
+
+      $('#modal-container').addClass('show').fadeIn();
+      $('body').addClass('modal-open');
+    })
+  })
+
+  $(document).on("click", "*",function(e) {
+    var target = $(e.target);
+    if (target.parents(".modal").length === 0) {
+      if(document.querySelector('.modal')) {
+        $('#modal-container').removeClass('show').fadeOut();
+        $('body').removeClass('modal-open');
+      }
+    }
+  });
+});
