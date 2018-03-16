fb618e57f7ed52f87d9c2de9b6bd9e68be50ba8b
Artyom Kravenko <artyom@ish.com.au>
2018-03-16 13:58:16 +0000

    OD-10728 Change name space for client side API.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50820 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout/src/js/services/WindowService.ts
+++ checkout/src/js/services/WindowService.ts
@@ -29,7 +29,7 @@ export class WindowService {
   static initCheckoutApi(store: Store<IshState>) {
     const w = window as any;
     
-    w.Checkout = {
+    w.ShopingCart = {
       
       addPromoCode: (code: string) => {
         store.dispatch({
