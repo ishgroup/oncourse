e2cb4e29782071c42d00db769ba6df0ccaa8b5f4
Andrey Davidovich <ancreo@ish.com.au>
2018-03-19 10:05:42 +0000

    OD-11671 fix label
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@50831 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor/src/js/containers/settings/containers/checkout/Checkout.tsx
+++ editor/src/js/containers/settings/containers/checkout/Checkout.tsx
@@ -105,7 +105,7 @@ export class Checkout extends React.Component<Props, any> {
             <div className="form-inline">
 
               <Checkbox
-                label="Collect parent or guardian details for students"
+                label="Collect parent or guardian details for students under"
                 className="inline"
                 name="collectParentDetails"
                 checked={collectParentDetails}
