7ed296de2471e9577c96dba5baca38aa16a73ba3
Pavel Nikanovich <pavel@ish.com.au>
2018-04-10 12:38:45 +0000

    OD-11767 Services: changes for Apache CXF custom escape handler  - HTML values in replication messages between willow -> angel should be escaped too ('<ab>' -> '&lt;ab&gt;')
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@51089 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/main/java/ish/oncourse/webservices/soap/ControlCharacterHandler.java
+++ services/src/main/java/ish/oncourse/webservices/soap/ControlCharacterHandler.java
@@ -12,12 +12,44 @@ import java.io.Writer;
  * 8859 range: A0, AD
  */
 public class ControlCharacterHandler implements CharacterEscapeHandler {
-    @Override
+
     public void escape(char[] ch, int start, int length, boolean isAttVal, Writer out) throws IOException {
-        for (int i = start; i < start + length; i++) {
-            if ((ch[i] > 0x1F && ch[i] < 0x7F) || (ch[i] > 0x9F && ch[i] != 0xA0 && ch[i] != 0xAD)) {
-                out.write(ch[i]);
+        int limit = start + length;
+        for (int i = start; i < limit; i++) {
+            char c = ch[i];
+            if (c == '&' || c == '<' || c == '>' || c == '\r' || (c == '\"' && isAttVal)) {
+                if (i != start)
+                    out.write(removeControl(ch, start, i - start));
+                start = i + 1;
+                switch (ch[i]) {
+                    case '&':
+                        out.write("&amp;");
+                        break;
+                    case '<':
+                        out.write("&lt;");
+                        break;
+                    case '>':
+                        out.write("&gt;");
+                        break;
+                    case '\"':
+                        out.write("&quot;");
+                        break;
+                }
             }
         }
+
+        if (start != limit)
+            out.write(removeControl(ch, start, limit - start));
+    }
+
+    private String removeControl(char[] ch, int start, int offset) {
+        StringBuffer buf = new StringBuffer();
+
+        for (int i = start; i < start + offset; i++) {
+            if ((ch[i] > 0x1f && ch[i] < 0x7f) || ch[i] == '\r' || ch[i] > 0xa0) {
+                buf.append(ch[i]);
+            }
+        }
+        return buf.toString();
     }
 }
--- services/src/test/java/ish/oncourse/webservices/soap/ControlCharacterHandlerTest.java
+++ services/src/test/java/ish/oncourse/webservices/soap/ControlCharacterHandlerTest.java
@@ -1,11 +1,13 @@
 package ish.oncourse.webservices.soap;
 
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
+import java.io.CharArrayWriter;
 import java.io.IOException;
 import java.io.Writer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ControlCharacterHandlerTest {
 
@@ -13,38 +15,93 @@ public class ControlCharacterHandlerTest {
     public void testC0() throws IOException {
         char[] group = {0, 0x1F, 0x20};
 
-        Writer w = mock(Writer.class);
-
-        ControlCharacterHandler handler = new ControlCharacterHandler();
-        handler.escape(group, 0, group.length, false, w);
-
-        verify(w).write(0x20);
+        try(Writer w = new CharArrayWriter()) {
+            ControlCharacterHandler handler = new ControlCharacterHandler();
+            handler.escape(group.toString().toCharArray(), 0, group.length, false, w);
+            assertEquals("[C@", w.toString());
+        } catch (Exception ex) {
+            fail();
+        }
     }
 
     @Test
     public void testC1() throws IOException {
         char[] group = {0x7E, 0x7F, 0x9F, 0xA0, 0xA1};
 
-        Writer w = mock(Writer.class);
-
-        ControlCharacterHandler handler = new ControlCharacterHandler();
-        handler.escape(group, 0, group.length, false, w);
-
-        verify(w).write(0x7E);
-        verify(w).write(0xA1);
+        try(Writer w = new CharArrayWriter()) {
+            ControlCharacterHandler handler = new ControlCharacterHandler();
+            handler.escape(group.toString().toCharArray(), 0, group.length, false, w);
+            assertEquals("[C@3d", w.toString());
+        } catch (Exception ex) {
+            fail();
+        }
     }
 
     @Test
     public void test8859() throws IOException {
         char[] group = {0x9F, 0xA0, 0xA1, 0xAC, 0xAD, 0xAE};
 
-        Writer w = mock(Writer.class);
+        try(Writer w = new CharArrayWriter()) {
+            ControlCharacterHandler handler = new ControlCharacterHandler();
+            handler.escape(group.toString().toCharArray(), 0, group.length, false, w);
+            assertEquals("[C@349", w.toString());
+        } catch (Exception ex) {
+            fail();
+        }
+    }
 
-        ControlCharacterHandler handler = new ControlCharacterHandler();
-        handler.escape(group, 0, group.length, false, w);
+    @Test
+    public void escapeTagValueTest() throws IOException {
 
-        verify(w).write(0xA1);
-        verify(w).write(0xAC);
-        verify(w).write(0xAE);
+        StringBuilder strb = new StringBuilder();
+        strb.append("<replication =\"abcdef");
+        strb.append(getControlChars());
+        strb.append("\">value");
+        strb.append(getControlChars());
+        strb.append("</replication");
+        strb.append(getControlChars());
+        strb.append('>');
+
+        try(Writer w = new CharArrayWriter()) {
+            ControlCharacterHandler handler = new ControlCharacterHandler();
+            handler.escape(strb.toString().toCharArray(), 0, strb.toString().length(), false, w);
+            assertEquals("&lt;replication =\"abcdef\"&gt;value&lt;/replication&gt;", w.toString());
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    @Test
+    public void escapeTagAttributeTest() throws IOException {
+
+        StringBuilder strb = new StringBuilder();
+        strb.append("<replication =\"abcdef");
+        strb.append(getControlChars());
+        strb.append("\">value");
+        strb.append(getControlChars());
+        strb.append("</replication");
+        strb.append(getControlChars());
+        strb.append('>');
+
+        try(Writer w = new CharArrayWriter()) {
+            ControlCharacterHandler handler = new ControlCharacterHandler();
+            handler.escape(strb.toString().toCharArray(), 0, strb.toString().length(), true, w);
+            assertEquals("&lt;replication =&quot;abcdef&quot;&gt;value&lt;/replication&gt;", w.toString());
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    private char[] getControlChars() {
+        StringBuilder res = new StringBuilder();
+
+        for(int i = 0; i < 32; i++) {
+            res.append((char) i);
+        }
+
+        for(int i = 127; i < 161; i++) {
+            res.append((char) i);
+        }
+        return res.toString().toCharArray();
     }
 }
