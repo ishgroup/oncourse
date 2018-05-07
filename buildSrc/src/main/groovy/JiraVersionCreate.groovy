b1a43979e716dc939735255a2cee7018588a1a34
Aristedes Maniatis <ari@ish.com.au>
2018-05-07 23:42:30 +0000

    Finish the release task to update Jira versions. Hardcode a special user/pass for this purpose (this is no less secure than committing an oauth certificate into svn, and much easier). Call this task on release from the jenkinsfile.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@51519 fb2e276d-fae9-0310-b578-b71ac85840b9

--- buildSrc/src/main/groovy/JiraVersionCreate.groovy
+++ buildSrc/src/main/groovy/JiraVersionCreate.groovy
@@ -6,59 +6,82 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 import javax.validation.constraints.NotNull
-
+/**
+ * This task will create a new version in Jira and assign it to the correct component
+ * It expects to get an oldVersion in the form "{version} {component}" for example
+ * "10 web" and will create the following integer version against the same component.
+ *
+ * It will also mark the existing version as released.
+ */
 public class JiraVersionCreate extends DefaultTask {
 
     @Input @NotNull
     String oldVersion
 
     @Input @NotNull
-    String jiraUsername
-
-    @Input @NotNull
-    String jiraPassword
+    String projectCode
 
     @TaskAction
     def void run() {
         def (thisVersion, componentName) = oldVersion.split(' ')
         def nextVersion = thisVersion.toInteger() + 1
 
-        def component = getJiraResponse('api/2/project/OD/components', null).find { it.name == componentName }
-        if (!component) {
+        // find the component id
+        def componentRecord = getJiraResponse("api/2/project/${projectCode}/components", null).find { it.name == componentName }
+        if (!componentRecord) {
             logger.error("Could not find component with name ${componentName}")
         }
+        logger.info "Found existing component id ${componentRecord.id}"
+
+        // find the existing version id
+        def oldVersionRecord = getJiraResponse("api/2/project/${projectCode}/versions", null).find { it.name == oldVersion }
+        logger.info "Found existing version id ${oldVersionRecord.id}"
+
+        // create a new version
         def versionData = JsonOutput.toJson([
             name: "${nextVersion} ${componentName}",
             archived: false,
             released: false,
-            project: "OD"
+            project: projectCode
         ])
         def newVersion = getJiraResponse('api/2/version', versionData)
+        logger.info "Created new version ${newVersion.id}"
 
+        // link to the new version to the correct component id
         def componentData = JsonOutput.toJson([
-            "componentId": component.id,
+            "componentId": componentRecord.id,
             "versionId": newVersion.id,
             "released": false,
         ])
         getJiraResponse('com.deniz.jira.mapping/latest/',componentData)
+        logger.info "Created mapping to component"
+
+        // mark the old version as released
+        def versionRelease = JsonOutput.toJson([
+                releaseDate: new Date().format('yyyy-MM-dd'),
+                released: true
+        ])
+        getJiraResponse("api/2/version/${oldVersionRecord.id}", versionRelease, "PUT")
+        logger.info "Updated old version and set it to released."
     }
 
-    def getJiraResponse(query, data) {
-        if (!jiraPassword || !jiraUsername) {
-            logger.error("Please put jiraPassword and jiraUsername properties into ~/.gradle/gradle.properties")
-        }
+    def getJiraResponse(query, data, action="POST") {
         // https://developer.atlassian.com/cloud/jira/platform/rest/
         def SQUISH_URL = "https://squish.ish.com.au/rest/"
+        def jiraUsername = "gradle"
+        def jiraPassword = "7Am1*^X8A#ul"
         def encoded_auth = "${jiraUsername}:${jiraPassword}".getBytes().encodeBase64().toString()
 
         def connection = (SQUISH_URL + query).toURL().openConnection()
         logger.lifecycle "Getting " + connection.getURL()
+        logger.debug "Sending data " + new JsonBuilder(data).toPrettyString()
+
         connection.addRequestProperty("Authorization", "Basic ${encoded_auth}")
         connection.addRequestProperty("Content-Type", "application/json")
         connection.addRequestProperty("Accept", "application/json")
 
         if (data) {
-            connection.setRequestMethod("POST")
+            connection.setRequestMethod(action)
             connection.setDoOutput(true)
 
             def writer = new OutputStreamWriter(connection.outputStream)
@@ -68,14 +91,12 @@ public class JiraVersionCreate extends DefaultTask {
         } else {
             connection.setRequestMethod("GET")
         }
-
-        logger.info(connection.toString())
         connection.connect()
 
         def slurper = new JsonSlurper()
-        def json_data = slurper.parseText(connection.content.text)
-        logger.info new JsonBuilder(json_data).toPrettyString()
+        def response = slurper.parseText(connection.content.text)
+        logger.info new JsonBuilder(response).toPrettyString()
 
-        return json_data
+        return response
     }
 }
