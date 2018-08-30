8e40ce757e2ff06d311437dddeade5ec222be33e
Artyom Kravenko <artyom@ish.com.au>
2018-08-30 11:45:54 +0000

    0000 Fix unit tests
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@53210 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/test/java/ish/oncourse/ui/utils/CourseItemModelTest.java
+++ common/common-ui/src/test/java/ish/oncourse/ui/utils/CourseItemModelTest.java
@@ -32,7 +32,7 @@ public class CourseItemModelTest {
 
 	@Test
 	public void test_selectRelatedCourses() {
-		CayenneService cayenneService = new CayenneService(testContext.getServerRuntime(), null);
+		CayenneService cayenneService = new CayenneService(testContext.getServerRuntime(), null, null);
 		Course course = query(Course.class, 1L).selectOne(cayenneService.newContext());
 		List<Course> courses = CourseItemModel.selectRelatedCourses(course);
 		assertEquals(4, courses.size());
