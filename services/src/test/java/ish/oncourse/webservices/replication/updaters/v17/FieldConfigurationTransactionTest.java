a5c1bb0161c483989a8ae5d5d59f1d5b5eb1dc68
Artyom Kravenko <artyom@ish.com.au>
2018-09-04 07:38:18 +0000

    OD-12052 Replication test  - adjust field configuration stubs in accordance with V17 replication
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@53275 fb2e276d-fae9-0310-b578-b71ac85840b9

--- services/src/test/java/ish/oncourse/webservices/replication/updaters/v17/FieldConfigurationTransactionTest.java
+++ services/src/test/java/ish/oncourse/webservices/replication/updaters/v17/FieldConfigurationTransactionTest.java
@@ -60,41 +60,80 @@ public class FieldConfigurationTransactionTest extends ServiceTest {
         configurationStub1.setName("Test configuration 1");
         configurationStub1.setModified(new Date());
         configurationStub1.setCreated(new Date());
-        configurationStub1.setEntityIdentifier(FieldConfiguration.class.getSimpleName());
-
+        configurationStub1.setEntityIdentifier(EnrolmentFieldConfiguration.class.getSimpleName());
+        configurationStub1.setType(1);
+        
         FieldConfigurationStub configurationStub2 = new FieldConfigurationStub();
         configurationStub2.setAngelId(2L);
         configurationStub2.setWillowId(null);
         configurationStub2.setName("Test configuration 2");
         configurationStub2.setModified(new Date());
         configurationStub2.setCreated(new Date());
-        configurationStub2.setEntityIdentifier(FieldConfiguration.class.getSimpleName());
+        configurationStub2.setEntityIdentifier(ApplicationFieldConfiguration.class.getSimpleName());
+        configurationStub2.setType(2);
+
+        FieldConfigurationStub configurationStub3 = new FieldConfigurationStub();
+        configurationStub3.setAngelId(3L);
+        configurationStub3.setWillowId(null);
+        configurationStub3.setName("Test configuration 2");
+        configurationStub3.setModified(new Date());
+        configurationStub3.setCreated(new Date());
+        configurationStub3.setEntityIdentifier(WaitingListFieldConfiguration.class.getSimpleName());
+        configurationStub3.setType(3);
         
         FieldConfigurationSchemeStub schemeStub = new FieldConfigurationSchemeStub();
         schemeStub.setName("Test scheme");
         schemeStub.setAngelId(1L);
-        schemeStub.setWaitingListFieldConfigurationId(1L);
-        schemeStub.setApplicationFieldConfigurationId(2L);
-        schemeStub.setEnrolFieldConfigurationId(2L);
+        
+        
         schemeStub.setModified(new Date());
         schemeStub.setCreated(new Date());
         schemeStub.setEntityIdentifier(FieldConfigurationScheme.class.getSimpleName());
 
-        FieldStub fieldStub = new FieldStub();
-        fieldStub.setAngelId(2L);
-        fieldStub.setFieldConfigurationId(1L);
-        fieldStub.setFieldHeadingId(1L);
-        fieldStub.setName("Test Field");
-        fieldStub.setMandatory(false);
-        fieldStub.setOrder(2);
-        fieldStub.setProperty("prop");
-        fieldStub.setModified(new Date());
-        fieldStub.setCreated(new Date());
-        fieldStub.setEntityIdentifier(Field.class.getSimpleName());
+        FieldConfigurationLinkStub linkStub1 = new FieldConfigurationLinkStub();
+        linkStub1.setModified(new Date());
+        linkStub1.setCreated(new Date());
+        linkStub1.setAngelId(1L);
+        linkStub1.setEntityIdentifier(FieldConfigurationLink.class.getSimpleName());
+        linkStub1.setSchemeId(1L);
+        linkStub1.setConfigurationId(1L);
+        linkStub1.setConfigurationType(1);
+
+        FieldConfigurationLinkStub linkStub2 = new FieldConfigurationLinkStub();
+        linkStub2.setModified(new Date());
+        linkStub2.setCreated(new Date());
+        linkStub2.setAngelId(2L);
+        linkStub2.setEntityIdentifier(FieldConfigurationLink.class.getSimpleName());
+        linkStub2.setSchemeId(1L);
+        linkStub2.setConfigurationId(2L);
+        linkStub2.setConfigurationType(2);
+
+        FieldConfigurationLinkStub linkStub3 = new FieldConfigurationLinkStub();
+        linkStub3.setModified(new Date());
+        linkStub3.setCreated(new Date());
+        linkStub3.setAngelId(3L);
+        linkStub3.setEntityIdentifier(FieldConfigurationLink.class.getSimpleName());
+        linkStub3.setSchemeId(1L);
+        linkStub3.setConfigurationId(3L);
+        linkStub3.setConfigurationType(3);
+
+
+        FieldStub fieldStub1= new FieldStub();
+        fieldStub1.setAngelId(1L);
+        fieldStub1.setFieldConfigurationId(1L);
+        fieldStub1.setFieldHeadingId(1L);
+        fieldStub1.setName("Test Field");
+        fieldStub1.setMandatory(false);
+        fieldStub1.setOrder(2);
+        fieldStub1.setProperty("prop");
+        fieldStub1.setModified(new Date());
+        fieldStub1.setCreated(new Date());
+        fieldStub1.setEntityIdentifier(Field.class.getSimpleName());
+        fieldStub1.setConfigurationType(1);
 
         FieldStub fieldStub2 = new FieldStub();
-        fieldStub2.setAngelId(1L);
-        fieldStub2.setFieldConfigurationId(1L);
+        fieldStub2.setAngelId(2L);
+        fieldStub2.setFieldConfigurationId(2L);
         fieldStub2.setFieldHeadingId(null);
         fieldStub2.setName("Test Field");
         fieldStub2.setMandatory(false);
@@ -103,6 +142,20 @@ public class FieldConfigurationTransactionTest extends ServiceTest {
         fieldStub2.setModified(new Date());
         fieldStub2.setCreated(new Date());
         fieldStub2.setEntityIdentifier(Field.class.getSimpleName());
+        fieldStub2.setConfigurationType(2);
+
+        FieldStub fieldStub3 = new FieldStub();
+        fieldStub3.setAngelId(3L);
+        fieldStub3.setFieldConfigurationId(3L);
+        fieldStub3.setFieldHeadingId(null);
+        fieldStub3.setName("Test Field");
+        fieldStub3.setMandatory(false);
+        fieldStub3.setOrder(1);
+        fieldStub3.setProperty("prop");
+        fieldStub3.setModified(new Date());
+        fieldStub3.setCreated(new Date());
+        fieldStub3.setEntityIdentifier(Field.class.getSimpleName());
+        fieldStub3.setConfigurationType(3);
 
         FieldHeadingStub fieldHeadingStub = new FieldHeadingStub();
         fieldHeadingStub.setName("Test heading");
@@ -112,6 +165,7 @@ public class FieldConfigurationTransactionTest extends ServiceTest {
         fieldHeadingStub.setModified(new Date());
         fieldHeadingStub.setCreated(new Date());
         fieldHeadingStub.setEntityIdentifier(FieldHeading.class.getSimpleName());
+        fieldHeadingStub.setConfigurationType(1);
 
         CourseStub courseStub = new CourseStub();
         courseStub.setAngelId(1L);
@@ -126,11 +180,18 @@ public class FieldConfigurationTransactionTest extends ServiceTest {
         GenericTransactionGroup transactionGroup = PortHelper.createTransactionGroup(V17);
         transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(schemeStub);
         transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(courseStub);
-        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(fieldStub);
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(fieldStub1);
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(fieldStub2);
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(fieldStub3);
+
         transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(configurationStub1);
         transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(configurationStub2);
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(configurationStub3);
+
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(linkStub1);
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(linkStub2);
+        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(linkStub3);
         transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(fieldHeadingStub);
-        transactionGroup.getGenericAttendanceOrBinaryDataOrBinaryInfo().add(fieldStub2);
         
         List<GenericReplicatedRecord> records = transactionGroupProcessor.processGroup(transactionGroup);
         assertEquals(SUCCESS,((ReplicatedRecord)records.get(0)).getStatus());
