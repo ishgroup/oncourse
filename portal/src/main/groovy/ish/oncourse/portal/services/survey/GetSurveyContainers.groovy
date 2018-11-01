bc6a1e06012b4d7ffc51a5af3533edcb5b3b680a
Artyom Kravenko <artyom@ish.com.au>
2018-11-01 13:13:39 +0000

    OD-11295 Adjust getter to prevent NPE
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@54252 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/groovy/ish/oncourse/portal/services/survey/GetSurveyContainers.groovy
+++ portal/src/main/groovy/ish/oncourse/portal/services/survey/GetSurveyContainers.groovy
@@ -1,15 +1,22 @@
 package ish.oncourse.portal.services.survey
 
 import ish.oncourse.model.Enrolment
+import ish.oncourse.model.FieldConfiguration
 import ish.oncourse.model.SurveyFieldConfiguration
 
 class GetSurveyContainers {
     private Enrolment enrolment
 
     List<SurveyContainer> get() {
-        return GetAvailableSurveyForms.valueOf(enrolment.courseClass).get().collect { f->
+        List<FieldConfiguration> configurations = GetAvailableSurveyForms.valueOf(enrolment.courseClass).get()
+        if (configurations) {
+            return configurations.collect { f->
                 new SurveyContainer(fieldConfiguration:(f as SurveyFieldConfiguration) , survey: enrolment.surveys.find {s -> f == s.fieldConfiguration} )
             }.sort { c -> c.fieldConfiguration.deliverySchedule.databaseValue }
+        } else {
+            return null
+        }
+        
     }
 
     static GetSurveyContainers valueOf(Enrolment enrolment) {
