b99a6e3c5cea3499040ba074f38874d859a1f600
Maxim Petrusevich <mpetrusevich@objectstyle.com>
2018-11-05 12:10:56 +0000

    OD-12145 Cannot update preferences: Fixed 3-field constraint with java-based validation
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@54295 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/cayenne-model/src/main/java/ish/oncourse/model/Preference.java
+++ common/cayenne-model/src/main/java/ish/oncourse/model/Preference.java
@@ -3,8 +3,12 @@ package ish.oncourse.model;
 import ish.oncourse.model.auto._Preference;
 import ish.oncourse.utils.QueueableObjectUtils;
 import ish.persistence.Preferences;
+import org.apache.cayenne.access.DataContext;
+import org.apache.cayenne.query.ObjectSelect;
+import org.apache.cayenne.validation.ValidationResult;
 
 public class Preference extends _Preference implements Queueable {
+
 	private static final long serialVersionUID = 8309390847931508840L;
 
 	public Long getId() {
@@ -14,5 +18,36 @@ public class Preference extends _Preference implements Queueable {
 	@Override
 	public boolean isAsyncReplicationAllowed() {
 		return getWebSite() == null && !Preferences.SERVICES_SECURITYKEY.equals(getName());
+	}
+
+	@Override
+	public void validateForInsert(ValidationResult validationResult) {
+		Preference preference = ObjectSelect.query(Preference.class)
+				.where(COLLEGE.eq(getCollege()))
+				.and(NAME.eq(getName()))
+				.selectFirst(objectContext);
+		boolean isPresentedInDatabase = (preference != null) && (preference.getWebSite() == null);
+		if ((isPresentedInDatabase) || (isPresentedInUncommitedObjects())) {
+			String propertyName = COLLEGE.getName() + ":" + NAME.getName();
+			validationResult.addFailure(ValidationFailure.validationFailure(
+					this, propertyName, "Entity with this data is exists."
+			));
+		}
+		super.validateForInsert(validationResult);
+	}
+
+	/**
+	 * Matches all preferences in ObjectStore with this object
+	 * @return result of matching with preferences, which presented in ObjectStore
+	 */
+	private boolean isPresentedInUncommitedObjects() {
+		return ((DataContext) objectContext).uncommittedObjects().stream()
+				.filter(item -> item instanceof Preference)
+				.anyMatch(item -> {
+					Preference uncommitedPreference = (Preference) item;
+					boolean hasSameColleges = getCollege().equals(uncommitedPreference.getCollege());
+					boolean hasSameNames = getName().equals(uncommitedPreference.getName());
+					return (hasSameColleges) && (hasSameNames) && (uncommitedPreference.getWebSite() == null);
+				});
 	}
 }
