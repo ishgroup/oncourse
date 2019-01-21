1eec170a300ee8c8a57b9038fe42ed32ace9d875
Pavel Nikanovich <pavel@ish.com.au>
2019-01-21 12:43:08 +0000

    OD-00000 add missed imports in usi verification service
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@55227 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/main/java/ish/oncourse/services/usi/IUSIVerificationService.java
+++ common/common-services/src/main/java/ish/oncourse/services/usi/IUSIVerificationService.java
@@ -3,6 +3,8 @@
  */
 package ish.oncourse.services.usi;
 
+import ish.common.types.LocateUSIRequest;
+import ish.common.types.LocateUSIResult;
 import ish.common.types.USIVerificationRequest;
 import ish.common.types.USIVerificationResult;
 
