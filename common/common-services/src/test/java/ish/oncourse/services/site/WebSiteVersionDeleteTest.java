df88a841d0e5376a7db61834d6d3363a85dc2eea
Pavel Nikanovich <pavel@ish.com.au>
2019-01-28 10:27:53 +0000

    OD-12357 commons : delete unused import
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@55298 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-services/src/test/java/ish/oncourse/services/site/WebSiteVersionDeleteTest.java
+++ common/common-services/src/test/java/ish/oncourse/services/site/WebSiteVersionDeleteTest.java
@@ -9,7 +9,6 @@ import ish.oncourse.services.ServiceTestModule;
 import ish.oncourse.services.persistence.ICayenneService;
 import ish.oncourse.test.LoadDataSet;
 import ish.oncourse.test.tapestry.ServiceTest;
-import ish.oncourse.test.tapestry.ServiceTestWithLiquibase;
 import org.apache.cayenne.Cayenne;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.PersistenceState;
