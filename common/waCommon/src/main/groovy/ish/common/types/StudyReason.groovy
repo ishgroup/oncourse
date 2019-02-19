740e6d3ecdc4c9769e6127abb14e780d21398bae
Andrey Narut <anarut@ish.com.au>
2019-02-19 13:05:53 +0000

    OD-12182 Added new value to enum.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@55641 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/waCommon/src/main/groovy/ish/common/types/StudyReason.groovy
+++ common/waCommon/src/main/groovy/ish/common/types/StudyReason.groovy
@@ -2,17 +2,17 @@
  * Copyright ish group pty ltd. All rights reserved. http://www.ish.com.au
  * No copying or use of this code is allowed without permission in writing from ish.
  */
-package ish.common.types;
+package ish.common.types
 
-import ish.common.util.DisplayableExtendedEnumeration;
-import ish.oncourse.API;
+import ish.common.util.DisplayableExtendedEnumeration
+import ish.oncourse.API
 
 /**
  * A set of values for AVETMISS reporting from the version 7 standard.
  * Consult the AVETMISS documentation for more detail about these options.
  */
 @API
-public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
+enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 
 	/**
 	 * Database value: -1
@@ -20,7 +20,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * Not stated
 	 */
 	@API
-	STUDY_REASON_NOT_STATED(-1, "Not stated"),
+	STUDY_REASON_NOT_STATED(-1, 'Not stated'),
 
 	/**
 	 * Database value: 0
@@ -28,7 +28,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To get a job
 	 */
 	@API
-	STUDY_REASON_JOB(0, "To get a job"),
+	STUDY_REASON_JOB(0, 'To get a job'),
 
 	/**
 	 * Database value: 1
@@ -36,7 +36,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To develop existing business
 	 */
 	@API
-	STUDY_REASON_DEVELOP_BUSINESS(1, "To develop my existing business"),
+	STUDY_REASON_DEVELOP_BUSINESS(1, 'To develop my existing business'),
 
 	/**
 	 * Database value: 2
@@ -44,7 +44,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To start own business
 	 */
 	@API
-	STUDY_REASON_START_BUSINESS(2, "To start my own business"),
+	STUDY_REASON_START_BUSINESS(2, 'To start my own business'),
 
 	/**
 	 * Database value: 4
@@ -52,7 +52,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To try for a different career
 	 */
 	@API
-	STUDY_REASON_CAREER_CHANGE(4, "To try for a different career"),
+	STUDY_REASON_CAREER_CHANGE(4, 'To try for a different career'),
 
 	/**
 	 * Database value: 8
@@ -60,7 +60,15 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To get a better job or promotion
 	 */
 	@API
-	STUDY_REASON_BETTER_JOB(8, "To get a better job or promotion"),
+	STUDY_REASON_BETTER_JOB(8, 'To get a better job or promotion'),
+
+	/**
+	 * Database value: 13
+	 *
+	 * To get skills for community/voluntary work
+	 */
+	@API
+	STUDY_REASON_VOLUNTARY_WORK(13, 'To get skills for community/voluntary work'),
 
 	/**
 	 * Database value: 16
@@ -68,7 +76,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * Was a requirement of a job
 	 */
 	@API
-	STUDY_REASON_JOB_REQUIREMENT(16, "It was a requirement of my job"),
+	STUDY_REASON_JOB_REQUIREMENT(16, 'It was a requirement of my job'),
 
 	/**
 	 * Database value: 32
@@ -76,7 +84,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To get extra skills for a job
 	 */
 	@API
-	STUDY_REASON_EXTRA_JOB_SKILLS(32, "I wanted extra skills for my job"),
+	STUDY_REASON_EXTRA_JOB_SKILLS(32, 'I wanted extra skills for my job'),
 
 	/**
 	 * Database value: 64
@@ -84,7 +92,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * To get into another course of study
 	 */
 	@API
-	STUDY_REASON_FOR_ANOTHER_COURSE(64, "To get into another course of study"),
+	STUDY_REASON_FOR_ANOTHER_COURSE(64, 'To get into another course of study'),
 
 	/**
 	 * Database value: 128
@@ -92,7 +100,7 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * Other reasons
 	 */
 	@API
-	STUDY_REASON_OTHER(128, "Other reasons"),
+	STUDY_REASON_OTHER(128, 'Other reasons'),
 
 	/**
 	 * Database value: 256
@@ -100,29 +108,29 @@ public enum StudyReason implements DisplayableExtendedEnumeration<Integer> {
 	 * For personal interest or self-development
 	 */
 	@API
-	STUDY_REASON_PERSONAL_INTEREST(256, "For personal interest or self-development");
+	STUDY_REASON_PERSONAL_INTEREST(256, 'For personal interest or self-development');
 
-	private String displayName;
-	private int value;
+	private String displayName
+	private int value
 
 	private StudyReason(int value, String displayName) {
-		this.value = value;
-		this.displayName = displayName;
+		this.value = value
+		this.displayName = displayName
 	}
 
 	@Override
-	public Integer getDatabaseValue() {
-		return this.value;
+	Integer getDatabaseValue() {
+		return this.value
 	}
 
 	@Override
-	public String getDisplayName() {
-		return this.displayName;
+	String getDisplayName() {
+		return this.displayName
 	}
 
 	@Override
-	public String toString() {
-		return getDisplayName();
+	String toString() {
+		return getDisplayName()
 	}
 
 }
