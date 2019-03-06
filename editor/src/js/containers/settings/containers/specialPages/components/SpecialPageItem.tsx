1fbf5a27efc89929310edb7572d974464d74bbc4
Yury Yasuchenya <yury@ish.com.au>
2019-03-06 15:03:31 +0000

    OD-12022 Updated Special page match type value field
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@55872 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor/src/js/containers/settings/containers/specialPages/components/SpecialPageItem.tsx
+++ editor/src/js/containers/settings/containers/specialPages/components/SpecialPageItem.tsx
@@ -13,11 +13,8 @@ import {
 } from "reactstrap";
 import {
   SpecialPageItem as SpecialPageItemModel,
-  SpecialPage,
   URLMatchRule,
 } from "../../../../../model";
-import {Checkbox} from "../../../../../common/components/Checkbox";
-import {URLMatchRuleKeys} from "../reducers/State";
 
 interface SpecialPageItemState extends SpecialPageItemModel {
   submitted?: boolean;
@@ -39,7 +36,7 @@ export class SpecialPageItem extends React.PureComponent<Props, any> {
           <h6 className="mb-0 ml-2">{item.specialPage}</h6>
 
           <div className="rule ml-2">
-            <Label>From</Label>
+            <Label>From ({item.matchType.toLowerCase()})</Label>
             <Input
               className={classnames({
                 invalid: item.error,
@@ -51,30 +48,6 @@ export class SpecialPageItem extends React.PureComponent<Props, any> {
               onChange={e => onChange(e, index, "from")}
             />
           </div>
-
-          <Label>Match Type</Label>
-          <Input
-            type="select"
-            name={`matchType-${index}`}
-            id={`matchType-${index}`}
-            value={item.matchType}
-            onChange={e => onChange(e, index, "matchType")}
-          >
-            {Object.keys(URLMatchRule).map((i, n) => (
-              <option key={n} value={URLMatchRule[i]}>
-                {URLMatchRule[i]}
-              </option>
-            ))}
-          </Input>
-
-          {/*<FormGroup className="ml-3">*/}
-            {/*<Checkbox*/}
-              {/*label="Exact"*/}
-              {/*name={`matchType-${index}`}*/}
-              {/*checked={item.matchType === URLMatchRuleKeys[0]}*/}
-              {/*onChange={e => onMatchTypeChange(e, index, "matchType")}*/}
-            {/*/>*/}
-          {/*</FormGroup>*/}
         </div>
 
         {item.error && (
