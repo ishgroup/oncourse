01373662a6903ac96d1d69293cf7a1a1236d167c
Yury Yasuchenya <yury@ish.com.au>
2019-03-11 10:19:23 +0000

    OD-12548 Added simplified email validation on checkout
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@55910 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout/src/js/enrol/containers/contact-add/actions/Validations.ts
+++ checkout/src/js/enrol/containers/contact-add/actions/Validations.ts
@@ -9,7 +9,7 @@ export const validateContact = (values: any): any => {
   }
   if (!values.email) {
     errors.email = "The student's email is required.";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  } else if (!/.+@.+\..+/i.test(values.email)) {
     errors.email = "The email address does not appear to be valid.";
   }
   return errors;
@@ -23,7 +23,7 @@ export const validateCompany = (values: any): any => {
   }
   if (!values.email) {
     errors.email = "The student's email is required.";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  } else if (!/.+@.+\..+/i.test(values.email)) {
     errors.email = "The email address does not appear to be valid.";
   }
   return errors;
