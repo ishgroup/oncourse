df0b47ab5d760b3d0a07c1fb70c6aa68d9cf8f08
Pavel Nikanovich <pavel@ish.com.au>
2019-11-21 15:32:07 +0000

    OD-13471 portal: relate payment form visibility with preference 'enrolment.creditCard..payment.enabled', hide if false
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@60029 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/java/ish/oncourse/portal/components/history/PaymentForm.java
+++ portal/src/main/java/ish/oncourse/portal/components/history/PaymentForm.java
@@ -1,16 +1,18 @@
 package ish.oncourse.portal.components.history;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tapestry5.annotations.Property;
 import ish.oncourse.portal.services.IPortalService;
 import ish.oncourse.portal.services.payment.Controller;
 import ish.oncourse.portal.services.payment.Request;
 import ish.oncourse.services.paymentexpress.INewPaymentGatewayServiceBuilder;
-import ish.oncourse.services.paymentexpress.IPaymentGatewayServiceBuilder;
+import ish.oncourse.services.preference.PreferenceController;
 import ish.oncourse.services.persistence.ICayenneService;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.util.TextStreamResponse;
 
@@ -37,7 +39,18 @@ public class PaymentForm {
 	@Inject
 	private org.apache.tapestry5.services.Request request;
 
+	@Inject
+	private PreferenceController preferenceController;
+
 	private ObjectMapper mapper = new ObjectMapper();
+
+	@Property
+	private boolean isCreditCardPaymentEnabled;
+
+	@SetupRender
+	public void setupRender() {
+		isCreditCardPaymentEnabled = preferenceController.isCreditCardPaymentEnabled();
+	}
 
 
 	@OnEvent(value = "process")
