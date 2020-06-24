bf41f140ed4b9fc9308f1c94c9dde1e29aded3a1
Artyom Kravenko <artyom@ish.com.au>
2020-06-24 11:14:06 +0000

    OD-14718 clean up all payment related code from portal. Add required data attributes required for react work
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@63839 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/groovy/ish/oncourse/portal/services/payment/IController.groovy
+++
@@ -1,9 +0,0 @@
-package ish.oncourse.portal.services.payment
-
-/**
- * User: akoiro
- * Date: 1/07/2016
- */
-interface IController {
-    public Response process(Request request)
-}
--- portal/src/main/java/ish/oncourse/portal/components/history/PaymentForm.java
+++ portal/src/main/java/ish/oncourse/portal/components/history/PaymentForm.java
@@ -1,22 +1,18 @@
 package ish.oncourse.portal.components.history;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import ish.math.Money;
+import ish.oncourse.model.Contact;
+import ish.oncourse.model.Invoice;
+import ish.oncourse.model.InvoiceDueDate;
+import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import ish.oncourse.portal.services.IPortalService;
-import ish.oncourse.portal.services.payment.Controller;
-import ish.oncourse.portal.services.payment.Request;
-import ish.oncourse.services.paymentexpress.INewPaymentGatewayServiceBuilder;
 import ish.oncourse.services.preference.PreferenceController;
 import ish.oncourse.services.persistence.ICayenneService;
-import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.util.TextStreamResponse;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by akoiro on 20/05/2016.
@@ -31,42 +27,27 @@ public class PaymentForm {
 	private IPortalService portalService;
 
 	@Inject
-	private INewPaymentGatewayServiceBuilder paymentGatewayServiceBuilder;
-
-	@Inject
-	private HttpServletRequest httpRequest;
-
-	@Inject
-	private org.apache.tapestry5.services.Request request;
-
-	@Inject
 	private PreferenceController preferenceController;
-
-	private ObjectMapper mapper = new ObjectMapper();
 
 	@Property
 	private boolean isCreditCardPaymentEnabled;
 
+	@Property
+	private Long payerId;
+
+	@Property
+	private Money balance;
+
 	@SetupRender
 	public void setupRender() {
 		isCreditCardPaymentEnabled = preferenceController.isCreditCardPaymentEnabled();
+		Contact payer = portalService.getContact();
+		payerId = payer.getId();
+		balance = payer.getInvoices().stream()
+				.map(Invoice::getAmountOwing)
+				.reduce(Money.ZERO, Money::add);
 	}
 
 
-	@OnEvent(value = "process")
-	public Object process() {
-		if (!this.request.isXHR()) {
-			throw new IllegalStateException();
-		}
-		try {
-			String json = IOUtils.toString(httpRequest.getInputStream(), "UTF-8");
-			Request request = mapper.readValue(json, Request.class);
-			Controller controller = new Controller(cayenneService, portalService, paymentGatewayServiceBuilder.buildService());
-			return new TextStreamResponse("text/json", mapper.writeValueAsString(controller.process(request)));
-		} catch (Exception e) {
-			LOGGER.catching(e);
-			throw new IllegalArgumentException(e);
-		}
-	}
 }
 
