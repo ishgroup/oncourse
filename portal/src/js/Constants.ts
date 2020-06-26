c314c145a124258d0d66ed1c3014f47a86f9dc25
Yury Yasuchenya <yury@ish.com.au>
2020-06-26 12:56:56 +0000

    OD-14718 Update test server port
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@63887 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/js/Constants.ts
+++ portal/src/js/Constants.ts
@@ -6,4 +6,4 @@
  * CONTEXT for willow-api requests
  */
 
-export const CONTEXT: string = process.env.RELEASE_VERSION ===  "development" ?  "http://127.0.0.1:8283/a" : "/a/";
+export const CONTEXT: string = process.env.RELEASE_VERSION ===  "development" ?  "http://127.0.0.1:8301/a" : "/a/";
