5d389ef8fc4e3f89ee7d48cb98effbbaa476047f
Yury Yasuchenya <yury@ish.com.au>
2020-07-01 13:03:35 +0000

    Adjusted portal test server host
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@63935 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/js/Constants.ts
+++ portal/src/js/Constants.ts
@@ -6,4 +6,4 @@
  * CONTEXT for willow-api requests
  */
 
-export const CONTEXT: string = process.env.RELEASE_VERSION ===  "development" ?  "http://127.0.0.1:8301/a" : "/a/";
+export const CONTEXT: string = process.env.RELEASE_VERSION ===  "development" ?  "http://10.29.67.16:8301/a" : "/a/";
