fe2792fa77a10ba568cec69ac39556a2b2632b1d
Artyom Kravenko <artyom@ish.com.au>
2020-07-14 09:46:15 +0000

    OD-14745 handle cayenne Compound Diff as a list of regular ObjectStore diffs
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64135 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout-api/src/main/groovy/org/apache/cayenne/access/ValidationFilter.groovy
+++ checkout-api/src/main/groovy/org/apache/cayenne/access/ValidationFilter.groovy
@@ -3,6 +3,7 @@ package org.apache.cayenne.access
 import org.apache.cayenne.DataChannelSyncFilter
 import org.apache.cayenne.DataChannelSyncFilterChain
 import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.graph.CompoundDiff
 import org.apache.cayenne.graph.GraphDiff
 
 import static ish.oncourse.willow.filters.XValidateFilter.ThreadLocalValidateOnly
@@ -12,7 +13,13 @@ class ValidationFilter implements DataChannelSyncFilter {
     @Override
     GraphDiff onSync(ObjectContext originatingContext, GraphDiff changes, int syncType, DataChannelSyncFilterChain filterChain) {
         if (ThreadLocalValidateOnly.get()) {
-            ((ObjectStoreGraphDiff) changes).validateAndCheckNoop()
+            if (changes instanceof CompoundDiff) {
+                changes.getDiffs().each { diff ->
+                    ((ObjectStoreGraphDiff) diff).validateAndCheckNoop()
+                }
+            } else if (changes instanceof ObjectStoreGraphDiff ) {
+                changes.validateAndCheckNoop()
+            }
             return changes
         } else {
             return filterChain.onSync(originatingContext, changes, syncType)
