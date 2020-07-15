5a78274ebe0d137e0df3373b5b31db5e476025c4
Yury Yasuchenya <yury@ish.com.au>
2020-07-15 08:08:16 +0000

    OD-14782 Change portal native redirect url parser with custom one to allow invalid url params parsing
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64143 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/js/app.tsx
+++ portal/src/js/app.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PaymentForm from "./payment-form/PaymentForm";
 
-const query = new URLSearchParams(window.location.search);
-const sessionId = query.get("sessionId");
-const status = query.get("paymentStatus");
+const query = window.location.href;
+const sessionIdMatch = query.match(/sessionId=([^&#?]+)/);
+const statusMatch = query.match(/paymentStatus=([^&#?]+)/);
 let isRedirect = false;
 
-if (sessionId && status) {
+if (sessionIdMatch && sessionIdMatch[1] && statusMatch && statusMatch[1]) {
   isRedirect = true;
   window.parent.postMessage(
     {
       payment: {
-        sessionId,
-        status,
+        sessionId: sessionIdMatch[1],
+        status: statusMatch[1],
       },
     },
     "*",
