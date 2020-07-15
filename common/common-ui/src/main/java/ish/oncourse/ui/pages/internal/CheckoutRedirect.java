c80f86fcd0bfea6891bd0da6946180ed674f6495
Artyom Kravenko <artyom@ish.com.au>
2020-07-15 10:13:42 +0000

    OD-14812 handle windcave url redirect with extra params
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64144 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/CheckoutRedirect.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/pages/internal/CheckoutRedirect.java
@@ -1,4 +1,39 @@
 package ish.oncourse.ui.pages.internal;
 
+import org.apache.tapestry5.ContentType;
+import org.apache.tapestry5.StreamResponse;
+import org.apache.tapestry5.annotations.Meta;
+import org.apache.tapestry5.util.TextStreamResponse;
+
+import java.io.InputStream;
+
+@Meta("tapestry.response-content-type=text/html")
 public class CheckoutRedirect {
+
+    private static final String HTML_SRC = "<html>\n" +
+            "<head>\n" +
+            "    <script>\n" +
+            "        const query = window.location.href;\n" +
+            "        const sessionIdMatch = query.match(/sessionId=([^&#?]+)/);\n" +
+            "        const statusMatch = query.match(/paymentStatus=([^&#?]+)/);\n" +
+            "        if (sessionIdMatch && sessionIdMatch[1] && statusMatch && statusMatch[1]) {\n" +
+            "            window.parent.postMessage(\n" +
+            "                {\n" +
+            "                    payment: {\n" +
+            "                        sessionId: sessionIdMatch[1],\n" +
+            "                        status: statusMatch[1],\n" +
+            "                    },\n" +
+            "                },\n" +
+            "                \"*\",\n" +
+            "            );\n" +
+            "        }\n" +
+            "    </script>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "</body>\n" +
+            "</html>";
+
+    StreamResponse onActivate() {
+        return new TextStreamResponse("text/html", HTML_SRC);
+    }
 }
