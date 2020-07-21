1509ce86bf8bb0264f7d0ecb2e8d935c7bd52113
Artyom Kravenko <artyom@ish.com.au>
2020-07-21 13:08:57 +0000

    move DataType enum to waCommon, extend it with extra types for customFields especially
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64236 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/waCommon/src/main/groovy/ish/common/types/DataType.groovy
@@ -0,0 +1,109 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. https://www.ish.com.au
+ * No copying or use of this code is allowed without permission in writing from ish.
+ */
+
+package ish.common.types
+
+import ish.common.util.DisplayableExtendedEnumeration
+import ish.oncourse.API
+
+/**
+ * Data types for AutomationBinding records represent the type of data they can hold.
+ */
+@API
+public enum DataType implements DisplayableExtendedEnumeration<Integer> {
+
+    /**
+     * Text value, also known as a String
+     *
+     * Database value: 0
+     */
+    @API
+    TEXT(0, "Text"),
+
+    /**
+     * Date (without time or timezone)
+     *
+     * Database value: 1
+     */
+    @API
+    DATE(1, "Date"),
+
+    /**
+     * Date and time, includes a timezone (by default the same as the server timezone)
+     *
+     * Database value: 2
+     */
+    @API
+    DATE_TIME(2, "Date time"),
+
+    /**
+     * Boolean (true or false)
+     *
+     * Database value: 3
+     */
+    @API
+    BOOLEAN(3, "Checkbox"),
+
+    /**
+     * Entity (the objectclass of a record in the database)
+     *
+     * Database value: 4
+     */
+    @API
+    ENTITY(4, "Record"),
+
+    /**
+     * File (extra file for evaluate scripts)
+     *
+     * Database value: 5
+     */
+    @API
+    FILE(5, "File"),
+
+    /**
+     * Money value
+     *
+     * Database value: 6
+     */
+    @API
+    MONEY(6, "Money"),
+
+
+    /**
+     * Choices list
+     *
+     * Database value: 7
+     */
+    @API
+    LIST(7, "List"),
+
+
+    /**
+     *  Map of label - code
+     *
+     * Database value: 7
+     */
+    @API
+    MAP(8, "Map");
+
+
+    private Integer value;
+    private String displayValue;
+
+    DataType(Integer value, String displayValue) {
+        this.value = value;
+        this.displayValue = displayValue;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return displayValue;
+    }
+
+    @Override
+    public Integer getDatabaseValue() {
+        return value;
+    }
+}
