b45eb2025388067046200b823983924da6333228
Artyom Kravenko <artyom@ish.com.au>
2020-08-06 13:56:29 +0000

    OD-14900 Allow to customize robot.txt content through webdav
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64495 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/pages/RobotsTXT.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/pages/RobotsTXT.java
@@ -3,6 +3,13 @@
  */
 package ish.oncourse.ui.pages;
 
+import ish.oncourse.model.WebNodeType;
+import ish.oncourse.model.WebSiteLayout;
+import ish.oncourse.model.WebTemplate;
+import ish.oncourse.services.persistence.ICayenneService;
+import ish.oncourse.services.site.WebSiteVersionService;
+import org.apache.cayenne.query.ObjectSelect;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.tapestry5.StreamResponse;
 import org.apache.tapestry5.annotations.Meta;
 import org.apache.tapestry5.ioc.Messages;
@@ -17,14 +24,44 @@ public class RobotsTXT {
     private Request request;
 
     @Inject
-    private Messages messages;
-    
-    private static final String ROBOTS_CONTENT = "robots-txt-content";
+    private ICayenneService cayenneService;
+
+    @Inject
+    private WebSiteVersionService webSiteVersionService;
+
+    private static final String ROBOT_FILE_NAME = "robot.txt";
+
     private static final String ROBOTS_CONTENT_TYPE = "text/plain";
 
+    private static final String USER_AGENT = "User-agent: *";
+
+    private static final String SITE_MAP_PATTERN = "Sitemap: https://%s/sitemap.xml";
+
+    private static final String DEFAULT_CONTENT =
+            "Disallow: /cms/\n" +
+            "Disallow: /asset/ \n" +
+            "Disallow: /enrol/\n" +
+            "Disallow: /addToCookies\n" +
+            "Disallow: /ui/courses.showmorecourses\n" +
+            "Disallow: /Timeline/\n" +
+            "Disallow: /site/kiosk/\n" +
+            "Disallow: /room/kiosk/";
+
+    private static final String ROBOTS_TXT_PATTERN = "%s\n%s\n%s";
 
     StreamResponse onActivate() {
-        return new TextStreamResponse(ROBOTS_CONTENT_TYPE, messages.format(ROBOTS_CONTENT, request.getServerName()));
+
+        WebTemplate robotTemplate = ObjectSelect.query(WebTemplate.class)
+                .where(WebTemplate.NAME.eq(ROBOT_FILE_NAME))
+                .and(WebTemplate.LAYOUT.dot(WebSiteLayout.LAYOUT_KEY).eq(WebNodeType.DEFAULT_LAYOUT_KEY))
+                .and(WebTemplate.LAYOUT.dot(WebSiteLayout.WEB_SITE_VERSION).eq(webSiteVersionService.getCurrentVersion()))
+                .selectOne(cayenneService.newContext());
+
+        String content = robotTemplate != null && StringUtils.trimToNull(robotTemplate.getContent()) != null ?  robotTemplate.getContent() : DEFAULT_CONTENT;
+        String siteMap = String.format(SITE_MAP_PATTERN, request.getServerName());
+        String robotTxt = String.format(ROBOTS_TXT_PATTERN, USER_AGENT, content, siteMap);
+
+        return new TextStreamResponse(ROBOTS_CONTENT_TYPE, robotTxt);
     }
     
 }
