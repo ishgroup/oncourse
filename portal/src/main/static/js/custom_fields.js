263a3065e5f57ea70950e33633259931edbec5f7
Yury Yasuchenya <yury@ish.com.au>
2020-08-09 10:08:24 +0000

    OD-14906 Add new custom field types processing for portal survey form
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64514 fb2e276d-fae9-0310-b578-b71ac85840b9

--- portal/src/main/static/js/custom_fields.js
+++ portal/src/main/static/js/custom_fields.js
@@ -7,33 +7,41 @@ var $j = jQuery.noConflict();
 $j(document).ready(function () {
     $j("input[name^='customField']").each(function () {
         if ($j(this).data('default')) {
-
             var dataType = $j(this).data('type');
             var dataValue = $j(this).data('default');
             var values;
+            var creatable;
+            var hiddenInput = $j('<input type="hidden"></input>')
 
             if (dataType === 7) {
-                values = dataValue.map(function(x) {
-                    return x.value.includes("*")
-                      ? {value: "Other", label: "Other"}
-                      : {value: x.value}
+               dataValue.forEach(function(x) {
+                    if(x.value.includes("*")) {
+                        creatable = true;
+                    }
+                });
+                values = dataValue.filter(function(x) {
+                    return !x.value.includes("*")
                 });
             }
 
             if (dataType === 8) {
+                hiddenInput.attr("data-key", $j(this).attr("data-key"));
+                hiddenInput.attr("name", $j(this).attr("name"));
+                $j(this).attr("data-key",null)
+                $j(this).attr("name",null)
+                $j(this).before(hiddenInput);
                 values = dataValue.filter(function(x) {
                     return !x.value.includes("*")
                 });
-
                 values = dataValue.map(function(x) {
                     return {value: x.value, label: x.label ? x.label + (" (" + x.value + ")" ) : "" }
                 });
             }
 
-
             if(![7,8].includes(dataType)) {
                 values = dataValue.split(';');
-                values = values.map(function(x){ return "*" === x ? "Other" : x });
+                creatable = dataValue.includes("*");
+                values = values.filter(function(x){ return x !== "*" });
             }
 
             if (values.length > 0) {
@@ -45,21 +53,29 @@ $j(document).ready(function () {
                         return false;
                     },
                     select: function( event, ui ) {
-                        $j(this).val( ui.item.label );
-                        $j(this).attr( "submit-value", ui.item.value );
+                        if(dataType === 8) {
+                            $j(this).val( ui.item.label );
+                            hiddenInput.attr( "value", ui.item.value );
+                        }
                         return false;
+                    },
+                    create: function() {
+                        $j(this).on("keydown", function(e) {
+                            if(!creatable) {
+                                e.preventDefault()
+                            }
+                        })
                     }
                 }).focus(function() {
                     $j(this).autocomplete('search', "")
                 })
                 if(!$j(this).val()) {
-                    $j(this).val(values[0].label ? values[0].label : values[0])
+                    $j(this).val(values[0].label ? values[0].label : values[0].value)
                 } else if (dataType === 8) {
                     var savedValue = $j(this).val();
                     var updated = values.find(function (v) {
                         return v.value === savedValue;
                     })
-
                     if(updated) {
                         $j(this).attr( "submit-value", updated.value );
                         $j(this).val(updated.label);
