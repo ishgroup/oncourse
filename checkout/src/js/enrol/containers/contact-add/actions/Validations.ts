a54a081ecec7c6d92ca9e626b1ac0249c8942bc6
Yury Yasuchenya <yury@ish.com.au>
2020-09-01 12:14:49 +0000

    OD-14668 Roll back emails validation rules
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64785 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout/src/js/enrol/containers/contact-add/actions/Validations.ts
+++ checkout/src/js/enrol/containers/contact-add/actions/Validations.ts
@@ -1,4 +1,4 @@
-const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+const EMAIL_REGEX = /^[^@\s]+@[^@\s.]+\..+$/i;
 
 export const validateContact = (values: any): any => {
   const errors: any = {};
