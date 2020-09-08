d24b34f05404abf4d727fa6edc6616a80c13eb05
Artyom Kochetkov <artyomk@ish.com.au>
2020-09-08 10:00:38 +0000

    OD-14950 Support renderers for block/pages - change textile to legacy and md to markdown labels
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@64848 fb2e276d-fae9-0310-b578-b71ac85840b9

--- editor/src/js/containers/content/constants/index.ts
+++ editor/src/js/containers/content/constants/index.ts
@@ -1,3 +1,7 @@
-export const CONTENT_MODES = [{id: "textile", title: "textile"}, {id: "md", title: "md"}, {id: "html", title: "html"}];
+export const CONTENT_MODES = [
+  {id: "textile", title: "legacy"},
+  {id: "md", title: "markdown"},
+  {id: "html", title: "html"},
+];
 
 export const DEFAULT_CONTENT_MODE_ID = CONTENT_MODES[0].id;
