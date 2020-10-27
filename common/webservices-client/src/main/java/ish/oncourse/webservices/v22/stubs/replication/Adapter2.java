36ebb95629687ef595f6455b438ffab6ab599c42
Rostislav Zenov <rostislav@ish.com.au>
2020-10-27 16:30:57 +0000

    OD-13634 fixed compiling
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65419 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/webservices-client/src/main/java/ish/oncourse/webservices/v22/stubs/replication/Adapter2.java
+++ common/webservices-client/src/main/java/ish/oncourse/webservices/v22/stubs/replication/Adapter2.java
@@ -1,7 +1,9 @@
 
 package ish.oncourse.webservices.v22.stubs.replication;
 
+import java.util.Calendar;
 import java.util.Date;
+import javax.xml.bind.DatatypeConverter;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class Adapter2
@@ -10,11 +12,19 @@ public class Adapter2
 
 
     public Date unmarshal(String value) {
-        return (org.apache.cxf.tools.common.DataTypeAdapter.parseDateTime(value));
+        if (value == null) {
+            return null;
+        }
+        return DatatypeConverter.parseDateTime(value).getTime();
     }
 
     public String marshal(Date value) {
-        return (org.apache.cxf.tools.common.DataTypeAdapter.printDateTime(value));
+        if (value == null) {
+            return null;
+        }
+        Calendar c = Calendar.getInstance();
+        c.setTime(value);
+        return DatatypeConverter.printDateTime(c);
     }
 
 }
