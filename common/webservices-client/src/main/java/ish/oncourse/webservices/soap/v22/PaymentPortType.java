9f4a19256e1137dbefc41a8ae2425b5acd8f0d5d
Artyom Kravenko <artyom@ish.com.au>
2020-10-30 08:58:57 +0000

    Clean up new replication version
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65465 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/webservices-client/src/main/java/ish/oncourse/webservices/soap/v22/PaymentPortType.java
+++ common/webservices-client/src/main/java/ish/oncourse/webservices/soap/v22/PaymentPortType.java
@@ -18,47 +18,18 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 @SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
 public interface PaymentPortType {
 
-    @WebMethod(action = "processPayment")
-    @WebResult(name = "paymentResponse", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/", partName = "transactionGroup")
-    public ish.oncourse.webservices.v22.stubs.replication.TransactionGroup processPayment(
-        @WebParam(partName = "transaction", name = "paymentRequest", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
-        ish.oncourse.webservices.v22.stubs.replication.TransactionGroup transaction,
-        @WebParam(partName = "paymentModel", name = "paymentModel", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
-        ish.oncourse.webservices.v22.stubs.replication.ParametersMap paymentModel
-    ) throws ReplicationFault;
-
-    @WebMethod(action = "processRefund")
-    @WebResult(name = "refundResponse", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/", partName = "transactionGroup")
-    public ish.oncourse.webservices.v22.stubs.replication.TransactionGroup processRefund(
-        @WebParam(partName = "paymentOut", name = "refundRequest", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
-        ish.oncourse.webservices.v22.stubs.replication.TransactionGroup paymentOut
-    ) throws ReplicationFault;
-
     @WebMethod(action = "getVouchers")
     @WebResult(name = "voucherResponse", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/", partName = "transactionResponse")
     public ish.oncourse.webservices.v22.stubs.replication.TransactionGroup getVouchers(
         @WebParam(partName = "transactionRequest", name = "voucherRequest", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
         ish.oncourse.webservices.v22.stubs.replication.TransactionGroup transactionRequest
     ) throws ReplicationFault;
-
-    @WebMethod(action = "verifyUSI")
-    @WebResult(name = "usiResponse", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/", partName = "verificationResponse")
-    public ish.oncourse.webservices.v22.stubs.replication.ParametersMap verifyUSI(
-        @WebParam(partName = "verificationRequest", name = "usiRequest", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
-        ish.oncourse.webservices.v22.stubs.replication.ParametersMap verificationRequest
-    ) throws ReplicationFault;
-
+    
     @WebMethod(action = "verifyCheckout")
     @WebResult(name = "checkoutResponse", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/", partName = "verifyCheckoutResponse")
     public ish.oncourse.webservices.v22.stubs.replication.ParametersMap verifyCheckout(
         @WebParam(partName = "verificationRequest", name = "checkoutRequest", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
         ish.oncourse.webservices.v22.stubs.replication.ParametersMap verificationRequest
     ) throws ReplicationFault;
-
-    @WebMethod(action = "getPaymentStatus")
-    @WebResult(name = "paymentStatusResponse", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/", partName = "transactionGroup")
-    public ish.oncourse.webservices.v22.stubs.replication.TransactionGroup getPaymentStatus(
-        @WebParam(partName = "sessionId", name = "paymentStatusRequest", targetNamespace = "http://repl.v22.soap.webservices.oncourse.ish/")
-        java.lang.String sessionId
-    ) throws ReplicationFault;
+    
 }
