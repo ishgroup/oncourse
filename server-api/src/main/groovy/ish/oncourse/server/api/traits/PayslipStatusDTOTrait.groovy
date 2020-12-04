5c456ff1882c301f529d1f726fff7467bf409ae9
Rostislav Zenov <rostislav@ish.com.au>
2020-12-05 05:51:56 +1100

    OD-15370 added payslip pay type dto model and added attribute to payslip dto model

--- server-api/src/main/groovy/ish/oncourse/server/api/traits/PayslipStatusDTOTrait.groovy
+++ server-api/src/main/groovy/ish/oncourse/server/api/traits/PayslipStatusDTOTrait.groovy
@@ -11,6 +11,41 @@
 
 package ish.oncourse.server.api.traits
 
+import ish.common.types.PayslipStatus
+import ish.oncourse.server.api.v1.model.PayslipStatusDTO
+
 trait PayslipStatusDTOTrait {
 
+    PayslipStatus getDbType() {
+        switch (this as PayslipStatusDTO) {
+            case PayslipStatusDTO.NEW:
+                return PayslipStatus.HOLLOW
+            case PayslipStatusDTO.COMPLETED:
+                return PayslipStatus.COMPLETED
+            case PayslipStatusDTO.APPROVED:
+                return PayslipStatus.APPROVED
+            case PayslipStatusDTO.PAID_EXPORTED:
+                return PayslipStatus.FINALISED
+            default:
+                throw new IllegalArgumentException("Wrong payslip status: ${toString()}")
+        }
+    }
+
+    PayslipStatusDTO fromDbType(PayslipStatus status) {
+        if (!status) {
+            return null
+        }
+        switch (status) {
+            case PayslipStatus.HOLLOW:
+                return PayslipStatusDTO.NEW
+            case PayslipStatus.COMPLETED:
+                return PayslipStatusDTO.COMPLETED
+            case PayslipStatus.APPROVED:
+                return PayslipStatusDTO.APPROVED
+            case PayslipStatus.FINALISED:
+                return PayslipStatusDTO.PAID_EXPORTED
+            default:
+                throw new IllegalArgumentException("Wrong payslip status: $status.displayName")
+        }
+    }
 }
