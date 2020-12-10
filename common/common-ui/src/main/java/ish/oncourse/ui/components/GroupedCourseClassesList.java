1f5a0f30cbcfe3cc11b291a951ee87bb52b4d3b4
Artyom Kravenko <artyom@ish.com.au>
2020-12-10 13:03:46 +0000

    OD-15352 New component to group classes by site
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65689 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/common-ui/src/main/java/ish/oncourse/ui/components/GroupedCourseClassesList.java
@@ -0,0 +1,69 @@
+package ish.oncourse.ui.components;
+
+import ish.math.Money;
+import ish.oncourse.model.CourseClass;
+import ish.oncourse.model.Site;
+import ish.oncourse.model.TutorRole;
+import ish.oncourse.ui.base.ISHCommon;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import static java.util.stream.Collectors.*;
+import java.util.*;
+import java.util.function.Function;
+import java.text.Format;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupedCourseClassesList  extends ISHCommon {
+
+    @Parameter
+    @Property
+    private boolean linkToLocationsMap;
+
+    @Parameter
+    @Property
+    private Format startDateFormat;
+
+    @Parameter
+    @Property
+    private boolean allowByAplication;
+
+    @Parameter
+    @Property
+    private Money feeOverride;
+
+    @SuppressWarnings("all")
+    @Parameter
+    @Property
+    private boolean isList;
+    
+    @Parameter
+    private List<CourseClass> courseClasses;
+
+    @Property
+    private Map<String, List<CourseClass>> sitesMap;
+
+    @Property
+    private Map.Entry<String, List<CourseClass>> siteEntry;
+
+    @Property
+    private List<CourseClass> unassignedClasses;
+    
+    @Property
+    private CourseClass courseClass;
+
+
+    @SetupRender
+    public void beforeRender() {
+        unassignedClasses =  courseClasses.stream().filter(cc -> cc.getRoom() == null).sorted().collect(Collectors.toList());
+
+        sitesMap= courseClasses.stream().filter(cc -> cc.getRoom() != null)
+                .collect(Collectors.groupingBy(cc -> cc.getRoom().getSite().getName(), TreeMap::new, Collectors.toList()));
+        
+    }
+    
+
+
+}
