199e4abce54c4a65e44e734b14923b642013f705
Artyom Kravenko <artyom@ish.com.au>
2020-12-11 08:44:41 +0000

    OD-15353 implement all kind of params (course, class, id, tagName, count) for tutor details component
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65691 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/common-ui/src/main/java/ish/oncourse/ui/components/TutorsDetails.java
+++ common/common-ui/src/main/java/ish/oncourse/ui/components/TutorsDetails.java
@@ -1,15 +1,15 @@
 package ish.oncourse.ui.components;
 
-import ish.oncourse.model.Course;
-import ish.oncourse.model.CourseClass;
-import ish.oncourse.model.Tutor;
+import ish.oncourse.model.*;
 import ish.oncourse.services.IRichtextConverter;
 import ish.oncourse.services.binary.IBinaryDataService;
+import ish.oncourse.services.html.IPlainTextExtractor;
 import ish.oncourse.services.persistence.ICayenneService;
 import ish.oncourse.services.site.IWebSiteService;
-import ish.oncourse.services.site.WebSiteService;
 import ish.oncourse.ui.base.ISHCommon;
 import ish.oncourse.util.ValidationErrors;
+import ish.oncourse.utils.StringUtilities;
+import org.apache.cayenne.exp.ExpressionFactory;
 import org.apache.cayenne.query.ObjectSelect;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.tapestry5.annotations.Parameter;
@@ -17,10 +17,13 @@ import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TutorsDetails extends ISHCommon {
+    
+    @Inject
+    private IPlainTextExtractor extractor;
     
     @Inject
     private IBinaryDataService binaryDataService;
@@ -45,7 +48,7 @@ public class TutorsDetails extends ISHCommon {
 
     @Parameter
     private String tagName;
-    
+
     @Parameter
     private Integer count;
 
@@ -63,11 +66,54 @@ public class TutorsDetails extends ISHCommon {
                     .and(Tutor.COLLEGE.eq(webSiteService.getCurrentCollege()))
                     .select(cayenneService.newContext());
         } else if (course != null) {
-//            tutors = course.getCourseClasses().stream().map(CourseClass::getTutorRoles)
+            tutors = course.getCourseClasses()
+                    .stream().map(CourseClass::getTutorRoles)
+                    .flatMap(Collection::stream).map(TutorRole::getTutor)
+                    .collect(Collectors.toList());
         } else if (courseClass != null) {
-//            tutors = course.getCourseClasses().stream().map(CourseClass::getTutorRoles)
+            tutors = courseClass.getTutorRoles()
+                    .stream().map(TutorRole::getTutor)
+                    .collect(Collectors.toList());
         } else if (tagName != null) {
-//            tutors = course.getCourseClasses().stream().map(CourseClass::getTutorRoles)
+            //Count can be no higher than 12 and defaults to 3 if no count is provided.
+            if (count == null || count < 1 ) {
+                count = 3;
+            } else if (count  > 12) {
+                count = 12;
+            }
+
+            List<Long> ids = ObjectSelect.query(Taggable.class)
+                    .where(Taggable.ENTITY_IDENTIFIER.eq("Contact"))
+                    .and(Taggable.COLLEGE.eq(webSiteService.getCurrentCollege()))
+                    .and(TaggableTag.TAG.dot(Tag.NAME).eq(tagName))
+                    .column(Taggable.ENTITY_WILLOW_ID).select(cayenneService.newContext());
+
+            if (ids.isEmpty()) {
+                return;
+            }
+            
+            List<Long> randomIds = new ArrayList<>();
+            Integer size = ids.size();
+            
+            if (count >= size) {
+                randomIds = ids;
+            } else {
+                Random ran = new Random();
+                while (count > 0) {
+                    int index = ran.nextInt(size);
+                    randomIds.add(ids.get(index));
+                    count--;
+                }
+
+            }
+
+            tutors = ObjectSelect.query(Contact.class)
+                    .where(ExpressionFactory.matchDbExp(Contact.ID_PK_COLUMN, randomIds))
+                    .and(Contact.TUTOR.isNotNull())
+                    .select(cayenneService.newContext())
+                    .stream().map(Contact::getTutor)
+                    .collect(Collectors.toList());
+                    
         }
 
     }
@@ -77,11 +123,17 @@ public class TutorsDetails extends ISHCommon {
     }
 
     public String getResume() {
+        String result = null;
         String resume = StringUtils.trimToNull(tutor.getResume());
         if (resume != null) {
-            return textileConverter.convertCustomText(resume, new ValidationErrors());
+            result = textileConverter.convertCustomText(resume, new ValidationErrors());
         } else {
-            return String.format("%s. Tutor of %s", tutor.getFullName(), webSiteService.getCurrentCollege().getName());
+            result = String.format("%s. Tutor of %s", tutor.getFullName(), webSiteService.getCurrentCollege().getName());
+        }
+        if (result.length() > 200) {
+            return StringUtilities.abbreviate(extractor.extractFromHtml(result), 200);
+        } else {
+            return result;
         }
     }
 }
