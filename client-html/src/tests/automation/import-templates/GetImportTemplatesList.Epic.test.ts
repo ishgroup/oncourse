65da860298f3d707f9aee35e6911f96a5e4617b6
Chintan Kotadia <chintan@ish.com.au>
2020-12-22 17:39:31 +0530

    OD-15411 Added jest test for automation email templates (get, create, update, delete and list), and added default test epic

---
+++ client-html/src/tests/automation/import-templates/GetImportTemplatesList.Epic.test.ts
@@ -0,0 +1,36 @@
+import { mockedAPI } from "../../TestEntry";
+import { EpicGetImportTemplatesList } from "../../../js/containers/automation/containers/import-templates/epics/EpicGetImportTemplatesList";
+import {
+  GET_IMPORT_TEMPLATES_LIST,
+  GET_IMPORT_TEMPLATES_LIST_FULFILLED
+} from "../../../js/containers/automation/containers/import-templates/actions";
+import { CommonListItem } from "../../../js/model/common/sidebar";
+import { DefaultEpic } from "../../common/Default.Epic";
+
+describe("Get import templates list epic tests", () => {
+  it("EpicGetImportTemplatesList should returns correct values", () => 
+     DefaultEpic({
+      action: {
+        type: GET_IMPORT_TEMPLATES_LIST
+      },
+      epic: EpicGetImportTemplatesList,
+      processData: () => {
+        const importTemplatesResponse = mockedAPI.db.getImportTemplates();
+
+        const importTemplates: CommonListItem[] = importTemplatesResponse.rows.map(r => ({
+          id: Number(r.id),
+          name: r.values[0],
+          keyCode: r.values[1],
+          hasIcon: r.values[1].startsWith("ish."),
+          grayOut: r.values[2] === "false"
+        }));
+
+        return [
+          {
+            type: GET_IMPORT_TEMPLATES_LIST_FULFILLED,
+            payload: { importTemplates }
+          }
+        ];
+      }
+    }));
+});
