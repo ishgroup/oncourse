cdec0048b72594320c36a6097fb62344b23041ff
Chintan Kotadia <chintan@ish.com.au>
2020-12-24 13:23:35 +0530

    OD-15411 Added jest test in automation for get pdf backgrounds list epic

---
+++ client-html/src/tests/automation/pdf-backgrounds/GetPdfBackgroundsList.Epic.test.ts
@@ -0,0 +1,35 @@
+import { mockedAPI } from "../../TestEntry";
+import { CommonListItem } from "../../../js/model/common/sidebar";
+import { DefaultEpic } from "../../common/Default.Epic";
+import {
+  GET_AUTOMATION_PDF_BACKGROUNDS_LIST,
+  GET_AUTOMATION_PDF_BACKGROUNDS_LIST_FULFILLED
+} from "../../../js/containers/automation/containers/pdf-backgrounds/actions";
+import { EpicGetPdfBackgroundsList } from "../../../js/containers/automation/containers/pdf-backgrounds/epics/EpicGetPdfBackgroundsList";
+
+describe("Get pdf backgrounds list epic tests", () => {
+  it("EpicGetPdfBackgroundsList should returns correct values", () =>
+    DefaultEpic({
+      action: {
+        type: GET_AUTOMATION_PDF_BACKGROUNDS_LIST
+      },
+      epic: EpicGetPdfBackgroundsList,
+      processData: () => {
+        const pdfBackgroundsResponse = mockedAPI.db.getReportOverlays();
+
+        const pdfBackgrounds: CommonListItem[] = pdfBackgroundsResponse.rows.map(r => ({
+          id: Number(r.id),
+          name: r.values[0],
+          isPortrait: r.values[1] === "true",
+          hasIcon: true
+        }));
+
+        return [
+          {
+            type: GET_AUTOMATION_PDF_BACKGROUNDS_LIST_FULFILLED,
+            payload: { pdfBackgrounds }
+          }
+        ];
+      }
+    }));
+});
