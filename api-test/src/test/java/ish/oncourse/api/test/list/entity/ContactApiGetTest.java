f33a352f4083bc61f17a6ccc826d9f833755c44a
Artyom Kravchenko <artyom@ish.com.au>
2020-12-28 20:06:39 +1100

    OD-15082 Fix Membership DTO schema

--- api-test/src/test/java/ish/oncourse/api/test/list/entity/ContactApiGetTest.java
+++ api-test/src/test/java/ish/oncourse/api/test/list/entity/ContactApiGetTest.java
@@ -5,16 +5,26 @@
 
 package ish.oncourse.api.test.list.entity;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import com.intuit.karate.junit4.Karate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.slf4j.Log4jMarker;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.apache.logging.log4j.Logger;
 
 import static org.junit.Assert.assertEquals;
 
 public class ContactApiGetTest {
+    Logger logger = LogManager.getLogger();
     @Test
     public void test() {
         Results results = Runner.path("classpath:ish/oncourse/api/test/list/entity/contact/get.feature").tags("~@ignore").parallel(1);
+        if ( results.getFailCount()>0) {
+            logger.error(results.getErrorMessages());
+        }
         assertEquals(results.getErrorMessages(), results.getFailCount(), 0);
     }
 }
