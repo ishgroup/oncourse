35db179ae37d37a6d9c6a72942273cf5694ff3d1
Artyom Kravchenko <artyom@ish.com.au>
2020-12-31 19:24:01 +1100

    OD-15082 print standard output stream for failed tests - simplify debugging

--- api-test/src/test/java/ish/oncourse/api/test/list/entity/ContactApiGetTest.java
+++ api-test/src/test/java/ish/oncourse/api/test/list/entity/ContactApiGetTest.java
@@ -5,15 +5,9 @@
 
 package ish.oncourse.api.test.list.entity;
 
-import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit4.Karate;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.slf4j.Log4jMarker;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.apache.logging.log4j.Logger;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,9 +15,6 @@ public class ContactApiGetTest {
     @Test
     public void test() {
         Results results = Runner.path("classpath:ish/oncourse/api/test/list/entity/contact/get.feature").tags("~@ignore").parallel(1);
-        if ( results.getFailCount()>0) {
-            System.out.println(results.getErrorMessages());
-        }
         assertEquals(results.getErrorMessages(), results.getFailCount(), 0);
     }
 }
