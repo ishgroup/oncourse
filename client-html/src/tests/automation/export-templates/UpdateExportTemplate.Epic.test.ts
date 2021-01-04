dd4630d1137f8a41015aea296b0662e2d6a3eb13
Chintan Kotadia <chintan@ish.com.au>
2021-01-04 16:39:15 +0530

    OD-15411 Added jest test in automation for update export template epic

---
+++ client-html/src/tests/automation/export-templates/UpdateExportTemplate.Epic.test.ts
@@ -0,0 +1,46 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import {
+  GET_EXPORT_TEMPLATE, GET_EXPORT_TEMPLATES_LIST,
+  UPDATE_EXPORT_TEMPLATE,
+  UPDATE_EXPORT_TEMPLATE_FULFILLED, updateExportTemplate
+} from "../../../js/containers/automation/containers/export-templates/actions";
+import { EpicUpdateExportTemplate } from "../../../js/containers/automation/containers/export-templates/epics/EpicUpdateExportTemplate";
+
+describe("Update export template epic tests", () => {
+  it("EpicUpdateExportTemplate should returns correct values", () => DefaultEpic({
+    action: updateExportTemplate({
+      id: 1,
+      name: "name 1",
+      keyCode: "keyCode 1",
+      enabled: false
+    })/*{
+      type: UPDATE_EXPORT_TEMPLATE,
+      payload: {
+        exportTemplate: {
+          id: 1,
+          name: "name 1",
+          keyCode: "keyCode 1",
+          enabled: false
+        }
+      }
+    }*/,
+    epic: EpicUpdateExportTemplate,
+    processData: () => [
+      {
+        type: UPDATE_EXPORT_TEMPLATE_FULFILLED
+      },
+      {
+        type: GET_EXPORT_TEMPLATE,
+        payload: 1
+      },
+      {
+        type: GET_EXPORT_TEMPLATES_LIST
+      },
+      {
+        type: FETCH_SUCCESS,
+        payload: { message: "Export template updated" }
+      }
+    ]
+  }));
+});
