c211f72171f1fa25675b48506d5a463344ad9a21
Chintan Kotadia <chintan@ish.com.au>
2021-01-04 16:56:41 +0530

    OD-15411 Added jest test in automation for get export templates list epic and updated default epic

---
+++ client-html/src/tests/automation/export-templates/GetExportTemplatesList.Epic.test.ts
@@ -0,0 +1,32 @@
+import { CommonListItem } from "../../../js/model/common/sidebar";
+import { DefaultEpic } from "../../common/Default.Epic";
+import {
+  GET_EXPORT_TEMPLATES_LIST_FULFILLED,
+  getExportTemplatesList
+} from "../../../js/containers/automation/containers/export-templates/actions";
+import { EpicGetExportTemplatesList } from "../../../js/containers/automation/containers/export-templates/epics/EpicGetExportTemplatesList";
+
+describe("Get export templates list epic tests", () => {
+  it("EpicGetExportTemplatesList should returns correct values", () => DefaultEpic({
+    action: getExportTemplatesList(),
+    epic: EpicGetExportTemplatesList,
+    processData: mockedAPI => {
+      const exportTemplatesResponse = mockedAPI.db.getExportTemplates();
+
+      const exportTemplates: CommonListItem[] = exportTemplatesResponse.rows.map(r => ({
+        id: Number(r.id),
+        name: r.values[0],
+        keyCode: r.values[1],
+        hasIcon: r.values[1].startsWith("ish."),
+        grayOut: r.values[2] === "false"
+      }));
+
+      return [
+        {
+          type: GET_EXPORT_TEMPLATES_LIST_FULFILLED,
+          payload: { exportTemplates }
+        }
+      ];
+    }
+  }));
+});
