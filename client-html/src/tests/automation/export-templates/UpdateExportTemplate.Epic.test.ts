d1c525e391f5cb56f4a58b1fa8a4942ea621e3e9
Chintan Kotadia <chintan@ish.com.au>
2021-01-04 16:44:20 +0530

    OD-15411 Removed commented code from update export template epic test

--- client-html/src/tests/automation/export-templates/UpdateExportTemplate.Epic.test.ts
+++ client-html/src/tests/automation/export-templates/UpdateExportTemplate.Epic.test.ts
@@ -1,9 +1,10 @@
 import { DefaultEpic } from "../../common/Default.Epic";
 import { FETCH_SUCCESS } from "../../../js/common/actions";
 import {
-  GET_EXPORT_TEMPLATE, GET_EXPORT_TEMPLATES_LIST,
-  UPDATE_EXPORT_TEMPLATE,
-  UPDATE_EXPORT_TEMPLATE_FULFILLED, updateExportTemplate
+  GET_EXPORT_TEMPLATE,
+  GET_EXPORT_TEMPLATES_LIST,
+  UPDATE_EXPORT_TEMPLATE_FULFILLED,
+  updateExportTemplate
 } from "../../../js/containers/automation/containers/export-templates/actions";
 import { EpicUpdateExportTemplate } from "../../../js/containers/automation/containers/export-templates/epics/EpicUpdateExportTemplate";
 
@@ -14,17 +15,7 @@ describe("Update export template epic tests", () => {
       name: "name 1",
       keyCode: "keyCode 1",
       enabled: false
-    })/*{
-      type: UPDATE_EXPORT_TEMPLATE,
-      payload: {
-        exportTemplate: {
-          id: 1,
-          name: "name 1",
-          keyCode: "keyCode 1",
-          enabled: false
-        }
-      }
-    }*/,
+    }),
     epic: EpicUpdateExportTemplate,
     processData: () => [
       {
