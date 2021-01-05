46c3adee7c6cc7547f53cde22a1369e74df6726b
Chintan Kotadia <chintan@ish.com.au>
2021-01-05 18:55:10 +0530

    OD-15411 Added jest test in automation for create script epic

---
+++ client-html/src/tests/automation/scripts/CreateScript.Epic.test.ts
@@ -0,0 +1,35 @@
+import { initialize } from "redux-form";
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import {
+  createScriptItem,
+  getScriptsList,
+  POST_SCRIPT_ENTITY_FULFILLED
+} from "../../../js/containers/automation/containers/scripts/actions";
+import { EpicCreateScriptItem } from "../../../js/containers/automation/containers/scripts/epics/EpicCreateScriptItem";
+import { SCRIPT_EDIT_VIEW_FORM_NAME } from "../../../js/containers/automation/containers/scripts/constants";
+
+describe("Create script epic tests", () => {
+  it("EpicCreateScriptItem should returns correct values", () => {
+    const script = {
+      name: "script 21",
+      enabled: true,
+      nextRun: new Date().toISOString()
+    };
+    return DefaultEpic({
+      action: createScriptItem(script),
+      epic: EpicCreateScriptItem,
+      processData: () => [
+        {
+          type: POST_SCRIPT_ENTITY_FULFILLED
+        },
+        {
+          type: FETCH_SUCCESS,
+          payload: { message: "New Script created" }
+        },
+        getScriptsList(script.name),
+        initialize(SCRIPT_EDIT_VIEW_FORM_NAME, script)
+      ]
+    });
+  });
+});
