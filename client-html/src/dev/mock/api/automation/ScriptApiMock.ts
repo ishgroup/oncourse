196145adc4f27300bf066dbb86abc4f522cd9023
Chintan Kotadia <chintan@ish.com.au>
2021-01-05 19:58:57 +0530

    OD-15411 Provide header in mock response for resolve/reject, Added jest test in automation for get run script result epic, and added mock for get script execute

--- client-html/src/dev/mock/api/automation/ScriptApiMock.ts
+++ client-html/src/dev/mock/api/automation/ScriptApiMock.ts
@@ -1,15 +1,16 @@
 import { promiseResolve } from "../../MockAdapter";
 
 export function ScriptApiMock(mock) {
+  this.api.onGet(new RegExp(`v1/list/entity/script/execute/\\d+`))
+    .reply(config => promiseResolve(config, null, { "content-type": "application/pdf" }));
+
   this.api.onGet(new RegExp(`v1/list/entity/script/\\d+`)).reply(config => {
     const params = config.url.split("/");
     const id = params[params.length - 1];
     return promiseResolve(config, this.db.getScript(id));
   });
 
-  this.api.onPatch(new RegExp(`v1/list/entity/script/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
+  this.api.onPatch(new RegExp(`v1/list/entity/script/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 
   this.api.onPost("v1/list/entity/script").reply(config => {
     this.db.createScript(config.data);
