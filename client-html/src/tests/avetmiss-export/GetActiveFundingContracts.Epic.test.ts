0dd4cba66b1f967a0eda431916fd990a6ff51995
Chintan Kotadia <chintan@ish.com.au>
2021-01-06 13:50:38 +0530

    OD-15411 Added jest test in avetmiss export for GetAvetmiss8ExportID epic, provide default mockedApi in default epic and updated GetActiveFundingContracts epic test

--- client-html/src/tests/avetmiss-export/GetActiveFundingContracts.Epic.test.ts
+++ client-html/src/tests/avetmiss-export/GetActiveFundingContracts.Epic.test.ts
@@ -1,46 +1,35 @@
-import { ActionsObservable } from "redux-observable";
-import { store, mockedAPI } from "../TestEntry";
-import { FETCH_START, FETCH_FINISH } from "../../js/common/actions";
+import { mockedAPI } from "../TestEntry";
 import {
   GET_ACTIVE_FUNDING_CONTRACTS_REQUEST,
   GET_ACTIVE_FUNDING_CONTRACTS_FULFILLED
 } from "../../js/containers/avetmiss-export/actions";
 import { EpicGetActiveFundingContracts } from "../../js/containers/avetmiss-export/epics/EpicGetActiveFundingContracts";
-import { filter, toArray } from "rxjs/operators";
+import { DefaultEpic } from "../common/Default.Epic";
 
 describe("Get active funding contracts epics tests", () => {
-  it("GetActiveFundingContracts should return correct actions", () => {
-    // Expected response
-    const response = mockedAPI.db.getAvetmissExportPlainList();
+  it("GetActiveFundingContracts should return correct actions", () => DefaultEpic({
+    action: {
+      type: GET_ACTIVE_FUNDING_CONTRACTS_REQUEST
+    },
+    epic: EpicGetActiveFundingContracts,
+    processData: () => {
+      // Expected response
+      const response = mockedAPI.db.getAvetmissExportPlainList();
 
-    // Redux action to trigger epic
-    const action$ = ActionsObservable.of({ type: GET_ACTIVE_FUNDING_CONTRACTS_REQUEST });
+      const contracts: any[] = response.rows.map(({ id, values }) => ({
+        id: Number(id),
+        name: values[0],
+        flavour: values[1]
+      }));
 
-    // Initializing epic instance
-    const epic$ = EpicGetActiveFundingContracts(action$, store, {});
+      contracts.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
 
-    const contracts: any[] = response.rows.map(({ id, values }) => ({
-      id: Number(id),
-      name: values[0],
-      flavour: values[1]
-    }));
-
-    contracts.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
-
-    // Testing epic to be resolved with array of actions
-    return expect(
-      epic$
-        .pipe(
-          // Filtering common actions
-          filter(a => [FETCH_START, FETCH_FINISH].includes(a.type) === false),
-          toArray()
-        )
-        .toPromise()
-    ).resolves.toEqual([
-      {
-        type: GET_ACTIVE_FUNDING_CONTRACTS_FULFILLED,
-        payload: { contracts }
-      }
-    ]);
-  });
+      return [
+        {
+          type: GET_ACTIVE_FUNDING_CONTRACTS_FULFILLED,
+          payload: { contracts }
+        }
+      ];
+    }
+  }));
 });
