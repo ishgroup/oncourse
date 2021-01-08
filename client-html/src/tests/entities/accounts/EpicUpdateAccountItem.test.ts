7b06c29dacc97de077349b213a8f6da1eb3eb77d
Chintan Kotadia <chintan@ish.com.au>
2021-01-08 21:18:02 +0530

    OD-15411 Added jest tests in accounts entity for update account item epic

---
+++ client-html/src/tests/entities/accounts/EpicUpdateAccountItem.test.ts
@@ -0,0 +1,40 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import {
+  GET_ACCOUNT_ITEM,
+  updateAccount
+} from "../../../js/containers/entities/accounts/actions";
+import { EpicUpdateAccountItem } from "../../../js/containers/entities/accounts/epics/EpicUpdateAccountItem";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
+
+describe("Update account epic tests", () => {
+  it("EpicUpdateAccountItem should returns correct values", () => {
+    const accountId: number = 2;
+    return DefaultEpic({
+      action: updateAccount("2", {
+        accountCode: "accountCode 2",
+        description: "description 3",
+        id: accountId,
+        isDefaultAccount: true,
+        isEnabled: true,
+        tax: null,
+        type: "asset"
+      }),
+      epic: EpicUpdateAccountItem,
+      processData: () => [
+        {
+          type: FETCH_SUCCESS,
+          payload: { message: "Account Record updated" }
+        },
+        {
+          type: GET_RECORDS_REQUEST,
+          payload: { entity: "Account", listUpdate: true, savedID: String(accountId) }
+        },
+        {
+          type: GET_ACCOUNT_ITEM,
+          payload: String(accountId)
+        }
+      ]
+    });
+  });
+});
