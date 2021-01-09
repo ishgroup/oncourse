d70c04c81cf3f6eb2d75188f6eaf59e0f14cea45
Chintan Kotadia <chintan@ish.com.au>
2021-01-09 17:04:59 +0530

    OD-15411 Added default mocked edit view components, added account edit view components test, and updated audit edit view components test

---
+++ client-html/src/tests/entities/accounts/Account.Components.test.tsx
@@ -0,0 +1,18 @@
+import { mockedEditView } from "../../common/MockedEditView.Components";
+import AccountsEditView from "../../../js/containers/entities/accounts/components/AccountsEditView";
+
+describe("Virtual rendered AccountsEditView", () => {
+  it("Accounts components should render with given values", () => {
+    mockedEditView({
+      entity: "Account",
+      EditView: AccountsEditView,
+      record: mockecApi => mockecApi.db.getAccount(1),
+      render: (wrapper, initialValues) => {
+        expect(wrapper.find("#accountCode").text()).toContain(initialValues.accountCode);
+        expect(wrapper.find("#type").text()).toContain(initialValues.type.toString());
+        expect(wrapper.find("#description").text()).toContain(initialValues.description);
+        expect(wrapper.find('input[type="checkbox"]').props().checked).toEqual(initialValues.isEnabled);
+      }
+    });
+  });
+});
