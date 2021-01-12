b4377d5a6a0b3f486e84f12b49ba3a98541ddde3
Chintan Kotadia <chintan@ish.com.au>
2021-01-12 18:29:13 +0530

    OD-15411 Upgraded default mocked edit view to support material-ui global classes, added *.gif files for transform in jest test, and updated audit components test

--- client-html/src/tests/TestEntry.tsx
+++ client-html/src/tests/TestEntry.tsx
@@ -4,9 +4,13 @@ import { configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { StateObservable } from "redux-observable";
 import { Subject } from "rxjs";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import TestStore from "../js/constants/Store";
 import RootComponent from "../js/RootComponent";
 import { initMockDB } from "../dev/mock/MockAdapter";
+import GlobalStylesProvider from "../js/common/styles/GlobalStylesProvider";
+import { darkTheme } from "../js/common/themes/ishTheme";
 
 // Configuring virtual rendering library and mockedAPI class
 
@@ -18,6 +22,13 @@ configure({ adapter: new Adapter() });
 
 export const TestEntry = ({ children }) => (
   <Provider store={TestStore as any}>
-    <RootComponent>{children}</RootComponent>
+    <RootComponent>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <GlobalStylesProvider>
+          {children}
+        </GlobalStylesProvider>
+      </ThemeProvider>
+    </RootComponent>
   </Provider>
 );
--- client-html/src/tests/audit/Audit.Components.test.tsx
+++ client-html/src/tests/audit/Audit.Components.test.tsx
@@ -4,18 +4,16 @@ import { III_DD_MMM_YYYY_HH_MM } from "../../js/common/utils/dates/format";
 import { mockedEditView } from "../common/MockedEditView.Components";
 
 describe("Virtual rendered AuditsEditView", () => {
-  it("Audit components should render with given values", () => {
-    mockedEditView({
-      entity: "Audit",
-      EditView: AuditsEditView,
-      record: mockedApi => mockedApi.db.getAudit(1),
-      render: (wrapper, initialValues) => {
-        expect(wrapper.find("#created").text()).toContain(format(new Date(initialValues.created), III_DD_MMM_YYYY_HH_MM));
-        expect(wrapper.find("#entityIdentifier").text()).toContain(initialValues.entityIdentifier);
-        expect(wrapper.find("#entityId").text()).toContain(initialValues.entityId);
-        expect(wrapper.find("#action").text()).toContain(initialValues.action);
-        expect(wrapper.find("#message").text()).toContain(initialValues.message);
-      }
-    });
+  mockedEditView({
+    entity: "Audit",
+    EditView: AuditsEditView,
+    record: mockedApi => mockedApi.db.getAudit(1),
+    render: (wrapper, initialValues) => {
+      expect(wrapper.find("#created").text()).toContain(format(new Date(initialValues.created), III_DD_MMM_YYYY_HH_MM));
+      expect(wrapper.find("#entityIdentifier").text()).toContain(initialValues.entityIdentifier);
+      expect(wrapper.find("#entityId").text()).toContain(initialValues.entityId);
+      expect(wrapper.find("#action").text()).toContain(initialValues.action);
+      expect(wrapper.find("#message").text()).toContain(initialValues.message);
+    }
   });
 });
