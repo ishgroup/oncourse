a3c03f86e6e9bbdda467b2c728a773ddf111c07d
Chintan Kotadia <chintan@ish.com.au>
2021-01-13 17:22:14 +0530

    OD-15411 Updated all get entities list epic and common get entities

--- client-html/src/tests/entities/assessments/GetAssessmentEntities.Epic.test.ts
+++ client-html/src/tests/entities/assessments/GetAssessmentEntities.Epic.test.ts
@@ -2,10 +2,6 @@ import { mockedAPI } from "../../TestEntry";
 import { GetEntities } from "../../common/GetEntities.Epic";
 
 describe("Get assessment entities epic tests", () => {
-  it("GetAssessmentEntities should returns correct actions", () => {
-    // Expected response
-    const assessment = mockedAPI.db.getAssessments();
-
-    return GetEntities("Assessment", assessment);
-  });
+  it("GetAssessmentEntities should returns correct actions", () =>
+    GetEntities("Assessment", mockedAPI.db.getAssessments()));
 });
