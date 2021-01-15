61961642dd2fe4afe80ffdcc7fec1520f5e960c3
Chintan Kotadia <chintan@ish.com.au>
2021-01-15 17:27:59 +0530

    OD-15411 Added jest tests in certificates entity for update certificate epic

---
+++ client-html/src/tests/entities/certificates/EpicUpdateCertificateItem.test.ts
@@ -0,0 +1,33 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
+import { EpicUpdateCertificateItem } from "../../../js/containers/entities/certificates/epics/EpicUpdateCertificateItem";
+import {
+  GET_CERTIFICATE_ITEM,
+  UPDATE_CERTIFICATE_ITEM_FULFILLED,
+  updateCertificate
+} from "../../../js/containers/entities/certificates/actions";
+
+describe("Update certificate epic tests", () => {
+  it("EpicUpdateCertificateItem should returns correct values", () => DefaultEpic({
+    action: mockedApi => updateCertificate("1", mockedApi.db.createNewCertificate(1)),
+    epic: EpicUpdateCertificateItem,
+    processData: () => [
+      {
+        type: UPDATE_CERTIFICATE_ITEM_FULFILLED
+      },
+      {
+        type: FETCH_SUCCESS,
+        payload: { message: "Certificate Record updated" }
+      },
+      {
+        type: GET_RECORDS_REQUEST,
+        payload: { entity: "Certificate", listUpdate: true, savedID: "1" }
+      },
+      {
+        type: GET_CERTIFICATE_ITEM,
+        payload: "1"
+      }
+    ]
+  }));
+});
