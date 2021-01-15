df9317c6da30641ed195f63a3850aa52ea38c2d3
Rostislav Zenov <rostislav@ish.com.au>
2021-01-15 18:32:54 +1100

    OD-15438 allow create password for system user by invitation without authentication

--- server/src/main/groovy/ish/oncourse/server/api/servlet/ApiFilter.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/servlet/ApiFilter.groovy
@@ -28,6 +28,7 @@ class ApiFilter implements Filter {
 
     private static final String LOGIN_PATH_INFO = '/v1/login'
     private static final String CHECK_PASSWORD_INFO = '/v1/user/checkPassword/'
+    private static final String INVITATION = '/v1/invite/'
     private static final String X_VALIDATE_ONLY = 'x-validate-only'
     private static final String XVALIDATEONLY = 'XValidateOnly'
 
@@ -60,6 +61,11 @@ class ApiFilter implements Filter {
         } else if (hasErrors(request, response) || !permissionService.authorize(request, response)) {
             return
         }
+        if (request.pathInfo == LOGIN_PATH_INFO || request.pathInfo.contains(CHECK_PASSWORD_INFO) || request.pathInfo.contains(INVITATION)) {
+            allowCrossOriginRequest(response)
+        } else if (hasErrors(request, response) || !permissionService.authorize(request, response)) {
+            return
+        }
 
         chain.doFilter(request, response)
     }
@@ -75,3 +81,4 @@ class ApiFilter implements Filter {
         response.addHeader('Access-Control-Allow-Headers', 'Content-Type, Ish-JXBrowser-Header')
     }
 }
+
