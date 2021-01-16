ec15dee59392d8fa1724db61f4f65f1dfb542d3a
Chintan Kotadia <chintan@ish.com.au>
2021-01-16 15:49:25 +0530

    OD-15411 Added jest tests in contacts entity for get merge contacts epic, and added mock route and data for merge contacts

--- client-html/src/dev/mock/api/entities/ContactApiMock.ts
+++ client-html/src/dev/mock/api/entities/ContactApiMock.ts
@@ -7,9 +7,7 @@ export function ContactApiMock(mock) {
     return promiseResolve(config, this.db.getContact(id));
   });
 
-  this.api.onPut(new RegExp(`v1/list/entity/contact/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
+  this.api.onPut(new RegExp(`v1/list/entity/contact/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 
   this.api.onPost("v1/list/entity/contact").reply(config => {
     this.db.createContact(config.data);
@@ -22,4 +20,6 @@ export function ContactApiMock(mock) {
     this.db.removeContact(id);
     return promiseResolve(config, this.db.getContacts());
   });
+
+  this.api.onGet("v1/list/entity/contact/merge").reply(config => promiseResolve(config, this.db.getMergeContacts()));
 }
