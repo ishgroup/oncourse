c6f89d392b75d5669eeb32eb537c36b4069b5b63
Chintan Kotadia <chintan@ish.com.au>
2021-01-16 14:01:07 +0530

    OD-15411 Added jest tests in contacts entity for get contact relation types epic

---
+++ client-html/src/tests/entities/contacts/EpicGetContactsRelationTypes.test.ts
@@ -0,0 +1,32 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import {
+  GET_CONTACTS_RELATION_TYPES_FULFILLED,
+  getContactsRelationTypes
+} from "../../../js/containers/entities/contacts/actions";
+import { EpicGetContactsRelationTypes } from "../../../js/containers/entities/contacts/epics/EpicGetContactsRelationTypes";
+
+describe("Get contact relation types types epic tests", () => {
+  it("EpicGetContactsRelationTypes should returns correct values", () => DefaultEpic({
+    action: getContactsRelationTypes(),
+    epic: EpicGetContactsRelationTypes,
+    processData: mockedApi => {
+      const items = mockedApi.db.contactRelationTypes;
+
+      const contactsRelationTypes = items.reduce((acc, r) => {
+        const { relationName, reverseRelationName, id } = r;
+
+        acc.push({ label: relationName, value: id, isReverseRelation: false });
+        acc.push({ label: reverseRelationName, value: id, isReverseRelation: true });
+
+        return acc;
+      }, []);
+
+      return [
+        {
+          type: GET_CONTACTS_RELATION_TYPES_FULFILLED,
+          payload: { contactsRelationTypes }
+        }
+      ];
+    }
+  }));
+});
