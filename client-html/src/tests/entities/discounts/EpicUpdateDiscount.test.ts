f7960a55577005eefaaf3772263d128ca3b6dec2
Chintan Kotadia <chintan@ish.com.au>
2021-01-18 19:25:16 +0530

    OD-15411 Added jest tests in discounts entity for update discount epic

---
+++ client-html/src/tests/entities/discounts/EpicUpdateDiscount.test.ts
@@ -0,0 +1,36 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
+import {
+  GET_DISCOUNT_ITEM,
+  UPDATE_DISCOUNT_ITEM_FULFILLED,
+  updateDiscount
+} from "../../../js/containers/entities/discounts/actions";
+import { EpicUpdateDiscount } from "../../../js/containers/entities/discounts/epics/EpicUpdateDiscount";
+
+describe("Update discount epic tests", () => {
+  it("EpicUpdateDiscount should returns correct values", () => {
+    const id = "2";
+    return DefaultEpic({
+      action: mockedApi => updateDiscount(id, mockedApi.db.createAndUpdateDiscount(id)),
+      epic: EpicUpdateDiscount,
+      processData: () => [
+        {
+          type: UPDATE_DISCOUNT_ITEM_FULFILLED
+        },
+        {
+          type: FETCH_SUCCESS,
+          payload: { message: "Discount Record updated" }
+        },
+        {
+          type: GET_RECORDS_REQUEST,
+          payload: { entity: "Discount", listUpdate: true, savedID: id }
+        },
+        {
+          type: GET_DISCOUNT_ITEM,
+          payload: id
+        }
+      ]
+    });
+  });
+});
