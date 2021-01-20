933ff0851d2a43cd24edffbca53729da4ae450d0
Rostislav Zenov <rostislav@ish.com.au>
2021-01-20 19:24:19 +1100

    OD-15444 removed redundant preferences

--- server/src/main/groovy/ish/oncourse/server/api/v1/function/PreferenceFunctions.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/v1/function/PreferenceFunctions.groovy
@@ -38,9 +38,6 @@ class PreferenceFunctions {
                                                              AUSKEY_CERTIFICATE,
                                                              AUSKEY_PRIVATE_KEY,
                                                              AUSKEY_SALT,
-                                                             STORAGE_BUCKET_NAME,
-                                                             STORAGE_ACCESS_ID,
-                                                             STORAGE_ACCESS_KEY,
                                                              EMAIL_POP3PASSWORD,
                                                              SMTP_PASSWORD]
 
@@ -60,10 +57,7 @@ class PreferenceFunctions {
                                                                LICENSE_FUNDING_CONTRACT,
                                                                AUSKEY_CERTIFICATE,
                                                                AUSKEY_PRIVATE_KEY,
-                                                               AUSKEY_SALT,
-                                                               STORAGE_BUCKET_NAME,
-                                                               STORAGE_ACCESS_ID,
-                                                               STORAGE_ACCESS_KEY,
+                                                               AUSKEY_SALT
                                                              /*USI_SOFTWARE_ID*/]
 
     static String toString(String key, PreferenceController controller) {
