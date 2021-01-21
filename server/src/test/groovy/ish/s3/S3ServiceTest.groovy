fdf174c030587c06a75ced8955f8fde42478b6c8
Rostislav Zenov <rostislav@ish.com.au>
2021-01-22 02:12:22 +1100

    OD-15444 fixed JUnit tests: use new serviced for S3

--- server/src/test/groovy/ish/s3/S3ServiceTest.groovy
+++ server/src/test/groovy/ish/s3/S3ServiceTest.groovy
@@ -4,16 +4,15 @@
 package ish.s3
 
 import ish.common.types.AttachmentInfoVisibility
-//import org.apache.commons.httpclient.HttpClient
-//import org.apache.commons.httpclient.HttpMethod
-//import org.apache.commons.httpclient.methods.GetMethod
 import org.apache.commons.io.IOUtils
-import org.jets3t.service.ServiceException
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+
 import static org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-
-import java.security.NoSuchAlgorithmException
 
 /**
  * User: akoiro
@@ -24,31 +23,37 @@ class S3ServiceTest {
 	private String accessKeyId
     private String secretKey
     private String bucket
+    private String region
 
     @Before
     void before() {
 		accessKeyId = System.getProperty("storage.access.id", "AKIAIF6UAA7LXPQMRDGQ")
         secretKey = System.getProperty("storage.access.key", "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")
         bucket = System.getProperty("storage.bucket", "ish-oncourse-andromeda")
+        region = System.getProperty("storage.region", "ap-southeast-2")
     }
 
 	@Test
-    void testCacheControl() throws IOException, ServiceException, NoSuchAlgorithmException {
+    void testCacheControl() throws IOException {
 
 		InputStream stream = S3ServiceTest.class.getClassLoader().getResourceAsStream("resources/picture.jpg")
         byte[] array = IOUtils.toByteArray(stream)
 
-        S3Service s3Service = new S3Service(accessKeyId, secretKey, bucket)
-        S3Service.UploadResult uploadResult = s3Service.putFile(array, "picture.jpg", AttachmentInfoVisibility.PUBLIC)
+        AmazonS3Service s3Service = new AmazonS3Service(accessKeyId, secretKey, bucket, region)
+        AmazonS3Service.UploadResult uploadResult = s3Service.putFile(array, "picture.jpg", AttachmentInfoVisibility.PUBLIC)
 
         String uuid = uploadResult.getUuid()
+        String versionId = uploadResult.getVersionId()
         try {
-			String url = s3Service.getFileUrl(uuid, AttachmentInfoVisibility.PUBLIC)
-//            HttpClient httpClient = new HttpClient()
-//            HttpMethod method = HttpMethod.fromString(url)
-//            httpClient.executeMethod(method)
-//            assertEquals("max-age=604800", method.getResponseHeader("Cache-Control").getValue())
-//            assertEquals("max-age=604800", method.getResponseHeader("x-amz-meta-cache-control").getValue())
+			String url = s3Service.getFileUrl(uuid, versionId, AttachmentInfoVisibility.PUBLIC)
+
+
+            HttpClient httpClient = HttpClients.createDefault()
+            HttpGet getMethod = new HttpGet(url)
+            HttpResponse response = httpClient.execute(getMethod)
+
+            assertEquals("inline;filename=\"picture.jpg\"", response.getHeaders("Content-Disposition")[0].getValue())
+            assertEquals("max-age=604800", response.getHeaders("x-amz-meta-cache-control")[0].getValue())
         } finally {
 			s3Service.removeFile(uuid)
         }
