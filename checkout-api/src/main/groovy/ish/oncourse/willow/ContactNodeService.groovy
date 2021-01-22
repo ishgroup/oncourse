2e3e24cde47246505cc6fe055c4c263d1a5c4359
Artyom Kravenko <artyom@ish.com.au>
2021-01-22 11:41:59 +0000

    OD-15326 Handle Product(bundle) related Items
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65800 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout-api/src/main/groovy/ish/oncourse/willow/ContactNodeService.groovy
+++ checkout-api/src/main/groovy/ish/oncourse/willow/ContactNodeService.groovy
@@ -1,48 +1,63 @@
 package ish.oncourse.willow
 
-import com.google.inject.Inject
+
+import groovy.transform.CompileStatic
 import ish.oncourse.api.cayenne.CayenneService
 import ish.oncourse.model.*
 import ish.oncourse.willow.checkout.functions.*
 import ish.oncourse.willow.model.checkout.ContactNode
-import ish.oncourse.willow.model.checkout.Enrolment
 import ish.oncourse.willow.model.checkout.request.ContactNodeRequest
 import ish.oncourse.willow.service.impl.CollegeService
 import org.apache.cayenne.ObjectContext
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
+@CompileStatic
 class ContactNodeService {
     
     final static  Logger logger = LogManager.getLogger(ContactNodeService)
 
-    private CayenneService cayenneService
-    private CollegeService collegeService
+    //params
     private EntityRelationService relationService
+    private ContactNodeRequest request
 
-    @Inject
-    ContactNodeService(CayenneService cayenneService, CollegeService collegeService, EntityRelationService relationService) {
-        this.cayenneService = cayenneService
-        this.collegeService = collegeService
+    //controller state
+    private List<CourseClass> shoppingCartClasses =[]
+    private Set<Course> shoppingCartCourses = []
+    private List<Product> shoppingCartProducts = []
+    private College college
+    private ObjectContext context
+
+    private Set<Course> programCourses = []
+    private Set<Product> programProducts = []
+    
+    //result
+    Contact contact
+
+
+    ContactNode node
+
+    ContactNodeService(CayenneService cayenneService, CollegeService collegeService, EntityRelationService relationService, ContactNodeRequest request) {
+        this.context = cayenneService.newContext()
+        this.college = collegeService.getCollege()
         this.relationService = relationService
+        this.request = request
     }
     
     ContactNode getContactNode(ContactNodeRequest request) {
 
-        ObjectContext context = cayenneService.newContext()
-        College college = collegeService.college
-
-        Contact contact = new GetContact(context, college, request.contactId).get(false)
+        contact = new GetContact(context, college, request.contactId).get(false)
 
         ContactNode node = new ContactNode()
         node.contactId = contact.id.toString()
-        
-        ProcessClasses classesController = null
-        
+
         if (!contact.isCompany) {
-            classesController = new ProcessClasses(context, contact, college, request.classIds, request.promotionIds).process()
+            ProcessClasses classesController = new ProcessClasses(context, contact, college, request.classIds, request.promotionIds).process()
             node.enrolments = classesController.enrolments
             node.applications = classesController.applications
+
+            shoppingCartClasses = classesController.classesToEnrol
+            shoppingCartCourses = shoppingCartClasses*.course.toSet()
 
             ProcessWaitingLists processWaitingLists = new ProcessWaitingLists(context, contact, college, request.waitingCourseIds).process()
             node.waitingLists += processWaitingLists.waitingLists
@@ -53,71 +68,111 @@ class ContactNodeService {
         node.memberships += processProducts.memberships
         node.vouchers += processProducts.vouchers
 
-        //handle enrolment program
-        if (classesController) {
-            List<CourseClass> shoppingCartClasses = classesController.classesToEnrol
-            Set<Course> shoppingCartCourses = shoppingCartClasses*.course.toSet()
+        shoppingCartProducts = processProducts.productsToPurchase
 
-            List<Product> shoppingCartProducts = processProducts.productsToPurchase
-
-            Set<Course> programCourses = []
-            Set<Product> programProducts = []
-
-            shoppingCartCourses.each { shoppingCartCourse ->
-                String relatedClassId = shoppingCartClasses.find {clazz -> clazz.course == shoppingCartCourse }.id.toString()
-                relationService.getCoursesToAdd(shoppingCartCourse, contact).each { relatedCourse, allowRemove ->
-                    if (!(relatedCourse.id in shoppingCartCourses*.id || relatedCourse.id in programCourses*.id)) {
-                        programCourses << relatedCourse
-                        
-                        CourseClass availableClass = relatedCourse.getAvailableClasses().find { clazz ->
-                            new ValidateEnrolment(context,college).validate(clazz, contact.student).errors.empty
-                        }
-
-                        Enrolment enrolment = null
-
-                        if (availableClass) {
-                            enrolment = new ProcessClass(context, contact, college, availableClass.id.toString(), null).process().enrolment
-                        }
-
-                        if (!enrolment) {
-                            enrolment= new Enrolment()
-                            enrolment.contactId = contact.id
-                            enrolment.selected = false
-                            enrolment.errors << "There are no classes for course ${relatedCourse.name} available${allowRemove?"":", but they are mandatory"}. Please contact the college to have them resolve this.".toString()
-                        }
-                        enrolment.courseId = relatedCourse.id
-                        enrolment.allowRemove = allowRemove
-                        enrolment.relatedClassId = relatedClassId
-                        node.enrolments << enrolment
-                    }
-                }
-                
-                relationService.getProductsToAdd(shoppingCartCourse).each {relatedProduct, allowRemove ->
-                    if (!(relatedProduct.id in shoppingCartProducts*.id || relatedProduct.id in programProducts*.id)) {
-                        programProducts << relatedProduct
-                        ProcessProduct processProduct = new ProcessProduct(context, contact, college, relatedProduct.id.toString(), 1, null, null).process()
-                        processProduct.article && node.articles << processProduct.article.relatedClassId(relatedClassId).allowRemove(allowRemove)
-                        processProduct.membership && node.memberships << processProduct.membership.relatedClassId(relatedClassId).allowRemove(allowRemove)
-                        processProduct.voucher && node.vouchers << processProduct.voucher.relatedClassId(relatedClassId).allowRemove(allowRemove)
-                    }
-                }
-            }
-
-
-            //add suggested items after adding all 'shopping cart' items (since suggested items could be in cart already)
-            shoppingCartCourses.each { shoppingCartCourse ->
-                node.suggestedCourseIds += relationService.getSuggestedCourses(shoppingCartCourse).findAll { suggestedCurse ->
-                    !(suggestedCurse.id in shoppingCartCourses*.id || suggestedCurse.id in programCourses*.id)
-                }.collect { it.id.toString() }
-
-                node.suggestedProductIds += relationService.getSuggestedProducts(shoppingCartCourse).findAll { suggestedProduct ->
-                    !(suggestedProduct.id in shoppingCartProducts*.id || suggestedProduct.id in programProducts*.id)
-                }.collect { it.id.toString() }
-            }
-            
+        if (!shoppingCartCourses.empty) {
+            addCourseRelations()
         }
+        
+        if (!shoppingCartProducts.empty) {
+            addProductRelations()
+        }
+
         node
     } 
+    
+    
+    void addCourseRelations() {
+        
+        shoppingCartCourses.each { shoppingCartCourse ->
+            String relatedClassId = shoppingCartClasses.find {clazz -> clazz.course == shoppingCartCourse }.id.toString()
+            relationService.getCoursesToAdd(shoppingCartCourse, contact).each { relatedCourse, allowRemove ->
+                addRelatedCourse(relatedCourse, allowRemove, relatedClassId.toString(), null)
+            }
+
+            relationService.getProductsToAdd(shoppingCartCourse).each {relatedProduct, allowRemove ->
+                addRelatedProduct(relatedProduct, allowRemove, relatedClassId, null )
+            }
+        }
+    }
+    
+    void addProductRelations() {
+        shoppingCartProducts.each { shoppingCartProduct ->
+            if (contact.student) {
+                relationService.getCoursesToAdd(shoppingCartProduct, contact).each { relatedCourse, allowRemove ->
+                    addRelatedCourse(relatedCourse, allowRemove, null, shoppingCartProduct.id.toString())
+                } 
+            }
+            relationService.getProductsToAdd(shoppingCartProduct).each {relatedProduct, allowRemove ->
+                addRelatedProduct(relatedProduct, allowRemove, null, shoppingCartProduct.id.toString() )
+            }
+        }
+    }
+
+    void addRelatedCourse(Course relatedCourse, Boolean allowRemove, String relatedClassId, String relatedProductId) {
+        if (!(relatedCourse.id in shoppingCartCourses*.id || relatedCourse.id in programCourses*.id)) {
+            programCourses << relatedCourse
+
+            CourseClass availableClass = relatedCourse.getAvailableClasses().find { clazz ->
+                new ValidateEnrolment(context,college).validate(clazz, contact.student).errors.empty
+            }
+
+            ish.oncourse.willow.model.checkout.Enrolment enrolment = null
+
+            if (availableClass) {
+                enrolment = new ProcessClass(context, contact, college, availableClass.id.toString(), null).process().enrolment
+            }
+
+            if (!enrolment) {
+                enrolment= new ish.oncourse.willow.model.checkout.Enrolment()
+                enrolment.contactId = contact.id
+                enrolment.selected = false
+                enrolment.errors << "There are no classes for course ${relatedCourse.name} available${allowRemove?"":", but they are mandatory"}. Please contact the college to have them resolve this.".toString()
+            }
+            enrolment.courseId = relatedCourse.id
+            enrolment.allowRemove = allowRemove
+            enrolment.relatedClassId = relatedClassId
+            enrolment.relatedProductId = relatedProductId
+            node.enrolments << enrolment
+        }
+
+    }
+
+    void addRelatedProduct(Product relatedProduct, Boolean allowRemove, String relatedClassId, String relatedProductId) {
+        if (!(relatedProduct.id in shoppingCartProducts*.id || relatedProduct.id in programProducts*.id)) {
+            programProducts << relatedProduct
+            ProcessProduct processProduct = new ProcessProduct(context, contact, college, relatedProduct.id.toString(), 1, null, null).process()
+            processProduct.article && node.articles << processProduct.article.relatedClassId(relatedClassId).relatedProductId(relatedProductId).allowRemove(allowRemove)
+            processProduct.membership && node.memberships << processProduct.membership.relatedClassId(relatedClassId).relatedProductId(relatedProductId).allowRemove(allowRemove)
+            processProduct.voucher && node.vouchers << processProduct.voucher.relatedClassId(relatedClassId).relatedProductId(relatedProductId).allowRemove(allowRemove)
+        }
+    }
+
+    void addSuggestedItems() {
+        //add suggested items after adding all 'shopping cart' items (since suggested items could be in cart already)
+        shoppingCartCourses.each { shoppingCartCourse ->
+            node.suggestedCourseIds += relationService.getSuggestedCourses(shoppingCartCourse).findAll { suggestedCurse ->
+                !(suggestedCurse.id in shoppingCartCourses*.id || suggestedCurse.id in programCourses*.id)
+            }.collect { it.id.toString() }
+
+            node.suggestedProductIds += relationService.getSuggestedProducts(shoppingCartCourse).findAll { suggestedProduct ->
+                !(suggestedProduct.id in shoppingCartProducts*.id || suggestedProduct.id in programProducts*.id)
+            }.collect { it.id.toString() }
+        }
+
+        shoppingCartCourses.each { shoppingCartCourse ->
+            node.suggestedCourseIds += relationService.getSuggestedCourses(shoppingCartCourse).findAll { suggestedCurse ->
+                !(suggestedCurse.id in shoppingCartCourses*.id || suggestedCurse.id in programCourses*.id)
+            }.collect { it.id.toString() }
+
+            node.suggestedProductIds += relationService.getSuggestedProducts(shoppingCartCourse).findAll { suggestedProduct ->
+                !(suggestedProduct.id in shoppingCartProducts*.id || suggestedProduct.id in programProducts*.id)
+            }.collect { it.id.toString() }
+        }
+        
+        
+    }
+    
     
 
 }
--- checkout-api/src/main/groovy/ish/oncourse/willow/EntityRelationService.groovy
+++ checkout-api/src/main/groovy/ish/oncourse/willow/EntityRelationService.groovy
@@ -1,6 +1,7 @@
 package ish.oncourse.willow
 
 import com.google.inject.Inject
+import ish.common.types.EntityRelationCartAction
 import ish.oncourse.api.cayenne.CayenneService
 import ish.oncourse.model.*
 import ish.oncourse.willow.service.impl.CollegeService
@@ -28,52 +29,46 @@ class EntityRelationService {
      * @return map key: course, value: allow remove from shopping  cart
      */
     Map<Course,Boolean> getCoursesToAdd(Course course, Contact contact) {
-        
-        ObjectContext context = cayenneService.newContext()
+        getCourses(Course, course.id, contact)
+    }
+    
+    Map<Course,Boolean> getCoursesToAdd(Product product, Contact contact) {
+        getCourses(Product, product.id, contact)
+    }
+
+    private Map<Course,Boolean> getCourses (Class from, Long id, Contact contact) {
+    
         Map<Course,Boolean> result = [:]
-        
-        List<EntityRelation> relations = ObjectSelect.query(EntityRelation)
-                .where(EntityRelation.FROM_ENTITY_IDENTIFIER.eq(Course.simpleName))
-                .and(EntityRelation.TO_ENTITY_IDENTIFIER.eq(Course.simpleName))
-                .and(EntityRelation.FROM_ENTITY_WILLOW_ID.eq(course.id))
-                .and(EntityRelation.RELATION_TYPE.dot(EntityRelationType.SHOPPING_CART).in(ADD_ALLOW_REMOVAL, ADD_NO_REMOVAL))
-                .select(context)
-        
-        relations.each { relation ->
-            Course relatedCourse = SelectById.query(Course, relation.toEntityWillowId).selectOne(context)
+        getRelations(from, id, Course, [ADD_ALLOW_REMOVAL, ADD_NO_REMOVAL]).each { relation, relatedCourse ->
+            
             if (relatedCourse) {
-                if (relation.relationType.considerHistory && contact.isEnrolled(course)) {
+                if (relation.relationType.considerHistory && contact.isEnrolled(relatedCourse)) {
                     //ignore this course since student already has enrolment
                 } else {
                     result[relatedCourse] = ADD_ALLOW_REMOVAL == relation.relationType.shoppingCart
                 }
             }
         }
-        
         return result
     }
 
     Map<Product,Boolean> getProductsToAdd(Course course) {
+        getProducts(Course, course.id)
+    }
+    
+    Map<Product,Boolean> getProductsToAdd(Product course) {
+        getProducts(Product, course.id)
+    }
 
-        ObjectContext context = cayenneService.newContext()
+    Map<Product,Boolean> getProducts(Class from, Long id) {
         Map<Product,Boolean> result = [:]
-
-        List<EntityRelation> relations = ObjectSelect.query(EntityRelation)
-                .where(EntityRelation.FROM_ENTITY_IDENTIFIER.eq(Course.simpleName))
-                .and(EntityRelation.TO_ENTITY_IDENTIFIER.eq(Product.simpleName))
-                .and(EntityRelation.FROM_ENTITY_WILLOW_ID.eq(course.id))
-                .and(EntityRelation.RELATION_TYPE.dot(EntityRelationType.SHOPPING_CART).in(ADD_ALLOW_REMOVAL, ADD_NO_REMOVAL))
-                .select(context)
-
-        relations.each { relation ->
-            Product relatedProduct = SelectById.query(Product, relation.toEntityWillowId).selectOne(context)
+        
+        getRelations(from, id, Product, [ADD_ALLOW_REMOVAL, ADD_NO_REMOVAL]).each { relation, relatedProduct ->
             if (relatedProduct) {
                 result[relatedProduct] = ADD_ALLOW_REMOVAL == relation.relationType.shoppingCart
             }
         }
-
         return result
-
     }
 
     List<Product> getSuggestedProducts(Course course) {
@@ -118,4 +113,15 @@ class EntityRelationService {
         return result
     }
 
+    
+    private <T>  Map<EntityRelation, T>  getRelations(Class from, Long fromId, Class<T> to , List<EntityRelationCartAction> actions) {
+        ObjectContext context = cayenneService.newContext()
+        return ObjectSelect.query(EntityRelation)
+                .where(EntityRelation.FROM_ENTITY_IDENTIFIER.eq(from.simpleName))
+                .and(EntityRelation.TO_ENTITY_IDENTIFIER.eq(to.simpleName))
+                .and(EntityRelation.FROM_ENTITY_WILLOW_ID.eq(fromId))
+                .and(EntityRelation.RELATION_TYPE.dot(EntityRelationType.SHOPPING_CART).in(actions))
+                .select(cayenneService.newContext())
+                .collectEntries {relation -> [(relation): SelectById.query(to, relation.toEntityWillowId).selectOne(context) ]}
+    }
 }
