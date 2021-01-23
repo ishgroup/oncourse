ea1cf3c51121cc0757681531f330faf98dbcb38b
Chintan Kotadia <chintan@ish.com.au>
2021-01-23 19:09:29 +0530

    OD-15411 Added jest tests in documents entity for get document epic, added route for search and get document, and added mock data for get and add document

---
+++ client-html/src/dev/mock/api/entities/DocumentApiMock.ts
@@ -0,0 +1,16 @@
+import { promiseResolve } from "../../MockAdapter";
+
+export function DocumentApiMock(mock) {
+  this.api.onGet(new RegExp(`v1/list/entity/document/\\d+`)).reply(config => {
+    const params = config.url.split("/");
+    const id = params[params.length - 1];
+    return promiseResolve(config, this.db.getDocument(id));
+  });
+
+  this.api.onPost("v1/list/entity/document/search").reply(config => promiseResolve(config, {}));
+
+  this.api.onPost("v1/list/entity/document").reply(config => {
+    this.db.addDocument(config.data);
+    return promiseResolve(config, this.db.getDocuments());
+  });
+}
