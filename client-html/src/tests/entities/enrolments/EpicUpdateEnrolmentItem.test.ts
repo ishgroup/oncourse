cac3bc95f99c97c5b7e8e74bf08faad8acb6cc4b
Chintan Kotadia <chintan@ish.com.au>
2021-01-23 18:09:09 +0530

    OD-15411 Added jest tests in enrolments entity for update enrolment epic

---
+++ client-html/src/tests/entities/enrolments/EpicUpdateEnrolmentItem.test.ts
@@ -0,0 +1,36 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
+import { EpicUpdateEnrolmentItem } from "../../../js/containers/entities/enrolments/epics/EpicUpdateEnrolmentItem";
+import {
+  GET_ENROLMENT_ITEM,
+  UPDATE_ENROLMENT_ITEM_FULFILLED,
+  updateEnrolment
+} from "../../../js/containers/entities/enrolments/actions";
+
+describe("Update enrolment epic tests", () => {
+  it("EpicUpdateEnrolmentItem should returns correct values", () => {
+    const id = 2;
+    return DefaultEpic({
+      action: mockedApi => updateEnrolment(id, mockedApi.db.getEnrolment(id)),
+      epic: EpicUpdateEnrolmentItem,
+      processData: () => [
+        {
+          type: UPDATE_ENROLMENT_ITEM_FULFILLED
+        },
+        {
+          type: FETCH_SUCCESS,
+          payload: { message: "Enrolment Record updated" }
+        },
+        {
+          type: GET_RECORDS_REQUEST,
+          payload: { entity: "Enrolment", listUpdate: true, savedID: id }
+        },
+        {
+          type: GET_ENROLMENT_ITEM,
+          payload: id
+        }
+      ]
+    });
+  });
+});
