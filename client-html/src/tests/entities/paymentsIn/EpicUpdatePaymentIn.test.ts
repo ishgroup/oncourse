ac89805e3f62b379fbd30f9924b7dc1e0265db89
Chintan Kotadia <chintan@ish.com.au>
2021-01-25 11:54:52 +0530

    OD-15411 Added jest tests in paymentsIn entity for update paymentIn epic

---
+++ client-html/src/tests/entities/paymentsIn/EpicUpdatePaymentIn.test.ts
@@ -0,0 +1,33 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
+import { EpicUpdatePaymentIn } from "../../../js/containers/entities/paymentsIn/epics/EpicUpdatePaymentIn";
+import {
+  GET_PAYMENT_IN_ITEM,
+  UPDATE_PAYMENT_IN_ITEM_FULFILLED,
+  updatePaymentIn
+} from "../../../js/containers/entities/paymentsIn/actions";
+
+describe("Update paymentIn epic tests", () => {
+  it("EpicUpdatePaymentIn should returns correct values", () => DefaultEpic({
+    action: mockedApi => updatePaymentIn("1", mockedApi.db.getPaymentIn(1)),
+    epic: EpicUpdatePaymentIn,
+    processData: () => [
+      {
+        type: UPDATE_PAYMENT_IN_ITEM_FULFILLED
+      },
+      {
+        type: FETCH_SUCCESS,
+        payload: { message: "PaymentIn Record updated" }
+      },
+      {
+        type: GET_RECORDS_REQUEST,
+        payload: { entity: "PaymentIn", listUpdate: true, savedID: "1" }
+      },
+      {
+        type: GET_PAYMENT_IN_ITEM,
+        payload: "1"
+      }
+    ]
+  }));
+});
