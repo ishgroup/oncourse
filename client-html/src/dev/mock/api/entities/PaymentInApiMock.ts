99eac2cf433747a30fe547a88c3baff4d0a47584
Chintan Kotadia <chintan@ish.com.au>
2021-01-25 11:45:21 +0530

    OD-15411 Added jest tests in paymentsIn entity for create reverse paymentIn epic

--- client-html/src/dev/mock/api/entities/PaymentInApiMock.ts
+++ client-html/src/dev/mock/api/entities/PaymentInApiMock.ts
@@ -7,9 +7,7 @@ export function PaymentInApiMock() {
     return promiseResolve(config, this.db.getPaymentIn(id));
   });
 
-  this.api.onPut(new RegExp(`v1/list/entity/paymentIn/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
+  this.api.onPut(new RegExp(`v1/list/entity/paymentIn/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 
   this.api.onPost("v1/list/entity/paymentIn").reply(config => {
     this.db.createPaymentIn(config.data);
@@ -22,4 +20,6 @@ export function PaymentInApiMock() {
     this.db.removePaymentIn(id);
     return promiseResolve(config, this.db.getPaymentsIn());
   });
+
+  this.api.onPost(new RegExp(`v1/list/entity/paymentIn/reverse/\\d+`)).reply(config => promiseResolve(config, {}));
 }
