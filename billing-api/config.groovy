a5c9c987fb3bd9f9a25e0c9e8abb3c70fa826a11
Artyom Kravenko <artyom@ish.com.au>
2021-01-26 12:51:05 +0000

    OD-15467 General gradle config for new billing app
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65818 fb2e276d-fae9-0310-b578-b71ac85840b9

--- billing-api/config.groovy
+++ billing-api/config.groovy
@@ -3,5 +3,5 @@ import org.codehaus.groovy.control.customizers.ASTTransformationCustomizer
 //
 //// Add the AST annotation @CompileStatic
 //// and @ToString to all classes.
-//configuration.addCompilationCustomizers(
-//        new ASTTransformationCustomizer(CompileStatic))
+configuration.addCompilationCustomizers(
+        new ASTTransformationCustomizer(CompileStatic))
