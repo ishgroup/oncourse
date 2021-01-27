ff72132d75c1abf38d6d8985d4f9875768aff985
Chintan Kotadia <chintan@ish.com.au>
2021-01-27 17:31:35 +0530

    OD-15411 Fixed delete membershipProduct item, updated entity response, and simplify to get params id for get and delete

--- client-html/src/dev/mock/api/entities/MembershipProductApiMock.ts
+++ client-html/src/dev/mock/api/entities/MembershipProductApiMock.ts
@@ -1,15 +1,13 @@
 import { promiseResolve } from "../../MockAdapter";
+import { getParamsId } from "../../mockUtils";
 
 export function MembershipProductApiMock(mock) {
   this.api.onGet(new RegExp(`v1/list/entity/membershipProduct/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
+    const id = getParamsId(config);
     return promiseResolve(config, this.db.getMembershipProduct(id));
   });
 
-  this.api.onPut(new RegExp(`v1/list/entity/membershipProduct/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
+  this.api.onPut(new RegExp(`v1/list/entity/membershipProduct/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 
   this.api.onPost("v1/list/entity/membershipProduct").reply(config => {
     this.db.createMembershipProduct(config.data);
@@ -17,8 +15,7 @@ export function MembershipProductApiMock(mock) {
   });
 
   this.api.onDelete(new RegExp(`v1/list/entity/membershipProduct/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
+    const id = getParamsId(config);
     this.db.removeMembershipProduct(id);
     return promiseResolve(config, this.db.getMembershipProducts());
   });
