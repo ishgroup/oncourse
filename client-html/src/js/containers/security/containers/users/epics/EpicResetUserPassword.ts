74926bc411649cb9bb1ed4ad4ee440fb49fe3ef6
Rostislav Zenov <rostislav@ish.com.au>
2021-01-29 01:36:22 +1100

    OD-15477 changed reset password method: it just send a invitation to user now, don't create a new password

--- client-html/src/js/containers/security/containers/users/epics/EpicResetUserPassword.ts
+++ client-html/src/js/containers/security/containers/users/epics/EpicResetUserPassword.ts
@@ -8,28 +8,26 @@ import { Epic } from "redux-observable";
 import * as EpicUtils from "../../../../../common/epics/EpicUtils";
 import UserService from "../services/UsersService";
 import { FETCH_SUCCESS } from "../../../../../common/actions";
-import { RESET_USER_PASSWORD, RESET_USER_PASSWORD_FULFILLED } from "../../../actions";
+import {POST_USER_REQUEST_FULFILLED, RESET_USER_PASSWORD, RESET_USER_PASSWORD_FULFILLED} from "../../../actions";
 import FetchErrorHandler from "../../../../../common/api/fetch-errors-handlers/FetchErrorHandler";
+import {User} from "@api/model";
 
 const request: EpicUtils.Request<any, any, any> = {
-  type: RESET_USER_PASSWORD,
-  hideLoadIndicator: true,
-  getData: (id: number) => UserService.resetPassword(id),
-  processData: (newPassword: string) => {
-    return [
-      {
-        type: RESET_USER_PASSWORD_FULFILLED,
-        payload: { newPassword }
-      },
-      {
-        type: FETCH_SUCCESS,
-        payload: { message: "New password was successfully generated" }
-      }
-    ];
-  },
-  processError: response => {
-    return FetchErrorHandler(response, "Error. Password was not changed");
-  }
+    type: RESET_USER_PASSWORD,
+    hideLoadIndicator: true,
+    getData: (id: number) => UserService.resetPassword(id),
+    retrieveData: () => UserService.getUsers(),
+    processData: (users: User[]) => [
+            {
+                type: POST_USER_REQUEST_FULFILLED,
+                payload: { users }
+            },
+            {
+                type: FETCH_SUCCESS,
+                payload: { message: "Invitation was successfully sent" }
+            }
+        ],
+    processError: response => FetchErrorHandler(response, "Invitation was not sent")
 };
 
 export const EpicResetUserPassword: Epic<any, any> = EpicUtils.Create(request);
