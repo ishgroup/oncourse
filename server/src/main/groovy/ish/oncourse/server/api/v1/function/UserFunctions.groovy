8585128f074ba8afbd43789f437a3e5aa69c27f6
Rostislav Zenov <rostislav@ish.com.au>
2021-01-29 17:34:53 +1100

    reverted wrong commits after rebasing

--- server/src/main/groovy/ish/oncourse/server/api/v1/function/UserFunctions.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/v1/function/UserFunctions.groovy
@@ -63,36 +63,36 @@ class UserFunctions {
                 user.role = systemUser.aclRoles[0].id
             }
             user.passwordUpdateRequired = systemUser.passwordUpdateRequired
-            user.inviteAgain = systemUser.invitationTokenExpiryDate && systemUser.invitationTokenExpiryDate < new Date()
+            user.inviteAgain = systemUser.password == null
             user.created = systemUser.createdOn.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime()
             user.modified = systemUser.modifiedOn.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime()
             user
         }
     }
 
-    static SystemUser toDbSystemUser(ObjectContext context, UserDTO user) {
-        SystemUser systemUser = user.id ?
-                getRecordById(context, SystemUser, user.id, SystemUser.DEFAULT_ADMINISTRATION_CENTRE.joint(), SystemUser.ACL_ROLES.joint()) :
+    static SystemUser toDbSystemUser(ObjectContext context, UserDTO dtoModel) {
+        SystemUser dbModel = dtoModel.id ?
+                getRecordById(context, SystemUser, dtoModel.id, SystemUser.DEFAULT_ADMINISTRATION_CENTRE.joint(), SystemUser.ACL_ROLES.joint()) :
                 context.newObject(SystemUser)
 
-        systemUser.isActive = user.active
-        systemUser.login = user.login
-        systemUser.firstName = user.firstName
-        systemUser.lastName = user.lastName
-        systemUser.email = user.email
-        systemUser.defaultAdministrationCentre = getRecordById(context, Site, user.administrationCentre)
-        systemUser.canEditCMS = user.accessEditor
-        if (user.password) {
-            systemUser.password = AuthenticationUtil.generatePasswordHash(user.password)
-            systemUser.passwordLastChanged = LocalDate.now()
+        dbModel.isActive = dtoModel.active
+        dbModel.login = dtoModel.login
+        dbModel.firstName = dtoModel.firstName
+        dbModel.lastName = dtoModel.lastName
+        dbModel.email = dtoModel.email
+        dbModel.defaultAdministrationCentre = getRecordById(context, Site, dtoModel.administrationCentre)
+        dbModel.canEditCMS = dtoModel.accessEditor
+        if (dtoModel.password) {
+            dbModel.password = AuthenticationUtil.generatePasswordHash(dtoModel.password)
+            dbModel.passwordLastChanged = LocalDate.now()
         }
-        systemUser.isAdmin = user.admin
-        new ArrayList<ACLRole>(systemUser.getAclRoles()).each { systemUser.removeFromAclRoles(it) }
-        if (!user.admin) {
-            systemUser.addToAclRoles(getRecordById(context, ACLRole, user.role))
+        dbModel.isAdmin = dtoModel.admin
+        new ArrayList<ACLRole>(dbModel.getAclRoles()).each { dbModel.removeFromAclRoles(it) }
+        if (!dtoModel.admin) {
+            dbModel.addToAclRoles(getRecordById(context, ACLRole, dtoModel.role))
         }
-        systemUser.passwordUpdateRequired = user.passwordUpdateRequired
-        systemUser
+        dbModel.passwordUpdateRequired = dtoModel.passwordUpdateRequired
+        dbModel
     }
 
     static String validateUserPassword(String email, String login, String password, boolean complexity) {
@@ -119,89 +119,99 @@ class UserFunctions {
         new Zxcvbn().measure(password).score < 2 ? 'Password does not satisfy complexity restrictions.' : null
     }
 
-    static ValidationErrorDTO validateForUpdate(ObjectContext context, UserDTO user, boolean complexity) {
-        if (user.id && !SelectById.query(SystemUser, user.id).selectOne(context)) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.EMAIL.name, "User doesn't exist.")
+    static ValidationErrorDTO validateForUpdate(ObjectContext context, UserDTO dtoModel, boolean complexity) {
+        if (dtoModel.id && !SelectById.query(SystemUser, dtoModel.id).selectOne(context)) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.EMAIL.name, "User doesn't exist.")
         }
 
-        if (!user.email?.trim()) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.EMAIL.name, "Email should be set.")
-        } else if (user.email.length() > 128) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.EMAIL.name, "The maximum email length is 128.")
+        if (!dtoModel.email?.trim()) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.EMAIL.name, "Email should be set.")
+        } else if (dtoModel.email.length() > 128) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.EMAIL.name, "The maximum email length is 128.")
         }
 
 
-        if (!user.firstName?.trim()) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.FIRST_NAME.name, "First name should be set.")
-        } else if (user.firstName.length() > 100) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.FIRST_NAME.name, "The maximum first name length is 100.")
+        if (!dtoModel.firstName?.trim()) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.FIRST_NAME.name, "First name should be set.")
+        } else if (dtoModel.firstName.length() > 100) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.FIRST_NAME.name, "The maximum first name length is 100.")
         }
 
-        if (!user.lastName?.trim()) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.LAST_NAME.name, "Last name should be set.")
-        } else if (user.lastName.length() > 100) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.LAST_NAME.name, "The maximum last name length is 100.")
+        if (!dtoModel.lastName?.trim()) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.LAST_NAME.name, "Last name should be set.")
+        } else if (dtoModel.lastName.length() > 100) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.LAST_NAME.name, "The maximum last name length is 100.")
         }
 
-        if (user.id && !user.inviteAgain) {
-            if (user.password) {
-                String errorMessage = validateUserPassword(user.email, user.login, user.password, complexity)
+        if (dtoModel.id && !dtoModel.inviteAgain) {
+            if (dtoModel.password) {
+                String errorMessage = validateUserPassword(dtoModel.email, dtoModel.login, dtoModel.password, complexity)
                 if (errorMessage) {
-                    return new ValidationErrorDTO(user.id?.toString(), SystemUser.PASSWORD.name, errorMessage)
+                    return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.PASSWORD.name, errorMessage)
                 }
             }
         } else {
-            if (user.password) {
-                return new ValidationErrorDTO(user.id?.toString(), SystemUser.PASSWORD.name, "Password cannot be created for new users.")
+            if (dtoModel.password) {
+                return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.PASSWORD.name, "Password cannot be created for new users.")
             }
         }
 
-        SystemUser systemUser = UserDao.getByEmail(context, user.email)
+        SystemUser dbModel = UserDao.getByEmail(context, dtoModel.email)
 
 
-        if (systemUser != null && systemUser.id != user.id) {
-            return new ValidationErrorDTO(user.id?.toString(), SystemUser.EMAIL.name, "Email should be unique.")
+        if (dbModel != null && dbModel.id != dtoModel.id) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), SystemUser.EMAIL.name, "Email should be unique.")
         }
 
         Site site = ObjectSelect.query(Site)
-                .where(Site.ID.eq(user.administrationCentre))
+                .where(Site.ID.eq(dtoModel.administrationCentre))
                 .selectOne(context)
         if (!site) {
-            return new ValidationErrorDTO(user.id?.toString(), 'administrationCentre', "Administration centre doesn't exist.")
+            return new ValidationErrorDTO(dtoModel.id?.toString(), 'administrationCentre', "Administration centre doesn't exist.")
         }
 
-        if (!user.admin && !user.role) {
-            return new ValidationErrorDTO(user.id?.toString(), 'role', "Not admin users can not be without role.")
+        if (!dtoModel.admin && !dtoModel.role) {
+            return new ValidationErrorDTO(dtoModel.id?.toString(), 'role', "Not admin users can not be without role.")
         }
 
-        if (user.role) {
+        if (dtoModel.role) {
             ACLRole aclRole = ObjectSelect.query(ACLRole)
-                    .where(ACLRole.ID.eq(user.role))
+                    .where(ACLRole.ID.eq(dtoModel.role))
                     .selectOne(context)
             if (!aclRole) {
-                return new ValidationErrorDTO(user.id?.toString(), 'role', "User role doesn't exist.")
+                return new ValidationErrorDTO(dtoModel.id?.toString(), 'role', "User role doesn't exist.")
             }
         }
 
         null
     }
 
-    static String sendInvitationEmailToSystemUser(SystemUser user,
-                                                  PreferenceController preferenceController,
-                                                  MailDeliveryService mailDeliveryService,
-                                                  String collegeKey) throws MessagingException {
+    static void updateLoginAttemptNumber(SystemUser user, Integer allowedNumberOfAttempts, Integer newAttemptNumber = null) {
+        user.loginAttemptNumber = newAttemptNumber ?: ++(user.loginAttemptNumber)
+
+        if (allowedNumberOfAttempts <= user.loginAttemptNumber) {
+            user.isActive = Boolean.FALSE
+        }
+    }
+
+
+    static String sendInvitationEmailToNewSystemUser(SystemUser currentUser, SystemUser newUser,
+                                                     PreferenceController preferenceController,
+                                                     MailDeliveryService mailDeliveryService,
+                                                     String collegeKey) throws MessagingException {
+        String messageStart = currentUser ? "${currentUser.fullName} has given you access" : "You were provided access"
         String invitationToken = generateUserInvitationToken()
         String subject = "Welcome to onCourse!"
         String messageText =
                 """
-${user.fullName} has given you access to the ish onCourse application for ${preferenceController.collegeName}. Please click here to accept this invitation.
+${messageStart} to the ish onCourse application for https://${collegeKey}.cloud.oncourse.cc. Please click here to accept this invitation.
 
 https://${collegeKey}.cloud.oncourse.cc/invite/${invitationToken}
 
 This invitation will expire in 24 hours.
                 """
 
-        SmtpParameters parameters = new SmtpParameters(preferenceController.emailFromAddress, preferenceController.emailFromName, user.email, subject, messageText)
+        SmtpParameters parameters = new SmtpParameters(preferenceController.emailFromAddress, preferenceController.emailFromName, newUser.email, subject, messageText)
         mailDeliveryService.sendEmail(MailDeliveryParamBuilder.valueOf(parameters).build())
 
         return invitationToken
