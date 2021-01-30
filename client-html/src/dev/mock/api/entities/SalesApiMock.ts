8c98d5ecfd48351fec72b3f74887558783722985
Chintan Kotadia <chintan@ish.com.au>
2021-01-30 18:51:52 +0530

    OD-15411 Added jest tests in sales entity for cancel sale epic, removed unused create mock method, and updated plain sales item list response

--- client-html/src/dev/mock/api/entities/SalesApiMock.ts
+++ client-html/src/dev/mock/api/entities/SalesApiMock.ts
@@ -9,10 +9,7 @@ export function SalesApiMock(mock) {
 
   this.api.onPut(new RegExp(`v1/list/entity/sales/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 
-  this.api.onPost("v1/list/entity/sales").reply(config => {
-    this.db.createProductItem(config.data);
-    return promiseResolve(config, {});
-  });
+  this.api.onPost("v1/list/entity/sales").reply(config => promiseResolve(config, JSON.parse(config.data)));
 
   this.api.onDelete(new RegExp(`v1/list/entity/sales/\\d+`)).reply(config => {
     const id = getParamsId(config);
