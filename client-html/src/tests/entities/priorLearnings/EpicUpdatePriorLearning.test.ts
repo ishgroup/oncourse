cd77291ffd9e41ce9da5ec517c81faf418a84d18
Chintan Kotadia <chintan@ish.com.au>
2021-01-30 12:00:14 +0530

    OD-15411 Added jest tests in priorLearnings entity for update priorLearning epic, and added mock route

---
+++ client-html/src/tests/entities/priorLearnings/EpicUpdatePriorLearning.test.ts
@@ -0,0 +1,33 @@
+import { DefaultEpic } from "../../common/Default.Epic";
+import { FETCH_SUCCESS } from "../../../js/common/actions";
+import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
+import { EpicUpdatePriorLearning } from "../../../js/containers/entities/priorLearnings/epics/EpicUpdatePriorLearning";
+import {
+  GET_PRIOR_LEARNING_ITEM,
+  UPDATE_PRIOR_LEARNING_ITEM_FULFILLED,
+  updatePriorLearning
+} from "../../../js/containers/entities/priorLearnings/actions";
+
+describe("Update priorLearning epic tests", () => {
+  it("EpicUpdatePriorLearning should returns correct values", () => DefaultEpic({
+    action: mockedApi => updatePriorLearning("1", mockedApi.db.getPriorLearning(1)),
+    epic: EpicUpdatePriorLearning,
+    processData: () => [
+      {
+        type: UPDATE_PRIOR_LEARNING_ITEM_FULFILLED
+      },
+      {
+        type: FETCH_SUCCESS,
+        payload: { message: "Prior learning record updated" }
+      },
+      {
+        type: GET_RECORDS_REQUEST,
+        payload: { entity: "PriorLearning", listUpdate: true, savedID: "1" }
+      },
+      {
+        type: GET_PRIOR_LEARNING_ITEM,
+        payload: "1"
+      }
+    ]
+  }));
+});
