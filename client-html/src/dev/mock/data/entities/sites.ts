b9f0bc7259dbf6f72c58c86a4f4d3d9db4a60df4
Chintan Kotadia <chintan@ish.com.au>
2021-02-01 10:43:24 +0530

    OD-15411 Updated sites entity list response and simplify params id for get site

--- client-html/src/dev/mock/data/entities/sites.ts
+++ client-html/src/dev/mock/data/entities/sites.ts
@@ -1,6 +1,8 @@
-import { generateArraysOfRecords } from "../../mockUtils";
+import { generateArraysOfRecords, getEntityResponse } from "../../mockUtils";
 
 export function mockSites() {
+  this.getSites = () => this.sites;
+
   this.getSite = id => {
     const row = this.sites.rows.find(row => row.id == id);
     return {
@@ -46,11 +48,7 @@ export function mockSites() {
         this.getTag(1)
       ],
       timezone: this.timezones[row.id],
-    }
-  };
-
-  this.getSites = () => {
-    return this.sites;
+    };
   };
 
   const rows = generateArraysOfRecords(20, [
@@ -64,109 +62,75 @@ export function mockSites() {
     values: [l.name, l.suburb, l.postcode, true]
   }));
 
-  const columns = [
-    {
-      title: "Name",
-      attribute: "name",
-      sortable: true,
-      visible: true,
-      width: 100,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "Country",
-      attribute: "country.name",
-      sortable: true,
-      visible: false,
-      width: 100,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "State",
-      attribute: "state",
-      sortable: true,
-      visible: false,
-      width: 100,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "Suburb",
-      attribute: "suburb",
-      sortable: true,
-      visible: true,
-      width: 100,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "Street",
-      attribute: "street",
-      sortable: true,
-      visible: false,
-      width: 100,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "Postcode",
-      attribute: "postcode",
-      sortable: true,
-      visible: true,
-      width: 100,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "Local timezone",
-      attribute: "localTimezone",
-      sortable: true,
-      visible: false,
-      width: 200,
-      type: null,
-      sortFields: []
-    },
-    {
-      title: "Administration centre",
-      attribute: "isAdministrationCentre",
-      sortable: true,
-      visible: true,
-      width: 200,
-      type: "Boolean",
-      sortFields: []
-    },
-    {
-      title: "Shown on web",
-      attribute: "isShownOnWeb",
-      sortable: true,
-      visible: false,
-      width: 200,
-      type: "Boolean",
-      sortFields: []
-    },
-    {
-      title: "Is virtual",
-      attribute: "isVirtual",
-      sortable: true,
-      visible: false,
-      width: 200,
-      type: "Boolean",
-      sortFields: []
-    }
-  ];
-
-  const response = { rows, columns } as any;
-
-  response.entity = "Site";
-  response.offset = 0;
-  response.filterColumnWidth = 200;
-  response.layout = "Three column";
-  response.pageSize = 20;
-  response.search = null;
-  response.count = rows.length;
-  response.sort = [];
-
-  return response;
+  return getEntityResponse({
+    entity: "Site",
+    rows,
+    columns: [
+      {
+        title: "Name",
+        attribute: "name",
+        sortable: true,
+        width: 100
+      },
+      {
+        title: "Country",
+        attribute: "country.name",
+        sortable: true,
+        visible: false,
+        width: 100
+      },
+      {
+        title: "State",
+        attribute: "state",
+        sortable: true,
+        visible: false,
+        width: 100
+      },
+      {
+        title: "Suburb",
+        attribute: "suburb",
+        sortable: true,
+        width: 100
+      },
+      {
+        title: "Street",
+        attribute: "street",
+        sortable: true,
+        visible: false,
+        width: 100
+      },
+      {
+        title: "Postcode",
+        attribute: "postcode",
+        sortable: true,
+        width: 100
+      },
+      {
+        title: "Local timezone",
+        attribute: "localTimezone",
+        sortable: true,
+        visible: false
+      },
+      {
+        title: "Administration centre",
+        attribute: "isAdministrationCentre",
+        sortable: true,
+        type: "Boolean"
+      },
+      {
+        title: "Shown on web",
+        attribute: "isShownOnWeb",
+        sortable: true,
+        visible: false,
+        type: "Boolean"
+      },
+      {
+        title: "Is virtual",
+        attribute: "isVirtual",
+        sortable: true,
+        visible: false,
+        type: "Boolean"
+      }
+    ]
+  });
 }
