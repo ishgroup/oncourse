342230a5a9f015b349cbad138a6c11a3d30068b1
Chintan Kotadia <chintan@ish.com.au>
2021-02-01 10:43:24 +0530

    OD-15411 Updated sites entity list response and simplify params id for get site

--- client-html/src/dev/mock/api/entities/SiteApiMock.ts
+++ client-html/src/dev/mock/api/entities/SiteApiMock.ts
@@ -1,14 +1,11 @@
 import { promiseResolve } from "../../MockAdapter";
+import { getParamsId } from "../../mockUtils";
 
 export function SiteApiMock(mock) {
   this.api.onGet(new RegExp(`v1/list/entity/site/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
+    const id = getParamsId(config);
     return promiseResolve(config, this.db.getSite(id));
   });
 
-  this.api.onPut(new RegExp(`v1/list/entity/site/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
-
+  this.api.onPut(new RegExp(`v1/list/entity/site/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 }
