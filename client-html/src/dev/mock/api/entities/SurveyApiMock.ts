0dd7a3cfae7b4ae22f68274b7d73106c7f5aea70
Chintan Kotadia <chintan@ish.com.au>
2021-02-02 15:19:55 +0530

    OD-15411 Updated surveys entity response, and removed unused mock route and data of delete survey

--- client-html/src/dev/mock/api/entities/SurveyApiMock.ts
+++ client-html/src/dev/mock/api/entities/SurveyApiMock.ts
@@ -1,20 +1,11 @@
 import { promiseResolve } from "../../MockAdapter";
+import { getParamsId } from "../../mockUtils";
 
 export function SurveyApiMock(mock) {
   this.api.onGet(new RegExp(`v1/list/entity/survey/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
+    const id = getParamsId(config);
     return promiseResolve(config, this.db.getSurvey(id));
   });
 
-  this.api.onPut(new RegExp(`v1/list/entity/survey/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
-
-  this.api.onDelete(new RegExp(`v1/list/entity/survey/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
-    this.db.removeSurvey(id);
-    return promiseResolve(config, this.db.getSurveys());
-  });
+  this.api.onPut(new RegExp(`v1/list/entity/survey/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 }
