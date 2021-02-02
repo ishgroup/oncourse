462f649495eab2e211b39fde23c466b8c476eec4
Artyom Kravenko <artyom@ish.com.au>
2021-02-02 14:47:14 +0000

    OD-15467 Verify reCaptcha token
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65844 fb2e276d-fae9-0310-b578-b71ac85840b9

--- billing-api/src/main/groovy/ish/oncourse/willow/billing/filter/SessionFilter.groovy
+++ billing-api/src/main/groovy/ish/oncourse/willow/billing/filter/SessionFilter.groovy
@@ -1,29 +1,74 @@
 package ish.oncourse.willow.billing.filter
 
 import com.google.inject.Inject
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
 import ish.oncourse.api.access.AuthFilter
 import ish.oncourse.api.access.SessionCookie
 import ish.oncourse.api.request.RequestService
-import org.eclipse.jetty.server.Request
 
+import javax.ws.rs.ClientErrorException
 import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.ContainerRequestFilter
+import javax.ws.rs.core.Response
 
 @AuthFilter
 class SessionFilter  implements ContainerRequestFilter {
 
     RequestService requestService
+    
+    private static final String RE_CAPTCHA_TOKEN = 'x-g-recaptcha'
+    private static final String RE_CAPTCHA_HOST = 'https://www.google.com'
+    private static final String RE_CAPTCHA_PATH = '/recaptcha/api/siteverify'
+    private static final String RE_CAPTCHA_SECRET = '6LenRkYaAAAAAKhZKrYLzuEcy5W6Qspj_NgZJ0yy'
+    
     @Inject
-    AuthenticationFilter(
-            RequestService requestService) {
+    AuthenticationFilter(RequestService requestService) {
         this.requestService = requestService
     }
     
     @Override
     void filter(ContainerRequestContext requestContext) throws IOException {
-        SessionCookie sessionCookie = SessionCookie.valueOf(requestService.request as Request)
+        SessionCookie sessionCookie = SessionCookie.valueOf(requestService.request)
+        if (sessionCookie.exist) {
+            verifySession(sessionCookie.sessionNode)
+        } else  {
+            String reCaptcha = requestService.request.getHeader(RE_CAPTCHA_TOKEN)
+            if  (reCaptcha) {
+                HTTPBuilder builder =  new HTTPBuilder()
+                builder.post(
+                        [uri: RE_CAPTCHA_HOST,
+                         path: RE_CAPTCHA_PATH,
+                         contentType: ContentType.JSON,
+                         requestContentType: ContentType.JSON,
+                         body: [
+                                 secret: RE_CAPTCHA_SECRET,
+                                 response: reCaptcha
+                                ]
+                        ])
+                builder.handler['failure'] = { response, body ->
+                    throw new ClientErrorException('Request rejected, reCaptcha check failed', Response.Status.NOT_ACCEPTABLE)
+                }
+                builder.handler['success'] = { response, body ->
+                    if (body.success) {
+                        createSession()
+                    } else {
+                        throw new ClientErrorException('Request rejected, reCaptcha check failed', Response.Status.NOT_ACCEPTABLE)
+                    }
+                }
+            } else {
+                throw new ClientErrorException('Request rejected, no reCaptcha token provided', Response.Status.NOT_ACCEPTABLE)
+            }
+            
+        }
+
+    }
+
+    void verifySession(String sessionNode) {
         
+    }
 
-
+    void createSession() {
+        
     }
 }
