68afb2f44a54739e8bb9e83aca51593beaf7ae34
Chintan Kotadia <chintan@ish.com.au>
2021-02-03 17:48:35 +0530

    OD-15411 Removed unused delete route and mock data of voucherProducts, and simplify get params id

--- client-html/src/dev/mock/api/entities/VoucherProductApiMock.ts
+++ client-html/src/dev/mock/api/entities/VoucherProductApiMock.ts
@@ -1,25 +1,16 @@
 import { promiseResolve } from "../../MockAdapter";
+import { getParamsId } from "../../mockUtils";
 
 export function VoucherProductApiMock(mock) {
   this.api.onGet(new RegExp(`v1/list/entity/voucherProduct/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
+    const id = getParamsId(config);
     return promiseResolve(config, this.db.getVoucherProduct(id));
   });
 
-  this.api.onPut(new RegExp(`v1/list/entity/voucherProduct/\\d+`)).reply(config => {
-    return promiseResolve(config, JSON.parse(config.data));
-  });
+  this.api.onPut(new RegExp(`v1/list/entity/voucherProduct/\\d+`)).reply(config => promiseResolve(config, JSON.parse(config.data)));
 
   this.api.onPost("v1/list/entity/voucherProduct").reply(config => {
     this.db.createVoucherProduct(config.data);
-    return promiseResolve(config, this.db.getVoucherProducts());
-  });
-
-  this.api.onDelete(new RegExp(`v1/list/entity/voucherProduct/\\d+`)).reply(config => {
-    const params = config.url.split("/");
-    const id = params[params.length - 1];
-    this.db.removeVoucherProduct(id);
     return promiseResolve(config, this.db.getVoucherProducts());
   });
 }
