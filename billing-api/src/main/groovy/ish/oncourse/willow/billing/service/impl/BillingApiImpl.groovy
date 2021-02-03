9c4fe46ccb79366d29ee16ecb1db6d28cfbcaef4
Artyom Kravenko <artyom@ish.com.au>
2021-02-03 14:26:05 +0000

    OD-15467 vefify college name
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65864 fb2e276d-fae9-0310-b578-b71ac85840b9

--- billing-api/src/main/groovy/ish/oncourse/willow/billing/service/impl/BillingApiImpl.groovy
+++ billing-api/src/main/groovy/ish/oncourse/willow/billing/service/impl/BillingApiImpl.groovy
@@ -1,13 +1,33 @@
 package ish.oncourse.willow.billing.service.impl
 
+import com.google.inject.Inject
+import ish.oncourse.model.College
+import ish.oncourse.model.WebSite
+import ish.oncourse.services.persistence.ICayenneService
 import ish.oncourse.willow.billing.v1.model.CollegeDTO
 import ish.oncourse.willow.billing.v1.service.BillingApi
+import org.apache.cayenne.query.ObjectSelect
 
 class BillingApiImpl implements BillingApi {
 
+    private ICayenneService cayenneService
 
+
+    @Inject
+    BillingApiImpl(ICayenneService cayenneService) {
+        this.cayenneService = cayenneService
+    }
+    
+    
     @Override
     void createCollege(CollegeDTO college) {
+    }
 
+    @Override
+    Boolean verifyCollegeName(String name, String xGRecaptcha) {
+        return ObjectSelect.query(College)
+                .where(College.COLLEGE_KEY.eq(name))
+                .or(College.WEB_SITES.dot(WebSite.NAME).eq(name))
+                .select(cayenneService.newContext()).empty
     }
 }
