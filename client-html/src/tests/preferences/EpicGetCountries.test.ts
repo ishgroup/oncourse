1bdb0fce24625d7cd99b246c8e6b5e53a6ef96df
Chintan Kotadia <chintan@ish.com.au>
2021-02-04 17:56:01 +0530

    OD-15411 Added jest tests in preferences for get countries epic

---
+++ client-html/src/tests/preferences/EpicGetCountries.test.ts
@@ -0,0 +1,24 @@
+import { DefaultEpic } from "../common/Default.Epic";
+import {
+  GET_COUNTRIES_REQUEST_FULFILLED,
+  getCountries
+} from "../../js/containers/preferences/actions";
+import { EpicGetCountries } from "../../js/containers/preferences/epics/EpicGetCountries";
+
+describe("Get countries epic tests", () => {
+  it("EpicGetCountries should returns correct values", () => DefaultEpic({
+    action: getCountries(),
+    epic: EpicGetCountries,
+    processData: mockedApi => {
+      const data = mockedApi.db.countries;
+      const countries = data.sort((a, b) => (a.name[0] > b.name[0] ? 1 : -1));
+
+      return [
+        {
+          type: GET_COUNTRIES_REQUEST_FULFILLED,
+          payload: { countries }
+        }
+      ];
+    }
+  }));
+});
