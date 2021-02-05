16153d134c4c8f7c71d6f63746c3de417573595b
Chintan Kotadia <chintan@ish.com.au>
2021-02-05 17:33:50 +0530

    OD-15411 Updated default preferences epic and updated other preferences by keys

--- client-html/src/tests/common/GetPreferences.Epic.ts
+++ client-html/src/tests/common/GetPreferences.Epic.ts
@@ -1,38 +1,23 @@
-import { store, mockedAPI } from "../TestEntry";
-import { ActionsObservable } from "redux-observable";
 import { PreferenceEnum } from "@api/model";
+import { mockedAPI } from "../TestEntry";
 import {
-  FETCH_FINISH,
-  FETCH_START,
-  GET_USER_PREFERENCES,
-  GET_USER_PREFERENCES_FULFILLED
+  GET_USER_PREFERENCES_FULFILLED,
+  getUserPreferences
 } from "../../js/common/actions";
 import { EpicGetUserPreferences } from "../../js/common/epics/EpicGetUserPreferences";
-import { filter, toArray } from "rxjs/operators";
+import { DefaultEpic } from "./Default.Epic";
 
-export const GetPreferences = (keys: PreferenceEnum[]) => {
-  // Expected response
-  const preferences = mockedAPI.db.getUserPreferences(keys);
+export const GetPreferences = (keys: PreferenceEnum[]) => DefaultEpic({
+  action: getUserPreferences(keys),
+  epic: EpicGetUserPreferences,
+  processData: () => {
+    const preferences = mockedAPI.db.getUserPreferences(keys);
 
-  // Redux action to trigger epic
-  const action$ = ActionsObservable.of({ type: GET_USER_PREFERENCES, payload: keys });
-
-  // Initializing epic instance
-  const epic$ = EpicGetUserPreferences(action$, store, {});
-
-  // Testing epic to be resolved with expected array of actions
-  return expect(
-    epic$
-      .pipe(
-        // Filtering common actions
-        filter(a => [FETCH_START, FETCH_FINISH].includes(a.type) === false),
-        toArray()
-      )
-      .toPromise()
-  ).resolves.toEqual([
-    {
-      type: GET_USER_PREFERENCES_FULFILLED,
-      payload: { preferences }
-    }
-  ]);
-};
+    return [
+      {
+        type: GET_USER_PREFERENCES_FULFILLED,
+        payload: { preferences }
+      }
+    ];
+  }
+});
