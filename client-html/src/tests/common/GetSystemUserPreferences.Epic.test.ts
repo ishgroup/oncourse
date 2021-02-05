5e7491729e13b87d152623a060f1670c67d4eda8
Chintan Kotadia <chintan@ish.com.au>
2021-02-05 18:00:45 +0530

    OD-15411 Renamed get preferences epic to get user preferences epic and updated related preferences test

--- client-html/src/tests/common/GetSystemUserPreferences.Epic.test.ts
+++ client-html/src/tests/common/GetSystemUserPreferences.Epic.test.ts
@@ -1,7 +1,7 @@
 import { SYSTEM_USER_ADMINISTRATION_CENTER } from "../../js/constants/Config";
-import { GetPreferences } from "./GetPreferences.Epic";
+import { GetUserPreferences } from "./GetUserPreferences.Epic";
 
 describe("Get system user preferences epic tests", () => {
   it("[SYSTEM_USER_ADMINISTRATION_CENTER] should return correct actions", () => 
-    GetPreferences([SYSTEM_USER_ADMINISTRATION_CENTER]));
+    GetUserPreferences([SYSTEM_USER_ADMINISTRATION_CENTER]));
 });
--- client-html/src/tests/entities/voucherProducts/GetDefaultLiabilityAccountPreferences.Epic.test.ts
+++ client-html/src/tests/entities/voucherProducts/GetDefaultLiabilityAccountPreferences.Epic.test.ts
@@ -1,7 +1,7 @@
 import { ACCOUNT_DEFAULT_VOUCHER_LIABILITY_ID } from "../../../js/constants/Config";
-import { GetPreferences } from "../../common/GetPreferences.Epic";
+import { GetUserPreferences } from "../../common/GetUserPreferences.Epic";
 
 describe("Get default voucher liability account preferences epic tests", () => {
   it("should return correct actions", () =>
-    GetPreferences([ACCOUNT_DEFAULT_VOUCHER_LIABILITY_ID]));
+    GetUserPreferences([ACCOUNT_DEFAULT_VOUCHER_LIABILITY_ID]));
 });
