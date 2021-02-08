bbca55f3f4343f01a8561e82eed1ccc7312e1556
Rostislav Zenov <rostislav@ish.com.au>
2021-02-09 05:48:18 +1100

    OD-15493 added a 'Checkout' entity to Cayenne model

---
+++ server/src/main/groovy/ish/oncourse/server/cayenne/Checkout.groovy
@@ -0,0 +1,14 @@
+/*
+ * Copyright ish group pty ltd 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.cayenne
+
+import ish.oncourse.server.cayenne.glue._Checkout
+
+class Checkout extends _Checkout implements Queueable {
+}
