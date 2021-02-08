8caea78000d01d4978547bcaf762a867c85983a8
Rostislav Zenov <rostislav@ish.com.au>
2021-02-08 18:49:00 +0000

    OD-15493 fixed relationship between Checkout and College
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@65891 fb2e276d-fae9-0310-b578-b71ac85840b9

--- common/cayenne-model/src/main/java/ish/oncourse/model/Checkout.java
+++ common/cayenne-model/src/main/java/ish/oncourse/model/Checkout.java
@@ -1,9 +1,19 @@
 package ish.oncourse.model;
 
 import ish.oncourse.model.auto._Checkout;
+import ish.oncourse.utils.QueueableObjectUtils;
 
-public class Checkout extends _Checkout {
+public class Checkout extends _Checkout implements Queueable {
 
-    private static final long serialVersionUID = 1L; 
+    private static final long serialVersionUID = 1L;
 
+    @Override
+    public Long getId() {
+        return QueueableObjectUtils.getId(this);
+    }
+
+    @Override
+    public boolean isAsyncReplicationAllowed() {
+        return true;
+    }
 }
