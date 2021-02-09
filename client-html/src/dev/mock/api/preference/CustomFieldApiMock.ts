af4a168e12c20c23990af5ebdee8f0142320329d
Chintan Kotadia <chintan@ish.com.au>
2021-02-09 15:33:57 +0530

    OD-15411 Added jest tests in preferences for update custom field epic and updated get custom fields plain response and removed unused error code from mock api

--- client-html/src/dev/mock/api/preference/CustomFieldApiMock.ts
+++ client-html/src/dev/mock/api/preference/CustomFieldApiMock.ts
@@ -14,17 +14,6 @@ export function CustomFieldApiMock(mock) {
    * Mock Custom Fields save success or error
    * */
   this.api.onPost("v1/preference/field/type").reply(config => {
-    this.returnError = !this.returnError;
-
-    if (this.returnError) {
-      const errorObj: ValidationError = {
-        id: "5684452",
-        propertyName: "defaultValue",
-        errorMessage: "Default Value is invalid"
-      };
-
-      return promiseReject(config, errorObj);
-    }
     const data = JSON.parse(config.data);
     data.forEach(i => {
       if (!i.id) i.id = Math.random().toString();
