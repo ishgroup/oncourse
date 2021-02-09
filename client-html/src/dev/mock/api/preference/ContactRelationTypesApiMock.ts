b086f0212c23c71054553767e96a471fabe40650
Chintan Kotadia <chintan@ish.com.au>
2021-02-09 11:58:48 +0530

    OD-15411 Added jest tests in preferences for delete contact relation type epic, update mock api and data for contact relation types

--- client-html/src/dev/mock/api/preference/ContactRelationTypesApiMock.ts
+++ client-html/src/dev/mock/api/preference/ContactRelationTypesApiMock.ts
@@ -1,31 +1,19 @@
-import { promiseReject, promiseResolve } from "../../MockAdapter";
 import { ValidationError } from "@api/model";
+import { promiseReject, promiseResolve } from "../../MockAdapter";
+import { getParamsId } from "../../mockUtils";
 
 export function ContactRelationTypesApiMock(mock) {
   /**
    * Contact Relation Type items
-   **/
+   * */
 
   this.returnError = false;
 
-  this.api.onGet("v1/preference/contact/relation/type").reply(config => {
-    return promiseResolve(config, this.db.contactRelationTypes);
-  });
+  this.api.onGet("v1/preference/contact/relation/type").reply(config => promiseResolve(config, this.db.contactRelationTypes));
   /**
    * Mock Contact Relation Types save success or error
-   **/
+   * */
   this.api.onPost("v1/preference/contact/relation/type").reply(config => {
-    this.returnError = !this.returnError;
-
-    if (this.returnError) {
-      const errorObj: ValidationError = {
-        id: "32435",
-        propertyName: "reverseRelationName",
-        errorMessage: "Reverse Relation Name is invalid"
-      };
-
-      return promiseReject(config, errorObj);
-    }
     const data = JSON.parse(config.data);
     data.forEach(i => {
       if (!i.id) i.id = Math.random().toString();
@@ -35,9 +23,9 @@ export function ContactRelationTypesApiMock(mock) {
   });
   /**
    * Mock Contact Relation Type delete
-   **/
+   * */
   this.api.onDelete(new RegExp(`v1/preference/contact/relation/type/\\d+`)).reply(config => {
-    const id = config.url.split("/")[5];
+    const id = getParamsId(config);
     this.db.removeContactRelationType(id);
     return promiseResolve(config, this.db.contactRelationTypes);
   });
