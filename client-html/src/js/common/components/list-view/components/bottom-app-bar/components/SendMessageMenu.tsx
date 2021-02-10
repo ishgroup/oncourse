11e5c07bbc07aca67382bc5c83e66653d61a6498
Yury Yasuchenya <yury@ish.com.au>
2021-02-10 11:09:17 +0300

    OD-15524 Fix Send message list option

--- client-html/src/js/common/components/list-view/components/bottom-app-bar/components/SendMessageMenu.tsx
+++ client-html/src/js/common/components/list-view/components/bottom-app-bar/components/SendMessageMenu.tsx
@@ -1,10 +1,8 @@
 import MenuItem from "@material-ui/core/MenuItem";
-import React, { useCallback, useEffect, useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { EMAIL_FROM_KEY } from "../../../../../../constants/Config";
 import { sendMessage } from "../../../../../../containers/entities/messages/actions";
-import { getEmailTemplatesWithKeyCode, getUserPreferences } from "../../../../../actions";
 import { setListNestedEditRecord } from "../../../actions";
 
 interface SendMessageMenuProps {
@@ -17,33 +15,12 @@ interface SendMessageMenuProps {
   closeAll: () => void;
 }
 
-const EntitiesToMessageTemplateEntitiesMap = {
-  Invoice: ["Contact", "Invoice"],
-  Application: ["Contact", "Application"],
-  Contact: ["Contact"],
-  Enrolment: ["Contact", "Enrolment"],
-  CourseClass: ["Contact", "CourseClass", "Enrolment"],
-  PaymentIn: ["Contact", "PaymentIn"],
-  PaymentOut: ["Contact", "PaymentOut"],
-  Payslip: ["Contact", "Payslip"],
-  ProductItem: ["Contact", "Voucher", "Membership", "Article", "ProductItem"],
-  WaitingList: ["Contact", "WaitingList"]
-};
-
-const getMessageTemplateEntities = entity => EntitiesToMessageTemplateEntitiesMap[entity] || [entity];
-
 const SendMessageMenu: React.FC<SendMessageMenuProps> = props => {
   const {
-    selection, entity, getMessageTemplates, getEmailFrom, openSendMessageEditView, closeAll
+    selection, entity, openSendMessageEditView, closeAll
   } = props;
 
   const isNew = useMemo(() => Array.isArray(selection) && selection.length >= 1 && selection[0].toLowerCase() === "new", [selection]);
-
-  useEffect(() => {
-    if (!entity) return;
-    getMessageTemplates(getMessageTemplateEntities(entity));
-    getEmailFrom();
-  }, [entity]);
 
   const onSendMessage = useCallback(() => {
     openSendMessageEditView({ selection });
@@ -57,10 +34,7 @@ const SendMessageMenu: React.FC<SendMessageMenuProps> = props => {
   );
 };
 
-
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  getMessageTemplates: (entities: string[]) => dispatch(getEmailTemplatesWithKeyCode(entities)),
-  getEmailFrom: () => dispatch(getUserPreferences([EMAIL_FROM_KEY])),
   openSendMessageEditView: (record: any) => dispatch(
     setListNestedEditRecord("SendMessage", record, model => dispatch(sendMessage(model)), true)
   )
