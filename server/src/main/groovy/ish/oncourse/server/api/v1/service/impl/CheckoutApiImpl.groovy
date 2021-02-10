7e4447e617673bad4aba1b19187a0796aeffb519
Rostislav Zenov <rostislav@ish.com.au>
2021-02-11 01:17:03 +1100

    OD-15458 to throw error if course doesn't afford to course-module relation

--- server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/CheckoutApiImpl.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/CheckoutApiImpl.groovy
@@ -17,14 +17,17 @@ import ish.common.types.EntityRelationCartAction
 import ish.oncourse.server.api.dao.ContactDao
 import ish.oncourse.server.api.dao.CourseDao
 import ish.oncourse.server.api.dao.EntityRelationDao
+import ish.oncourse.server.api.dao.ModuleDao
 import ish.oncourse.server.api.dao.ProductDao
 import ish.oncourse.server.api.v1.model.CheckoutSaleRelationDTO
 import ish.oncourse.server.api.v1.model.EntityRelationCartActionDTO
 import ish.oncourse.server.api.v1.model.SaleDTO
 import ish.oncourse.server.api.v1.model.SaleTypeDTO
 import ish.oncourse.server.cayenne.Course
+import ish.oncourse.server.cayenne.Module
 import ish.oncourse.server.cayenne.EntityRelation
 import ish.oncourse.server.cayenne.EntityRelationType
+import ish.oncourse.server.cayenne.Outcome
 import ish.oncourse.server.cayenne.Product
 import org.apache.commons.lang3.StringUtils
 
@@ -40,7 +43,6 @@ import ish.oncourse.server.api.checkout.Checkout
 import ish.oncourse.server.api.checkout.CheckoutController
 import ish.oncourse.server.api.dao.FundingSourceDao
 import ish.oncourse.server.api.dao.PaymentInDao
-import ish.oncourse.server.api.function.CayenneFunctions
 import ish.oncourse.server.api.service.ArticleProductApiService
 import ish.oncourse.server.api.service.ContactApiService
 import ish.oncourse.server.api.service.CourseClassApiService
@@ -63,7 +65,6 @@ import ish.oncourse.server.api.v1.service.CheckoutApi
 import ish.oncourse.server.cayenne.Article
 import ish.oncourse.server.cayenne.Contact
 import ish.oncourse.server.cayenne.CourseClass
-import ish.oncourse.server.cayenne.Discount
 import ish.oncourse.server.cayenne.DiscountCourseClass
 import ish.oncourse.server.cayenne.Membership
 import ish.oncourse.server.cayenne.MembershipProduct
@@ -107,6 +108,9 @@ class CheckoutApiImpl implements CheckoutApi {
     
     @Inject
     ProductDao productDao
+
+    @Inject
+    ModuleDao moduleDao
     
     @Inject
     PreferenceController preferenceController
@@ -192,7 +196,18 @@ class CheckoutApiImpl implements CheckoutApi {
     List<CheckoutSaleRelationDTO> getSaleRelations(Long id, String entityName, Contact contact) {
         ObjectContext context = cayenneService.newContext
         List<EntityRelation> relations = EntityRelationDao.getRelatedToOrEqual(context, entityName, id)
-                .findAll { EntityRelationCartAction.NO_ACTION != it.relationType.shoppingCart }
+
+        relations.findAll { Module.simpleName == it.toEntityIdentifier }.each { relation ->
+            Module module = moduleDao.getById(context, relation.toEntityAngelId)
+
+            if (!(contact.student?.enrolments?.collect { it.outcomes } as List<Outcome>)?.collect { it.module }?.any { module == it }) {
+                if (!(contact.student?.priorLearnings?.collect { it.outcomes } as List<Outcome>)?.collect { it.module }?.any { module == it }) {
+                    hanbleError(VALIDATION_ERROR, [new CheckoutValidationErrorDTO(error: "You don't have necessary outcomes for that Course")])
+                }
+            }
+        }
+
+        relations = relations.findAll { EntityRelationCartAction.NO_ACTION != it.relationType.shoppingCart }
         List<CheckoutSaleRelationDTO> result = []
         
         relations.findAll { Course.simpleName == it.toEntityIdentifier && id != it.toEntityAngelId }.each { relation ->
