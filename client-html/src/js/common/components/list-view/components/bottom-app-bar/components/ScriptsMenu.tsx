b9df060e365393d2cc6442ede8dfb7abf14fc913
Yury Yasuchenya <yury@ish.com.au>
2021-02-10 16:54:25 +0300

    OD-15524 Adjust common scripts menu rendering logic

--- client-html/src/js/common/components/list-view/components/bottom-app-bar/components/ScriptsMenu.tsx
+++ client-html/src/js/common/components/list-view/components/bottom-app-bar/components/ScriptsMenu.tsx
@@ -3,19 +3,14 @@
  * No copying or use of this code is allowed without permission in writing from ish.
  */
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import MenuItem from "@material-ui/core/MenuItem";
-import { connect } from "react-redux";
 import { Script } from "@api/model";
 import { Menu } from "@material-ui/core";
-import { Dispatch } from "redux";
-import { getScripts } from "../../../../../actions";
 
 interface ScriptsMenuProps {
   scripts?: Script[];
   classes: any;
-  getScripts: (entity: string) => void;
-  executeScript: (scriptId: number, entity: string, entityIds: string[]) => void;
   closeAll: () => void;
   entity: string;
   openScriptModal: (scriptId: number) => void;
@@ -23,16 +18,10 @@ interface ScriptsMenuProps {
 
 const ScriptsMenu = React.memo<ScriptsMenuProps>(props => {
   const {
-    getScripts, entity, scripts, classes, closeAll, openScriptModal
+     scripts, classes, closeAll, openScriptModal
   } = props;
 
   const [scriptsMenuOpen, setScriptsMenuOpen] = useState(null);
-
-  useEffect(() => {
-    if (!scripts) {
-      getScripts(entity);
-    }
-  }, []);
 
   return (
     <>
@@ -74,9 +63,4 @@ const ScriptsMenu = React.memo<ScriptsMenuProps>(props => {
   );
 });
 
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
-  getScripts: (entity: string) => dispatch(getScripts(entity))
-});
-
-export default connect<any, any, any>(null, mapDispatchToProps)(ScriptsMenu);
+export default ScriptsMenu;
