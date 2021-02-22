8cfe3fd27f1183c5b9e0cc752bd62fad35e02037
Rostislav Zenov <rostislav@ish.com.au>
2021-02-23 04:46:13 +1100

    OD-15540 implemented GET method which return data of AssessmentSubmission

--- server/src/main/groovy/ish/oncourse/server/api/service/AssessmentSubmissionApiService.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/service/AssessmentSubmissionApiService.groovy
@@ -8,21 +8,41 @@
 
 package ish.oncourse.server.api.service
 
+import com.google.inject.Inject
 import ish.oncourse.server.api.dao.AssessmentSubmissionDao
 import ish.oncourse.server.api.v1.model.AssessmentSubmissionDTO
 import ish.oncourse.server.cayenne.AssessmentSubmission
+import ish.oncourse.server.document.DocumentService
+import ish.util.DateFormatter
+import ish.util.LocalDateUtils
 import org.apache.cayenne.ObjectContext
+
+import static ish.oncourse.server.api.v1.function.DocumentFunctions.toRestDocument
 
 class AssessmentSubmissionApiService extends EntityApiService<AssessmentSubmissionDTO, AssessmentSubmission, AssessmentSubmissionDao> {
 
+    @Inject
+    private DocumentService documentService
+
     @Override
     Class<AssessmentSubmission> getPersistentClass() {
-        return null
+        return AssessmentSubmission
     }
 
     @Override
     AssessmentSubmissionDTO toRestModel(AssessmentSubmission cayenneModel) {
-        return null
+        new AssessmentSubmissionDTO().with { dtoModel ->
+            dtoModel.id = cayenneModel.id
+            dtoModel.createdOn = LocalDateUtils.dateToTimeValue(cayenneModel.createdOn)
+            dtoModel.modifiedOn = LocalDateUtils.dateToTimeValue(cayenneModel.modifiedOn)
+            dtoModel.submittedOn = LocalDateUtils.dateToTimeValue(cayenneModel.submittedOn)
+            dtoModel.markedOn = LocalDateUtils.dateToTimeValue(cayenneModel.markedOn)
+            dtoModel.studentName = cayenneModel.studentName
+            dtoModel.courseClass = cayenneModel.courseClassName
+            dtoModel.assessment = cayenneModel.assessmentName
+            dtoModel.documents = cayenneModel.activeAttachments.collect { toRestDocument(it.document, it.documentVersion?.id, documentService) }
+            dtoModel
+        }
     }
 
     @Override
@@ -38,5 +58,24 @@ class AssessmentSubmissionApiService extends EntityApiService<AssessmentSubmissi
     @Override
     void validateModelBeforeRemove(AssessmentSubmission cayenneModel) {
 
+    }
+
+    Closure getAction (String key, String value) {
+        Closure action = super.getAction(key, value)
+        switch (key) {
+            case AssessmentSubmission.SUBMITTED_ON.name:
+                action = { AssessmentSubmission submission ->
+                    submission.submittedOn = DateFormatter.parseDate(value, null)
+                }
+                break
+            case AssessmentSubmission.MARKED_ON.name:
+                action = { AssessmentSubmission submission ->
+                    submission.markedOn = DateFormatter.parseDate(value, null)
+                }
+                break
+            default:
+                validator.throwClientErrorException(key, "Unsupported attribute")
+        }
+        action
     }
 }
