8d545dad6e57d09430697037bb043dd28342df94
Vadim Haponov <vadim@ish.com.au>
2021-02-24 13:08:51 +0300

    OD-15546 Add auto update, add links for scripts, update styles

--- client-html/src/js/containers/dashboard/components/action-body/components/ScriptStatistic.tsx
+++ client-html/src/js/containers/dashboard/components/action-body/components/ScriptStatistic.tsx
@@ -20,6 +20,7 @@ import createStyles from "@material-ui/core/styles/createStyles";
 import withStyles from "@material-ui/core/styles/withStyles";
 import EntityService from "../../../../../common/services/EntityService";
 import { III_DD_MMM_YYYY_HH_MM } from "../../../../../common/utils/dates/format";
+import { openInternalLink } from "../../../../../common/utils/links";
 
 const styles = theme => createStyles({
   statisticGroup: {
@@ -55,12 +56,13 @@ const styles = theme => createStyles({
   smallScriptText: {
     fontSize: "12px",
     marginRight: "12px",
-    maxWidth: "130px",
-    width: "130px"
+    minWidth: "50%",
   },
   lastRunText: {
     width: "70px",
+    minWidth: "70px",
     fontSize: "12px",
+    marginLeft: theme.spacing(1),
   }
 });
 
@@ -69,7 +71,7 @@ const ScriptStatistic = (props: any) => {
 
   const { classes } = props;
 
-  useEffect(() => {
+  const getScriptsData = () => {
     EntityService.getPlainRecords(
       "Script",
       "name",
@@ -123,9 +125,9 @@ const ScriptStatistic = (props: any) => {
           }
 
           sortable.sort((a, b) => differenceInSeconds(
-              parseISO(a[1]),
-              parseISO(b[1]),
-            ));
+            parseISO(a[1]),
+            parseISO(b[1]),
+          ));
 
           const resultForRender = sortable.reverse().reduce((result, elem) => (
             { ...result, [elem[0]]: [...scriptsWithName[elem[0]]] }), {});
@@ -133,6 +135,14 @@ const ScriptStatistic = (props: any) => {
           setScripts(resultForRender);
         });
       });
+  };
+
+  useEffect(() => {
+    getScriptsData();
+
+    const interval = setInterval(getScriptsData, 60000);
+
+    return () => clearInterval(interval);
   }, []);
 
   const getTime = (date: any) => {
@@ -151,15 +161,22 @@ const ScriptStatistic = (props: any) => {
       <Grid container className={clsx(classes.containerStatisticGroup, classes.displayBlock)}>
         {Object.keys(scripts).map((key: string) => (
           <Grid item className={classes.statisticGroup}>
-            <ListItem dense disableGutters className={classes.smallScriptGroup}>
-              <Typography className={classes.smallScriptText} noWrap>
-                {key}
-              </Typography>
-              <Typography className={classes.lastRunText}>
-                {getTime(scripts[key][0].date)}
-              </Typography>
-              {scripts[key] && [...scripts[key]].reverse().map((elem: any) => (
-                elem.status === "SCRIPT_EXECUTED"
+            <ListItem className={classes.smallScriptGroup} dense disableGutters>
+              <Grid xs={6}>
+                <Typography
+                  onClick={() => openInternalLink(`/automation/script/${scripts[key][0].id}`)}
+                  className={clsx(classes.smallText, "linkDecoration", classes.leftColumn, classes.smallScriptText)}
+                  noWrap
+                >
+                  {key}
+                </Typography>
+              </Grid>
+              <Grid className={classes.smallScriptGroup} xs={6}>
+                <Typography className={classes.lastRunText}>
+                  {getTime(scripts[key][0].date)}
+                </Typography>
+                {scripts[key] && [...scripts[key]].reverse().map((elem: any) => (
+                  elem.status === "SCRIPT_EXECUTED"
                   ? (
                     <Tooltip title={`Succeeded at ${format(new Date(elem.date), III_DD_MMM_YYYY_HH_MM)}`}>
                       <Check className={classes.doneIcon} />
@@ -171,16 +188,17 @@ const ScriptStatistic = (props: any) => {
                     </Tooltip>
                   )
               ))}
-              <Link
-                href={`${window.location.origin}/audit?search=entityId is ${scripts[key][0].id} and entityIdentifier is "Script"`}
-                target="_blank"
-                color="textSecondary"
-                underline="none"
-              >
-                <Tooltip title="more...">
-                  <span> ...</span>
-                </Tooltip>
-              </Link>
+                <Link
+                  href={`${window.location.origin}/audit?search=entityId is ${scripts[key][0].id} and entityIdentifier is "Script"`}
+                  target="_blank"
+                  color="textSecondary"
+                  underline="none"
+                >
+                  <Tooltip title="more...">
+                    <span> ...</span>
+                  </Tooltip>
+                </Link>
+              </Grid>
             </ListItem>
           </Grid>
         ))}
