ba1693f03a237524deca46b0dc8e5d92b4d1ceb1
Yury Yasuchenya <yury@ish.com.au>
2021-03-02 19:08:34 +0300

    OD-15626 Add query card results counter. Fix material ui warnings

--- client-html/src/js/containers/automation/containers/scripts/components/cards/CardBase.tsx
+++ client-html/src/js/containers/automation/containers/scripts/components/cards/CardBase.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { ExpansionPanel, AccordionSummary, AccordionDetails } from "@material-ui/core";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import DragIndicator from "@material-ui/icons/DragIndicator";
 import Typography from "@material-ui/core/Typography";
@@ -71,7 +73,7 @@ const CardBase = props => {
   } = props;
 
   return (
-    <ExpansionPanel
+    <Accordion
       classes={{
         root: classes.panelRoot,
         expanded: disableExpandedBottomMargin ? classes.panelExpandedWithoutMargin : classes.panelExpanded
@@ -129,7 +131,7 @@ const CardBase = props => {
       >
         {children}
       </AccordionDetails>
-    </ExpansionPanel>
+    </Accordion>
   );
 };
 
