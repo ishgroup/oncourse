8bdfc8581b83c355ff06b82f63a5078caea5bd25
Rostislav Zenov <rostislav@ish.com.au>
2021-03-04 21:14:26 +1100

    OD-15581 refreshed mailChimp pull unsubscribes script: use query closure, use number of days as option etc

--- server/src/main/resources/scripts/ish.script.mailchimpPullUnsubscribes.groovy
+++ server/src/main/resources/scripts/ish.script.mailchimpPullUnsubscribes.groovy
@@ -1,25 +1,25 @@
-import org.apache.cayenne.query.ObjectSelect
-
-import java.time.LocalDate
-
-def sinceDate
-
-
-def preference = ObjectSelect.query(Preference).where(Preference.NAME.eq("mailchimp.pullUnsubscribes.lastRun")).selectOne(context)
-if (!preference) {
-    preference = context.newObject(Preference)
-    preference.name = "mailchimp.pullUnsubscribes.lastRun"
-    preference.uniqueKey = "mailchimp.pullUnsubscribes.lastRun"
-    // pull all unsubscribes
-    preference.valueString = "1970-01-01"
-    context.commitChanges()
+records = query {
+    entity "Preference"
+    query "name is 'mailchimp.pullUnsubscribes.lastRun'"
 }
-sinceDate = LocalDate.parse(preference.valueString).minusDays(7)
+
+def lastRun
+if (records.empty) {
+    lastRun = context.newObject(Preference)
+    lastRun.name = "mailchimp.pullUnsubscribes.lastRun"
+    lastRun.uniqueKey = "mailchimp.pullUnsubscribes.lastRun"
+    // pull all unsubscribes
+    lastRun.valueString = "1970-01-01"
+    context.commitChanges()
+} else {
+    lastRun = records[0]
+}
+def sinceDate = java.time.LocalDate.parse(lastRun.valueString).minusDays(numberOfDays.toInteger())
 
 mailchimp {
     action "pull unsubscribes"
     since sinceDate
 }
 
-preference.valueString = LocalDate.now().toString()
-context.commitChanges()
+lastRun.valueString = java.time.LocalDate.now().toString()
+lastRun.context.commitChanges()
