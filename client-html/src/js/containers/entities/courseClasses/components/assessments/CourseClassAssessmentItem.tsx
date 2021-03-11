661482e9be24257a3930f6e1eff2d62185a59ffe
Vadim Haponov <vadim@ish.com.au>
2021-03-11 10:46:22 +0300

    OD-15537 Add AS

--- client-html/src/js/containers/entities/courseClasses/components/assessments/CourseClassAssessmentItem.tsx
+++ client-html/src/js/containers/entities/courseClasses/components/assessments/CourseClassAssessmentItem.tsx
@@ -3,7 +3,10 @@
  * No copying or use of this code is allowed without permission in writing from ish.
  */
 
-import React, { useCallback, useMemo, useRef } from "react";
+import React, {
+ useCallback, useEffect, useMemo, useRef, useState 
+} from "react";
+import clsx from "clsx";
 import { Dispatch } from "redux";
 import { change, Field } from "redux-form";
 import { connect } from "react-redux";
@@ -11,19 +14,52 @@ import Grid from "@material-ui/core/Grid";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { differenceInDays } from "date-fns";
 import { Assessment, AssessmentClass, CourseClassTutor } from "@api/model";
+import { createStyles, withStyles } from "@material-ui/core/styles";
 import FormField from "../../../../../common/components/form/form-fields/FormField";
 import { StyledCheckbox } from "../../../../../common/components/form/form-fields/CheckboxField";
 import { validateSingleMandatoryField } from "../../../../../common/utils/validation";
 import { stubComponent } from "../../../../../common/utils/common";
 import { defaultContactName } from "../../../contacts/utils";
 import { State } from "../../../../../reducers/state";
-import { StudentAttendanceIconButton } from "../attendance/AttendanceIconButtons";
+import AssesmentsSubmissionActionsMenu from "./AssessmentsSubnissionActionMenu";
+import AssessmentSubmissionIconButton, { AssessmentsSubmissionType } from "./AssessmentSubmissionIconButton";
+import { AppTheme } from "../../../../../model/common/Theme";
+
+const styles = (theme: AppTheme) =>
+  createStyles({
+    rowWrapper: {
+      minHeight: "36px",
+      padding: "0 8px",
+      "&:hover .invisible": {
+        visibility: "visible",
+      }
+    },
+    items: {
+      marginLeft: -8,
+      marginRight: -8,
+      "& > div:nth-child(even)": {
+        backgroundColor: theme.table.contrastRow.light
+      },
+      "&:first-child": {
+        marginTop: 0
+      }
+    },
+    tableHeader: {
+      marginLeft: -8,
+      marginRight: -8,
+    },
+    center: {
+      display: "flex",
+      justifyContent: "center"
+    }
+  });
 
 interface Props {
   form: string;
   item: string;
   dispatch: Dispatch;
   tutors: CourseClassTutor[];
+  classes,
   courseClassEnrolments?: any[],
   twoColumn?: boolean;
   row?: AssessmentClass;
@@ -35,12 +71,72 @@ const CourseClassAssessmentItem: React.FC<Props> = props => {
     form,
     row,
     rows,
+    classes,
     item,
     tutors,
     twoColumn,
     courseClassEnrolments,
     dispatch
   } = props;
+
+  const [studentsForRender, setStudentsForRender] = useState([]);
+
+  useEffect(() => {
+    const result = courseClassEnrolments && courseClassEnrolments.reduce((acc, elem) => [...acc, {
+      studentName: elem.student,
+      sumbittedIndex: 0,
+      submittedValue: "Not submitted",
+      submittedOn: null,
+      markedIndex: 0,
+      markedValue: "Not submitted",
+      markedOn: null,
+      tutorName: '',
+      tutorId: tutors ? tutors[0].contactId : 0,
+    }], []);
+
+    setStudentsForRender(result);
+  }, [courseClassEnrolments]);
+
+  const onChangeStatus = (type: string, student: any) => {
+    if (type === "Submitted") {
+      const newStudentsForRender = studentsForRender.map(elem => {
+        if (student.studentName === elem.studentName) {
+          const type = [2, -1].includes(student.sumbittedIndex)
+            ? AssessmentsSubmissionType[0] : AssessmentsSubmissionType[student.sumbittedIndex + 1];
+          elem.submittedValue = type;
+          elem.sumbittedIndex = [2, -1].includes(student.sumbittedIndex) ? 0 : student.sumbittedIndex + 1;
+        }
+
+        return elem;
+      });
+      setStudentsForRender(newStudentsForRender);
+    }
+
+    if (type === "Marked") {
+      const newStudentsForRender = studentsForRender.map(elem => {
+        if (student.studentName === elem.studentName) {
+          const type = [2, -1].includes(student.markedIndex)
+            ? AssessmentsSubmissionType[0] : AssessmentsSubmissionType[student.markedIndex + 1];
+          elem.markedValue = type;
+          elem.markedIndex = [2, -1].includes(student.markedIndex) ? 0 : student.markedIndex + 1;
+        }
+
+        return elem;
+      });
+      setStudentsForRender(newStudentsForRender);
+    }
+  };
+
+  const onSelectTutor = (tutorName: string, studentName:string) => {
+    const newStudentsForRender = studentsForRender.map(elem => {
+      if (elem.studentName === studentName) {
+        elem.tutorName = tutorName;
+      }
+
+      return elem;
+    });
+    setStudentsForRender(newStudentsForRender);
+  };
 
   const tutorsUpdater = useRef<any>();
 
@@ -175,17 +271,45 @@ const CourseClassAssessmentItem: React.FC<Props> = props => {
 
       <Grid container className="pb-3">
         <div className="heading">Assessment Submission</div>
-        {courseClassEnrolments.map((elem: any) => (
-          <Grid container>
-            {elem.student}
-            {/* <StudentAttendanceIconButton */}
-            {/*  attendance={{ */}
-            {/*    attendanceType: "Unmarked", */}
-            {/*  }} */}
-            {/*  onClick={e => onStudentIconClick(e, attendance.index)} */}
-            {/* /> */}
+        <Grid container xs={12} className={classes.tableHeader}>
+          <Grid item xs={4} />
+          <Grid item xs={2} className={classes.center}>
+            Submitted
           </Grid>
-        ))}
+          <Grid xs={2} className={classes.center}>
+            Marked
+          </Grid>
+        </Grid>
+        <Grid container xs={12} className={classes.items}>
+          {studentsForRender.map((elem: any) => (
+            <Grid container key={elem.studentName} className={clsx(classes.rowWrapper, "align-items-center d-inline-flex-center")}>
+              <Grid item xs={4} className="d-inline-flex-center">
+                {elem.studentName}
+                &#8194;
+                {elem.tutorName ? `(${elem.tutorName})` : ""}
+
+                <AssesmentsSubmissionActionsMenu
+                  className="invisible"
+                  onChange={onSelectTutor}
+                  items={tutors}
+                  student={elem}
+                />
+              </Grid>
+              <Grid item xs={2} className={classes.center}>
+                <AssessmentSubmissionIconButton
+                  status={AssessmentsSubmissionType[elem.sumbittedIndex]}
+                  onClick={() => onChangeStatus("Submitted", elem)}
+                />
+              </Grid>
+              <Grid item xs={2} className={classes.center}>
+                <AssessmentSubmissionIconButton
+                  status={AssessmentsSubmissionType[elem.markedIndex]}
+                  onClick={() => onChangeStatus("Marked", elem)}
+                />
+              </Grid>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     </Grid>
   );
@@ -195,4 +319,4 @@ const mapStateToProps = (state: State) => ({
   courseClassEnrolments: state.courseClass.enrolments,
 });
 
-export default connect(mapStateToProps, null)(CourseClassAssessmentItem);
+export default connect(mapStateToProps, null)(withStyles(styles)(CourseClassAssessmentItem));
