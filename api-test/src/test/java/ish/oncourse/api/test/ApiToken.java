99c6661901910f974295e7db9661aee6bcd084ae
Artyom Kravchenko <artyom@ish.com.au>
2021-03-12 20:01:39 +1100

    OD-15548 Test to check api access with valid, inactive and un existed api tokens

---
+++ api-test/src/test/java/ish/oncourse/api/test/ApiToken.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright ish group pty ltd 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.api.test;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApiToken {
+    @Test
+    public void test() {
+        Results results = Runner.path("classpath:ish/oncourse/api/test/token/auth.feature").tags("~@ignore").parallel(1);
+        assertEquals(results.getErrorMessages(), results.getFailCount(), 0);
+    }
+}
