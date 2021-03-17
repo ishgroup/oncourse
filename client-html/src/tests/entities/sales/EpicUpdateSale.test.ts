246c3adfc1abf1daeddb72e110d220d1faa70128
Chintan Kotadia <chintan@ish.com.au>
2021-03-17 19:09:32 +0530

    OD-15751 Fixed update sale epic tests

--- client-html/src/tests/entities/sales/EpicUpdateSale.test.ts
+++ client-html/src/tests/entities/sales/EpicUpdateSale.test.ts
@@ -2,7 +2,7 @@ import { DefaultEpic } from "../../common/Default.Epic";
 import { FETCH_SUCCESS } from "../../../js/common/actions";
 import { GET_RECORDS_REQUEST } from "../../../js/common/components/list-view/actions";
 import { EpicUpdateSale } from "../../../js/containers/entities/sales/epics/EpicUpdateSale";
-import { GET_SALE, UPDATE_SALE_FULFILLED, updateSale } from "../../../js/containers/entities/sales/actions";
+import { UPDATE_SALE_FULFILLED, updateSale } from "../../../js/containers/entities/sales/actions";
 
 describe("Update sale epic tests", () => {
   it("EpicUpdateSale should returns correct values", () => DefaultEpic({
@@ -19,10 +19,6 @@ describe("Update sale epic tests", () => {
       {
         type: GET_RECORDS_REQUEST,
         payload: { entity: "ProductItem", listUpdate: true, savedID: "1" }
-      },
-      {
-        type: GET_SALE,
-        payload: { id: "1" }
       }
     ]
   }));
