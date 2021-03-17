4b593d25c6d73c34bd01b4bafb843267c5a28aa9
Artyom Kravenko <artyom@ish.com.au>
2021-03-17 14:19:14 +0000

    OD-15720 new table/entity for web layout paths. Create index for 'path' column - important for search
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@66098 fb2e276d-fae9-0310-b578-b71ac85840b9

---
+++ common/cayenne-model/src/main/java/ish/oncourse/model/WebLayoutPath.java
@@ -0,0 +1,9 @@
+package ish.oncourse.model;
+
+import ish.oncourse.model.auto._WebLayoutPath;
+
+public class WebLayoutPath extends _WebLayoutPath {
+
+    private static final long serialVersionUID = 1L; 
+
+}
