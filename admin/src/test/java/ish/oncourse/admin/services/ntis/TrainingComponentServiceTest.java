507371bc723fc1bbcc21fae5a2b35ec290fcf7d6
Aristedes Maniatis <ari@ish.com.au>
2021-03-19 04:13:54 +0000

    No need to hammer the NTIS server.
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@66113 fb2e276d-fae9-0310-b578-b71ac85840b9

--- admin/src/test/java/ish/oncourse/admin/services/ntis/TrainingComponentServiceTest.java
+++ admin/src/test/java/ish/oncourse/admin/services/ntis/TrainingComponentServiceTest.java
@@ -10,32 +10,27 @@ import org.junit.Test;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 public class TrainingComponentServiceTest extends ServiceTest {
-	
+
 	@Before
 	public void setup() throws Exception {
 		initTest("ish.oncourse.admin.services", "", AdminTestModule.class);
 	}
-	
+
 	/**
      * This method performs ten calls to the GetServerTime operation.
      */
     @Test
     public void testGetServerTime() throws Exception {
-    	        
+
     	ITrainingComponentService port = getService(ITrainingComponentService.class);
-        
-        for (int i = 0; i < 10; i++) {
-        	
-            long startTime = System.currentTimeMillis();
 
-            XMLGregorianCalendar serverDate = port.getServerTime();
+        long startTime = System.currentTimeMillis();
+        XMLGregorianCalendar serverDate = port.getServerTime();
 
-            long finishTime = System.currentTimeMillis();
-            double elapsedTime = (finishTime - startTime) / 1000d; // elapsed time in seconds
+        long finishTime = System.currentTimeMillis();
+        double elapsedTime = (finishTime - startTime) / 1000d; // elapsed time in seconds
 
-            System.out.println("Date retrieved - " + serverDate.toString() + " in " + elapsedTime + " seconds");
-
-			Assert.assertNotNull(serverDate);
-        }
+        System.out.println("Date retrieved - " + serverDate.toString() + " in " + elapsedTime + " seconds");
+        Assert.assertNotNull(serverDate);
     }
 }
