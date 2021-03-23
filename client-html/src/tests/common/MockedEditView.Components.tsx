f5ee814d884c9a33660f393a7015f59aa3526d1c
Chintan Kotadia <chintan@ish.com.au>
2021-03-23 15:55:38 +0530

    OD-15791 Added jest test edit view component for preferences concession-types, added default component edit view in common, updated concession types renderer and updated mocked edit view component for entity edit view

--- client-html/src/tests/common/MockedEditView.Components.tsx
+++ client-html/src/tests/common/MockedEditView.Components.tsx
@@ -3,6 +3,7 @@ import { createMount } from "@material-ui/core/test-utils";
 import { LIST_EDIT_VIEW_FORM_NAME } from "../../js/common/components/list-view/constants";
 import ListEditView from "../../js/common/components/list-view/components/edit-view/EditView";
 import { mockedAPI, TestEntry } from "../TestEntry";
+import { defaultComponents } from "./Default.Components";
 
 interface Props {
   entity: string;
@@ -15,44 +16,20 @@ export const mockedEditView: ({
   entity, EditView, record, render
 }: Props) => void = ({
   entity, EditView, record, render
-}) => {
-  const initialValues = record(mockedAPI);
-  let mount;
-
-  beforeAll(() => {
-    mount = createMount();
-  });
-
-  afterAll(() => {
-    mount.cleanUp();
-  });
-
-  const defaultProps = {
+}) => defaultComponents({
+  entity,
+  View: pr => <ListEditView {...pr} />,
+  record,
+  beforeFn: () => {
+    window.performance.getEntriesByName = jest.fn(() => []);
+  },
+  defaultProps: ({ entity }) => ({
     EditViewContent: props => <EditView {...props} />,
     rootEntity: entity,
-    initialValues,
-    values: initialValues,
     form: LIST_EDIT_VIEW_FORM_NAME,
     hasSelected: true,
     creatingNew: false
-  };
+  }),
+  render
+});
 
-  const MockedEditView = pr => <ListEditView {...{ ...pr, ...defaultProps }} />;
-
-  window.performance.getEntriesByName = jest.fn(() => []);
-
-  it(`${entity} components should render with given values`, () => {
-    const wrapper = mount(
-      <TestEntry>
-        <MockedEditView />
-      </TestEntry>
-    );
-
-    return new Promise(resolve => {
-      setTimeout(() => {
-        render(wrapper, initialValues);
-        resolve();
-      }, 1000);
-    });
-  });
-};
