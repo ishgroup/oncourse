b539c0c5b798617c0cc923c4ea3fa61c8d79aebc
Rostislav Zenov <rostislav@ish.com.au>
2021-04-07 06:53:53 +0000

    OW-3710 updated the current script which send tutor contract email
    git-svn-id: https://svn.ish.com.au/ish/angel-secret/trunk@66186 fb2e276d-fae9-0310-b578-b71ac85840b9

--- customers/byron/scripts/tutorContract.groovy
+++ customers/byron/scripts/tutorContract.groovy
@@ -1,18 +1,5 @@
-/*
- * Copyright ish group pty ltd. All rights reserved. https://www.ish.com.au
- * No copying or use of this code is allowed without permission in writing from ish.
- */
-
-def run(args) {
-	def courseClass = args.entity
-
-	// For now assume there is only one tutor
-	def tutor = courseClass.tutorRoles[0]?.tutor
-
-	email {
-		to tutor.contact
-		from "vicki@byroncollege.org.au"
-		template "tutor contract"
-		bindings tutor: tutor, cc: courseClass
-	}
+message {
+	from "vicki@byroncollege.org.au"
+	template tutorContractEmailTemplate
+	record records
 }
