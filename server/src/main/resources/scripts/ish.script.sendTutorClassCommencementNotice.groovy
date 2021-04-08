d98c4020a6fded37e2ace9c2e623b9389acf9859
Rostislav Zenov <rostislav@ish.com.au>
2021-04-09 02:03:29 +1000

    OD-15608 fixed typo; removed comments

--- server/src/main/resources/scripts/ish.script.sendTutorClassCommencementNotice.groovy
+++ server/src/main/resources/scripts/ish.script.sendTutorClassCommencementNotice.groovy
@@ -1,27 +1,23 @@
 if (send_before_each_session) {
 
-    // Query closure start
     records = query {
         entity "TutorAttendance"
         query "session.courseClass.startDateTime not is null and session.courseClass.isCancelled is false and session.startDatetime after today + 1 days and session.startDatetime before today + 3 days"
     }
-    // Query closure end
 
-    records.each { attandance ->
+    records.each { attendance ->
         message {
             template sessionCommencementTemplate
-            to attandance.courseClassTutor.tutor.contact
-            tutorAttendance attandance
+            to attendance.courseClassTutor.tutor.contact
+            tutorAttendance attendance
         }
     }
 
 } else {
-    // Query closure start
     records = query {
         entity "CourseClass"
         query "isCancelled is false and startDateTime not is null and startDateTime after today + 1 days and startDateTime before today + 3 days"
     }
-    // Query closure end
 
     message {
         template classCommencementTemplate
