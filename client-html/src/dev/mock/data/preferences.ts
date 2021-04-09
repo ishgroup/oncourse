5c5f4d8c18950e18952f7b00bfd18d05879fde44
Chintan Kotadia <chintan@ish.com.au>
2021-04-09 12:50:47 +0530

    OD-15869 Added jest test edit view component for preferences financial, and fixed financial mock form field values

--- client-html/src/dev/mock/data/preferences.ts
+++ client-html/src/dev/mock/data/preferences.ts
@@ -185,11 +185,11 @@ export function mockPreferences() {
     [Model.AccountTax.uniqueKey]: "3",
     [Model.AccountStudentEnrolments.uniqueKey]: "5",
     [Model.AccountPrepaidFees.uniqueKey]: "4",
-    [Model.AccountPrepaidFeesPostAt.uniqueKey]: "7",
+    [Model.AccountPrepaidFeesPostAt.uniqueKey]: "everySession",
     [Model.AccountVoucherLiability.uniqueKey]: "3",
     [Model.AccountVoucherUnderpayment.uniqueKey]: "9",
-    [Model.AccountDefaultCurrency.uniqueKey]: "2",
-    [Model.AccountInvoiceTerms.uniqueKey]: "1",
+    [Model.AccountDefaultCurrency.uniqueKey]: "AUD",
+    [Model.AccountInvoiceTerms.uniqueKey]: "7",
     [Model.PaymentInfo.uniqueKey]: "Some payment info",
     [Model.QePaymentDefaultZero.uniqueKey]: false
   };
