816367b08630f7249dee4124ff41e4e4fb4a48df
Rostislav Zenov <rostislav@ish.com.au>
2021-04-16 01:37:36 +1000

    OD-15882 added grading type dao

---
+++ server/src/main/groovy/ish/oncourse/server/api/dao/GradingTypeDao.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright ish group pty ltd 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.api.dao
+
+import ish.oncourse.server.cayenne.GradingType
+import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.query.ObjectSelect
+import org.apache.cayenne.query.SelectById
+
+class GradingTypeDao implements CayenneLayer<GradingType> {
+
+    @Override
+    GradingType newObject(ObjectContext context) {
+        return context.newObject(GradingType)
+    }
+
+    @Override
+    GradingType getById(ObjectContext context, Long id) {
+        return SelectById.query(GradingType, id)
+                .selectOne(context)
+    }
+
+    static List<GradingType> getGradingTypes(ObjectContext context) {
+        return ObjectSelect.query(GradingType)
+                .prefetch(GradingType.GRADING_ITEMS.joint())
+                .select(context)
+    }
+}
