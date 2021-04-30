cc14e6c92da9773d2aff6f75b4c2308e0c6c45b7
Rostislav Zenov <rostislav@ish.com.au>
2021-04-30 22:03:41 +1000

    fixed assertion according to new avetmiss rules

--- server/src/test/groovy/ish/oncourse/server/imports/avetmiss/AvetmissStudentImportTest.groovy
+++ server/src/test/groovy/ish/oncourse/server/imports/avetmiss/AvetmissStudentImportTest.groovy
@@ -6,10 +6,12 @@ package ish.oncourse.server.imports.avetmiss
 import ish.CayenneIshTestCase
 import ish.common.types.AvetmissStudentPriorEducation
 import ish.common.types.Gender
+import ish.common.types.UsiStatus
 import ish.imports.ImportParameter
 import ish.oncourse.common.ResourcesUtil
 import ish.oncourse.server.ICayenneService
 import ish.oncourse.server.cayenne.Contact
+import ish.oncourse.server.cayenne.Student
 import ish.oncourse.server.imports.ImportService
 import ish.oncourse.server.upgrades.DataPopulation
 import org.apache.cayenne.ObjectContext
@@ -41,9 +43,9 @@ class AvetmissStudentImportTest extends CayenneIshTestCase {
 
         Map<String, byte[]> data = new HashMap<>()
         data.put("avetmiss80", IOUtils.toByteArray(
-				ResourcesUtil.getResourceAsInputStream("ish/oncourse/server/export/avetmiss8/import/NAT00080.txt")))
+				ResourcesUtil.getResourceAsInputStream("ish/oncourse/server/imports/avetmiss8/NAT00080.txt")))
         data.put("avetmiss85", IOUtils.toByteArray(
-				ResourcesUtil.getResourceAsInputStream("ish/oncourse/server/export/avetmiss8/import/NAT00085.txt")))
+				ResourcesUtil.getResourceAsInputStream("ish/oncourse/server/imports/avetmiss8/NAT00085.txt")))
 
         parameter.setData(data)
 
@@ -62,6 +64,8 @@ class AvetmissStudentImportTest extends CayenneIshTestCase {
         assertTrue(contact1.getIsStudent())
         assertEquals(LocalDate.of(1964, 7, 2), contact1.getBirthDate())
         assertEquals(Gender.MALE, contact1.gender)
+        assertEquals("aBcd12345Z", contact1.student.usi)
+        assertEquals(UsiStatus.DEFAULT_NOT_SUPPLIED, contact1.student.usiStatus)
         assertEquals("0289834839", contact1.getHomePhone())
         assertEquals("049898923", contact1.getMobilePhone())
         assertEquals("0289893567", contact1.getWorkPhone())
@@ -69,6 +73,21 @@ class AvetmissStudentImportTest extends CayenneIshTestCase {
         assertEquals("2, 10, GULI ST", contact1.getStreet())
         assertEquals("SULAXITO", contact1.getSuburb())
         assertEquals(AvetmissStudentPriorEducation.MISC, contact1.getStudent().getPriorEducationCode())
-        assertEquals(new Integer(1976), contact1.getStudent().getYearSchoolCompleted())
+
+        List<Contact> withoutSuburb = ObjectSelect.query(Contact.class)
+                .where(Contact.SUBURB.isNull()).select(context)
+        assertEquals(1, withoutSuburb.size())
+
+        List<Contact> internationalUsiStatus = ObjectSelect.query(Contact.class)
+                .where(Contact.STUDENT.dot(Student.USI_STATUS).eq(UsiStatus.INTERNATIONAL)).select(context)
+        assertEquals(1, internationalUsiStatus.size())
+
+        List<Contact> exemptionUsiStatus = ObjectSelect.query(Contact.class)
+                .where(Contact.STUDENT.dot(Student.USI_STATUS).eq(UsiStatus.EXEMPTION)).select(context)
+        assertEquals(1, exemptionUsiStatus.size())
+
+        List<Contact> withEmptyEmail = ObjectSelect.query(Contact.class)
+                .where(Contact.EMAIL.isNull()).select(context)
+        assertEquals(3, withEmptyEmail.size())
     }
 }
