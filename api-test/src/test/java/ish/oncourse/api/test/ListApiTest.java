72265a28999bbb174a6527f28cb63695e2576caa
Aristedes Maniatis <ari@ish.com.au>
2021-05-03 14:52:54 +1000

    Fix some junit5 migration issues in the api-tests

--- api-test/src/test/java/ish/oncourse/api/test/ListApiTest.java
+++ api-test/src/test/java/ish/oncourse/api/test/ListApiTest.java
@@ -1,9 +1,6 @@
 package ish.oncourse.api.test;
 
-import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
-// was divided on small features by path api/test/list/...
-//@RunWith(Karate.class)
+
 @CucumberOptions(tags = "~@ignore", features = "classpath:ish/oncourse/api/test/list")
 public class ListApiTest {}
