58dab8492ba94c3d6ef9135c391a81181d2565fa
Yury Yasuchenya <yury@ish.com.au>
2021-05-04 14:36:39 +0000

    OD-15932 Catch cookie error
    git-svn-id: https://svn.ish.com.au/ish/willow/trunk@66289 fb2e276d-fae9-0310-b578-b71ac85840b9

--- checkout/src/js/common/utils/Cookie.ts
+++ checkout/src/js/common/utils/Cookie.ts
@@ -24,16 +24,21 @@ export function setCookie(name, value, options: any = {}) {
 
 export function getCookie(cname) {
   const name = cname + "=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1);
+  try {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
+  }
+  catch (e) {
+    return "";
   }
   return "";
 }
