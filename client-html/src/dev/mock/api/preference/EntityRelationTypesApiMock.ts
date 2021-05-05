a1238a4a35284287f6131b036d9479e1f8f4d6ec
Chintan Kotadia <chintan@ish.com.au>
2021-05-05 19:05:45 +0530

    OD-16058 Added mock route for preferences update entity relation type

--- client-html/src/dev/mock/api/preference/EntityRelationTypesApiMock.ts
+++ client-html/src/dev/mock/api/preference/EntityRelationTypesApiMock.ts
@@ -5,4 +5,8 @@ export function EntityRelationTypesApiMock(mock) {
    * Entity Relation Type items
    * */
   this.api.onGet("v1/preference/entity/relation/type").reply(config => promiseResolve(config, this.db.getEntityRelationTypes()));
+
+  this.api.onPost("v1/preference/entity/relation/type").reply(config => {
+    return promiseResolve(config, this.db.getEntityRelationTypes());
+  });
 }
