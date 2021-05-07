2c79097a00b0c0b047ac87c760b936d47491072b
Chintan Kotadia <chintan@ish.com.au>
2021-05-07 20:49:42 +0530

    OD-16070 Added jest tests component for preferences security settings form

---
+++ client-html/src/tests/security/SettingsForm.Components.test.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { mockedAPI } from "../TestEntry";
+import * as PreferencesModel from "../../js/model/preferences";
+import { defaultComponents } from "../common/Default.Components";
+import Settings from "../../js/containers/security/containers/settings/Settings";
+
+describe("Virtual rendered SecuritySettingsForm", () => {
+  defaultComponents({
+    entity: "SecuritySettingsForm",
+    View: props => <Settings {...props} />,
+    record: () => ({}),
+    defaultProps: () => ({
+      form: "SecuritySettingsForm",
+      history: jest.fn()
+    }),
+    render: wrapper => {
+      expect(wrapper.find(`input[type="checkbox"]`).at(0).props().checked).toEqual(
+        false
+      );
+
+      expect(wrapper.find(`input[type="checkbox"]`).at(1).props().checked).toEqual(
+        false
+      );
+
+      expect(wrapper.find("#security-password-expiry-period").text()).toContain(
+        30
+      );
+
+      expect(wrapper.find(`input[type="checkbox"]`).at(2).props().checked).toEqual(
+        false
+      );
+
+      expect(wrapper.find("#security-tfa-expiry-period").text()).toContain(
+        16
+      );
+
+      expect(wrapper.find(`input[type="checkbox"]`).at(3).props().checked).toEqual(
+        false
+      );
+
+      expect(wrapper.find("#security-number-login-attempts").text()).toContain(
+        5
+      );
+    }
+  });
+});
