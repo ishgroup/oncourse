6c6b89cf4a33308a5e62945486ff6327b960f598
Chintan Kotadia <chintan@ish.com.au>
2021-05-08 11:57:04 +0530

    OD-16070 Added mock route and method for remove security user roles of preferences

--- client-html/src/dev/mock/api/security/SecurityApiMock.ts
+++ client-html/src/dev/mock/api/security/SecurityApiMock.ts
@@ -1,8 +1,15 @@
 import { promiseResolve } from "../../MockAdapter";
+import { getParamsId } from "../../mockUtils";
 
 export function SecurityApiMock() {
   this.api.onGet("v1/role").reply(config =>
     promiseResolve(config, this.db.getSecurityRoles()));
+
+  this.api.onDelete(new RegExp(`v1/role/\\d+`)).reply(config => {
+    const id = getParamsId(config);
+    this.db.removeSecurityUserRole(id);
+    return promiseResolve(config, this.db.getSecurityRoles());
+  });
 
   this.api.onGet("v1/user").reply(config =>
     promiseResolve(config, this.db.getSecurityUsers()));
