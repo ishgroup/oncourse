47cdf941434ca8d33df93553c9ea3c02e87bd9c1
Chintan Kotadia <chintan@ish.com.au>
2021-05-13 16:22:41 +0530

    OD-16089 Added mock route for create tag

--- client-html/src/dev/mock/api/common/TagApiMock.ts
+++ client-html/src/dev/mock/api/common/TagApiMock.ts
@@ -1,13 +1,16 @@
-import { promiseResolve } from "../../MockAdapter";
 import { Tag } from "@api/model";
+import { promiseResolve } from "../../MockAdapter";
 
 export function TagApiMock(mock) {
   /**
    * List items
-   **/
+   * */
   this.api.onGet("/v1/tag").reply(config => {
     const tags: Tag[] = this.db.getTags();
 
     return promiseResolve(config, tags);
   });
+
+  this.api.onPost("/v1/tag").reply(config =>
+    promiseResolve(config, {}));
 }
