f7001c2556d22f54a558048bbf22e36014d88234
Yury Yasuchenya <yury@ish.com.au>
2021-05-26 15:38:40 +0300

    OD-16127 Added charts data switch

--- client-html/src/js/containers/entities/outcomes/components/OutcomeProgressionChart.tsx
+++ client-html/src/js/containers/entities/outcomes/components/OutcomeProgressionChart.tsx
@@ -6,11 +6,11 @@
  *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
  */
 
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import {
- PieChart, Pie, Legend, Cell
+  PieChart, Pie, Legend, Cell
 } from "recharts";
-import { useTheme, fade } from '@material-ui/core/styles';
+import { useTheme, fade, Tooltip } from '@material-ui/core';
 import { OutcomeProgression } from "@api/model";
 import { normalizeNumber } from "../../../../common/utils/numbers/numbersNormalizing";
 
@@ -23,12 +23,18 @@ export function AttendanceChart({ data }: Props) {
     data = {};
   }
 
+  const [includeFuture, setIncludeFuture] = useState(true);
+
+  const tooltipText = `Click to ${includeFuture ? "exclude" : "include"} future timetable`;
+
   const chartData = useMemo(() => {
     const result = [];
     Object.keys(data).forEach(k => {
       switch (k) {
         case "futureTimetable":
-          return result.push({ name: `future timetable (${data[k]} hrs)`, value: data[k] });
+          return includeFuture
+            ? result.push({ name: `future timetable (${data[k]} hrs)`, value: data[k] })
+            : undefined;
         case "notMarked":
           return result.push({ name: `not marked (${data[k]} hrs)`, value: data[k] });
         case "absent":
@@ -40,43 +46,57 @@ export function AttendanceChart({ data }: Props) {
       }
     });
     return result;
-  }, [data]);
+  }, [data, includeFuture]);
 
   const { palette: { text: { primary }, background: { paper }, getContrastText } } = useTheme();
 
-  const COLORS = [fade(getContrastText(paper), 0.4), fade(getContrastText(paper), 0.6), "red", "green"];
+  const COLORS = [
+    ...includeFuture ? [fade(getContrastText(paper), 0.4)] : [],
+    fade(getContrastText(paper), 0.6),
+    "red",
+    "green"
+  ];
+
+  const attendedIndex = chartData.findIndex(c => c.name.includes("attended"));
 
   return (
-    <PieChart width={200} height={270}>
-      <Pie
-        data={chartData}
-        cx={95}
-        cy={90}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={0}
-        dataKey="value"
-      >
-        {chartData.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <text x={100} y={85} dy={8} textAnchor="middle" fill={primary}>
-        <tspan fontSize="30">
-          {normalizeNumber(chartData.length && chartData[2].value && Math.round(chartData[2].value / (chartData.reduce((p, c) => p + c.value, 0) / 100)))}
-          %
-        </tspan>
-        <tspan x="100" dy="15">attended</tspan>
-      </text>
-      <Legend
-        iconType="circle"
-        iconSize={10}
-        layout="vertical"
-        verticalAlign="bottom"
-        align="center"
-      />
-    </PieChart>
+    <Tooltip title={tooltipText} placement="top">
+      <div onClick={() => setIncludeFuture(prevState => !prevState)}>
+        <PieChart width={200} height={270} style={{ cursor: "pointer" }}>
+          <Pie
+            data={chartData}
+            cx={95}
+            cy={90}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={0}
+            dataKey="value"
+          >
+            {chartData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <text x={100} y={85} dy={8} textAnchor="middle" fill={primary}>
+            <tspan fontSize="30">
+              {normalizeNumber(chartData.length
+                && chartData[attendedIndex].value
+                && Math.round(chartData[attendedIndex].value / (chartData.reduce((p, c) => p + c.value, 0) / 100)))}
+              %
+            </tspan>
+            <tspan x="100" dy="15">attended</tspan>
+          </text>
+          <Legend
+            iconType="circle"
+            iconSize={10}
+            layout="vertical"
+            verticalAlign="bottom"
+            align="center"
+          />
+        </PieChart>
+      </div>
+    </Tooltip>
+
   );
 }
 
@@ -85,12 +105,20 @@ export function AssessmentChart({ data }: Props) {
     data = {};
   }
 
+  const [includeUnreleased, setIncludeUnreleased] = useState(true);
+
+  const tooltipText = `Click to ${includeUnreleased ? "exclude" : "include"} unreleased assessments`;
+
   const chartData = useMemo(() => {
     const result = [];
     Object.keys(data).forEach(k => {
       switch (k) {
+        case "released":
+          return result.push({ name: `released (${data[k]})`, value: data[k] });
         case "notReleased":
-          return result.push({ name: `not released (${data[k]})`, value: data[k] });
+          return includeUnreleased
+            ? result.push({ name: `not released (${data[k]})`, value: data[k] })
+            : undefined;
         case "submitted":
           return result.push({ name: `submitted (${data[k]})`, value: data[k] });
         case "marked":
@@ -100,43 +128,56 @@ export function AssessmentChart({ data }: Props) {
       }
     });
     return result;
-  }, [data]);
+  }, [data, includeUnreleased]);
 
   const { palette: { text: { primary }, background: { paper }, getContrastText } } = useTheme();
 
-  const COLORS = [fade(getContrastText(paper), 0.4), "#0088FE", "#FF8042"];
+  const COLORS = [
+    ...includeUnreleased ? [fade(getContrastText(paper), 0.4)] : [],
+    fade(getContrastText(paper), 0.6),
+    "#0088FE",
+    "#FF8042"
+  ];
+
+  const markedIndex = chartData.findIndex(c => c.name.includes("marked"));
 
   return (
-    <PieChart width={200} height={270}>
-      <Pie
-        data={chartData}
-        cx={95}
-        cy={90}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={0}
-        dataKey="value"
-      >
-        {chartData.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <text x={100} y={85} dy={8} textAnchor="middle" fill={primary}>
-        <tspan fontSize="30">
-          {normalizeNumber(chartData.length && chartData[2].value && Math.round(chartData[2].value / (chartData.reduce((p, c) => p + c.value, 0) / 100)))}
-          %
-        </tspan>
-        <tspan x="100" dy="15">assessments</tspan>
-        <tspan x="100" dy="15">marked</tspan>
-      </text>
-      <Legend
-        iconType="circle"
-        iconSize={10}
-        layout="vertical"
-        verticalAlign="bottom"
-        align="center"
-      />
-    </PieChart>
+    <Tooltip title={tooltipText} placement="top">
+      <div onClick={() => setIncludeUnreleased(prevState => !prevState)}>
+        <PieChart width={200} height={270} style={{ cursor: "pointer" }}>
+          <Pie
+            data={chartData}
+            cx={95}
+            cy={90}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={0}
+            dataKey="value"
+          >
+            {chartData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <text x={100} y={85} dy={8} textAnchor="middle" fill={primary}>
+            <tspan fontSize="30">
+              {normalizeNumber(chartData.length
+                && chartData[markedIndex].value
+                && Math.round(chartData[markedIndex].value / (chartData.reduce((p, c) => p + c.value, 0) / 100)))}
+              %
+            </tspan>
+            <tspan x="100" dy="15">assessments</tspan>
+            <tspan x="100" dy="15">marked</tspan>
+          </text>
+          <Legend
+            iconType="circle"
+            iconSize={10}
+            layout="vertical"
+            verticalAlign="bottom"
+            align="center"
+          />
+        </PieChart>
+      </div>
+    </Tooltip>
   );
 }
