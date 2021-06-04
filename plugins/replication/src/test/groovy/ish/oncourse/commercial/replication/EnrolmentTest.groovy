78445af1345f2f537f9bc97b0b0241453e12c5b8
Artyom Kravchenko <artyom@ish.com.au>
2021-06-04 23:40:54 +1000

    migrate tests to TestWithDatabase + junt5

--- plugins/replication/src/test/groovy/ish/oncourse/commercial/replication/EnrolmentTest.groovy
+++ plugins/replication/src/test/groovy/ish/oncourse/commercial/replication/EnrolmentTest.groovy
@@ -5,13 +5,14 @@
 
 package ish.oncourse.commercial.replication
 
-import ish.CayenneIshTestCase
+import ish.DatabaseSetup
+import ish.TestWithDatabase
 import ish.common.types.EnrolmentStatus
 import ish.common.types.PaymentSource
 import ish.oncourse.commercial.replication.cayenne.QueuedRecord
+import ish.oncourse.commercial.replication.cayenne.QueuedRecordAction
 import ish.oncourse.commercial.replication.cayenne.QueuedTransaction
 import ish.oncourse.server.ICayenneService
-import ish.oncourse.server.PreferenceController
 import ish.oncourse.server.cayenne.CourseClass
 import ish.oncourse.server.cayenne.Enrolment
 import ish.oncourse.server.cayenne.Student
@@ -20,37 +21,25 @@ import org.apache.cayenne.exp.Expression
 import org.apache.cayenne.query.SelectQuery
 import org.apache.commons.lang3.time.DateUtils
 import org.dbunit.dataset.ReplacementDataSet
-import org.dbunit.dataset.xml.FlatXmlDataSet
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 @groovy.transform.CompileStatic
 import static org.junit.Assert.assertEquals
 
-class EnrolmentTest extends CayenneIshTestCase {
+@DatabaseSetup
+class EnrolmentTest extends TestWithDatabase {
 
     private ICayenneService cayenneService
 
-
-    @Before
-    void setup() throws Exception {
-        wipeTables()
-        this.cayenneService = injector.getInstance(ICayenneService.class)
-
-        InputStream st = EnrolmentTest.class.getClassLoader().getResourceAsStream("ish/oncourse/commercial/replication/enrolment-outcomeTest.xml")
-        FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().build(st)
-        ReplacementDataSet rDataSet = new ReplacementDataSet(dataSet)
+    protected void dataSourceReplaceValues(ReplacementDataSet rDataSet) {
         Date start1 = DateUtils.addDays(new Date(), -2)
         Date start2 = DateUtils.addDays(new Date(), -2)
         rDataSet.addReplacementObject("[start_date1]", start1)
         rDataSet.addReplacementObject("[start_date2]", start2)
         rDataSet.addReplacementObject("[end_date1]", DateUtils.addHours(start1, 2))
         rDataSet.addReplacementObject("[end_date2]", DateUtils.addHours(start2, 2))
-
-        executeDatabaseOperation(rDataSet)
-        injector.getInstance(PreferenceController.class).setReplicationEnabled(true)
     }
+
     
     @Test
     void testAuxillaryRecordsAndQueue() throws Exception {
