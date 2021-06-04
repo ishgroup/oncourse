6e6777c9c3d643f596e8cad641cb40f4a82bacce
Rostislav Zenov <rostislav@ish.com.au>
2021-06-05 04:34:00 +1000

    OD-16154 added custom exception class

---
+++ plugins/service-nsw/src/main/groovy/ish/oncourse/commercial/plugin/nsw/ServiceNswException.groovy
@@ -0,0 +1,13 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. https://www.ish.com.au
+ * No copying or use of this code is allowed without permission in writing from ish.
+ */
+
+package ish.oncourse.commercial.plugin.nsw
+
+class ServiceNswException extends RuntimeException {
+    ServiceNswException(String message) {
+        super(message)
+    }
+
+}
