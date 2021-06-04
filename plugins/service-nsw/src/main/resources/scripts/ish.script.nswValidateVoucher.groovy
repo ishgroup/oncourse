7c34dc5e204381ddaa393a0eceaff4f9a3b0d6c9
Rostislav Zenov <rostislav@ish.com.au>
2021-06-05 04:31:35 +1000

    OD-16154 use dynamic call to custom field value

--- plugins/service-nsw/src/main/resources/scripts/ish.script.nswValidateVoucher.groovy
+++ plugins/service-nsw/src/main/resources/scripts/ish.script.nswValidateVoucher.groovy
@@ -1,8 +1,6 @@
-if (record.customFields.find {it.customFieldType.key == "serviceNswVoucher"}?.value != null) {
-    if (record.customFields.find {it.customFieldType.key == "serviceNswRedeemedOn"}?.value == null) {
-        service_nsw {
-            action "validate"
-            voucher record
-        }
+if (record.serviceNswVoucher && !record.serviceNswRedeemedOn) {
+    service_nsw {
+        action "validate"
+        voucher record
     }
 }
