b7261910e5b5820beb7698831aaf57ca5f558b47
Yury Yasuchenya <sammaell@yandex.ru>
2021-06-11 18:12:38 +0300

    OD-16158 Added swagger based login service

--- portal-native/src/js/services/LoginService.ts
+++ portal-native/src/js/services/LoginService.ts
@@ -1,10 +1,9 @@
-import { AuthenticationApi, LoginRequest } from "@api/model";
+import {AuthenticationApi, LoginRequest} from "@api/model";
 import {DefaultHttpService} from "../constants/HttpService";
 
 
-
 class LoginService {
-  private loginApi = new AuthenticationApi(new DefaultHttpService);
+  private loginApi = new AuthenticationApi(new DefaultHttpService());
 
   public login(details: LoginRequest): Promise<any> {
     return this.loginApi.login(details);
