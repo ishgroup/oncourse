dd6a46027eb6edf266e6cef07343ffef5e7e0e62
Rostislav Zenov <rostislav@ish.com.au>
2021-06-11 20:36:18 +1000

    OD-16194 made the script more readable

--- plugins/service-nsw/src/main/resources/scripts/ish.script.nswVoucherRedemption.groovy
+++ plugins/service-nsw/src/main/resources/scripts/ish.script.nswVoucherRedemption.groovy
@@ -1,14 +1,22 @@
 records = query {
-    entity "CustomField"
-    query "entityIdentifier = \"Voucher\" and customFieldType.key is \"serviceNswVoucher\""
+    entity "Voucher"
+    query "serviceNswVoucher not is null and serviceNswRedeemedOn is null"
 }
+def now = new Date()
+records.each { record ->
+    def paymentInLines = record.voucherPaymentsIn*.paymentIn*.paymentInLines.flatten()
+    if (!paymentInLines.empty) {
 
-records.findAll { it.relatedObject.serviceNswRedeemedOn == null}.each { voucherCustomField ->
-    PaymentInLine paymentInLine = voucherCustomField.relatedObject.voucherPaymentsIn*.paymentIn*.paymentInLines.flatten()[0] as PaymentInLine
-    if (paymentInLine?.invoice?.invoiceLines?.collect { it.enrolment.courseClass.startDateTime}?.unique()?.any {it < new Date() }) {
-        service_nsw {
-            action "redeem"
-            voucher record
+        // take the first paymentInLine because the service NSW vouchers always have to be redeemed in one go against just a single invoice.
+        Invoice invoice = paymentInLines.first().invoice as Invoice
+        def classesStartDates = invoice.invoiceLines.collect { it.enrolment.courseClass.startDateTime }.unique()
+
+        if (classesStartDates.any { it < now } ) {
+            service_nsw {
+                action "redeem"
+                voucher record
+                errorsTo emailAddress
+            }
         }
     }
 }
