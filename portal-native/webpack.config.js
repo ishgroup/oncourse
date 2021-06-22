458fc418e92a2a5793e423b06da06e7134e291cb
Yury Yasuchenya <sammaell@yandex.ru>
2021-06-22 13:28:06 +0300

    OD-16210 Adjust Bugsnag config to prevent build errors

--- portal-native/webpack.config.js
+++ portal-native/webpack.config.js
@@ -2,16 +2,20 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const path = require('path');
 
 // Expo CLI will await this method so you can optionally return a promise.
-module.exports = async function(env, argv) {
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@bugsnag/plugin-react-native-unhandled-rejection']
+    }
+  }, argv);
 
-  const config = await createExpoWebpackConfigAsync(env, argv);
-
-  if (config.mode === "production") {
+  if (config.mode === 'production') {
     const buildPath = path.resolve(__dirname, 'build', 'web');
     config.output.path = buildPath;
     config.plugins[1].patterns[0].to = buildPath;
     config.plugins[1].patterns[1].to = buildPath;
-    config.plugins[2].options.filename = buildPath + "/index.html";
+    config.plugins[2].options.filename = `${buildPath}/index.html`;
   }
 
   return config;
