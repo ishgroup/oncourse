39cd5f7807130b3483633545ab1baee654a4c7c1
Vadim Haponov <vadim@ish.com.au>
2021-06-23 10:56:08 +0300

    OD-16229 Assessment submission - add assessor for bulk edit

--- client-html/src/js/containers/entities/assessmentSubmissions/AssessmentSubmission.tsx
+++ client-html/src/js/containers/entities/assessmentSubmissions/AssessmentSubmission.tsx
@@ -5,7 +5,7 @@
  *
  *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
  */
-import React, { Dispatch, useEffect } from "react";
+import React, { Dispatch, useCallback, useEffect } from "react";
 import { connect } from "react-redux";
 import { AssessmentSubmission as AssessmentSubmissionModel } from "@api/model";
 import ListView from "../../../common/components/list-view/ListView";
@@ -16,6 +16,10 @@ import AssessmentSubmissionEditView from "./components/AssessmentSubmissionsEdit
 import { getAssessmentSubmissionsItem, removeAssessmentSubmissionsItem, updateAssessmentSubmissionsItem } from "./actions";
 import { notesAsyncValidate } from "../../../common/components/form/notes/utils";
 import BulkEditCogwheelOption from "../common/components/BulkEditCogwheelOption";
+import { State } from "../../../reducers/state";
+import EntityService from "../../../common/services/EntityService";
+import { getContactName } from "../contacts/utils";
+import instantFetchErrorHandler from "../../../common/api/fetch-errors-handlers/InstantFetchErrorHandler";
 
 const filterGroups: FilterGroup[] = [
   {
@@ -46,8 +50,9 @@ const nameCondition = (val: AssessmentSubmissionModel) => val.studentName;
 
 const AssessmentSubmission = (props: any) => {
   const {
-    clearListState, getAssessmentSubmissionsItem, getFilters, getTags, onDelete, onSave
+    clearListState, getAssessmentSubmissionsItem, getFilters, getTags, onDelete, onSave, selection, dispatch
   } = props;
+
   useEffect(() => {
     getFilters();
     getTags();
@@ -55,6 +60,50 @@ const AssessmentSubmission = (props: any) => {
       clearListState();
     };
   }, []);
+
+  const getCustomBulkEditFields = useCallback(async () => {
+    if (!selection || !selection.length) return [];
+
+    let result = [];
+
+    await EntityService.getPlainRecords(
+      "Contact",
+      "firstName,lastName",
+      `tutor.assessmentClassTutors.assessmentClass.assessmentSubmissions.id ${selection.length > 1 ? "in" : "is"} ${selection}`
+    )
+      .then(res => {
+        const tutors = (res.rows.map(r => ({
+          label: getContactName({ firstName: r.values[0], lastName: r.values[1] }),
+          value: Number(r.id),
+        })));
+
+        result = [
+          {
+            keyCode: "submittedOn",
+            label: "Submitted On",
+            name: "Submitted On",
+            type: "Date"
+          },
+          {
+            keyCode: "markedOn",
+            label: "Marked On",
+            name: "Marked On",
+            type: "Date"
+          },
+          {
+            keyCode: "markedById",
+            label: "Assessor",
+            name: "Assessor",
+            type: "Select",
+            items: tutors,
+            defaultValue: "Not set",
+          }
+        ];
+      })
+      .catch(err => instantFetchErrorHandler(dispatch, err));
+
+    return result;
+  }, [selection]);
 
   return (
     <ListView
@@ -75,11 +124,17 @@ const AssessmentSubmission = (props: any) => {
       onSave={onSave}
       onDelete={onDelete}
       CogwheelAdornment={BulkEditCogwheelOption}
+      getCustomBulkEditFields={getCustomBulkEditFields}
     />
   );
 };
 
+const mapStateToProps = (state: State) => ({
+  selection: state.list.selection,
+});
+
 const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  dispatch,
   getAssessmentSubmissionsItem: (id: number) => dispatch(getAssessmentSubmissionsItem(id)),
   clearListState: () => dispatch(clearListState()),
   getFilters: () => dispatch(getFilters("AssessmentSubmission")),
@@ -89,4 +144,4 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
   onDelete: (id: number) => dispatch(removeAssessmentSubmissionsItem(id))
 });
 
-export default connect<any, any, any>(null, mapDispatchToProps)(AssessmentSubmission);
+export default connect<any, any, any>(mapStateToProps, mapDispatchToProps)(AssessmentSubmission);
