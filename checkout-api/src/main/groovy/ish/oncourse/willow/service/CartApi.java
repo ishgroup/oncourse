37cbf44917e97e9cfeb2cabd00e08a6f31f3595d
Rostislav Zenov <rostislav@ish.com.au>
2021-06-29 23:21:16 +1000

    OD-15924 annotated cartApi with collegeInfo: it calls requestFilter which provide to college info

--- checkout-api/src/main/groovy/ish/oncourse/willow/service/CartApi.java
+++ checkout-api/src/main/groovy/ish/oncourse/willow/service/CartApi.java
@@ -8,18 +8,21 @@ public interface CartApi  {
     @POST
     @Path("/v1/cart/{id}")
     @Produces({ "application/json" })
+    @CollegeInfo
     void create(@PathParam("id") String id, String checkout);
 
     @DELETE
     @Path("/v1/cart/{id}")
     @Consumes({ "application/json" })
     @Produces({ "application/json" })
+    @CollegeInfo
     void delete(@PathParam("id") String id);
 
     @GET
     @Path("/v1/cart/{id}")
     @Consumes({ "application/json" })
     @Produces({ "application/json" })
+    @CollegeInfo
     String get(@PathParam("id") String id);
 }
 
