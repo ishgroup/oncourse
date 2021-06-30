97a35b8ca80c1d0774d6a336bd2efd498a47f38d
Yury Yasuchenya <sammaell@yandex.ru>
2021-06-30 13:41:16 +0300

    OD-15501 Added global setBaseURL method

--- billing/src/js/api/services/DefaultHttpClient.ts
+++ billing/src/js/api/services/DefaultHttpClient.ts
@@ -4,4 +4,8 @@ import * as Constants from "../constants";
 const instance = axios.create();
 instance.defaults.baseURL = Constants.CONTEXT;
 
+(window as any).setBaseURL = (url) => {
+  instance.defaults.baseURL = url;
+}
+
 export const defaultAxios = instance;
