5b90cd877b10f33b2e53d5915b8bfd318bb017a0
Artyom Kravchenko <artyom@ish.com.au>
2021-07-06 01:03:47 +1000

    OD-16261 clean up groovy cache complitelly. We anly use GroovyClassLoader to load Script or EmailTemplate. All our groovy  sources we load with regular Java 'app' class loader. We never reuse Script or template meta class cache so we can safety clean it up

---
+++ server/src/main/java/ish/oncourse/server/scripting/api/MetaclassCleaner.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright ish group pty ltd 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.scripting.api
+
+import groovy.text.SimpleTemplateEngine
+import groovy.text.Template
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.codehaus.groovy.runtime.InvokerHelper
+
+import java.lang.reflect.Field
+
+@CompileStatic
+class MetaclassCleaner {
+    private static final Logger logger = LogManager.logger
+
+    @CompileDynamic
+    static void clearGroovyCashe(final Template template) {
+        try {
+            Script script = template.@script as Script
+            ClassLoader classLoader = script.metaClass.theClass.classLoader
+            if (classLoader?.parent  instanceof GroovyClassLoader) {
+                (classLoader.parent as GroovyClassLoader).clearCache()
+            }
+        } catch(Exception e) {
+            logger.catching(e)
+        }
+        
+    }
+
+
+}
