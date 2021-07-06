e2983ec02631991b0189f821272daad121b23c3c
Rostislav Zenov <rostislav@ish.com.au>
2021-07-07 00:37:20 +1000

    OD-16270 added a basic LeadDao class for use it in EntityApiService

---
+++ server/src/main/groovy/ish/oncourse/server/api/dao/LeadDao.groovy
@@ -0,0 +1,26 @@
+/*
+ * Copyright ish group pty ltd 2021.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.api.dao
+
+import ish.oncourse.server.cayenne.Lead
+import org.apache.cayenne.ObjectContext
+import org.apache.cayenne.query.SelectById
+
+class LeadDao implements CayenneLayer<Lead> {
+    @Override
+    Lead newObject(ObjectContext context) {
+        context.newObject(Lead.class)
+    }
+
+    @Override
+    Lead getById(ObjectContext context, Long id) {
+        SelectById.query(Lead.class, id)
+                .selectOne(context)
+    }
+}
