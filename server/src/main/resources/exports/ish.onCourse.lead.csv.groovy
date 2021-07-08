a8a3fbf9c0ac3e3064d018bf9ac51751df714416
Rostislav Zenov <rostislav@ish.com.au>
2021-07-09 01:50:27 +1000

    OD-16272 added a csv Lead export template

---
+++ server/src/main/resources/exports/ish.onCourse.lead.csv.groovy
@@ -0,0 +1,29 @@
+import org.apache.commons.lang3.StringUtils
+
+records.each { Lead lead ->
+    csv << [
+            "created"        : lead.createdOn?.format("yyyy-MM-dd'T'HH:mm:ssXXX"),
+            "Student count"  : lead.studentCount,
+            "Notes"          : lead.notes,
+            "Student notes"  : lead.studentNotes,
+            "Estimated value": lead.estimatedValue,
+            "Next action on" : lead.nextActionOn?.format("yyyy-MM-dd'T'HH:mm:ssXXX"),
+            "Notification"   : lead.notify,
+            "Active"         : lead.status,
+            "Title"          : lead.customer.title,
+            "First name"     : lead.customer.firstName,
+            "Last name"      : lead.customer.lastName,
+            "Email"          : lead.customer.email,
+            "Street"         : lead.customer.street,
+            "Suburb"         : lead.customer.suburb,
+            "State"          : lead.customer.state,
+            "Post code"      : lead.customer.postcode,
+            "Mobile phone"   : lead.customer.mobilePhone,
+            "Home phone"     : lead.customer.homePhone,
+            "Work phone"     : lead.customer.workPhone,
+            "Birth date"     : lead.customer.birthDate?.format("yyyy-MM-dd"),
+            "Courses"        : StringUtils.join(lead.courses.collect {it.name}, ','),
+            "Products"       : StringUtils.join(lead.products.collect {it.name}, ','),
+            "Assigned to"    : lead.assignedTo?.email
+    ]
+}
