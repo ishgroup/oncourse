37afcee98b12028b1070c4575bf95e0cbe969f56
Vadim Haponov <vadim@ish.com.au>
2021-07-09 10:28:03 +0300

    OD-16231 Add new fields

--- client-html/src/js/containers/entities/leads/components/LeadGeneral.tsx
+++ client-html/src/js/containers/entities/leads/components/LeadGeneral.tsx
@@ -8,6 +8,10 @@
 
 import * as React from "react";
 import { connect } from "react-redux";
+import clsx from "clsx";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { change } from "redux-form";
 import FormField from "../../../../common/components/form/form-fields/FormField";
 import { State } from "../../../../reducers/state";
 import { validateTagsList } from "../../../../common/components/form/simpleTagListComponent/validateTagsList";
@@ -17,88 +21,101 @@ import CourseItemRenderer from "../../courses/components/CourseItemRenderer";
 import { courseFilterCondition, openCourseLink } from "../../courses/utils";
 import { LinkAdornment } from "../../../../common/components/form/FieldAdornments";
 import { contactLabelCondition, defaultContactName, openContactLink } from "../../contacts/utils";
-import clsx from "clsx";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-class LeadGeneral extends React.PureComponent<any, any> {
-  validateTagList = (value, allValues, props) => {
-    const { tags } = this.props;
+const items = [{ label: "Open", value: true }, { label: "Close", value: false }];
+
+const LeadGeneral = (props: any) => {
+  const validateTagList = (value, allValues) => {
+    const { tags } = props;
     return validateTagsList(tags, value, allValues, props);
   };
 
-  render() {
-    const {
-      values,
-      tags,
-      dispatch,
-      form
-    } = this.props;
+  const changeValue = (e, checked) => {
+    const { form, dispatch } = props;
 
-    return (
-      <div className="generalRoot">
-        <div className="pt-2">
-          <FormField
-            type="remoteDataSearchSelect"
-            entity="Contact"
-            aqlFilter="isStudent is true"
-            name="contactId"
-            label="Student"
-            selectValueMark="id"
-            selectLabelCondition={contactLabelCondition}
-            defaultDisplayValue={values && defaultContactName(values.studentName)}
-            labelAdornment={
-              <LinkAdornment linkHandler={openContactLink} link={values.contactId} disabled={!values.contactId} />
-            }
-            itemRenderer={ContactSelectItemRenderer}
-            rowHeight={55}
-            required
-          />
-        </div>
-        <div>
-          <FormField
-            type="tags"
-            name="tags"
-            tags={tags}
-            validate={tags && tags.length ? this.validateTagList : undefined}
-          />
-        </div>
-        <FormField type="number" name="studentCount" label="Number of students" />
-        <FormField type="dateTime" name="nextActionOn" label="Next action on" />
-        <FormField type="checkbox" name="notify" label="Notify student" />
-        {/*<FormControlLabel*/}
-        {/*  className={clsx("pr-3", {*/}
-        {/*    // "mt-2": !twoColumn*/}
-        {/*  })}*/}
-        {/*  control={<Checkbox onChange={this.reconcileAllPayments} checked={this.isAllPaymentsReconciled()} />}*/}
-        {/*  label="Notify student"*/}
-        {/*/>*/}
+    dispatch(change(form, "notify", checked));
+  };
+
+  const {
+    values,
+    tags,
+    dispatch,
+    form
+  } = props;
+
+  return (
+    <div className="generalRoot">
+      <div className="pt-2">
         <FormField
           type="remoteDataSearchSelect"
-          entity="Course"
-          // aqlFilter="allowLeads is true"
-          name="courseId"
-          label="Course"
+          entity="Contact"
+          aqlFilter="isStudent is true"
+          name="contactId"
+          label="Student"
           selectValueMark="id"
-          selectLabelCondition={v => v.name}
-          selectFilterCondition={courseFilterCondition}
-          defaultDisplayValue={values && values.courseName}
-          labelAdornment={<LinkAdornment link={values.courseId} linkHandler={openCourseLink} />}
-          itemRenderer={CourseItemRenderer}
+          selectLabelCondition={contactLabelCondition}
+          defaultDisplayValue={values && defaultContactName(values.studentName)}
+          labelAdornment={
+            <LinkAdornment linkHandler={openContactLink} link={values.contactId} disabled={!values.contactId} />
+          }
+          itemRenderer={ContactSelectItemRenderer}
           rowHeight={55}
           required
         />
-        <CustomFields
-          entityName="Lead"
-          fieldName="customFields"
-          entityValues={values}
-          dispatch={dispatch}
-          form={form}
+      </div>
+      <div>
+        <FormField
+          type="tags"
+          name="tags"
+          tags={tags}
+          validate={tags && tags.length ? validateTagList : undefined}
         />
       </div>
-    );
-  }
-}
+      <FormField type="number" name="studentCount" label="Number of students" />
+      <FormField type="dateTime" name="nextActionOn" label="Next action on" />
+      <FormControlLabel
+        className={clsx("pr-3", {
+          // "mt-2": !twoColumn
+        })}
+        control={<Checkbox onChange={changeValue} checked={values.notify} />}
+        label="Notify student"
+      />
+      <FormField
+        type="money"
+        name="estimatedValue"
+        label="Estimated value"
+      />
+      <FormField
+        type="select"
+        name="active"
+        label="Status"
+        items={items}
+      />
+      <FormField
+        type="remoteDataSearchSelect"
+        entity="Course"
+        // aqlFilter="allowLeads is true"
+        name="courseId"
+        label="Course"
+        selectValueMark="id"
+        selectLabelCondition={v => v.name}
+        selectFilterCondition={courseFilterCondition}
+        defaultDisplayValue={values && values.courseName}
+        labelAdornment={<LinkAdornment link={values.courseId} linkHandler={openCourseLink} />}
+        itemRenderer={CourseItemRenderer}
+        rowHeight={55}
+        required
+      />
+      <CustomFields
+        entityName="Lead"
+        fieldName="customFields"
+        entityValues={values}
+        dispatch={dispatch}
+        form={form}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state: State) => ({
   tags: state.tags.entityTags["Lead"],
