2365d5a23536d6991b0f7bf207e5cb18243d3268
Rostislav Zenov <rostislav@ish.com.au>
2021-07-28 18:17:46 +1000

    OD-16308 divided current cayenne listeners for invoice and invoiceLine to abstract and not abstract: common fields for invoice and quote entries should be validated in abstract listeners

--- server/src/main/java/ish/oncourse/server/lifecycle/InvoiceLineLifecycleListener.java
+++ server/src/main/java/ish/oncourse/server/lifecycle/InvoiceLineLifecycleListener.java
@@ -12,11 +12,7 @@ package ish.oncourse.server.lifecycle;
 
 import ish.oncourse.server.accounting.AccountTransactionService;
 import ish.oncourse.server.accounting.builder.InvoiceLineTransactionsBuilder;
-import ish.oncourse.server.cayenne.Account;
 import ish.oncourse.server.cayenne.InvoiceLine;
-import ish.util.AccountUtil;
-import org.apache.cayenne.ObjectContext;
-import org.apache.cayenne.annotation.PostAdd;
 import org.apache.cayenne.annotation.PostPersist;
 import org.apache.cayenne.annotation.PrePersist;
 
@@ -38,27 +34,10 @@ public class InvoiceLineLifecycleListener {
 		if (invoiceLine.getAccount() == null) {
 			invoiceLineInitHelper.processInvoiceLine(invoiceLine);
 		}
-		if (invoiceLine.getInvoiceLineDiscounts() != null && invoiceLine.getInvoiceLineDiscounts().size() > 0) {
-		    if (invoiceLine.getInvoiceLineDiscounts().get(0).getDiscount().getCosAccount() != null) {
-                invoiceLine.setCosAccount(invoiceLine.getInvoiceLineDiscounts().get(0).getDiscount().getCosAccount());
-            }
-		}
 	}
 
 	@PostPersist(value = InvoiceLine.class)
 	public void postPersist(InvoiceLine invoiceLine) {
 		accountTransactionService.createTransactions(InvoiceLineTransactionsBuilder.valueOf(invoiceLine));
-	}
-
-	@PostAdd(value = InvoiceLine.class)
-	public void postAdd(InvoiceLine invLine) {
-		var aContext = invLine.getContext();
-
-		if (invLine.getTax() == null) {
-			//get Default Tax from Default Income account. We get this account as default
-			//see InvoiceLineInitHelper.assignDefaults()
-			var defaultIncomeAccount = AccountUtil.getDefaultStudentEnrolmentsAccount(aContext, Account.class);
-			invLine.setTax(defaultIncomeAccount.getTax());
-		}
 	}
 }
