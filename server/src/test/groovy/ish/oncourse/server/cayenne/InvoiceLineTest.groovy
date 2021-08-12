58f4082bac392e205679e8427dcf5140a42cc2ea
Artyom Kravchenko <artyom@ish.com.au>
2021-08-13 00:26:29 +1000

    OD-16313 Fix compillation

--- server/src/test/groovy/ish/oncourse/server/cayenne/InvoiceLineTest.groovy
+++ server/src/test/groovy/ish/oncourse/server/cayenne/InvoiceLineTest.groovy
@@ -369,7 +369,7 @@ class InvoiceLineTest extends TestWithDatabase {
         enrolment.setStudent(student)
         enrolment.setCourseClass(courseClass)
         enrolment.setSource(PaymentSource.SOURCE_ONCOURSE)
-        invoiceLine.enrolment(enrolment)
+        invoiceLine.enrolment=enrolment
 
         invoice.updateAmountOwing()
 
