8be8e6aa7614fa9b58300867bf6359ed05fbea5d
Yury Yasuchenya <sammaell@yandex.ru>
2021-08-13 18:24:37 +0300

    OD-16371 Added mock requests delay

--- portal-native/src/dev/MockAdapter.ts
+++ portal-native/src/dev/MockAdapter.ts
@@ -2,16 +2,18 @@
  * Service for mock backend functionality, used in dev mode
  * Library: axios-mock-adapter - https://github.com/ctimmerm/axios-mock-adapter
  * */
+
 import MockAdapter from 'axios-mock-adapter';
 import { defaultAxios } from '../js/constants/DefaultHttpClient';
 import { promiseReject } from './utils';
 import { MockAdapterType } from './types';
 import LoginApiMock from './api/LoginApiMock';
+import { IS_JEST } from '../js/constants/Environment';
 import SessionApiMock from './api/SessionApiMock';
 import CourseClassApiMock from './api/CourseClassApiMock';
 
 export class MockApi implements MockAdapterType {
-  public api = new MockAdapter(defaultAxios);
+  public api = new MockAdapter(defaultAxios, { delayResponse: IS_JEST ? 0 : 1000 });
 
   constructor() {
     LoginApiMock.apply(this);
