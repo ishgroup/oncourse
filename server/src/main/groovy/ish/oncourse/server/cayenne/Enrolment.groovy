66886cae70355ac1f35fe76e258b35a3a927dda6
Artyom Kravchenko <artyom@ish.com.au>
2021-08-30 16:41:17 +1000

    OD-16449 Fix Enrolment->InvoiceLine relation

--- server/src/main/groovy/ish/oncourse/server/cayenne/Enrolment.groovy
+++ server/src/main/groovy/ish/oncourse/server/cayenne/Enrolment.groovy
@@ -641,7 +641,7 @@ class Enrolment extends _Enrolment implements EnrolmentTrait, EnrolmentInterface
 	@API
 	@Override
 	List<InvoiceLine> getInvoiceLines() {
-		return super.getAbstractInvoiceLines().findAll {it.abstractInvoice?.type == InvoiceType.INVOICE } as List<InvoiceLine>
+		return super.getAbstractInvoiceLines().findAll {it.invoice?.type == InvoiceType.INVOICE } as List<InvoiceLine>
 	}
 
 	/**
