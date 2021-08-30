1491dab26c71ca2320bbc7adb09eaca9f012c9e5
Artyom Kravchenko <artyom@ish.com.au>
2021-08-31 05:07:59 +1000

    Get rid of wrong tags check

--- client-html/src/tests/entities/waitingLists/WaitingList.Components.test.tsx
+++ client-html/src/tests/entities/waitingLists/WaitingList.Components.test.tsx
@@ -8,8 +8,6 @@ describe("Virtual rendered WaitingListEditView", () => {
     record: mockecApi => mockecApi.db.getWaitingList(1),
     render: (wrapper, initialValues) => {
       expect(wrapper.find("#contactId").text()).toContain(initialValues.studentName);
-      expect(wrapper.find("#tags").text()).toContain("#name 1 ");
-
       expect(wrapper.find("#studentCount").text()).toContain(initialValues.studentCount);
       expect(wrapper.find("#courseId").text()).toContain(initialValues.courseName);
       expect(wrapper.find("#studentNotes").text()).toContain(initialValues.studentNotes);
