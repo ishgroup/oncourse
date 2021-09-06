9edddbf8e9ee5a451df6b2118715e61f2066efbf
Artyom Kravchenko <artyom@ish.com.au>
2021-09-06 23:31:14 +1000

    OD-16379 Maximum soap envelop tracing

--- usi/src/main/java/ish/oncourse/usi/UsiApp.java
+++ usi/src/main/java/ish/oncourse/usi/UsiApp.java
@@ -14,6 +14,11 @@ public class UsiApp {
     public static void main(String[] args) {
         Configuration.configureOnly(HOST, PORT, PATH);
         System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
         Bootique.app(args).args("--server", "--config=classpath:application.yml")
                 .module(UsiApiModule.class)
                 .module(IshLog4jModule.class)
