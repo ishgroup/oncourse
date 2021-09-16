6b8aa5cdbcaafe6e4a6317f0bc40412db404d15e
Yury Yasuchenya <sammaell@yandex.ru>
2021-09-16 12:14:55 +0300

    OD-16513 Changed waiting list add confirmation message

--- checkout/src/js/enrol/containers/result/components/SuccessfulWaitingCourses.tsx
+++ checkout/src/js/enrol/containers/result/components/SuccessfulWaitingCourses.tsx
@@ -1,29 +1,7 @@
-import * as React from "react";
+import * as React from 'react';
 
-interface Props {
-  successLink?: string;
-  contacts?: any;
-}
-
-export class SuccessfulWaitingCourses extends React.Component<Props, any> {
-  render() {
-    const {successLink, contacts} = this.props;
-
-    return (
-      <div>
-        <h2>Thank you for your request.</h2>
-
-        {contacts && contacts.map((c, index) => (
-          <div key={index}>
-            The student <b>{c.name}</b> was successfully added to waiting list for course <br/>
-            {c.courses.map((w, index) => (
-              <p key={index}>{w.name}</p>
-            ))}
-          </div>
-        ))}
-
-        <p><a className="link-continue" href={successLink}>Close</a></p>
-      </div>
-    );
-  }
-}
+export const SuccessfulWaitingCourses = () => (
+  <div>
+    <h2>Your request was successful</h2>
+  </div>
+);
