7a187bbab5aa63f645faae7df962f6ad66e07c12
Artyom Kravchenko <artyom@ish.com.au>
2021-09-19 18:19:13 +1000

    OW-3695 create assessment submition bases on completed task from Xapiapps

--- customers/acapma/script/acapma.script.xapiapps.history.groovy
+++ customers/acapma/script/acapma.script.xapiapps.history.groovy
@@ -2,13 +2,17 @@
  * Copyright ish group pty ltd. All rights reserved. https://www.ish.com.au
  * No copying or use of this code is allowed without permission in writing from ish.
  */
-import groovy.transform.CompileDynamic
 import groovyx.net.http.Method
 import groovyx.net.http.RESTClient
 import groovyx.net.http.HttpResponseException
 import groovyx.net.http.ContentType
+import ish.common.types.EnrolmentStatus
+import ish.oncourse.server.cayenne.AssessmentClass
+import ish.oncourse.server.cayenne.AssessmentSubmission
 import ish.oncourse.server.cayenne.Contact
+import ish.oncourse.server.cayenne.Enrolment
 import ish.oncourse.server.cayenne.Student
+import ish.statistics.EnrolmentStats
 import org.apache.cayenne.query.ObjectSelect
 
 Xapiapps xapiapps = new Xapiapps(url, publicToken, secretToken, groupStoreKey)
@@ -18,16 +22,15 @@ def changes = xapiapps.history()
 if (changes && !changes.empty ) {
     changes.each { result ->
         def task = result.document
-        String studentUUID = task.roles?.target?.key // 2e37ee8f-7056-4270-89a0-bc9d3e826ebf
-        String pathUUID = task.path?.key // 2c9d9c0b-536c-4125-8c14-db5fc9f4d431*path
+        String studentUUID = task.roles?.target?.key 
+        String pathUUID = task.path?.key
         if (studentUUID || pathUUID) {
-            def user = xapiapps.getUser(studentUUID)
+            def user = xapiapps.getUser(studentUUID) 
             def path = xapiapps.path(pathUUID)
             String courseName = path.name
             String email = user.email
             String assessmentName = task.config?.title // Weclome Module Placeholder
             createAssessment(email, courseName, assessmentName)
-
         }
     }
 }
@@ -40,20 +43,44 @@ def error(String errorMessage) {
     }
 }
 
-def createAssessment(String email, String courseName, String assessment) {
+def createAssessment(String email, String courseName, String assessmentName) {
     List<Student> students = ObjectSelect.query(Student)
             .where(Student.CONTACT.dot(Contact.EMAIL).eq(email))
             .select(context)
     if (students.empty) {
-        error("can not find student with email: $email")
-        return
+        return error("can not find student with email: $email")
+
     }
     if (students.size() > 1) {
-        error("there are more than one student withemail: $email")
-        return
+        return error("there are more than one student withemail: $email")
+
     }
     Student student = students[0]
-    //todo
+    Enrolment enrolment = student.enrolments
+            .find { e -> e.status == EnrolmentStatus.SUCCESS && e.courseClass.course.name.equalsIgnoreCase(courseName)}
+    if (!enrolment) {
+        return error("student: $email has no active enrolment for $courseName")
+
+    }
+
+    AssessmentClass assessmentClass = enrolment.courseClass.assessmentClasses.find { it.assessment.name == assessmentName }
+    if (!assessmentClass) {
+        return error("Class $enrolment.courseClass.course.name ($enrolment.courseClass.uniqueCode) has not $assessmentName assessment task added, student: $email")
+    }
+
+    AssessmentSubmission submission = enrolment.assessmentSubmissions.find {it.assessmentClass == assessmentClass }
+    Date lastDay = new Date().clearTime() -1
+    if (!submission) {
+        submission = context.newObject(AssessmentSubmission)
+        submission.assessmentClass = assessmentClass
+        submission.enrolment = enrolment
+        submission.createdOn = lastDay
+        submission.modifiedOn = lastDay
+    }
+    submission.submittedOn = lastDay
+    submission.markedOn = lastDay
+    submission.grade = new BigDecimal(100)
+    context.commitChanges()
 }
 
 
@@ -113,9 +140,6 @@ class Xapiapps {
     }
     
     def path(String uuid) {
-        // note that we need to fetch the history in small
-        // batch sizes
-    
         def httpClient = new RESTClient(url + "api2/r/search")
         httpClient.request(Method.POST, ContentType.JSON) {
             body = [
