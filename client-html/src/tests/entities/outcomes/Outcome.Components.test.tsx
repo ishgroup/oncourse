287e1b75825ff4bd4b0c8f490f6e4618df300f29
Artyom Kravchenko <artyom@ish.com.au>
2021-09-24 23:15:14 +1000

    OD-16536 Adjust jests in accodance with DOM structure changes

--- client-html/src/tests/entities/outcomes/Outcome.Components.test.tsx
+++ client-html/src/tests/entities/outcomes/Outcome.Components.test.tsx
@@ -7,23 +7,13 @@ describe("Virtual rendered OutcomeEditView", () => {
     EditView: OutcomeEditView,
     record: mockecApi => mockecApi.db.getOutcome(1),
     render: (wrapper, initialValues) => {
-      expect(wrapper.find(".textField").at(0).text()).toContain(initialValues.studentName);
       expect(wrapper.find("#moduleCode input").val()).toContain(initialValues.moduleCode);
       expect(wrapper.find("#moduleName input").val()).toContain(initialValues.moduleName);
-
-      expect(wrapper.find("#trainingPlanStartDate input").val()).toContain("No value");
-      expect(wrapper.find("#trainingPlanEndDate input").val()).toContain("No value");
-      expect(wrapper.find("#actualStartDate input").val()).toContain("No value");
-      expect(wrapper.find("#actualEndDate input").val()).toContain("No value");
-
+      
       expect(wrapper.find("#deliveryMode input").val()).toContain(initialValues.deliveryMode);
       expect(wrapper.find("#reportableHours input").val()).toContain(initialValues.reportableHours);
       expect(wrapper.find("#fundingSource input").val()).toContain(initialValues.fundingSource);
-      expect(wrapper.find("#status input").val()).toContain("No value");
       expect(wrapper.find("#hoursAttended input").val()).toContain(initialValues.hoursAttended);
-      expect(wrapper.find("#vetFundingSourceStateID input").val()).toContain("No value");
-      expect(wrapper.find("#vetPurchasingContractID input").val()).toContain("No value");
-      expect(wrapper.find("#vetPurchasingContractScheduleID input").val()).toContain("No value");
       expect(wrapper.find("#specificProgramIdentifier input").val()).toContain(initialValues.specificProgramIdentifier);
     }
   });
--- client-html/src/tests/preferences/data-collection-forms/DataCollectionForm.Components.test.tsx
+++ client-html/src/tests/preferences/data-collection-forms/DataCollectionForm.Components.test.tsx
@@ -36,7 +36,7 @@ describe("Virtual rendered DataCollectionForm", () => {
       const form = initialValues[0];
       const items = parseDataCollectionFormData(form);
 
-      expect(wrapper.find("h6[id='form.type'] input").val()).toContain(form.type === "Survey" ? "Student Feedback" : form.type);
+      expect(wrapper.find("h6[id='form.type']").text()).toContain(form.type === "Survey" ? "Student Feedback" : form.type);
 
       if (form.type === "Survey") {
         expect(wrapper.find("div[id='form.deliverySchedule'] input").val()).toContain(form.deliverySchedule);
@@ -45,13 +45,12 @@ describe("Virtual rendered DataCollectionForm", () => {
       items.forEach((field, key) => {
         const warpperId = `#data-collection-form-${key}`;
         if (field.baseType === "field") {
-          expect(wrapper.find(`${warpperId} div[id="items[${key}].label"]`).text()).toContain(field.label);
+          expect(wrapper.find(`${warpperId} div[id="items[${key}].label"] input`).val()).toContain(field.label);
           expect(wrapper.find(`${warpperId} div[id="items[${key}].helpText"]`).text()).toContain(field.helpText);
           expect(wrapper.find(`${warpperId} h6[id="items[${key}].type"]`).text()).toContain(field.type.label);
-          expect(wrapper.find(`${warpperId} input[type="checkbox"]`).at(0).props().checked).toEqual(field.mandatory);
         } else {
-          expect(wrapper.find(`${warpperId} div[id="items[${key}].name"]`).text()).toContain(field.name);
-          expect(wrapper.find(`${warpperId} div[id="items[${key}].description"]`).text()).toContain(field.description);
+          expect(wrapper.find(`${warpperId} div[id="items[${key}].name"]  input`).val()).toContain(field.name);
+          expect(wrapper.find(`${warpperId} div[id="items[${key}].description"] textarea`).text()).toContain(field.description);
         }
       });
     }
