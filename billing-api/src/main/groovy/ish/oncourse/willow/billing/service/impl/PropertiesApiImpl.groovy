9ed226e8789c5922ab4778e2fb2ab14eb46e035a
dmitrij <dmitriy.tarasienko@gmail.com>
2021-10-04 13:56:21 +0300

    feat: request for client_id and api_key application properties

---
+++ billing-api/src/main/groovy/ish/oncourse/willow/billing/service/impl/PropertiesApiImpl.groovy
@@ -0,0 +1,25 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. https://www.ish.com.au
+ * No copying or use of this code is allowed without permission in writing from ish.
+ */
+
+package ish.oncourse.willow.billing.service.impl
+
+import groovy.transform.CompileStatic
+import ish.oncourse.configuration.Configuration
+import ish.oncourse.willow.billing.v1.model.PropertiesDTO
+import ish.oncourse.willow.billing.v1.service.PropertiesApi
+
+import static ish.oncourse.configuration.Configuration.AdminProperty.API_KEY
+import static ish.oncourse.configuration.Configuration.AdminProperty.CLIENT_ID
+
+@CompileStatic
+class PropertiesApiImpl implements PropertiesApi {
+
+    @Override
+    PropertiesDTO getProperties() {
+        def clientId = Configuration.getValue(CLIENT_ID)
+        def apiKey = Configuration.getValue(API_KEY)
+        return new PropertiesDTO(clientId, apiKey)
+    }
+}
