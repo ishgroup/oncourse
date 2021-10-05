0c5636701bc53b8f772d6e0a0d2d5be75242ce95
dmitrij <dmitriy.tarasienko@gmail.com>
2021-10-05 14:33:25 +0300

    feat: move myob requests functional to plugins and configure client-side form for myob properties

--- client-html/src/js/containers/automation/containers/integrations/components/forms/MYOBForm.tsx
+++ client-html/src/js/containers/automation/containers/integrations/components/forms/MYOBForm.tsx
@@ -4,35 +4,107 @@
  */
 
 import * as React from "react";
-import { initialize, reduxForm } from "redux-form";
+import { change, getFormValues, initialize, reduxForm } from "redux-form";
+import { connect } from "react-redux";
 import Typography from "@material-ui/core/Typography";
 import Button from "../../../../../../common/components/buttons/Button";
 import FormField from "../../../../../../common/components/form/formFields/FormField";
 import CustomAppBar from "../../../../../../common/components/layout/CustomAppBar";
 import RouteChangeConfirm from "../../../../../../common/components/dialog/confirm/RouteChangeConfirm";
 import { onSubmitFail } from "../../../../../../common/utils/highlightFormClassErrors";
+import { validateSingleMandatoryField } from "../../../../../../common/utils/validation";
+import { State } from "../../../../../../reducers/state";
 
 class MYOBBaseForm extends React.Component<any, any> {
   constructor(props) {
     super(props);
 
+    this.state = {
+      hideConfig: false,
+      loading: false
+    };
     // Initializing form with values
     props.dispatch(initialize("MYOBForm", props.item));
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.item.id !== this.props.item.id) {
-      // Reinitializing form with values
-      this.props.dispatch(initialize("MYOBForm", this.props.item));
+  componentDidMount() {
+    const {
+      match: { params: { name } }, item, dispatch
+    } = this.props;
+
+    if (name && !item.name) {
+      dispatch(change("MYOBForm", "name", name));
     }
+  }
+
+  componentDidUpdate(prevProps) {
+    const {
+      item, dispatch, location: { search }, history, dirty
+    } = this.props;
+    const { hideConfig } = this.state;
+
+    if (prevProps.item.id !== item.id) {
+      // Reinitializing form with values
+      dispatch(initialize("MYOBForm", item));
+      this.setState({
+        hideConfig: false
+      });
+    }
+
+    if (prevProps.dirty === true && dirty === false) {
+      this.setState({
+        hideConfig: false
+      });
+    }
+
+    if (search && !hideConfig) {
+      const params = new URLSearchParams(search);
+      const code = params.get("code");
+
+      if (code) {
+        this.setState({
+          hideConfig: true
+        });
+        dispatch(change("MYOBForm", "verificationCode", code));
+        params.delete('code');
+
+        history.replace({
+          pathname: history.location.pathname,
+          search: decodeURIComponent(params.toString())
+        });
+      }
+    }
+  }
+
+
+  showTokenField = () => {
+    const { values } = this.props;
+
+    this.setState({
+      loading: true
+    });
+
+    window.open(
+      // eslint-disable-next-line max-len
+      `https://secure.myob.com/oauth2/account/authorize?client_id=07545d14-9a95-4398-b907-75af3b3841ae&redirect_uri=https://secure-payment.oncourse.net.au/services/s/integrations/myob/auth.html&response_type=code&scope=CompanyFile&state=${window.location.href}${values.id ? "" : `/${values.name}`}`,
+      "_self"
+    );
+  }
+
+  onDisconnect = () => {
+    const { dispatch } = this.props;
+    this.setState({
+      hideConfig: true
+    });
+    dispatch(change("MYOBForm", "fields.active", "false"));
   }
 
   render() {
     const {
-     handleSubmit, onSubmit, AppBarContent, dirty, item, form
+     handleSubmit, onSubmit, AppBarContent, values = {}, dirty, item, form
     } = this.props;
     const configured = item && item.id;
-
+    const { hideConfig, loading } = this.state;
     const configuredLabel = "MYOB access has been set up. Press ‘Configure’ to restart the configuration process.";
     const unconfiguredLabel = ' Press "Configure" to proceed with authorising onCourse to access your MYOB account.';
 
@@ -42,23 +114,70 @@ class MYOBBaseForm extends React.Component<any, any> {
         <CustomAppBar>
           <AppBarContent />
         </CustomAppBar>
-        <FormField name="fields.baseUrl" label="Base URL" type="text" fullWidth />
-        <FormField name="fields.user" label="User" type="text" fullWidth />
-        <FormField name="fields.password" type="password" label="Password" fullWidth />
+        <FormField name="fields.myobBaseUrl" label="Base URL" type="text" fullWidth />
+        <FormField name="fields.myobFileName" label="File name" type="text" fullWidth />
+        <FormField name="fields.myobUser" label="File owner" type="text" fullWidth />
+        <FormField name="fields.myobPassword" type="password" label="File owner password" fullWidth />
 
         <Typography variant="caption" component="div">
           {configured ? configuredLabel : unconfiguredLabel}
         </Typography>
+        {values?.fields?.active === "true" ? (
+          <>
+            <Typography variant="caption" component="div">
+              {`You are connected to Myob`}
+            </Typography>
+            <Button
+              text="Disconnect from Myob"
+              variant="contained"
+              className="mt-1"
+              onClick={this.onDisconnect}
+            />
+          </>
+        ) : (
+          <>
+            {
+              !hideConfig && (
+                <FormField
+                  type="stub"
+                  name="verificationCode"
+                  validate={validateSingleMandatoryField}
+                />
+              )
+            }
 
-        <a href="myobAuthAccess" className="link">
-          <Button text="Configure" variant="contained" className="mt-1" />
-        </a>
+            <Typography variant="caption" component="div">
+              {hideConfig
+                ? 'Myob access has been set up. Press "Save" to complete configuration process.'
+                : 'Press to proceed with authorising onCourse to access your Myob account.'}
+            </Typography>
+
+            {!hideConfig && (
+              <Button
+                text="Connect to Myob"
+                variant="contained"
+                className="mt-1"
+                disabled={!values.name}
+                loading={loading}
+                onClick={this.showTokenField}
+              />
+            )}
+          </>
+        )}
       </form>
     );
   }
 }
 
+const mapStateToProps = (state: State) => ({
+  values: getFormValues("MYOBForm")(state)
+});
+
 export const MYOBForm = reduxForm({
   form: "MYOBForm",
   onSubmitFail
-})(MYOBBaseForm);
+})(
+  connect<any, any, any>(
+    mapStateToProps
+  )(MYOBBaseForm)
+);
