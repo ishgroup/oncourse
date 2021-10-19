2b481baaea86dcdd7dcac3314abddeec92bf06f9
Artyom Kravchenko <artyom@ish.com.au>
2021-10-19 21:23:12 +1100

    OD-16596 new replication version spec

---
+++ common/webservices-client/src/main/java/ish/oncourse/webservices/v24/stubs/replication/GradingTypeStub.java
@@ -0,0 +1,153 @@
+
+package ish.oncourse.webservices.v24.stubs.replication;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+/**
+ * <p>Java class for gradingTypeStub complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="gradingTypeStub"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;extension base="{http://repl.v24.soap.webservices.oncourse.ish/}replicationStub"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="minValue" type="{http://www.w3.org/2001/XMLSchema}decimal"/&gt;
+ *         &lt;element name="maxValue" type="{http://www.w3.org/2001/XMLSchema}decimal"/&gt;
+ *         &lt;element name="entryType" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/extension&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "gradingTypeStub", propOrder = {
+    "name",
+    "minValue",
+    "maxValue",
+    "entryType"
+})
+public class GradingTypeStub
+    extends ReplicationStub
+{
+
+    @XmlElement(required = true)
+    protected String name;
+    @XmlElement(required = true)
+    protected BigDecimal minValue;
+    @XmlElement(required = true)
+    protected BigDecimal maxValue;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(Adapter4 .class)
+    @XmlSchemaType(name = "int")
+    protected Integer entryType;
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the minValue property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getMinValue() {
+        return minValue;
+    }
+
+    /**
+     * Sets the value of the minValue property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setMinValue(BigDecimal value) {
+        this.minValue = value;
+    }
+
+    /**
+     * Gets the value of the maxValue property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getMaxValue() {
+        return maxValue;
+    }
+
+    /**
+     * Sets the value of the maxValue property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setMaxValue(BigDecimal value) {
+        this.maxValue = value;
+    }
+
+    /**
+     * Gets the value of the entryType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public Integer getEntryType() {
+        return entryType;
+    }
+
+    /**
+     * Sets the value of the entryType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEntryType(Integer value) {
+        this.entryType = value;
+    }
+
+}
