354924f78a0cf85875138dfa18660ebbdbfbf990
Chintan Kotadia <chintankotadia13@gmail.com>
2021-11-12 11:53:47 +0530

    OD-16674 Implemented sticky header app bar for custom fields form of Preferences

--- client-html/src/js/containers/preferences/styles/formCommonStyles.ts
+++ client-html/src/js/containers/preferences/styles/formCommonStyles.ts
@@ -22,7 +22,21 @@ export const formCommonStyles = theme => createStyles({
   },
   field: {
     paddingRight: "20px"
-  }
+  },
+  fullScreenTitleItem: {
+    paddingLeft: `${theme.spacing(12)} !important`,
+    marginTop: theme.spacing(1),
+  },
+  scriptAddMenu: {
+    position: "absolute",
+    zIndex: theme.zIndex.drawer + 2,
+    left: 26,
+    top: 43,
+    "& > .appBarFab": {
+      top: 0,
+      left: 0,
+    }
+  },
 });
 
 export const cardsFormStyles = theme => createStyles({
