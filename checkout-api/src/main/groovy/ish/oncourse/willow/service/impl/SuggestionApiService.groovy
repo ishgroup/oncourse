5c642819762b9ddc2c190f8d145631689fddc44d
Kristina Trukhan <keyris.xd.snow@gmail.com>
2021-11-30 12:51:04 +0300

    OD-16766 Add SuggestionApiService

---
+++ checkout-api/src/main/groovy/ish/oncourse/willow/service/impl/SuggestionApiService.groovy
@@ -0,0 +1,78 @@
+/*
+ * Copyright ish group pty ltd. All rights reserved. https://www.ish.com.au
+ * No copying or use of this code is allowed without permission in writing from ish.
+ */
+
+package ish.oncourse.willow.service.impl
+
+import com.google.inject.Inject
+import ish.oncourse.api.cayenne.CayenneService
+import ish.oncourse.model.College
+import ish.oncourse.model.Course
+import ish.oncourse.model.CourseClass
+import ish.oncourse.willow.EntityRelationService
+import ish.oncourse.willow.checkout.functions.GetCourse
+import ish.oncourse.willow.checkout.functions.GetProduct
+import ish.oncourse.willow.model.v2.suggestion.SuggestionResonse
+import ish.oncourse.willow.service.SuggestionApi
+import org.apache.cayenne.ObjectContext
+
+import java.time.LocalDateTime
+
+class SuggestionApiService implements SuggestionApi {
+
+    @Inject
+    private CayenneService cayenneService
+
+    @Inject
+    private CollegeService collegeService
+
+    @Inject
+    private EntityRelationService relationService
+
+
+    @Override
+    SuggestionResonse getSuggestion(String courseIds, String productIds) {
+
+        ObjectContext context = cayenneService.newContext()
+        College college = collegeService.college
+
+        List<String> courseClasses = new ArrayList<>()
+        List<String> products = new ArrayList<>()
+
+        Arrays.stream(courseIds.split(',')).each { id ->
+            def cayenneCourse = new GetCourse(context, college, (id as String).trim()).get()
+
+            products.addAll(relationService.getSuggestedProducts(cayenneCourse).collect {
+                it.id.toString()
+            })
+            courseClasses.addAll(relationService.getSuggestedCourses(cayenneCourse).
+                    collect { getNearestCourseClass(it) }.
+                    findAll { it != null }.
+                    collect { it.id.toString() }
+            )
+        }
+
+        Arrays.stream(productIds.split(',')).each { id ->
+            def cayenneProduct = new GetProduct(context, college, (id as String).trim()).get()
+
+            products.addAll(relationService.getSuggestedProducts(cayenneProduct).collect { it.id.toString() })
+            courseClasses.addAll(relationService.getSuggestedCourses(cayenneProduct).
+                    collect { getNearestCourseClass(it) }.
+                    findAll { it != null }.
+                    collect { it.id.toString() }
+            )
+        }
+
+        return new SuggestionResonse().courseClasses(courseClasses.asList()).products(products.asList())
+    }
+
+    private CourseClass getNearestCourseClass(Course course) {
+        def courseClass = course.availableClasses
+                .findAll { it.startDateTime != null && it.startDateTime.toLocalDateTime().isAfter(LocalDateTime.now()) }
+                .findAll { it != null }
+                .sort { it.startDateTime }
+        return courseClass.empty ? null : courseClass.get(0)
+    }
+
+}
