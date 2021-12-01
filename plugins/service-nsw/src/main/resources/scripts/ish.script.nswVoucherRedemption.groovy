1554f2429583467f0351619b90852e0324a9ab1b
James Swinbanks <64567930+swinbanks@users.noreply.github.com>
2021-12-01 12:02:49 +1100

    Add code to redeem all vouchers a certain number of days before the end of the year. OD-16783

--- plugins/service-nsw/src/main/resources/scripts/ish.script.nswVoucherRedemption.groovy
+++ plugins/service-nsw/src/main/resources/scripts/ish.script.nswVoucherRedemption.groovy
@@ -3,6 +3,15 @@ records = query {
     query "serviceNswVoucher not is null and serviceNswRedeemedOn is null"
 }
 def now = new Date()
+
+// Service NSW require all vouchers to be redeemed before the end of the year, even if
+// the class runs next year. So let's give ourselves a few days grace to check all is OK
+def endOfyear = now.copyWith(
+        month: Calendar.DECEMBER,
+        dayOfMonth: 31
+)
+def isEndOfYear = daysBeforeYearEnd && (endOfyear - now < daysBeforeYearEnd)
+
 records.each { record ->
     def paymentInLines = record.voucherPaymentsIn*.paymentIn*.paymentInLines.flatten()
     if (!paymentInLines.empty) {
@@ -10,8 +19,7 @@ records.each { record ->
         // take the first paymentInLine because the service NSW vouchers always have to be redeemed in one go against just a single invoice.
         Invoice invoice = paymentInLines.first().invoice as Invoice
         def classesStartDates = invoice.invoiceLines.collect { it.enrolment.courseClass.startDateTime }.unique()
-
-        if (classesStartDates.any { it < now } ) {
+        if (classesStartDates.any { it < now || isEndOfYear } ) {
             service_nsw {
                 action "redeem"
                 voucher record
