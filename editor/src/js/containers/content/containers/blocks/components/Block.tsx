ab6d3d0f867937ba4f7e46fc751a38c2981b493e
Yury Yasuchenya <sammaell@yandex.ru>
2021-12-07 13:38:51 +0300

    OD-16802 Fixed editor block content formatting

--- editor/src/js/containers/content/containers/blocks/components/Block.tsx
+++ editor/src/js/containers/content/containers/blocks/components/Block.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import clsx from "clsx";
-import marked from "marked";
-import {withStyles} from "@material-ui/core/styles";
-import Editor from "../../../../../common/components/editor/HtmlEditor";
-import {BlockState} from "../reducers/State";
-import {addContentMarker} from "../../../utils";
-import MarkdownEditor from "../../../../../common/components/editor/MarkdownEditor";
-import {ContentMode} from "../../../../../model";
-import ContentModeSwitch from "../../../../../common/components/ContentModeSwitch";
-import CustomButton from "../../../../../common/components/CustomButton";
+import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
+import marked from 'marked';
+import { withStyles } from '@material-ui/core/styles';
+import Editor from '../../../../../common/components/editor/HtmlEditor';
+import { BlockState } from '../reducers/State';
+import { addContentMarker } from '../../../utils';
+import MarkdownEditor from '../../../../../common/components/editor/MarkdownEditor';
+import { ContentMode } from '../../../../../model';
+import ContentModeSwitch from '../../../../../common/components/ContentModeSwitch';
+import CustomButton from '../../../../../common/components/CustomButton';
 
-const styles = theme => ({
+const styles = (theme) => ({
   cancelButton: {
     marginRight: theme.spacing(2),
   },
@@ -24,54 +24,33 @@ interface Props {
 }
 
 // custom event to reinitialize site plugins on editing content
-const pluginInitEvent = new Event("plugins:init");
+const pluginInitEvent = new Event('plugins:init');
 
-const Block: React.FC<Props> = ({block, classes, onSave, setContentMode}) => {
-  const [editMode, setEditMode] = useState(true);
-  const [draftContent, setDraftContent] = useState("");
+const Block: React.FC<Props> = ({
+  block, classes, onSave, setContentMode
+}) => {
+  const [draftContent, setDraftContent] = useState('');
 
   useEffect(() => {
     document.dispatchEvent(pluginInitEvent);
-
-    setDraftContent(block.content || "");
-    if (block.contentMode === "md") {
-      setDraftContent(marked(block.content || ""));
-    }
+    setDraftContent(block.contentMode === 'html' ? marked(block.content || '') : (block.content || ''));
   }, [block]);
 
-  const onClickArea = e => {
-    e.preventDefault();
-    setEditMode(true);
-    setDraftContent(block.content || "");
-  };
-
-  const onChangeArea = val => {
+  const onChangeArea = (val) => {
     setDraftContent(val);
   };
 
   const handleSave = () => {
-    // setEditMode(false);
     onSave(block.id, addContentMarker(draftContent, block.contentMode));
   };
 
   const handleCancel = () => {
-    // setEditMode(false);
-    setDraftContent(block.content || "");
+    setDraftContent(block.content || '');
   };
-
-  // useEffect(() => {
-  //   if (!editMode && block.content) {
-  //     document.dispatchEvent(pluginInitEvent);
-  //   }
-  // }, [editMode, block, block && block.content]);
-
-  // const onContentModeChange = e => {
-  //   setContentMode(block.id, e.target.value);
-  // };
 
   const renderEditor = () => {
     switch (block.contentMode) {
-      case "md": {
+      case 'md': {
         return (
           <MarkdownEditor
             height={window.innerHeight - 30 - 48 - 45 - 51}
@@ -80,8 +59,8 @@ const Block: React.FC<Props> = ({block, classes, onSave, setContentMode}) => {
           />
         );
       }
-      case "textile":
-      case "html":
+      case 'textile':
+      case 'html':
       default: {
         return (
           <Editor
@@ -96,42 +75,32 @@ const Block: React.FC<Props> = ({block, classes, onSave, setContentMode}) => {
 
   return (
     <div>
-      {editMode && <>
-        <div className={
-          clsx("editor-wrapper", (block.contentMode === "html" || block.contentMode === "textile") && "ace-wrapper",
-          )
-        }>
-          <ContentModeSwitch
-            contentModeId={block.contentMode}
-            moduleId={block.id}
-            setContentMode={setContentMode}
-          />
-          {renderEditor()}
-        </div>
-        <div className="mt-3">
-          <CustomButton
-            styleType="cancel"
-            styles={classes.cancelButton}
-            onClick={handleCancel}
-          >
-            Cancel
-          </CustomButton>
-          <CustomButton
-            styleType="submit"
-            onClick={handleSave}
-          >
-            Save
-          </CustomButton>
-        </div>
-      </>}
-
-      {/*{!editMode &&*/}
-      {/*  <div onClick={onClickArea}>*/}
-      {/*    <div className={clsx("editor-area", !block.content && 'editor-area--empty')}>*/}
-      {/*      {block.content}*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*}*/}
+      <div className={
+        clsx('editor-wrapper', (block.contentMode === 'html' || block.contentMode === 'textile') && 'ace-wrapper')
+      }
+      >
+        <ContentModeSwitch
+          contentModeId={block.contentMode}
+          moduleId={block.id}
+          setContentMode={setContentMode}
+        />
+        {renderEditor()}
+      </div>
+      <div className="mt-3">
+        <CustomButton
+          styleType="cancel"
+          styles={classes.cancelButton}
+          onClick={handleCancel}
+        >
+          Cancel
+        </CustomButton>
+        <CustomButton
+          styleType="submit"
+          onClick={handleSave}
+        >
+          Save
+        </CustomButton>
+      </div>
     </div>
   );
 };
