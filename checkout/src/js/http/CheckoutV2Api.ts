4934c5afa4d585771a82eff60eb559ec4cf5c8b5
Yury Yasuchenya <sammaell@yandex.ru>
2021-12-09 11:15:25 +0300

    Fixed make payment params

--- checkout/src/js/http/CheckoutV2Api.ts
+++ checkout/src/js/http/CheckoutV2Api.ts
@@ -1,21 +1,26 @@
-import {HttpService} from "../common/services/HttpService";
-import {ContactNode} from "../model/checkout/ContactNode";
-import {ContactNodeRequest} from "../model/checkout/request/ContactNodeRequest";
-import {CommonError} from "../model/common/CommonError";
-import {PaymentRequest} from "../model/v2/checkout/payment/PaymentRequest";
-import {PaymentResponse} from "../model/v2/checkout/payment/PaymentResponse";
+import { HttpService } from '../common/services/HttpService';
+import { ContactNode } from '../model/checkout/ContactNode';
+import { ContactNodeRequest } from '../model/checkout/request/ContactNodeRequest';
+import { PaymentRequest } from '../model/v2/checkout/payment/PaymentRequest';
+import { PaymentResponse } from '../model/v2/checkout/payment/PaymentResponse';
 
 export class CheckoutV2Api {
   constructor(private http: HttpService) {
   }
 
   getContactNodeV2(contactNodeRequest: ContactNodeRequest): Promise<ContactNode> {
-    return this.http.POST(`/v2/getContactNode`, contactNodeRequest);
+    return this.http.POST('/v2/getContactNode', contactNodeRequest);
   }
+
   getStatus(sessionId: string, payerId: string): Promise<PaymentResponse> {
-    return this.http.GET(`/v2/getPaymentStatus/${sessionId}`);
+    return this.http.GET(`/v2/getPaymentStatus/${sessionId}`, { headers: { payerId } });
   }
-  makePaymentV2(paymentRequest: PaymentRequest, xValidateOnly: boolean, payerId: string, xOrigin: string): Promise<PaymentResponse> {
-    return this.http.POST(`/v2/makePayment`, paymentRequest);
+
+  makePaymentV2(paymentRequest: PaymentRequest, xValidateOnly: boolean, payerId: string): Promise<PaymentResponse> {
+    return this.http.POST(
+      '/v2/makePayment',
+      paymentRequest,
+      { headers: { xValidateOnly, payerId, xOrigin: window.location.href } },
+    );
   }
 }
