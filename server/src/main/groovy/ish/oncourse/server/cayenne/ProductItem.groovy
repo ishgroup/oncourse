fec9663e41757a3a754a2945aa167232dd6b9e1b
dmitrij <dmitriy.tarasienko@gmail.com>
2021-12-13 11:16:35 +0300

    OD-16719 feat: request for abstract taggable entity

--- server/src/main/groovy/ish/oncourse/server/cayenne/ProductItem.groovy
+++ server/src/main/groovy/ish/oncourse/server/cayenne/ProductItem.groovy
@@ -31,7 +31,7 @@ import static ish.persistence.CommonExpressionFactory.previousMidnight
  */
 @API
 @QueueableEntity
-class ProductItem extends _ProductItem implements Queueable, NotableTrait {
+class ProductItem extends _ProductItem implements Queueable, NotableTrait, AbstractTaggable {
 
 	private static final Logger logger = LogManager.getLogger()
 
@@ -177,6 +177,11 @@ class ProductItem extends _ProductItem implements Queueable, NotableTrait {
 	 */
 	@Override
 	Class<? extends TagRelation> getTagRelationClass() {
+		return null
+	}
+
+	@Override
+	List<? extends TagRelation> getTaggingRelations() {
 		return null
 	}
 }
