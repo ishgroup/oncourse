bb9a24e3fbbd1e5ed4ad0b6e045d1072a8407ddd
Yury Yasuchenya <sammaell@yandex.ru>
2021-12-24 17:25:11 +0300

    OD-16730 Adjusted current page selection logic

--- editor/src/js/containers/content/containers/pages/containers/PagesSidebar.tsx
+++ editor/src/js/containers/content/containers/pages/containers/PagesSidebar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import {connect} from "react-redux";
-import {Dispatch} from "redux";
-import clsx from "clsx";
-import {Page, Theme} from "../../../../../model";
-import PageSettings from "../components/PageSettings";
-import {URL} from "../../../../../routes";
-import {addPage, deletePage, savePage} from "../actions";
-import SidebarList from "../../../../../components/Sidebar/SidebarList";
-import {showModal} from "../../../../../common/containers/modal/actions";
-import {State} from "../../../../../reducers/state";
-import PageService from "../../../../../services/PageService";
-import { hideNavigation, setActiveUrl, showNavigation } from "../../../../../common/containers/Navigation/actions";
-import {SHOW_MESSAGE} from "../../../../../common/components/message/actions";
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import clsx from 'clsx';
+import { Page, Theme } from '../../../../../model';
+import PageSettings from '../components/PageSettings';
+import { URL } from '../../../../../routes';
+import { addPage, deletePage, savePage } from '../actions';
+import SidebarList from '../../../../../components/Sidebar/SidebarList';
+import { showModal } from '../../../../../common/containers/modal/actions';
+import { State } from '../../../../../reducers/state';
+import PageService from '../../../../../services/PageService';
+import { hideNavigation, setActiveUrl, showNavigation } from '../../../../../common/containers/Navigation/actions';
+import { SHOW_MESSAGE } from '../../../../../common/components/message/actions';
 import { getLayouts, getThemes } from '../../../../design/containers/themes/actions';
 import { getBlocks } from '../../blocks/actions';
 
 interface Props {
   pages: Page[];
+  currentPage: Page;
   themes: Theme[];
   match: any;
   onEditSettings: (id, settings) => any;
@@ -39,7 +40,7 @@ export class PagesSidebar extends React.Component<Props, any> {
   }
 
   goBack() {
-    this.props.history.push(URL.CONTENT, {updateActiveUrl: true});
+    this.props.history.push(URL.CONTENT, { updateActiveUrl: true });
   }
 
   resetActivePage() {
@@ -47,22 +48,24 @@ export class PagesSidebar extends React.Component<Props, any> {
   }
 
   onAddPage() {
-    const {onAddPage} = this.props;
+    const { onAddPage } = this.props;
     onAddPage();
   }
 
   getDefaultLink(items, page) {
-    const defaultUrl = items.find(item => item.isDefault);
+    const defaultUrl = items.find((item) => item.isDefault);
     return defaultUrl ? defaultUrl.link : PageService.generateBasetUrl(page).link;
   }
 
   render() {
-    const {pages, match, onEditSettings, onDeletePage, showModal, history, showNavigation, fetching, showError, themes} = this.props;
-    const activePage = match.params.id && pages.find(page => page.id == match.params.id);
+    const {
+      pages, match, currentPage, onEditSettings, onDeletePage, showModal, history, showNavigation, fetching, showError, themes
+    } = this.props;
 
     return (
-      <div className={clsx((fetching && "fetching"))}>
-        {!activePage &&
+      <div className={clsx((fetching && 'fetching'))}>
+        {!match.params.id
+          && (
           <SidebarList
             items={pages}
             idKey="id"
@@ -73,48 +76,48 @@ export class PagesSidebar extends React.Component<Props, any> {
             onAdd={() => this.onAddPage()}
             showNavigation={showNavigation}
           />
-        }
+          )}
 
-        {activePage &&
+        {match.params.id && currentPage
+          && (
           <PageSettings
-            page={activePage}
+            page={currentPage}
             pages={pages}
             themes={themes}
             onBack={() => this.resetActivePage()}
-            onEdit={prop => onEditSettings(activePage.id, prop)}
-            onDelete={pageId => onDeletePage(pageId)}
+            onEdit={(prop) => onEditSettings(currentPage.id, prop)}
+            onDelete={(pageId) => onDeletePage(pageId)}
             showModal={showModal}
             showError={showError}
             history={history}
             showNavigation={showNavigation}
           />
-        }
+          )}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state: State) => ({
+  currentPage: state.page.currentPage,
   pages: state.page.items,
   themes: state.theme.items,
   fetching: state.fetching,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<any>) => {
-  return {
-    getThemes: () => dispatch(getThemes()),
-    onEditSettings: (id, settings) => dispatch(savePage(id, settings)),
-    onDeletePage: pageId => dispatch(deletePage(pageId)),
-    showError: title => dispatch(
-      {
-        type: SHOW_MESSAGE,
-        payload: {message: title, success: false},
-      }
-    ),
-    onAddPage: () => dispatch(addPage()),
-    showModal: props => dispatch(showModal(props)),
-    showNavigation: () => dispatch(showNavigation()),
-  };
-};
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  getThemes: () => dispatch(getThemes()),
+  onEditSettings: (id, settings) => dispatch(savePage(id, settings)),
+  onDeletePage: (pageId) => dispatch(deletePage(pageId)),
+  showError: (title) => dispatch(
+    {
+      type: SHOW_MESSAGE,
+      payload: { message: title, success: false },
+    }
+  ),
+  onAddPage: () => dispatch(addPage()),
+  showModal: (props) => dispatch(showModal(props)),
+  showNavigation: () => dispatch(showNavigation()),
+});
 
 export default connect<any, any, any>(mapStateToProps, mapDispatchToProps)(PagesSidebar as any);
