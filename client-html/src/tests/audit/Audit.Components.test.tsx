7a2d8247f6310254fef231090a0a99381cd9c7b6
Chintan Kotadia <chintankotadia13@gmail.com>
2021-12-25 18:57:11 +0530

    OD-16686 Updated jest tests library and fixed Audit components test.

--- client-html/src/tests/audit/Audit.Components.test.tsx
+++ client-html/src/tests/audit/Audit.Components.test.tsx
@@ -3,19 +3,17 @@ import AuditsEditView from "../../js/containers/audits/components/AuditsEditView
 import { III_DD_MMM_YYYY_HH_MM } from "../../js/common/utils/dates/format";
 import { mockedEditView } from "../common/MockedEditView.Components";
 
-// TODO Enable test on fix
-
-describe.skip("Virtual rendered AuditsEditView", () => {
+describe("Virtual rendered AuditsEditView", () => {
   mockedEditView({
     entity: "Audit",
     EditView: AuditsEditView,
     record: mockedApi => mockedApi.db.getAudit(1),
-    render: (wrapper, initialValues) => {
-      expect(wrapper.find("#created input").val()).toContain(format(new Date(initialValues.created), III_DD_MMM_YYYY_HH_MM));
-      expect(wrapper.find("#entityIdentifier input").val()).toContain(initialValues.entityIdentifier);
-      expect(wrapper.find("#entityId input").val()).toContain(initialValues.entityId);
-      expect(wrapper.find("#action input").val()).toContain(initialValues.action);
-      expect(wrapper.find("#message textarea").val()).toContain(initialValues.message);
+    render: ({ screen, initialValues }) => {
+      expect(screen.getByLabelText('Date and time').value).toBe(format(new Date(initialValues.created), III_DD_MMM_YYYY_HH_MM));
+      expect(screen.getByLabelText("Entity name").value).toBe(initialValues.entityIdentifier);
+      expect(screen.getByLabelText("Entity ID").value).toBe(initialValues.entityId);
+      expect(screen.getByLabelText("Action").value).toBe(initialValues.action);
+      expect(screen.getByLabelText("Message").value).toBe(initialValues.message);
     }
   });
 });
