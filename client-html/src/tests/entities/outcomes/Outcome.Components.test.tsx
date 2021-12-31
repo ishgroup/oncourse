88b0047d1fde4f0f091e33c1ff8596128b3b519b
Chintan Kotadia <chintankotadia13@gmail.com>
2021-12-31 19:15:00 +0530

    OD-16686 Jest tests: Fixed Outcome edit view component and updated outcome mock data.

--- client-html/src/tests/entities/outcomes/Outcome.Components.test.tsx
+++ client-html/src/tests/entities/outcomes/Outcome.Components.test.tsx
@@ -6,15 +6,20 @@ describe("Virtual rendered OutcomeEditView", () => {
     entity: "Outcome",
     EditView: OutcomeEditView,
     record: mockecApi => mockecApi.db.getOutcome(1),
-    render: (wrapper, initialValues) => {
-      expect(wrapper.find("#moduleCode input").val()).toContain(initialValues.moduleCode);
-      expect(wrapper.find("#moduleName input").val()).toContain(initialValues.moduleName);
-      
-      expect(wrapper.find("#deliveryMode input").val()).toContain(initialValues.deliveryMode);
-      expect(wrapper.find("#reportableHours input").val()).toContain(initialValues.reportableHours);
-      expect(wrapper.find("#fundingSource input").val()).toContain(initialValues.fundingSource);
-      expect(wrapper.find("#hoursAttended input").val()).toContain(initialValues.hoursAttended);
-      expect(wrapper.find("#specificProgramIdentifier input").val()).toContain(initialValues.specificProgramIdentifier);
+    render: ({ screen, initialValues, formRoleName }) => {
+      expect(screen.getByRole(formRoleName)).toHaveFormValues({
+        moduleCode: initialValues.moduleCode,
+        moduleName: initialValues.moduleName,
+        deliveryMode: initialValues.deliveryMode,
+        reportableHours: initialValues.reportableHours,
+        fundingSource: initialValues.fundingSource,
+        status: initialValues.status,
+        hoursAttended: initialValues.hoursAttended,
+        vetFundingSourceStateID: initialValues.vetFundingSourceStateID,
+        vetPurchasingContractID: initialValues.vetPurchasingContractID,
+        vetPurchasingContractScheduleID: initialValues.vetPurchasingContractScheduleID,
+        specificProgramIdentifier: initialValues.specificProgramIdentifier,
+      });
     }
   });
 });
