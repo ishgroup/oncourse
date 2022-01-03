fbe75103e2acf8a35fdc7140c6f8f42d0d8e3db6
Chintan Kotadia <chintankotadia13@gmail.com>
2022-01-03 13:18:36 +0530

    OD-16686 Jest tests: Fixed ProductItem (Sales) edit view component.

--- client-html/src/tests/entities/sales/ProductItem.Components.test.tsx
+++ client-html/src/tests/entities/sales/ProductItem.Components.test.tsx
@@ -8,12 +8,11 @@ describe("Virtual rendered SalesEditView", () => {
     entity: "ProductItem",
     EditView: SalesEditView,
     record: mockecApi => mockecApi.db.getProductItem(1),
-    render: (wrapper, initialValues) => {
-      const inputs = wrapper.find("input");
-      expect(inputs[0].attribs.value).toContain(initialValues.purchasedByName);
-      expect(inputs[1].attribs.value).toContain(format(new Date(initialValues.purchasedOn), EEE_D_MMM_YYYY).toString());
-      expect(inputs[2].attribs.value).toContain(initialValues.purchasePrice);
-      expect(inputs[3].attribs.value).toContain(initialValues.status);
+    render: ({ screen, initialValues }) => {
+      expect(screen.getByLabelText('Purchased by', { selector: 'input' }).value).toBe(initialValues.purchasedByName);
+      expect(screen.getByLabelText('Purchased on').value).toBe(format(new Date(initialValues.purchasedOn), EEE_D_MMM_YYYY).toString());
+      expect(screen.getByLabelText('Purchase price').value).toBe(initialValues.purchasePrice.toFixed(2));
+      expect(screen.getByLabelText('Status').value).toBe(initialValues.status);
     }
   });
 });
