9c4fb2a11df7ec1d99f8dff486757d3ff123f05e
Chintan Kotadia <chintankotadia13@gmail.com>
2022-03-14 17:14:52 +0530

    OD-16971 Jest test: fixed state initialization issue.

--- client-html/src/tests/entities/courseClasses/EpicCreateCourseClass.test.ts
+++ client-html/src/tests/entities/courseClasses/EpicCreateCourseClass.test.ts
@@ -5,11 +5,15 @@ import { EpicCreateCourseClass } from "../../../js/containers/entities/courseCla
 import {
   GET_RECORDS_REQUEST, setListCreatingNew, setListFullScreenEditView, setListSelection
 } from "../../../js/common/components/list-view/actions";
+import { LIST_EDIT_VIEW_FORM_NAME } from "../../../js/common/components/list-view/constants";
 
 describe("Create course class epic tests", () => {
   it("EpicCreateCourseClass should returns correct values", () => DefaultEpic({
     action: mockedApi => createCourseClass(mockedApi.db.getCourseClass(1)),
     epic: EpicCreateCourseClass,
+    store: {
+      form: { [LIST_EDIT_VIEW_FORM_NAME]: { values: { tutors: [] } } }
+    },
     processData: mockedApi => [
       {
         type: GET_RECORDS_REQUEST,
