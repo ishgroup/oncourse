af9a4e78b36db3152d312c297a9512e02ee4e31b
Chintan Kotadia <chintankotadia13@gmail.com>
2022-03-14 17:14:52 +0530

    OD-16971 Jest test: fixed state initialization issue.

--- client-html/src/tests/common/Default.Epic.ts
+++ client-html/src/tests/common/Default.Epic.ts
@@ -1,23 +1,29 @@
-import { ActionsObservable, Epic } from "redux-observable";
+import { ActionsObservable, Epic, StateObservable } from "redux-observable";
 import { filter, toArray } from "rxjs/operators";
-import { mockedAPI, store } from "../TestEntry";
+import { Subject } from "rxjs";
+import { mockedAPI } from "../TestEntry";
 import { FETCH_FINISH, FETCH_START } from "../../js/common/actions";
+import TestStore from "../../js/constants/Store";
 
 interface Props {
   action: any;
   epic: Epic<any, any>,
   processData: (mockedApi: any) => any;
   beforeProcess?: () => void;
+  store?: Object;
 }
 
 export const DefaultEpic = ({
-  action, epic, processData, beforeProcess = () => {}
+  action, epic, processData, beforeProcess = () => {}, store = {}
 }: Props) => {
+  // Initialize / override default state
+  const state = new StateObservable(new Subject(), { ...TestStore.getState(), ...store });
+
   // Redux action to trigger epic
   const action$ = ActionsObservable.of(typeof action === "function" ? action(mockedAPI) : action );
 
   // Initializing epic instance
-  const epic$ = epic(action$, store, {});
+  const epic$ = epic(action$, state, {});
 
   if (beforeProcess) beforeProcess();
 
