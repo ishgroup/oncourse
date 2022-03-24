deba7008feaa5ecd9a6a95bafaa2eafef089bc15
Yury Yasuchenya <sammaell@yandex.ru>
2022-03-24 17:22:06 +0300

    OD-17122 Refactored common aql search input components to be validated on server side only. Refactored non generic plain records getters. Fixed various logic errors

--- client-html/src/js/containers/entities/discounts/reducers/index.ts
+++ client-html/src/js/containers/entities/discounts/reducers/index.ts
@@ -1,29 +1,9 @@
 import { IAction } from "../../../../common/actions/IshAction";
-import {
-  CLEAR_DISCOUNTS,
-  GET_DISCOUNT_CONTACT_RELATION_TYPES_FULFILLED,
-  GET_DISCOUNT_COS_ACCOUNTS_FULFILLED,
-  GET_DISCOUNT_MEMBERSHIPS_FULFILLED,
-  GET_DISCOUNTS_FULFILLED
-} from "../actions";
+import { GET_DISCOUNT_CONTACT_RELATION_TYPES_FULFILLED, GET_DISCOUNT_COS_ACCOUNTS_FULFILLED, } from "../actions";
 import { DiscountsState } from "./state";
 
 export const discountsReducer = (state: DiscountsState = {}, action: IAction<any>): any => {
   switch (action.type) {
-    case GET_DISCOUNTS_FULFILLED: {
-      return {
-        ...state,
-        ...action.payload
-      };
-    }
-
-    case GET_DISCOUNT_MEMBERSHIPS_FULFILLED: {
-      return {
-        ...state,
-        ...action.payload
-      };
-    }
-
     case GET_DISCOUNT_CONTACT_RELATION_TYPES_FULFILLED: {
       return {
         ...state,
@@ -36,10 +16,6 @@ export const discountsReducer = (state: DiscountsState = {}, action: IAction<any
         ...state,
         ...action.payload
       };
-    }
-
-    case CLEAR_DISCOUNTS: {
-      return action.payload;
     }
 
     default:
