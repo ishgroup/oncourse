c738f4cdb26b8ecedb8552485bd5ac6a165e206b
dmitrij <dmitriy.tarasienko@gmail.com>
2022-03-29 16:52:22 +0300

    OD-17143 feat: contact intersections api implementation without intersections models

--- server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/ContactInteractionApiImpl.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/ContactInteractionApiImpl.groovy
@@ -8,13 +8,48 @@
 
 package ish.oncourse.server.api.v1.service.impl
 
+import com.google.inject.Inject
+import ish.common.types.ApplicationStatus
+import ish.common.types.LeadStatus
+import ish.oncourse.server.ICayenneService
+import ish.oncourse.server.api.dao.ContactDao
+import ish.oncourse.server.api.dao.UserDao
+import ish.oncourse.server.api.v1.model.ContactActivityDTO
 import ish.oncourse.server.api.v1.model.ContactInsightDTO
+import ish.oncourse.server.api.v1.model.ContactOverviewDTO
 import ish.oncourse.server.api.v1.service.ContactInteractionApi
 
+import java.time.Duration
+import java.time.Instant
+
 class ContactInteractionApiImpl implements ContactInteractionApi{
+    @Inject
+    private ContactDao contactDao
+
+    @Inject
+    private ICayenneService cayenneService
+
     @Override
     ContactInsightDTO getInteraction(Long id) {
+        def context = cayenneService.newReadonlyContext
+        def contact = contactDao.getById(context, id)
+        def user = UserDao.getByEmail(context, contact.email)
 
-        return null
+        def contactActivityDto = new ContactActivityDTO()
+        contactActivityDto.lastContacted(user.lastAccess.toLocalDate())
+        contactActivityDto.inactiveDays(Duration.between(Instant.now(),user.lastAccess.toInstant()).toDays())
+        //TODO: intercections initializing
+
+        def contactOverview = new ContactOverviewDTO()
+        contactOverview.enrolments(contact.student?.enrolments?.collect {it.id})
+        contactOverview.openApplications(contact?.student?.applications?.findAll {it.status != ApplicationStatus.REJECTED && it.status != ApplicationStatus.WITHDRAWN}?.collect {it.id})
+        contactOverview.closeApplications(contact?.student?.applications?.findAll {it.status == ApplicationStatus.REJECTED || it.status == ApplicationStatus.WITHDRAWN}?.collect {it.id})
+        contactOverview.openLeads(contact.leads.findAll{it.status == LeadStatus.OPEN}.collect{it.id})
+        contactOverview.closeLeads(contact.leads.findAll{it.status == LeadStatus.CLOSED}.collect{it.id})
+
+        def contactInsight = new ContactInsightDTO()
+        contactInsight.activity(contactActivityDto)
+        contactInsight.overview(contactOverview)
+        contactInsight
     }
 }
