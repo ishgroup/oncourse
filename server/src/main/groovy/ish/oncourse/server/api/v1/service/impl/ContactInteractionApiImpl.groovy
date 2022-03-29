2bd058d0b817732f7101e29e961ae698253539f1
dmitrij <dmitriy.tarasienko@gmail.com>
2022-03-29 13:27:52 +0300

    OD-17142 feat: models and endpoint for contacts interactions api

---
+++ server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/ContactInteractionApiImpl.groovy
@@ -0,0 +1,20 @@
+/*
+ * Copyright ish group pty ltd 2022.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.api.v1.service.impl
+
+import ish.oncourse.server.api.v1.model.ContactInsightDTO
+import ish.oncourse.server.api.v1.service.ContactInteractionApi
+
+class ContactInteractionApiImpl implements ContactInteractionApi{
+    @Override
+    ContactInsightDTO getInteraction(Long id) {
+
+        return null
+    }
+}
