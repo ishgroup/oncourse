5da9d9c1fd7a4e14f39081f7e230c51bdda78f9b
dmitrij <dmitriy.tarasienko@gmail.com>
2022-04-01 18:43:21 +0300

    OD-17098 feat: test for json export; fix: bug with output printing

--- server/src/test/groovy/ish/oncourse/server/export/JsonExportsTest.groovy
+++ server/src/test/groovy/ish/oncourse/server/export/JsonExportsTest.groovy
@@ -11,23 +11,16 @@ package ish.oncourse.server.export
 import groovy.transform.CompileStatic
 import ish.DatabaseSetup
 import ish.TestWithDatabase
-import ish.common.types.AutomationStatus
 import ish.export.ExportParameter
 import ish.export.ExportResult
-import ish.oncourse.common.ResourceType
-import ish.oncourse.common.ResourcesUtil
-import ish.oncourse.server.cayenne.Contact
-import ish.oncourse.server.cayenne.Script
-import ish.oncourse.server.scripting.GroovyScriptService
-import ish.oncourse.server.scripting.ScriptParameters
-import ish.scripting.ScriptResult
+import ish.oncourse.common.ResourceProperty
+import ish.oncourse.server.upgrades.DataPopulationUtils
+import ish.oncourse.types.OutputType
 import org.apache.cayenne.exp.ExpressionFactory
-import org.apache.cayenne.query.ObjectSelect
 import org.apache.commons.io.IOUtils
 import org.apache.commons.lang3.StringUtils
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import ish.oncourse.server.integration.PluginService
 
 @CompileStatic
 @DatabaseSetup(value = "ish/oncourse/server/export/jsonExport.xml")
@@ -36,35 +29,38 @@ class JsonExportsTest extends TestWithDatabase {
 
     @Test
     void testJsonReportGenerating() {
-        GroovyScriptService scriptService = injector.getInstance(GroovyScriptService.class)
-        def scriptName = PluginService.getPluggableResources(ResourceType.EXPORT.getResourcePath(), "ish.onCourse.contact.json.groovy")
-        String scriptText = ResourcesUtil.readFile((scriptName as LinkedHashSet).first() as String)
-
-        Script script = cayenneContext.newObject(Script.class)
-        script.setAutomationStatus(AutomationStatus.ENABLED)
-        script.setScript(scriptText)
-
-        def records = ObjectSelect.query(Contact).select(cayenneContext)
+        Map<String, Object> props = new HashMap<>()
+        props.put(ResourceProperty.KEY_CODE.displayName, "ish.onCourse.contact.json")
+        props.put(ResourceProperty.NAME.displayName, "Json export")
+        props.put(ResourceProperty.ENTITY_CLASS.displayName, "Contact")
+        props.put(ResourceProperty.OUTPUT_TYPE.displayName, OutputType.JSON)
+        props.put(ResourceProperty.BODY.displayName, "ish.onCourse.contact.json.groovy")
+        DataPopulationUtils.updateExport(cayenneContext, props)
 
 
-        ScriptResult result = scriptService
-                .runScript(script,
-                        ScriptParameters.empty().add("records", records),
-                        cayenneService.getNewContext())
+        ExportParameter param = new ExportParameter()
+
+        param.setEntity("Contact")
+        param.setXslKeyCode("ish.onCourse.contact.json")
+        param.setExpression(ExpressionFactory.expTrue())
+
+        ExportService export = injector.getInstance(ExportService.class)
+
+        ExportResult result = export.export(param)
+        String resultExportString = StringUtils.replace(new String(result.getResult()), StringUtils.CR, StringUtils.EMPTY)
 
         byte[] sampleExport = IOUtils.toByteArray(AllExportTemplatesTest.class.getClassLoader().getResourceAsStream("ish/oncourse/server/export/output/jsonExport.json"))
 
         String sampleExportString = StringUtils.replace(new String(sampleExport), StringUtils.CR, StringUtils.EMPTY)
-        //String resultExportString = StringUtils.replace(new String(result.getResult()), StringUtils.CR, StringUtils.EMPTY)
+
 
         String[] sampleExportSplit = sampleExportString.split(LINE_SEPARATOR)
-        //String[] resultExportSplit = resultExportString.split(LINE_SEPARATOR)
+        String[] resultExportSplit = resultExportString.split(LINE_SEPARATOR)
 
-/*
         Assertions.assertEquals(sampleExportSplit.length, resultExportSplit.length)
 
         for (int i = 0; i < sampleExportSplit.length; i++) {
             Assertions.assertEquals(sampleExportSplit[i].trim(), resultExportSplit[i].trim())
-        }*/
+        }
     }
 }
