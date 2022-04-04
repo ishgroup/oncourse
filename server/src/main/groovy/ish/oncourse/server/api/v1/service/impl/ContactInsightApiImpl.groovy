b7ff330a3cde7a127f569010ba52f6515f7c2f6d
dmitrij <dmitriy.tarasienko@gmail.com>
2022-04-04 20:41:54 +0300

    OD-17142 fix: add loading of all documents and notes, related to contact activity

--- server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/ContactInsightApiImpl.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/v1/service/impl/ContactInsightApiImpl.groovy
@@ -18,14 +18,32 @@ import ish.oncourse.server.api.v1.model.ContactInsightDTO
 import ish.oncourse.server.api.v1.model.ContactInteractionDTO
 import ish.oncourse.server.api.v1.model.ContactOverviewDTO
 import ish.oncourse.server.api.v1.service.ContactInsightApi
+import ish.oncourse.server.cayenne.Application
 import ish.oncourse.server.cayenne.AssessmentSubmission
+import ish.oncourse.server.cayenne.AttachmentRelation
+import ish.oncourse.server.cayenne.Certificate
 import ish.oncourse.server.cayenne.Contact
+import ish.oncourse.server.cayenne.Document
+import ish.oncourse.server.cayenne.Enrolment
 import ish.oncourse.server.cayenne.InteractableTrait
+import ish.oncourse.server.cayenne.Invoice
+import ish.oncourse.server.cayenne.Lead
+import ish.oncourse.server.cayenne.Note
+import ish.oncourse.server.cayenne.NoteRelation
+import ish.oncourse.server.cayenne.PaymentIn
+import ish.oncourse.server.cayenne.PaymentOut
+import ish.oncourse.server.cayenne.Payslip
+import ish.oncourse.server.cayenne.ProductItem
+import ish.oncourse.server.cayenne.Quote
 import ish.oncourse.server.document.DocumentService
+import org.apache.cayenne.exp.Expression
+import org.apache.cayenne.exp.Property
+import org.apache.cayenne.exp.parser.ASTTrue
+import org.apache.cayenne.query.ObjectSelect
 
 import static ish.oncourse.server.api.v1.function.DocumentFunctions.getProfilePicture
 
-class ContactInsightApiImpl implements ContactInsightApi{
+class ContactInsightApiImpl implements ContactInsightApi {
     @Inject
     private ContactDao contactDao
 
@@ -35,21 +53,35 @@ class ContactInsightApiImpl implements ContactInsightApi{
     @Inject
     private DocumentService documentService
 
+    private static Map<String, List<Long>> entitiesWithAttachments = new HashMap<>()
+
     @Override
     ContactInsightDTO getInsight(Long id) {
         def context = cayenneService.newReadonlyContext
         def contact = contactDao.getById(context, id)
-
+        def student = contact.student
+        def invoices = contact.student?.invoices
+        def applications = contact.student?.applications
+        def enrolments = student?.enrolments
 
         def contactOverview = new ContactOverviewDTO()
         contactOverview.firstSeen = contact.createdOn.toLocalDate()
-        contactOverview.owing = (contact.student?.invoices*.amountOwing?.sum() as Money ?: Money.ZERO).toBigDecimal()
-        contactOverview.spent = (contact.student?.invoices*.amountPaid?.sum() as Money ?: Money.ZERO).toBigDecimal()
-        contactOverview.enrolments(contact.student?.enrolments?.collect {it.id})
-        contactOverview.openApplications(contact?.student?.applications?.findAll {it.status != ApplicationStatus.REJECTED && it.status != ApplicationStatus.WITHDRAWN}?.collect {it.id})
-        contactOverview.closeApplications(contact?.student?.applications?.findAll {it.status == ApplicationStatus.REJECTED || it.status == ApplicationStatus.WITHDRAWN}?.collect {it.id})
-        contactOverview.openLeads(contact.leads.findAll{it.status == LeadStatus.OPEN}.collect{it.id})
-        contactOverview.closeLeads(contact.leads.findAll{it.status == LeadStatus.CLOSED}.collect{it.id})
+        contactOverview.owing = (invoices*.amountOwing?.sum() as Money ?: Money.ZERO).toBigDecimal()
+        contactOverview.spent = (invoices*.amountPaid?.sum() as Money ?: Money.ZERO).toBigDecimal()
+        contactOverview.enrolments(enrolments?.collect { it.id } ?: new ArrayList<Long>())
+        contactOverview.openApplications(applications
+                ?.findAll { it.status != ApplicationStatus.REJECTED && it.status != ApplicationStatus.WITHDRAWN }
+                ?.collect { it.id } ?: new ArrayList<Long>())
+        contactOverview.closeApplications(applications
+                ?.findAll { it.status == ApplicationStatus.REJECTED || it.status == ApplicationStatus.WITHDRAWN }
+                ?.collect { it.id } ?: new ArrayList<Long>())
+        contactOverview.openLeads(contact.leads.findAll { it.status == LeadStatus.OPEN }.collect { it.id })
+        contactOverview.closeLeads(contact.leads.findAll { it.status == LeadStatus.CLOSED }.collect { it.id })
+
+        entitiesWithAttachments.put(Invoice.class.simpleName, invoices?.collect { it.id })
+        entitiesWithAttachments.put(Enrolment.class.simpleName, enrolments?.collect { it.id })
+        entitiesWithAttachments.put(Application.class.simpleName, applications?.collect { it.id })
+        entitiesWithAttachments.put(Lead.class.simpleName, contact.leads.collect { it.id })
 
 
         def contactInsight = new ContactInsightDTO()
@@ -66,27 +98,72 @@ class ContactInsightApiImpl implements ContactInsightApi{
         contactInsight
     }
 
-    private static List<ContactInteractionDTO> interactionsOf(Contact contact){
+    private List<ContactInteractionDTO> interactionsOf(Contact contact) {
+        def student = contact?.student
+        def enrolments = student?.enrolments
+        def assessmentSubmissions = enrolments*.assessmentSubmissions?.flatten() as List<AssessmentSubmission>
         def interactions = new ArrayList<ContactInteractionDTO>()
-        interactions.addAll(interactionsOfList(contact?.student?.enrolments))
-        interactions.addAll(interactionsOfList(contact?.student?.applications))
-        interactions.addAll(interactionsOfList(contact?.student?.waitingLists))
+        interactions.addAll(interactionsOfList(enrolments))
+        interactions.addAll(interactionsOfList(student?.applications))
+        interactions.addAll(interactionsOfList(student?.waitingLists))
         interactions.addAll(interactionsOfList(contact?.leads))
         interactions.addAll(interactionsOfList(contact?.paymentsIn))
         interactions.addAll(interactionsOfList(contact?.paymentsOut))
         interactions.addAll(interactionsOfList(contact?.invoices))
         interactions.addAll(interactionsOfList(contact?.quotes))
-        interactions.addAll(interactionsOfList((contact?.student?.enrolments*.assessmentSubmissions?.flatten() as List<AssessmentSubmission>)))
-        interactions.addAll(interactionsOfList(contact?.student?.certificates))
+        interactions.addAll(interactionsOfList(assessmentSubmissions))
+        interactions.addAll(interactionsOfList(student?.certificates))
         interactions.addAll(interactionsOfList(contact?.payslips))
         interactions.addAll(interactionsOfList(contact?.productItems))
-        interactions.addAll(interactionsOfList(contact?.noteRelations*.note))
-        interactions.addAll(interactionsOfList(contact?.documents))
-        interactions.sort {a,b-> b.date<=>a.date}
+
+        entitiesWithAttachments.put(PaymentIn.class.simpleName, contact?.paymentsIn?.collect { it.id })
+        entitiesWithAttachments.put(PaymentOut.class.simpleName, contact?.paymentsOut?.collect { it.id })
+        entitiesWithAttachments.put(Quote.class.simpleName, contact?.quotes?.collect { it.id })
+        entitiesWithAttachments.put(AssessmentSubmission.class.simpleName, assessmentSubmissions?.collect { it.id })
+        entitiesWithAttachments.put(Certificate.class.simpleName, assessmentSubmissions?.collect { it.id })
+        entitiesWithAttachments.put(Payslip.class.simpleName, assessmentSubmissions?.collect { it.id })
+        entitiesWithAttachments.put(ProductItem.class.simpleName, assessmentSubmissions?.collect { it.id })
+
+
+        def notes = ObjectSelect.query(Note)
+                .where(buildNotesExpression())
+                .select(cayenneService.newReadonlyContext)
+        def attachmentRelations = ObjectSelect.query(AttachmentRelation)
+                .where(buildDocumentsExpression())
+                .select(cayenneService.newReadonlyContext)
+
+
+        interactions.addAll(interactionsOfList(notes))
+        interactions.addAll(interactionsOfList(attachmentRelations))
+        interactions.sort { a, b -> b.date <=> a.date }
+
         interactions
     }
 
-    private static List<ContactInteractionDTO> interactionsOfList(List<? extends InteractableTrait> records){
-        records?.collect {it.toInteraction()} ?: new ArrayList<ContactInteractionDTO>()
+    private static Expression buildNotesExpression() {
+        buildRelationExpression(Note.NOTE_RELATIONS.dot(NoteRelation.ENTITY_IDENTIFIER),Note.NOTE_RELATIONS.dot(NoteRelation.ENTITY_RECORD_ID))
+    }
+
+    private static Expression buildDocumentsExpression() {
+        buildRelationExpression(AttachmentRelation.ENTITY_IDENTIFIER,AttachmentRelation.ENTITY_RECORD_ID)
+    }
+
+    private static Expression buildRelationExpression(Property<String>entityIdentifierProperty, Property<Long> entityIdProperty){
+        Expression expression = new ASTTrue()
+        for (def entry : entitiesWithAttachments.entrySet()) {
+            if(entry.getValue() && !entry.getValue().isEmpty()) {
+                expression = expression.orExp(
+                        entityIdentifierProperty.eq(entry.getKey())
+                                .andExp(
+                                        entityIdProperty.in(entry.getValue())
+                                )
+                )
+            }
+        }
+        expression
+    }
+
+    private static List<ContactInteractionDTO> interactionsOfList(List<? extends InteractableTrait> records) {
+        records?.collect { it.toInteraction() } ?: new ArrayList<ContactInteractionDTO>()
     }
 }
