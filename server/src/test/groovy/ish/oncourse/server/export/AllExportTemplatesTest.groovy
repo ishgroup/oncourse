5ada09272c0d9428b808a2d3a3daeb553cedb7dc
dmitrij <dmitriy.tarasienko@gmail.com>
2022-04-08 14:00:30 +0300

    OD-16742 feat: rewrite all exports test in accordance with last backend changes

--- server/src/test/groovy/ish/oncourse/server/export/AllExportTemplatesTest.groovy
+++ server/src/test/groovy/ish/oncourse/server/export/AllExportTemplatesTest.groovy
@@ -5,34 +5,43 @@
 package ish.oncourse.server.export
 
 import groovy.transform.CompileStatic
+import ish.DatabaseSetup
 import ish.TestWithDatabase
+import ish.TestWithDatabaseExtension
 import ish.export.ExportParameter
 import ish.export.ExportResult
 import ish.oncourse.common.ResourceProperty
 import ish.oncourse.common.ResourceType
 import ish.oncourse.common.ResourcesUtil
+import ish.oncourse.server.cayenne.Account
+import ish.oncourse.server.cayenne.CourseClass
 import ish.oncourse.server.cayenne.ExportTemplate
 import ish.oncourse.server.cayenne.ExportTemplateAutomationBinding
+import ish.oncourse.server.cayenne.PaymentMethod
+import ish.oncourse.server.cayenne.Tax
 import ish.oncourse.server.integration.PluginService
 import ish.oncourse.server.upgrades.DataPopulation
+import ish.oncourse.server.upgrades.DataPopulationUtils
 import org.apache.cayenne.exp.ExpressionFactory
 import org.apache.cayenne.query.ObjectSelect
 import org.apache.commons.io.IOUtils
 import org.apache.commons.lang3.StringUtils
+import org.dbunit.database.IDatabaseConnection
 import org.dbunit.dataset.ReplacementDataSet
 import org.dbunit.dataset.xml.FlatXmlDataSet
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder
+import org.dbunit.operation.DatabaseOperation
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Disabled
-import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.yaml.snakeyaml.Yaml
 
 import java.time.LocalDate
 
 @CompileStatic
-@Disabled
+@DatabaseSetup
 class AllExportTemplatesTest extends TestWithDatabase {
     private static final String UTC_TIMEZONE_ID = "UTC"
 
@@ -41,10 +50,11 @@ class AllExportTemplatesTest extends TestWithDatabase {
     private static final String LINE_SEPARATOR = StringUtils.LF
 
     void setup(String testDataFile) {
-
+        //cleanUpDB()
         // set default timezone to UTC to receive same export output regardless of
         // default timezone of building machine
         TimeZone.setDefault(TimeZone.getTimeZone(UTC_TIMEZONE_ID))
+
 
         try {
             InputStream st = AllExportTemplatesTest.class.getClassLoader().getResourceAsStream("ish/oncourse/server/export/" + testDataFile)
@@ -55,8 +65,11 @@ class AllExportTemplatesTest extends TestWithDatabase {
             ReplacementDataSet rDataSet = new ReplacementDataSet(dataSet)
             rDataSet.addReplacementObject("[null]", null)
 
-            executeDatabaseOperation(rDataSet)
-
+            IDatabaseConnection testDatabaseConnection = (dbExtension as TestWithDatabaseExtension).getTestDatabaseConnection()
+            DatabaseOperation.CLEAN_INSERT.execute(testDatabaseConnection, rDataSet)
+            testDatabaseConnection.connection.commit()
+            //cayenneContext = cayenneService.newContext
+           // executeDatabaseOperation(rDataSet)
             DataPopulation dataPopulation = injector.getInstance(DataPopulation.class)
 
             dataPopulation.run()
@@ -82,32 +95,69 @@ class AllExportTemplatesTest extends TestWithDatabase {
         TimeZone.setDefault(null)
     }
 
-    static Collection<Arguments> values() throws Exception {
+    @BeforeEach
+    void temp(){}
+
+    Collection<Arguments> values() throws Exception {
 
         List<Arguments> result = new ArrayList<>()
-        def resourcesList = PluginService.getPluggableResources(ResourceType.EXPORT.getResourcePath(), ResourceType.EXPORT.getFilePattern())
+        def pathsList = PluginService.getPluggableResources(ResourceType.EXPORT.getResourcePath(), ResourceType.EXPORT.getFilePattern())
+        List<Map<String, Object>> resourcesList = new ArrayList<>()
 
-        for (String propFile : resourcesList) {
-            InputStream resourceAsStream = ResourcesUtil.getResourceAsInputStream(propFile)
-            Properties props = new Properties()
-            props.load(resourceAsStream)
+        for (String propFile : pathsList) {
+            def resourceAsStream = ResourcesUtil.getResourceAsInputStream(propFile)
+            Yaml yaml = new Yaml()
+            def sources = yaml.load(resourceAsStream)
+            resourcesList.addAll(sources as List<Map<String,Object>>)
 
-            String keyCode = (String) props.get(ResourceProperty.KEY_CODE.getDisplayName())
-            String entityName = ((String) props.get(ResourceProperty.ENTITY_CLASS.getDisplayName()))
-            String outputExtention = keyCode.split("\\.").last()
-            String dataSet = keyCode.split("\\.")[2] + "DataSet.xml"
-            String output = keyCode.split("\\.")[2] + "SampleOutput." + outputExtention
+            for(Map<String,Object>props:resourcesList){
+                DataPopulationUtils.updateExport(cayenneContext, props)
+                String keyCode = (String) props.get(ResourceProperty.KEY_CODE.getDisplayName())
+                String entityName = ((String) props.get(ResourceProperty.ENTITY_CLASS.getDisplayName()))
+                String outputExtention = keyCode.split("\\.").last()
+                String dataSet = keyCode.split("\\.")[2] + "DataSet.xml"
+                String output = keyCode.split("\\.")[2] + "SampleOutput." + outputExtention
 
-            result.add(Arguments.of(keyCode, entityName, dataSet, output))
-
+                result.add(Arguments.of(keyCode, entityName, dataSet, output))
+            }
         }
         return result
     }
 
+    @Test
+    void testAllExports(){
+        def pathsList = PluginService.getPluggableResources(ResourceType.EXPORT.getResourcePath(), ResourceType.EXPORT.getFilePattern())
+        List<Map<String, Object>> resourcesList = new ArrayList<>()
+
+        for (String propFile : pathsList) {
+            def resourceAsStream = ResourcesUtil.getResourceAsInputStream(propFile)
+            Yaml yaml = new Yaml()
+            def sources = yaml.load(resourceAsStream)
+            resourcesList.addAll(sources as List<Map<String,Object>>)
+
+            int count = 0
+            for(Map<String,Object>props:resourcesList){
+                DataPopulationUtils.updateExport(cayenneContext, props)
+                String keyCode = (String) props.get(ResourceProperty.KEY_CODE.getDisplayName())
+                String entityName = ((String) props.get(ResourceProperty.ENTITY_CLASS.getDisplayName()))
+                String outputExtention = keyCode.split("\\.").last()
+                String dataSet = keyCode.split("\\.")[2] + "DataSet.xml"
+                String output = keyCode.split("\\.")[2] + "SampleOutput." + outputExtention
+                if(count>2)
+                    testExport(keyCode, entityName, dataSet, output)
+                count++
+            }
+        }
+    }
+
+
+    /*@Test
     @ParameterizedTest(name = "{1}-{0}")
-    @MethodSource("values")
-    void testExport(String keyCode, String entityName, String dataSet, output) throws Exception {
+    @MethodSource("values")*/
+    void testExport(String keyCode, String entityName, String dataSet, String output) throws Exception {
         setup(dataSet)
+        //cleanUpDB()
+        //def records = ObjectSelect.query(Account).select(cayenneContext)
         // exclude exports for Script entity - IshTestCase updates scripts from resources after table wipe
         if (entityName != "Script") {
             ExportParameter param = new ExportParameter()
@@ -119,6 +169,7 @@ class AllExportTemplatesTest extends TestWithDatabase {
             ExportService export = injector.getInstance(ExportService.class)
 
             ExportResult result = export.export(param)
+
 
             byte[] sampleExport = IOUtils.toByteArray(AllExportTemplatesTest.class.getClassLoader().getResourceAsStream("ish/oncourse/server/export/output/" + output))
 
@@ -138,7 +189,9 @@ class AllExportTemplatesTest extends TestWithDatabase {
                     sampleExportSplit[i] = sampleExportSplit[i].replace("diff", (LocalDate.now().getYear() - 2014).toString())
                 }
                 if (!(PAYSLIP_MICROPAY_KEYCODE.equals(keyCode) && i == 0)) {
-                    Assertions.assertEquals(sampleExportSplit[i].trim(), resultExportSplit[i].trim())
+                    def resultStr = resultExportSplit[i].trim()
+                    def sampleStr = sampleExportSplit[i].trim()
+                    Assertions.assertEquals(sampleStr, resultStr)
                 }
             }
         }
