0974122eeee072487cb34a06ae5b8d6df4779450
dmitrij <dmitriy.tarasienko@gmail.com>
2022-04-12 13:31:51 +0300

    OD-16742 feat: add variables to possible export parameters to make service more flexible

--- server/src/main/groovy/ish/oncourse/server/export/ExportService.groovy
+++ server/src/main/groovy/ish/oncourse/server/export/ExportService.groovy
@@ -65,7 +65,7 @@ class ExportService {
 	 * @param expParam export specification
 	 * @return {@link ExportResult} object containing resulting export data
 	 */
-	ExportResult export(ExportParameter expParam) {
+	ExportResult export(ExportParameter expParam, Map<String, Object> variables = [:]) {
 		def entity = expParam.entity
 
 		def exportables = expParam.expression == null && expParam.ids.size() > 0 ?
@@ -77,7 +77,7 @@ class ExportService {
 
 		ExportResult result = new ExportResult()
 
-		String out = performExport(template, exportables)
+		String out = performExport(template, exportables, variables)
 		result.setResult(out.bytes)
 
 		return result
