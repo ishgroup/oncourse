0b6148f57165c2c3743b45739519713c366214d3
Yury Yasuchenya <sammaell@yandex.ru>
2022-05-24 16:58:40 +0300

    fix failing tests

--- client-html/src/tests/automation/pdf-backgrounds/CreatePdfBackground.Epic.test.ts
+++ client-html/src/tests/automation/pdf-backgrounds/CreatePdfBackground.Epic.test.ts
@@ -1,21 +1,28 @@
+import { initialize } from "redux-form";
 import { DefaultEpic } from "../../common/Default.Epic";
 import { FETCH_SUCCESS } from "../../../js/common/actions";
 import {
   createAutomationPdfBackground,
   getAutomationPdfBackgroundsList
 } from "../../../js/containers/automation/containers/pdf-backgrounds/actions";
+import {
+  PDF_BACKGROUND_FORM_NAME
+} from "../../../js/containers/automation/containers/pdf-backgrounds/PdfBackgrounds";
 import { EpicCreatePdfBackground } from "../../../js/containers/automation/containers/pdf-backgrounds/epics/EpicCreatePdfBackground";
+
+const overlay = {
+  name: "name 21",
+  lastModified: new Date().getTime(),
+  size: 5176,
+  type: "image/png"
+} as File;
 
 describe("Create pdf background epic tests", () => {
   it("EpicCreatePdfBackground should returns correct values", () => DefaultEpic({
-    action: createAutomationPdfBackground("name 21", {
-      name: "name 21",
-      lastModified: new Date().getTime(),
-      size: 5176,
-      type: "image/png"
-    } as File),
+    action: createAutomationPdfBackground("name 21", overlay),
     epic: EpicCreatePdfBackground,
     processData: () => [
+      initialize(PDF_BACKGROUND_FORM_NAME, overlay),
       getAutomationPdfBackgroundsList(false, "name 21"),
       {
         type: FETCH_SUCCESS,
