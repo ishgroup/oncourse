815d7327f41335864ae25720163eb5647ed5b428
Yury Yasuchenya <sammaell@yandex.ru>
2022-06-16 12:42:59 +0300

    OD-17245 Refactored remaining route change handler usages. Remove redundant imports

--- client-html/src/js/containers/preferences/containers/concession-types/components/ConcessionTypesForm.tsx
+++ client-html/src/js/containers/preferences/containers/concession-types/components/ConcessionTypesForm.tsx
@@ -15,7 +15,6 @@ import ConcessionTypesRenderer from "./ConcessionTypesRenderer";
 import { getManualLink } from "../../../../../common/utils/getManualLink";
 import { idsToString } from "../../../../../common/utils/numbers/numbersNormalizing";
 import { State } from "../../../../../reducers/state";
-import { setNextLocation } from "../../../../../common/actions";
 import { cardsFormStyles } from "../../../styles/formCommonStyles";
 import { ShowConfirmCaller } from "../../../../../model/common/Confirm";
 import AppBarContainer from "../../../../../common/components/layout/AppBarContainer";
@@ -39,8 +38,7 @@ interface Props {
   onUpdate: (concessionTypes: ConcessionType[]) => void;
   openConfirm?: ShowConfirmCaller;
   history?: any,
-  nextLocation?: string,
-  setNextLocation?: (nextLocation: string) => void,
+  nextLocation?: string
 }
 
 class ConcessionTypesBaseForm extends React.Component<Props, any> {
@@ -93,12 +91,11 @@ class ConcessionTypesBaseForm extends React.Component<Props, any> {
       this.props.onUpdate(this.getTouchedAndNew(value.types));
     })
       .then(() => {
-        const { nextLocation, history, setNextLocation } = this.props;
+        const { nextLocation, history } = this.props;
 
         this.props.dispatch(initialize(CONCESSION_TYPES_FORM, { types: this.props.concessionTypes }));
 
         nextLocation && history.push(nextLocation);
-        setNextLocation('');
       })
       .catch(error => {
         this.isPending = false;
@@ -203,13 +200,9 @@ const mapStateToProps = (state: State) => ({
   nextLocation: state.nextLocation,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
-  setNextLocation: (nextLocation: string) => dispatch(setNextLocation(nextLocation)),
-});
-
 const ConcessionTypesForm = reduxForm({
   onSubmitFail,
   form: CONCESSION_TYPES_FORM
-})(connect<any, any, any>(mapStateToProps, mapDispatchToProps)(withStyles(cardsFormStyles)(withRouter(ConcessionTypesBaseForm)) as any));
+})(connect<any, any, any>(mapStateToProps, null)(withStyles(cardsFormStyles)(withRouter(ConcessionTypesBaseForm)) as any));
 
 export default ConcessionTypesForm;
--- client-html/src/js/containers/tags/epics/EpicCreateTag.ts
+++ client-html/src/js/containers/tags/epics/EpicCreateTag.ts
@@ -5,25 +5,31 @@
 
 import { Epic } from "redux-observable";
 import { Tag } from "@api/model";
+import { initialize } from "redux-form";
 import * as EpicUtils from "../../../common/epics/EpicUtils";
 import TagsService from "../services/TagsService";
 import { CREATE_TAG_REQUEST, getAllTags } from "../actions";
 import { FETCH_SUCCESS } from "../../../common/actions";
 import FetchErrorHandler from "../../../common/api/fetch-errors-handlers/FetchErrorHandler";
-import { initialize } from "redux-form";
 import { TAGS_FORM_NAME } from "../constants";
+import history from "../../../constants/History";
 
 const request: EpicUtils.Request<Tag[], Tag> = {
   type: CREATE_TAG_REQUEST,
   getData: tag => TagsService.create(tag),
-  processData: (r, s, tag) => [
+  processData: (r, s, tag) => {
+    if (s.nextLocation) {
+      history.push(s.nextLocation);
+    }
+    return [
+      initialize(TAGS_FORM_NAME, tag),
       {
         type: FETCH_SUCCESS,
         payload: { message: `${tag.type} was successfully created` }
       },
-      initialize(TAGS_FORM_NAME, tag),
       getAllTags(tag.name)
-    ],
+    ];
+  },
   processError: (r, t) => FetchErrorHandler(r, `Error. ${t.type} was not created`)
 };
 
