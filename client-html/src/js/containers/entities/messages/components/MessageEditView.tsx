0c6c8ad5e2acf433fd6e504d6e7c025c5de61bc9
Yury Yasuchenya <sammaell@yandex.ru>
2022-08-25 16:34:24 +0300

    OD-17475 Added contact insight shortcut to Message record

--- client-html/src/js/containers/entities/messages/components/MessageEditView.tsx
+++ client-html/src/js/containers/entities/messages/components/MessageEditView.tsx
@@ -17,6 +17,7 @@ import { format } from "date-fns";
 import Uneditable from "../../../../common/components/form/Uneditable";
 import { III_DD_MMM_YYYY } from "../../../../common/utils/dates/format";
 import { defaultContactName } from "../../contacts/utils";
+import { ContactLinkAdornment } from "../../../../common/components/form/FieldAdornments";
 
 interface MessageEditViewProps {
   values?: Message;
@@ -61,7 +62,9 @@ const MessageEditView = React.memo<MessageEditViewProps>(props => {
           <Uneditable
             value={defaultContactName(values.sentToContactFullname)}
             label="Sent to"
-            url={`/contact?search=messages.message.id=${values.id}`}
+            labelAdornment={(
+              <ContactLinkAdornment id={values?.contactId} />
+            )}
           />
         </Grid>
         <Grid item xs={twoColumn ? 2 : 6}>
