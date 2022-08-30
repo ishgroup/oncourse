aad8298fb00f30e7b6b4ebe913a6ddbe12b6c392
Yury Yasuchenya <sammaell@yandex.ru>
2022-08-30 13:42:29 +0300

    OD-17468 Adjusted list loading indicator placing

--- client-html/src/js/common/components/list-view/ListView.tsx
+++ client-html/src/js/common/components/list-view/ListView.tsx
@@ -1095,6 +1095,8 @@ class ListView extends React.PureComponent<Props & OwnProps, ComponentState> {
 
     return (
       <div className={classes.root}>
+        <LoadingIndicator customLoading={isAsyncValidating} transparentBackdrop allowInteractions />
+
         <FullScreenEditView
           {...editViewProps}
           shouldAsyncValidate={shouldAsyncValidate}
@@ -1173,7 +1175,6 @@ class ListView extends React.PureComponent<Props & OwnProps, ComponentState> {
 
         <div className="flex-fill d-flex flex-column overflow-hidden user-select-none">
           <div className="flex-fill d-flex relative">
-            <LoadingIndicator customLoading={isAsyncValidating} transparentBackdrop allowInteractions />
             {threeColumn ? (
               <ResizableWrapper
                 onResizeStop={this.handleResizeMainContentCallBack}
