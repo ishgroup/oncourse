1424d08792bdff8b2866a12e32837ca2ed31e2b7
dmitrij <dmitriy.tarasienko@gmail.com>
2022-09-26 13:08:27 +0300

    OD-17518 feat: test changeset to add image quality property to preferences

---
+++ server/src/main/java/ish/oncourse/server/upgrades/liquibase/change/CreateHighQualityPreference.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright ish group pty ltd 2022.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.upgrades.liquibase.change;
+
+import ish.oncourse.server.ICayenneService;
+import ish.oncourse.server.api.v1.model.PreferenceEnumDTO;
+import ish.oncourse.server.cayenne.Preference;
+import ish.oncourse.server.db.SchemaUpdateService;
+import liquibase.database.Database;
+import liquibase.exception.CustomChangeException;
+import org.apache.cayenne.access.DataContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CreateHighQualityPreference extends IshTaskChange {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public void execute(Database database) throws CustomChangeException {
+        ICayenneService cayenneService = SchemaUpdateService.sharedCayenneService;
+        DataContext context = cayenneService.getNewContext();
+
+        logger.warn("Running upgrade...");
+
+        Preference preference = context.newObject(Preference.class);
+        preference.setName(PreferenceEnumDTO.IMAGE_HIGHQUALITY_SCALE.toString());
+        preference.setUniqueKey(PreferenceEnumDTO.IMAGE_HIGHQUALITY_SCALE.toString());
+        preference.setValueString(String.valueOf(10));
+        context.commitChanges();
+    }
+}
