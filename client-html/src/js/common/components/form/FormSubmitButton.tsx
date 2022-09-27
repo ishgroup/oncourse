392e8a9987a9490c044ce80f16f09b7194a9e36f
Yury Yasuchenya <sammaell@yandex.ru>
2022-09-27 13:21:17 +0300

    OD-17529 Added form submit timeout to process blur events

--- client-html/src/js/common/components/form/FormSubmitButton.tsx
+++ client-html/src/js/common/components/form/FormSubmitButton.tsx
@@ -3,7 +3,7 @@
  * No copying or use of this code is allowed without permission in writing from ish.
  */
 
-import React from "react";
+import React, { useRef } from "react";
 import Button from "@mui/material/Button";
 import ErrorOutline from "@mui/icons-material/ErrorOutline";
 import { IS_JEST } from "../../../constants/EnvironmentConstants";
@@ -24,17 +24,36 @@ const FormSubmitButton = React.memo<Props>(({
     text = "Save",
     className,
   }) => {
+    const ref = useRef<HTMLButtonElement>();
+    const defaultPrevented = useRef(false);
+  
     const buttonProps = IS_JEST ? {
       "data-testid": "appbar-submit-button"
     } : {};
+
+    // timeout to process blur events
+    const onClick = e => {
+      if (!defaultPrevented.current) {
+        e.preventDefault();
+        defaultPrevented.current = true;
+        setTimeout(() => {
+          ref.current.click();
+        }, 600);
+        return;
+      }
+      defaultPrevented.current = false;
+    };
+
     return (
       <Button
         type="submit"
+        ref={ref}
         classes={{
           root: fab ? "saveButtonEditView" : "whiteAppBarButton",
           disabled: fab ? "saveButtonEditViewDisabled" : "whiteAppBarButtonDisabled"
         }}
         disabled={disabled}
+        onClick={onClick}
         startIcon={invalid && <ErrorOutline color="error" />}
         variant="contained"
         color="primary"
