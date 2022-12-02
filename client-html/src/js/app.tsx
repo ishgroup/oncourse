3df0f8b62f885a1abb83f3321b29b3330d02fc9e
dlitvinko <dlitvinko@objectstyle.com>
2022-12-02 14:40:34 +0300

    Test no labels , no java changes, js changes

--- client-html/src/js/app.tsx
+++ client-html/src/js/app.tsx
@@ -12,6 +12,7 @@ import { DEFAULT_CONFIG } from "./constants/Config";
 import store from "./constants/Store";
 import { EnvironmentConstants } from "./constants/EnvironmentConstants";
 
+// no-label-no-java-changes-js-changes
 if (process.env.NODE_ENV === EnvironmentConstants.production) {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
