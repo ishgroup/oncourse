0a97695d5d6b11d791a5261c7c26004d7931fba8
Yury Yasuchenya <yury@ish.com.au>
2022-12-02 14:11:52 +0300

    OD-17737 fix class payment plans calculation

--- client-html/src/js/containers/entities/courseClasses/components/budget/CourseClassBudgetTab.tsx
+++ client-html/src/js/containers/entities/courseClasses/components/budget/CourseClassBudgetTab.tsx
@@ -57,7 +57,7 @@ import instantFetchErrorHandler from "../../../../../common/api/fetch-errors-han
 import { getTutorPayInitial } from "../tutors/utils";
 import { getClassCostTypes } from "../../utils";
 import { BooleanArgFunction, StringArgFunction } from "../../../../../model/common/CommonFunctions";
-import { dateForCompare, getClassFeeTotal } from "./utils";
+import { dateForCompare, excludeOnEnrolPaymentPlan, getClassFeeTotal, includeOnEnrolPaymentPlan } from "./utils";
 import PreferencesService from "../../../../preferences/services/PreferencesService";
 import BudgetItemRow from "./BudgetItemRow";
 import uniqid from "../../../../../common/utils/uniqid";
@@ -456,9 +456,8 @@ const CourseClassBudgetTab = React.memo<Props>(
         const role = tutorRoles.find(r => r.id === tutor.roleId);
         onCostRate = (role && role["currentPayrate.oncostRate"]) ? parseFloat(role["currentPayrate.oncostRate"]) : 0;
       }
-
       setCourseClassBudgetModalOpened(true, isNaN(onCostRate) ? 0 : onCostRate);
-      dispatch(initialize(COURSE_CLASS_COST_DIALOG_FORM, data));
+      dispatch(initialize(COURSE_CLASS_COST_DIALOG_FORM, excludeOnEnrolPaymentPlan(data, currentTax)));
       closeAddBudgetMenu();
     };
 
@@ -482,7 +481,7 @@ const CourseClassBudgetTab = React.memo<Props>(
           bindedActionId = tutor && tutor.temporaryId;
         }
 
-        const postData = { ...data };
+        const postData = includeOnEnrolPaymentPlan(data, taxes);
         delete postData.index;
 
         if (postData.id === null) {
@@ -549,7 +548,7 @@ const CourseClassBudgetTab = React.memo<Props>(
             }
 
             dispatch(addActionToQueue(putCourseClassCost(postData), "PUT", "ClassCost", data.id));
-            dispatch(arraySplice(form, "budget", data.index, 1, data));
+            dispatch(arraySplice(form, "budget", data.index, 1, postData));
             closeModal();
           })
           .catch(response => instantFetchErrorHandler(dispatch, response));
