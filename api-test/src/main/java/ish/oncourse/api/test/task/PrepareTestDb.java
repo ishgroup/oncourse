2b6079c88ca361a58b7d5c265b1a05877694d185
dmitrij <dmitriy.tarasienko@gmail.com>
2022-12-06 16:55:03 +0300

    OD-17738 feat: keyword #future_year for year, which is needed to always be in the future

--- api-test/src/main/java/ish/oncourse/api/test/task/PrepareTestDb.java
+++ api-test/src/main/java/ish/oncourse/api/test/task/PrepareTestDb.java
@@ -17,12 +17,22 @@ import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
 
 public class PrepareTestDb {
 
@@ -37,7 +47,35 @@ public class PrepareTestDb {
             Connection connection = DriverManager.getConnection(args[DB_URL_ARG_INDEX]);
             IDatabaseConnection testDatabaseConnection = new DatabaseConnection(connection, null);
             testDatabaseConnection.getConfig().setProperty(DatabaseConfig.FEATURE_ALLOW_EMPTY_FIELDS, true);
-            InputStream st = new FileInputStream(args[DATASET_PATH_ARG_INDEX]);
+
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(new File(args[DATASET_PATH_ARG_INDEX]));
+            String tempFileName = args[DATASET_PATH_ARG_INDEX]+"-temp";
+            Element documentElement = doc.getDocumentElement();
+            var nodeList = documentElement.getChildNodes();
+            int length = nodeList.getLength();
+            for(int i = 0; i < length; i++){
+                Node item = nodeList.item(i);
+                NamedNodeMap attributes = item.getAttributes();
+                if(attributes == null)
+                    continue;
+                int attributesLength = attributes.getLength();
+                for(int j = 0; j<attributesLength; j++){
+                    var attribute = attributes.item(j);
+                    var attributeValue = attribute.getNodeValue();
+                    if(attributeValue.contains("#future_year")){
+                        attributes.item(j).setNodeValue(attributeValue.replaceAll("#future_year", String.valueOf(Calendar.getInstance().get(Calendar.YEAR) + 2)));
+                    }
+                }
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(tempFileName));
+            transformer.transform(source, result);
+
+            InputStream st = new FileInputStream(tempFileName);
             FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().setCaseSensitiveTableNames(false).setColumnSensing(true).build(st);
             DatabaseOperation.REFRESH.execute(testDatabaseConnection, dataSet);
 
@@ -51,7 +89,8 @@ public class PrepareTestDb {
                         "WHERE `tableName` = 'student'");
 
             }
-            
+
+            new File(tempFileName).deleteOnExit();
         } else {
             throw new RuntimeException("Test data wasn't inserted. Invalid parameters count!");
         }
