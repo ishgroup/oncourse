1f35228fa8c7911bedea6143625c867c9bb25bbe
dmitrij <dmitriy.tarasienko@gmail.com>
2022-12-30 11:29:00 +0300

    Revert "OD-16175 fix: allow custom data upgrades from plugins"
    This reverts commit 0c139bbd8add65826c9464fc9afad2114b68195f.

--- server/src/main/java/ish/oncourse/server/db/SchemaUpdateService.java
+++ server/src/main/java/ish/oncourse/server/db/SchemaUpdateService.java
@@ -43,7 +43,7 @@ public class SchemaUpdateService {
 
 
 
-	private static final String DATA_UPGRADE = "data/";
+	private static final String DATA_UPGRADE = "data/upgrades.yml";
 	private static final String RESOURCES_PATH = "database";
 
 	private final ICayenneService cayenneService;
@@ -105,17 +105,10 @@ public class SchemaUpdateService {
 	public void upgradeData() throws SQLException, DatabaseException {
 		sharedCayenneService = cayenneService;
 
-		List<String> yamlFiles = PluginService.getPluggableResources(DATA_UPGRADE, ".*\\.yml")
-				.stream()
-				.sorted( new NumberedFilesComparator())
-				.collect(Collectors.toList());
+		final var connection = cayenneService.getDataSource()
+				.getConnection();
 
-		for(var yamlFile:yamlFiles){
-			final var connection = cayenneService.getDataSource()
-					.getConnection();
-			applyChangeLog(connection, yamlFile);
-		}
-
+		applyChangeLog(connection, DATA_UPGRADE);
 
 		sharedCayenneService = null;
 	}
