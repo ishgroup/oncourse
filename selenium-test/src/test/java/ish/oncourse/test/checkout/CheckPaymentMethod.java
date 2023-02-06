56f65c11c2e9934238aa8ebbc40ab914aabe38c8
Kristina Trukhan <ktrukhan@objectstyle.com>
2023-02-06 08:54:54 +0100

    OD-17621 update configs

--- selenium-test/src/test/java/ish/oncourse/test/checkout/CheckPaymentMethod.java
+++ selenium-test/src/test/java/ish/oncourse/test/checkout/CheckPaymentMethod.java
@@ -16,8 +16,10 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 
 @TestInstance(TestInstance. Lifecycle.PER_CLASS)
@@ -43,36 +45,51 @@ public class CheckPaymentMethod extends AbstractSeleniumTest{
 
     @Test
     void test() {
-        // Test name: checkout 1
-        // Step # | name | target | value
         // 1 | open | https://127.0.0.1:8182/checkout |
         driver.get("https://127.0.0.1:8182/checkout");
-        // 2 | click | name=contacts |
+        // 3 | click | name=contacts |
         driver.findElement(By.name("contacts")).click();
-        // 3 | type | name=contacts | Kristina Ish
+        // 4 | type | name=contacts | Kristina Ish
         driver.findElement(By.name("contacts")).sendKeys("Kristina Ish");
-        // 4 | click | css=span:nth-child(1) > strong |
-//        driver.findElement(By.xpath("//div[@id=\'client\']/div/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/nav/div[2]/div/span/strong")).click();
-//        // 5 | click | name=items |
-//        driver.findElement(By.name("items")).click();
-//        // 6 | type | name=items | dcftc1
-//        driver.findElement(By.name("items")).sendKeys("dcftc1");
-//        // 7 | click | css=strong |
-//        driver.findElement(By.cssSelector("strong")).click();
-//        // 8 | click | css=.PrivateSwitchBase-input |
-//        driver.findElement(By.cssSelector(".PrivateSwitchBase-input")).click();
-//        // 9 | click | css=.MuiPaper-root:nth-child(3) .MuiSvgIcon-root |
-//        driver.findElement(By.cssSelector(".MuiPaper-root:nth-child(3) .MuiSvgIcon-root")).click();
-//        // 10 | click | css=.MuiIconButton-sizeSmall > .MuiSvgIcon-root |
-//        driver.findElement(By.cssSelector(".MuiIconButton-sizeSmall > .MuiSvgIcon-root")).click();
-//        // 11 | click | id=:rq:-option-0 |
-//        driver.findElement(By.id(":rq:-option-0")).click();
-//        // 12 | mouseOver | css=.MuiStepButton-root |
-//        {
-//            WebElement element = driver.findElement(By.cssSelector(".MuiStepButton-root"));
-//            Actions builder = new Actions(driver);
-//            builder.moveToElement(element).perform();
-//        }
+        // 5 | click | css=span:nth-child(3) > strong |
+        {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span:nth-child(3) > strong")));
+        }
+        driver.findElement(By.cssSelector("span:nth-child(3) > strong")).click();
+        // 6 | click | name=items |
+        driver.findElement(By.name("items")).click();
+        // 7 | type | name=items | dcftc1
+        driver.findElement(By.name("items")).sendKeys("dcftc1");
+        // 8 | click | css=.MuiListItemText-root |
+        driver.findElement(By.cssSelector(".MuiListItemText-root")).click();
+        // 9 | mouseOver | css=.mui-1d3bbye > .MuiGrid-grid-xs-1 |
+        {
+            WebElement element = driver.findElement(By.cssSelector(".mui-1d3bbye > .MuiGrid-grid-xs-1"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        // 10 | mouseOut | css=.mui-1d3bbye > .MuiGrid-grid-xs-1 |
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        // 11 | click | css=.PrivateSwitchBase-input |
+        driver.findElement(By.cssSelector(".PrivateSwitchBase-input")).click();
+        // 12 | click | css=.MuiPaper-root:nth-child(3) .MuiSvgIcon-root |
+        driver.findElement(By.cssSelector(".MuiPaper-root:nth-child(3) .MuiSvgIcon-root")).click();
+        // 13 | waitForElementVisible | name=payment_method | 30000
+        {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("payment_method")));
+        }
+        // 14 | click | name=payment_method |
+        driver.findElement(By.name("payment_method")).click();
+        // 15 | click | id=:rq:-option-0 |
+        driver.findElement(By.id(":rq:-option-0")).click();
+        // 16 | click | css=.centeredFlex:nth-child(2) .jss311 |
+        driver.findElement(By.cssSelector(".centeredFlex:nth-child(2) .jss311")).click();
     }
 
 }
