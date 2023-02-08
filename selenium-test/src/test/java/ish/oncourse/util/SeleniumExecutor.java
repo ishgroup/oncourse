39e2e853764ea4e4d828ac1afb8376488e694264
Kristina Trukhan <ktrukhan@objectstyle.com>
2023-02-08 01:39:48 +0100

    config

---
+++ selenium-test/src/test/java/ish/oncourse/util/SeleniumExecutor.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright ish group pty ltd 2023.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SeleniumExecutor {
+
+    public static WebElement findElement(WebDriver driver, By selector, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSeconds));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(selector));
+    }
+}
