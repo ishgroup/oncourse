cfba1386c388de45978f3983f6afa142059cd67f
dlitvinko <dlitvinko@objectstyle.com>
2023-03-15 13:16:08 +0300

    OD-17739 Added EWAY_TEST enum to PaymentGatewayType and payment.gateway.eway.api.key to preferences for save eWay API key and save windcave api key in payment.gateway.api.key (support 2 payment system and store 2 API keys for 2 different payment system)

--- types/src/main/java/ish/persistence/CommonPreferenceController.java
+++ types/src/main/java/ish/persistence/CommonPreferenceController.java
@@ -164,6 +164,14 @@ public abstract class CommonPreferenceController {
 		return apiKey;
 	}
 
+	public  String getPaymentGatewayPassEWay() {
+		String apiKey = getValue(PAYMENT_GATEWAY_PASS_EWAY, false);
+		if (apiKey == null) {
+			throw new IllegalArgumentException();
+		}
+		return apiKey;
+	}
+
 	/**
 	 * @return the timezone ID or null
 	 */
