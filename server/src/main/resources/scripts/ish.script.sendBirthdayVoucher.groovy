321ac97e98598967c3b83148555c0eef959ed697
dmitrij <dmitriy.tarasienko@gmail.com>
2023-03-24 16:18:29 +0300

    OD-18033 feat: improve performance of script, split batches for committing to 25 instead of 100 records, replace big request for contacts with repeatable by 100 records

--- server/src/main/resources/scripts/ish.script.sendBirthdayVoucher.groovy
+++ server/src/main/resources/scripts/ish.script.sendBirthdayVoucher.groovy
@@ -7,52 +7,62 @@ records = query {
 def product = records[0]
 def dataContext = product.context
 def counter = 0
-def request = new SQLTemplate(Contact.class, "SELECT * FROM Contact WHERE DAYOFYEAR(BIRTHDATE) BETWEEN DAYOFYEAR(NOW())+1 AND DAYOFYEAR(NOW())+7")
+def previousContactId = 0L
+def requestStr = "SELECT * FROM Contact WHERE DAYOFYEAR(BIRTHDATE) BETWEEN DAYOFYEAR(NOW())+1 AND DAYOFYEAR(NOW())+7 AND ID > %d LIMIT 100"
+def request = new SQLTemplate(Contact.class, String.format(requestStr, previousContactId))
 def contacts = dataContext.performQuery(request)
-contacts.each { contact ->
+previousContactId = contacts.empty ? previousContactId : contacts.last().id
 
-    //must create an invoice/invoiceline for voucher validation
-    Invoice invoice = dataContext.newObject(Invoice)
-    invoice.contact = contact
-    invoice.amountOwing = Money.ZERO
-    invoice.source = PaymentSource.SOURCE_ONCOURSE
-    invoice.confirmationStatus = ConfirmationStatus.DO_NOT_SEND
+while(!contacts.empty) {
+    contacts.each { contact ->
 
-    InvoiceLine invoiceLine = dataContext.newObject(InvoiceLine)
-    invoiceLine.invoice = invoice
-    invoiceLine.account = product.liabilityAccount
-    invoiceLine.tax = product.tax
-    invoiceLine.title = product.name
-    invoiceLine.priceEachExTax = Money.ZERO
-    invoiceLine.taxEach = Money.ZERO
-    invoiceLine.discountEachExTax = Money.ZERO
-    invoiceLine.quantity = 1
-    invoiceLine.description = "${contact.getName(true)} (${product.sku} ${product.name})"
+        //must create an invoice/invoiceline for voucher validation
+        Invoice invoice = dataContext.newObject(Invoice)
+        invoice.contact = contact
+        invoice.amountOwing = Money.ZERO
+        invoice.source = PaymentSource.SOURCE_ONCOURSE
+        invoice.confirmationStatus = ConfirmationStatus.DO_NOT_SEND
 
-    // Creates the voucher from the product
-    Voucher gift = dataContext.newObject(Voucher)
-    gift.product = product
-    gift.redeemableBy = contact
-    gift.code = SecurityUtil.generateVoucherCode()
-    gift.source = PaymentSource.SOURCE_ONCOURSE
-    gift.status = ProductStatus.ACTIVE
-    gift.invoiceLine = invoiceLine
-    gift.redemptionValue = product.value
-    gift.valueOnPurchase = product.value
-    gift.redeemedCourseCount = 0
-    gift.expiryDate = ProductUtil.calculateExpiryDate(ProductUtil.getToday(), product.expiryType, product.expiryDays)
-    gift.confirmationStatus = ConfirmationStatus.DO_NOT_SEND
+        InvoiceLine invoiceLine = dataContext.newObject(InvoiceLine)
+        invoiceLine.invoice = invoice
+        invoiceLine.account = product.liabilityAccount
+        invoiceLine.tax = product.tax
+        invoiceLine.title = product.name
+        invoiceLine.priceEachExTax = Money.ZERO
+        invoiceLine.taxEach = Money.ZERO
+        invoiceLine.discountEachExTax = Money.ZERO
+        invoiceLine.quantity = 1
+        invoiceLine.description = "${contact.getName(true)} (${product.sku} ${product.name})"
 
-    message {
-        template birthdayVoucherTemplate
-        record contact
-        voucher gift
+        // Creates the voucher from the product
+        Voucher gift = dataContext.newObject(Voucher)
+        gift.product = product
+        gift.redeemableBy = contact
+        gift.code = SecurityUtil.generateVoucherCode()
+        gift.source = PaymentSource.SOURCE_ONCOURSE
+        gift.status = ProductStatus.ACTIVE
+        gift.invoiceLine = invoiceLine
+        gift.redemptionValue = product.value
+        gift.valueOnPurchase = product.value
+        gift.redeemedCourseCount = 0
+        gift.expiryDate = ProductUtil.calculateExpiryDate(ProductUtil.getToday(), product.expiryType, product.expiryDays)
+        gift.confirmationStatus = ConfirmationStatus.DO_NOT_SEND
+
+        message {
+            template birthdayVoucherTemplate
+            record contact
+            voucher gift
+        }
+
+        if (++counter == 25) {
+            dataContext.commitChanges()
+            counter = 0
+        }
     }
 
-    if (++counter == 100) {
-        dataContext.commitChanges()
-        counter = 0
-    }
+    request = new SQLTemplate(Contact.class, String.format(requestStr, previousContactId))
+    contacts = dataContext.performQuery(request)
+    previousContactId = contacts.empty ? previousContactId : contacts.last().id
 }
 if (counter > 0) {
     dataContext.commitChanges()
