f7754eaa27bbeb2ff76887ea161dd544b440dc42
Yury Yasuchenya <yury@ish.com.au>
2023-05-15 17:08:31 +0200

    OD-18112 Fixed long tag names parsing

--- client-html/src/js/common/components/form/formFields/EditInPlaceQuerySelect.tsx
+++ client-html/src/js/common/components/form/formFields/EditInPlaceQuerySelect.tsx
@@ -121,7 +121,7 @@ const completeSuggestions = (
 
       variants = (operatorsFilter && operatorsFilter !== ENUM_CONSTRUCTOR_NAME) || !match ? [] : Object.keys(match);
 
-      if (!pathFilter && customFields) {
+      if (!pathFilter && !operatorsFilter && customFields) {
         variants = [...variants, ...customFields];
       }
 
@@ -611,7 +611,7 @@ class EditInPlaceQuerySelect extends React.PureComponent<EditInPlaceQueryFieldPr
         new RegExp((this.state.searchValue.match(/[+*()]/) ? "\\" : "") + this.state.searchValue + "$"),
         this.state.searchValue.match(/\s/) ? " " : ""
       )
-      + value[0].label
+      + value[0].value
       + (value[0].token === "SEPARATOR" || value[0].token === "'@'" || value[0].token === "'#'"
         ? ""
         : Entities[propType] && Entities[propType].constructor.name !== ENUM_CONSTRUCTOR_NAME
@@ -619,7 +619,7 @@ class EditInPlaceQuerySelect extends React.PureComponent<EditInPlaceQueryFieldPr
         : " ");
 
     if (value[0].queryPrefix) {
-      const tagStr = "#" + value[0].label;
+      const tagStr = "#" + value[0].value;
       inputValue = inputValue.replace(tagStr, `${value[0].queryPrefix} ${tagStr}`);
     }
 
