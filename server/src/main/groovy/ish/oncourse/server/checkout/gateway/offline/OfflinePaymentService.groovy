51d883858fda0e14949932ca86c949f249d4c87d
dlitvinko <dlitvinko@objectstyle.com>
2023-05-18 13:14:44 +0300

    OD-18135 Make error message in one format and fixed showing it on UI

--- server/src/main/groovy/ish/oncourse/server/checkout/gateway/offline/OfflinePaymentService.groovy
+++ server/src/main/groovy/ish/oncourse/server/checkout/gateway/offline/OfflinePaymentService.groovy
@@ -8,36 +8,38 @@
 
 package ish.oncourse.server.checkout.gateway.offline
 
+import ish.common.checkout.gateway.PaymentGatewayError
 import ish.common.checkout.gateway.SessionAttributes
 import ish.math.Money
 import ish.oncourse.server.api.checkout.Checkout
 import ish.oncourse.server.api.v1.model.CheckoutResponseDTO
+import ish.oncourse.server.api.v1.model.CheckoutValidationErrorDTO
 import ish.oncourse.server.cayenne.Contact
 import ish.oncourse.server.checkout.gateway.PaymentServiceInterface
 
 class OfflinePaymentService implements PaymentServiceInterface {
     @Override
     SessionAttributes createSession(String origin, Money amount, String merchantReference, Boolean storeCard, Contact contact) {
-        throw new IllegalArgumentException("Sorry, you can't use createSession method using Offline payment type. Please contact the administrator.")
+        handleError(PaymentGatewayError.PAYMENT_ERROR.errorNumber, [new CheckoutValidationErrorDTO(error: "Sorry, you cannot make a purchase. The credit card payment method is prohibited for the Offline payment system. Please contact the administrator.")])
     }
 
     @Override
     void succeedPaymentAndCompleteTransaction(CheckoutResponseDTO dtoResponse, Checkout checkout, Boolean sendInvoice, SessionAttributes sessionAttributes, Money amount, String merchantReference) {
-        throw new IllegalArgumentException("Sorry, you can't use succeedPaymentAndCompleteTransaction method using Offline payment type. Please contact the administrator.")
+        handleError(PaymentGatewayError.PAYMENT_ERROR.errorNumber, [new CheckoutValidationErrorDTO(error: "Sorry, you cannot make a purchase. The credit card payment method is prohibited for the Offline payment system. Please contact the administrator.")])
     }
 
     @Override
     SessionAttributes checkStatus(String sessionIdOrAccessCode) {
-        throw new IllegalArgumentException("Sorry, you can't use checkStatus method using Offline payment type. Please contact the administrator.")
+        handleError(PaymentGatewayError.PAYMENT_ERROR.errorNumber, [new CheckoutValidationErrorDTO(error: "Sorry, you cannot make a purchase. The credit card payment method is prohibited for the Offline payment system. Please contact the administrator.")])
     }
 
     @Override
     SessionAttributes makeRefund(Money amount, String merchantReference, String transactionId) {
-        throw new IllegalArgumentException("Sorry, you can't use makeRefund method using Offline payment type. Please contact the administrator.")
+        handleError(PaymentGatewayError.PAYMENT_ERROR.errorNumber, [new CheckoutValidationErrorDTO(error: "Sorry, you cannot make a purchase. The credit card payment method is prohibited for the Offline payment system. Please contact the administrator.")])
     }
 
     @Override
     SessionAttributes makeTransaction(Money amount, String merchantReference, String cardId) {
-        throw new IllegalArgumentException("Sorry, you can't use makeTransaction method using Offline payment type. Please contact the administrator.")
+        handleError(PaymentGatewayError.PAYMENT_ERROR.errorNumber, [new CheckoutValidationErrorDTO(error: "Sorry, you cannot make a purchase. The credit card payment method is prohibited for the Offline payment system. Please contact the administrator.")])
     }
 }
