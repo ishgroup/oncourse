de7b1e23be16f7f5ce001c5d8243698e350689e2
Yury Yasuchenya <yury@ish.com.au>
2023-06-28 14:49:15 +0200

    OD-18256 Fixed common unreachable app bar buttons issue. Adjusted common styles

--- client-html/src/js/containers/entities/courses/components/CourseEditView.tsx
+++ client-html/src/js/containers/entities/courses/components/CourseEditView.tsx
@@ -31,7 +31,7 @@ const items: TabsListItem[] = [
   },
   {
     label: "Notes",
-    component: ({ classes, ...rest }) => <OwnApiNotes {...rest} />
+    component: ({ classes, ...rest }) => <OwnApiNotes {...rest} className="pl-3 pr-3" />
   },
   {
     label: "Vet",
@@ -43,7 +43,7 @@ const items: TabsListItem[] = [
   }
 ];
 
-const styles = theme =>
+const styles = () =>
   createStyles({
     icon: {
       alignItems: "right"
