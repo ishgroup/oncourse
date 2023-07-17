7f8c36eae51274c377ea4327e3297be2a860430f
dmitrij <dmitriy.tarasienko@gmail.com>
2023-07-17 18:31:59 +0300

    OD-18326 feat: add check if shopping cart doesn't contain products or classes to default abandoned cart reminder

--- server/src/main/resources/scripts/ish.script.abandonedCartReminder.groovy
+++ server/src/main/resources/scripts/ish.script.abandonedCartReminder.groovy
@@ -2,6 +2,8 @@ records = query {
     entity "Checkout"
     query "createdOn yesterday"
 }
+
+records = records.findAll{!it.getShoppingCartProducts().empty || !it.getShoppingCartClasses().empty}
 message {
     template emailTemplate
     record records
