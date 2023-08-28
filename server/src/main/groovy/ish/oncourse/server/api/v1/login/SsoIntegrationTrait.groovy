910968f4034dc5b1433f94db629ee3614d47f3a6
dmitrij <dmitriy.tarasienko@gmail.com>
2023-08-28 09:01:25 +0300

    OD-18354 feat: okta sso initialization

---
+++ server/src/main/groovy/ish/oncourse/server/api/v1/login/SsoIntegrationTrait.groovy
@@ -0,0 +1,17 @@
+/*
+ * Copyright ish group pty ltd 2023.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ */
+
+package ish.oncourse.server.api.v1.login
+
+
+import ish.oncourse.server.integration.PluginTrait
+
+trait SsoIntegrationTrait extends PluginTrait {
+
+    abstract String getUserEmailByCode(String accessCode)
+}
