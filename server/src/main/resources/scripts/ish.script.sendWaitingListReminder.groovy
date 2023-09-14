19d6fc231f0d17bab5488272eb7aee2597e4a4ee
dmitrij <dmitriy.tarasienko@gmail.com>
2023-09-14 11:45:31 +0300

    OD-18484 feat: take in waitlist reminder only waiting lists, related to courses, that have a course class added during the last week

--- server/src/main/resources/scripts/ish.script.sendWaitingListReminder.groovy
+++ server/src/main/resources/scripts/ish.script.sendWaitingListReminder.groovy
@@ -1,6 +1,16 @@
-def waitingLists = query {
-    entity "WaitingList"
-}
+import ish.oncourse.server.cayenne.Course
+import ish.oncourse.server.cayenne.CourseClass
+import ish.oncourse.server.cayenne.WaitingList
+import org.apache.cayenne.query.ObjectSelect
+
+def updatedCoursesIds = ObjectSelect.columnQuery(Course, Course.ID)
+        .where(Course.COURSE_CLASSES.dot(CourseClass.CREATED_ON).gte(new Date().minus(7)))
+        .select(context)
+
+def waitingLists = ObjectSelect.query(WaitingList)
+        .where(WaitingList.COURSE.dot(Course.ID).in(updatedCoursesIds))
+        .select(context)
+
 waitingLists.each() { waitingList ->
     def classes = waitingList.course.courseClasses.findAll() { courseClass ->
         courseClass.isActive && courseClass.isShownOnWeb && courseClass.successAndQueuedEnrolments.size() < courseClass.maximumPlaces && (courseClass.isDistantLearningCourse || new Date() < courseClass.startDateTime)
