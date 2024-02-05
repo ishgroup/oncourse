fcd658e73c5470c88c6c6175c93daef4dec1173d
dmitrij <dmitriy.tarasienko@gmail.com>
2024-02-05 15:00:03 +0300

    OD-18696 feat: isClassComplete Aql attribute for enrolment with filtering by groove streams and performance of requests upgrade

--- server/src/main/java/ish/oncourse/aql/model/attribute/SyntheticEnrolmentIsClassCompletedNode.java
+++ server/src/main/java/ish/oncourse/aql/model/attribute/SyntheticEnrolmentIsClassCompletedNode.java
@@ -12,22 +12,29 @@
 package ish.oncourse.aql.model.attribute;
 
 import ish.common.types.AttendanceType;
+import ish.common.types.CourseClassType;
 import ish.common.types.EnrolmentStatus;
 import ish.oncourse.aql.impl.CompilationContext;
+import ish.oncourse.aql.impl.DateTimeInterval;
 import ish.oncourse.aql.impl.LazyExpressionNode;
-import ish.oncourse.server.cayenne.CourseClass;
-import ish.oncourse.server.cayenne.Enrolment;
+import ish.oncourse.aql.impl.converter.LazyDateTimeScalar;
+import ish.oncourse.server.api.v1.function.EnrolmentFunctions;
+import ish.oncourse.server.cayenne.*;
 import ish.oncourse.server.cayenne.glue._Enrolment;
 import org.apache.cayenne.exp.parser.*;
+import org.apache.cayenne.query.ColumnSelect;
 import org.apache.cayenne.query.ObjectSelect;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
-
+ *
  */
 class SyntheticEnrolmentIsClassCompletedNode extends LazyExpressionNode {
 
@@ -50,40 +57,70 @@ class SyntheticEnrolmentIsClassCompletedNode extends LazyExpressionNode {
 
         var pathNode = (ASTObjPath) args.get(1);
         var path = pathNode.getPath();
-        var index = path.indexOf("." +  "isClassCompleted");
+        var index = path.indexOf("." + "isClassCompleted");
         var prefix = index <= 0 ? "" : path.substring(0, index);
 
         Boolean value;
-        if (args.get(2) instanceof ASTScalar &&  ((ASTScalar) args.get(2)).getValue() instanceof Boolean) {
+        if (args.get(2) instanceof ASTScalar && ((ASTScalar) args.get(2)).getValue() instanceof Boolean) {
             value = (Boolean) ((ASTScalar) args.get(2)).getValue();
         } else {
             throw new IllegalArgumentException("Invalid search expression, can not convert " + args.get(2) + " to Boolean value");
         }
 
-        if (!value) {
-            parent = (parent instanceof ASTEqual) ? new ASTNotIn() : new ASTIn();
+        var isClassCompletedNode = new ASTAnd();
+
+        prefix = prefix.isEmpty() ? "" : prefix + ".";
+        var statusCheckNode = new ASTEqual();
+        statusCheckNode.jjtAddChild(new ASTObjPath(prefix + "status"), 0);
+        statusCheckNode.jjtAddChild(new ASTScalar(EnrolmentStatus.SUCCESS.getDatabaseValue()), 1);
+
+        var endDateCheck = new ASTLess();
+        endDateCheck.jjtAddChild(new ASTObjPath(prefix + "courseClass.endDateTime"), 0);
+        endDateCheck.jjtAddChild(new ASTScalar(LocalDateTime.now()), 1);
+
+        var hybridMainCheck = new ASTOr();
+
+        var notHybridCheck = new ASTNotEqual();
+        notHybridCheck.jjtAddChild(new ASTObjPath(prefix + "courseClass.type"), 0);
+        notHybridCheck.jjtAddChild(new ASTScalar(CourseClassType.HYBRID.getDatabaseValue()), 1);
+
+        long basicEnrolmentId = -1L;
+        List<Enrolment> enrolments;
+        Set<Long> enrolmentIds = new HashSet<>();
+        do {
+            enrolments = ObjectSelect.query(Enrolment.class)
+                    .where(Enrolment.ID.gt(basicEnrolmentId).andExp(Enrolment.STATUS.eq(EnrolmentStatus.SUCCESS)).andExp(
+                                    Enrolment.COURSE_CLASS.dot(CourseClass.END_DATE_TIME).isNotNull())
+                            .andExp(Enrolment.COURSE_CLASS.dot(CourseClass.END_DATE_TIME).lt(new Date()))
+                            .andExp(Enrolment.COURSE_CLASS.dot(CourseClass.TYPE).eq(CourseClassType.HYBRID))
+                    )
+                    .limit(200)
+                    .orderBy(Enrolment.ID.getName())
+                    .select(ctx.getContext());
+
+            enrolmentIds.addAll(EnrolmentFunctions.filterEnrolmentsWithCompletedClasses(enrolments));
+            basicEnrolmentId = !enrolments.isEmpty() ? enrolments.get(enrolments.size() - 1).getId() : basicEnrolmentId;
+        } while (!enrolments.isEmpty());
+
+        Node hybridCheck;
+        if (!enrolmentIds.isEmpty()) {
+            hybridCheck = new ASTIn();
+            ASTObjPath astObjPath = new ASTObjPath(prefix + "." + "id");
+            hybridCheck.jjtAddChild(astObjPath, 0);
+            hybridCheck.jjtAddChild(new ASTList(enrolmentIds), 1);
         } else {
-            parent = (parent instanceof ASTEqual) ? new ASTIn() : new ASTNotIn();
+            hybridCheck = new ASTFalse();
         }
 
-        var enrolments = ObjectSelect.query(Enrolment.class)
-                .where(Enrolment.STATUS.eq(EnrolmentStatus.SUCCESS).andExp(
-                        Enrolment.COURSE_CLASS.dot(CourseClass.END_DATE_TIME).isNotNull()
-                                .andExp(Enrolment.COURSE_CLASS.dot(CourseClass.END_DATE_TIME).lt(new Date()))
-                ))
-                .select(ctx.getContext());
+        hybridMainCheck.jjtAddChild(notHybridCheck, 0);
+        hybridMainCheck.jjtAddChild(hybridCheck, 1);
 
-        Set<Long> enrolmentIds = enrolments.stream().filter(enrolment -> !enrolment.getCourseClass().getIsHybrid() ||
-                enrolment.getCourseClass().getIsHybrid() && enrolment.getAttendances().stream()
-                        .filter(attendance -> attendance.getAttendanceType().equals(AttendanceType.ATTENDED))
-                        .count() >= enrolment.getCourseClass().getMinimumSessionsToComplete() )
-                .map(_Enrolment::getId)
-                .collect(Collectors.toSet());
+        isClassCompletedNode.jjtAddChild(statusCheckNode, 0);
+        isClassCompletedNode.jjtAddChild(endDateCheck, 1);
+        isClassCompletedNode.jjtAddChild(hybridMainCheck, 2);
 
-        //List<Enrolment> foundEnrolments = enrolmentIds.stream().filter(enrolment -> !enrolment.getDisplayStatus().equals("Complete")).collect(Collectors.toList());
-        ASTObjPath astObjPath = prefix.isBlank() ? new ASTObjPath("id") : new ASTObjPath(prefix + "." + "id");
-        parent.jjtAddChild(astObjPath, 0);
-        parent.jjtAddChild(new ASTList(enrolmentIds), 1);
+        parent.jjtAddChild(isClassCompletedNode, 0);
+        parent.jjtAddChild(args.get(2), 1);
         return parent;
     }
 
