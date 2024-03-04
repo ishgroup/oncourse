f78d925331432a51057ac67e95225bb721db0dfc
dmitrij <dmitriy.tarasienko@gmail.com>
2024-03-04 17:02:29 +0300

    OD-18747 fix: removing of related overridden subdomain to removed portal instance

--- server/src/main/groovy/ish/oncourse/server/cayenne/PortalWebsite.groovy
+++ server/src/main/groovy/ish/oncourse/server/cayenne/PortalWebsite.groovy
@@ -19,18 +19,19 @@ import org.apache.cayenne.query.ObjectSelect
 @QueueableEntity
 class PortalWebsite extends _PortalWebsite implements Queueable {
 
-	@Override
-	boolean isAsyncReplicationAllowed() {
-		return false
-	}
+    @Override
+    boolean isAsyncReplicationAllowed() {
+        return false
+    }
 
-	@Override
-	protected void preRemove() {
-		def customFields = ObjectSelect.query(CustomField)
-				.where(CustomField.CUSTOM_FIELD_TYPE.dot(CustomFieldType.DATA_TYPE).eq(DataType.PORTAL_SUBDOMAIN))
-				.select(context)
+    @Override
+    protected void preRemove() {
+        def customFields = ObjectSelect.query(CustomField)
+                .where(CustomField.CUSTOM_FIELD_TYPE.dot(CustomFieldType.DATA_TYPE).eq(DataType.PORTAL_SUBDOMAIN)
+                        .andExp(CustomField.VALUE.eq(this.subDomain)))
+                .select(context)
 
-		if(!customFields.empty)
-			context.deleteObjects(customFields)
-	}
+        if (!customFields.empty)
+            context.deleteObjects(customFields)
+    }
 }
