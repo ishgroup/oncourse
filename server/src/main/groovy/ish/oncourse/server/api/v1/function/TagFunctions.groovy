11e0e04a1a8a00038edef283e922945878787d26
dmitrij <dmitriy.tarasienko@gmail.com>
2024-05-24 17:55:27 +0300

    OD-19032 fix: change validation of tag requirements when extended search types allowed to be attended to subject tags only

--- server/src/main/groovy/ish/oncourse/server/api/v1/function/TagFunctions.groovy
+++ server/src/main/groovy/ish/oncourse/server/api/v1/function/TagFunctions.groovy
@@ -150,7 +150,7 @@ class TagFunctions {
             return new ValidationErrorDTO(tag.id?.toString(), 'name', 'Name should be unique.')
         }
 
-        if (subjectsAsEntity) {
+        if (NodeSpecialType.SUBJECTS == dbTag.getSpecialType() && subjectsAsEntity) {
             error = validateSubjectAsEntity(tag, dbTag)
             if (error)
                 return error
