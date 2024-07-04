94ec19c4b75dadc8b9ed99888df180badbcda84f
Yury Yasuchenya <yury@ish.com.au>
2024-07-04 10:12:37 +0200

    Revert "OD-19159 Disabled Send message function"
    This reverts commit e46c95a1c2323f12120d894f4e9d93546b2ebc01.

--- client-html/src/js/common/components/list-view/components/bottom-app-bar/components/SendMessageMenu.tsx
+++ client-html/src/js/common/components/list-view/components/bottom-app-bar/components/SendMessageMenu.tsx
@@ -1,5 +1,4 @@
 import MenuItem from "@mui/material/MenuItem";
-import Tooltip from "@mui/material/Tooltip";
 import React, { useCallback, useMemo } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
@@ -25,13 +24,9 @@ const SendMessageMenu: React.FC<SendMessageMenuProps> = props => {
   }, [selection, entity]);
 
   return (
-    <Tooltip title="Service is temporarily unavailable. We are already working on a solution to enable it as soon as possible">
-      <div>
-        <MenuItem disabled className="listItemPadding" onClick={onSendMessage}>
-          Send message
-        </MenuItem>
-      </div>
-    </Tooltip>
+    <MenuItem disabled={isNew} className="listItemPadding" onClick={onSendMessage}>
+      Send message
+    </MenuItem>
   );
 };
 
