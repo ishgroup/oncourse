99745887f5de2a78540756df94bcac09a4324778
dmitrij <dmitriy.tarasienko@gmail.com>
2025-01-29 23:08:32 +0300

    SST-66 feat: notification url for windcave

--- server/src/main/groovy/ish/oncourse/server/checkout/gateway/windcave/WindcavePaymentAPI.groovy
+++ server/src/main/groovy/ish/oncourse/server/checkout/gateway/windcave/WindcavePaymentAPI.groovy
@@ -18,6 +18,8 @@ import groovyx.net.http.HttpResponseDecorator
 import groovyx.net.http.HttpResponseException
 import ish.common.checkout.gateway.SessionAttributes
 import ish.common.checkout.gateway.windcave.WindcaveResponseCode
+import ish.oncourse.server.api.servlet.ISessionManager
+import ish.oncourse.server.checkout.CheckoutUtils
 import ish.util.DateFormatter
 import org.apache.commons.lang3.time.DateUtils
 
@@ -47,6 +49,9 @@ class WindcavePaymentAPI {
 
     @Inject
     PreferenceController preferenceController
+
+    @Inject
+    private ISessionManager sessionManager
 
     Closure failHandler =  { Object response, Object body, SessionAttributes attributes  ->
         logger.error('Fail to create session {} {}', body, response)
@@ -88,7 +93,8 @@ class WindcavePaymentAPI {
                                                 approved :  origin + '/checkout?paymentStatus=success',
                                                 declined :  origin + '/checkout?paymentStatus=fail',
                                                 cancelled:  origin + '/checkout?paymentStatus=cancel'
-                                            ]
+                                            ],
+                                        notificationUrl: sessionManager.host + CheckoutUtils.SERVER_REDIRECT_PATH+"?xPaymentSessionId=$merchantReference"
                                         ]
             if (storeCard) {
                 body['storeCard'] = true
