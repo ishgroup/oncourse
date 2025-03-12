bd0f118afb7368623e84af5cf98e656c119b368f
Yury Yasuchenya <yury@ish.com.au>
2025-03-12 13:10:03 +0100

    CS-88 Added loading indicator for checkout summary

--- client-html/src/js/containers/checkout/epics/summary/EpicUpdateSummaryPrices.ts
+++ client-html/src/js/containers/checkout/epics/summary/EpicUpdateSummaryPrices.ts
@@ -19,7 +19,6 @@ import { getCheckoutModel, getCheckoutModelMembershipsValidTo } from '../../util
 
 const request: EpicUtils.Request = {
   type: CHECKOUT_UPDATE_SUMMARY_PRICES,
-  hideLoadIndicator: true,
   getData: async (p, s) => {
     const model = getCheckoutModel(s, true);
     await getCheckoutModelMembershipsValidTo(model);
