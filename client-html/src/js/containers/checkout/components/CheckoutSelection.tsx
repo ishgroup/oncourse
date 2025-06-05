f7fdae12cc248a7cbecd4e6827d86a781cd452eb
Yury Yasuchenya <yury@ish.com.au>
2025-06-05 14:45:14 +0300

    SST-169 Fixed ResizeObserver loop error

--- client-html/src/js/containers/checkout/components/CheckoutSelection.tsx
+++ client-html/src/js/containers/checkout/components/CheckoutSelection.tsx
@@ -684,14 +684,14 @@ const CheckoutSelectionForm = React.memo<Props>(props => {
     (...props) => {
       setSidebarWidth(props[2].getClientRects()[0].width);
     },
-    [sidebarWidth]
+    []
   );
 
   const handleResizeStopCallback = useCallback(
     (...props) => {
       updateColumnsWidth(props[2].getClientRects()[0].width);
     },
-    [sidebarWidth]
+    []
   );
 
   const onClearContactsSearch = useCallback(() => {
@@ -1020,6 +1020,7 @@ const CheckoutSelectionForm = React.memo<Props>(props => {
   return (
     <div className={clsx("root", classes.root)}>
       <ResizableWrapper
+        maxWidth="60%"
         onResizeStop={handleResizeStopCallback}
         onResize={handleResizeCallback}
         sidebarWidth={sidebarWidth || SIDEBAR_DEFAULT_WIDTH}
