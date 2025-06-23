065e4acf46e2cb9797746a06c5b566d7311c53c4
Yury Yasuchenya <yury@ish.com.au>
2025-06-23 16:55:15 +0200

    Reapply "CS-107 Fixed zero tax rate issues on class edit view"
    This reverts commit 9348ed01c12c900dc33449063ebbf5f69e2da2a2.

--- client-html/src/js/containers/entities/common/utils/index.ts
+++ client-html/src/js/containers/entities/common/utils/index.ts
@@ -3,10 +3,20 @@
  * No copying or use of this code is allowed without permission in writing from ish.
  */
 
-import { Account, Course, EntityRelationType, Module, Qualification, Sale, SaleType } from '@api/model';
+import {
+  Account,
+  ArticleProduct,
+  Course,
+  EntityRelationType, MembershipProduct,
+  Module,
+  Qualification,
+  Sale,
+  SaleType,
+  Tax
+} from '@api/model';
 import { format } from 'date-fns';
 import { EEE_D_MMM_YYYY } from 'ish-ui';
-import { initialize } from 'redux-form';
+import { change, initialize } from 'redux-form';
 import {
   checkPermissionsRequestFulfilled,
   clearActionsQueue,
@@ -17,6 +27,7 @@ import { getNoteItems } from '../../../../common/components/form/notes/actions';
 import { getRecords, SET_LIST_EDIT_RECORD, setListSelection } from '../../../../common/components/list-view/actions';
 import { LIST_EDIT_VIEW_FORM_NAME } from '../../../../common/components/list-view/constants';
 import AccessService from '../../../../common/services/AccessService';
+import { getFeeExTaxByFeeIncTax, getTotalByFeeExTax } from '../../../../common/utils/financial';
 import { NOTE_ENTITIES } from '../../../../constants/Config';
 import { AccessByPath, EntityName, ListActionEntity } from '../../../../model/entities/common';
 import { EntityRelationTypeRendered } from '../../../../model/entities/EntityRelations';
@@ -286,4 +297,29 @@ export const getAccessesByPath = async (pathes: string[], state: State, method =
   }
 
   return accesses;
+};
+
+// Products financial fields handlers
+export const handleChangeProductFeeExTax = (taxRate, dispatch, form) => value => {
+  dispatch(change(form, "totalFee", getTotalByFeeExTax(taxRate, value)));
+};
+
+export const handleChangeProductFeeIncTax = (taxRate, dispatch, form) => value => {
+  dispatch(change(form, "feeExTax", getFeeExTaxByFeeIncTax(taxRate, value)));
+};
+
+export const handleChangeProductTax = (taxes: Tax[], dispatch, form, feeExTax) => value => {
+  const tax = taxes.find(item => item.id === value);
+  const taxRate = tax ? tax.rate : 0;
+  dispatch(change(form, "totalFee", getTotalByFeeExTax(taxRate, feeExTax)));
+};
+
+export const handleChangeProductAccount = (values: ArticleProduct | MembershipProduct, taxes: Tax[], accounts: Account[], dispatch, form) => value => {
+  const account = accounts.find(item => item.id === value);
+  const tax = taxes.find(item => item.id === Number(account["tax.id"]));
+  if (tax.id !== values.taxId) {
+    const taxRate = tax ? tax.rate : 0;
+    dispatch(change(form, "taxId", tax.id));
+    dispatch(change(form, "totalFee", getTotalByFeeExTax(taxRate, values.feeExTax)));
+  }
 };
