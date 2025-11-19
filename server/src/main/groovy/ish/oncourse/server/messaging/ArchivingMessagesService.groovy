8bdc6999a4f3e6096ac6c60c18d740f561df7c32
dmitrij <dmitriy.tarasienko@gmail.com>
2025-11-19 13:16:36 +0300

    OD-19046 feat: remove archive entity, add upgrade to archive messages after new version starts

--- server/src/main/groovy/ish/oncourse/server/messaging/ArchivingMessagesService.groovy
+++ server/src/main/groovy/ish/oncourse/server/messaging/ArchivingMessagesService.groovy
@@ -15,7 +15,6 @@ import ish.common.types.AttachmentInfoVisibility
 import ish.oncourse.server.ICayenneService
 import ish.oncourse.server.PreferenceController
 import ish.oncourse.server.api.validation.EntityValidator
-import ish.oncourse.server.cayenne.Archive
 import ish.oncourse.server.cayenne.Message
 import ish.oncourse.server.document.ArchiveService
 import ish.oncourse.server.document.DocumentService
@@ -26,7 +25,6 @@ import ish.oncourse.types.AuditAction
 import ish.s3.AmazonS3Service
 import org.apache.cayenne.query.ObjectSelect
 import org.apache.cayenne.query.SQLTemplate
-import org.apache.cayenne.query.SelectById
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
@@ -36,15 +34,13 @@ import java.sql.ResultSet
 import java.sql.Statement
 import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.time.temporal.ChronoUnit
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.zip.GZIPOutputStream
 
 class ArchivingMessagesService {
-    private static final long MIN_YEARS_BEFORE_TO_ARCHIVE = 3
     private static final String TEMP_ARCHIVES_DIRECTORY = "temp-messages-archives"
     private static final SimpleDateFormat SQL_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd")
-    private static final SimpleDateFormat FILE_NAME_DATE_FORMAT = new SimpleDateFormat("MM_yyyy")
+    private static final SimpleDateFormat FILE_NAME_DATE_FORMAT = new SimpleDateFormat("yyyy_MM_dd")
 
     private static AtomicBoolean archiveInProgress = new AtomicBoolean(false)
     private static final Logger logger = LogManager.logger
@@ -75,11 +71,6 @@ class ArchivingMessagesService {
     }
 
     void archiveMessages(LocalDate localDateToArchive) {
-        def yearsBetween = ChronoUnit.YEARS.between(localDateToArchive, LocalDate.now())
-        if (yearsBetween < MIN_YEARS_BEFORE_TO_ARCHIVE) {
-            validator.throwClientErrorException("archiveDate", "You cannot archive all messages before date, less then $MIN_YEARS_BEFORE_TO_ARCHIVE year ago")
-        }
-
         def dateToArchive = localDateToArchive.toDate()
 
         while (archiveInProgress.get() == Boolean.TRUE)
@@ -103,21 +94,12 @@ class ArchivingMessagesService {
             logger.warn("Full start time of archiving: " + System.currentTimeMillis())
 
             String fileName = buildCompressedCsvForExcludeIntervalAndGetFilename(firstMessage.createdOn, dateToArchive)
-            String uuid = uploadArchiveAndGetKey(fileName)
+            uploadArchive(fileName)
             new File("$TEMP_ARCHIVES_DIRECTORY/$fileName").delete()
             removeMessagesCreatedInInterval(dateToArchive)
 
             preferenceController.setDateMessageBeforeArchived(dateToArchive)
-
-            def archive = cayenneService.newContext.newObject(Archive)
-            archive.createdOn = new Date()
-            archive.fileName = fileName
-            archive.uniqueCode = uuid
-            archive.dateFrom = firstMessage.createdOn
-            archive.dateTo = dateToArchive
-            archive.context.commitChanges()
-
-            auditService.submit(archive, AuditAction.MESSAGES_ARCHIVING_COMPLETED, "Messages before $dateToArchive archived successfully")
+            auditService.submit(firstMessage, AuditAction.MESSAGES_ARCHIVING_COMPLETED, "Messages before $dateToArchive archived successfully")
             logger.warn("Full end time of archiving: " + System.currentTimeMillis())
         } catch (Exception e) {
             logger.catching(e)
@@ -185,7 +167,7 @@ class ArchivingMessagesService {
     private String getFreeFileName(String collegeName, Date startDate, Date endDate) {
         def formattedStart = FILE_NAME_DATE_FORMAT.format(startDate)
         def formattedEnd = FILE_NAME_DATE_FORMAT.format(endDate)
-        String fileName = "$collegeName-$formattedStart-$formattedEnd" + ".csv.gz"
+        String fileName = "$collegeName/$formattedStart-$formattedEnd" + ".csv.gz"
 
         def file = new File(TEMP_ARCHIVES_DIRECTORY + "/" + fileName)
         if (file.exists())
@@ -198,22 +180,15 @@ class ArchivingMessagesService {
         return fileName
     }
 
-    private String uploadArchiveAndGetKey(String fileName) {
+    private String uploadArchive(String fileName) {
         def file = new File("$TEMP_ARCHIVES_DIRECTORY/$fileName")
         def inputStream = new FileInputStream(file)
-        String key = UUID.randomUUID().toString()
-        s3Service.putFileFromStream(key, fileName, inputStream, AttachmentInfoVisibility.PRIVATE, (int) file.length())
-        return key
+        s3Service.putFileFromStream(fileName, fileName, inputStream, AttachmentInfoVisibility.PRIVATE, (int) file.length())
     }
 
     private void removeMessagesCreatedInInterval(Date endDate) {
         SQLTemplate sqlTemplate = new SQLTemplate(Message.class, "Delete from Message where createdOn < '${SQL_DATE_FORMAT.format(endDate)}'")
         cayenneService.newContext.performGenericQuery(sqlTemplate)
-    }
-
-    String getLink(Long archiveId) {
-        def archive = SelectById.query(Archive, archiveId).selectOne(cayenneService.newReadonlyContext)
-        return s3Service.getFileUrl(archive.uniqueCode, AttachmentInfoVisibility.PRIVATE)
     }
 
 }
