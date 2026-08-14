d964f9ddd00f0ef171d6562ae70cbbf1d6000342
Dmitriy Tarasenko <dtarasenko@objectstyle.com>
2026-08-14 13:31:57 +0300

    fix: datasets and wrong tests scenarios; add audit service test

---
+++ server/src/test/groovy/ish/oncourse/server/services/AuditServiceTest.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright ish group pty ltd 2020.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+package ish.oncourse.server.services
+
+import groovy.transform.CompileStatic
+import ish.oncourse.server.ICayenneService
+import org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import static org.mockito.Mockito.mock
+
+@CompileStatic
+class AuditServiceTest {
+
+    @Test
+    void testShutdownDoesNotThrow() {
+        ICayenneService cayenneService = mock(ICayenneService)
+        ISystemUserService systemUserService = mock(ISystemUserService)
+
+        AuditService service = new AuditService(cayenneService, systemUserService)
+
+        assertDoesNotThrow({ -> service.shutdown() })
+    }
+
+    @Test
+    void testShutdownCanBeCalledMultipleTimes() {
+        ICayenneService cayenneService = mock(ICayenneService)
+        ISystemUserService systemUserService = mock(ISystemUserService)
+
+        AuditService service = new AuditService(cayenneService, systemUserService)
+
+        assertDoesNotThrow({ ->
+            service.shutdown()
+            service.shutdown()
+        })
+    }
+}
